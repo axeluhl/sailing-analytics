06fe20eb385c9b9131b7e85f1bc7285293cd204c
Axel Uhl <axel.uhl@sap.com>
2011-05-02 16:34:02 +0200

    First trivial test case

---
+++ com.sap.sailing.base.test/src/com/sap/sailing/base/test/PositionTests.scala
@@ -0,0 +1,12 @@
+package com.sap.sailing.base.test
+
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+
+class PositionTests extends AssertionsForJUnit {
+	@Test def simpleTest() {
+	  assertTrue(true);
+	}
+}
