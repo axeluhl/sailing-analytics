5bfc14c7dd063ea68586e15801940862a6eaab30
Axel Uhl <axel.uhl@sap.com>
2011-05-11 10:44:14 +0200

    added TracTrac client module as an OSGi-wrapped bundle, added test project

--- com.sap.sailing.domain/src/com/sap/sailing/domain/standings/RaceStandingsSnapshot.java
+++ com.sap.sailing.domain/src/com/sap/sailing/domain/standings/RaceStandingsSnapshot.java
@@ -5,11 +5,17 @@ import com.sap.sailing.domain.base.Leg;
 import com.sap.sailing.domain.base.Position;
 import com.sap.sailing.domain.base.RaceDefinition;
 import com.sap.sailing.domain.base.Timed;
+import com.sap.sailing.domain.base.Waypoint;
 
 public interface RaceStandingsSnapshot extends Timed {
 	RaceDefinition getRace();
 	Position getLastKnownPosition(Boat boat);
 	boolean hasStarted(Boat boat);
 	boolean hasFinished(Boat boat);
-	Leg getLastKnownLeg(Boat boat);
+	Leg getCurrentLeg(Boat boat);
+	
+	/**
+	 * @return 0 in case the boat didn't pass <code>mark</code> yet
+	 */
+	int getRankAtMark(Boat boat, Waypoint mark);
 }
