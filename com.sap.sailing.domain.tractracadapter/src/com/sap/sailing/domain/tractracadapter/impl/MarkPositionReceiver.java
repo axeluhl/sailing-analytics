64a471f0b2bff12ac244d3d4929bc6784a284876
Axel Uhl <axel.uhl@sap.com>
2011-06-08 12:38:02 +0200

    implemented future pattern for trackedRace receiving in MarkPositionReceiver.
    This was now necessary because the RaceDefinition and therefore the TrackedRace
    only becomes available after the RaceCourseDefinition has been received. However,
    this shouldn't block the MarkPositionReceiver's constructor which may be invoked
    before the RaceCourseReceiver becomes active.

--- com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/impl/MarkPositionReceiver.java
+++ com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/impl/MarkPositionReceiver.java
@@ -2,6 +2,7 @@ package com.sap.sailing.domain.tractracadapter.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.maptrack.client.io.TypeController;
 import com.sap.sailing.domain.base.Buoy;
@@ -21,28 +22,53 @@ import com.tractrac.clientmodule.data.ControlPointPositionData;
 import com.tractrac.clientmodule.data.ICallbackData;
 
 /**
- * The positions of the {@link ControlPoint}s of a {@link Course} are received
- * dynamically through a callback interface. Therefore, when connected to an
- * {@link Event}, and even after receiving the order of the marks for a race
- * course, these orders are not yet defined. An instance of this class
- * can be used to create the listeners needed to receive this information and
- * set it on an {@link Event}'s {@link ControlPoint}s.
+ * The positions of the {@link ControlPoint}s of a {@link Course} are received dynamically through a callback interface.
+ * Therefore, when connected to an {@link Event}, and even after receiving the order of the marks for a race course,
+ * these orders are not yet defined. An instance of this class can be used to create the listeners needed to receive
+ * this information and set it on an {@link Event}'s {@link ControlPoint}s.
+ * <p>
+ * 
+ * As a {@link MarkPositionReceiver} requires a tracked race in order to update the mark positions received, and since a
+ * {@link TrackedRace} depends on the {@link RaceDefinition} which in turn depends on having received the race course
+ * description, at least a {@link RaceCourseReceiver} has to be active as well. Otherwise, the first call to
+ * {@link #getTrackedRace} will block forever.
  * 
  * @author Axel Uhl (d043530)
  * 
  */
 public class MarkPositionReceiver extends AbstractReceiverWithQueue<ControlPoint, ControlPointPositionData, Boolean> {
-    private final TrackedRace trackedRace;
+    private static final Logger logger = Logger.getLogger(MarkPositionReceiver.class.getName());
+    
+    private TrackedRace trackedRace;
     private final com.tractrac.clientmodule.Event tractracEvent;
     private int received;
     
-    public MarkPositionReceiver(TrackedEvent trackedEvent, com.tractrac.clientmodule.Event tractracEvent, DomainFactory domainFactory) {
+    public MarkPositionReceiver(final TrackedEvent trackedEvent, com.tractrac.clientmodule.Event tractracEvent, final DomainFactory domainFactory) {
         super();
         this.tractracEvent = tractracEvent;
         // assumption: there is currently only one race per TracTrac Event object
-        Race race = tractracEvent.getRaceList().iterator().next();
-        RaceDefinition raceDefinition = domainFactory.getRaceDefinition(race);
-        this.trackedRace = trackedEvent.getTrackedRace(raceDefinition);
+        final Race race = tractracEvent.getRaceList().iterator().next();
+        new Thread("MarkPositionReceiver waiting for RaceDefinition for "+race.getName()) {
+            public void run() {
+                RaceDefinition raceDefinition = domainFactory.getRaceDefinition(race);
+                TrackedRace blockingTrackedRace = trackedEvent.getTrackedRace(raceDefinition);
+                synchronized(MarkPositionReceiver.this) {
+                    trackedRace = blockingTrackedRace;
+                    MarkPositionReceiver.this.notifyAll();
+                }
+            }
+        }.start();
+    }
+    
+    private synchronized TrackedRace getTrackedRaceBlocking() {
+        while (trackedRace == null) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                logger.warning("Interrupted wait for trackedRace: "+e.getMessage()+". Continuing to wait.");
+            }
+        }
+        return trackedRace;
     }
 
     /**
@@ -75,7 +101,7 @@ public class MarkPositionReceiver extends AbstractReceiverWithQueue<ControlPoint
             }
         }
         Buoy buoy = DomainFactory.INSTANCE.getBuoy(event.getA(), event.getB());
-        ((DynamicTrack<Buoy, GPSFix>) trackedRace.getTrack(buoy)).addGPSFix(DomainFactory.INSTANCE
+        ((DynamicTrack<Buoy, GPSFix>) getTrackedRaceBlocking().getTrack(buoy)).addGPSFix(DomainFactory.INSTANCE
                 .createGPSFixMoving(event.getB()));
     }
 
