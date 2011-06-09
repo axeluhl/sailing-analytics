558a01683d8119ea8b10bc83eb1f266507b8ec1c
Axel Uhl <axel.uhl@sap.com>
2011-06-10 00:00:44 +0200

    made abstract, avoiding JUnit / Maven to try to instantiate it

--- com.sap.sailing.declination.test/src/com/sap/sailing/declination/test/AbstractDeclinationTest.java
+++ com.sap.sailing.declination.test/src/com/sap/sailing/declination/test/AbstractDeclinationTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 
 import com.sap.sailing.declination.impl.NOAAImporterForTesting;
 
-public class AbstractDeclinationTest {
+public abstract class AbstractDeclinationTest {
     protected NOAAImporterForTesting importer;
     protected final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
     
