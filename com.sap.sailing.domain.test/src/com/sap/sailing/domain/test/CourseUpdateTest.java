6b5cb8ecf1ad86c13ca89f26b51ebc8eabb936d4
Axel Uhl <axel.uhl@sap.com>
2011-06-10 10:23:19 +0200

    preparing a test for course updates

---
+++ com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/CourseUpdateTest.java
@@ -0,0 +1,49 @@
+package com.sap.sailing.domain.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sap.sailing.domain.base.Course;
+import com.sap.sailing.domain.base.Event;
+import com.sap.sailing.domain.base.RaceDefinition;
+import com.sap.sailing.domain.tracking.DynamicTrackedEvent;
+import com.sap.sailing.domain.tracking.impl.EmptyWindStore;
+import com.sap.sailing.domain.tractracadapter.DomainFactory;
+import com.sap.sailing.domain.tractracadapter.Receiver;
+import com.sap.sailing.domain.tractracadapter.ReceiverType;
+import com.sap.sailing.util.Util;
+
+public class CourseUpdateTest extends AbstractTracTracLiveTest {
+    private Course course;
+    private Event domainEvent;
+    private DynamicTrackedEvent trackedEvent;
+    
+    public CourseUpdateTest() throws URISyntaxException, MalformedURLException {
+        super();
+    }
+
+    @Before
+    public void setUp() throws MalformedURLException, IOException, InterruptedException {
+        super.setUp();
+        domainEvent = DomainFactory.INSTANCE.createEvent(getEvent());
+        trackedEvent = DomainFactory.INSTANCE.trackEvent(domainEvent);
+        Iterable<Receiver> myReceivers = DomainFactory.INSTANCE.getUpdateReceivers(trackedEvent, getEvent(),
+                EmptyWindStore.INSTANCE, ReceiverType.RACECOURSE);
+        addListenersForStoredDataAndStartController(myReceivers);
+        RaceDefinition race = DomainFactory.INSTANCE.getRaceDefinition(getEvent().getRaceList().iterator().next());
+        course = race.getCourse();
+    }
+    
+    @Test
+    public void testLastWaypointRemoved() {
+        assertNotNull(course);
+        assertEquals(3, Util.size(course.getWaypoints()));
+    }
+}
