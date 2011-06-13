9b4ca2fe6aaddf7588376e5dee640f21931ffb9e
Axel Uhl <axel.uhl@sap.com>
2011-06-13 19:51:21 +0200

    added missing files

---
+++ java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/impl/RaceHandleImpl.java
@@ -0,0 +1,36 @@
+package com.sap.sailing.domain.tractracadapter.impl;
+
+import com.sap.sailing.domain.base.RaceDefinition;
+import com.sap.sailing.domain.tracking.DynamicTrackedEvent;
+import com.sap.sailing.domain.tractracadapter.DomainFactory;
+import com.sap.sailing.domain.tractracadapter.RaceHandle;
+import com.tractrac.clientmodule.Event;
+
+public class RaceHandleImpl implements RaceHandle {
+    private final Event tractracEvent;
+    private final DomainFactory domainFactory;
+    private final DynamicTrackedEvent trackedEvent;
+    
+    public RaceHandleImpl(DomainFactory domainFactory, Event tractracEvent, DynamicTrackedEvent trackedEvent) {
+        this.domainFactory = domainFactory;
+        this.tractracEvent = tractracEvent;
+        this.trackedEvent = trackedEvent;
+    }
+
+    @Override
+    public com.sap.sailing.domain.base.Event getEvent() {
+        return domainFactory.createEvent(tractracEvent);
+    }
+
+    @Override
+    public RaceDefinition getRace() {
+        // we assume there is exactly one Race per TracTrac event
+        return domainFactory.getRaceDefinition(tractracEvent.getRaceList().iterator().next());
+    }
+    
+    @Override
+    public DynamicTrackedEvent getTrackedEvent() {
+        return trackedEvent;
+    }
+    
+}
