27761f1ec82cd2a0ad506eacef79194fc61aa80a
Simon Pamies <simon.marcel.pamies@sap.com>
2011-06-16 23:49:24 +0200

    Fixed headings of columns, added correct link for race switch

--- python/src/sailing.web/sailing/web/config.py
+++ python/src/sailing.web/sailing/web/config.py
@@ -33,7 +33,7 @@ VN = {
         'CSPDMS' : ('CSPDMS', 'Current Speed (m/s)', '%.1f', 'currentSpeedOverGroundInMetersPerSecond'),
         'ASPDKNOT': ('ASPDKN', 'Avg Speed (kn)', '%.2f', 'averageSpeedOverGroundInKnots'),
         'ASPDMS' : ('ASPDMS', 'Avg Speed (m/s)', '%.1f', 'averageSpeedOverGroundInMetersPerSecond'),
-        'DSTTRV' : ('DSTTRV', 'Dist. Traveld (m)', '%.f', 'distanceTraveledOverGroundInMeters'),
+        'DSTTRV' : ('DSTTRV', 'Distance Traveled (m)', '%.f', 'distanceTraveledOverGroundInMeters'),
         'VMGKNOT': ('VMGKN', 'VMG (kn)', '%.1f', 'velocityMadeGoodInKnots'),
         'VMGMS' : ('VMGMS', 'VMG (m/s)', '%.1f', 'velocityMadeGoodInMetersPerSecond'),
         'AVMGKNOT': ('AVMGKN', 'Avg VMG (kn)', '%.1f', 'averageVelocityMadeGoodInKnots'),
