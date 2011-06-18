f085a4db91334f04fa5d641276224de61c5ef1b5
Axel Uhl <axel.uhl@sap.com>
2011-06-18 12:31:43 +0200

    fixed event naming uniqueness by adding boat class name

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/Event.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/Event.java
@@ -1,5 +1,13 @@
 package com.sap.sailing.domain.base;
 
+/**
+ * The name shall be unique across all events tracked concurrently. In particular, if you want to
+ * keep apart events in different boat classes, make sure the boat class name becomes part of the
+ * event name.
+ * 
+ * @author Axel Uhl (d043530)
+ *
+ */
 public interface Event extends Named {
     Iterable<RaceDefinition> getAllRaces();
     
