b5390899d3867ec7eabf111c9cb127b8886c3773
Axel Uhl <axel.uhl@sap.com>
2011-06-21 02:41:54 +0200

    fixed wind calculation for sparse wind tracks

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/WindTrackImpl.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/WindTrackImpl.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.domain.tracking.impl;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.NavigableSet;
 import java.util.Set;
 import java.util.logging.Level;
@@ -87,40 +88,72 @@ public class WindTrackImpl extends TrackImpl<Wind> implements WindTrack {
         }
     }
 
+    /**
+     * Tries to find at least {@link #millisecondsOverWhichToAverage} worth of wind data. The algorithm
+     * oscillates in the wind track around <code>at</code>, starting with fixes left and right that are
+     * closest to <code>at</code>. In each direction, left and right, we count the interval covered so far,
+     * starting at the fix nearest to <code>at</code>. We progress on the side where we are still closer to
+     * <code>at</code>, unless there are no more fixes in that direction. From all fixes found this way,
+     * the speed average and the bearing average are computed separately and combined into a new {@link Wind}
+     * object.
+     */
     @Override
     public synchronized Wind getEstimatedWind(Position p, TimePoint at) {
         DummyWind atTimed = new DummyWind(at);
         NavigableSet<Wind> beforeSet = getInternalFixes().headSet(atTimed, /* inclusive */ true);
         NavigableSet<Wind> afterSet = getInternalFixes().tailSet(atTimed, /* inclusive */ true);
-        if (beforeSet.isEmpty()) {
-        	// try after:
-            if (afterSet.isEmpty()) {
-                return null;
-            } else {
-                return afterSet.iterator().next();
-            }
-        }
+        Iterator<Wind> beforeIter = beforeSet.descendingIterator();
+        Iterator<Wind> afterIter = afterSet.iterator();
         double knotSum = 0;
         double bearingDegSum = 0;
         int count = 0;
-        for (Wind before : beforeSet.descendingSet()) {
-            if (count == 0 || at.asMillis() - before.getTimePoint().asMillis() < millisecondsOverWhichToAverage) { 
-                knotSum += before.getKnots();
-                bearingDegSum += before.getBearing().getDegrees();
+        long beforeDistanceToAt = 0;
+        long afterDistanceToAt = 0;
+        TimePoint beforeIntervalEnd = null;
+        TimePoint afterIntervalStart = null;
+        long beforeIntervalLength = 0;
+        long afterIntervalLength = 0;
+        Wind beforeWind = null;
+        if (beforeIter.hasNext()) {
+            beforeWind = beforeIter.next();
+            beforeDistanceToAt = at.asMillis() - beforeWind.getTimePoint().asMillis();
+        }
+        Wind afterWind = null;
+        if (afterIter.hasNext()) {
+            afterWind = afterIter.next();
+            afterDistanceToAt = afterWind.getTimePoint().asMillis() - at.asMillis();
+        }
+        do {
+            if (beforeWind != null && (beforeDistanceToAt <= afterDistanceToAt || afterWind == null)) {
+                if (beforeIntervalEnd == null) {
+                    beforeIntervalEnd = beforeWind.getTimePoint();
+                }
+                knotSum += beforeWind.getKnots();
+                bearingDegSum += beforeWind.getBearing().getDegrees();
                 count++;
+                if (beforeIter.hasNext()) {
+                    beforeWind = beforeIter.next();
+                    beforeDistanceToAt = at.asMillis() - beforeWind.getTimePoint().asMillis();
+                    beforeIntervalLength = beforeIntervalEnd.asMillis() - beforeWind.getTimePoint().asMillis();
+                } else {
+                    beforeWind = null;
+                }
             } else {
-                break;
+                if (afterIntervalStart == null) {
+                    afterIntervalStart = afterWind.getTimePoint();
+                }
+                knotSum += afterWind.getKnots();
+                bearingDegSum += afterWind.getBearing().getDegrees();
+                count++;
+                if (afterIter.hasNext()) {
+                    afterWind = afterIter.next();
+                    afterDistanceToAt = afterWind.getTimePoint().asMillis() - at.asMillis();
+                    afterIntervalLength = afterWind.getTimePoint().asMillis() - afterIntervalStart.asMillis();
+                } else {
+                    afterWind = null;
+                }
             }
-        }
-        long lastMillis = beforeSet.last().getTimePoint().asMillis();
-        if (count == 1 && at.asMillis() - lastMillis >= millisecondsOverWhichToAverage && !afterSet.isEmpty()) {
-            // last was out of interval; check if next is closer than last
-            Wind next = afterSet.first();
-            if (at.asMillis() - lastMillis > next.getTimePoint().asMillis() - at.asMillis()) {
-                // next is closer than last
-                return next;
-            }
-        }
+        } while (beforeIntervalLength + afterIntervalLength < millisecondsOverWhichToAverage && (beforeWind != null || afterWind != null));
         SpeedWithBearing avgWindSpeed = new KnotSpeedWithBearingImpl(knotSum / count, new DegreeBearingImpl(bearingDegSum/count));
         return new WindImpl(p, at, avgWindSpeed);
     }
