1dc9f89e70f0bc0384f38f8d45a284dfebdc6191
Axel Uhl <axel.uhl@sap.com>
2011-07-22 10:42:17 +0200

    wrote up a few TODOs regarding preparing for a second tracking adapter; added missing BearingCluster class

---
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/BearingCluster.java
@@ -0,0 +1,40 @@
+package com.sap.sailing.domain.tracking.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sap.sailing.domain.base.Bearing;
+import com.sap.sailing.domain.base.impl.DegreeBearingImpl;
+
+/**
+ * Contains a number of {@link Bearing} objects and maintains the average bearing. For a given {@link Bearing} it
+ * can determine the difference to this cluster's average bearing.
+ * 
+ * @author Axel Uhl (d043530)
+ *
+ */
+public class BearingCluster {
+    private final List<Bearing> bearings;
+    private double sumDegrees;
+    
+    public BearingCluster() {
+        bearings = new ArrayList<Bearing>();
+        sumDegrees = 0.0;
+    }
+    
+    public void add(Bearing bearing) {
+        bearings.add(bearing);
+        sumDegrees += bearing.getDegrees();
+    }
+    
+    public Bearing getAverage() {
+        return new DegreeBearingImpl(sumDegrees / bearings.size());
+    }
+    
+    /**
+     * If there is no bearing stored in this cluster yet, 0.0 is returned.
+     */
+    public double getDifferenceFromAverage(Bearing bearing) {
+        return Math.abs(sumDegrees / bearings.size() - bearing.getDegrees());
+    }
+}
