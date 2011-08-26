1f3c31fcc78062b97b26ea8b6d13c544b293f06f
Axel Uhl <axel.uhl@sap.com>
2011-08-27 01:54:13 +0200

    fixed bug in ArrayListNavigableSet when inserting at the end; added medal to medal race display

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/SortableColumn.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/SortableColumn.java
@@ -2,6 +2,7 @@ package com.sap.sailing.gwt.ui.client;
 
 import java.util.Comparator;
 
+import com.google.gwt.user.cellview.client.Header;
 import com.google.gwt.user.cellview.client.TextColumn;
 
 public abstract class SortableColumn<T> extends TextColumn<T> {
@@ -11,5 +12,5 @@ public abstract class SortableColumn<T> extends TextColumn<T> {
     
     public abstract Comparator<T> getComparator();
     
-    public abstract String getHeaderName();
+    public abstract Header<String> getHeader();
 }
