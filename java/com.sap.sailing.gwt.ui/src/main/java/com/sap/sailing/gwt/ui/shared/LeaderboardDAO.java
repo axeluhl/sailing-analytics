b92925273e50434e494867ebb91565c79a35ae37
Axel Uhl <axel.uhl@sap.com>
2011-08-31 20:21:15 +0200

    tried to add rank column; entirely untested

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/LeaderboardDAO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/LeaderboardDAO.java
@@ -1,5 +1,7 @@
 package com.sap.sailing.gwt.ui.shared;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +23,19 @@ public class LeaderboardDAO implements IsSerializable {
     public boolean hasCarriedPoints;
     public int[] discardThresholds;
     
+    private boolean competitorsOrderedAccordingToTotalRank;
+    
+    private final TotalRankingComparator totalRankingComparator;
+    
+    public LeaderboardDAO() {
+        totalRankingComparator = new TotalRankingComparator();
+        competitorsOrderedAccordingToTotalRank = false;
+    }
+    
+    public TotalRankingComparator getTotalRankingComparator() {
+        return totalRankingComparator;
+    }
+
     public boolean scoredInMedalRace(CompetitorDAO competitor) {
         LeaderboardRowDAO row = rows.get(competitor);
         for (Map.Entry<String, Boolean> raceNameAndMedalRace : raceNamesAndMedalRace.entrySet()) {
@@ -30,4 +45,52 @@ public class LeaderboardDAO implements IsSerializable {
         }
         return false;
     }
+    
+    public int getTotalPoints(LeaderboardRowDAO object) {
+        int totalPoints = object.carriedPoints==null?0:object.carriedPoints;
+        for (LeaderboardEntryDAO e : object.fieldsByRaceName.values()) {
+            totalPoints += e.totalPoints;
+        }
+        return totalPoints;
+    }
+
+    private class TotalRankingComparator implements Comparator<LeaderboardRowDAO> {
+        @Override
+        public int compare(LeaderboardRowDAO o1, LeaderboardRowDAO o2) {
+            int result;
+            if (scoredInMedalRace(o1.competitor)) {
+                if (scoredInMedalRace(o2.competitor)) {
+                    // both scored in medal race
+                    result = getTotalPoints(o1) - getTotalPoints(o2);
+                } else {
+                    // only o1 scored in medal race, so o1 scores better = "less"
+                    result = -1;
+                }
+            } else {
+                if (scoredInMedalRace(o2.competitor)) {
+                    // only o2 scored in medal race, so o2 scores better, o1 scores worse = "greater"
+                    result = 1;
+                } else {
+                    // neither one scored in any medal race
+                    result = getTotalPoints(o1) - getTotalPoints(o2);
+                }
+                
+            }
+            return result;
+        }
+    }
+
+    public int getRank(CompetitorDAO competitor) {
+        if (!competitorsOrderedAccordingToTotalRank) {
+            Collections.sort(competitors, new Comparator<CompetitorDAO>() {
+                @Override
+                public int compare(CompetitorDAO o1, CompetitorDAO o2) {
+                    return getTotalRankingComparator().compare(rows.get(o1), rows.get(o2));
+                }
+            });
+            competitorsOrderedAccordingToTotalRank = true;
+        }
+        return competitors.indexOf(competitor);
+    };
+
 }
