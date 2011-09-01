91cb628134959c273e75811ea4314888ef3d326d
Axel Uhl <axel.uhl@sap.com>
2011-09-01 18:19:56 +0200

    preparing for leg-specific details in leaderboard

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/LeaderboardRowDAO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/LeaderboardRowDAO.java
@@ -4,6 +4,12 @@ import java.util.Map;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+/**
+ * Holds data about one competitor and all races represented by the owning {@link LeaderboardDAO leaderboard}.
+ * 
+ * @author Axel Uhl (D043530)
+ *
+ */
 public class LeaderboardRowDAO implements IsSerializable {
     public CompetitorDAO competitor;
     public Map<String, LeaderboardEntryDAO> fieldsByRaceName;
