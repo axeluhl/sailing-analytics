de132689f0bae5e13c26f33e7b419274ad14520c
Axel Uhl <axel.uhl@sap.com>
2011-09-01 13:05:26 +0200

    fixed JSNI for collator implementation

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/Collator.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/Collator.java
@@ -7,7 +7,7 @@ public class Collator {
         return instance;
     }
     
-    public native int compare(String o1, String o2); /*-{
+    public native int compare(String o1, String o2) /*-{
         return o1.localeCompare(o2);
-    }-*/
+    }-*/;
 }
