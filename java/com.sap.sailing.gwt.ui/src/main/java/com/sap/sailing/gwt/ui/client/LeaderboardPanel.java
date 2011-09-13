46114180fec20845306d8d6723b292cab84fe84e
Axel Uhl <axel.uhl@sap.com>
2011-09-13 12:46:53 +0200

    pulled LegColumn into separate compilation unit

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/LeaderboardPanel.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/LeaderboardPanel.java
@@ -33,7 +33,6 @@ import com.google.gwt.view.client.MultiSelectionModel;
 import com.sap.sailing.gwt.ui.shared.LeaderboardDAO;
 import com.sap.sailing.gwt.ui.shared.LeaderboardEntryDAO;
 import com.sap.sailing.gwt.ui.shared.LeaderboardRowDAO;
-import com.sap.sailing.gwt.ui.shared.LegEntryDAO;
 import com.sap.sailing.gwt.ui.shared.Pair;
 
 /**
@@ -201,74 +200,9 @@ public class LeaderboardPanel extends FormPanel {
             int legCount = getLeaderboard().getLegCount(getRaceName());
             List<SortableColumn<LeaderboardRowDAO, ?>> result = new ArrayList<SortableColumn<LeaderboardRowDAO,?>>();
             for (int i=0; i<legCount; i++) {
-                result.add(new LegColumn(getRaceName(), /* legIndex */ i));
+                result.add(new LegColumn(LeaderboardPanel.this, getRaceName(), /* legIndex */ i));
             }
             return result;
-        }
-    }
-    
-    /**
-     * Displays competitor's rank in leg and makes the column sortable by rank. The leg is
-     * identified as an index into the {@link LeaderboardEntryDAO#legDetails} list.
-     * 
-     * @author Axel Uhl (D043530)
-     * 
-     */
-    protected class LegColumn extends ExpandableSortableColumn<String> {
-        private final String raceName;
-        private final int legIndex;
-        
-        public LegColumn(String raceName, int legIndex) {
-            super(LeaderboardPanel.this, /* expandable */ true /* all legs have details */, new TextCell());
-            this.raceName = raceName;
-            this.legIndex = legIndex;
-        }
-        
-        private int getLegIndex() {
-            return legIndex;
-        }
-        
-        private String getRaceName() {
-            return raceName;
-        }
-
-        private LegEntryDAO getLegEntry(LeaderboardRowDAO row) {
-            LegEntryDAO legEntry = null;
-            LeaderboardEntryDAO entry = row.fieldsByRaceName.get(getRaceName());
-            if (entry != null && entry.legDetails != null) {
-                legEntry = entry.legDetails.get(getLegIndex());
-            }
-            return legEntry;
-        }
-        
-        @Override
-        public Comparator<LeaderboardRowDAO> getComparator() {
-            return new Comparator<LeaderboardRowDAO>() {
-                @Override
-                public int compare(LeaderboardRowDAO o1, LeaderboardRowDAO o2) {
-                    return safeGetLegRank(o1) - safeGetLegRank(o2);
-                }
-            };
-        }
-
-        private int safeGetLegRank(LeaderboardRowDAO row) {
-            int result = 0;
-            LegEntryDAO legEntry = getLegEntry(row);
-            if (legEntry != null) {
-                result = legEntry.rank;
-            }
-            return result;
-        }
-
-        @Override
-        public Header<SafeHtml> getHeader() {
-            return new SortableExpandableColumnHeader(/* title */ "Leg "+(legIndex+1),
-                    /* iconURL */ null, LeaderboardPanel.this, this);
-        }
-
-        @Override
-        public String getValue(LeaderboardRowDAO row) {
-            return ""+safeGetLegRank(row);
         }
     }
     
