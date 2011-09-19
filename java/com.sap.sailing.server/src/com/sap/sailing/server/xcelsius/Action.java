4426452f8b0676abaca07806048fb5d2d2fe48c4
Axel Uhl <axel.uhl@sap.com>
2011-09-19 21:10:30 +0200

    by Matthias Nott: preparations to integrate with Xcelsius

---
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/xcelsius/Action.java
@@ -0,0 +1,269 @@
+package com.sap.sailing.server.xcelsius;
+
+import com.sap.sailing.domain.base.Event;
+import com.sap.sailing.domain.base.RaceDefinition;
+import com.sap.sailing.domain.base.TimePoint;
+import com.sap.sailing.domain.base.impl.MillisecondsTimePoint;
+import com.sap.sailing.domain.tracking.TrackedRace;
+import com.sap.sailing.server.RacingEventService;
+import com.sap.sailing.util.InvalidDateException;
+
+import org.jdom.Document;
+import org.jdom.Element;
+
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import java.io.IOException;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Action {
+  private HttpServletRequest  req;
+  private HttpServletResponse res;
+
+  private RacingEventService  service;
+
+  private Document            table;
+
+  private Element             data;
+
+  private Element             currentRow;
+
+  public Action() {}
+
+
+  public Action(HttpServletRequest req, HttpServletResponse res, RacingEventService service) {
+    this.req     = req;
+    this.res     = res;
+    this.service = service;
+  }
+
+  public String getAttribute(String name) {
+    return this.req.getParameter(name);
+  }
+
+
+  public RacingEventService getService() {
+    return service;
+  }
+
+
+  public HashMap<String, Event> getEvents() {
+    final HashMap<String, Event> result = new HashMap<String, Event>();
+
+    for (final Event event : this.service.getAllEvents()) {
+      result.put(event.getName(), event);
+    }
+
+    return result;
+  }
+
+
+  public Event getEvent() throws IOException {
+    /*
+     * EVENT
+     */
+    final String eventName = getAttribute("event");
+
+    if (eventName == null) {
+      say("Use the event= parameter to specify the event");
+
+      return null;
+    }
+
+    final Event event = getEvent(eventName);
+
+    if (event == null) {
+      say("Event " + eventName + " not found.");
+
+      return null;
+    }
+
+    return event;
+  }
+
+
+  public Event getEvent(String name) {
+    for (final Event event : this.service.getAllEvents()) {
+      if (name.equals(event.getName())) {
+        return event;
+      }
+    }
+
+    return null;
+  }
+
+
+  public RaceDefinition getRace(Event event) throws IOException {
+    /*
+     * Get the race
+     */
+    final String raceName = getAttribute("race");
+
+    if (raceName == null) {
+      say("Use the race= parameter to specify the race.");
+
+      return null;
+    }
+
+    /*
+     * RACE
+     */
+    final RaceDefinition race = getRace(event, raceName);
+
+    if (race == null) {
+      say("Race " + raceName + " not found.");
+
+      return null;
+    }
+
+    return race;
+  }
+
+
+  public HashMap<String, RaceDefinition> getRaces(Event event) {
+    final HashMap<String, RaceDefinition> result = new HashMap<String, RaceDefinition>();
+
+    for (final RaceDefinition race : event.getAllRaces()) {
+      result.put(race.getName(), race);
+    }
+
+    return result;
+  }
+
+
+  public RaceDefinition getRace(Event event, String name) {
+    if ((event != null) && (name != null)) {
+      for (RaceDefinition race : event.getAllRaces()) {
+        if (name.equals(race.getName())) {
+          return race;
+        }
+      }
+    }
+
+    return null;
+  }
+
+
+  public TrackedRace getTrackedRace(Event event, RaceDefinition race) throws IOException {
+    final TrackedRace trackedRace = getService().getDomainFactory().getTrackedEvent(event).getTrackedRace(race);
+
+    return trackedRace;
+  }
+
+
+  public TimePoint getTimePoint(TrackedRace race) throws IOException, InvalidDateException {
+    final String time = getAttribute("time");
+
+    if (time == null) {
+      say("Use time= parameter");
+
+      return null;
+    }
+
+    if ((time != null) && (time.length() > 0)) {
+      final TimePoint aTimePoint = race.getStart();
+
+      if (aTimePoint == null) {
+        return null;
+      }
+
+      final long      mTime      = aTimePoint.asMillis() + (StringUtility.StringToInteger(time, 0) * 1000 * 60);
+
+      final TimePoint bTimePoint = new MillisecondsTimePoint(mTime);
+
+      return bTimePoint;
+    }
+
+    return null;
+  }
+
+
+  public Document getTable(String variable) {
+    this.table = new Document();
+
+    final Element data = new Element("data");
+
+    this.data = data;
+
+    this.table.addContent(data);
+
+    final Element var = new Element("variable");
+    var.setAttribute("name", variable);
+
+    data.addContent(var);
+
+    return this.table;
+  }
+
+
+  public Element addRow() {
+    final Element row = new Element("row");
+    this.currentRow = row;
+    this.data.addContent(row);
+
+    return row;
+  }
+
+
+  public Element addColumn(String content) {
+    final Element col = new Element("column");
+    col.setText(content);
+    this.currentRow.addContent(col);
+
+    return col;
+  }
+
+
+  public static void say(String msg, HttpServletResponse res) throws IOException {
+    final Document doc = new Document();
+
+    final Element  el  = new Element("message");
+    el.setText(msg);
+
+    doc.addContent(el);
+
+    res.getWriter().print(getXMLAsString(doc));
+  }
+
+
+  public void say(Document doc) throws IOException {
+    this.res.getWriter().print(getXMLAsString(doc));
+    ;
+  }
+
+
+  public void say(String msg) throws IOException {
+    say(msg, this.res);
+  }
+
+
+  public static String getXMLAsString(Object context) {
+    return getXMLAsString(context, "UTF-8");
+  }
+
+
+  public static String getXMLAsString(Object context, String encoding) {
+    //final XMLOutputter outputter = new XMLOutputter("  ", true, encoding);
+    final Format format = Format.getPrettyFormat();
+    format.setIndent("  ");
+    format.setEncoding(encoding);
+
+    final XMLOutputter outputter = new XMLOutputter(format);
+
+    if (context instanceof Document) {
+      return outputter.outputString((Document) context);
+    }
+
+    if (context instanceof Element) {
+      return outputter.outputString((Element) context);
+    }
+
+    return "";
+  }
+}
