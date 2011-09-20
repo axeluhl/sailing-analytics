f53b80e38e433e6fe936ffeed3e461216090eee8
Axel Uhl <axel.uhl@sap.com>
2011-09-20 14:32:39 +0200

    use translatable names for settings dialog in leaderboard panel

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/LegDetailSelectionProvider.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/LegDetailSelectionProvider.java
@@ -15,6 +15,28 @@ public interface LegDetailSelectionProvider {
         DISTANCE_TRAVELED, AVERAGE_SPEED_OVER_GROUND_IN_KNOTS, RANK_GAIN, CURRENT_SPEED_OVER_GROUND_IN_KNOTS,
         ESTIMATED_TIME_TO_NEXT_WAYPOINT_IN_SECONDS, VELOCITY_MADE_GOOD_IN_KNOTS, GAP_TO_LEADER_IN_SECONDS,
         WINDWARD_DISTANCE_TO_GO_IN_METERS;
+
+        public String toString(StringConstants stringConstants) {
+            switch (this) {
+            case DISTANCE_TRAVELED:
+                return stringConstants.distanceInMeters();
+            case AVERAGE_SPEED_OVER_GROUND_IN_KNOTS:
+                return stringConstants.averageSpeedInKnots();
+            case RANK_GAIN:
+                return stringConstants.rankGain();
+            case CURRENT_SPEED_OVER_GROUND_IN_KNOTS:
+                return stringConstants.currentSpeedOverGroundInKnots();
+            case ESTIMATED_TIME_TO_NEXT_WAYPOINT_IN_SECONDS:
+                return stringConstants.estimatedTimeToNextWaypointInSeconds();
+            case VELOCITY_MADE_GOOD_IN_KNOTS:
+                return stringConstants.velocityMadeGoodInKnots();
+            case GAP_TO_LEADER_IN_SECONDS:
+                return stringConstants.gapToLeaderInSeconds();
+            case WINDWARD_DISTANCE_TO_GO_IN_METERS:
+                return stringConstants.windwardDistanceToGoInMeters();
+            }
+            return null;
+        }
     }
 
     List<LegDetailColumnType> getLegDetailsToShow();
