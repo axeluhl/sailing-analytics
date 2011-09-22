497b742b727a84fd890a4027d8c58055a597897b
Axel Uhl <axel.uhl@sap.com>
2011-09-22 17:50:57 +0200

    added values about best-ranked competitors and first leg position/bearing to listEvents xcelsius action

--- java/com.sap.sailing.server/src/com/sap/sailing/server/xcelsius/ListEvents.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/xcelsius/ListEvents.java
@@ -4,14 +4,23 @@ import java.net.URLEncoder;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jdom.Document;
 
+import com.sap.sailing.domain.base.Competitor;
 import com.sap.sailing.domain.base.Event;
+import com.sap.sailing.domain.base.Leg;
+import com.sap.sailing.domain.base.Position;
 import com.sap.sailing.domain.base.RaceDefinition;
+import com.sap.sailing.domain.base.Waypoint;
+import com.sap.sailing.domain.tracking.TrackedLeg;
 import com.sap.sailing.domain.tracking.TrackedRace;
 import com.sap.sailing.server.RacingEventService;
 
@@ -38,7 +47,32 @@ public class ListEvents extends Action {
                 addColumn(raceName);
                 addColumn(URLEncoder.encode(eventName, "UTF-8"));
                 addColumn(URLEncoder.encode(raceName, "UTF-8"));
-                addColumn("SomeTime");
+                List<Leg> legs = race.getCourse().getLegs();
+                TrackedLeg lastTrackedLeg = trackedRace.getTrackedLeg(legs.get(legs.size()-1));
+                LinkedHashMap<Competitor, Integer> finalRanks = lastTrackedLeg.getRanks(trackedRace.getTimePointOfNewestEvent());
+                Iterator<Map.Entry<Competitor, Integer>> entryIter = finalRanks.entrySet().iterator();
+                for (int i=0; i<3; i++) {
+                    String nationality = "";
+                    String sailID = "";
+                    String competitorName = "";
+                    if (entryIter.hasNext()) {
+                        Map.Entry<Competitor, Integer> next = entryIter.next();
+                        nationality = next.getKey().getTeam().getNationality().getThreeLetterIOCAcronym();
+                        sailID = next.getKey().getBoat().getSailID();
+                        competitorName = next.getKey().getName();
+                    }
+                    addColumn(nationality);
+                    addColumn(sailID);
+                    addColumn(competitorName);
+                }
+                Iterator<Waypoint> waypointsIter= race.getCourse().getWaypoints().iterator();
+                Position startPos = trackedRace.getApproximatePosition(waypointsIter.next(),
+                        trackedRace.getStart());
+                Position secondMarkPos = trackedRace.getApproximatePosition(waypointsIter.next(),
+                        trackedRace.getStart());
+                addColumn(""+startPos.getBearingGreatCircle(secondMarkPos).getDegrees());
+                addColumn(""+startPos.getLatDeg());
+                addColumn(""+startPos.getLngDeg());
             }
         }
         say(table);
