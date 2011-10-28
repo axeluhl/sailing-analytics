f2adf4d3f56a115cea83d4eecbd8d0724fdbe6e1
Axel Uhl <axel.uhl@sap.com>
2011-10-28 17:00:52 +0200

    added wind display for course-defined wind and always providing an estimation if requested, for current time if no other wind fix exists

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/mock/MockedTrackedRace.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/mock/MockedTrackedRace.java
@@ -389,4 +389,10 @@ public class MockedTrackedRace implements DynamicTrackedRace {
         return null;
     }
 
+    @Override
+    public Wind getDirectionFromStartToNextMark(TimePoint at) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
