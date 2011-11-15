4c21773ec76276a6fc8254499961abc27e0a8c18
fmittag <fmittag@gmx.net>
2011-11-15 16:50:20 +0100

    created two .txt files containing a text file version of the swiss timing sample and a corresponding file with the missing RAC and STL evens; additional a prototype of a reader which can deal with binary and text file based swiss time messages (ScriptedMessagesReader)

---
+++ java/com.sap.sailing.domain.swisstimingadapter.test/src/com/sap/sailing/domain/swisstimingadapter/test/ScriptedMessagesReader.java
@@ -0,0 +1,134 @@
+package com.sap.sailing.domain.swisstimingadapter.test;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ScriptedMessagesReader {
+
+    protected List<String> messages;
+
+    public ScriptedMessagesReader() {
+        messages = new ArrayList<String>();
+    }
+
+    public int getMessageCount() {
+        return messages.size();
+    }
+
+    public boolean addMessagesFromRawMessageFile(InputStream scriptInputStream) {
+        
+        Map<String, Integer> messageTypes = new HashMap<String, Integer>();
+        Map<String, String> comp = new HashMap<String, String>();
+        
+        try { 
+            // read the script from an inputstream
+            DataInputStream in = new DataInputStream(scriptInputStream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            char STX = '\u0002';  // ascii for START OF TEXT
+            char ETX = '\u0003';  // ascii for END OF TEXT
+            
+            StreamTokenizer tokenizer = new StreamTokenizer(br);
+            tokenizer.ordinaryChar(STX);
+            tokenizer.ordinaryChar(ETX);
+            
+            char[] validChars = new char[] { '|', ':', ';', '+', '-', ' '};
+            for(char c: validChars)
+                tokenizer.wordChars(c, c);
+            
+            int token;
+            while((token = tokenizer.nextToken()) != StreamTokenizer.TT_EOF) {
+                
+                switch(token) {
+                    case StreamTokenizer.TT_WORD:
+                        String s = tokenizer.sval;
+
+                        String type = s.substring(0, 3);
+                        
+                        if(messageTypes.containsKey(type) == false)  {
+                            messageTypes.put(type, 1);
+                        } else {
+                            messageTypes.put(type, messageTypes.get(type) + 1);
+                        }
+          /*  
+                        if("TMD".equals(type))  // just for internal analysis
+                        {
+                            System.out.println(type + " message with length " + s.length());
+                            System.out.println(s);
+
+                            String[] xyz = s.split("\\|");
+                            String race = xyz[1];
+                            String boat = xyz[2];
+                            
+                            if(comp.containsKey(boat) == false)
+                                comp.put(boat, "STL|"+ race + "|1|" + boat + ";" + boat.substring(0,3) + ";Competitor " + comp.size());
+                        }
+                        */
+                        messages.add(s);
+
+                        break;
+                    default:
+                        int controlChar = tokenizer.ttype;
+                }
+            }
+            
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        /*
+        System.out.println(messages.size());
+        System.out.println(messageTypes);
+
+        System.out.println(comp.size());
+        for(String stl: comp.values())
+            System.out.println(stl);
+        */
+        return true;
+    }
+    
+    public boolean addMessagesFromTextFile(InputStream scriptInputStream) {
+    
+        try { 
+            // read the script from an inputstream
+            DataInputStream in = new DataInputStream(scriptInputStream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            
+            String strLine;
+            
+            // read line by line
+            while ((strLine = br.readLine()) != null)   {
+                messages.add(strLine);
+            }
+            
+            in.close(); 
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        
+        return true;
+    }
+
+    public void addMessagesFromListScriptedMessagesReader(List<String> scriptMessages) {
+        this.messages.addAll(scriptMessages);
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<String> scriptMessages) {
+        this.messages = scriptMessages;
+    }
+
+}
