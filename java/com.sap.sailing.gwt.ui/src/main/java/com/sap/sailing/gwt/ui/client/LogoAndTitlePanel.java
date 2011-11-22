5d23d3de97c8b829a5745d3d377f82d9b1b6d1ba
Marcus Kammer <marcus.kammer@sap.com>
2011-11-22 10:13:19 +0100

    class panel for LogoAndTitle

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/LogoAndTitlePanel.java
@@ -0,0 +1,21 @@
+package com.sap.sailing.gwt.ui.client;
+
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+
+public class LogoAndTitlePanel extends FlowPanel {
+
+    public LogoAndTitlePanel(StringConstants stringConstants) {
+        Anchor sapLogo = new Anchor(new SafeHtmlBuilder().appendHtmlConstant("<img class=\"linkNoBorder\" src=\"/images/sap_66_transparent.png\"/>").toSafeHtml());
+        sapLogo.setHref("http://www.sap.com");
+        this.add(sapLogo);
+        Label sailingAnalyticsLabel = new Label(stringConstants.sapSailingAnalytics());     
+        FlowPanel labelPanel = new FlowPanel();
+        labelPanel.add(sailingAnalyticsLabel);
+        this.add(labelPanel);
+        sailingAnalyticsLabel.addStyleName("boldLabel");
+    }
+
+}
