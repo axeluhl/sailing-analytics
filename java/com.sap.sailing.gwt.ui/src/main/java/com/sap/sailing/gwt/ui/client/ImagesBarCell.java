27762bee79c51f4d13df9c4aefa16edf9a9533bc
Axel Uhl <axel.uhl@sap.com>
2011-12-01 15:31:38 +0100

    updated to gwt version 2.4.0, consolidated imports

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/ImagesBarCell.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/ImagesBarCell.java
@@ -7,7 +7,6 @@ import com.google.gwt.dom.client.EventTarget;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.text.shared.SafeHtmlRenderer;
 import com.google.gwt.text.shared.SimpleSafeHtmlRenderer;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
