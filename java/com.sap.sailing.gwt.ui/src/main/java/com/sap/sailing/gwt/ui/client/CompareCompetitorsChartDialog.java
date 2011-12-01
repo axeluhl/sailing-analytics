08052b486d887a0e115e891c65df68887921c5d6
Benjamin Ebling <benjamin.ebling@sap.com>
2011-12-01 16:53:03 +0100

    Added new close button in title bar.

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/CompareCompetitorsChartDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/CompareCompetitorsChartDialog.java
@@ -2,47 +2,72 @@ package com.sap.sailing.gwt.ui.client;
 
 import java.util.List;
 
+import com.google.gwt.dom.client.EventTarget;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.sap.sailing.gwt.ui.client.CompareCompetitorsPanel.DataLoadedEvent;
 import com.sap.sailing.gwt.ui.client.CompareCompetitorsPanel.DataLoadedHandler;
 import com.sap.sailing.gwt.ui.shared.CompetitorDAO;
 import com.sap.sailing.gwt.ui.shared.RaceIdentifier;
 
 public class CompareCompetitorsChartDialog extends DialogBox {
+    private Anchor closeAnchor;
 
     public CompareCompetitorsChartDialog(SailingServiceAsync sailingService, List<CompetitorDAO> competitors, RaceIdentifier[] races, StringConstants stringConstants){
         super(true);
-        VerticalPanel mainPanel = new VerticalPanel();
-        mainPanel.setSpacing(5);
-        mainPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
-        CompareCompetitorsPanel ccp = new CompareCompetitorsPanel(sailingService, competitors, races, stringConstants, (int) (Window.getClientWidth()*0.8), (int) (Window.getClientHeight()*0.8));
+        CompareCompetitorsPanel ccp = new CompareCompetitorsPanel(sailingService, competitors, races, stringConstants, (int) (Window.getClientWidth()-300), (int) (Window.getClientHeight()-200));
         ccp.addDataLoadedHandler(new DataLoadedHandler() {
             
             @Override
             public void onDataLoaded(DataLoadedEvent event) {
                 CompareCompetitorsChartDialog.this.setPopupPosition(5, 5);
+                CompareCompetitorsChartDialog.this.setSize(Window.getClientWidth() + "px",Window.getClientHeight() + "px");
                 CompareCompetitorsChartDialog.this.show();
             }
         });
-        mainPanel.add(ccp);
-        Button closeButton = new Button(stringConstants.close());
-        closeButton.addClickHandler(new ClickHandler() {
-            
+        this.add(ccp);
+
+        this.setPopupPosition(5, 5);
+        this.setSize(Window.getClientWidth() + "px",Window.getClientHeight() + "px");
+        closeAnchor = new Anchor("x");
+
+        FlexTable captionLayoutTable = new FlexTable();
+        captionLayoutTable.setWidth("100%");
+        captionLayoutTable.setText(0, 0, stringConstants.compareCompetitors());
+        captionLayoutTable.setWidget(0, 1, closeAnchor);
+        captionLayoutTable.getCellFormatter().setHorizontalAlignment(0, 1,HasHorizontalAlignment.ALIGN_RIGHT);
+
+        HTML caption = (HTML) getCaption();
+        caption.getElement().appendChild(captionLayoutTable.getElement());
+
+        caption.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                CompareCompetitorsChartDialog.this.hide();
+                EventTarget target = event.getNativeEvent().getEventTarget();
+                Element targetElement = (Element) target.cast();
+
+                if (targetElement == closeAnchor.getElement()) {
+                    closeAnchor.fireEvent(event);
+                }
             }
         });
-        mainPanel.add(closeButton);
-        this.add(mainPanel);
+        addCloseHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                hide();
+            }
+        });
 
-        this.setTitle(stringConstants.compareCompetitors());
-        this.setPopupPosition(5, 5);
+    }
+    
+    public void addCloseHandler(ClickHandler handler) {
+        closeAnchor.addClickHandler(handler);
     }
 }
