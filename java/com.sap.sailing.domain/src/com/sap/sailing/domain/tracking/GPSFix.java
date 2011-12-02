3fed20a0f2cb6fb013013d00cccdbf151af01b5a
Axel Uhl <axel.uhl@sap.com>
2011-12-02 11:56:01 +0100

    introduced caching for validity of GPSFix in GPSFixTrack

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/GPSFix.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/GPSFix.java
@@ -3,6 +3,6 @@ package com.sap.sailing.domain.tracking;
 import com.sap.sailing.domain.base.SpeedWithBearing;
 import com.sap.sailing.domain.base.Timed;
 
-public interface GPSFix extends Positioned, Timed {
+public interface GPSFix extends Positioned, Timed, WithValidityCache {
     SpeedWithBearing getSpeedAndBearingRequiredToReach(GPSFix to);
 }
