f8d2daff3ac480c09dd56519aba2b35db3edd97f
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2011-12-08 14:23:39 +0100

    Corrected imports of import EventNameAndRaceName

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/TrackedRaceChangedListener.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/TrackedRaceChangedListener.java
@@ -1,6 +1,6 @@
 package com.sap.sailing.gwt.ui.client;
 
-import com.sap.sailing.gwt.ui.shared.EventNameAndRaceName;
+import com.sap.sailing.server.api.EventNameAndRaceName;
 
 public interface TrackedRaceChangedListener {
     void changeTrackingRace(EventNameAndRaceName eventNameAndRaceName, boolean isTracked);
