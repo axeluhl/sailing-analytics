007d79a3166e373e73900f9eba7994f8de3e0812
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2011-12-12 10:37:36 +0100

    Test for StopTrackingRace tests the stopTracking Method correctly.

--- java/com.sap.sailing.server.test/src/com/sap/sailing/server/test/RaceTrackerStartStopTest.java
+++ java/com.sap.sailing.server.test/src/com/sap/sailing/server/test/RaceTrackerStartStopTest.java
@@ -1,12 +1,12 @@
 package com.sap.sailing.server.test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.junit.Before;
@@ -19,7 +19,6 @@ import com.sap.sailing.domain.base.impl.BoatClassImpl;
 import com.sap.sailing.domain.base.impl.EventImpl;
 import com.sap.sailing.domain.base.impl.RaceDefinitionImpl;
 import com.sap.sailing.domain.tracking.RaceTracker;
-import com.sap.sailing.util.Util.Pair;
 
 public class RaceTrackerStartStopTest {
 
@@ -31,11 +30,13 @@ public class RaceTrackerStartStopTest {
     private BoatClass boatClass;
     private Set<RaceTracker> raceTrackerSet = new HashSet<RaceTracker>();
 
-    private List<Pair<Long, RaceTracker>> raceTrackerAndId;
-
     private RaceDefinition raceDef1;
     private RaceDefinition raceDef2;
     private RaceDefinition raceDef3;
+
+    private RaceTrackerMock raceTracker1;
+    private RaceTrackerMock raceTracker2;
+    private RaceTrackerMock raceTracker3;
 
     @Before
     public void setUp() {
@@ -44,10 +45,6 @@ public class RaceTrackerStartStopTest {
         event = new EventImpl(EVENTNAME, boatClass);
         racingEventService.getEventsByNameMap().put(EVENTNAME, event);
         raceTrackerSet = new HashSet<RaceTracker>();
-        raceTrackerAndId = new ArrayList<Pair<Long, RaceTracker>>();
-
-        // raceDef1 = new RaceDefinitionImpl("racedef1", new CourseImpl("race1", null), new
-        // BoatClassImpl(BOATCLASSNAME), null);
         raceDef1 = new RaceDefinitionImpl("racedef1", null, new BoatClassImpl(BOATCLASSNAME), null);
         raceDef2 = new RaceDefinitionImpl("racedef2", null, new BoatClassImpl(BOATCLASSNAME), null);
         raceDef3 = new RaceDefinitionImpl("racedef3", null, new BoatClassImpl(BOATCLASSNAME), null);
@@ -64,12 +61,9 @@ public class RaceTrackerStartStopTest {
         Long trackerID1 = new Long(1);
         Long trackerID2 = new Long(2);
         Long trackerID3 = new Long(3);
-        RaceTrackerMock raceTracker1 = new RaceTrackerMock(new Long(1), event, raceDefinitioSetRace1, true);
-        RaceTrackerMock raceTracker2 = new RaceTrackerMock(new Long(2), event, raceDefinitioSetRace2, true);
-        RaceTrackerMock raceTracker3 = new RaceTrackerMock(new Long(3), event, raceDefinitioSetRace3, true);
-        raceTrackerAndId.add(new Pair<Long, RaceTracker>(new Long(1), raceTracker1));
-        raceTrackerAndId.add(new Pair<Long, RaceTracker>(new Long(2), raceTracker2));
-        raceTrackerAndId.add(new Pair<Long, RaceTracker>(new Long(2), raceTracker3));
+        raceTracker1 = new RaceTrackerMock(new Long(1), event, raceDefinitioSetRace1, true);
+        raceTracker2 = new RaceTrackerMock(new Long(2), event, raceDefinitioSetRace2, true);
+        raceTracker3 = new RaceTrackerMock(new Long(3), event, raceDefinitioSetRace3, true);
         raceTrackerSet.add(raceTracker1);
         raceTrackerSet.add(raceTracker2);
         raceTrackerSet.add(raceTracker3);
@@ -80,15 +74,19 @@ public class RaceTrackerStartStopTest {
     }
 
     @Test
-    public void testUntrackRace() throws MalformedURLException, IOException, InterruptedException {
+    public void testStopTrackingRace() throws MalformedURLException, IOException, InterruptedException {
         racingEventService.stopTracking(event, raceDef2);
-        assertFalse(racingEventService.getRaceTrackersByIDMap().containsValue(raceDef2));
-        
-        
-        
-        assertFalse(racingEventService.getRaceTrackersByEventMap().containsValue(raceTrackerAndId.get(0).getB()));
-        assertFalse(racingEventService.getRaceTrackersByEventMap().containsValue(raceTrackerAndId.get(1).getB()));
-        assertFalse(racingEventService.getRaceTrackersByEventMap().containsValue(raceTrackerAndId.get(2).getB()));
+        assertTrue(racingEventService.getRaceTrackersByIDMap().containsValue(raceTracker1));
+        assertFalse(racingEventService.getRaceTrackersByIDMap().containsValue(raceTracker2));
+        assertFalse(racingEventService.getRaceTrackersByIDMap().containsValue(raceTracker3));
+        // The RaceTrakcersByEvent map should contain a tracker with a set of RaceDefinitions, containing the
+        // raceDefinition1
+        assert racingEventService.getRaceTrackersByEventMap().size() == 1;
+        Iterator<RaceTracker> raceTrackerIter = racingEventService.getRaceTrackersByEventMap().get(event).iterator();
+        while(raceTrackerIter.hasNext()){
+            RaceTracker currentTracker = raceTrackerIter.next();
+            assert(currentTracker.equals(raceTracker1));
+        }
     }
 
     @Test
