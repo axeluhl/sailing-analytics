fb21f13de2c94c2360fd525b2c0d418bf2516c90
Axel Uhl <axel.uhl@sap.com>
2011-12-23 11:19:14 +0100

    added enumeration constants for estimated wind sources

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/WindSource.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/WindSource.java
@@ -7,5 +7,25 @@ package com.sap.sailing.domain.tracking;
  *
  */
 public enum WindSource {
-    WEB, EXPEDITION
+    /**
+     * Manually entered via a web form or received through a REST service call, e.g., from BeTomorrow's estimation
+     */
+    WEB,
+    
+    /**
+     * Measured using wind sensors
+     */
+    EXPEDITION,
+    
+    /**
+     * Inferred from the race course layout if the course is known to have its first leg be an upwind leg
+     */
+    COURSE_BASED,
+    
+    /**
+     * Estimates wind conditions by analyzing the boat tracks; may not have results for all time points, e.g.,
+     * because at a given time point all boats may sail on the same tack and hence no averaging between the
+     * two tacks is possible. This is the more likely to happen the smaller the fleet tracked is.
+     */
+    TRACK_BASED_ESTIMATION
 }
