e0d7bd4429cc577d0441ca86b1081cf87c935260
Axel Uhl <axel.uhl@sap.com>
2012-01-05 14:08:43 +0100

    removed unnecessary cast

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
@@ -820,7 +820,7 @@ public class SailingServiceImpl extends RemoteServiceServlet implements SailingS
    }
 
     protected RacingEventService getService() {
-        return (RacingEventService) racingEventServiceTracker.getService(); // grab the service
+        return racingEventServiceTracker.getService(); // grab the service
     }
 
     @Override
