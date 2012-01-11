38669a725fcbb4dd0b96deba03df07bb958d1cf4
Lennart Hensler <lennart.hensler@sap.com>
2012-01-11 16:14:25 +0100

    Added documentation

--- java/com.sap.sailing.geocoding/src/com/sap/sailing/geocoding/Placemark.java
+++ java/com.sap.sailing.geocoding/src/com/sap/sailing/geocoding/Placemark.java
@@ -22,13 +22,30 @@ public interface Placemark {
      */
     String getCountryCode();
     /**
-     * @return The  {@link Position} of the Placemark
+     * @return The {@link Position} of the Placemark
      */
     Position getPosition();
+    /**
+     * @return The population of the Placemark
+     */
     long getPopulation();
 
-    Distance distanceFrom(Position p);
+    /**
+     * @param position The {@link Position} from which the distance is calculated
+     * @return The {@link Distance} between the given {@link Position} and the Placemark
+     */
+    Distance distanceFrom(Position position);
+    /**
+     * Creates a new {@link Position} out of the given coordinates and calculates the distance between {@link Position} and
+     * the Placemark via {@link Placemark#distanceFrom(Position) distanceFrom(Position)}.
+     * @param latDeg Latitude in degrees
+     * @param lngDeg Longitude in degrees
+     * @return The {@link Distance} between the given coordinates and the Placemark
+     */
     Distance distanceFrom(double latDeg, double lngDeg);
+    /**
+     * @return The name of the country where the Placemark is located
+     */
     String getCountryName();
     
     
