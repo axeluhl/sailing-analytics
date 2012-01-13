37b33bea6eee1a0e68cef9a20caa3b4811591f5e
Axel Uhl <axel.uhl@sap.com>
2012-01-13 21:20:08 +0100

    added first pieces of "micro-framework" for settings dialog handling for UI components / widgets

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/StringMessages.java
@@ -0,0 +1,7 @@
+package com.sap.sailing.gwt.ui.client;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface StringMessages extends Messages {
+    String settingsForComponent(String localizedComponentName);
+}
