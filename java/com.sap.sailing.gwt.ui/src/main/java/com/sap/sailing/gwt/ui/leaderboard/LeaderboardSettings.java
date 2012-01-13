7a1f3d165982fee6597d2060c6a4a2aeee1ce55a
Axel Uhl <axel.uhl@sap.com>
2012-01-13 21:33:40 +0100

    started to move leaderboard settings dialog logic into micro-framework for settings / component / widget handling

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/leaderboard/LeaderboardSettings.java
@@ -0,0 +1,5 @@
+package com.sap.sailing.gwt.ui.leaderboard;
+
+public class LeaderboardSettings {
+
+}
---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/leaderboard/LeaderboardSettingsDialogComponent.java
@@ -0,0 +1,35 @@
+package com.sap.sailing.gwt.ui.leaderboard;
+
+import com.google.gwt.user.client.ui.FocusWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.gwt.ui.client.DataEntryDialog;
+import com.sap.sailing.gwt.ui.client.DataEntryDialog.Validator;
+import com.sap.sailing.gwt.ui.shared.components.SettingsDialogComponent;
+
+public class LeaderboardSettingsDialogComponent implements SettingsDialogComponent<LeaderboardSettings> {
+
+    @Override
+    public Widget getAdditionalWidget(DataEntryDialog<LeaderboardSettings> dialog) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public LeaderboardSettings getResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Validator<LeaderboardSettings> getValidator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public FocusWidget getFocusWidget() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
