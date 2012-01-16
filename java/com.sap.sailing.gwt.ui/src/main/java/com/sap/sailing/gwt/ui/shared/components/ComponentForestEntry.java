41168bf2410e17505e78566476392d2ab95bd233
Axel Uhl <axel.uhl@sap.com>
2012-01-16 16:41:26 +0100

    re-added comment

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/components/ComponentForestEntry.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/components/ComponentForestEntry.java
@@ -8,5 +8,10 @@ public interface ComponentForestEntry {
      */
     String getLocalizedShortName();
     
+    /**
+     * A component forest entry may or may not have a {@link Widget} used to render its contents. Typically,
+     * entries with sub-entries don't afford a widget of their own but let the framework choose a container
+     * in which to aggregate their entries' widgets.
+     */
     Widget getEntryWidget();
 }
