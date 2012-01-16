942a4abf4fb6ad24e1136483013a8eb679b38c39
Marcus Kammer <marcus.kammer@sap.com>
2012-01-16 15:39:03 +0100

    add package for racinganalysis page

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/racinganalysis/RacingAnalysisPage.java
@@ -0,0 +1,9 @@
+package com.sap.sailing.gwt.ui.racinganalysis;
+
+import com.sap.sailing.gwt.ui.client.AbstractEntryPoint;
+
+public class RacingAnalysisPage extends AbstractEntryPoint {
+    public void onModuleLoad() {
+
+    }
+}
