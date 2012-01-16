c29fbccb2283a90b15a2def107d02df017f7a948
Marcus Kammer <marcus.kammer@sap.com>
2012-01-16 21:16:19 +0100

    add flowpanel && rootpanel to racinganalysis page

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/racinganalysis/RacingAnalysisPage.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/racinganalysis/RacingAnalysisPage.java
@@ -1,9 +1,15 @@
 package com.sap.sailing.gwt.ui.racinganalysis;
 
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.sap.sailing.gwt.ui.client.AbstractEntryPoint;
 
 public class RacingAnalysisPage extends AbstractEntryPoint {
     public void onModuleLoad() {
-
+        // defines FlowPanel
+        FlowPanel outerPanel = new FlowPanel(); // outer div which centered page content
+        outerPanel.addStyleName("outerPanel");
+        
+        RootPanel.get().add(outerPanel);
     }
 }
