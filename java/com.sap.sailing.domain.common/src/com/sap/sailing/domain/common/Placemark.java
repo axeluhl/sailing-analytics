87c77b1f9ae1ef650ba6254d1a99c3a58caf7973
Lennart Hensler <lennart.hensler@sap.com>
2012-01-17 14:39:37 +0100

    Comparators in Placemark were not the problem

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/Placemark.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/Placemark.java
@@ -50,66 +50,69 @@ public interface Placemark extends Serializable {
 //     * @return The name of the country where the Placemark is located
 //     */
 //    String getCountryName();
-//    
-//    
-//    
-//    
-//    
-//    
-//    /**
-//     * Sorts the Placemarks by Population from low to high.
-//     * @author Lennart Hensler (D054527)
-//     *
-//     */
-//    public class ByPopulation implements Comparator<Placemark> {
-//        @Override
-//        public int compare(Placemark p1, Placemark p2) {
+    
+    
+    
+    
+    
+    
+    /**
+     * Sorts the Placemarks by Population from low to high.
+     * @author Lennart Hensler (D054527)
+     *
+     */
+    public class ByPopulation implements Comparator<Placemark> {
+        @Override
+        public int compare(Placemark p1, Placemark p2) {
 //            return p1.getPopulation() > p2.getPopulation() ? 1 : p1.getPopulation() < p2.getPopulation() ? -1 : 0;
-//        }
-//    }
-//    
-//    /**
-//     * Sorts the Placemarks by distance to a {@link Position} from near to far.<br /><br />
-//     * 
-//     * If you use this for {@link ReverseGeocoder#getPlacemarkLast(Position, float, Comparator) getPlacemarkBest(Position position,...)} to 
-//     * get the nearest Placemark for <code>position</code> you can use {@link ReverseGeocoder#getPlacemarkNearest(Position) getPlacemark} instead.
-//     * <code>getPlacemark</code> returns per default the nearest Placemark for a Position.
-//     * @author Lennart Hensler (D054527)
-//     *
-//     */
-//    public class ByDistance implements Comparator<Placemark> {
-//        
-//        private Position position;
-//        
-//        public ByDistance(Position position) {
-//            this.position = position;
-//        }
-//        
-//        @Override
-//        public int compare(Placemark p1, Placemark p2) {
+            return 0;
+        }
+    }
+    
+    /**
+     * Sorts the Placemarks by distance to a {@link Position} from near to far.<br /><br />
+     * 
+     * If you use this for {@link ReverseGeocoder#getPlacemarkLast(Position, float, Comparator) getPlacemarkBest(Position position,...)} to 
+     * get the nearest Placemark for <code>position</code> you can use {@link ReverseGeocoder#getPlacemarkNearest(Position) getPlacemark} instead.
+     * <code>getPlacemark</code> returns per default the nearest Placemark for a Position.
+     * @author Lennart Hensler (D054527)
+     *
+     */
+    public class ByDistance implements Comparator<Placemark> {
+        
+        private Position position;
+        
+        public ByDistance(Position position) {
+            this.position = position;
+        }
+        
+        @Override
+        public int compare(Placemark p1, Placemark p2) {
 //            Distance d1 = p1.distanceFrom(position);
 //            Distance d2 = p2.distanceFrom(position);
 //            
 //            return d1.compareTo(d2);
-//        }
-//    }
-//    
-//    public class ByPopulationDistanceRatio implements Comparator<Placemark> {
-//        
-//        private Position position;
-//
-//        public ByPopulationDistanceRatio(Position position) {
-//            this.position = position;
-//        }
-//
-//        @Override
-//        public int compare(Placemark p1, Placemark p2) {
+            return 0;
+        }
+    }
+    
+    public class ByPopulationDistanceRatio implements Comparator<Placemark> {
+        
+        private Position position;
+
+        public ByPopulationDistanceRatio(Position position) {
+            this.position = position;
+        }
+
+        @Override
+        public int compare(Placemark p1, Placemark p2) {
 //            double r1 = p1.getPopulation() / p1.distanceFrom(position).getKilometers();
 //            double r2 = p2.getPopulation() / p2.distanceFrom(position).getKilometers();
 //
 //            return r1 > r2 ? 1 : r1 < r2 ? -1 : 0;
-//        }
-//        
-//    }
+            return 0;
+        }
+        
+    }
     
 }
