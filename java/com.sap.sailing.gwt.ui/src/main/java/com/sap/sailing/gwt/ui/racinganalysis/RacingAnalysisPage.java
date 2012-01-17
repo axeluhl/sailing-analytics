e053f62241093615a4ffffbcf79ee0a165ac3248
Marcus Kammer <marcus.kammer@sap.com>
2012-01-17 10:44:33 +0100

    add logoAndTitlePanel

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/racinganalysis/RacingAnalysisPage.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/racinganalysis/RacingAnalysisPage.java
@@ -3,13 +3,17 @@ package com.sap.sailing.gwt.ui.racinganalysis;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.sap.sailing.gwt.ui.client.AbstractEntryPoint;
+import com.sap.sailing.gwt.ui.client.LogoAndTitlePanel;
 
 public class RacingAnalysisPage extends AbstractEntryPoint {
     public void onModuleLoad() {
+        super.onModuleLoad();
         // defines FlowPanel
         FlowPanel outerPanel = new FlowPanel(); // outer div which centered page content
         outerPanel.addStyleName("outerPanel");
-        
+        LogoAndTitlePanel logoAndTitlePanel = new LogoAndTitlePanel(stringConstants);
+        logoAndTitlePanel.addStyleName("LogoAndTitlePanel");
+        RootPanel.get().add(logoAndTitlePanel);
         RootPanel.get().add(outerPanel);
     }
 }
