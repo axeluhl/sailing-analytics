618a9c8861cc37880b87b9ae446b033449dbf0b0
Marcus Kammer <marcus.kammer@sap.com>
2012-01-17 13:33:45 +0100

    add panels for timeline, footershadow, content correct order of position:fixed panels

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/racinganalysis/RacingAnalysisPage.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/racinganalysis/RacingAnalysisPage.java
@@ -9,13 +9,28 @@ public class RacingAnalysisPage extends AbstractEntryPoint {
     public void onModuleLoad() {
         super.onModuleLoad();
         
-        FlowPanel outerPanel = new FlowPanel(); // outer div which centered page content
-        outerPanel.addStyleName("outerPanel");
-        
         LogoAndTitlePanel logoAndTitlePanel = new LogoAndTitlePanel(stringConstants);
         logoAndTitlePanel.addStyleName("LogoAndTitlePanel");
         
-        RootPanel.get().add(logoAndTitlePanel);
-        RootPanel.get().add(outerPanel);
+        FlowPanel contentOuterPanel = new FlowPanel(); // outer div which centered page content
+        contentOuterPanel.addStyleName("contentOuterPanel");
+        
+        FlowPanel timelinePanel = new FlowPanel();
+        timelinePanel.addStyleName("timelinePanel");
+        
+        FlowPanel timelineInnerPanel = new FlowPanel();
+        timelineInnerPanel.addStyleName("timelineInnerPanel");
+        
+        FlowPanel footerShadowPanel = new FlowPanel();
+        footerShadowPanel.addStyleName("footerShadowPanel");
+        
+        timelinePanel.add(timelineInnerPanel);
+        
+        RootPanel.get().add(contentOuterPanel);
+        
+        // Don't change this order because of the inner logic in html of "position fixed"-elements
+        RootPanel.get().add(logoAndTitlePanel);                 // position:fixed        
+        RootPanel.get().add(timelinePanel);                     // position:fixed
+        RootPanel.get().add(footerShadowPanel);                 // position:fixed
     }
 }
