f546d6a9d4771975b785a1feb5ee735fa4d81649
fmittag <fmittag@gmx.net>
2012-01-18 17:36:30 +0100

    fix merge problem with ComponentToolbar

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/components/ComponentToolbar.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/components/ComponentToolbar.java
@@ -1,7 +1,45 @@
 package com.sap.sailing.gwt.ui.shared.components;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.sap.sailing.gwt.ui.client.StringMessages;
 
-public class ComponentTitleBar extends HorizontalPanel {
+public class ComponentToolbar extends HorizontalPanel {
+    
+    private static ComponentResources resources = GWT.create(ComponentResources.class);
+
+    private final Component<?> component;
+    final StringMessages stringMessages;
+    
+    public ComponentToolbar(final Component<?> component, final StringMessages stringMessages) {
+        super();
+        
+        this.setSpacing(10);
+        this.component = component;
+        this.stringMessages = stringMessages;
+    }
+
+    public void addSettingsButton() {
+        if(component.hasSettings()) {
+            ImageResource settingsImage = resources.settingsIcon();
+            Anchor showConfigAnchor = new Anchor(AbstractImagePrototype.create(settingsImage).getSafeHtml());
+            showConfigAnchor.setTitle(stringMessages.configuration());
+            showConfigAnchor.addClickHandler(new ClickHandler() {
+                @Override
+                public void onClick(ClickEvent event) {
+                    new SettingsDialog(component, stringMessages).show();
+                }
+            });
+    
+            this.add(showConfigAnchor);
+        }
+   }
 
 }
+
+
