721adfd236a46563aa7270c24cc39833535949c7
fmittag <fmittag@gmx.net>
2012-01-19 17:52:09 +0100

    added Timepanel to RaceBoard and connected TimePanel to RaceMap and LeaderBoard

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/RaceBoardPanel.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/RaceBoardPanel.java
@@ -4,63 +4,99 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.domain.common.impl.Util.Triple;
+import com.sap.sailing.gwt.ui.adminconsole.RaceMap;
+import com.sap.sailing.gwt.ui.adminconsole.RaceMapSettings;
+import com.sap.sailing.gwt.ui.adminconsole.TimePanel;
 import com.sap.sailing.gwt.ui.client.CompetitorSelectionModel;
 import com.sap.sailing.gwt.ui.client.ErrorReporter;
 import com.sap.sailing.gwt.ui.client.SailingServiceAsync;
 import com.sap.sailing.gwt.ui.client.StringMessages;
+import com.sap.sailing.gwt.ui.client.Timer;
 import com.sap.sailing.gwt.ui.leaderboard.LeaderboardPanel;
 import com.sap.sailing.gwt.ui.leaderboard.LeaderboardSettings;
+import com.sap.sailing.gwt.ui.shared.EventDTO;
+import com.sap.sailing.gwt.ui.shared.RaceDTO;
+import com.sap.sailing.gwt.ui.shared.RegattaDTO;
 import com.sap.sailing.gwt.ui.shared.components.Component;
 import com.sap.sailing.gwt.ui.shared.components.SettingsDialogComponent;
-import com.sap.sailing.server.api.DefaultLeaderboardName;
 
 public class RaceBoardPanel extends FormPanel implements Component<RaceBoardSettings> {
 
     private final SailingServiceAsync sailingService;
-
     private final ErrorReporter errorReporter;
-
-//    private final StringMessages stringMessages;
-
+    private final Triple<EventDTO, RegattaDTO, RaceDTO> selectedRace;
     private String raceBoardName;
 
+    private final Timer timer;
     private final List<CollapsableComponentViewer<?>> collapsableViewers;
     
-    public RaceBoardPanel(SailingServiceAsync sailingService, String raceBoardName, ErrorReporter errorReporter,
-            final StringMessages stringMessages) {
+    public RaceBoardPanel(SailingServiceAsync sailingService, final Triple<EventDTO, RegattaDTO, RaceDTO> theSelectedRace, String leaderboardName, 
+            ErrorReporter errorReporter, final StringMessages stringMessages) {
         this.sailingService = sailingService;
-        this.setRaceBoardName(raceBoardName);
+        this.selectedRace = theSelectedRace;
+        this.setRaceBoardName(selectedRace.getC().name);
         this.errorReporter = errorReporter;
         VerticalPanel mainPanel = new VerticalPanel();
         mainPanel.setSize("100%", "100%");
         setWidget(mainPanel);
+
+        timer = new Timer(/* delayBetweenAutoAdvancesInMilliseconds */500);
         collapsableViewers = new ArrayList<CollapsableComponentViewer<?>>();
-        for(int i = 0; i < 4; i++) {
-            if(i == 0) {
-                CompetitorSelectionModel competitorSelectionModel = new CompetitorSelectionModel(/* hasMultiSelection */ true);
-                //final RaceMapPanel raceMapPanel = new RaceMapPanel(sailingService, competitorSelectionModel, this, this, stringMessages);
-                LeaderboardPanel defaultLeaderboardPanel = new LeaderboardPanel(sailingService, competitorSelectionModel,
-                        DefaultLeaderboardName.DEFAULT_LEADERBOARD_NAME, errorReporter, stringMessages);
+        CompetitorSelectionModel competitorSelectionModel = new CompetitorSelectionModel(/* hasMultiSelection */ true);
 
-                CollapsableComponentViewer<LeaderboardSettings> viewer = new CollapsableComponentViewer<LeaderboardSettings>(
-                        defaultLeaderboardPanel, stringMessages);
-                collapsableViewers.add(viewer);
-            } else {
-                SimpleComponentGroup<Object> componentGroup = new SimpleComponentGroup<Object>("Component Group " + i);
-                componentGroup.addComponent(new SimpleComponent("My Component"));
-                componentGroup.addComponent(new SimpleComponent("My Component 2"));
-                componentGroup.addComponent(new SimpleComponent("My Component 3"));
+        // create the default leaderboard
+        LeaderboardPanel leaderboardPanel = new LeaderboardPanel(sailingService, competitorSelectionModel,
+                leaderboardName, errorReporter, stringMessages);
 
-                //LeaderboardPanel leaderboardPanel = LeaderboardPanel(); 
-                CollapsableComponentViewer<Object> viewer = new CollapsableComponentViewer<Object>(componentGroup, stringMessages);
-                collapsableViewers.add(viewer);
-            }
+        CollapsableComponentViewer<LeaderboardSettings> leaderboardViewer = new CollapsableComponentViewer<LeaderboardSettings>(
+                leaderboardPanel, "100%", "100%", stringMessages);
+        collapsableViewers.add(leaderboardViewer);
+
+        // create the race map
+        RaceMap raceMap = new RaceMap(sailingService, errorReporter, timer, competitorSelectionModel, stringMessages);
+        CollapsableComponentViewer<RaceMapSettings> raceMapViewer = new CollapsableComponentViewer<RaceMapSettings>(
+                raceMap, "600px", "300px", stringMessages);
+        raceMap.loadMapsAPI((Panel) raceMapViewer.getViewerWidget().getContent());
+        List<Triple<EventDTO, RegattaDTO, RaceDTO>> races = new ArrayList<Triple<EventDTO, RegattaDTO, RaceDTO>>();
+        races.add(selectedRace);
+        raceMap.onRaceSelectionChange(races);
+        collapsableViewers.add(raceMapViewer);
+        
+        // create some sample components
+        for(int i = 0; i < 2; i++) {
+            SimpleComponentGroup<Object> componentGroup = new SimpleComponentGroup<Object>("Component Group " + i);
+            componentGroup.addComponent(new SimpleComponent("My Component"));
+            componentGroup.addComponent(new SimpleComponent("My Component 2"));
+            componentGroup.addComponent(new SimpleComponent("My Component 3"));
+
+            collapsableViewers.add(new CollapsableComponentViewer<Object>(componentGroup, "100%", "100px", stringMessages));
         }
-        for (CollapsableComponentViewer<?> viewer : collapsableViewers) {
-            mainPanel.add(viewer.getViewerWidget());
+
+        for (CollapsableComponentViewer<?> componentViewer : collapsableViewers) {
+            mainPanel.add(componentViewer.getViewerWidget());
         }
+
+        timer.addTimeListener(leaderboardPanel);
+        timer.addTimeListener(raceMap);
+
+        TimePanel timePanel = new TimePanel(stringMessages, timer);
+        RaceDTO race = selectedRace.getC();
+        if (race.startOfRace != null) {
+            timePanel.timeChanged(race.startOfRace);
+            timer.setTime(race.startOfRace.getTime());
+        }
+        if (race.startOfTracking != null) {
+            timePanel.setMin(race.startOfTracking);
+        }
+        if (race.timePointOfNewestEvent != null) {
+            timePanel.setMax(race.timePointOfNewestEvent);
+        }
+        
+        mainPanel.add(timePanel);
     }
     
     @Override
