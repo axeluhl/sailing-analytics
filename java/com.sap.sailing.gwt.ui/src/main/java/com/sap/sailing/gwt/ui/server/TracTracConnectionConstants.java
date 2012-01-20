de4b65f0ccda341f766828598b2a2c4d86352bab
Lennart Hensler <lennart.hensler@sap.com>
2012-01-20 09:17:29 +0100

    Added interface for connection constants

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/TracTracConnectionConstants.java
@@ -0,0 +1,13 @@
+package com.sap.sailing.gwt.ui.server;
+
+public interface TracTracConnectionConstants {
+    
+    final String HOST_NAME = "germanmaster.traclive.dk";
+    
+    final int PORT_TUNNEL_LIVE = 1520;   //For official usage use 4412;
+    final int PORT_TUNNEL_STORED = 1521; //For official usage use 4413;
+    
+    final int PORT_LIVE = 1520;          //For official usage use 4400;
+    final int PORT_STORED = 1521;        //For official usage use 4401;
+
+}
