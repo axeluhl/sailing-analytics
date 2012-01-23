cf5832d5b2da6066253dde514ca6e4093595e650
Marcus Kammer <marcus.kammer@sap.com>
2012-01-23 16:00:02 +0100

    add RaceboardEntryPoint from mak-raceboard branch Add different flowPanels with label for headline

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/RaceBoardEntryPoint.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/RaceBoardEntryPoint.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.sap.sailing.gwt.ui.client.AbstractEntryPoint;
 import com.sap.sailing.gwt.ui.client.LogoAndTitlePanel;
@@ -91,7 +93,34 @@ public class RaceBoardEntryPoint extends AbstractEntryPoint {
         if (padding != null && Boolean.valueOf(padding)) {
             raceBoardPanel.addStyleName("leftPaddedPanel");
         }
-        RootPanel.get().add(logoAndTitlePanel);
-        RootPanel.get().add(raceBoardPanel);
+                    FlowPanel contentOuterPanel = new FlowPanel(); // outer div which centered page content
+                    contentOuterPanel.addStyleName("contentOuterPanel");
+                    contentOuterPanel.add(raceBoardPanel);
+                    
+                    FlowPanel timelinePanel = new FlowPanel();
+                    timelinePanel.addStyleName("timelinePanel");
+                    
+                    FlowPanel timelineInnerPanel = new FlowPanel();
+                    timelineInnerPanel.addStyleName("timelineInnerPanel");
+                    
+                    FlowPanel footerShadowPanel = new FlowPanel();
+                    footerShadowPanel.addStyleName("footerShadowPanel");
+                    
+                    FlowPanel breadcrumbPanel = new FlowPanel();
+                    breadcrumbPanel.addStyleName("breadcrumbPanel");
+                    Label eventNameLabel = new Label(eventName);
+                    eventNameLabel.addStyleName("eventNameHeadline");
+                    breadcrumbPanel.add(eventNameLabel);
+                    
+                    
+                    timelinePanel.add(timelineInnerPanel);
+                    
+                    RootPanel.get().add(breadcrumbPanel);
+                    RootPanel.get().add(contentOuterPanel);
+                    
+                    // Don't change this order because of the inner logic in html of "position fixed"-elements
+                    RootPanel.get().add(logoAndTitlePanel);                 // position:fixed        
+                    RootPanel.get().add(timelinePanel);                     // position:fixed
+                    RootPanel.get().add(footerShadowPanel);                 // position:fixed
     }
 }
