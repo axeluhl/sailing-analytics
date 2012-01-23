d306d95f8453de63af8e3ed4a0d9abeef88dae08
Lennart Hensler <lennart.hensler@sap.com>
2012-01-23 13:43:53 +0100

    Small refactoring

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/impl/KilometersPerHourSpeedImpl.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/impl/KilometersPerHourSpeedImpl.java
@@ -1,5 +1,6 @@
 package com.sap.sailing.domain.common.impl;
 
+import com.sap.sailing.domain.common.AbstractSpeedImpl;
 import com.sap.sailing.domain.common.Mile;
 import com.sap.sailing.domain.common.Speed;
 
