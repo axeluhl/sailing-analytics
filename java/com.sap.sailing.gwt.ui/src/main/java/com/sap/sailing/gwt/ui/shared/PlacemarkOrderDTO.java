5280dece8af2da1cbba02cb0a319248e36da6b62
Lennart Hensler <lennart.hensler@sap.com>
2012-02-23 12:58:31 +0100

    Created DTOs for Placemark and RacePlaceOrder

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/PlacemarkOrderDTO.java
@@ -0,0 +1,44 @@
+package com.sap.sailing.gwt.ui.shared;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class PlacemarkOrderDTO extends NamedDTO implements IsSerializable {
+    
+    private List<PlacemarkDTO> placemarks;
+    
+    public PlacemarkOrderDTO() {
+        placemarks = new ArrayList<PlacemarkDTO>();
+    }
+    
+    public PlacemarkOrderDTO(Collection<PlacemarkDTO> placemarks) {
+        this.placemarks = new ArrayList<PlacemarkDTO>(placemarks);
+    }
+    
+    public List<PlacemarkDTO> getPlacemarks() {
+        return placemarks;
+    }
+    
+    public void setPlacemarks(Collection<PlacemarkDTO> placemarks) {
+        this.placemarks = new ArrayList<PlacemarkDTO>(placemarks);;
+    }
+    
+    public String placemarksAsString() {
+        StringBuilder sb = new StringBuilder();
+        PlacemarkDTO placemarkBefore = placemarks.get(0);
+        sb.append(placemarkBefore.asString());
+        for (int i = 1; i < placemarks.size(); i++) {
+            PlacemarkDTO placemark = placemarks.get(i);
+            if (!placemarkBefore.equals(placemark)) {
+                sb.append(" -> ");
+                sb.append(placemark.asString());
+                placemarkBefore = placemark;
+            }
+        }
+        return sb.toString();
+    }
+    
+}
