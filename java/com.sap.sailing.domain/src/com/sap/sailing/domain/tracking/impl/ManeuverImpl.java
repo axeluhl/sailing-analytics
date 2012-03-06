696475e7782adfcaf0255cd297510baaf819e7c8
Axel Uhl <axel.uhl@sap.com>
2012-03-06 12:55:57 +0100

    added missing unit in toString()

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/ManeuverImpl.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/ManeuverImpl.java
@@ -68,6 +68,6 @@ public class ManeuverImpl extends AbstractGPSFixImpl implements Maneuver {
         return super.toString() + " " + type + " on new tack " + newTack + " on position " + position
                 + " at time point " + timePoint + ". " + "Speed before maneuver " + speedWithBearingBefore
                 + " speed after maneuver " + speedWithBearingAfter + ". The maneuver changed the course by "
-                + directionChangeInDegrees + ".";
+                + directionChangeInDegrees + "deg.";
     }
 }
