8ae9ba0ade42ecc0a36ae72171b148737c8f7e42
Lennart Hensler <lennart.hensler@sap.com>
2012-03-06 11:27:54 +0100

    Created the RaceTimesInfoProvider

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/RaceTimesInfoProvider.java
@@ -0,0 +1,96 @@
+package com.sap.sailing.gwt.ui.client;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.RepeatingCommand;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.sap.sailing.domain.common.RaceIdentifier;
+import com.sap.sailing.gwt.ui.shared.RaceTimesInfoDTO;
+
+public class RaceTimesInfoProvider {
+    
+    private SailingServiceAsync sailingService;
+    private ErrorReporter errorReporter;
+    
+    private RaceIdentifier raceIdentifier;
+    private long requestInterval;
+    
+    private RaceTimesInfoDTO raceTimesInfo;
+    
+    private Set<RaceTimesInfoChangeListener> listeners;
+
+    public RaceTimesInfoProvider(SailingServiceAsync sailingService, ErrorReporter errorReporter, RaceIdentifier raceIdentifier, long requestInterval) {
+        this.sailingService = sailingService;
+        this.errorReporter = errorReporter;
+        this.raceIdentifier = raceIdentifier;
+        this.requestInterval = requestInterval;
+        raceTimesInfo = null;
+        listeners = new HashSet<RaceTimesInfoChangeListener>();
+        
+        RepeatingCommand command = new RepeatingCommand() {
+            @Override
+            public boolean execute() {
+                readTimesInfo();
+                Scheduler.get().scheduleFixedPeriod(this, (int) RaceTimesInfoProvider.this.requestInterval);
+                return false;
+            }
+        };
+        Scheduler.get().scheduleFixedPeriod(command, (int) this.requestInterval);
+    }
+    
+    public RaceIdentifier getRaceIdentifier() {
+        return raceIdentifier;
+    }
+    
+    /**
+     * Sets the race identifier and resets the time info to <code>null</code>.
+     */
+    public void setRaceIdentifier(RaceIdentifier raceIdentifier) {
+        this.raceIdentifier = raceIdentifier;
+        raceTimesInfo = null;
+    }
+    
+    public long getRequestInterval() {
+        return requestInterval;
+    }
+    
+    public void setRequestInterval(long requestInterval){
+        this.requestInterval = requestInterval;
+    }
+    
+    /**
+     * @return The {@link RaceTimesInfoDTO times info} for the current {@link #raceIdentifier}, or <code>null</code> if no time info is available.
+     */
+    public RaceTimesInfoDTO getRaceTimesInfo(){
+        return raceTimesInfo;
+    }
+    
+    public void addRaceTimesInfoChangeListener(RaceTimesInfoChangeListener listener) {
+        listeners.add(listener);
+    }
+    
+    public void removeRaceTimesInfoChangeListener(RaceTimesInfoChangeListener listener){
+        listeners.remove(listener);
+    }
+    
+    private void readTimesInfo() {
+        if (raceIdentifier != null) {
+            sailingService.getRaceTimesInfo(raceIdentifier, new AsyncCallback<RaceTimesInfoDTO>() {
+                @Override
+                public void onSuccess(RaceTimesInfoDTO raceTimesInfo) {
+                    RaceTimesInfoProvider.this.raceTimesInfo = raceTimesInfo;
+                    for (RaceTimesInfoChangeListener listener : listeners) {
+                        listener.raceTimesInfoChanged(getRaceTimesInfo());
+                    }
+                }
+                @Override
+                public void onFailure(Throwable caught) {
+                    errorReporter.reportError("Error reading race timepoints: " + caught.getMessage());
+                }
+            });
+        }
+    }
+    
+}
