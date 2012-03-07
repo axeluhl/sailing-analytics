c5e6e035a7d9b8dffca33938cba84c53596fbc65
unknown <I059829@BUHN60198997A.(none)>
2012-03-07 17:39:31 +0100

    added a servlet

--- java/com.sap.sailing.simulator.test/src/com/sap/sailing/simulator/test/SimulatorTest.java
+++ java/com.sap.sailing.simulator.test/src/com/sap/sailing/simulator/test/SimulatorTest.java
@@ -1,10 +1,17 @@
 package com.sap.sailing.simulator.test;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
+
+import com.sap.sailing.simulator.impl.SailingSimulatorImpl;
 
 public class SimulatorTest {
 	@Test
 	public void myFirstTest() {
 		System.out.println("Hello world");
+		fail("Intentional failure");
+		SailingSimulatorImpl s = new SailingSimulatorImpl();
+		s.m();
 	}
 }
---
+++ java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/impl/SailingSimulatorImpl.java
@@ -0,0 +1,9 @@
+package com.sap.sailing.simulator.impl;
+
+import com.sap.sailing.simulator.SailingSimulator;
+
+public class SailingSimulatorImpl implements SailingSimulator {
+	public void m() {
+		
+	}
+}
