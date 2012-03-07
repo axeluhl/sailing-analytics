23e58256f1841fb2599e908e4d4ed4d1b1231145
unknown <I059829@BUHN60198997A.(none)>
2012-03-07 16:04:13 +0100

    Added two bundles for the SAP Sailing Simulator

---
+++ java/com.sap.sailing.simulator.test/src/com/sap/sailing/simulator/test/SimulatorTest.java
@@ -0,0 +1,10 @@
+package com.sap.sailing.simulator.test;
+
+import org.junit.Test;
+
+public class SimulatorTest {
+	@Test
+	public void myFirstTest() {
+		System.out.println("Hello world");
+	}
+}
---
+++ java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/SailingSimulator.java
@@ -0,0 +1,5 @@
+package com.sap.sailing.simulator;
+
+public interface SailingSimulator {
+
+}
