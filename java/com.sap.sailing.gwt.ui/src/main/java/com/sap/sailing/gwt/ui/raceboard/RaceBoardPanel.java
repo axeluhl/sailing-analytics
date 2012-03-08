97567a2996f89e5be6e8b5321cdd3cf810dfc648
fmittag <fmittag@gmx.net>
2012-03-08 12:20:20 +0100

    changed overall Layout mechanism of the Raceboard for the ONESCREEN mode from RootPanel to RootLayoutPanel

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/RaceBoardPanel.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/RaceBoardPanel.java
@@ -106,10 +106,10 @@ public class RaceBoardPanel extends FormPanel implements EventDisplayer, RaceSel
         componentsNavigationPanel.addStyleName("raceBoardNavigation");
 
         switch (viewMode) {
-            case CASCADING:
+            case CASCADE:
                 createCascadingView(leaderboardName, leaderboardGroupName, mainPanel);
                 break;
-            case ONE_SCREEN:
+            case ONESCREEN:
                 createOneScreenView(leaderboardName, leaderboardGroupName, mainPanel);                
                 break;
         }
@@ -145,7 +145,8 @@ public class RaceBoardPanel extends FormPanel implements EventDisplayer, RaceSel
         windChart.setVisible(false);
         components.add(windChart);
         
-        SideBySideComponentViewer leaderboardAndMapViewer = new SideBySideComponentViewer(leaderboardPanel, raceMap, components, "auto", "500px");  
+        SideBySideComponentViewer leaderboardAndMapViewer = new SideBySideComponentViewer(leaderboardPanel, raceMap, 
+                components, "100%", "100%");  
         componentViewers.add(leaderboardAndMapViewer);
             
         for (ComponentViewer componentViewer : componentViewers) {
@@ -153,7 +154,7 @@ public class RaceBoardPanel extends FormPanel implements EventDisplayer, RaceSel
         }
         
         addComponentAsToogleButtonToNavigationMenu(leaderboardAndMapViewer, leaderboardPanel);
-        addComponentAsToogleButtonToNavigationMenu(leaderboardAndMapViewer, raceMap);
+        //addComponentAsToogleButtonToNavigationMenu(leaderboardAndMapViewer, raceMap);
         addComponentAsToogleButtonToNavigationMenu(leaderboardAndMapViewer, windChart);
         addComponentAsToogleButtonToNavigationMenu(leaderboardAndMapViewer, competitorCharts);
     }
