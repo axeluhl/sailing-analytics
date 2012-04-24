dde49f87e56fee5c477b29e85cb032d1cb2424a0
Axel Uhl <axel.uhl@sap.com>
2012-04-24 15:06:20 +0200

    provided empty replication test class with a test method to avoid Maven / Surefire error message

--- java/com.sap.sailing.server.replication.test/src/com/sap/sailing/server/replication/test/ServerReplicationTest.java
+++ java/com.sap.sailing.server.replication.test/src/com/sap/sailing/server/replication/test/ServerReplicationTest.java
@@ -1,5 +1,10 @@
 package com.sap.sailing.server.replication.test;
 
-public class ServerReplicationTest {
+import org.junit.Test;
 
+public class ServerReplicationTest {
+    @Test
+    public void testSomething() {
+        // does nothing but avoid a Maven / Surefire error message
+    }
 }
