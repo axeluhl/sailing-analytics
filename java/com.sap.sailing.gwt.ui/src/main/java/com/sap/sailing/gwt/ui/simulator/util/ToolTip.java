34c2a472099b49f6c606cf161503249f0ac48d06
Nidhi Sawhney <Nidhi.Sawhney@sap.com>
2012-04-27 10:41:38 +0200

    Helper class to display tool tips for the CanvasOverlay class.

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/util/ToolTip.java
@@ -0,0 +1,74 @@
+package com.sap.sailing.gwt.ui.simulator.util;
+
+/**
+ * A class to implement a 2d point to support the capture of area aroud the point for displaying tooltips.
+ * The co-ordinates are in pixels.
+ * 
+ * @author Nidhi Sawhney(D054070)
+ */
+public class ToolTip {
+
+    private double x;
+    private double y;
+    
+    /*
+     * The tolerance in pixels around the point where the tool tip is to be displayed 
+     */
+    static final int toolTipTolerance = 50;
+    
+    /**
+     * The dimensions of the tool tip to be displayed
+     */
+    public static final double toolRectW = 80;
+    public static final double toolRectH = 30;
+    
+    /**
+     * 
+     * @param x coordinate value in pixels
+     * @param y coordinate value in pixels
+     */
+    public ToolTip(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other != null) {
+            ToolTip otherPoint = (ToolTip) other;
+
+            boolean value = (this.getX() >= otherPoint.getX() - toolTipTolerance && this.getX() <= otherPoint.getX()
+                    + toolTipTolerance)
+                    && (this.getY() >= otherPoint.getY() - toolTipTolerance && this.getY() <= otherPoint.getY()
+                            + toolTipTolerance);
+            return value;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (x + y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
