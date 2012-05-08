a227f15386e03ecf9cd5aa0fb4e5ebbac4af3eaa
Alexandru Dimofte <alexandru.dimofte@sap.com>
2012-05-08 14:15:06 +0300

    added dummy functionallity to SailingSimulatorImpl - a dummy path is generated

--- java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/impl/SailingSimulatorImpl.java
+++ java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/impl/SailingSimulatorImpl.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.sap.sailing.domain.base.impl.MillisecondsTimePoint;
+import com.sap.sailing.domain.common.Bearing;
 import com.sap.sailing.domain.common.Position;
 import com.sap.sailing.domain.common.TimePoint;
 import com.sap.sailing.simulator.Boundary;
 import com.sap.sailing.simulator.Path;
+import com.sap.sailing.simulator.PolarDiagram;
 import com.sap.sailing.simulator.SailingSimulator;
 import com.sap.sailing.simulator.SimulationParameters;
 import com.sap.sailing.simulator.TimedPositionWithSpeed;
@@ -31,10 +33,18 @@ public class SailingSimulatorImpl implements SailingSimulator {
 	public Path getOptimumPath() {
 		Boundary boundary = simulationParameters.getBoundaries();
 		WindField wf = simulationParameters.getWindField();
+		PolarDiagram pd = simulationParameters.getBoatPolarDiagram();
 		Position start = simulationParameters.getCourse().get(0);
 		Position end = simulationParameters.getCourse().get(1);
 		TimePoint startTime = new MillisecondsTimePoint(0);
 		List<TimedPositionWithSpeed> lst = new ArrayList<TimedPositionWithSpeed>();
+		
+		pd.setWind(wf.getWind(new TimedPositionWithSpeedSimple(start)));
+		Bearing direct = start.getBearingGreatCircle(end);
+		TimedPositionWithSpeed p1 = new TimedPositionWithSpeedImpl(startTime, start, pd.getSpeedAtBearing(direct));
+		TimedPositionWithSpeed p2 = new TimedPositionWithSpeedImpl(new MillisecondsTimePoint(3600000), end, null);
+		lst.add(p1);
+		lst.add(p2);
 		
 		//algorithm goes here
 		
