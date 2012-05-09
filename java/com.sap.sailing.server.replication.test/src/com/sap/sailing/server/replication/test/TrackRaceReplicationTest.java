01dcde73a3cac963d39829726576e39a3b23a943
Axel Uhl <axel.uhl@sap.com>
2012-05-09 13:38:34 +0200

    test replication of default re-assignment of tracked race to referencing leaderboard(s)

--- java/com.sap.sailing.server.replication.test/src/com/sap/sailing/server/replication/test/TrackRaceReplicationTest.java
+++ java/com.sap.sailing.server.replication.test/src/com/sap/sailing/server/replication/test/TrackRaceReplicationTest.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import com.sap.sailing.domain.base.impl.MillisecondsTimePoint;
 import com.sap.sailing.domain.common.DefaultLeaderboardName;
 import com.sap.sailing.domain.common.EventAndRaceIdentifier;
+import com.sap.sailing.domain.common.EventNameAndRaceName;
 import com.sap.sailing.domain.common.impl.Util;
 import com.sap.sailing.domain.leaderboard.Leaderboard;
 import com.sap.sailing.domain.leaderboard.RaceInLeaderboard;
@@ -27,12 +28,17 @@ import com.sap.sailing.domain.tracking.TrackedRace;
 import com.sap.sailing.domain.tracking.impl.EmptyWindStore;
 import com.sap.sailing.server.OperationExecutionListener;
 import com.sap.sailing.server.RacingEventServiceOperation;
+import com.sap.sailing.server.operationaltransformation.AddColumnToLeaderboard;
+import com.sap.sailing.server.operationaltransformation.ConnectTrackedRaceToLeaderboardColumn;
+import com.sap.sailing.server.operationaltransformation.CreateLeaderboard;
 import com.sap.sailing.server.operationaltransformation.CreateTrackedRace;
 
 public class TrackRaceReplicationTest extends AbstractServerReplicationTest {
     private TrackedRace masterTrackedRace;
     private EventAndRaceIdentifier raceIdentifier;
     private RacesHandle racesHandle;
+    private final boolean[] notifier = new boolean[1];
+    private RaceTrackingConnectivityParameters trackingParams;
 
     @Before
     @Override
@@ -48,7 +54,6 @@ public class TrackRaceReplicationTest extends AbstractServerReplicationTest {
         MillisecondsTimePoint startOfTracking = new MillisecondsTimePoint(cal.getTimeInMillis());
         cal.set(2011, 05, 23, 15, 14, 31);
         MillisecondsTimePoint endOfTracking = new MillisecondsTimePoint(cal.getTimeInMillis());
-        final boolean[] notifier = new boolean[1];
         master.addOperationExecutionListener(new OperationExecutionListener() {
             @Override
             public <T> void executed(RacingEventServiceOperation<T> operation) {
@@ -60,20 +65,32 @@ public class TrackRaceReplicationTest extends AbstractServerReplicationTest {
                 }
             }
         });
-        RaceTrackingConnectivityParameters trackingParams = com.sap.sailing.domain.tractracadapter.DomainFactory.INSTANCE.createTrackingConnectivityParameters(paramURL,
+        trackingParams = com.sap.sailing.domain.tractracadapter.DomainFactory.INSTANCE.createTrackingConnectivityParameters(paramURL,
                 liveURI, storedURI, startOfTracking, endOfTracking, EmptyWindStore.INSTANCE);
+    }
+
+    private void startTracking() throws Exception, InterruptedException {
+        startTrackingOnMaster();
+        waitForTrackRaceReplicationTrigger();
+        raceIdentifier = racesHandle.getRaceTracker().getRaceIdentifiers().iterator().next();
+        masterTrackedRace = master.getTrackedRace(raceIdentifier);
+    }
+
+    private void startTrackingOnMaster() throws Exception {
         racesHandle = master.addRace(trackingParams, EmptyWindStore.INSTANCE, /* timeoutInMilliseconds */ 60000);
+    }
+
+    private void waitForTrackRaceReplicationTrigger() throws InterruptedException {
         while (!notifier[0]) {
             synchronized (notifier) {
                 notifier.wait();
             }
         }
-        raceIdentifier = racesHandle.getRaceTracker().getRaceIdentifiers().iterator().next();
-        masterTrackedRace = master.getTrackedRace(raceIdentifier);
     }
     
     @Test
     public void testStartTrackingRaceReplication() throws Exception {
+        startTracking();
         Thread.sleep(1000);
         TrackedRace replicaTrackedRace = replica.getTrackedRace(raceIdentifier);
         assertNotNull(replicaTrackedRace);
@@ -82,6 +99,30 @@ public class TrackRaceReplicationTest extends AbstractServerReplicationTest {
         assertEquals(Util.size(masterTrackedRace.getRace().getCompetitors()), Util.size(replicaTrackedRace.getRace().getCompetitors()));
         Leaderboard replicaDefaultLeaderboard = replica.getLeaderboardByName(DefaultLeaderboardName.DEFAULT_LEADERBOARD_NAME);
         RaceInLeaderboard column = replicaDefaultLeaderboard.getRaceColumnByName(replicaTrackedRace.getRace().getName());
+        assertNotNull(column);
+        assertSame(replicaTrackedRace, column.getTrackedRace());
+        tearDown();
+    }
+
+    @Test
+    public void testReassignmentToLeaderboardReplication() throws Exception {
+        final String leaderboardName = "Test Leaderboard";
+        master.apply(new CreateLeaderboard(leaderboardName, new int[0]));
+        final String columnName = "R1";
+        RaceInLeaderboard masterColumn = master.apply(new AddColumnToLeaderboard(columnName, leaderboardName, /* medalRace */ false));
+        master.apply(new ConnectTrackedRaceToLeaderboardColumn(leaderboardName, columnName, new EventNameAndRaceName(
+                "Academy Tracking 2011 (STG)", "weym470may122011")));
+        startTracking();
+        assertNotNull(masterColumn.getTrackedRace()); // ensure the re-assignment worked on the master
+        Thread.sleep(1000);
+        TrackedRace replicaTrackedRace = replica.getTrackedRace(raceIdentifier);
+        assertNotNull(replicaTrackedRace);
+        assertNotSame(masterTrackedRace, replicaTrackedRace);
+        assertNotSame(masterTrackedRace.getRace(), replicaTrackedRace.getRace());
+        assertEquals(Util.size(masterTrackedRace.getRace().getCompetitors()), Util.size(replicaTrackedRace.getRace().getCompetitors()));
+        Leaderboard replicaLeaderboard = replica.getLeaderboardByName(leaderboardName);
+        assertNotNull(replicaLeaderboard);
+        RaceInLeaderboard column = replicaLeaderboard.getRaceColumnByName(columnName);
         assertNotNull(column);
         assertSame(replicaTrackedRace, column.getTrackedRace());
         tearDown();
