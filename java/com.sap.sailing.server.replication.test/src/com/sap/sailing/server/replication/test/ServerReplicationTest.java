b4dc4008a9b1a1e0c8c35c9febe1a29af285515c
Axel Uhl <axel.uhl@sap.com>
2012-05-09 12:16:50 +0200

    fixed first part of bug 597, testing the replication of tracking a race

--- java/com.sap.sailing.server.replication.test/src/com/sap/sailing/server/replication/test/ServerReplicationTest.java
+++ java/com.sap.sailing.server.replication.test/src/com/sap/sailing/server/replication/test/ServerReplicationTest.java
@@ -4,10 +4,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import org.junit.Test;
 
@@ -19,14 +24,25 @@ import com.sap.sailing.domain.base.Event;
 import com.sap.sailing.domain.base.RaceDefinition;
 import com.sap.sailing.domain.base.Waypoint;
 import com.sap.sailing.domain.base.impl.CourseImpl;
+import com.sap.sailing.domain.base.impl.MillisecondsTimePoint;
 import com.sap.sailing.domain.base.impl.RaceDefinitionImpl;
 import com.sap.sailing.domain.common.DefaultLeaderboardName;
+import com.sap.sailing.domain.common.EventAndRaceIdentifier;
 import com.sap.sailing.domain.common.EventName;
 import com.sap.sailing.domain.common.impl.Util;
 import com.sap.sailing.domain.leaderboard.Leaderboard;
+import com.sap.sailing.domain.leaderboard.RaceInLeaderboard;
+import com.sap.sailing.domain.test.AbstractTracTracLiveTest;
+import com.sap.sailing.domain.tracking.RaceTrackingConnectivityParameters;
+import com.sap.sailing.domain.tracking.RacesHandle;
+import com.sap.sailing.domain.tracking.TrackedRace;
+import com.sap.sailing.domain.tracking.impl.EmptyWindStore;
+import com.sap.sailing.server.OperationExecutionListener;
+import com.sap.sailing.server.RacingEventServiceOperation;
 import com.sap.sailing.server.operationaltransformation.AddEvent;
 import com.sap.sailing.server.operationaltransformation.AddRaceDefinition;
 import com.sap.sailing.server.operationaltransformation.CreateLeaderboard;
+import com.sap.sailing.server.operationaltransformation.CreateTrackedRace;
 import com.sap.sailing.server.operationaltransformation.RemoveLeaderboard;
 
 public class ServerReplicationTest extends AbstractServerReplicationTest {
@@ -102,7 +118,49 @@ public class ServerReplicationTest extends AbstractServerReplicationTest {
     }
     
     @Test
-    public void testStartTrackingRaceReplication() {
-        
+    public void testStartTrackingRaceReplication() throws Exception {
+        final String eventID = "event_20110505_SailingTea";
+        final String raceID = "bd8c778e-7c65-11e0-8236-406186cbf87c";
+        URL paramURL = AbstractTracTracLiveTest.getParamURL(eventID, raceID);
+        URI liveURI = AbstractTracTracLiveTest.getLiveURI();
+        URI storedURI = AbstractTracTracLiveTest.getStoredURI();
+        GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        cal.set(2011, 05, 23, 13, 14, 31);
+        MillisecondsTimePoint startOfTracking = new MillisecondsTimePoint(cal.getTimeInMillis());
+        cal.set(2011, 05, 23, 15, 14, 31);
+        MillisecondsTimePoint endOfTracking = new MillisecondsTimePoint(cal.getTimeInMillis());
+        final boolean[] notifier = new boolean[1];
+        master.addOperationExecutionListener(new OperationExecutionListener() {
+            @Override
+            public <T> void executed(RacingEventServiceOperation<T> operation) {
+                if (operation instanceof CreateTrackedRace) {
+                    synchronized (notifier) {
+                        notifier[0] = true;
+                        notifier.notifyAll();
+                    }
+                }
+            }
+        });
+        RaceTrackingConnectivityParameters trackingParams = com.sap.sailing.domain.tractracadapter.DomainFactory.INSTANCE.createTrackingConnectivityParameters(paramURL,
+                liveURI, storedURI, startOfTracking, endOfTracking, EmptyWindStore.INSTANCE);
+        RacesHandle racesHandle = master.addRace(trackingParams, EmptyWindStore.INSTANCE, /* timeoutInMilliseconds */ 60000);
+        while (!notifier[0]) {
+            synchronized (notifier) {
+                notifier.wait();
+            }
+        }
+        EventAndRaceIdentifier raceIdentifier = racesHandle.getRaceTracker().getRaceIdentifiers().iterator().next();
+        TrackedRace masterTrackedRace = master.getTrackedRace(raceIdentifier);
+        Thread.sleep(1000);
+        TrackedRace replicaTrackedRace = replica.getTrackedRace(raceIdentifier);
+        assertNotNull(replicaTrackedRace);
+        assertNotSame(masterTrackedRace, replicaTrackedRace);
+        assertNotSame(masterTrackedRace.getRace(), replicaTrackedRace.getRace());
+        assertEquals(Util.size(masterTrackedRace.getRace().getCompetitors()), Util.size(replicaTrackedRace.getRace().getCompetitors()));
+        Leaderboard replicaDefaultLeaderboard = replica.getLeaderboardByName(DefaultLeaderboardName.DEFAULT_LEADERBOARD_NAME);
+        RaceInLeaderboard column = replicaDefaultLeaderboard.getRaceColumnByName(replicaTrackedRace.getRace().getName());
+        assertNotNull(column);
+        assertSame(replicaTrackedRace, column.getTrackedRace());
+        racesHandle.getRaceTracker().stop();
     }
 }
