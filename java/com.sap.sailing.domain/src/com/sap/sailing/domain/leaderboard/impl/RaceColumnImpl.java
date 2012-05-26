9a87148983289fcef2b80bb4a5ff1e9036862166
Axel Uhl <axel.uhl@sap.com>
2012-05-26 19:37:44 +0200

    addressing bug 612: JSON/REST export of leaderboard data

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/leaderboard/impl/RaceColumnImpl.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/leaderboard/impl/RaceColumnImpl.java
@@ -1,5 +1,9 @@
 package com.sap.sailing.domain.leaderboard.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import com.sap.sailing.domain.base.Fleet;
 import com.sap.sailing.domain.base.impl.AbstractRaceColumn;
 import com.sap.sailing.domain.leaderboard.FlexibleRaceColumn;
@@ -17,7 +21,12 @@ public class RaceColumnImpl extends AbstractRaceColumn implements FlexibleRaceCo
     public RaceColumnImpl(String name, boolean medalRace, Iterable<Fleet> fleets) {
         super(name);
         this.medalRace = medalRace;
-        this.fleets = fleets;
+        List<Fleet> myFleets = new ArrayList<Fleet>();
+        for (Fleet fleet : fleets) {
+            myFleets.add(fleet);
+        }
+        Collections.sort(myFleets);
+        this.fleets = myFleets;
     }
     
     @Override
