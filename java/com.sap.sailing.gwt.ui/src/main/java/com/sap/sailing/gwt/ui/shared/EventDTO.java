79c0d2c85f7213fbde34c57534f1ec3f0728138b
fmittag <fmittag@gmx.net>
2012-05-30 18:04:02 +0200

    first functions of the new UI for managing events, regattas, races, etc Includes a renaming of listEvents() in RacingEventService to getRegattas()

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/EventDTO.java
@@ -0,0 +1,19 @@
+package com.sap.sailing.gwt.ui.shared;
+
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class EventDTO extends NamedDTO implements IsSerializable {
+    public List<RegattaDTO> regattas;
+    public VenueDTO venue;
+    
+    public EventDTO() {
+    }
+
+    public EventDTO(String name, List<RegattaDTO> regattas) {
+        super(name);
+        this.regattas = regattas;
+    }
+
+}
