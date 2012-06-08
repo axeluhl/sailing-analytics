1fceac9d2d1243bada76617be1564cc3b58b848e
Axel Uhl <axel.uhl@sap.com>
2012-06-08 13:39:23 +0200

    added more logging; first implementation class for b+m KiWo result importer

--- java/com.sap.sailing.kiworesultimport/src/com/sap/sailing/kiworesultimport/ResultListParser.java
+++ java/com.sap.sailing.kiworesultimport/src/com/sap/sailing/kiworesultimport/ResultListParser.java
@@ -1,7 +1,12 @@
 package com.sap.sailing.kiworesultimport;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
 public interface ResultListParser {
-    ResultList parse(InputStream inputStream);
+    ResultList parse(InputStream inputStream) throws SAXException, IOException, ParserConfigurationException;
 }
