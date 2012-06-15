41ab835868d7e23d5b4b09ccb18425191f685e05
Axel Uhl <axel.uhl@sap.com>
2012-06-15 17:34:51 +0200

    re-enabled test case because special serializable class replaced by Throwable

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/SerializableObjectWithJavaLangObjectHashCode.java
+++
@@ -1,7 +0,0 @@
-package com.sap.sailing.domain.test;
-
-import java.io.Serializable;
-
-public class SerializableObjectWithJavaLangObjectHashCode implements Serializable {
-    private static final long serialVersionUID = 696920899165910830L;
-}
