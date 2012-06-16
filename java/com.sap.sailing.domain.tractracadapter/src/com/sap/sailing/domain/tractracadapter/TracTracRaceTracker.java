ba6a2c7da0b4dc2eeeb1dac490f34057c4e31851
Axel Uhl <axel.uhl@sap.com>
2012-06-16 21:58:05 +0200

    fixing bug 773: waiting only for some time for the RaceDefinition; if wrong regatta, tracker stops

--- java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/TracTracRaceTracker.java
+++ java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/TracTracRaceTracker.java
@@ -7,7 +7,6 @@ import com.sap.sailing.domain.common.impl.Util.Triple;
 import com.sap.sailing.domain.tracking.RaceTracker;
 
 public interface TracTracRaceTracker extends RaceTracker {
-
     /**
      * returns the paramURL, liveURI and storedURI for the TracTrac connection maintained by this tracker
      */
