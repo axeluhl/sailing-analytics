3dab42377a845f2d9de3d2c0ca34a8f8f761d179
Nidhi Sawhney <Nidhi.Sawhney@sap.com>
2012-06-20 13:49:44 +0200

    Sorting by path time to get the same ordering.

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/PathDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/PathDTO.java
@@ -23,5 +23,13 @@ public class PathDTO extends NamedDTO implements IsSerializable {
     public void setMatrix(List<WindDTO> matrix) {
         this.matrix = matrix;
     }
+    
+    public long getPathTime() {
+    	if (matrix == null || matrix.size() == 0) {
+    		return 0;
+    	}
+    	
+    	return matrix.get(matrix.size()-1).timepoint - matrix.get(0).timepoint;
+    }
 
 }
