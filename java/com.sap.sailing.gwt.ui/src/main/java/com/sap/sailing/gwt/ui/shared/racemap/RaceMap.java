axeluhl/sailing-analytics

commit ed821991306715364fcc3365f208c289603c674f

reverted back to using map view by default; satellite doesn't support the same zoom level as map

java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/racemap/RaceMap.java
@@ -267,7 +267,6 @@ public class RaceMap extends AbsolutePanel implements TimeListener, CompetitorSe
                 map.setScrollWheelZoomEnabled(true);
                 map.setContinuousZoom(true);
                 map.setCurrentMapType(MapType.getSatelliteMap());
-                RaceMap.this.add(map, 0, 0);
                 RaceMap.this.add(combinedWindPanel, 10, 10);
                 RaceMap.this.raceMapImageManager.loadMapIcons(map);
                 map.setSize("100%", "100%");

commit 488b9a8743793a7ceea134bff43b32305f34844e

reverted horrible accidental bug introduced by ed821991306715364fcc3365f208c289603c674f which elminiated the Google Map altogether

java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/racemap/RaceMap.java
@@ -20,7 +20,6 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.DateTimeFormat.PredefinedFormat;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.maps.client.InfoWindowContent;
-import com.google.gwt.maps.client.MapType;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.Maps;
 import com.google.gwt.maps.client.control.ControlAnchor;
@@ -47,9 +46,9 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RequiresResize;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.sap.sailing.domain.common.RegattaAndRaceIdentifier;
 import com.sap.sailing.domain.common.ManeuverType;
 import com.sap.sailing.domain.common.RaceIdentifier;
+import com.sap.sailing.domain.common.RegattaAndRaceIdentifier;
 import com.sap.sailing.domain.common.Tack;
 import com.sap.sailing.domain.common.WindSource;
 import com.sap.sailing.domain.common.WindSourceType;
@@ -266,7 +265,7 @@ public class RaceMap extends AbsolutePanel implements TimeListener, CompetitorSe
                 // Add the map to the HTML host page
                 map.setScrollWheelZoomEnabled(true);
                 map.setContinuousZoom(true);
-                map.setCurrentMapType(MapType.getSatelliteMap());
+                RaceMap.this.add(map, 0, 0);
                 RaceMap.this.add(combinedWindPanel, 10, 10);
                 RaceMap.this.raceMapImageManager.loadMapIcons(map);
                 map.setSize("100%", "100%");