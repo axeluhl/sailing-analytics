1c24d022dd90069d641ea95d0d32bee455077b83
Axel Uhl <axel.uhl@sap.com>
2012-07-27 09:33:57 +0200

    fixed bug 892: discarding rules

--- java/com.sap.sailing.server.test/src/com/sap/sailing/server/test/LeaderboardDiscardingRulesTest.java
+++ java/com.sap.sailing.server.test/src/com/sap/sailing/server/test/LeaderboardDiscardingRulesTest.java
@@ -13,6 +13,7 @@ import com.sap.sailing.domain.base.DomainFactory;
 import com.sap.sailing.domain.base.Fleet;
 import com.sap.sailing.domain.base.impl.FleetImpl;
 import com.sap.sailing.domain.base.impl.MillisecondsTimePoint;
+import com.sap.sailing.domain.common.MaxPointsReason;
 import com.sap.sailing.domain.common.NoWindException;
 import com.sap.sailing.domain.common.TimePoint;
 import com.sap.sailing.domain.leaderboard.FlexibleLeaderboard;
@@ -90,4 +91,45 @@ public class LeaderboardDiscardingRulesTest {
         assertEquals(123.+123., totalPointsHasso,  0.000000001);
     }
 
+    /**
+     * See bug 892: If a competitor has two or more equally-scored races of which the to-be-discarded number
+     * has to be discarded, ensure that not only one but all of them are discarded and that the sailor isn't awarded
+     * more races sailed than the competition
+     */
+    @Test
+    public void testDiscardingRulesForMultipleEquallyBadRacesWithNonDiscardableDisqualification() throws NoWindException {
+        racingEventService.removeLeaderboard(LEADERBOARDNAME);
+        racingEventService.addFlexibleLeaderboard(LEADERBOARDNAME, new int[] { 1, 2 });
+        FlexibleLeaderboard leaderboard = (FlexibleLeaderboard) racingEventService.getLeaderboardByName(LEADERBOARDNAME);
+        assertNotNull(leaderboard);
+        BoatClass boatClass = DomainFactory.INSTANCE.getOrCreateBoatClass("29erXX", /* typicallyStartsUpwind */ true);
+        Fleet fleet = new FleetImpl("Default");
+
+        Competitor hasso = AbstractLeaderboardTest.createCompetitor("Dr. Hasso Plattner");
+        final TrackedRace race1 = new MockedTrackedRaceWithFixedRank(hasso, /* rank */ 123, /* started */ true, boatClass);
+        leaderboard.addRace(race1, "R1", /* medalRace */ false, fleet);
+        final TrackedRace race2 = new MockedTrackedRaceWithFixedRank(hasso, /* rank */ 123, /* started */ true, boatClass);
+        leaderboard.addRace(race2, "R2", /* medalRace */ false, fleet);
+
+        Competitor wolfgang = AbstractLeaderboardTest.createCompetitor("Wolfgang Hunger");
+        final TrackedRace race3 = new MockedTrackedRaceWithFixedRank(wolfgang, /* rank */ 123, /* started */ true, boatClass);
+        leaderboard.addRace(race3, "R3", /* medalRace */ false, fleet);
+        final TrackedRace race4 = new MockedTrackedRaceWithFixedRank(wolfgang, /* rank */ 124, /* started */ true, boatClass);
+        leaderboard.addRace(race4, "R4", /* medalRace */ false, fleet);
+
+        leaderboard.getScoreCorrection().correctScore(hasso, leaderboard.getRaceColumnByName("R3"), 123.);
+        leaderboard.getScoreCorrection().correctScore(hasso, leaderboard.getRaceColumnByName("R4"), 123.);
+        leaderboard.getScoreCorrection().setMaxPointsReason(hasso, leaderboard.getRaceColumnByName("R1"), MaxPointsReason.DNE);
+        leaderboard.getScoreCorrection().correctScore(hasso, leaderboard.getRaceColumnByName("R1"), 123.);
+        leaderboard.getScoreCorrection().correctScore(wolfgang, leaderboard.getRaceColumnByName("R1"), 122.);
+        leaderboard.getScoreCorrection().correctScore(wolfgang, leaderboard.getRaceColumnByName("R2"), 122.);
+        
+        TimePoint now = MillisecondsTimePoint.now();
+        // Hunger scores better than Plattner in this case because Hunger's two discards are 123/124, keeping 122/122
+        // whereas Plattner is to discard two of the four 123 results, keeping 123/123, being two points worse than Hunger:
+        double totalPointsHasso = leaderboard.getTotalPoints(hasso, now);
+        double totalPointsWolfgang = leaderboard.getTotalPoints(wolfgang, now);
+        assertEquals(122.+122., totalPointsWolfgang, 0.0000000001);
+        assertEquals(123.+123., totalPointsHasso,  0.000000001);
+    }
 }
