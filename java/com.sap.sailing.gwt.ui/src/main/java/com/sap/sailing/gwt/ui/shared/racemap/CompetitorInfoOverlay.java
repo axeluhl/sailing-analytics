9b1df540948b2df2cd6c15bbf4bdb4e809a280d6
unknown <Frank@OpenSPORTS.(none)>
2012-08-05 13:50:59 +0200

    some finetuning of the info window

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/racemap/CompetitorInfoOverlay.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/racemap/CompetitorInfoOverlay.java
@@ -28,6 +28,8 @@ public class CompetitorInfoOverlay extends CanvasOverlay {
 
     private int canvasWidth;
     private int canvasHeight;
+    private int infoBoxHeight;
+    private int infoBoxWidth;
     private double cornerRadius;
     
     public CompetitorInfoOverlay(CompetitorDTO competitorDTO, RaceMapImageManager raceMapImageManager) {
@@ -36,8 +38,10 @@ public class CompetitorInfoOverlay extends CanvasOverlay {
         this.raceMapImageManager = raceMapImageManager;
         
         canvasWidth = 20;
-        canvasHeight = 25;
-        cornerRadius = 10;
+        canvasHeight = 45;
+        infoBoxWidth = 20;
+        infoBoxHeight = 20;
+        cornerRadius = 4;
 
         if(getCanvas() != null) {
             getCanvas().setWidth(String.valueOf(canvasWidth));
@@ -62,31 +66,52 @@ public class CompetitorInfoOverlay extends CanvasOverlay {
             }
 
             Context2d context2d = getCanvas().getContext2d();
+            CssColor grayTransparentColor = CssColor.make("rgba(255,255,255,0.75)");
 
             context2d.setFont("12px bold Verdana sans-serif");
             TextMetrics measureText = context2d.measureText(competitorDTO.sailID);
             double textWidth = measureText.getWidth();
 
             canvasWidth = (int) textWidth + 17;
+            infoBoxWidth = canvasWidth;
             getCanvas().setWidth(String.valueOf(canvasWidth));
             getCanvas().setCoordinateSpaceWidth(canvasWidth);
             
             // Change origin and dimensions to match true size (a stroke makes the shape a bit larger)
-            context2d.setFillStyle("gray");
-//            context2d.strokeRect(cornerRadius/2, cornerRadius/2, canvasWidth-cornerRadius, canvasHeight-cornerRadius);
-            context2d.setFillStyle(CssColor.make("rgba(255,255,255,0.75)"));
-            context2d.fillRect(cornerRadius/2, cornerRadius/2, canvasWidth-cornerRadius, canvasHeight-cornerRadius);
+            context2d.setFillStyle(grayTransparentColor);
+            drawRoundedRect(context2d, cornerRadius/2, cornerRadius/2, infoBoxWidth-cornerRadius, infoBoxHeight-cornerRadius, cornerRadius);
             
+            context2d.setStrokeStyle("black");
+            context2d.setLineWidth(1);
+            context2d.beginPath();
+            context2d.moveTo(cornerRadius/2, infoBoxHeight/2);
+            context2d.lineTo(cornerRadius/2, canvasHeight);
+            context2d.stroke(); 
+
             context2d.beginPath();
             context2d.setFillStyle("black");
-            
-            context2d.fillText(competitorDTO.sailID, 8, 17);
+            context2d.fillText(competitorDTO.sailID, 8, 14);
             context2d.stroke(); 
 
             Point boatPositionInPx = getMap().convertLatLngToDivPixel(latLngPosition);
-            getPane().setWidgetPosition(getCanvas(), boatPositionInPx.getX(), boatPositionInPx.getY());
+            getPane().setWidgetPosition(getCanvas(), boatPositionInPx.getX() - 10, boatPositionInPx.getY() - canvasHeight);
         }
     }
+    
+	public static void drawRoundedRect(Context2d context, double x, double y, double w, double h, double r) {
+		context.beginPath();
+		context.moveTo(x + r, y);
+		context.lineTo(x + w - r, y);
+		context.quadraticCurveTo(x + w, y, x + w, y + r);
+		context.lineTo(x + w, y + h - r);
+		context.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+		context.lineTo(x + r, y + h);
+		context.quadraticCurveTo(x, y + h, x, y + h - r);
+		context.lineTo(x, y + r);
+		context.quadraticCurveTo(x, y, x + r, y);
+		context.stroke();
+		context.fill();
+	}
     
     public GPSFixDTO getBoatFix() {
         return boatFix;
