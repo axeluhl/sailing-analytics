9c9211c3fe20847c29a379bb1171ce89fcacb863
Axel Uhl <axel.uhl@sap.com>
2012-08-17 11:37:33 +0200

    Jetty bundles seem to have moved from Eclipse download to archive

---
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/leaderboard/meta/FlexibleMetaLeaderboard.java
@@ -0,0 +1,54 @@
+package com.sap.sailing.domain.leaderboard.meta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sap.sailing.domain.leaderboard.Leaderboard;
+import com.sap.sailing.domain.leaderboard.ScoringScheme;
+import com.sap.sailing.domain.leaderboard.ThresholdBasedResultDiscardingRule;
+import com.sap.sailing.util.impl.LockUtil;
+import com.sap.sailing.util.impl.NamedReentrantReadWriteLock;
+
+public class FlexibleMetaLeaderboard extends AbstractMetaLeaderboard {
+    private static final long serialVersionUID = 789076326144062944L;
+    
+    private final List<Leaderboard> leaderboards;
+    
+    private final NamedReentrantReadWriteLock leaderboardsLock;
+
+    public FlexibleMetaLeaderboard(String name, ScoringScheme scoringScheme,
+            ThresholdBasedResultDiscardingRule resultDiscardingRule) {
+        super(name, scoringScheme, resultDiscardingRule);
+        leaderboards = new ArrayList<Leaderboard>();
+        leaderboardsLock = new NamedReentrantReadWriteLock("leaderboards collection of "+FlexibleMetaLeaderboard.class.getSimpleName()+" "+getName(),
+                /* fair */ false);
+    }
+
+    @Override
+    protected Iterable<Leaderboard> getLeaderboards() {
+        LockUtil.lockForRead(leaderboardsLock);
+        try {
+            return new ArrayList<Leaderboard>(leaderboards);
+        } finally {
+            LockUtil.unlockAfterRead(leaderboardsLock);
+        }
+    }
+
+    public void addLeaderboard(Leaderboard leaderboard) {
+        LockUtil.lockForWrite(leaderboardsLock);
+        try {
+            leaderboards.add(leaderboard);
+        } finally {
+            LockUtil.unlockAfterWrite(leaderboardsLock);
+        }
+    }
+
+    public void removeLeaderboard(Leaderboard leaderboard) {
+        LockUtil.lockForWrite(leaderboardsLock);
+        try {
+            leaderboards.remove(leaderboard);
+        } finally {
+            LockUtil.unlockAfterWrite(leaderboardsLock);
+        }
+    }
+}
