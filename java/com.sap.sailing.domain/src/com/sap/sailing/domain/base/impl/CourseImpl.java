f5ae5b9eb5baf897f124262703a1d7f82b3c2791
Axel Uhl <axel.uhl@sap.com>
2012-09-10 16:20:22 +0200

    fixing bug 951 by making getFirstWaypoint tolerant to empty course, returning null

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/impl/CourseImpl.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/impl/CourseImpl.java
@@ -332,7 +332,7 @@ public class CourseImpl extends NamedImpl implements Course {
     public Waypoint getFirstWaypoint() {
         lockForRead();
         try {
-            return waypoints.get(0);
+            return waypoints.isEmpty() ? null : waypoints.get(0);
         } finally {
             unlockAfterRead();
         }
