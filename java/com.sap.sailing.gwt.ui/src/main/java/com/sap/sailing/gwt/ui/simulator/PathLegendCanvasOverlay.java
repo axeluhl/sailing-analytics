e7214c0987d7ce330ac93db2746247dc554cc9d3
Christopher Ronnewinkel (D036654) <christopher.ronnewinkel@sap.com>
2012-09-18 18:05:28 +0200

    white semi-transparent background for course labels & times

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/PathLegendCanvasOverlay.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/PathLegendCanvasOverlay.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.canvas.dom.client.TextMetrics;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.TimeZone;
 import com.google.gwt.maps.client.geom.Point;
@@ -37,6 +38,8 @@ public class PathLegendCanvasOverlay extends CanvasOverlay {
 
     public String textColor = "Black";
 
+    public String textFont = "normal 10pt UbuntuRegular";
+    
     public PathLegendCanvasOverlay() {
         this.setPathOverlays(null);
     }
@@ -54,9 +57,17 @@ public class PathLegendCanvasOverlay extends CanvasOverlay {
         }
         setCanvasSettings();
         int index = 0;
+        Context2d context2d = canvas.getContext2d();
+        context2d.setFont(textFont);
+        TextMetrics txtmet;
+        double txtmaxwidth = 0.0;
+        for (PathCanvasOverlay path : pathOverlays) {            
+             txtmet = context2d.measureText(path.name+" 00:00:00");
+             txtmaxwidth = Math.max(txtmaxwidth, txtmet.getWidth());
+        }
         for (PathCanvasOverlay path : pathOverlays) {
             drawRectangleWithText(xOffset, yOffset + (pathOverlays.size()-1-index) * rectHeight, path.pathColor, 
-                    path.name + " " + getFormattedTime(path.getPathTime()));
+                    path.name + " " + getFormattedTime(path.getPathTime()),txtmaxwidth);
             index++;
         }
     }
@@ -113,12 +124,16 @@ public class PathLegendCanvasOverlay extends CanvasOverlay {
 
     }
 
-    protected void drawRectangleWithText(double x, double y, String color, String text) {
+    protected void drawRectangleWithText(double x, double y, String color, String text, double textmaxwidth) {
         Context2d context2d = canvas.getContext2d();
+        context2d.setFont(textFont);
         drawRectangle(x, y, color);
+        context2d.setGlobalAlpha(0.65);
+        context2d.setFillStyle("white");
+        context2d.fillRect(x + rectWidth, y, 10.0 + textmaxwidth, rectHeight);
+        context2d.setGlobalAlpha(1.0);
         context2d.setFillStyle(textColor);
-        context2d.setFont("normal 12pt Calibri");
-        context2d.fillText(text, x + rectWidth + 5.0, y + 12.0+4.0);
+        context2d.fillText(text, x + rectWidth + 5.0, y + 12.0 + 3.0);
     }
 
     protected String getFormattedTime(long pathTime) {
