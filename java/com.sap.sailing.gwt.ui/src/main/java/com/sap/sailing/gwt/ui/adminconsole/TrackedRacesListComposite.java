92c36c3b7df493b613a168b75d0fe4666c2b838b
fmittag <fmittag@gmx.net>
2012-09-25 10:49:32 +0200

    fix for Bug 978 - NPE in TrackedRacesListComposite

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/TrackedRacesListComposite.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/TrackedRacesListComposite.java
@@ -317,7 +317,7 @@ public class TrackedRacesListComposite extends SimplePanel implements Component<
         TextColumn<RaceDTO> raceLiveDelayColumn = new TextColumn<RaceDTO>() {
             @Override
             public String getValue(RaceDTO raceDTO) {
-                if (raceDTO.trackedRace.delayToLiveInMs > 0)
+                if (raceDTO.isTracked && raceDTO.trackedRace.delayToLiveInMs > 0)
                     return "" + raceDTO.trackedRace.delayToLiveInMs / 1000;
 
                 return "";
