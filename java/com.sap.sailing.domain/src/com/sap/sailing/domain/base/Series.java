7aafb1413e2e311cc98fc1ebb59780e7630213b4
Lukas Niemeier <lukas.niemeier@sap.com>
2012-10-10 09:59:16 +0200

    # fixed a typo in the comment

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/Series.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/Series.java
@@ -7,7 +7,7 @@ import com.sap.sailing.domain.tracking.TrackedRegattaRegistry;
 /**
  * A series is a part of a {@link Regatta}. Rounds are ordered within the regatta, and rules for who is assigned to
  * which series may exist on the regatta. For example, a regatta may have a qualification series, a final series, and a
- * medal "series" with usually only a single meda race. Each series has one or more fleets, deciding how many races per
+ * medal "series" with usually only a single medal race. Each series has one or more fleets, deciding how many races per
  * race column have to be run in this round. For example, if the 49er regatta has so many competitors that they cannot
  * all start in one race, the qualification round can be split into two {@link Fleet}s, "Yellow" and "Blue," each
  * getting their separate races. Fleet assignment may or may not vary. This usually depends on the round's
