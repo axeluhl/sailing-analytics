cb8d5157fc4b0f00d5024323e5e09acbf28d1fe9
I077899 <bogdan-eugen.mihai@sap.com>
2012-10-15 13:23:03 +0300

    I've modified the ConfigurationManager object.

--- java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/impl/ConfigurationManager.java
+++ java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/impl/ConfigurationManager.java
@@ -5,32 +5,47 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class ConfigurationManager {
+	
     private static ConfigurationManager INSTANCE = null;
 
     private final String _environmentVariableName = "STG_CONFIG";
     private final String _defaultConfigFileLocation = "resources/STG_configuration.csv";
-
     private final ArrayList<Tuple<String, Double, String>> _boatClassesInfo = new ArrayList<Tuple<String, Double, String>>();
+    
+    private ReadingConfigurationFileStatus status = ReadingConfigurationFileStatus.SUCCESS;
+    private String errorMessage = ""; 
 
     private ConfigurationManager() {
+    	
         String configFileLocation = System.getenv(this._environmentVariableName);
-        if (configFileLocation == null || configFileLocation == "") {
-            configFileLocation = this._defaultConfigFileLocation;
-        } else {
-            File file = new File(configFileLocation);
-            if (file.exists() == false) {
-                configFileLocation = this._defaultConfigFileLocation;
-            }
-        }
-
+        InputStream inputStream = null;
         try {
-            InputStream inputStream = (this.getClass().getClassLoader()).getResourceAsStream(configFileLocation);
+	        if (configFileLocation == null || configFileLocation == "") {
+	        	configFileLocation = this._defaultConfigFileLocation;
+	            inputStream = this.getClass().getClassLoader().getResourceAsStream(configFileLocation);
+	            this.status = ReadingConfigurationFileStatus.ERROR_READING_ENV_VAR_VALUE;
+	            this.errorMessage = "Cannot find STG_CONFIG environment variable! Using default configuration values!";
+	            
+	        } else if (new File(configFileLocation).exists()) {
+                URL csvFileURL = new URL("file:///" + configFileLocation);
+                inputStream = csvFileURL.openStream();
+	            this.status = ReadingConfigurationFileStatus.SUCCESS;
+	            this.errorMessage = "";
+	            
+            }
+            else {
+                configFileLocation = this._defaultConfigFileLocation;
+                inputStream = this.getClass().getClassLoader().getResourceAsStream(configFileLocation);
+	            this.status = ReadingConfigurationFileStatus.ERROR_FINDING_CONFIG_FILE;
+	            this.errorMessage = "Invalid configuration file path ( " + configFileLocation + ")! Using default configuration values!";
+            }
+            
             InputStreamReader reader = new InputStreamReader(inputStream);
             BufferedReader buffer = new BufferedReader(reader);
-
             String line = null;
             String[] elements = null;
             int index = 0;
@@ -50,14 +65,18 @@ public class ConfigurationManager {
             buffer.close();
             reader.close();
             inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            
+        } 
+        catch (IOException exception) {
+        	this.status = ReadingConfigurationFileStatus.IO_ERROR;
+        	this.errorMessage = "An IO error occured when parsing the configuration file ( " + this._defaultConfigFileLocation + ")! The original error message is " + exception.getMessage();
         }
     }
-
+    
     public static ConfigurationManager getDefault() {
-        if (INSTANCE == null)
+        if (INSTANCE == null) {
             INSTANCE = new ConfigurationManager();
+        }
 
         return INSTANCE;
     }
@@ -73,4 +92,13 @@ public class ConfigurationManager {
     public String getPolarDiagramFileLocation(int index) {
         return this._boatClassesInfo.get(index).third;
     }
+
+	public ReadingConfigurationFileStatus getStatus() {
+		return this.status;
+	}
+
+	public String getErrorMessage() {
+		return this.errorMessage;
+	}
 }
+
---
+++ java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/impl/ReadingConfigurationFileStatus.java
@@ -0,0 +1,9 @@
+package com.sap.sailing.simulator.impl;
+
+public enum ReadingConfigurationFileStatus {
+	SUCCESS,
+	ERROR_READING_ENV_VAR_VALUE,
+	ERROR_FINDING_CONFIG_FILE,
+	IO_ERROR
+}
+
