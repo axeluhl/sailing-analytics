cef4d5818378358ff750d6b8ac062b0590834a1a
Axel Uhl <axel.uhl@sap.com>
2012-10-24 07:38:02 +0200

    refactored the ImagesBarCell class and subclasses

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/LeaderboardGroupConfigImagesBarCell.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/LeaderboardGroupConfigImagesBarCell.java
@@ -1,52 +1,30 @@
 package com.sap.sailing.gwt.ui.adminconsole;
 
+import java.util.Arrays;
+
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.safecss.shared.SafeStyles;
-import com.google.gwt.safecss.shared.SafeStylesUtils;
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.text.shared.SafeHtmlRenderer;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 
 public class LeaderboardGroupConfigImagesBarCell extends ImagesBarCell {
 
-    public LeaderboardGroupConfigImagesBarCell(StringMessages stringConstants) {
+    public LeaderboardGroupConfigImagesBarCell(StringMessages stringMessages) {
         super();
-        this.stringConstants = stringConstants;
+        this.stringMessages = stringMessages;
     }
 
-    public LeaderboardGroupConfigImagesBarCell(SafeHtmlRenderer<String> renderer, StringMessages stringConstants) {
+    public LeaderboardGroupConfigImagesBarCell(SafeHtmlRenderer<String> renderer, StringMessages stringMessages) {
         super();
-        this.stringConstants = stringConstants;
+        this.stringMessages = stringMessages;
     }
-
-    private static ImagesBarTemplates templates = GWT.create(ImagesBarTemplates.class);
 
     private static AdminConsoleResources resources = GWT.create(AdminConsoleResources.class);
 
-    private static final SafeHtml ICON_EDIT = makeImage(resources.editIcon());
-
-    private static final SafeHtml ICON_REMOVE = makeImage(resources.removeIcon());
-
-    private StringMessages stringConstants;
+    private StringMessages stringMessages;
     
     @Override
-    protected void render(com.google.gwt.cell.client.Cell.Context context, SafeHtml data, SafeHtmlBuilder sb) {
-        /*
-         * Always do a null check on the value. Cell widgets can pass null to
-         * cells if the underlying data contains a null, or if the data arrives
-         * out of order.
-         */
-        if (data == null) {
-            return;
-        }
-
-        SafeStyles imgStyle = SafeStylesUtils.fromTrustedString("float:left;cursor:hand;cursor:pointer;padding-right:5px;");
-
-        SafeHtml rendered = templates.cell("ACTION_EDIT", imgStyle, stringConstants.actionEdit(), ICON_EDIT);
-        sb.append(rendered);
-
-        rendered = templates.cell("ACTION_REMOVE", imgStyle, stringConstants.actionRemove(), ICON_REMOVE);
-        sb.append(rendered);
+    protected Iterable<ImageSpec> getImageSpecs() {
+        return Arrays.asList(new ImageSpec("ACTION_EDIT", stringMessages.actionEdit(), makeImagePrototype(resources.editIcon())),
+                new ImageSpec("ACTION_REMOVE", stringMessages.actionRemove(), makeImagePrototype(resources.removeIcon())));
     }
 }
