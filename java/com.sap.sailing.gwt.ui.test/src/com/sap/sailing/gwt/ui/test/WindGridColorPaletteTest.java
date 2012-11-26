1599bc536dfffc6c836673aeed30c7f2b86a0d74
Nidhi Sawhney <nidhi.sawhney@sap.com>
2012-11-26 11:50:13 +0100

    WindGrid now in shades of ocean blue.

--- java/com.sap.sailing.gwt.ui.test/src/com/sap/sailing/gwt/ui/test/WindGridColorPaletteTest.java
+++ java/com.sap.sailing.gwt.ui.test/src/com/sap/sailing/gwt/ui/test/WindGridColorPaletteTest.java
@@ -13,10 +13,15 @@ public class WindGridColorPaletteTest {
         int max = 100;
         WindGridColorPalette cp = new WindGridColorPalette(min,max);
         
-        assertEquals("Lightest color", "#FFFFFF", cp.getColor(min));
-        assertEquals("Darkest color", "#0000FF", cp.getColor(max));
+        //assertEquals("Lightest color", "#FFFFFF", cp.getColor(min));
+        //assertEquals("Darkest color", "#0000FF", cp.getColor(max));
         
-        assertEquals("In between color", "#7F7FFF", cp.getColor((max+min)/2));
+        //assertEquals("In between color", "#7F7FFF", cp.getColor((max+min)/2));
+       
+        assertEquals("Lightest color", "#FFFFFF", cp.getColor(min));
+        assertEquals("Darkest color", "#4B7FBB", cp.getColor(max));
+        
+        assertEquals("In between color", "#A5BFDD", cp.getColor((max+min)/2));
     }
 
 }
--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/util/WindGridColorPalette.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/util/WindGridColorPalette.java
@@ -13,6 +13,14 @@ public class WindGridColorPalette implements ColorPalette {
     /*Black is 0 or 00 */
     public int maxColor = 0;//100;
     
+    public int rminColor = 255;
+    public int rmaxColor = 75;
+    public int gminColor = 255;
+    public int gmaxColor = 127;
+    public int bminColor = 255;
+    public int bmaxColor = 187;
+    
+    
     private double min;
     private double max;
     
@@ -45,10 +53,10 @@ public class WindGridColorPalette implements ColorPalette {
      * 
      * @param value
      * @return a color representation in hex prefixed with a # corresponding to the value
-     * Currently only shades of grea are supported hence RGB have the same values
-     * if the value is in th range [min,max], returns null otherwise
+     * a shade of grey is returned hence RGB have the same values
+     * if the value is in the range [min,max], returns null otherwise
      */
-    public String getColor(double value) {
+    public String getGreyColor(double value) {
         if (min <= value && value <= max) {
             
             int colorValue = (int)(minColor + (value-min)/Math.max(1,(max - min)) * (maxColor - minColor));
@@ -56,11 +64,58 @@ public class WindGridColorPalette implements ColorPalette {
             if (colorValueHex.length() < 2) {
                 colorValueHex = "0" + colorValueHex;
             }
-            //String rgb = "#" + colorValueHex + colorValueHex + colorValueHex;
-            String rgb = "#" + colorValueHex + colorValueHex + "FF";
+            String rgb = "#" + colorValueHex + colorValueHex + colorValueHex;
             return rgb;
         }
         return null;
     }
 
+    /**
+     * 
+     * @param value
+     * @return a color representation in hex prefixed with a # corresponding to the value
+     * The base shade is Google map's ocean blue with shading depending on the value
+     * if the value is in the range [min,max], returns null otherwise
+     */
+    public String getColor(double value) {
+        if (min <= value && value <= max) {
+            int rbase = (rminColor + rmaxColor)/2;
+            int gbase = (gminColor + gmaxColor)/2;
+            int bbase = (bminColor + bmaxColor)/2;
+            double avgValue = (max+min)/2.0;
+            int rColorValue;
+            int gColorValue;
+            int bColorValue;
+            if (value <= avgValue) {
+                
+                rColorValue = (int)(rbase - (avgValue - value)/Math.max(1,(avgValue - min)) * (rbase - rminColor));
+                gColorValue = (int)(gbase - (avgValue - value)/Math.max(1,(avgValue - min)) * (gbase - gminColor));
+                bColorValue = (int)(bbase - (avgValue -value)/Math.max(1,(avgValue - min)) * (bbase - bminColor));
+           
+            } else {
+                rColorValue = (int)(rbase + (value - avgValue)/Math.max(1,(max - avgValue)) * (rmaxColor - rbase));
+                gColorValue = (int)(gbase + (value - avgValue)/Math.max(1,(max - avgValue)) * (gmaxColor - gbase));
+                bColorValue = (int)(bbase + (value - avgValue)/Math.max(1,(max - avgValue)) * (bmaxColor - bbase));
+                
+            }
+            
+          
+            String rcolorValueHex = Integer.toHexString(rColorValue).toUpperCase();
+            if (rcolorValueHex.length() < 2) {
+                rcolorValueHex = "0" + rcolorValueHex;
+            }
+            String gcolorValueHex = Integer.toHexString(gColorValue).toUpperCase();
+            if (gcolorValueHex.length() < 2) {
+                gcolorValueHex = "0" + gcolorValueHex;
+            }
+            String bcolorValueHex = Integer.toHexString(bColorValue).toUpperCase();
+             if (bcolorValueHex.length() < 2) {
+                 bcolorValueHex = "0" + bcolorValueHex;
+             }
+             String rgb = "#" + rcolorValueHex + gcolorValueHex + bcolorValueHex;
+             return rgb;
+       
+        }
+        return null;
+    }
 }
