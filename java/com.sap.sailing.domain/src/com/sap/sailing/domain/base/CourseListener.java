a6b1b36ab7c26ab1943b8b3f91ebd0046077bb0e
Axel Uhl <axel.uhl@sap.com>
2012-12-18 00:44:29 +0100

    fixed IndexOutOfBounds bug during logging that may occur if last waypoint is removed

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/CourseListener.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/CourseListener.java
@@ -3,5 +3,12 @@ package com.sap.sailing.domain.base;
 public interface CourseListener {
     void waypointAdded(int zeroBasedIndex, Waypoint waypointThatGotAdded);
 
+    /**
+     * @param waypointThatGotRemoved
+     *            Note that the waypoint already has been removed. You have to expect exceptions if trying to perform
+     *            methods on adjacent legs that would refer to the waypoint that already got removed, in particular
+     *            calling {@link Leg#getFrom()} on the leg that starts at the waypoint removed if this was the last
+     *            waypoint in the course.
+     */
     void waypointRemoved(int zeroBasedIndex, Waypoint waypointThatGotRemoved);
 }
