9f1993a7e94c2f43a9beb9c13fdcf1d20add7608
Armin Zamani <armin.zamani.farahani@sap.com>
2012-12-25 20:19:30 +0100

    extended the test cases for the RC event track, changed the rc event comparator to order naturally (no inverted order anymore)

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/RaceCommitteeTrackTest.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/RaceCommitteeTrackTest.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.domain.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -16,8 +17,10 @@ import com.sap.sailing.domain.racecommittee.Flags;
 import com.sap.sailing.domain.racecommittee.RaceCommitteeEvent;
 import com.sap.sailing.domain.racecommittee.RaceCommitteeEventTrack;
 import com.sap.sailing.domain.racecommittee.RaceCommitteeFlagEvent;
+import com.sap.sailing.domain.racecommittee.RaceCommitteeStartTimeEvent;
 import com.sap.sailing.domain.racecommittee.impl.RaceCommitteeEventTrackImpl;
 import com.sap.sailing.domain.racecommittee.impl.RaceCommitteeFlagEventImpl;
+import com.sap.sailing.domain.racecommittee.impl.RaceCommitteeStartTimeEventImpl;
 
 public class RaceCommitteeTrackTest {
 	
@@ -44,6 +47,169 @@ public class RaceCommitteeTrackTest {
 			while(iterator.hasNext());
 			
 			assertEquals(count, 1);
+			
+		} finally {
+			rcEventTrack.unlockAfterRead();
+		}
+	}
+	
+	@Test
+	public void testTrackAPIForEventLogUsage() {
+		RaceCommitteeEventTrack rcEventTrack = new RaceCommitteeEventTrackImpl("RaceCommitteeEventTrack");
+		List<RaceCommitteeEvent> expectedOrderingList = new ArrayList<RaceCommitteeEvent>();
+		TimePoint t1 = MillisecondsTimePoint.now();
+		TimePoint t2 = new MillisecondsTimePoint(t1.asMillis() - 1000);
+		TimePoint t3 = new MillisecondsTimePoint(t1.asMillis() - 4000);
+		TimePoint t4 = new MillisecondsTimePoint(t1.asMillis() - 5000);
+		TimePoint t5 = new MillisecondsTimePoint(t1.asMillis() - 10000);
+		
+		TimePoint tx = new MillisecondsTimePoint(t1.asMillis() - 7000);
+		
+		List<Competitor> competitors = new ArrayList<Competitor>();
+		int passId = 0;
+		RaceCommitteeFlagEvent rcEvent1 = new RaceCommitteeFlagEventImpl(t1, UUID.randomUUID(), competitors, passId, Flags.CLASS, Flags.NONE, false);
+		RaceCommitteeFlagEvent rcEvent2 = new RaceCommitteeFlagEventImpl(t2, UUID.randomUUID(), competitors, passId, Flags.PAPA, Flags.NONE, false);
+		RaceCommitteeFlagEvent rcEvent3 = new RaceCommitteeFlagEventImpl(t3, UUID.randomUUID(), competitors, passId, Flags.PAPA, Flags.NONE, true);
+		RaceCommitteeFlagEvent rcEvent4 = new RaceCommitteeFlagEventImpl(t4, UUID.randomUUID(), competitors, passId, Flags.CLASS, Flags.NONE, true);
+		RaceCommitteeStartTimeEvent rcEvent5 = new RaceCommitteeStartTimeEventImpl(t5, UUID.randomUUID(), competitors, passId, t1);
+		
+		rcEventTrack.add(rcEvent5);
+		rcEventTrack.add(rcEvent3);
+		rcEventTrack.add(rcEvent2);
+		rcEventTrack.add(rcEvent1);
+		rcEventTrack.add(rcEvent4);
+		
+		expectedOrderingList.add(rcEvent5);
+		expectedOrderingList.add(rcEvent4);
+		expectedOrderingList.add(rcEvent3);
+		expectedOrderingList.add(rcEvent2);
+		expectedOrderingList.add(rcEvent1);
+		
+		rcEventTrack.lockForRead();
+		try {
+			
+			Iterator<RaceCommitteeEvent> iterator = rcEventTrack.getFixes().iterator();
+			int count = 0;
+			
+			do 
+			{
+				RaceCommitteeEvent event = iterator.next();
+				assertEquals(event, expectedOrderingList.get(count));
+				count++;
+			} 
+			while(iterator.hasNext());
+			
+			assertEquals(count, 5);
+			
+			iterator = rcEventTrack.getRawFixes().iterator();
+			count = 0;
+			
+			do 
+			{
+				RaceCommitteeEvent event = iterator.next();
+				assertEquals(event, expectedOrderingList.get(count));
+				count++;
+			} 
+			while(iterator.hasNext());
+			
+			assertEquals(count, 5);
+			
+			RaceCommitteeEvent event = rcEventTrack.getFirstRawFix();
+			if (event != null) {
+				assertEquals(event, rcEvent5);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getLastRawFix();
+			if (event != null) {
+				assertEquals(event, rcEvent1);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getFirstFixAfter(t3);
+			if (event != null) {
+				assertEquals(event, rcEvent2);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getFirstFixAtOrAfter(t3);
+			if (event != null) {
+				assertEquals(event, rcEvent3);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getFirstRawFixAfter(t2);
+			if (event != null) {
+				assertEquals(event, rcEvent1);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getFirstRawFixAtOrAfter(t2);
+			if (event != null) {
+				assertEquals(event, rcEvent2);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getLastFixBefore(t3);
+			if (event != null) {
+				assertEquals(event, rcEvent4);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getLastFixAtOrBefore(t3);
+			if (event != null) {
+				assertEquals(event, rcEvent3);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getLastRawFixBefore(t4);
+			if (event != null) {
+				assertEquals(event, rcEvent5);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			event = rcEventTrack.getLastRawFixAtOrBefore(t4);
+			if (event != null) {
+				assertEquals(event, rcEvent4);
+			} else {
+				fail("Returned event was null");
+			}
+			
+			iterator = rcEventTrack.getFixesIterator(tx, true);
+			count = 0;
+			
+			do 
+			{
+				event = iterator.next();
+				assertEquals(event, expectedOrderingList.get(count + 1));
+				count++;
+			} 
+			while(iterator.hasNext());
+			
+			assertEquals(count, 4);
+			
+			iterator = rcEventTrack.getRawFixesIterator(tx, true);
+			count = 0;
+			
+			do 
+			{
+				event = iterator.next();
+				assertEquals(event, expectedOrderingList.get(count + 1));
+				count++;
+			} 
+			while(iterator.hasNext());
+			
+			assertEquals(count, 4);
+			
 			
 		} finally {
 			rcEventTrack.unlockAfterRead();
--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/racecommittee/impl/RaceCommitteeEventComparator.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/racecommittee/impl/RaceCommitteeEventComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import com.sap.sailing.domain.base.Timed;
 
 /**
- * This comparator compares two timed objects, in this case two {@link RaceCommitteeEvent}s and is used for the event track. The newest event shall be on top of the list.
+ * This comparator compares two timed objects, in this case two {@link RaceCommitteeEvent}s and is used for the race committee event track. The event on top of the list shall be the latest
  *
  */
 public class RaceCommitteeEventComparator implements Comparator<Timed> {
@@ -14,7 +14,7 @@ public class RaceCommitteeEventComparator implements Comparator<Timed> {
 
 	@Override
 	public int compare(Timed left, Timed right) {
-		return (-1)*left.getTimePoint().compareTo(right.getTimePoint());
+		return left.getTimePoint().compareTo(right.getTimePoint());
 	}
 
 }
