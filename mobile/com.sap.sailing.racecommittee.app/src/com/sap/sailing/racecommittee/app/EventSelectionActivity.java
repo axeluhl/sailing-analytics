f119e71496cfae2b43a84d2daa83134ad8657fe1
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2013-01-16 18:38:49 +0100

    # adding the app

---
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/EventSelectionActivity.java
@@ -0,0 +1,20 @@
+package com.sap.sailing.racecommittee.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class EventSelectionActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_selection);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_event_selection, menu);
+        return true;
+    }
+}
