8415ea46a0a58394e96c03fa8339d3aa3ac3d65a
Frederik Petersen <frederik.petersen@sap.com>
2013-01-22 09:07:43 +0100

    Added PolarSheetsEntryPoint
    just some raw constructs for starting with polar sheets development

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/polarsheets/PolarSheetsEntryPoint.java
@@ -0,0 +1,11 @@
+package com.sap.sailing.gwt.ui.polarsheets;
+
+import java.util.logging.Logger;
+
+import com.sap.sailing.gwt.ui.client.AbstractEntryPoint;
+
+public class PolarSheetsEntryPoint extends AbstractEntryPoint {
+	
+	private static final Logger logger = Logger.getLogger(PolarSheetsEntryPoint.class.getName());
+
+}
