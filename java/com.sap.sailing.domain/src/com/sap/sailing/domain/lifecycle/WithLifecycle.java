bd18e80bab8fb02fb76b2fc594ad6beaa5b60919
Simon Pamies <simon.marcel.pamies@sap.com>
2013-01-29 12:36:54 +0100

    Updated documentation (cont.)

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/lifecycle/WithLifecycle.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/lifecycle/WithLifecycle.java
@@ -1,7 +1,8 @@
 package com.sap.sailing.domain.lifecycle;
 
 /**
- * <p>Interface all objects need to implement that have a lifecycle</p>
+ * <p>Interface all objects need to implement that have a lifecycle.
+ * See {@link Lifecycle} for more information. </p>
  * 
  * @author Simon Pamies (info@pamies.de)
  * @since Jan 28, 2013
