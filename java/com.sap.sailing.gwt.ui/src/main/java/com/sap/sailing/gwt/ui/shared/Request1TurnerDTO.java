73f27c9b24d9c30f53e33ab344af4b295de4ad4f
I077899 <bogdan-eugen.mihai@sap.com>
2013-02-08 12:00:40 +0200

    I've removed the unnecessary fields.

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/Request1TurnerDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/Request1TurnerDTO.java
@@ -1,38 +1,25 @@
 package com.sap.sailing.gwt.ui.shared;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class Request1TurnerDTO implements IsSerializable {
 
     public Integer boatClassID = 0;
-    public List<SimulatorWindDTO> allPoints = null;
     public SimulatorWindDTO firstPoint = null;
     public PositionDTO secondPoint = null;
-    public Boolean useRealAverageWindSpeed = false;
-    public Integer stepDurationMilliseconds = 0;
     public Boolean leftSide = false;
 
     public Request1TurnerDTO() {
         this.boatClassID = 0;
-        this.allPoints = new ArrayList<SimulatorWindDTO>();
         this.firstPoint = null;
         this.secondPoint = null;
-        this.useRealAverageWindSpeed = true;
-        this.stepDurationMilliseconds = 2000;
         this.leftSide = false;
     }
 
-    public Request1TurnerDTO(int boatClassID, List<SimulatorWindDTO> allPoints, SimulatorWindDTO firstPoint, PositionDTO secondPoint,
-            boolean useRealAverageWindSpeed, int stepDurationMilliseconds, boolean leftSide) {
+    public Request1TurnerDTO(int boatClassID, SimulatorWindDTO firstPoint, PositionDTO secondPoint, boolean leftSide) {
         this.boatClassID = boatClassID;
-        this.allPoints = allPoints;
         this.firstPoint = firstPoint;
         this.secondPoint = secondPoint;
-        this.useRealAverageWindSpeed = useRealAverageWindSpeed;
-        this.stepDurationMilliseconds = stepDurationMilliseconds;
         this.leftSide = leftSide;
     }
 }
