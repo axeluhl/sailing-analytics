b9d0caf2607e2004a8aa32ff4d64ff51a77eead8
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2013-02-09 19:46:51 +0100

    # serialize and deserialize BoatClass if RegattaLeaderboard

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/domain/ManagedRace.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/domain/ManagedRace.java
@@ -2,11 +2,14 @@ package com.sap.sailing.racecommittee.app.domain;
 
 import com.sap.sailing.domain.common.Named;
 import com.sap.sailing.domain.common.WithID;
+import com.sap.sailing.domain.racelog.RaceLog;
 
 public interface ManagedRace extends ManagedRaceIdentifier, Named, WithID {
 	
 	public ManagedRaceIdentifier getIdentifier();
 
 	public RaceStatus getStatus();
+	
+	public RaceLog getRaceLog();
 	
 }
