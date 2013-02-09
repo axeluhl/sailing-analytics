1c49ec0e2488f4114907a56d3fe48ab00033cb7c
Armin Zamani <armin.zamani.farahani@sap.com>
2013-02-09 19:55:10 +0100

    Changed persistence for racelog to store using the new RaceColumnIdentifier instead of Regatta and RaceDefinition

--- java/com.sap.sailing.domain.persistence/src/com/sap/sailing/domain/persistence/impl/MongoRaceLogListener.java
+++ java/com.sap.sailing.domain.persistence/src/com/sap/sailing/domain/persistence/impl/MongoRaceLogListener.java
@@ -4,40 +4,35 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.sap.sailing.domain.persistence.MongoObjectFactory;
+import com.sap.sailing.domain.racelog.RaceColumnIdentifier;
 import com.sap.sailing.domain.racelog.RaceLogFlagEvent;
 import com.sap.sailing.domain.racelog.RaceLogListener;
 import com.sap.sailing.domain.racelog.RaceLogStartTimeEvent;
-import com.sap.sailing.domain.tracking.TrackedRace;
-import com.sap.sailing.domain.tracking.TrackedRegatta;
 
 public class MongoRaceLogListener implements RaceLogListener {
 	
-	private final TrackedRegatta trackedRegatta;
-    private final TrackedRace trackedRace;
+	private final RaceColumnIdentifier raceColumnIdentifier;
     private final MongoObjectFactoryImpl mongoObjectFactory;
     private final DBCollection raceLogsCollection;
     
-    public MongoRaceLogListener(TrackedRegatta trackedRegatta, TrackedRace trackedRace,
+    public MongoRaceLogListener(RaceColumnIdentifier identifier,
     		MongoObjectFactory mongoObjectFactory, DB database) {
         super();
-        this.trackedRegatta = trackedRegatta;
-        this.trackedRace = trackedRace;
+        this.raceColumnIdentifier = identifier;
         this.mongoObjectFactory = (MongoObjectFactoryImpl) mongoObjectFactory;
         this.raceLogsCollection = this.mongoObjectFactory.getRaceLogCollection();
     }
 
 	@Override
 	public void flagEventReceived(RaceLogFlagEvent flagEvent) {
-		DBObject flagEventTrackEntry = mongoObjectFactory.storeRaceLogEntry(trackedRegatta.getRegatta(), 
-				trackedRace.getRace(), flagEvent);
+		DBObject flagEventTrackEntry = mongoObjectFactory.storeRaceLogEntry(raceColumnIdentifier, flagEvent);
 		raceLogsCollection.insert(flagEventTrackEntry);
 	}
 
 	@Override
 	public void startTimeEventReceived(
 			RaceLogStartTimeEvent startTimeEvent) {
-		DBObject startTimeEventTrackEntry = mongoObjectFactory.storeRaceLogEntry(trackedRegatta.getRegatta(), 
-				trackedRace.getRace(), startTimeEvent);
+		DBObject startTimeEventTrackEntry = mongoObjectFactory.storeRaceLogEntry(raceColumnIdentifier, startTimeEvent);
 		raceLogsCollection.insert(startTimeEventTrackEntry);
 
 	}
