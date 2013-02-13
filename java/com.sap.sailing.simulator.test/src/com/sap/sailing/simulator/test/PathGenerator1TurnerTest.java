f80bc445adf85a84e044d0aafbd9c3edad6f1fa3
I077899 <bogdan-eugen.mihai@sap.com>
2013-02-13 13:45:57 +0200

    I've added the code for generating the 1 turner point.

---
+++ java/com.sap.sailing.simulator.test/src/com/sap/sailing/simulator/test/PathGenerator1TurnerTest.java
@@ -0,0 +1,132 @@
+package com.sap.sailing.simulator.test;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sap.sailing.domain.base.impl.MillisecondsTimePoint;
+import com.sap.sailing.domain.common.Position;
+import com.sap.sailing.domain.common.TimePoint;
+import com.sap.sailing.domain.common.impl.DegreePosition;
+import com.sap.sailing.simulator.PolarDiagram;
+import com.sap.sailing.simulator.SimulationParameters;
+import com.sap.sailing.simulator.TimedPositionWithSpeed;
+import com.sap.sailing.simulator.impl.PathGenerator1Turner;
+import com.sap.sailing.simulator.impl.PolarDiagramCSV;
+import com.sap.sailing.simulator.impl.SimulationParametersImpl;
+import com.sap.sailing.simulator.util.SailingSimulatorUtil;
+import com.sap.sailing.simulator.windfield.WindFieldGenerator;
+
+public class PathGenerator1TurnerTest {
+
+    // private static final String POLAR_DIAGRAM_49_CSV_FILE_PATH = "resources/PolarDiagram49.csv";
+    // private static final String POLAR_DIAGRAM_49_BETHWAITE_CSV_FILE_PATH = "resources/PolarDiagram49Bethwaite.csv";
+    // private static final String POLAR_DIAGRAM_49_ORC_CSV_FILE_PATH = "resources/PolarDiagram49ORC.csv";
+    private static final String POLAR_DIAGRAM_49_STG_CSV_FILE_PATH = "resources/PolarDiagram49STG.csv";
+    // private static final String POLAR_DIAGRAM_505_STG_CSV_FILE_PATH = "resources/PolarDiagram505STG.csv";
+
+    private SimulationParameters _simulationParameters = null;
+    private PolarDiagram _polarDiagram = null;
+    private WindFieldGenerator _windField = null;
+    private PathGenerator1Turner _pathGenerator = null;
+
+    @Before
+    public void initialize() throws IOException, ClassNotFoundException {
+
+        this._polarDiagram = new PolarDiagramCSV(POLAR_DIAGRAM_49_STG_CSV_FILE_PATH);
+        this._simulationParameters = new SimulationParametersImpl(null, this._polarDiagram, null, SailingSimulatorUtil.measured);
+        this._pathGenerator = new PathGenerator1Turner(this._simulationParameters);
+
+        this._windField = readWindFieldGeneratorFromExternalFile("C:\\java\\workspace\\sapsailingcapture\\java\\com.sap.sailing.simulator\\src\\resources\\windField.dat");
+    }
+
+    @Test
+    public void test_get1Turner_allCourse() {
+
+        Position start = new DegreePosition(53.97207883355604, 10.890463283282314);
+        Position end = new DegreePosition(53.967186999999996, 10.891352);
+        TimePoint startTime = new MillisecondsTimePoint(1360533600140L);
+        boolean leftSide = true;
+        double reachingTolerance = 0.03;
+        int stepMax = 800;
+        long timeStep = 6666;
+
+        TimedPositionWithSpeed result = this._pathGenerator.get1Turner(this._windField, this._polarDiagram, start, end, startTime, leftSide,
+                reachingTolerance,
+                stepMax, timeStep);
+
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getPosition());
+        Assert.assertNotNull(result.getSpeed());
+        Assert.assertNotNull(result.getSpeed().getBearing());
+        Assert.assertNotNull(result.getTimePoint());
+
+        Assert.assertEquals(3.375, result.getSpeed().getKnots(), 0.001);
+        Assert.assertEquals(131.919809, result.getSpeed().getBearing().getDegrees(), 0.000001);
+        Assert.assertEquals(53.969782, result.getPosition().getLatDeg(), 0.000001);
+        Assert.assertEquals(10.894810, result.getPosition().getLngDeg(), 0.000001);
+        Assert.assertEquals(1360533820118L, result.getTimePoint().asMillis());
+    }
+
+    @Test
+    public void test_get1Turner_only1Segment() {
+
+        Position start = new DegreePosition(53.969066999999995, 10.893665);
+        Position end = new DegreePosition(53.97042597845473, 10.895004272460938);
+        TimePoint startTime = new MillisecondsTimePoint(1317552751000L);
+        boolean leftSide = true;
+        double reachingTolerance = 0.15;
+        int stepMax = 800;
+        long timeStep = 4000;
+
+        TimedPositionWithSpeed result = this._pathGenerator.get1Turner(this._windField, this._polarDiagram, end, start, startTime, leftSide,
+                reachingTolerance,
+                stepMax, timeStep);
+
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getPosition());
+        Assert.assertNotNull(result.getSpeed());
+        Assert.assertNotNull(result.getSpeed().getBearing());
+        Assert.assertNotNull(result.getTimePoint());
+
+        Assert.assertEquals(3.375, result.getSpeed().getKnots(), 0.001);
+        Assert.assertEquals(131.915966, result.getSpeed().getBearing().getDegrees(), 0.000001);
+        Assert.assertEquals(53.970300, result.getPosition().getLatDeg(), 0.000001);
+        Assert.assertEquals(10.895241, result.getPosition().getLngDeg(), 0.000001);
+        Assert.assertEquals(1317552763000L, result.getTimePoint().asMillis());
+    }
+
+    public static WindFieldGenerator readWindFieldGeneratorFromExternalFile(final String fileName) {
+        WindFieldGenerator result = null;
+
+        try {
+            final InputStream file = new FileInputStream(fileName);
+            final InputStream buffer = new BufferedInputStream(file);
+            final ObjectInput input = new ObjectInputStream(buffer);
+
+            try {
+                result = (WindFieldGenerator) input.readObject();
+            } finally {
+                input.close();
+                buffer.close();
+                file.close();
+            }
+        } catch (final ClassNotFoundException ex) {
+            System.err.println("[ERROR][PathGenerator1Turner][readFromExternalFile][ClassNotFoundException] " + ex.getMessage());
+            result = null;
+        } catch (final IOException ex) {
+            System.err.println("[ERROR][PathGenerator1Turner][readFromExternalFile][IOException]  " + ex.getMessage());
+            result = null;
+        }
+
+        return result;
+    }
+}
