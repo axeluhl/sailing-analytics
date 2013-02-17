351a835cc6cf6a90f3d86e4db19d5e0fb2bf2564
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2013-02-17 14:39:02 +0100

    # tested integration of DomainFactory in deserialization

--- java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/impl/CompetitorDeserializer.java
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/impl/CompetitorDeserializer.java
@@ -3,15 +3,23 @@ package com.sap.sailing.server.gateway.deserialization.impl;
 import org.json.simple.JSONObject;
 
 import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.DomainFactory;
 import com.sap.sailing.server.gateway.deserialization.JsonDeserializationException;
 import com.sap.sailing.server.gateway.deserialization.JsonDeserializer;
 
 public class CompetitorDeserializer implements JsonDeserializer<Competitor> {
+	
+	@SuppressWarnings("unused")
+	private DomainFactory factory;
+	
+	public CompetitorDeserializer(DomainFactory factory) {
+		this.factory = factory;
+	}
 
 	@Override
 	public Competitor deserialize(JSONObject object)
 			throws JsonDeserializationException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
