baf3fa8bc08a4e3063a261bdd9452b3de2a9e375
Armin Zamani <armin.zamani.farahani@sap.com>
2013-02-18 02:49:09 +0100

    removed unneccessary tests, since the race log is no longer part of the tracked race (need to be rewritten somewhere else), added two ignored dummy methods to two serialization tests so that they do not fail anymore

--- java/com.sap.sailing.server.gateway.serialization.test/src/com/sap/sailing/server/gateway/serialization/test/RegattaJsonSerializerTest.java
+++ java/com.sap.sailing.server.gateway.serialization.test/src/com/sap/sailing/server/gateway/serialization/test/RegattaJsonSerializerTest.java
@@ -1,6 +1,12 @@
 package com.sap.sailing.server.gateway.serialization.test;
 
+import org.junit.Ignore;
+
 /// TODO write test for RegattaJsonSerializer
 public class RegattaJsonSerializerTest {
 
+	@Ignore
+	public void testRegattaJsonSerializer() {
+		
+	}
 }
