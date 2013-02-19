ac424f091e1c0c820ee2f6ca97e43eeff18edfe8
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2013-02-19 20:04:33 +0100

    # renaming of parameter

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/racelog/RaceLogIdentifierTemplateResolver.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/racelog/RaceLogIdentifierTemplateResolver.java
@@ -4,6 +4,6 @@ import com.sap.sailing.domain.racelog.impl.RaceLogOnLeaderboardIdentifier;
 import com.sap.sailing.domain.racelog.impl.RaceLogOnRegattaIdentifier;
 
 public interface RaceLogIdentifierTemplateResolver {
-    void resolveOnRegattaIdentifier(RaceLogOnRegattaIdentifier raceLogStoreOnRegattaIdentifier);
-    void resolveOnLeaderboardIdentifier(RaceLogOnLeaderboardIdentifier raceLogStoreOnLeaderboardIdentifier);
+    void resolveOnRegattaIdentifier(RaceLogOnRegattaIdentifier identifierTemplate);
+    void resolveOnLeaderboardIdentifier(RaceLogOnLeaderboardIdentifier identifierTemplate);
 }
