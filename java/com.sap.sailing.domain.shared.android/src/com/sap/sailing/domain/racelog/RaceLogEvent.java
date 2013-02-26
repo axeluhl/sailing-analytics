f26bb9b040ca27b5736e7f90aad439eb91615bac
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2013-02-27 00:49:31 +0100

    # some comments for racelog package

--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/racelog/RaceLogEvent.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/racelog/RaceLogEvent.java
@@ -7,15 +7,37 @@ import com.sap.sailing.domain.base.Timed;
 import com.sap.sailing.domain.common.WithID;
 
 /**
- * Records an event that is sent by the race log via the Race Committee Cockpit App. The event is timed and may influence the state of a race depending on its context the the current
- * state of the RaceLog.
- * Subtypes may be flag events (AP, N, X, P, etc.) or operational race events
+ * Records an event that is sent by the race log via the Race Committee Cockpit App. 
+ * The event is timed and may influence the state of a race depending on its context 
+ * the the current state of the RaceLog.
+ * 
+ * Subclasses may be flag events (AP, N, X, P, etc.) or operational race events.
  *
  */
 public interface RaceLogEvent extends Timed, WithID {
-	List<Competitor> getInvolvedBoats();
 	
-	int getPassId();
+    /**
+     * Gets the event's pass identifier.
+     * 
+     * Each {@link RaceLogEvent} is associated to a certain pass. A pass
+     * is every attempt to start and run a race. A new pass is initiated
+     * when a new start time is proposed (e.g. after the race was aborted).
+     * 
+     */
+    int getPassId();
+    
+    /**
+     * Gets the list of associated {@link Competitor}s.
+     * 
+     * A {@link RaceLogEvent} might be associated with a list of competitors,
+     * which are somehow relevant for this kind of event. An example is a list
+     * of competitors who are marked for an individual recall.
+     */
+    List<Competitor> getInvolvedBoats();
 	
-	void accept(RaceLogEventVisitor visitor);
+    /**
+     * Visitor pattern to implement certain {@link RaceLogEvent} subclass specific
+     * behavior.
+     */
+    void accept(RaceLogEventVisitor visitor);
 }
--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/racelog/RaceLogRaceStatusEvent.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/racelog/RaceLogRaceStatusEvent.java
@@ -1,5 +1,9 @@
 package com.sap.sailing.domain.racelog;
 
+/**
+ * Events to indicate the new race status which is valid for all events
+ * arriving after this event (on the same pass).
+ */
 public interface RaceLogRaceStatusEvent extends RaceLogEvent {
 	
 	RaceLogRaceStatus getNextStatus();
