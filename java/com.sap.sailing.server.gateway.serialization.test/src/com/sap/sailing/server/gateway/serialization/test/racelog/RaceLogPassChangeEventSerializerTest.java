c1f9b0b662b22bfc2a014228d000794809bfa490
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2013-03-04 16:48:35 +0100

    # added serializer for RaceLogPassChangeEvent

---
+++ java/com.sap.sailing.server.gateway.serialization.test/src/com/sap/sailing/server/gateway/serialization/test/racelog/RaceLogPassChangeEventSerializerTest.java
@@ -0,0 +1,24 @@
+package com.sap.sailing.server.gateway.serialization.test.racelog;
+
+import static org.mockito.Mockito.mock;
+
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.racelog.RaceLogEvent;
+import com.sap.sailing.domain.racelog.RaceLogPassChangeEvent;
+import com.sap.sailing.server.gateway.serialization.JsonSerializer;
+import com.sap.sailing.server.gateway.serialization.impl.racelog.RaceLogPassChangeEventSerializer;
+
+public class RaceLogPassChangeEventSerializerTest extends AbstractEventSerializerTest<RaceLogPassChangeEvent> {
+
+    @Override
+    protected RaceLogPassChangeEvent createMockEvent() {
+        RaceLogPassChangeEvent event = mock(RaceLogPassChangeEvent.class);
+        return event;
+    }
+
+    @Override
+    protected JsonSerializer<RaceLogEvent> createSerializer(JsonSerializer<Competitor> competitorSerializer) {
+        return new RaceLogPassChangeEventSerializer(competitorSerializer);
+    }
+
+}
