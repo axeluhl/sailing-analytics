a0f490835b6d7add7467d606e3d914e3a4eb0170
Armin Zamani <armin.zamani.farahani@sap.com>
2013-03-07 17:24:49 +0400

    Just formatting

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/DataStore.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/DataStore.java
@@ -10,24 +10,24 @@ import com.sap.sailing.racecommittee.app.domain.ManagedRace;
 
 public interface DataStore {
 
-	public Collection<EventData> getEvents();
-	public EventData getEvent(Serializable id);
-	public boolean hasEvent(Serializable id);
-	public void addEvent(EventData event);
-	
-	public Collection<CourseArea> getCourseAreas(EventData event);
-	public CourseArea getCourseArea(Serializable id);
-	public boolean hasCourseArea(Serializable id);
-	public void addCourseArea(EventData event, CourseArea courseArea);
-	
-	public Collection<ManagedRace> getRaces();
-	public void addRace(ManagedRace race);
-	public ManagedRace getRace(Serializable id);
-	public boolean hasRace(Serializable id);
-	
-	public Collection<Mark> getMarks();
-        public Mark getMark(Serializable id);
-        public boolean hasMark(Serializable id);
-	public void addMark(Mark mark);
-	
+    public Collection<EventData> getEvents();
+    public EventData getEvent(Serializable id);
+    public boolean hasEvent(Serializable id);
+    public void addEvent(EventData event);
+
+    public Collection<CourseArea> getCourseAreas(EventData event);
+    public CourseArea getCourseArea(Serializable id);
+    public boolean hasCourseArea(Serializable id);
+    public void addCourseArea(EventData event, CourseArea courseArea);
+
+    public Collection<ManagedRace> getRaces();
+    public void addRace(ManagedRace race);
+    public ManagedRace getRace(Serializable id);
+    public boolean hasRace(Serializable id);
+
+    public Collection<Mark> getMarks();
+    public Mark getMark(Serializable id);
+    public boolean hasMark(Serializable id);
+    public void addMark(Mark mark);
+
 }
--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/InMemoryDataStore.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/InMemoryDataStore.java
@@ -11,134 +11,134 @@ import com.sap.sailing.racecommittee.app.domain.ManagedRace;
 import com.sap.sailing.racecommittee.app.utils.CollectionUtils;
 
 public enum InMemoryDataStore implements DataStore {
-	INSTANCE;
-	
-	private HashMap<Serializable, EventData> eventsById;
-	private HashMap<Serializable, ManagedRace> managedRaceById;
-	private HashMap<Serializable, Mark> marksById;
-	
-	private InMemoryDataStore() {
-		this.eventsById = new HashMap<Serializable, EventData>();
-		this.managedRaceById = new HashMap<Serializable, ManagedRace>();
-		this.marksById = new HashMap<Serializable, Mark>();
-	}
-	
-	/*
-	 * * * * * *
-	 *  EVENTS *
-	 * * * * * *
-	 */
+    INSTANCE;
 
-	public Collection<EventData> getEvents() {
-		return eventsById.values();
-	}
+    private HashMap<Serializable, EventData> eventsById;
+    private HashMap<Serializable, ManagedRace> managedRaceById;
+    private HashMap<Serializable, Mark> marksById;
 
-	public EventData getEvent(Serializable id) {
-		return eventsById.get(id);
-	}
-
-	public boolean hasEvent(Serializable id) {
-		return eventsById.containsKey(id);
-	}
-
-	public void addEvent(EventData event) {
-		eventsById.put(event.getId(), event);
-	}
-	
-	/*
-	 * * * * * * * *
-	 * COURSE AREA *
-	 * * * * * * * *
-	 */
-
-	public Collection<CourseArea> getCourseAreas(EventData event) {
-		if (event.getVenue() != null) {
-			return CollectionUtils.newArrayList(event.getVenue().getCourseAreas());
-		}
-		return null;
-	}
-
-	public CourseArea getCourseArea(EventData event, String name) {
-		Collection<CourseArea> courseAreas = getCourseAreas(event);
-		if (courseAreas != null) {
-			for (CourseArea courseArea : courseAreas) {
-				if (courseArea.getName().equals(name)) {
-					return courseArea;
-				}
-			}
-		}
-		return null;
-	}
-
-	public CourseArea getCourseArea(Serializable id) {
-		for (EventData event : eventsById.values()) {
-			for (CourseArea courseArea : getCourseAreas(event)) {
-				if (courseArea.getId().equals(id))
-					return courseArea;
-			}
-		}
-		return null;
-	}
-
-	public boolean hasCourseArea(Serializable id) {
-		for (EventData event : eventsById.values()) {
-			for (CourseArea courseArea : getCourseAreas(event)) {
-				if (courseArea.getId().equals(id))
-					return true;
-			}
-		}
-		return false;
-	}
-
-	public void addCourseArea(EventData event, CourseArea courseArea) {
-		if (event.getVenue() != null) {
-			event.getVenue().addCourseArea(courseArea);
-		}
-	}
-	
-	/*
-	 * * * * * * *  *
-	 * MANAGED RACE *
-	 * * * * * * *  *
-	 */
-
-	public Collection<ManagedRace> getRaces() {
-		return managedRaceById.values();
-	}
-
-	public void addRace(ManagedRace race) {
-		managedRaceById.put(race.getId(), race);
-	}
-
-	public ManagedRace getRace(Serializable id) {
-		return managedRaceById.get(id);
-	}
-
-	public boolean hasRace(Serializable id) {
-		return managedRaceById.containsKey(id);
-	}
-	
-	/*
-         * * * * * *
-         *  MARKS  *
-         * * * * * *
-         */
-
-	@Override
-	public Collection<Mark> getMarks() {
-            return marksById.values();
+    private InMemoryDataStore() {
+        this.eventsById = new HashMap<Serializable, EventData>();
+        this.managedRaceById = new HashMap<Serializable, ManagedRace>();
+        this.marksById = new HashMap<Serializable, Mark>();
     }
 
-	@Override
+    /*
+     * * * * * *
+     *  EVENTS *
+     * * * * * *
+     */
+
+    public Collection<EventData> getEvents() {
+        return eventsById.values();
+    }
+
+    public EventData getEvent(Serializable id) {
+        return eventsById.get(id);
+    }
+
+    public boolean hasEvent(Serializable id) {
+        return eventsById.containsKey(id);
+    }
+
+    public void addEvent(EventData event) {
+        eventsById.put(event.getId(), event);
+    }
+
+    /*
+     * * * * * * * *
+     * COURSE AREA *
+     * * * * * * * *
+     */
+
+    public Collection<CourseArea> getCourseAreas(EventData event) {
+        if (event.getVenue() != null) {
+            return CollectionUtils.newArrayList(event.getVenue().getCourseAreas());
+        }
+        return null;
+    }
+
+    public CourseArea getCourseArea(EventData event, String name) {
+        Collection<CourseArea> courseAreas = getCourseAreas(event);
+        if (courseAreas != null) {
+            for (CourseArea courseArea : courseAreas) {
+                if (courseArea.getName().equals(name)) {
+                    return courseArea;
+                }
+            }
+        }
+        return null;
+    }
+
+    public CourseArea getCourseArea(Serializable id) {
+        for (EventData event : eventsById.values()) {
+            for (CourseArea courseArea : getCourseAreas(event)) {
+                if (courseArea.getId().equals(id))
+                    return courseArea;
+            }
+        }
+        return null;
+    }
+
+    public boolean hasCourseArea(Serializable id) {
+        for (EventData event : eventsById.values()) {
+            for (CourseArea courseArea : getCourseAreas(event)) {
+                if (courseArea.getId().equals(id))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public void addCourseArea(EventData event, CourseArea courseArea) {
+        if (event.getVenue() != null) {
+            event.getVenue().addCourseArea(courseArea);
+        }
+    }
+
+    /*
+     * * * * * * *  *
+     * MANAGED RACE *
+     * * * * * * *  *
+     */
+
+    public Collection<ManagedRace> getRaces() {
+        return managedRaceById.values();
+    }
+
+    public void addRace(ManagedRace race) {
+        managedRaceById.put(race.getId(), race);
+    }
+
+    public ManagedRace getRace(Serializable id) {
+        return managedRaceById.get(id);
+    }
+
+    public boolean hasRace(Serializable id) {
+        return managedRaceById.containsKey(id);
+    }
+
+    /*
+     * * * * * *
+     *  MARKS  *
+     * * * * * *
+     */
+
+    @Override
+    public Collection<Mark> getMarks() {
+        return marksById.values();
+    }
+
+    @Override
     public Mark getMark(Serializable id) {
-            return marksById.get(id);
+        return marksById.get(id);
     }
 
-	@Override
+    @Override
     public boolean hasMark(Serializable id) {
-            return marksById.containsKey(id);
+        return marksById.containsKey(id);
     }
-    
+
     @Override
     public void addMark(Mark mark) {
         marksById.put(mark.getId(), mark);
