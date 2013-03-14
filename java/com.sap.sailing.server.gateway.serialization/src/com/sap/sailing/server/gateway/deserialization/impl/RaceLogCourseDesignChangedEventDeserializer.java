d107a242aa78dbd1bbe948e06be88bd7e1179d94
Armin Zamani <armin.zamani.farahani@sap.com>
2013-03-14 21:23:28 +0100

    Implemented the deserializer for course change events

---
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/impl/RaceLogCourseDesignChangedEventDeserializer.java
@@ -0,0 +1,35 @@
+package com.sap.sailing.server.gateway.deserialization.impl;
+
+import java.io.Serializable;
+import java.util.Collections;
+
+import org.json.simple.JSONObject;
+
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.CourseData;
+import com.sap.sailing.domain.common.TimePoint;
+import com.sap.sailing.domain.racelog.RaceLogEvent;
+import com.sap.sailing.domain.racelog.RaceLogEventFactory;
+import com.sap.sailing.server.gateway.deserialization.JsonDeserializationException;
+import com.sap.sailing.server.gateway.deserialization.coursedata.impl.CourseDataDeserializer;
+import com.sap.sailing.server.gateway.serialization.racelog.impl.RaceLogCourseDesignChangedEventSerializer;
+
+public class RaceLogCourseDesignChangedEventDeserializer extends BaseRaceLogEventDeserializer {
+    
+    private final CourseDataDeserializer courseDataDeserializer;
+    
+    public RaceLogCourseDesignChangedEventDeserializer(CourseDataDeserializer courseDataDeserializer) {
+        this.courseDataDeserializer = courseDataDeserializer;
+    }
+
+    @Override
+    protected RaceLogEvent deserialize(JSONObject object, Serializable id, TimePoint timePoint, int passId)
+            throws JsonDeserializationException {
+
+        JSONObject jsonCourseDesign = (JSONObject) object.get(RaceLogCourseDesignChangedEventSerializer.FIELD_COURSE_DESIGN);
+        CourseData courseData = courseDataDeserializer.deserialize(jsonCourseDesign);
+
+        return RaceLogEventFactory.INSTANCE.createCourseDesignChangedEvent(timePoint, id, Collections.<Competitor> emptyList(), passId, courseData);
+    }
+
+}
--- java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/serialization/coursedata/impl/WaypointJsonSerializer.java
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/serialization/coursedata/impl/WaypointJsonSerializer.java
@@ -7,8 +7,6 @@ import com.sap.sailing.domain.base.Waypoint;
 import com.sap.sailing.server.gateway.serialization.JsonSerializer;
 
 public class WaypointJsonSerializer implements JsonSerializer<Waypoint> {
-    public static final String FIELD_ID = "id";
-    public static final String FIELD_NAME = "name";
     public static final String FIELD_PASSING_SIDE = "passingSide";
     public static final String FIELD_CONTROL_POINT = "controlPoint";
 
@@ -22,8 +20,6 @@ public class WaypointJsonSerializer implements JsonSerializer<Waypoint> {
     public JSONObject serialize(Waypoint object) {
         JSONObject result = new JSONObject();
 
-        result.put(FIELD_ID, object.getId().toString());
-        result.put(FIELD_NAME, object.getName());
         if (object.getPassingSide() != null) {
             result.put(FIELD_PASSING_SIDE, object.getPassingSide());
         }
