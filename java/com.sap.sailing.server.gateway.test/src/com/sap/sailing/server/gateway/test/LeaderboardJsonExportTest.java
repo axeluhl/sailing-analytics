b1101a4740364716a40e640f5b34052ef35772ec
fmittag <fmittag@gmx.net>
2013-03-15 16:05:20 +0100

    created a gateway test bundle and added it to the main pom.xml

---
+++ java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/LeaderboardJsonExportTest.java
@@ -0,0 +1,11 @@
+package com.sap.sailing.server.gateway.test;
+
+import org.junit.Test;
+
+public class LeaderboardJsonExportTest {
+
+    @Test
+    public void exportLeaderboardAsJson() {
+        
+    }
+}
