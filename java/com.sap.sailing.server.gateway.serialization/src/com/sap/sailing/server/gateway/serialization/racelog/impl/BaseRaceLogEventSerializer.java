b2639e03791d8af340536dc72bc816a9aefaa6d6
Lukas Niemeier <lukas.niemeier@sap.com>
2013-03-20 00:14:03 +0100

    # added failing serialization test

--- java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/serialization/racelog/impl/BaseRaceLogEventSerializer.java
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/serialization/racelog/impl/BaseRaceLogEventSerializer.java
@@ -10,6 +10,7 @@ import com.sap.sailing.server.gateway.serialization.JsonSerializer;
 public abstract class BaseRaceLogEventSerializer implements JsonSerializer<RaceLogEvent> {
     public static final String FIELD_CLASS = "@class";
     public static final String FIELD_ID = "id";
+    public static final Object FIELD_CREATED_AT = "createdAt";
     public static final String FIELD_TIMESTAMP = "timestamp";
     public static final String FIELD_PASS_ID = "passId";
     public static final String FIELD_COMPETITORS = "competitors";
