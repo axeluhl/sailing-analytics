d48872b809439bab476106bb390780f1836c3060
fmittag <fmittag@gmx.net>
2013-03-20 17:35:37 +0100

    used a different id for regatta creation due to bug 1311

--- java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/JsonExportTests.java
+++ java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/JsonExportTests.java
@@ -7,12 +7,12 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import java.io.PrintWriter;
-import java.io.Serializable;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,9 +23,14 @@ import org.json.simple.JSONValue;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sap.sailing.domain.base.DomainFactory;
+import com.sap.sailing.domain.base.Fleet;
 import com.sap.sailing.domain.base.Regatta;
+import com.sap.sailing.domain.base.Series;
+import com.sap.sailing.domain.base.impl.FleetImpl;
 import com.sap.sailing.domain.base.impl.RegattaImpl;
-import com.sap.sailing.domain.common.RegattaName;
+import com.sap.sailing.domain.base.impl.SeriesImpl;
+import com.sap.sailing.domain.common.ScoringSchemeType;
 import com.sap.sailing.domain.leaderboard.LeaderboardGroup;
 import com.sap.sailing.domain.leaderboard.RegattaLeaderboard;
 import com.sap.sailing.mongodb.MongoDBConfiguration;
@@ -56,8 +61,27 @@ public class JsonExportTests {
         
         racingEventService = new RacingEventServiceImpl();
         
-        Serializable id = new RegattaName(regattaName);
-        regatta = racingEventService.getOrCreateRegatta(regattaName, boatClassName, id);
+        List<Series> series = new ArrayList<Series>();
+        
+        series = new ArrayList<Series>();
+        String[] fleetNames = new String[] { "Fleet1", "Fleet2" };
+ 
+        if (fleetNames != null && fleetNames.length > 0) {
+            List<Fleet> fleets = new ArrayList<Fleet>();
+            for (String fleetName : fleetNames) {
+                fleets.add(new FleetImpl(fleetName));
+            }
+            List<String> raceColumnNames = new ArrayList<String>();
+            for (int i = 1; i <= 5; i++) {
+                raceColumnNames.add("R" + i);
+            }
+            Series qualifyingSeries = new SeriesImpl("TestSeries", /* isMedal */false, fleets,
+                    raceColumnNames, /* trackedRegattaRegistry */null);
+            series.add(qualifyingSeries);
+        }
+
+        regatta = racingEventService.createRegatta(regattaName, boatClassName, UUID.randomUUID(), series, /*persistent*/ true,
+                DomainFactory.INSTANCE.createScoringScheme(ScoringSchemeType.LOW_POINT), null);
         
         regattaLeaderboard = racingEventService.addRegattaLeaderboard(regatta.getRegattaIdentifier(), "Testregatta displayName", new int[] { 3, 5 });
 
@@ -68,7 +92,7 @@ public class JsonExportTests {
     }
 
     @Test
-    public void createAndexportRegattasAsJsonTest() throws Exception {          
+    public void testExportRegattasAsJson() throws Exception {          
         String jsonString = callJsonHttpServlet(new RegattasJsonExportServlet(), "GET", null);
         
         Object obj= JSONValue.parse(jsonString);
@@ -85,7 +109,7 @@ public class JsonExportTests {
     }
 
     @Test
-    public void exportLeaderboardGroupsAsJsonTest() throws Exception {   
+    public void testExportLeaderboardGroupsAsJson() throws Exception {   
         String jsonString = callJsonHttpServlet(new LeaderboardGroupsJsonExportServlet(), "GET", null);
         
         Object obj= JSONValue.parse(jsonString);
@@ -99,7 +123,7 @@ public class JsonExportTests {
     }
 
     @Test
-    public void exportLeaderboardAsJsonTest() throws Exception {
+    public void testExportLeaderboardAsJson() throws Exception {
         Map<String, String> requestParameters = new HashMap<>();
         requestParameters.put("leaderboardName", regatta.getName());      
         requestParameters.put("resultState", "Final");      
