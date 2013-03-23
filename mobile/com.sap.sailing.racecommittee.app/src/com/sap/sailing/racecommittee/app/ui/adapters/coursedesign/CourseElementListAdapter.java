0e385260f11ae3df47d9160ad3ec940d50b2401e
Armin Zamani <armin.zamani.farahani@sap.com>
2013-03-23 23:44:55 +0100

    The pictures for the marks are shown now in the app

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/adapters/coursedesign/CourseElementListAdapter.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/adapters/coursedesign/CourseElementListAdapter.java
@@ -12,74 +12,73 @@ import android.widget.TextView;
 
 import com.sap.sailing.racecommittee.app.R;
 import com.sap.sailing.racecommittee.app.domain.RoundingDirection;
+import com.sap.sailing.racecommittee.app.utils.MarkImageHelper;
 
 public class CourseElementListAdapter extends ArrayAdapter<CourseListDataElement> {
-	
-	public CourseElementListAdapter(Context context, int textViewResourceId, List<CourseListDataElement> objects) {
-		super(context, textViewResourceId, objects);
-	}
-	
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View view = convertView;
-		if (convertView == null) {
 
-			LayoutInflater li = (LayoutInflater) (getContext()
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE));
-			view = li.inflate(R.layout.welter_one_row_three_columns, null);
-		}
+    public CourseElementListAdapter(Context context, int textViewResourceId, List<CourseListDataElement> objects) {
+        super(context, textViewResourceId, objects);
+    }
 
-		CourseListDataElement courseElement = getItem(position);
-		
-		TextView leftBuoyText = (TextView) view
-				.findViewById(R.id.Welter_Cell_OneRowThreeColumns_columnOne_txtLeftMark);
-		TextView roundingDirectionText = (TextView) view
-				.findViewById(R.id.Welter_Cell_OneRowThreeColumns_txtRoundingDirection);
-		TextView rightBuoyText = (TextView) view
-				.findViewById(R.id.Welter_Cell_OneRowThreeColumns_columnThree_txtRightMark);
-		
-		ImageView leftBuoyImage = (ImageView) view.findViewById(R.id.Welter_Cell_OneRowThreeColumns_columnOne_imgImage);
-		ImageView rightBuoyImage = (ImageView) view.findViewById(R.id.Welter_Cell_OneRowThreeColumns_columnThree_imgImage);
-		
-		leftBuoyText.setText(courseElement.getLeftMark().getName());
-		leftBuoyImage.setVisibility(View.INVISIBLE);
-		
-//		if (courseElement.getLeftMark().getImage() != null) {
-//			Bitmap bitmap = BitmapFactory.decodeByteArray(courseElement.getLeftMark().getImage(), 0, courseElement.getLeftMark().getImage().length);
-//			leftBuoyImage.setImageBitmap(bitmap);
-//		}
-		
-		if (courseElement.getRoundingDirection() != null)
-			roundingDirectionText.setText(getDisplayValueForRounding(courseElement.getRoundingDirection()));
-		else
-			roundingDirectionText.setText(R.string.empty);
-		
-		if (courseElement.getRightMark() != null) {
-			rightBuoyText.setText(courseElement.getRightMark().getName());
-			
-//			if (courseElement.getRightMark().getImage() != null) {
-//				Bitmap bitmap = BitmapFactory.decodeByteArray(courseElement.getRightMark().getImage(), 0, courseElement.getRightMark().getImage().length);
-//				rightBuoyImage.setVisibility(View.VISIBLE);
-//				rightBuoyImage.setImageBitmap(bitmap);
-//			}
-		} else {
-			rightBuoyText.setText(R.string.empty);
-			rightBuoyImage.setVisibility(View.INVISIBLE);
-		}
-			
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (convertView == null) {
 
-		return view;
-	}
-	
-	protected String getDisplayValueForRounding(RoundingDirection direction) {
-		if (direction.equals(RoundingDirection.Gate))
-			return "Gate";
-		else if (direction.equals(RoundingDirection.Port))
-			return "P";
-		else if (direction.equals(RoundingDirection.Starboard))
-			return "S";
-		
-		return"";
-	}
+            LayoutInflater li = (LayoutInflater) (getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE));
+            view = li.inflate(R.layout.welter_one_row_three_columns, null);
+        }
+
+        CourseListDataElement courseElement = getItem(position);
+
+        TextView leftMarkText = (TextView) view
+                .findViewById(R.id.Welter_Cell_OneRowThreeColumns_columnOne_txtLeftMark);
+        TextView roundingDirectionText = (TextView) view
+                .findViewById(R.id.Welter_Cell_OneRowThreeColumns_txtRoundingDirection);
+        TextView rightMarkText = (TextView) view
+                .findViewById(R.id.Welter_Cell_OneRowThreeColumns_columnThree_txtRightMark);
+
+        ImageView leftMarkImage = (ImageView) view.findViewById(R.id.Welter_Cell_OneRowThreeColumns_columnOne_imgImage);
+        ImageView rightMarkImage = (ImageView) view.findViewById(R.id.Welter_Cell_OneRowThreeColumns_columnThree_imgImage);
+
+        leftMarkText.setText(courseElement.getLeftMark().getName());
+        leftMarkImage.setVisibility(View.INVISIBLE);
+
+        if (courseElement.getLeftMark() != null) {
+            int drawable = MarkImageHelper.INSTANCE.resolveMarkImage(courseElement.getLeftMark());
+            leftMarkImage.setImageResource(drawable);
+            leftMarkImage.setVisibility(View.VISIBLE);
+        }
+
+        if (courseElement.getRoundingDirection() != null)
+            roundingDirectionText.setText(getDisplayValueForRounding(courseElement.getRoundingDirection()));
+        else
+            roundingDirectionText.setText(R.string.empty);
+
+        if (courseElement.getRightMark() != null) {
+            rightMarkText.setText(courseElement.getRightMark().getName());
+
+            int drawable = MarkImageHelper.INSTANCE.resolveMarkImage(courseElement.getRightMark());
+            rightMarkImage.setImageResource(drawable);
+            rightMarkImage.setVisibility(View.VISIBLE);
+        } else {
+            rightMarkText.setText(R.string.empty);
+            rightMarkImage.setVisibility(View.INVISIBLE);
+        }
+
+        return view;
+    }
+
+    protected String getDisplayValueForRounding(RoundingDirection direction) {
+        if (direction.equals(RoundingDirection.Gate))
+            return "Gate";
+        else if (direction.equals(RoundingDirection.Port))
+            return "P";
+        else if (direction.equals(RoundingDirection.Starboard))
+            return "S";
+
+        return"";
+    }
 
 }
