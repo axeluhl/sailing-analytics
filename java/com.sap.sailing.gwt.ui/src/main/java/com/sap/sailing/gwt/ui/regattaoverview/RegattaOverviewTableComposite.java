5311c5d504108bdc07a7c917b83bbc5811333ef6
Armin Zamani <armin.zamani.farahani@sap.com>
2013-03-25 21:54:48 +0100

    implemented start/pause for the regatta overview page, added a column for fleets

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/regattaoverview/RegattaOverviewTableComposite.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/regattaoverview/RegattaOverviewTableComposite.java
@@ -4,13 +4,17 @@ import java.util.Date;
 
 import com.google.gwt.cell.client.ImageResourceCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -21,6 +25,7 @@ import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sailing.gwt.ui.client.TimeListener;
 import com.sap.sailing.gwt.ui.client.Timer;
 import com.sap.sailing.gwt.ui.client.Timer.PlayModes;
+import com.sap.sailing.gwt.ui.client.Timer.PlayStates;
 import com.sap.sailing.gwt.ui.shared.FleetDTO;
 import com.sap.sailing.gwt.ui.shared.RaceColumnDTO;
 import com.sap.sailing.gwt.ui.shared.RaceInfoDTO;
@@ -28,7 +33,7 @@ import com.sap.sailing.gwt.ui.shared.StrippedLeaderboardDTO;
 
 public class RegattaOverviewTableComposite extends Composite {
 
-    private final long serverUpdateRate = 30000;
+    private final long serverUpdateRate = 5000;
     private final long uiUpdateRate = 1000;
 
     private final CellTable<RaceInfoDTO> regattaOverviewTable;
@@ -38,6 +43,7 @@ public class RegattaOverviewTableComposite extends Composite {
     private final Timer serverUpdateTimer;
     private final Timer uiUpdateTimer;
     private final Label timeLabel;
+    private final Button startStopUpdatingButton;
     private final DateTimeFormat timeFormatter = DateTimeFormat.getFormat("HH:mm:ss");
 
     private final SailingServiceAsync sailingService;
@@ -82,8 +88,40 @@ public class RegattaOverviewTableComposite extends Composite {
         regattaOverviewTable = createRegattaTable();
 
         timeLabel = new Label();
+        
+        Button refreshNowButton = new Button("Refresh now");
+        refreshNowButton.addClickHandler(new ClickHandler() {
 
-        panel.add(timeLabel);
+            @Override
+            public void onClick(ClickEvent event) {
+                loadAndUpdateEventLog();
+            }
+            
+        });
+        
+        this.startStopUpdatingButton = new Button("Stop updating");
+        this.startStopUpdatingButton.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                if (serverUpdateTimer.getPlayState().equals(PlayStates.Playing)) {
+                    serverUpdateTimer.pause();
+                    startStopUpdatingButton.setText("Start updating");
+                } else if (serverUpdateTimer.getPlayState().equals(PlayStates.Paused)) {
+                    serverUpdateTimer.play();
+                    startStopUpdatingButton.setText("Stop updating");
+                }
+            }
+            
+        });
+        
+        Grid gridTimeRefreshStop = new Grid(1, 4);
+        gridTimeRefreshStop.setWidget(0, 0, new Label("Current time: "));
+        gridTimeRefreshStop.setWidget(0, 1, timeLabel);
+        gridTimeRefreshStop.setWidget(0, 2, refreshNowButton);
+        gridTimeRefreshStop.setWidget(0, 3, startStopUpdatingButton);
+
+        panel.add(gridTimeRefreshStop);
         panel.add(regattaOverviewTable);
 
         initWidget(mainPanel);
@@ -128,6 +166,13 @@ public class RegattaOverviewTableComposite extends Composite {
         CellTable<RaceInfoDTO> table = new CellTable<RaceInfoDTO>(/* pageSize */10000, tableRes);
         raceListDataProvider.addDataDisplay(table);
         table.setWidth("100%");
+        
+        TextColumn<RaceInfoDTO> fleetNameColumn = new TextColumn<RaceInfoDTO>() {
+            @Override
+            public String getValue(RaceInfoDTO raceInfo) {
+                return raceInfo.fleet;
+            }
+        };
 
         TextColumn<RaceInfoDTO> raceNameColumn = new TextColumn<RaceInfoDTO>() {
             @Override
@@ -139,19 +184,21 @@ public class RegattaOverviewTableComposite extends Composite {
         TextColumn<RaceInfoDTO> raceStartTimeColumn = new TextColumn<RaceInfoDTO>() {
             @Override
             public String getValue(RaceInfoDTO raceInfo) {
+                String result = stringMessages.noStarttimeAnnouncedYet();
                 if (raceInfo.startTime != null) {
-                    return timeFormatter.format(raceInfo.startTime);
+                    result = timeFormatter.format(raceInfo.startTime);
                 }
-                return stringMessages.noStarttimeAnnouncedYet();
+                return result;
             }
         };
         
         TextColumn<RaceInfoDTO> raceStatusColumn = new TextColumn<RaceInfoDTO>() {
             @Override
             public String getValue(RaceInfoDTO raceInfo) {
+                String result = "";
                 if(raceInfo.lastStatus!=null)
-                   return raceInfo.lastStatus.toString();
-                return "";
+                    result =  raceInfo.lastStatus.toString();
+                return result;
             }
         };
 
@@ -169,6 +216,7 @@ public class RegattaOverviewTableComposite extends Composite {
             }
         };
 
+        table.addColumn(fleetNameColumn, stringMessages.fleet());
         table.addColumn(raceNameColumn, stringMessages.race());
         table.addColumn(raceStartTimeColumn, stringMessages.startTime());
         table.addColumn(raceStatusColumn, stringMessages.status());
