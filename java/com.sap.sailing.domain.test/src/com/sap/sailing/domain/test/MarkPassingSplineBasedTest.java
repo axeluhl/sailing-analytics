de481713a2f2a3f7c280fe24b4550e5b9afe6c5d
Martin Hanysz <martin.hanysz@sap.com>
2013-03-27 12:26:04 +0100

    Commented all classes and methods of the spline based algorithm.

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/MarkPassingSplineBasedTest.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/MarkPassingSplineBasedTest.java
@@ -14,7 +14,7 @@ import com.sap.sailing.domain.tracking.MarkPassing;
 
 /**
  * This class tests a mark passing detection algorithm that interpolates the boats' positions between
- * their GPS fixes.
+ * their GPS fixes using Hermite Splines.
  * @author Martin Hanysz
  *
  */
@@ -49,6 +49,12 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			}
 		}
 		
+		/**
+		 * Construct and initialize an {@link DoubleMatrix} of dimensionX to dimensionY values.
+		 * @param dimensionX - how many values in x dimension (length of one row)
+		 * @param dimensionY - how many values in y direction (length of one column)
+		 * @param values - initial values of the matrix ordered row by row
+		 */
 		public DoubleMatrix(int dimensionX, int dimensionY, List<Double> values) {
 			if (values.size() != dimensionX * dimensionY) {
 				throw new IllegalArgumentException("Given dimensions of matrix does not match the number of given initial values.");
@@ -63,18 +69,33 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			}
 		}
 
+		/**
+		 * Returns the specified entry of the matrix identified by its x and y coordinates.
+		 * @param x - the x coordinate (column) of the entry to return
+		 * @param y - the y coordinate (row) of the entry to return
+		 * @return the value at column x and row y
+		 */
 		public double get(int x, int y) {
 			return matrix.get(y).get(x);
 		}
 
+		/**
+		 * Returns the number of rows (y dimension) of the matrix.
+		 * @return the number of rows of the matrix
+		 */
 		public int getRowCount() {
 			return matrix.size();
 		}
 		
+		/**
+		 * Returns the number of columns (x dimension) of the matrix.
+		 * @return the number of columns of the matrix
+		 */
 		public int getColumnCount() {
 			return matrix.get(0).size();
 		}
 
+		@Override
 		public String toString() {
 			String result = "DoubleMatrix: ";
 			for (List<Double> row : matrix) {
@@ -83,24 +104,39 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			return result;
 		}
 
-		public List<Double> getColumn(int i) {
-			if (i > matrix.get(0).size()) {
+		/**
+		 * Returns all entries of the column with the index x.
+		 * @param x - the number of the column to return
+		 * @return a {@link List} of all values of the column number x
+		 */
+		public List<Double> getColumn(int x) {
+			if (x > matrix.get(0).size()) {
 				throw new IllegalArgumentException("The matrix does not contain the given amount of columns");
 			}
 			ArrayList<Double> column = new ArrayList<Double>();
 			for (List<Double> row : matrix) {
-				column.add(row.get(i));
+				column.add(row.get(x));
 			}
 			return column;
 		}
 		
-		public List<Double> getRow(int i) {
-			if (i > matrix.size()) {
+		/**
+		 * Returns all entries of the row with the index y.
+		 * @param y - the number of the row to return
+		 * @return a {@link List} of all values of the row number y
+		 */
+		public List<Double> getRow(int y) {
+			if (y > matrix.size()) {
 				throw new IllegalArgumentException("The matrix does not contain the given amount of rows");
 			}
-			return matrix.get(i);
+			return matrix.get(y);
 		}
 
+		/**
+		 * Multiplies this {@link DoubleMatrix} with the given {@link DoubleMatrix}.
+		 * @param matrix - the {@link DoubleMatrix} to multiply this matrix with
+		 * @return the {@link DoubleMatrix} representing the result of multiplying this matrix with the given matrix
+		 */
 		public DoubleMatrix multiply(DoubleMatrix matrix) {
 			if (this.getColumnCount() != matrix.getRowCount()) {
 				throw new IllegalArgumentException("Matrices can NOT be multiplied. Row count of the given matrix ( " + matrix.toString() + " ) does NOT equal column count of this matrix (" + this.toString() + ").");
@@ -119,6 +155,11 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			return new DoubleMatrix(matrix.getColumnCount(), this.getRowCount(), resultValues);
 		}
 		
+		/**
+		 * Multiplies this {@link DoubleMatrix} with the given scalar.
+		 * @param scalar - the scalar to multiply this matrix with
+		 * @return the {@link DoubleMatrix} representing the result of multiplying this matrix with the given scalar
+		 */
 		public DoubleMatrix multiply(double scalar) {
 			ArrayList<Double> resultValues = new ArrayList<Double>();
 			for (int x = 0; x < getColumnCount(); x++) {
@@ -130,9 +171,20 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 		}
 	}
 	
+	/**
+	 * A vector in the 2 dimensional plane that supports numerous vector operations.
+	 * @author Martin Hanysz
+	 *
+	 */
 	private class Vector2D {
 		private List<Double> components;
 		
+		/**
+		 * Creates a {@link Vector2D} with the given components.
+		 * It is possible to give more than 2 dimensions, but only the x and y dimensions
+		 * are used for the vector operations.
+		 * @param components - the component values of the vector to create
+		 */
 		public Vector2D(double ... components) {
 			this.components = new ArrayList<Double>(2);
 			for (int i = 0; i < 2; i++) {
@@ -140,6 +192,12 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			}
 		}
 		
+		/**
+		 * Creates a {@link Vector2D} with the given components.
+		 * It is possible to give more than 2 dimensions, but only the x and y dimensions
+		 * are used for the vector operations.
+		 * @param components - a {@link List} of the component values of the vector to create
+		 */
 		public Vector2D(List<Double> components) {
 			this.components = new ArrayList<Double>();
 			for (int i = 0; i < 2; i++) {
@@ -147,80 +205,165 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			}
 		}
 		
+		/**
+		 * Get a {@link Vector2D} that is perpendicular to this vector.
+		 * @return a {@link Vector2D} perpendicular to this one
+		 */
 		public Vector2D getPerpendicularVector() {
 			return new Vector2D(-this.y(), this.y());
 		}
 		
+		/**
+		 * Adds the given {@link Vector2D} to this vector.
+		 * @param v - the {@link Vector2D} to add to this vector
+		 * @return the {@link Vector2D} representing the result of adding the given vector to this one
+		 */
 		public Vector2D add(Vector2D v) {
 			return new Vector2D(x() + v.x(), y() + v.y());
 		}
 		
+		/**
+		 * Subtracts the given {@link Vector2D} from this vector.
+		 * @param v - the {@link Vector2D} to subtract from this vector
+		 * @return the {@link Vector2D} representing the result of subtracting the given vector from this one
+		 */
 		public Vector2D subtract(Vector2D v) {
 			return new Vector2D(x() - v.x(), y() - v.y());
 		}
 		
+		/**
+		 * Multiplies this vector with the given scalar.
+		 * @param scalar -  the scalar to multiply this vector with
+		 * @return the {@link Vector2D} representing the result of multiplying this vector with the given scalar
+		 */
 		public Vector2D multiply(double scalar) {
 			return new Vector2D(x() * scalar, y() * scalar);
 		}
 		
+		/**
+		 * Divides this vector by the given scalar.
+		 * @param scalar -  the scalar to divide this vector by
+		 * @return the {@link Vector2D} representing the result of dividing this vector by the given scalar
+		 */
 		public Vector2D divide(double scalar) {
 			return multiply(1.0/scalar);
 		}
 		
+		/**
+		 * Creates the dot product of this vector and the given {@link Vector2D}.
+		 * @param v - the {@link Vector2D} to create the dot product with
+		 * @return the dot product of this vector and the given one
+		 */
 		public double dotProduct(Vector2D v) {
 			return x()*v.x() + y()*v.y();
 		}
 		
+		/**
+		 * Returns the length of this vector.
+		 * @return the length of this vector
+		 */
 		public double getLength() {
 			return Math.sqrt(Math.pow(x(), 2) + Math.pow(y(), 2));
 		}
 		
+		/**
+		 * Calculates the distance from the point represented by this vector to the point represented by the given {@link Vector2D}.
+		 * @param p - the {@link Vector2D} representing the point to calculate the distance to
+		 * @return the distance from the point represented by this vector to the point represented by the given vector
+		 */
 		public double getDistanceToPoint(Vector2D p) {
 			return p.subtract(this).getLength();
 		}
 		
+		/**
+		 * Returns the value of the component with the given index.
+		 * @param i -  the index of the component value to return
+		 * @return the value of the component at index i
+		 */
 		public double get(int i) {
 			return components.get(i);
 		}
 		
+		/**
+		 * Returns the x component of this vector.
+		 * @return the x component of this vector
+		 */
 		public double x() {
 			return components.get(0);
 		}
 		
+		/**
+		 * Returns the y component of this vector.
+		 * @return the y component of this vector
+		 */
 		public double y() {
 			return components.get(1);
 		}
 
+		/**
+		 * Sets the value of the component at index i to d
+		 * @param i - the index of the component to set
+		 * @param d - the value to set the specified component to
+		 */
 		public void set(int i, double d) {
 			components.set(i, d);
 		}
 		
+		@Override
 		public String toString() {
 			return "Vector2D: " + components.toString();
 		}
 
+		/**
+		 * Returns a normalized version of this vector.
+		 * @return the normalized version of this vector
+		 */
 		public Vector2D normalize() {
 			return divide(getLength());
 		}
 	}
 	
+	/**
+	 * A straight line, represented by a location and a direction vector.
+	 * @author Martin Hanysz
+	 *
+	 */
 	private class StraightLine {
 		private final Vector2D locationVector;
 		private final Vector2D directionVector;
 		
+		/**
+		 * Instantiates a {@link StraightLine} through the given location vector and
+		 * going into the direction of the given direction vector.
+		 * @param locationVector - a {@link Vector2D} representing a known point of the {@link StraightLine}
+		 * @param directionVector - a {@link Vector2D} representing the direction of the {@link StraightLine}
+		 */
 		public StraightLine(Vector2D locationVector, Vector2D directionVector) {
 			this.locationVector = locationVector;
 			this.directionVector = directionVector;
 		}
 		
+		/**
+		 * Returns the location vector of this straight line.
+		 * @return the location vector of this straight line
+		 */
 		public Vector2D getLocationVector() {
 			return locationVector;
 		}
 		
+		/**
+		 * Returns the direction vector of this straight line.
+		 * @return the direction vector of this straight line
+		 */
 		public Vector2D getDirectionVector() {
 			return directionVector;
 		}
 
+		/**
+		 * Calculates the shortest distance from this straight line to the point identified by the given {@link Vector2D}.
+		 * @param point - a {@link Vector2D} representing the point to calculate the distance to
+		 * @return the shortest distance to the given point
+		 */
 		public double getDistanceToPoint(Vector2D point) {
 			Vector2D normal = getDirectionVector().getPerpendicularVector();
 			// y = z - ((z-r)*n)/(n*n) * n with z = point
@@ -229,6 +372,7 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			return perpendicularFoot.getDistanceToPoint(point);
 		}
 		
+		@Override
 		public String toString() {
 			String p = "p = (" + getLocationVector().x() + "/" + getLocationVector().y() + ")";
 			String d = "d = (" + getDirectionVector().x() + "/" + getDirectionVector().y() + ")";
@@ -236,12 +380,26 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 		}
 	}
 	
+	/**
+	 * An hermite curve that interpolates points of the curve between its known starting and end point
+	 * by using only these points and the tangents of the curve at these two points as input parameters.
+	 * @author Martin Hanysz
+	 *
+	 */
 	private class HermiteCurve {
 		
 		private final DoubleMatrix hermiteBaseMatrix = new DoubleMatrix(4, 4, 2, -2, 1, 1, -3, 3, -2, -1, 0, 0, 1, 0, 1, 0, 0, 0);
 		private final List<Double> exponents = Arrays.asList(3.0, 2.0, 1.0, 0.0);
 		private final DoubleMatrix geometryMatrix;
 		
+		/**
+		 * Instantiates a hermite curve from the point identified by the given {@link Vector2D} point1 to the given {@link Vector2D} point2
+		 * with the tangents given by the {@link Vector2D}s tangent1 and tangent2 at point1 and point 2 respectively.
+		 * @param point1 - the {@link Vector2D} that represents the starting point of the hermite curve
+		 * @param point2 - the {@link Vector2D} that represents the end point of the hermite curve
+		 * @param tangent1 - the {@link Vector2D} that represents the tangent at the starting point of the hermite curve
+		 * @param tangent2 - the {@link Vector2D} that represents the tangent at the end point of the hermite curve
+		 */
 		public HermiteCurve(Vector2D point1, Vector2D point2, Vector2D tangent1, Vector2D tangent2) {
 			geometryMatrix = new DoubleMatrix(	/* dimensions */ 2, 4,
 												point1.x(), point1.y(),
@@ -250,6 +408,11 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 												tangent2.x(), tangent2.y());
 		}
 		
+		/**
+		 * Intersects this hermite curve with the given {@link StraightLine}.
+		 * @param line - the {@link StraightLine} to intersect this hermite curve with
+		 * @return the {@link List} of {@link Vector2D}s representing the intersection points if they exist, an empty {@link List} if no intersections exist
+		 */
 		public List<Vector2D> intersectWith(StraightLine line) {			
 			/*
 			 * We start with the hermite spline as a parametric form h(x(t), y(t)) and the straight line in implicit form g(x,y) = 0 with
@@ -340,22 +503,44 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			return results;
 		}
 
+		/**
+		 * Returns the {@link Vector2D} representing the tangent vector at the end point of this hermite curve.
+		 * @return the {@link Vector2D} representing the tangent vector at the end point of this hermite curve
+		 */
 		public Vector2D getTangentAtSecondPoint() {
 			return new Vector2D(geometryMatrix.getRow(3));
 		}
 
+		/**
+		 * Returns the {@link Vector2D} representing the tangent vector at the starting point of this hermite curve.
+		 * @return the {@link Vector2D} representing the tangent vector at the starting point of this hermite curve
+		 */
 		public Vector2D getTangentAtFirstPoint() {
 			return new Vector2D(geometryMatrix.getRow(2));
 		}
 
+		/**
+		 * Returns the {@link Vector2D} representing the end point of this hermite curve.
+		 * @return the {@link Vector2D} representing the end point of this hermite curve
+		 */
 		public Vector2D getSecondPoint() {
 			return new Vector2D(geometryMatrix.getRow(1));
 		}
 
+		/**
+		 * Returns the {@link Vector2D} representing the starting point of this hermite curve.
+		 * @return the {@link Vector2D} representing the starting point of this hermite curve
+		 */
 		public Vector2D getFirstPoint() {
 			return new Vector2D(geometryMatrix.getRow(0));
 		}
 
+		/**
+		 * Interpolates the point of the hermite curve at the interpolation parameter t with t being in the interval of <code>0.0</code> to <code>1.0</code>.
+		 * Using t = 0.0 will yield the starting point of this hermite curve and t = 1.0 will yield the end point of it.
+		 * @param t - the interpolation parameter to interpolate the point at
+		 * @return the {@link Vector2D} representing the point of the hermite curve at the interpolation parameter t
+		 */
 		public Vector2D interpolateCurvePoint(double t) {
 			if (t < 0.0 || t > 1.0) {
 				throw new IllegalArgumentException("Value of t (" + t + ") is not in the interval of 0.0 to 1.0");
@@ -371,6 +556,12 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			return new Vector2D(result.getRow(0));
 		}
 
+		/**
+		 * Returns the exponent matrix of this hermite curve for the given interpolation parameter t.
+		 * It looks like <code>[t^3 t^2 t^1 t^0]</code>.
+		 * @param t - the interpolation parameter to calculate the exponent matrix for
+		 * @return the {@link DoubleMatrix} representing the exponent matrix for the interpolation parameter t
+		 */
 		private DoubleMatrix getExponentMatrix(double t) {
 			ArrayList<Double> exponentMatrixValues = new ArrayList<Double>();
 			for (double e : exponents) {
@@ -379,6 +570,7 @@ public class MarkPassingSplineBasedTest extends AbstractMarkPassingTest {
 			return new DoubleMatrix(4, 1, exponentMatrixValues);
 		}
 		
+		@Override
 		public String toString() {
 			String p1 = "p1 = (" + getFirstPoint().x() + "/" + getFirstPoint().y() + ")";
 			String p2 = "p2 = (" + getSecondPoint().x() + "/" + getSecondPoint().y() + ")";
