4ad22fbe85fe9d762d33e075d8e047a45d4d26d5
I077899 <bogdan-eugen.mihai@sap.com>
2013-03-29 16:57:41 +0200

    New objects to handle the selection from the comboboxes in the simulator GUI.

---
+++ java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/SimulatorUISelection.java
@@ -0,0 +1,21 @@
+package com.sap.sailing.simulator;
+
+public interface SimulatorUISelection {
+
+    int getBoatClassIndex();
+
+    void setBoatClassIndex(int boatClassIndex);
+
+    int getRaceIndex();
+
+    void setRaceIndex(int raceIndex);
+
+    int getCompetitorIndex();
+
+    void setCompetitorIndex(int competitorIndex);
+
+    int getLegIndex();
+
+    void setLegIndex(int legIndex);
+
+}
