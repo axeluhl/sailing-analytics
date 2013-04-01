71f07329c9d97b702df36312bc8800e8887ccdc7
Armin Zamani <armin.zamani.farahani@sap.com>
2013-04-01 19:24:51 +0200

    renamed competitorDeserializer to CompetitorIdDeserializer, implemented the deserialization when a competitor id is given

---
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/impl/CompetitorIdDeserializer.java
@@ -0,0 +1,28 @@
+package com.sap.sailing.server.gateway.deserialization.impl;
+
+import java.io.Serializable;
+
+import org.json.simple.JSONObject;
+
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.SharedDomainFactory;
+import com.sap.sailing.server.gateway.deserialization.JsonDeserializationException;
+import com.sap.sailing.server.gateway.deserialization.JsonDeserializer;
+import com.sap.sailing.server.gateway.serialization.competitor.impl.CompetitorJsonSerializer;
+
+public class CompetitorIdDeserializer implements JsonDeserializer<Competitor> {
+
+    private SharedDomainFactory factory;
+
+    public CompetitorIdDeserializer(SharedDomainFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public Competitor deserialize(JSONObject object) throws JsonDeserializationException {
+        Serializable competitorId = (Serializable) object.get(CompetitorJsonSerializer.FIELD_ID);
+        Competitor competitor = factory.getExistingCompetitorById(competitorId);
+        return competitor;
+    }
+
+}
