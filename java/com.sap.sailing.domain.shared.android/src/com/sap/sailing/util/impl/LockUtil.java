d2006706191f67d3c4d227ae21514087eadc7a4a
Axel Uhl <axel.uhl@sap.com>
2013-04-13 02:45:14 +0200

    added a comment why the "fix" currently doesn't work; need to count actual read locks obtained and unlock in each thread where obtained

--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/util/impl/LockUtil.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/util/impl/LockUtil.java
@@ -34,42 +34,36 @@ public class LockUtil {
      * have been fixed in JDK 7(b79) we should be careful. This method tries to acquire a lock, allowing for five seconds to pass.
      * After five seconds and not having retrieved the lock, tries again until the lock has been acquired.
      */
-    private static void lock(Lock lock, String lockDescriptionForTimeoutLogMessage, NamedReentrantReadWriteLock lockParent) {
+    private static boolean lock(Lock lock, String lockDescriptionForTimeoutLogMessage, NamedReentrantReadWriteLock lockParent) {
         boolean locked = false;
-        boolean interrupted = false;
-        while (!locked) {
-            try {
-                locked = lock.tryLock(NUMBER_OF_SECONDS_TO_WAIT_FOR_LOCK, TimeUnit.SECONDS);
-                if (!locked) {
-                    StringBuilder message = new StringBuilder();
-                    message.append("Couldn't acquire lock ");
-                    message.append(lockDescriptionForTimeoutLogMessage);
-                    message.append(" in ");
-                    message.append(NUMBER_OF_SECONDS_TO_WAIT_FOR_LOCK);
-                    message.append("s in thread "+Thread.currentThread().getName()+" at ");
-                    message.append(getCurrentStackTrace());
-                    Thread writer = lockParent.getWriter();
-                    if (writer != null) {
-                        message.append("\nThe current writer is:\n");
-                        appendThreadData(message, writer);
-                    }
-                    message.append("\nThe current readers are:\n");
-                    for (Thread reader : lockParent.getReaders()) {
-                        appendThreadData(message, reader);
-                    }
-                    message.append("Trying again...");
-                    logger.info(message.toString());
+        try {
+            locked = lock.tryLock(NUMBER_OF_SECONDS_TO_WAIT_FOR_LOCK, TimeUnit.SECONDS);
+            if (!locked) {
+                StringBuilder message = new StringBuilder();
+                message.append("Couldn't acquire lock ");
+                message.append(lockDescriptionForTimeoutLogMessage);
+                message.append(" in ");
+                message.append(NUMBER_OF_SECONDS_TO_WAIT_FOR_LOCK);
+                message.append("s in thread " + Thread.currentThread().getName() + " at ");
+                message.append(getCurrentStackTrace());
+                Thread writer = lockParent.getWriter();
+                if (writer != null) {
+                    message.append("\nThe current writer is:\n");
+                    appendThreadData(message, writer);
                 }
+                message.append("\nThe current readers are:\n");
+                for (Thread reader : lockParent.getReaders()) {
+                    appendThreadData(message, reader);
+                }
+                message.append("Trying again...");
+                logger.info(message.toString());
             }
-            catch (InterruptedException ex) {
-                interrupted = true;
-            }
-        }
-        if (interrupted) {
+        } catch (InterruptedException ex) {
             // re-assert interrupt state that occurred while we
             // were acquiring the lock
             Thread.currentThread().interrupt();
         }
+        return locked;
     }
 
     private static void appendThreadData(StringBuilder message, Thread writer) {
@@ -80,9 +74,29 @@ public class LockUtil {
     }
     
     public static void lockForRead(NamedReentrantReadWriteLock lock) {
-        if (!ifIsInCurrentThreadsLockSetThenIncrementLockCount(lock.readLock())) {
-            lock(lock.readLock(), lock.getReadLockName(), lock);
-            incrementLockCountForCurrentThread(lock.readLock());
+        boolean locked = false;
+        while (!locked) {
+            synchronized (getCurrentThreadsLockCounts()) {
+                if (!ifIsInCurrentThreadsLockSetThenIncrementLockCount(lock.readLock())) {
+                    // FIXME if propagation is about to happen while we're waiting for the lock and we're then getting the lock,
+                    // then this synchronized block ends, the propagation takes place, and we're holding the lock although the counters
+                    // suggest we didn't actually acquire the lock but got it through propagation. This will then lead to the unlock
+                    // method to not actually unlock again but just decrement the counter.
+                    // The fix will need to also count successful actual lock attempts by the current thread; the problem is that
+                    // read locks *may* actually be granted to multiple threads at the same time (more than one concurrent reader) and
+                    // that therefore both, the propagating and the receiving thread may hold on to the actual lock. It then needs to
+                    // be clear to both that they both will need to release the read lock eventually.
+                    locked = lock(lock.readLock(), lock.getReadLockName(), lock);
+                    if (locked) {
+                        logger.finest("actually acquired read lock " + lock.getName() + " in thread " + Thread.currentThread().getName());
+                        incrementLockCountForCurrentThread(lock.readLock());
+                    }
+                } else {
+                    logger.finest("only incremented count for read lock " + lock.getName() + " in thread "
+                            + Thread.currentThread().getName());
+                    locked = true;
+                }
+            }
         }
     }
     
@@ -151,57 +165,72 @@ public class LockUtil {
 
     public static void unlockAfterRead(NamedReentrantReadWriteLock lock) {
         assert isInCurrentThreadsLockSet(lock.readLock());
-        if (getCurrentThreadsLockCountSynchronzied(lock.readLock()) == 1) {
-            lock.readLock().unlock();
-            decrementLockCountForCurrentThread(lock.readLock());
-        } else {
-            decrementLockCountForCurrentThread(lock.readLock());
+        Map<Lock, Integer> currentThreadLockCounts = getCurrentThreadsLockCounts();
+        synchronized (currentThreadLockCounts) {
+            final Integer countForLock = currentThreadLockCounts.get(lock.readLock());
+            if (countForLock == 1) {
+                logger.finest("actually unlocking read lock "+lock.getName()+" in thread "+Thread.currentThread().getName());
+                lock.readLock().unlock();
+                decrementLockCountForCurrentThread(lock.readLock());
+            } else {
+                decrementLockCountForCurrentThread(lock.readLock());
+                logger.finest("only decrementing counter for read lock "+lock.getName()+" in thread "+Thread.currentThread().getName()+" from "+countForLock);
+            }
         }
     }
     
     public static void lockForWrite(NamedReentrantReadWriteLock lock) {
-        if (!ifIsInCurrentThreadsLockSetThenIncrementLockCount(lock.writeLock())) {
-            lock(lock.writeLock(), lock.getWriteLockName(), lock);
-            incrementLockCountForCurrentThread(lock.writeLock());
-            synchronized (lastTimeWriteLockWasObtained) {
-                lastTimeWriteLockWasObtained.put(lock, MillisecondsTimePoint.now());
+        boolean locked = false;
+        while (!locked) {
+            synchronized (getCurrentThreadsLockCounts()) {
+                if (!ifIsInCurrentThreadsLockSetThenIncrementLockCount(lock.writeLock())) {
+                    locked = lock(lock.writeLock(), lock.getWriteLockName(), lock);
+                    if (locked) {
+                        logger.finest("actually acquired write lock " + lock.getName() + " in thread " + Thread.currentThread().getName());
+                        incrementLockCountForCurrentThread(lock.writeLock());
+                        synchronized (lastTimeWriteLockWasObtained) {
+                            lastTimeWriteLockWasObtained.put(lock, MillisecondsTimePoint.now());
+                        }
+                    }
+                } else {
+                    logger.finest("only incremented count for write lock " + lock.getName() + " in thread "
+                            + Thread.currentThread().getName());
+                    locked = true;
+                }
             }
         }
-    }
-    
-    private static int getCurrentThreadsLockCountSynchronzied(Lock lock) {
-        Map<Lock, Integer> currentThreadLockCounts = getCurrentThreadsLockCounts();
-        final int result;
-        synchronized (currentThreadLockCounts) {
-            result = currentThreadLockCounts.get(lock);
-        }
-        return result;
     }
     
     public static void unlockAfterWrite(NamedReentrantReadWriteLock lock) {
         assert isInCurrentThreadsLockSet(lock.writeLock());
-        if (getCurrentThreadsLockCountSynchronzied(lock.writeLock()) == 1) {
-            lock.writeLock().unlock();
-            decrementLockCountForCurrentThread(lock.writeLock());
-            final TimePoint timePointWriteLockWasObtained;
-            synchronized (lastTimeWriteLockWasObtained) {
-                timePointWriteLockWasObtained = lastTimeWriteLockWasObtained.get(lock);
-            }
-            if (timePointWriteLockWasObtained == null) {
-                logger.info("Internal error: write lock "+lock.getName()+" to be unlocked but no time recorded for when it was last obtained.\n"+
-                        "This is where the lock interaction happened:\n"+getCurrentStackTrace());
-            } else {
-                TimePoint now = MillisecondsTimePoint.now();
-                if (now.asMillis()-timePointWriteLockWasObtained.asMillis() > 10000l) {
-                    String stackTrace = getCurrentStackTrace();
-                    logger.info("write lock "+lock.getName()+" was held for more than 10s. It got unlocked here: "+
-                            stackTrace);
+        Map<Lock, Integer> currentThreadLockCounts = getCurrentThreadsLockCounts();
+        synchronized (currentThreadLockCounts) {
+            final Integer countForLock = currentThreadLockCounts.get(lock.writeLock());
+            if (countForLock == 1) {
+                logger.finest("actually unlocking write lock "+lock.getName()+" in thread "+Thread.currentThread().getName());
+                lock.writeLock().unlock();
+                decrementLockCountForCurrentThread(lock.writeLock());
+                final TimePoint timePointWriteLockWasObtained;
+                synchronized (lastTimeWriteLockWasObtained) {
+                    timePointWriteLockWasObtained = lastTimeWriteLockWasObtained.get(lock);
                 }
+                if (timePointWriteLockWasObtained == null) {
+                    logger.info("Internal error: write lock " + lock.getName()
+                            + " to be unlocked but no time recorded for when it was last obtained.\n"
+                            + "This is where the lock interaction happened:\n" + getCurrentStackTrace());
+                } else {
+                    TimePoint now = MillisecondsTimePoint.now();
+                    if (now.asMillis() - timePointWriteLockWasObtained.asMillis() > 10000l) {
+                        String stackTrace = getCurrentStackTrace();
+                        logger.info("write lock " + lock.getName()
+                                + " was held for more than 10s. It got unlocked here: " + stackTrace);
+                    }
+                }
+            } else {
+                decrementLockCountForCurrentThread(lock.writeLock());
+                logger.finest("only decrementing counter for write lock "+lock.getName()+" in thread "+Thread.currentThread().getName()+" from "+countForLock);
             }
-        } else {
-            decrementLockCountForCurrentThread(lock.writeLock());
         }
-
     }
     
     private static String getStackTrace(Thread thread) {
