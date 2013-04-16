45cf3ca09e761c671f013ce22005d01563c2b798
Axel Uhl <axel.uhl@sap.com>
2013-04-16 23:03:00 +0200

    preparing Barbados / "winregatta" import for consolidation with other file-based result imports

--- java/com.sap.sailing.xrr.resultimport/src/com/sap/sailing/xrr/resultimport/impl/Activator.java
+++ java/com.sap.sailing.xrr.resultimport/src/com/sap/sailing/xrr/resultimport/impl/Activator.java
@@ -1,42 +1,29 @@
 package com.sap.sailing.xrr.resultimport.impl;
 
 import java.io.File;
-import java.util.logging.Logger;
 
-import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 import com.sap.sailing.domain.common.ScoreCorrectionProvider;
+import com.sap.sailing.resultimport.impl.AbstractFileBasedScoreCorrectionProviderActivator;
 import com.sap.sailing.resultimport.impl.FileBasedResultDocumentProvider;
 import com.sap.sailing.xrr.resultimport.ParserFactory;
 
-public class Activator implements BundleActivator {
-    private static final Logger logger = Logger.getLogger(Activator.class.getName());
-    
+public class Activator extends AbstractFileBasedScoreCorrectionProviderActivator {
     private static final String SCAN_DIR_PATH_PROPERTY_NAME = "xrr.results";
     private static final String DEFAULT_SCAN_DIR = "xrrresults";
     
-    @Override
-    public void start(BundleContext context) throws Exception {
-        final String scanDirPath;
-        if (context.getProperty(SCAN_DIR_PATH_PROPERTY_NAME) == null) {
-            scanDirPath = System.getProperty(SCAN_DIR_PATH_PROPERTY_NAME, DEFAULT_SCAN_DIR);
-        } else {
-            scanDirPath = context.getProperty(SCAN_DIR_PATH_PROPERTY_NAME);
-        }
-        final ScoreCorrectionProviderImpl service = new ScoreCorrectionProviderImpl(new FileBasedResultDocumentProvider(
-                new File(scanDirPath)),
-                ParserFactory.INSTANCE);
-        context.registerService(ScoreCorrectionProvider.class,
-                service, /* properties */ null);
-        logger.info("Scanning "+scanDirPath+" for official XRR result lists of "+service.getName());
-        context.registerService(ScoreCorrectionProvider.class,
-                service, /* properties */ null);
-        logger.info("Scanning "+scanDirPath+" for official result lists of "+service.getName());
+    public Activator() {
+        super(SCAN_DIR_PATH_PROPERTY_NAME, DEFAULT_SCAN_DIR);
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
+    }
+
+    @Override
+    protected ScoreCorrectionProvider create(File scanDir) {
+        return new ScoreCorrectionProviderImpl(new FileBasedResultDocumentProvider(scanDir), ParserFactory.INSTANCE);
     }
 
 }
