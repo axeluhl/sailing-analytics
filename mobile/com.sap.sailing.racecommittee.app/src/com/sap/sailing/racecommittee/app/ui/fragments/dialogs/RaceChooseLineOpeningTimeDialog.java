7b0ccf74de89c4019a4ad4a198544f68ec379122
Fabian Tschirschnitz <fabian.tschirschnitz@sap.com>
2013-04-24 22:33:07 +0200

    added forced dialogs for pathfinder and gatelineopening time in the app, if the race is started into the past to ensure that we collect all relevant information.

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/dialogs/RaceChooseLineOpeningTimeDialog.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/dialogs/RaceChooseLineOpeningTimeDialog.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.racecommittee.app.ui.fragments.dialogs;
 
 import com.sap.sailing.racecommittee.app.R;
+import com.sap.sailing.racecommittee.app.domain.startprocedure.impl.GateStartProcedure;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RaceChooseLineOpeningTimeDialog extends RaceDialogFragment {
 
@@ -38,6 +40,7 @@ public class RaceChooseLineOpeningTimeDialog extends RaceDialogFragment {
         getDialog().setTitle(getString(R.string.set_gate_line_opening_time_title));
 
         lineOpeningTimeEditText = (EditText) getView().findViewById(R.id.lineOpeningTimeText);
+        lineOpeningTimeEditText.setText(String.valueOf((GateStartProcedure.startPhaseGolfDownStandardInterval/(60*1000))));
         chooseButton = (Button) getDialog().findViewById(R.id.chooseLineOpeningTimeButton);
 
         chooseButton.setOnClickListener(new OnClickListener() {
@@ -48,18 +51,18 @@ public class RaceChooseLineOpeningTimeDialog extends RaceDialogFragment {
     }
 
     protected void onChooseClicked(View view) {
-        int lineOpeningTime = convertToInteger(lineOpeningTimeEditText.getText().toString());
+        try{
+        Integer lineOpeningTime = convertToInteger(lineOpeningTimeEditText.getText().toString());
         this.getRace().getState().setGateLineOpeningTime(Long.valueOf(60 * 1000 * lineOpeningTime));
         Log.i("RACE_SET_GATELINE_OPENING_TIME", String.valueOf(lineOpeningTime));
         dismiss();
+        } catch(NumberFormatException nfe){
+            Toast.makeText(getActivity(), "Please enter a valid number of minutes", Toast.LENGTH_LONG).show();
+        }
     }
 
-    protected Integer convertToInteger(String text) {
-        try {
+    protected Integer convertToInteger(String text) throws NumberFormatException{
             return Integer.parseInt(text);
-        } catch (NumberFormatException ex) {
-            throw new IllegalStateException("An NumberFormatException occured");
-        }
     }
 
     @Override
