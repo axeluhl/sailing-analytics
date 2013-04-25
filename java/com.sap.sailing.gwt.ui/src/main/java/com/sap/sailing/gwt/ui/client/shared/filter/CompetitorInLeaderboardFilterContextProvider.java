bd2d15cc2149bdd1b189577fcef36528dbd02951
Axel Uhl <axel.uhl@sap.com>
2013-04-25 04:55:40 +0200

    removed unused CompetitorInLeaderboardFilterContextProvider

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/filter/CompetitorInLeaderboardFilterContextProvider.java
+++
@@ -1,6 +0,0 @@
-package com.sap.sailing.gwt.ui.client.shared.filter;
-
-import com.sap.sailing.gwt.ui.leaderboard.LeaderboardFetcher;
-
-public interface CompetitorInLeaderboardFilterContextProvider extends LeaderboardFetcher {
-}
