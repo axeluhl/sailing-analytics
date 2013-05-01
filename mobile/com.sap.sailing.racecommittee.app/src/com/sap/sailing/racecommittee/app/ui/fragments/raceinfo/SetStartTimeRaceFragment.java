870635ad1eb117876543871364ec0ed43732faf6
Lukas Niemeier <lukas.niemeier@sap.com>
2013-05-01 17:49:58 +0800

    # override setting for default start procedure type

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/raceinfo/SetStartTimeRaceFragment.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/raceinfo/SetStartTimeRaceFragment.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -58,7 +59,7 @@ public class SetStartTimeRaceFragment extends RaceFragment {
         isReset = getArguments().getBoolean(AppConstants.RESET_TIME_FRAGMENT_IS_RESET);
 
         spinnerStartProcedure = (Spinner) getView().findViewById(R.id.race_reset_time_spinner_procedure);
-        setupSpinner();
+        setupStartProcedureSpinner();
         pickerTime = (TimePicker) getView().findViewById(R.id.race_reset_time_picker_time);
         setupTimePicker();
         btSetDate = (Button) getView().findViewById(R.id.race_reset_time_btn_date);
@@ -87,13 +88,13 @@ public class SetStartTimeRaceFragment extends RaceFragment {
                 showAPModeDialog();
             }
         });
-        
+
         textInfoText = (TextView) getView().findViewById(R.id.race_reset_time_text_infotext);
     }
 
     private void setupTimePicker() {
         pickerTime.setIs24HourView(true);
-        
+
         // In 10 minutes from now, but always a 5-minute-mark.
         Calendar now = Calendar.getInstance();
         now.add(Calendar.MINUTE, 10);
@@ -103,12 +104,12 @@ public class SetStartTimeRaceFragment extends RaceFragment {
         if (minutes >= 60) {
             hours++;
         }
-        
+
         pickerTime.setCurrentHour(hours);
         pickerTime.setCurrentMinute(minutes);
     }
 
-    private void setupSpinner() {
+    private void setupStartProcedureSpinner() {
         ArrayAdapter<StartProcedureType> adapter = new ArrayAdapter<StartProcedureType>(getActivity(),
                 android.R.layout.simple_spinner_dropdown_item, StartProcedureType.values());
         spinnerStartProcedure.setAdapter(adapter);
@@ -122,7 +123,16 @@ public class SetStartTimeRaceFragment extends RaceFragment {
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-        StartProcedureType type = getRace().getState().getStartProcedureType();
+
+        StartProcedureType type = StartProcedureType.ESS;
+        boolean overrideStartProcedureType = PreferenceManager.getDefaultSharedPreferences(getActivity()).getBoolean(
+                "overrideDefaultStartProcedureType", false);
+        if (overrideStartProcedureType) {
+            type = StartProcedureType.valueOf(PreferenceManager.getDefaultSharedPreferences(getActivity()).getString(
+                    "defaultStartProcedureType", StartProcedureType.ESS.name()));
+        } else {
+            type = getRace().getState().getStartProcedureType();
+        }
         spinnerStartProcedure.setSelection(adapter.getPosition(type));
     }
 
@@ -132,19 +142,19 @@ public class SetStartTimeRaceFragment extends RaceFragment {
         ExLog.i(SetStartTimeRaceFragment.class.getName(),
                 String.format("Fragment %s is now shown", SetStartTimeRaceFragment.class.getName()));
     }
-    
+
     @Override
     public void notifyTick() {
         super.notifyTick();
-        
+
         textInfoText.setText(String.format("Start %s", getTimeStringToStart()));
     }
 
     private Date getStartTime() {
         Calendar newStartTimeCal = Calendar.getInstance();
-        //newStartTimeCal.set(Calendar.YEAR, 0);
-        //newStartTimeCal.set(Calendar.MONTH, 0);
-        //newStartTimeCal.set(Calendar.DAY_OF_MONTH, 0);
+        // newStartTimeCal.set(Calendar.YEAR, 0);
+        // newStartTimeCal.set(Calendar.MONTH, 0);
+        // newStartTimeCal.set(Calendar.DAY_OF_MONTH, 0);
         newStartTimeCal.set(Calendar.HOUR_OF_DAY, pickerTime.getCurrentHour());
         newStartTimeCal.set(Calendar.MINUTE, pickerTime.getCurrentMinute());
         newStartTimeCal.set(Calendar.SECOND, 0);
@@ -156,7 +166,7 @@ public class SetStartTimeRaceFragment extends RaceFragment {
     private String getTimeStringToStart() {
         Date startTime = getStartTime();
         Date now = new Date();
-        
+
         long differenceInSeconds = (startTime.getTime() - now.getTime()) / 1000;
         boolean isInPast = Math.signum(differenceInSeconds) < 0;
         if (isInPast) {
@@ -165,8 +175,10 @@ public class SetStartTimeRaceFragment extends RaceFragment {
 
         long diff[] = new long[] { 0, 0, 0, 0 };
         /* sec */diff[3] = (differenceInSeconds >= 60 ? differenceInSeconds % 60 : differenceInSeconds);
-        /* min */diff[2] = (differenceInSeconds = (differenceInSeconds / 60)) >= 60 ? differenceInSeconds % 60 : differenceInSeconds;
-        /* hours */diff[1] = (differenceInSeconds = (differenceInSeconds / 60)) >= 24 ? differenceInSeconds % 24 : differenceInSeconds;
+        /* min */diff[2] = (differenceInSeconds = (differenceInSeconds / 60)) >= 60 ? differenceInSeconds % 60
+                : differenceInSeconds;
+        /* hours */diff[1] = (differenceInSeconds = (differenceInSeconds / 60)) >= 24 ? differenceInSeconds % 24
+                : differenceInSeconds;
         /* days */diff[0] = (differenceInSeconds = (differenceInSeconds / 24));
 
         String timeText = "";
@@ -180,7 +192,7 @@ public class SetStartTimeRaceFragment extends RaceFragment {
             timeText += String.format("%d minute%s, ", diff[2], diff[2] > 1 ? "s" : "");
         }
         timeText += String.format("%d second%s", diff[3], diff[3] > 1 ? "s" : "");
-        
+
         return isInPast ? String.format("%s %s", timeText, "ago") : String.format("%s %s", "in", timeText);
     }
 
