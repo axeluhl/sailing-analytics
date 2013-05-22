7c1102512302a4aa379c812a68f731e64545240e
Armin Zamani <armin.zamani.farahani@sap.com>
2013-05-22 16:54:10 +0200

    Changes in classpath after updating Android SDK tools

--- java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/impl/EventBaseJsonDeserializer.java
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/impl/EventBaseJsonDeserializer.java
@@ -12,28 +12,21 @@ import com.sap.sailing.server.gateway.deserialization.JsonDeserializer;
 import com.sap.sailing.server.gateway.serialization.impl.EventJsonSerializer;
 
 public class EventBaseJsonDeserializer implements JsonDeserializer<EventBase> {
-	private JsonDeserializer<Venue> venueDeserializer;
-	
-	public EventBaseJsonDeserializer(JsonDeserializer<Venue> venueDeserializer) {
-		this.venueDeserializer = venueDeserializer;
-	}
-	
-	public EventBase deserialize(JSONObject object) throws JsonDeserializationException {
-		Serializable id = (Serializable) object.get(EventJsonSerializer.FIELD_ID);
-		String name = object.get(EventJsonSerializer.FIELD_NAME).toString();
-		String publicationUrl = object.get(EventJsonSerializer.FIELD_PUBLICATION_URL).toString();
-		
-		JSONObject venueObject = Helpers.getNestedObjectSafe(
-				object, 
-				EventJsonSerializer.FIELD_VENUE);
-		Venue venue = venueDeserializer.deserialize(venueObject);
-		
-		return new EventBaseImpl(
-				name, 
-				venue, 
-				publicationUrl, 
-				true, 
-				Helpers.tryUuidConversion(id));
-	}
+    private JsonDeserializer<Venue> venueDeserializer;
+
+    public EventBaseJsonDeserializer(JsonDeserializer<Venue> venueDeserializer) {
+        this.venueDeserializer = venueDeserializer;
+    }
+
+    public EventBase deserialize(JSONObject object) throws JsonDeserializationException {
+        Serializable id = (Serializable) object.get(EventJsonSerializer.FIELD_ID);
+        String name = object.get(EventJsonSerializer.FIELD_NAME).toString();
+        String publicationUrl = object.get(EventJsonSerializer.FIELD_PUBLICATION_URL).toString();
+
+        JSONObject venueObject = Helpers.getNestedObjectSafe(object, EventJsonSerializer.FIELD_VENUE);
+        Venue venue = venueDeserializer.deserialize(venueObject);
+
+        return new EventBaseImpl(name, venue, publicationUrl, true, Helpers.tryUuidConversion(id));
+    }
 
 }
