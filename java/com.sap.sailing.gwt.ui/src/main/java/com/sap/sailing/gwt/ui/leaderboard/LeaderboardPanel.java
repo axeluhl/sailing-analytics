fb15107f01bfb579eaeb02a199cb8d5087e22a1a
Axel Uhl <axel.uhl@sap.com>
2013-06-04 11:44:34 +0200

    fixed bug with race average speed introduced in commit e6eac56b301f9d5f7511c06cb23f71fb49c55e19

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/leaderboard/LeaderboardPanel.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/leaderboard/LeaderboardPanel.java
@@ -955,14 +955,15 @@ public class LeaderboardPanel extends FormPanel implements TimeListener, PlaySta
                     double distanceTraveledInMeters = 0;
                     long timeInMilliseconds = 0;
                     for (LegEntryDTO legDetail : fieldsForRace.legDetails) {
-                        if (legDetail != null && legDetail.distanceTraveledInMeters != null &&
-                                legDetail.timeInMilliseconds != null) {
-                            distanceTraveledInMeters += legDetail.distanceTraveledInMeters;
-                            timeInMilliseconds += legDetail.timeInMilliseconds;
-                        } else {
-                            distanceTraveledInMeters = 0;
-                            timeInMilliseconds = 0;
-                            break;
+                        if (legDetail != null) {
+                            if (legDetail.distanceTraveledInMeters != null && legDetail.timeInMilliseconds != null) {
+                                distanceTraveledInMeters += legDetail.distanceTraveledInMeters;
+                                timeInMilliseconds += legDetail.timeInMilliseconds;
+                            } else {
+                                distanceTraveledInMeters = 0;
+                                timeInMilliseconds = 0;
+                                break;
+                            }
                         }
                     }
                     if (timeInMilliseconds != 0) {
