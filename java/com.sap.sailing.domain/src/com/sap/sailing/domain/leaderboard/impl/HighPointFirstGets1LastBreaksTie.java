37a659c5dd733552606a1fb43bd0abf05c70cf3d
Axel Uhl <axel.uhl@sap.com>
2013-06-04 14:22:54 +0200

    penalty for high-point scoring scheme where winner gets one point is -1

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/leaderboard/impl/HighPointFirstGets1LastBreaksTie.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/leaderboard/impl/HighPointFirstGets1LastBreaksTie.java
@@ -2,20 +2,29 @@ package com.sap.sailing.domain.leaderboard.impl;
 
 import java.util.List;
 
+import com.sap.sailing.domain.base.Competitor;
 import com.sap.sailing.domain.base.RaceColumn;
+import com.sap.sailing.domain.common.MaxPointsReason;
 import com.sap.sailing.domain.common.ScoringSchemeType;
 import com.sap.sailing.domain.common.impl.Util.Pair;
+import com.sap.sailing.domain.leaderboard.NumberOfCompetitorsInLeaderboardFetcher;
 
 public class HighPointFirstGets1LastBreaksTie extends HighPointFirstGetsFixedScore {
     private static final long serialVersionUID = 1L;
 
     public HighPointFirstGets1LastBreaksTie() {
-        super(10.0);
+        super(1.0);
     }
 
     @Override
     public ScoringSchemeType getType() {
         return ScoringSchemeType.HIGH_POINT_FIRST_GETS_ONE;
+    }
+
+    @Override
+    public Double getPenaltyScore(RaceColumn raceColumn, Competitor competitor, MaxPointsReason maxPointsReason, Integer numberOfCompetitorsInRace,
+            NumberOfCompetitorsInLeaderboardFetcher numberOfCompetitorsInLeaderboardFetcher) {
+        return -1.0;
     }
 
     @Override
