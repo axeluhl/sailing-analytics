dd431c211f21478f00ea504a8713a0f8f1d91f06
Axel Uhl <axel.uhl@sap.com>
2013-06-11 10:15:01 +0200

    commented code for generating a serialized LeaderboardDTO for testing purposes

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
@@ -2,7 +2,6 @@ package com.sap.sailing.gwt.ui.server;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
