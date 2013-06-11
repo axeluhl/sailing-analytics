70ee4cf6bfe2356b2e159baca634e27d8cbd1957
Lukas Niemeier <lukas.niemeier@sap.com>
2013-06-11 21:41:06 +0200

    formatting

--- java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/racegroup/impl/SeriesWithRowsDeserializer.java
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/racegroup/impl/SeriesWithRowsDeserializer.java
@@ -15,26 +15,23 @@ import com.sap.sailing.server.gateway.serialization.racegroup.impl.RaceRowsOfSer
 
 public class SeriesWithRowsDeserializer implements JsonDeserializer<SeriesWithRows> {
 
-	private JsonDeserializer<RaceRow> raceRowDeserializer;
-	
-	public SeriesWithRowsDeserializer(JsonDeserializer<RaceRow> raceRowDeserializer) {
-		this.raceRowDeserializer = raceRowDeserializer;
-	}
+    private JsonDeserializer<RaceRow> raceRowDeserializer;
 
-	public SeriesWithRows deserialize(JSONObject object)
-			throws JsonDeserializationException {
-		String name = object.get("name").toString();
-		boolean isMedal = (Boolean) object.get("isMedal");
-		
-		Collection<RaceRow> rows = new ArrayList<RaceRow>();
-		for (Object fleetObject : Helpers.getNestedArraySafe(
-				object, 
-				RaceRowsOfSeriesWithRowsSerializer.FIELD_FLEETS)) {
-			JSONObject fleetJson = Helpers.toJSONObjectSafe(fleetObject);
-			rows.add(raceRowDeserializer.deserialize(fleetJson));
-		}
-		
-		return new SeriesWithRowsImpl(name, isMedal, rows);
-	}
-	
+    public SeriesWithRowsDeserializer(JsonDeserializer<RaceRow> raceRowDeserializer) {
+        this.raceRowDeserializer = raceRowDeserializer;
+    }
+
+    public SeriesWithRows deserialize(JSONObject object) throws JsonDeserializationException {
+        String name = object.get("name").toString();
+        boolean isMedal = (Boolean) object.get("isMedal");
+
+        Collection<RaceRow> rows = new ArrayList<RaceRow>();
+        for (Object fleetObject : Helpers.getNestedArraySafe(object, RaceRowsOfSeriesWithRowsSerializer.FIELD_FLEETS)) {
+            JSONObject fleetJson = Helpers.toJSONObjectSafe(fleetObject);
+            rows.add(raceRowDeserializer.deserialize(fleetJson));
+        }
+
+        return new SeriesWithRowsImpl(name, isMedal, rows);
+    }
+
 }
--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/parsers/ManagedRacesDataParser.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/parsers/ManagedRacesDataParser.java
@@ -9,6 +9,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 import com.sap.sailing.domain.base.Fleet;
+import com.sap.sailing.domain.base.SeriesBase;
 import com.sap.sailing.domain.base.racegroup.RaceCell;
 import com.sap.sailing.domain.base.racegroup.RaceGroup;
 import com.sap.sailing.domain.base.racegroup.RaceRow;
@@ -23,54 +24,46 @@ import com.sap.sailing.server.gateway.deserialization.JsonDeserializer;
 import com.sap.sailing.server.gateway.deserialization.impl.Helpers;
 
 public class ManagedRacesDataParser implements DataParser<Collection<ManagedRace>> {
-	// private static final String TAG = ManagedRacesDataParser.class.getName();
-	
-	private JsonDeserializer<RaceGroup> deserializer;
-	
-	public ManagedRacesDataParser(JsonDeserializer<RaceGroup> deserializer) {
-		this.deserializer = deserializer;
-	}
-	
-	public Collection<ManagedRace> parse(Reader reader) throws Exception {
-		Object parsedResult = JSONValue.parse(reader);
-		JSONArray jsonArray = Helpers.toJSONArraySafe(parsedResult);
-		
-		Collection<ManagedRace> managedRaces = new ArrayList<ManagedRace>();
-		for (Object element : jsonArray) {
-			JSONObject json = Helpers.toJSONObjectSafe(element);
-			
-			RaceGroup group = deserializer.deserialize(json);
-			addManagedRaces(managedRaces, group);
-		}
-		
-		return managedRaces;
-	}
+    // private static final String TAG = ManagedRacesDataParser.class.getName();
 
-	private void addManagedRaces(Collection<ManagedRace> target, RaceGroup raceGroup) {
-		for (SeriesWithRows series : raceGroup.getSeries()) {
-			for (RaceRow raceRow : series.getRaceRows()) {
-				Fleet fleet = raceRow.getFleet();
-				for (RaceCell cell : raceRow.getCells()) {
-					ManagedRace race = createManagedRace(
-							raceGroup, 
-							series, 
-							fleet, 
-							cell.getName(), 
-							cell.getRaceLog());
-					target.add(race);
-				}
-			}
-		}
-	}
+    private JsonDeserializer<RaceGroup> deserializer;
 
-	private ManagedRace createManagedRace(RaceGroup raceGroup, SeriesWithRows series,
-			Fleet fleet, String name, RaceLog raceLog) {
-		return new ManagedRaceImpl(
-				new ManagedRaceIdentifierImpl(name, 
-						new FleetIdentifierImpl(fleet, series, raceGroup)),
-				StartProcedureType.ESS,
-				raceLog);
-		
-	}
+    public ManagedRacesDataParser(JsonDeserializer<RaceGroup> deserializer) {
+        this.deserializer = deserializer;
+    }
+
+    public Collection<ManagedRace> parse(Reader reader) throws Exception {
+        Object parsedResult = JSONValue.parse(reader);
+        JSONArray jsonArray = Helpers.toJSONArraySafe(parsedResult);
+
+        Collection<ManagedRace> managedRaces = new ArrayList<ManagedRace>();
+        for (Object element : jsonArray) {
+            JSONObject json = Helpers.toJSONObjectSafe(element);
+
+            RaceGroup group = deserializer.deserialize(json);
+            addManagedRaces(managedRaces, group);
+        }
+
+        return managedRaces;
+    }
+
+    private void addManagedRaces(Collection<ManagedRace> target, RaceGroup raceGroup) {
+        for (SeriesWithRows series : raceGroup.getSeries()) {
+            for (RaceRow raceRow : series.getRaceRows()) {
+                Fleet fleet = raceRow.getFleet();
+                for (RaceCell cell : raceRow.getCells()) {
+                    ManagedRace race = createManagedRace(raceGroup, series, fleet, cell.getName(), cell.getRaceLog());
+                    target.add(race);
+                }
+            }
+        }
+    }
+
+    private ManagedRace createManagedRace(RaceGroup raceGroup, SeriesBase series, Fleet fleet, String name,
+            RaceLog raceLog) {
+        return new ManagedRaceImpl(new ManagedRaceIdentifierImpl(name,
+                new FleetIdentifierImpl(fleet, series, raceGroup)), StartProcedureType.ESS, raceLog);
+
+    }
 
 }
