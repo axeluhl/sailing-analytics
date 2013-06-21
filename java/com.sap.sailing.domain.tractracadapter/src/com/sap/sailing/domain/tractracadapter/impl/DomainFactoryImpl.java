6dcd3e56ce1083d9e8affe3af4d269b11ff4e2b2
Axel Uhl <axel.uhl@sap.com>
2013-06-21 17:03:59 +0200

    log exception if nationality is not known

--- java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/impl/DomainFactoryImpl.java
+++ java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/impl/DomainFactoryImpl.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.sap.sailing.domain.base.Boat;
@@ -235,6 +236,7 @@ public class DomainFactoryImpl implements DomainFactory {
             } catch (IllegalArgumentException iae) {
                 // the country code was probably not a legal IOC country code
                 nationality = null;
+                logger.log(Level.SEVERE, "Unknown nationality "+competitor.getNationality()+" for competitor "+competitor.getName()+"; leaving null", iae);
             }
             Team team = getOrCreateTeam(competitor.getName(), nationality, competitor.getId());
             Boat boat = new BoatImpl(competitor.getShortName(), boatClass, competitor.getShortName());
