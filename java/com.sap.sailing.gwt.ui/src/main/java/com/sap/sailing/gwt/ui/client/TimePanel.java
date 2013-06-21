5409cd7459e274503ce7b6956f8ece228d6ac166
Lennart Hensler <lennart.hensler@sap.com>
2013-06-21 20:25:50 +0200

    Fixed some strings in the german version

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/TimePanel.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/TimePanel.java
@@ -498,7 +498,7 @@ public class TimePanel<T extends TimePanelSettings> extends SimplePanel implemen
 
     @Override
     public String getLocalizedShortName() {
-        return "Time control";
+        return stringMessages.timePanelName();
     }
     
     @Override
