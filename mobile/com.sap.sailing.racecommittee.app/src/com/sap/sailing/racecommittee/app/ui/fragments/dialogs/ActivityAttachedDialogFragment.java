40d19f2c39ca98d670bfdf981590d99d338e69f5
Lukas Niemeier <lukas.niemeier@sap.com>
2013-06-21 18:56:56 +0200

    # prepared dialog listener infrastructure for multiple dialogs per fragment or activity

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/dialogs/ActivityAttachedDialogFragment.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/dialogs/ActivityAttachedDialogFragment.java
@@ -3,25 +3,25 @@ package com.sap.sailing.racecommittee.app.ui.fragments.dialogs;
 
 public abstract class ActivityAttachedDialogFragment extends BaseDialogFragment {
 
-    protected DialogFragmentButtonListener hostActivity;
+    protected DialogListenerHost hostActivity;
 
     public void onAttach(android.app.Activity activity) {
         super.onAttach(activity);
 
-        if (activity instanceof DialogFragmentButtonListener) {
-            this.hostActivity = (DialogFragmentButtonListener) activity;
+        if (activity instanceof DialogListenerHost) {
+            this.hostActivity = (DialogListenerHost) activity;
         } else {
             throw new IllegalStateException(
                     String.format(
                             "Instance of %s must be attached to instances of %s. Tried to attach to %s.",
                             ActivityAttachedDialogFragment.class.getName(),
-                            DialogFragmentButtonListener.class.getName(),
+                            DialogListenerHost.class.getName(),
                             activity.getClass().getName()));
         }
     };
 
     @Override
-    protected DialogFragmentButtonListener getListener() {
+    protected DialogListenerHost getHost() {
         return this.hostActivity;
     }
 
