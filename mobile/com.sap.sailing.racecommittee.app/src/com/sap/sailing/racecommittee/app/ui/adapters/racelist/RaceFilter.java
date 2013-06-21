4eedab233800cb4fcb6030d9e44e868cc4e43ec2
Lukas Niemeier <lukas.niemeier@sap.com>
2013-06-21 17:14:22 +0200

    # simplified race filtering

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/adapters/racelist/RaceFilter.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/adapters/racelist/RaceFilter.java
@@ -9,15 +9,21 @@ import android.widget.Filter;
 import com.sap.sailing.domain.common.racelog.RaceLogRaceStatus;
 import com.sap.sailing.racecommittee.app.ui.fragments.lists.ManagedRaceListFragment.FilterMode;
 
+/**
+ * Filters races by status.
+ * 
+ * For each "run" of unscheduled races only the first will be taken.
+ * For each "run" of finished races only the last will be taken.
+ */
 public class RaceFilter extends Filter {
 
     public interface FilterSubscriber {
         void onResult(List<RaceListDataType> filtered);
     }
 
-    private final FilterSubscriber subscriber;
-    private Collection<RaceListDataType> items;
-    private FilterMode filterMode;
+    protected final FilterSubscriber subscriber;
+    protected Collection<RaceListDataType> items;
+    protected FilterMode filterMode;
 
     public RaceFilter(Collection<RaceListDataType> items, FilterSubscriber resultSubscriber) {
         this.items = items;
@@ -29,7 +35,7 @@ public class RaceFilter extends Filter {
         filter("");
     }
 
-    private static FilterResults createResults(Collection<RaceListDataType> result) {
+    protected static FilterResults createResults(Collection<RaceListDataType> result) {
         FilterResults results = new FilterResults();
         results.values = new ArrayList<RaceListDataType>(result);
         results.count = result.size();
@@ -41,77 +47,41 @@ public class RaceFilter extends Filter {
         if (filterMode == null || filterMode.equals(FilterMode.ALL)) {
             return createResults(items);
         }
-
+        
         List<RaceListDataType> filteredItems = new ArrayList<RaceListDataType>();
-        // boolean changedGroup = true;
-        RaceListDataTypeRace newestFinishedItem = null;
-        RaceListDataTypeRace nextUnscheduldedItem = null;
-
-        // To filter the races...
-        List<RaceListDataType> array = new ArrayList<RaceListDataType>(items);
-        for (RaceListDataType item : array) {
-            int itemIndex = array.indexOf(item);
-            // ... we always take the headers...
+        RaceListDataTypeRace currentUnscheduledItem = null;
+        RaceListDataTypeRace currentFinishedItem = null;
+        
+        List<RaceListDataType> allItems = new ArrayList<RaceListDataType>(items);
+        for (RaceListDataType item : allItems) {
             if (item instanceof RaceListDataTypeHeader) {
                 RaceListDataTypeHeader headerItem = (RaceListDataTypeHeader) item;
                 filteredItems.add(headerItem);
-
+                
+                // new run for both types!
+                currentUnscheduledItem = null;
+                currentFinishedItem = null;
             } else if (item instanceof RaceListDataTypeRace) {
-                // ... but...
                 RaceListDataTypeRace raceItem = (RaceListDataTypeRace) item;
                 RaceLogRaceStatus status = raceItem.getCurrentStatus();
-                if (status.equals(RaceLogRaceStatus.FINISHED)) {
-                    // ... only the newest (by sort order) finished race...
-                    if (newestFinishedItem != null) {
-                        filteredItems.remove(newestFinishedItem);
+                if (currentUnscheduledItem == null && status.equals(RaceLogRaceStatus.UNSCHEDULED)) {
+                    filteredItems.add(raceItem);
+                    currentUnscheduledItem = raceItem;
+                } else if (status.equals(RaceLogRaceStatus.FINISHED)) {
+                    if (filteredItems.contains(currentFinishedItem)) {
+                        filteredItems.remove(currentFinishedItem);
                     }
                     filteredItems.add(raceItem);
-                    newestFinishedItem = raceItem;
-                } else if (status.equals(RaceLogRaceStatus.UNSCHEDULED)) {
-                    // ... the first (by sort order) unscheduled race and the next after the (current) newest
-                    // finished or scheduled...
-                    if (nextUnscheduldedItem == null) {
-                        filteredItems.add(raceItem);
-                        nextUnscheduldedItem = raceItem;
-                    } else if (array.indexOf(newestFinishedItem) == itemIndex - 1) {
-                        filteredItems.add(raceItem);
-                    }
+                    currentFinishedItem = raceItem;
                 } else if (RaceLogRaceStatus.isActive(status)) {
-                    // ... and all active races...
                     filteredItems.add(raceItem);
+                    // new run for both types!
+                    currentUnscheduledItem = null;
+                    currentFinishedItem = null;
                 }
-            } else {
-                throw new IllegalStateException("Unknown race list item type.");
-            }
-
-            // ... and every time we are starting with a new header reset the state!
-            boolean isGroupEnd = itemIndex == array.size() - 1
-                    || array.get(itemIndex + 1) instanceof RaceListDataTypeHeader;
-            if (isGroupEnd) {
-                if (filteredItems.size() > 0) {
-                    RaceListDataType lastItemOfPreviousGroup = filteredItems.get(filteredItems.size() - 1);
-                    if (lastItemOfPreviousGroup != nextUnscheduldedItem) {
-                        int indexOfLastItemOfPreviousGroup = array.indexOf(lastItemOfPreviousGroup);
-                        // ... if there are some items filtered between this header and the last item
-                        // of the previous group ...
-                        if (indexOfLastItemOfPreviousGroup != (array.indexOf(item) - 1)
-                                && indexOfLastItemOfPreviousGroup != array.size() - 1) {
-                            RaceListDataType followingItem = array.get(indexOfLastItemOfPreviousGroup + 1);
-                            if (followingItem instanceof RaceListDataTypeRace) {
-                                RaceListDataTypeRace raceItem = (RaceListDataTypeRace) followingItem;
-                                // ... take it if it's unscheduled!
-                                if (raceItem.getCurrentStatus().equals(RaceLogRaceStatus.UNSCHEDULED)) {
-                                    filteredItems.add(followingItem);
-                                }
-                            }
-                        }
-                    }
-                }
-                newestFinishedItem = null;
-                nextUnscheduldedItem = null;
             }
         }
-
+        
         return createResults(filteredItems);
     }
 
