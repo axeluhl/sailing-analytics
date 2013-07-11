0e10bfc2ea94ebcf28029cb8e7755fbf29cf6905
Lennart Hensler <lennart.hensler@sap.com>
2013-07-11 14:49:02 +0200

    Added extractor related classes and interfaces.

---
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/shared/Extractor.java
@@ -0,0 +1,11 @@
+package com.sap.sailing.datamining.shared;
+
+import java.util.List;
+
+import com.sap.sailing.domain.tracking.GPSFixMoving;
+
+public interface Extractor {
+    
+    public List<Double> extractDataFrom(List<GPSFixMoving> gpsFixes);
+
+}
