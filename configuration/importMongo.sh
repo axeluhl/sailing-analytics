b6cc3737d7f482d1e26299f2b5002178b3a47a41
Simon Pamies <spamsch@gmail.com>
2013-07-15 09:36:33 +0200

    Uncompressing data before importing

--- configuration/importMongo.sh
+++ configuration/importMongo.sh
@@ -2,5 +2,7 @@
 MONGOPORTS=$@
 MONGOIMPORT="/opt/mongodb/bin/mongoexport --port $PORT -d winddb"
 
+echo "Uncompressing data..."
+tar xvzf exported.tar.gz
 echo "Importing data to $MONOGPORTS..."
 for i in *.json; do c=`basename $i .json`; $MONGOIMPORT --upsert --port $MONGOPORT -d winddb -c $c $i; done
