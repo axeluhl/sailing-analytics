9a9f3de2780460a6eb4ad12fbcb8d5bfb7e5406e
Axel Uhl <axel.uhl@sap.com>
2013-07-17 15:10:05 +0200

    removed tabs; replaced by spaces

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/clients/LoadClient.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/clients/LoadClient.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.racecommittee.app.data.clients;
 
 public interface LoadClient<T> {
-	public void onLoadFailed(Exception reason);
-	public void onLoadSucceded(T data);
+    public void onLoadFailed(Exception reason);
+
+    public void onLoadSucceded(T data);
 }
