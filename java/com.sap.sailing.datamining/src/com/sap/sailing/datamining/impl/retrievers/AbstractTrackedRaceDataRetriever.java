c731ba4a03f3bf946d3a315066f622f9d771b7bd
Lennart Hensler <lennart.hensler@sap.com>
2013-07-29 17:00:17 +0200

    Implemented the data retriever for a set of competitor names.

---
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/impl/retrievers/AbstractTrackedRaceDataRetriever.java
@@ -0,0 +1,50 @@
+package com.sap.sailing.datamining.impl.retrievers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sap.sailing.datamining.DataRetriever;
+import com.sap.sailing.datamining.GPSFixContext;
+import com.sap.sailing.datamining.GPSFixWithContext;
+import com.sap.sailing.datamining.impl.GPSFixContextImpl;
+import com.sap.sailing.datamining.impl.GPSFixWithContextImpl;
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.tracking.GPSFixMoving;
+import com.sap.sailing.domain.tracking.GPSFixTrack;
+import com.sap.sailing.domain.tracking.TrackedRace;
+
+public abstract class AbstractTrackedRaceDataRetriever implements DataRetriever {
+
+    private TrackedRace trackedRace;
+
+    public AbstractTrackedRaceDataRetriever(TrackedRace trackedRace) {
+        this.trackedRace = trackedRace;
+    }
+
+    @Override
+    public List<GPSFixWithContext> retrieveData() {
+        List<GPSFixWithContext> data = new ArrayList<GPSFixWithContext>();
+        for (Competitor competitor : trackedRace.getRace().getCompetitors()) {
+            if (retrieveDataFor(competitor)) {
+                GPSFixTrack<Competitor, GPSFixMoving> track = trackedRace.getTrack(competitor);
+                track.lockForRead();
+                try {
+                    for (GPSFixMoving gpsFix : track.getFixes()) {
+                        GPSFixContext context = new GPSFixContextImpl(trackedRace, competitor);
+                        data.add(new GPSFixWithContextImpl(gpsFix, context));
+                    }
+                } finally {
+                    track.unlockAfterRead();
+                }
+            }
+        }
+        return data;
+    }
+
+    protected TrackedRace getTrackedRace() {
+        return trackedRace;
+    }
+    
+    protected abstract boolean retrieveDataFor(Competitor competitor);
+
+}
---
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/impl/retrievers/CompetitorNameDataRetriever.java
@@ -0,0 +1,28 @@
+package com.sap.sailing.datamining.impl.retrievers;
+
+import java.util.Collection;
+
+import com.google.gwt.dev.util.collect.HashSet;
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.tracking.TrackedRace;
+
+public class CompetitorNameDataRetriever extends AbstractTrackedRaceDataRetriever {
+
+    private HashSet<String> competitorNames;
+
+    public CompetitorNameDataRetriever(TrackedRace trackedRace, Collection<String> competitorNames) {
+        super(trackedRace);
+        this.competitorNames = new HashSet<String>(competitorNames);
+    }
+
+    @Override
+    protected boolean retrieveDataFor(Competitor competitor) {
+        for (String competitorName : competitorNames) {
+            if (competitorName.equals(competitor.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
