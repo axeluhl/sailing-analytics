c556a95b9314460448eaf0f575ff603ea6b62a5f
Axel Uhl <axel.uhl@sap.com>
2013-08-08 16:06:00 +0200

    added web socket example

---
+++ java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/impl/WebSocketTestServlet.java
@@ -0,0 +1,44 @@
+package com.sap.sailing.server.gateway.impl;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+public class WebSocketTestServlet extends WebSocketServlet {
+    private static final long serialVersionUID = -5160225187387804019L;
+
+    @Override
+    public void configure(WebSocketServletFactory factory) {
+        factory.register(MyFirstWebSocket.class);
+    }
+    
+    @WebSocket
+    public static class MyFirstWebSocket extends WebSocketAdapter {
+        @Override
+        public void onWebSocketText(String s) {
+            System.out.println(s);
+        }
+
+        @Override
+        public void onWebSocketBinary(byte[] payload, int offset, int len) {
+            super.onWebSocketBinary(payload, offset, len);
+        }
+
+        @Override
+        public void onWebSocketClose(int statusCode, String reason) {
+            super.onWebSocketClose(statusCode, reason);
+        }
+
+        @Override
+        public void onWebSocketConnect(Session sess) {
+            super.onWebSocketConnect(sess);
+        }
+
+        @Override
+        public void onWebSocketError(Throwable cause) {
+            super.onWebSocketError(cause);
+        }
+    }
+}
