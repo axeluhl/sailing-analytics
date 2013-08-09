9abf06013da4cd6147834abd398827389c288679
Lennart Hensler <lennart.hensler@sap.com>
2013-08-09 11:24:38 +0200

    Added a test for range filter criterias

--- java/com.sap.sailing.datamining.test/src/com/sap/sailing/datamining/test/TestFilterCriterias.java
+++ java/com.sap.sailing.datamining.test/src/com/sap/sailing/datamining/test/TestFilterCriterias.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.sap.sailing.datamining.ClusterOfComparable;
 import com.sap.sailing.datamining.FilterCriteria;
+import com.sap.sailing.datamining.impl.ClusterOfComparableImpl;
+import com.sap.sailing.datamining.impl.criterias.RangeFilterCriteria;
 import com.sap.sailing.datamining.impl.criterias.RegexFilterCriteria;
 
 public class TestFilterCriterias {
@@ -27,6 +30,24 @@ public class TestFilterCriterias {
             assertTrue("Failed to match " + stringToMatch, regexFilterCriteria.matches(stringToMatch));
         }
         assertFalse("'Regatta (29ER)' shouldn't be matched", regexFilterCriteria.matches("Regatta (29ER)"));
+    }
+    
+    @Test
+    public void testRangeFilterCriteria() {
+        ClusterOfComparable<Integer> cluster = new ClusterOfComparableImpl<Integer>("Test", 3, 1);
+        FilterCriteria<Integer> rangeFilterCriteria = new RangeFilterCriteria<Integer, Integer>(cluster) {
+            @Override
+            public Integer getValue(Integer data) {
+                return data;
+            }
+        };
+
+        assertTrue(rangeFilterCriteria.matches(1));
+        assertTrue(rangeFilterCriteria.matches(2));
+        assertTrue(rangeFilterCriteria.matches(3));
+
+        assertFalse(rangeFilterCriteria.matches(0));
+        assertFalse(rangeFilterCriteria.matches(4));
     }
 
     private class StringRegexFilterCriteria extends RegexFilterCriteria<String> {
