9fac395474555ad6ec90d7e4b8491925b32b3028
Lennart Hensler <LennartHensler@aol.com>
2013-08-11 20:42:20 +0200

    Added a generic group key

---
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/impl/GenericGroupKey.java
@@ -0,0 +1,52 @@
+package com.sap.sailing.datamining.impl;
+
+import com.sap.sailing.datamining.shared.GroupKey;
+
+public abstract class GenericGroupKey<T> extends AbstractGroupKey {
+    
+    private T value;
+
+    public GenericGroupKey(T value) {
+        this.value = value;
+    }
+
+    protected T getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean hasSubKey() {
+        return false;
+    }
+
+    @Override
+    public GroupKey getSubKey() {
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GenericGroupKey<?> other = (GenericGroupKey<?>) obj;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
+}
--- java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/impl/StringGroupKey.java
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/impl/StringGroupKey.java
@@ -1,53 +1,14 @@
 package com.sap.sailing.datamining.impl;
 
-import com.sap.sailing.datamining.shared.GroupKey;
-
-public class StringGroupKey extends AbstractGroupKey {
+public class StringGroupKey extends GenericGroupKey<String> {
     
-    private String value;
-
     public StringGroupKey(String value) {
-        this.value = value;
+        super(value);
     }
 
     @Override
     public String asString() {
-        return value;
-    }
-
-    @Override
-    public boolean hasSubKey() {
-        return false;
-    }
-
-    @Override
-    public GroupKey getSubKey() {
-        return null;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((value == null) ? 0 : value.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        StringGroupKey other = (StringGroupKey) obj;
-        if (value == null) {
-            if (other.value != null)
-                return false;
-        } else if (!value.equals(other.value))
-            return false;
-        return true;
+        return getValue();
     }
 
 }
