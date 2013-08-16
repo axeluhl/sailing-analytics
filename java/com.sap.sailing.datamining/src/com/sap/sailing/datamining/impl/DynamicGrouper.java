dc8b81d8d5d867ad935abdac37802971aa42652f
Lennart Hensler <lennart.hensler@sap.com>
2013-08-16 12:56:43 +0200

    Increased performance of groovy execution

--- java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/impl/DynamicGrouper.java
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/impl/DynamicGrouper.java
@@ -2,26 +2,26 @@ package com.sap.sailing.datamining.impl;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
+import groovy.lang.Script;
 
 import com.sap.sailing.datamining.shared.GenericGroupKey;
 import com.sap.sailing.datamining.shared.GroupKey;
 
 public class DynamicGrouper<DataType> extends AbstractGrouper<DataType> {
     
-    private String scriptText;
-    private Binding binding;
+    private Script script;
 
     public DynamicGrouper(String scriptText) {
         super();
-        this.scriptText = scriptText;
-        binding = new Binding();
+        script = new GroovyShell().parse(scriptText);
     }
 
     @Override
     protected GroupKey getGroupKeyFor(DataType dataEntry) {
+        Binding binding = new Binding();
         binding.setVariable("data", dataEntry);
-        GroovyShell shell = new GroovyShell(binding);
-        Object value = shell.evaluate(scriptText);
+        script.setBinding(binding);
+        Object value = script.run();
         return new GenericGroupKey<Object>(value);
     }
 
