6acb01339f8489b83805dbd55da4fb0395ee6cec
D049941 <riccardo.nimser@sap.com>
2013-09-12 17:42:25 +0200

    New interface and bundle for test support added. The interface defines a method to clear the state of the application (including persisted data) and is used by the new bundle to create a well known state without any data, except the default leaderboard.
    Since this method should only be used in an isolated test environment,
    the defining interface is only visible to the new bundle, which is not
    deployed by default.

---
+++ java/com.sap.sailing.server.gateway.test.support/src/com/sap/sailing/server/gateway/test/support/ClearStateServlet.java
@@ -0,0 +1,63 @@
+package com.sap.sailing.server.gateway.test.support;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.BundleContext;
+
+import org.osgi.util.tracker.ServiceTracker;
+
+import com.sap.sailing.server.RacingEventService;
+import com.sap.sailing.server.test.support.RacingEventServiceWithTestSupport;
+
+public class ClearStateServlet extends HttpServlet {
+
+    private static final String OSGI_BUNDLECONTEXT_ATTRIBUTE_NAME = "osgi-bundlecontext"; 
+    
+    private ServiceTracker<RacingEventServiceWithTestSupport, RacingEventServiceWithTestSupport> tracker;
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {  
+        super.init(config);  
+        
+        ServletContext servletContext = config.getServletContext();
+        BundleContext bundleContext = (BundleContext) servletContext.getAttribute(OSGI_BUNDLECONTEXT_ATTRIBUTE_NAME);
+        
+        this.tracker = new ServiceTracker<>(bundleContext, RacingEventService.class.getName(), null);
+        this.tracker.open();
+    }
+
+    @Override
+    public void destroy() {
+        if(this.tracker != null) {
+            this.tracker.close();
+        }
+        
+        super.destroy();
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        try {
+            RacingEventServiceWithTestSupport service = getService();
+            service.clearSate();
+            
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        } catch (Exception exception) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+    }
+    
+    public RacingEventServiceWithTestSupport getService() {
+        return this.tracker.getService();
+    }
+}
