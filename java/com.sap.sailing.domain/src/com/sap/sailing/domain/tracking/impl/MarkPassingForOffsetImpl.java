ec45db46e010be2366547fb9dbeb8278ad8c9821
Nick Klose <nicolas.klose@sap.com>
2013-09-25 14:02:10 +0200

    Bug 1530: Added MarkPassingForOffsetWaypoint interface to create Controlpoints with a second MarkPassing

---
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/MarkPassingForOffsetImpl.java
@@ -0,0 +1,63 @@
+package com.sap.sailing.domain.tracking.impl;
+
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.Mark;
+import com.sap.sailing.domain.base.Waypoint;
+import com.sap.sailing.domain.common.TimePoint;
+import com.sap.sailing.domain.tracking.MarkPassing;
+import com.sap.sailing.domain.tracking.MarkPassingForOffsetWayPoint;
+
+public class MarkPassingForOffsetImpl implements MarkPassingForOffsetWayPoint {
+
+    private static final long serialVersionUID = -2168312586388285697L;
+    private final TimePoint timePoint;
+    private final Waypoint waypoint;
+    private final Competitor competitor;
+    private final Mark mark;
+    private final MarkPassing markpassingforOffset;
+
+    
+    public MarkPassingForOffsetImpl(TimePoint timePoint, Waypoint waypoint, Mark mark, Competitor competitor, MarkPassing markPassingforOffset) {
+        super();
+        this.timePoint = timePoint;
+        this.waypoint = waypoint;
+        this.competitor = competitor;
+        this.mark = mark;
+        this.markpassingforOffset = markPassingforOffset;
+    }
+
+    @Override
+    public TimePoint getTimePoint() {
+        return timePoint;
+    }
+
+    @Override
+    public Waypoint getWaypoint() {
+        return waypoint;
+    }
+    
+    @Override
+    public Mark getMark() {
+                return mark;
+    }
+
+    @Override
+    public Competitor getCompetitor() {
+        return competitor;
+    }
+    
+    @Override
+    public String toString() {
+        return ""+getTimePoint()+": "+getCompetitor()+" passed "+getWaypoint();
+    }
+
+    @Override
+    public MarkPassing getOffsetPassing() {
+        
+        return markpassingforOffset;
+    }
+
+}
+
+
+   
