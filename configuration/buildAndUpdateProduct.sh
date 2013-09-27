275c3a99291f99ead3700a4aec363b79642a88f9
STG (Server) <simon.marcel.pamies@sap.com>
2013-09-27 17:42:44 +0200

    fixed matching of line end for permutation reduction in build script

--- configuration/buildAndUpdateProduct.sh
+++ configuration/buildAndUpdateProduct.sh
@@ -273,15 +273,17 @@ if [[ "$@" == "build" ]] || [[ "$@" == "all" ]]; then
 	if [ $onegwtpermutationonly -eq 1 ]; then
 	    echo "INFO: Patching .gwt.xml files such that only one GWT permutation needs to be compiled"
 	    for i in com.sap.sailing.gwt.ui/src/main/resources/com/sap/sailing/gwt/ui/*.gwt.xml; do
+	        echo "INFO: Patching $i files such that only one GWT permutation needs to be compiled"
 		cp $i $i.bak
-	        cat $i | sed -e 's/^[	 ]*<extend-property name="locale" values="de"\/>[	 ]*$/<!-- <extend-property name="locale" values="de"\/> --> <set-property name="user.agent" value="safari" \/>/' >$i.sed
+	        cat $i | sed -e 's/^[	 ]*<extend-property  *name="locale"  *values="de" *\/>/<!-- <extend-property name="locale" values="de"\/> --> <set-property name="user.agent" value="safari" \/>/' >$i.sed
 		mv $i.sed $i
 	    done
 	else
             echo "INFO: Patching .gwt.xml files such that all GWT permutations are compiled"
 	    for i in com.sap.sailing.gwt.ui/src/main/resources/com/sap/sailing/gwt/ui/*.gwt.xml; do
+	        echo "INFO: Patching $i files such that all GWT permutations are compiled"
 		cp $i $i.bak
-	        cat $i | sed -e 's/<!-- <extend-property name="locale" values="de"\/> --> <set-property name="user.agent" value="safari" \/>/<extend-property name="locale" values="de"\/>/' >$i.sed
+	        cat $i | sed -e 's/<!-- <extend-property  *name="locale"  *values="de" *\/> --> <set-property name="user.agent" value="safari" \/>/<extend-property name="locale" values="de"\/>/' >$i.sed
 		mv $i.sed $i
 	    done
 	fi
