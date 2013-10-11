5a1f4449fec5dd2b49148168a42a4a64ce3f23fc
Lukas Niemeier <lukas.niemeier@sap.com>
2013-10-11 19:26:56 +0200

    # added some logging when remote configuration is applied

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/utils/TabletConfigurationHelper.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/utils/TabletConfigurationHelper.java
@@ -4,22 +4,33 @@ import android.content.Context;
 
 import com.sap.sailing.domain.base.TabletConfiguration;
 import com.sap.sailing.racecommittee.app.AppPreferences;
+import com.sap.sailing.racecommittee.app.logging.ExLog;
 
 public class TabletConfigurationHelper {
     
+    private static final String TAG = TabletConfigurationHelper.class.getName();
+
     public static void apply(Context context, TabletConfiguration configuration) {
         if (configuration.getAllowedCourseAreaNames() != null) {
             AppPreferences.setManagedCourseAreaNames(context, configuration.getAllowedCourseAreaNames());
+            logApply("course areas", configuration.getAllowedCourseAreaNames());
         }
         if (configuration.getMinimumRoundsForCourse() != null) {
             AppPreferences.setMinRounds(context, configuration.getMinimumRoundsForCourse());
+            logApply("minimum rounds", configuration.getMinimumRoundsForCourse());
         }
         if (configuration.getMaximumRoundsForCourse() != null) {
             AppPreferences.setMaxRounds(context, configuration.getMaximumRoundsForCourse());
+            logApply("maximum rounds", configuration.getMaximumRoundsForCourse());
         }
         if (configuration.getResultsMailRecipient() != null) {
             AppPreferences.setMailRecipient(context, configuration.getResultsMailRecipient());
+            logApply("mail recipient", configuration.getResultsMailRecipient());
         }
+    }
+    
+    private static void logApply(String configurationName, Object value) {
+        ExLog.i(TAG, String.format("Applied %s configuration: %s", configurationName, value.toString()));
     }
 
 }
