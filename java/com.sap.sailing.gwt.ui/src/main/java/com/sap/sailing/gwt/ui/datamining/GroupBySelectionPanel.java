b88f7efe9d80db965061e662dbb6770158ddccc4
Lennart Hensler <lennart.hensler@sap.com>
2013-10-11 13:33:37 +0200

    Extracted the group by selection from SimpleQueryDefinitionProvider

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/datamining/GroupBySelectionPanel.java
@@ -0,0 +1,190 @@
+package com.sap.sailing.gwt.ui.datamining;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.text.shared.Renderer;
+import com.google.gwt.user.client.ui.DeckPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.ValueListBox;
+import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.datamining.shared.QueryDefinition;
+import com.sap.sailing.datamining.shared.SharedDimension;
+import com.sap.sailing.datamining.shared.Components.GrouperType;
+import com.sap.sailing.gwt.ui.client.StringMessages;
+
+public abstract class GroupBySelectionPanel extends FlowPanel {
+
+    private StringMessages stringMessages;
+
+    private ValueListBox<GrouperType> grouperTypeListBox;
+    private DeckPanel groupByOptionsPanel;
+    private TextArea customGrouperScriptTextBox;
+    private HorizontalPanel dimensionsToGroupByPanel;
+    private List<ValueListBox<SharedDimension>> dimensionsToGroupByBoxes;
+
+    public GroupBySelectionPanel(StringMessages stringMessages) {
+        this.stringMessages = stringMessages;
+        dimensionsToGroupByBoxes = new ArrayList<ValueListBox<SharedDimension>>();
+
+        add(createGrouperTypeSelectionPanel());
+
+        groupByOptionsPanel = new DeckPanel();
+        add(groupByOptionsPanel);
+
+        dimensionsToGroupByPanel = new HorizontalPanel();
+        dimensionsToGroupByPanel.setSpacing(5);
+        groupByOptionsPanel.add(dimensionsToGroupByPanel);
+        
+        ValueListBox<SharedDimension> dimensionToGroupByBox = createDimensionToGroupByBox();
+        dimensionsToGroupByPanel.add(dimensionToGroupByBox);
+        dimensionsToGroupByBoxes.add(dimensionToGroupByBox);
+
+        FlowPanel dynamicGroupByPanel = new FlowPanel();
+        groupByOptionsPanel.add(dynamicGroupByPanel);
+        dynamicGroupByPanel.add(new Label("public Object getValueToGroupByFrom(GPSFix data) {"));
+        customGrouperScriptTextBox = new TextArea();
+        customGrouperScriptTextBox.setCharacterWidth(100);
+        customGrouperScriptTextBox.setVisibleLines(1);
+        dynamicGroupByPanel.add(customGrouperScriptTextBox);
+        dynamicGroupByPanel.add(new Label("}"));
+
+        groupByOptionsPanel.showWidget(0);
+    }
+
+    public void apply(QueryDefinition queryDefinition) {
+        grouperTypeListBox.setValue(queryDefinition.getGrouperType(), true);
+        
+        switch (queryDefinition.getGrouperType()) {
+        case Custom:
+            customGrouperScriptTextBox.setText(queryDefinition.getCustomGrouperScriptText());
+            break;
+        case Dimensions:
+            applyDimensionsToGroupBy(queryDefinition);
+            break;
+        default:
+            throw new IllegalArgumentException("Not yet implemented for the given data type: " + queryDefinition.getGrouperType().toString());
+        }
+    }
+
+    private void applyDimensionsToGroupBy(QueryDefinition queryDefinition) {
+        int index = 0;
+        for (SharedDimension dimension : queryDefinition.getDimensionsToGroupBy()) {
+            dimensionsToGroupByBoxes.get(index).setValue(dimension, true);
+            index++;
+        }
+    }
+
+    public GrouperType getGrouperType() {
+        return grouperTypeListBox.getValue();
+    }
+
+    public String getCustomGrouperScriptText() {
+        return getGrouperType() == GrouperType.Custom ? customGrouperScriptTextBox.getText() : "";
+    }
+
+    public Collection<SharedDimension> getDimensionsToGroupBy() {
+        Collection<SharedDimension> dimensionsToGroupBy = new ArrayList<SharedDimension>();
+        if (getGrouperType() == GrouperType.Dimensions) {
+            for (ValueListBox<SharedDimension> dimensionToGroupByBox : dimensionsToGroupByBoxes) {
+                if (dimensionToGroupByBox.getValue() != null) {
+                    dimensionsToGroupBy.add(dimensionToGroupByBox.getValue());
+                }
+            }
+        }
+        return dimensionsToGroupBy;
+    }
+
+    private HorizontalPanel createGrouperTypeSelectionPanel() {
+        HorizontalPanel selectGroupByPanel = new HorizontalPanel();
+        selectGroupByPanel.setSpacing(5);
+        
+        selectGroupByPanel.add(new Label(this.stringMessages.groupBy() + ": "));
+        grouperTypeListBox = new ValueListBox<GrouperType>(new Renderer<GrouperType>() {
+            @Override
+            public String render(GrouperType grouperType) {
+                if (grouperType == null) {
+                    return "";
+                }
+                return grouperType.toString();
+            }
+
+            @Override
+            public void render(GrouperType grouperType, Appendable appendable) throws IOException {
+                appendable.append(render(grouperType));
+            }
+        });
+        grouperTypeListBox.setValue(GrouperType.Dimensions, false);
+        grouperTypeListBox.setAcceptableValues(Arrays.asList(GrouperType.values()));
+        selectGroupByPanel.add(grouperTypeListBox);
+        
+        grouperTypeListBox.addValueChangeHandler(new ValueChangeHandler<GrouperType>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<GrouperType> event) {
+                if (event.getValue() != null) {
+                    switch (event.getValue()) {
+                    case Custom:
+                        groupByOptionsPanel.showWidget(1);
+                        break;
+                    case Dimensions:
+                        groupByOptionsPanel.showWidget(0);
+                        break;
+                    }
+                }
+                finishValueChangedHandling();
+            }
+        });
+        
+        return selectGroupByPanel;
+    }
+
+    private ValueListBox<SharedDimension> createDimensionToGroupByBox() {
+        ValueListBox<SharedDimension> dimensionToGroupByBox = new ValueListBox<SharedDimension>(
+                new Renderer<SharedDimension>() {
+                    @Override
+                    public String render(SharedDimension gpsFixDimension) {
+                        if (gpsFixDimension == null) {
+                            return "";
+                        }
+                        return gpsFixDimension.toString();
+                    }
+
+                    @Override
+                    public void render(SharedDimension gpsFixDimension, Appendable appendable)
+                            throws IOException {
+                        appendable.append(render(gpsFixDimension));
+
+                    }
+                });
+        dimensionToGroupByBox.addValueChangeHandler(new ValueChangeHandler<SharedDimension>() {
+            private boolean firstChange = true;
+
+            @Override
+            public void onValueChange(ValueChangeEvent<SharedDimension> event) {
+                if (firstChange && event.getValue() != null) {
+                    ValueListBox<SharedDimension> newBox = createDimensionToGroupByBox();
+                    dimensionsToGroupByPanel.add(newBox);
+                    dimensionsToGroupByBoxes.add(newBox);
+                    firstChange = false;
+                } else if (event.getValue() == null) {
+                    dimensionsToGroupByPanel.remove((Widget) event.getSource());
+                    dimensionsToGroupByBoxes.remove(event.getSource());
+                }
+                finishValueChangedHandling();
+            }
+        });
+        dimensionToGroupByBox.setAcceptableValues(Arrays.asList(SharedDimension.values()));
+        return dimensionToGroupByBox;
+    }
+    
+    protected abstract void finishValueChangedHandling();
+    
+}
--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/datamining/SimpleQueryDefinitionProvider.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/datamining/SimpleQueryDefinitionProvider.java
@@ -1,10 +1,7 @@
 package com.sap.sailing.gwt.ui.datamining;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map.Entry;
 
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -13,16 +10,13 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.text.shared.Renderer;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DeckPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.ValueListBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.SelectionChangeEvent;
 import com.sap.sailing.datamining.shared.AggregatorType;
-import com.sap.sailing.datamining.shared.Components.GrouperType;
 import com.sap.sailing.datamining.shared.QueryDefinition;
 import com.sap.sailing.datamining.shared.SharedDimension;
 import com.sap.sailing.datamining.shared.SimpleQueryDefinition;
@@ -36,19 +30,16 @@ public class SimpleQueryDefinitionProvider extends AbstractQueryDefinitionProvid
     private FlowPanel mainPanel;
 
     private SelectionTablesPanel selectionTablesPanel;
-    private ValueListBox<GrouperType> grouperTypeListBox;
-    private TextArea customGrouperScriptTextBox;
-    private HorizontalPanel dimensionsToGroupByPanel;
-    private List<ValueListBox<SharedDimension>> dimensionsToGroupByBoxes;
     
     private StatisticsProvider statisticsProvider;
     private ValueListBox<StatisticAndAggregatorType> statisticsListBox;
+
+    private GroupBySelectionPanel groupBySelectionPanel;
 
     public SimpleQueryDefinitionProvider(StringMessages stringMessages, SailingServiceAsync sailingService,
             ErrorReporter errorReporter) {
         super(stringMessages, sailingService, errorReporter);
         mainPanel = new FlowPanel();
-        dimensionsToGroupByBoxes = new ArrayList<ValueListBox<SharedDimension>>();
         statisticsProvider = new SimpleStatisticsProvider();
 
         selectionTablesPanel = new SelectionTablesPanel(stringMessages, sailingService, errorReporter);
@@ -64,14 +55,14 @@ public class SimpleQueryDefinitionProvider extends AbstractQueryDefinitionProvid
     
     @Override
     public QueryDefinition getQueryDefinition() {
-        SimpleQueryDefinition queryDTO = new SimpleQueryDefinition(getGrouperType(), getStatisticType(), getAggregatorType());
+        SimpleQueryDefinition queryDTO = new SimpleQueryDefinition(groupBySelectionPanel.getGrouperType(), getStatisticType(), getAggregatorType());
         switch (queryDTO.getGrouperType()) {
         case Custom:
-            queryDTO.setCustomGrouperScriptText(getCustomGrouperScriptText());
+            queryDTO.setCustomGrouperScriptText(groupBySelectionPanel.getCustomGrouperScriptText());
             break;
         case Dimensions:
         default:
-            for (SharedDimension dimension : getDimensionsToGroupBy()) {
+            for (SharedDimension dimension : groupBySelectionPanel.getDimensionsToGroupBy()) {
                 queryDTO.appendDimensionToGroupBy(dimension);
             }
             break;
@@ -85,63 +76,16 @@ public class SimpleQueryDefinitionProvider extends AbstractQueryDefinitionProvid
     @Override
     public void applyQueryDefinition(QueryDefinition queryDefinition) {
         setBlockChangeNotification(true);
-        applySelection(queryDefinition);
-        applyGrouping(queryDefinition);
+        selectionTablesPanel.applySelection(queryDefinition);
+        groupBySelectionPanel.apply(queryDefinition);
         applyStatistic(queryDefinition);
         setBlockChangeNotification(false);
         
         notifyQueryDefinitionChanged();
     }
 
-    private void applySelection(QueryDefinition queryDefinition) {
-        selectionTablesPanel.applySelection(queryDefinition);
-    }
-
-    private void applyGrouping(QueryDefinition queryDefinition) {
-        grouperTypeListBox.setValue(queryDefinition.getGrouperType(), true);
-        
-        switch (queryDefinition.getGrouperType()) {
-        case Custom:
-            customGrouperScriptTextBox.setText(queryDefinition.getCustomGrouperScriptText());
-            break;
-        case Dimensions:
-            applyDimensionsToGroupBy(queryDefinition);
-            break;
-        default:
-            throw new IllegalArgumentException("Not yet implemented for the given data type: " + queryDefinition.getGrouperType().toString());
-        }
-    }
-
-    private void applyDimensionsToGroupBy(QueryDefinition queryDefinition) {
-        int index = 0;
-        for (SharedDimension dimension : queryDefinition.getDimensionsToGroupBy()) {
-            dimensionsToGroupByBoxes.get(index).setValue(dimension, true);
-            index++;
-        }
-    }
-
     private void applyStatistic(QueryDefinition queryDefinition) {
         statisticsListBox.setValue(statisticsProvider.getStatistic(queryDefinition.getStatisticType(), queryDefinition.getAggregatorType()), false);
-    }
-
-    private GrouperType getGrouperType() {
-        return grouperTypeListBox.getValue();
-    }
-
-    private String getCustomGrouperScriptText() {
-        return getGrouperType() == GrouperType.Custom ? customGrouperScriptTextBox.getText() : "";
-    }
-
-    private Collection<SharedDimension> getDimensionsToGroupBy() {
-        Collection<SharedDimension> dimensionsToGroupBy = new ArrayList<SharedDimension>();
-        if (getGrouperType() == GrouperType.Dimensions) {
-            for (ValueListBox<SharedDimension> dimensionToGroupByBox : dimensionsToGroupByBoxes) {
-                if (dimensionToGroupByBox.getValue() != null) {
-                    dimensionsToGroupBy.add(dimensionToGroupByBox.getValue());
-                }
-            }
-        }
-        return dimensionsToGroupBy;
     }
 
     private StatisticType getStatisticType() {
@@ -165,7 +109,13 @@ public class SimpleQueryDefinitionProvider extends AbstractQueryDefinitionProvid
         });
         functionsPanel.add(clearSelectionButton);
 
-        functionsPanel.add(createGroupByPanel());
+        groupBySelectionPanel = new GroupBySelectionPanel(getStringMessages()) {
+            @Override
+            protected void finishValueChangedHandling() {
+                notifyQueryDefinitionChanged();
+            }
+        };
+        functionsPanel.add(groupBySelectionPanel);
 
         functionsPanel.add(new Label(getStringMessages().statisticToCalculate() + ": "));
         statisticsListBox = new ValueListBox<StatisticAndAggregatorType>(new Renderer<StatisticAndAggregatorType>() {
@@ -196,111 +146,6 @@ public class SimpleQueryDefinitionProvider extends AbstractQueryDefinitionProvid
         functionsPanel.add(statisticsListBox);
 
         return functionsPanel;
-    }
-
-    private FlowPanel createGroupByPanel() {
-        FlowPanel groupByPanel = new FlowPanel();
-
-        HorizontalPanel selectGroupByPanel = new HorizontalPanel();
-        selectGroupByPanel.setSpacing(5);
-        selectGroupByPanel.add(new Label(getStringMessages().groupBy() + ": "));
-        groupByPanel.add(selectGroupByPanel);
-
-        grouperTypeListBox = new ValueListBox<GrouperType>(new Renderer<GrouperType>() {
-            @Override
-            public String render(GrouperType grouperType) {
-                if (grouperType == null) {
-                    return "";
-                }
-                return grouperType.toString();
-            }
-
-            @Override
-            public void render(GrouperType grouperType, Appendable appendable) throws IOException {
-                appendable.append(render(grouperType));
-            }
-        });
-        grouperTypeListBox.setValue(GrouperType.Dimensions, false);
-        grouperTypeListBox.setAcceptableValues(Arrays.asList(GrouperType.values()));
-        selectGroupByPanel.add(grouperTypeListBox);
-
-        final DeckPanel groupByOptionsPanel = new DeckPanel();
-        groupByPanel.add(groupByOptionsPanel);
-        grouperTypeListBox.addValueChangeHandler(new ValueChangeHandler<GrouperType>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<GrouperType> event) {
-                if (event.getValue() != null) {
-                    switch (event.getValue()) {
-                    case Custom:
-                        groupByOptionsPanel.showWidget(1);
-                        break;
-                    case Dimensions:
-                        groupByOptionsPanel.showWidget(0);
-                        break;
-                    }
-                }
-                notifyQueryDefinitionChanged();
-            }
-        });
-
-        dimensionsToGroupByPanel = new HorizontalPanel();
-        dimensionsToGroupByPanel.setSpacing(5);
-        groupByOptionsPanel.add(dimensionsToGroupByPanel);
-        
-        ValueListBox<SharedDimension> dimensionToGroupByBox = createDimensionToGroupByBox();
-        dimensionsToGroupByPanel.add(dimensionToGroupByBox);
-        dimensionsToGroupByBoxes.add(dimensionToGroupByBox);
-
-        FlowPanel dynamicGroupByPanel = new FlowPanel();
-        groupByOptionsPanel.add(dynamicGroupByPanel);
-        dynamicGroupByPanel.add(new Label("public Object getValueToGroupByFrom(GPSFix data) {"));
-        customGrouperScriptTextBox = new TextArea();
-        customGrouperScriptTextBox.setCharacterWidth(100);
-        customGrouperScriptTextBox.setVisibleLines(1);
-        dynamicGroupByPanel.add(customGrouperScriptTextBox);
-        dynamicGroupByPanel.add(new Label("}"));
-
-        groupByOptionsPanel.showWidget(0);
-        return groupByPanel;
-    }
-
-    private ValueListBox<SharedDimension> createDimensionToGroupByBox() {
-        ValueListBox<SharedDimension> dimensionToGroupByBox = new ValueListBox<SharedDimension>(
-                new Renderer<SharedDimension>() {
-                    @Override
-                    public String render(SharedDimension gpsFixDimension) {
-                        if (gpsFixDimension == null) {
-                            return "";
-                        }
-                        return gpsFixDimension.toString();
-                    }
-
-                    @Override
-                    public void render(SharedDimension gpsFixDimension, Appendable appendable)
-                            throws IOException {
-                        appendable.append(render(gpsFixDimension));
-
-                    }
-                });
-        dimensionToGroupByBox.addValueChangeHandler(new ValueChangeHandler<SharedDimension>() {
-            private boolean firstChange = true;
-
-            @Override
-            public void onValueChange(ValueChangeEvent<SharedDimension> event) {
-                if (firstChange && event.getValue() != null) {
-                    ValueListBox<SharedDimension> newBox = createDimensionToGroupByBox();
-                    dimensionsToGroupByPanel.add(newBox);
-                    dimensionsToGroupByBoxes.add(newBox);
-                    firstChange = false;
-                } else if (event.getValue() == null) {
-                    dimensionsToGroupByPanel.remove((Widget) event.getSource());
-                    dimensionsToGroupByBoxes.remove(event.getSource());
-                }
-                notifyQueryDefinitionChanged();
-            }
-        });
-        dimensionToGroupByBox.setAcceptableValues(Arrays.asList(SharedDimension.values()));
-        return dimensionToGroupByBox;
     }
 
     @Override
