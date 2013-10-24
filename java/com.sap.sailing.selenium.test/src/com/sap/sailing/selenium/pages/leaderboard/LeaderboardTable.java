84f6b5ec78468da19135d64af4f9b0c389a83a85
D049941 <riccardo.nimser@sap.com>
2013-10-24 16:36:22 +0200

    Selenium updated to version 2.37.0

---
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/leaderboard/LeaderboardTable.java
@@ -0,0 +1,65 @@
+package com.sap.sailing.selenium.pages.leaderboard;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.sap.sailing.selenium.pages.gwt.CellTable2;
+import com.sap.sailing.selenium.pages.gwt.DataEntry;
+
+import com.sap.sailing.selenium.pages.leaderboard.LeaderboardTable.LeaderboardEntry;
+
+public class LeaderboardTable extends CellTable2<LeaderboardEntry> {
+    public class LeaderboardEntry extends DataEntry {
+        private static final String TOTAL_RANK = "Total rank";
+        private static final String COMPETITOR = "Competitor";
+        private static final String NAME = "Name";
+        
+        protected LeaderboardEntry(WebElement element) {
+            super(LeaderboardTable.this, element);
+        }
+        
+        
+        public String getDescriptor() {
+            return getCompetitor();
+        }
+        
+        public int getTotalRank() {
+            return Integer.parseInt(getColumnContent(TOTAL_RANK));
+        }
+        
+        public String getCompetitor() {
+            return getColumnContent(COMPETITOR);
+        }
+        
+        public String getName() {
+            return getColumnContent(NAME);
+        }
+        
+//        public int getTotalSailedDistance() {
+//        }
+//        public void getAverageSpeedOverGround() {
+//        }
+//        public void getMaximumSpeedOverGround() {
+//        }
+//        public int getTotalDownwindTime() {
+//        }
+//        public int getTotalUpwindTime() {
+//        }
+//        public int getTotalReachingLegsTime() {
+//        }
+//        public int getTotalSailingTime() {
+//        }
+//        public int getTotalPoints() {
+//        }
+    }
+    
+    public LeaderboardTable(WebDriver driver, WebElement element) {
+        super(driver, element);
+        
+    }
+    
+    @Override
+    protected LeaderboardEntry createDataEntry(WebElement element) {
+        return new LeaderboardEntry(element);
+    }
+}
