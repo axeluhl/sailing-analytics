df9445499bfb7f21d7e99b314f4285d4077fff67
Jens Rommel <jens.rommel@sap.com>
2013-11-13 17:52:23 +0100

    - changed time stamp source from UTC (system time) to GPS Time - replaced hard-wired column numbers by order given by header line - fixed issue with Excel date conversion. Now settings miliseconds to zero.

--- java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/expeditionimport/WindLogParser.java
+++ java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/expeditionimport/WindLogParser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -38,17 +39,21 @@ public class WindLogParser {
 		private static final Calendar EXCEL_EPOCH_START = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
 		static {
 			EXCEL_EPOCH_START.set(1899, Calendar.DECEMBER, 30, 0, 0, 0);
+			EXCEL_EPOCH_START.set(Calendar.MILLISECOND, 0);
 		}
-		private static final int MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+		private static final BigDecimal MILLISECONDS_PER_DAY = BigDecimal.valueOf(24 * 60 * 60 * 1000);
 
 		private TimePoint timePoint;
 		private Position position;
 		private SpeedWithBearing trueWindSpeedWithBearing;
 
 		public void updateTime(String time_ExcelEpoch) {
-			double excelTimeStamp = Double.parseDouble(time_ExcelEpoch);
-			long millisecondsSinceExcelEpochStart = (long) (MILLISECONDS_PER_DAY * excelTimeStamp);
-			this.timePoint = new MillisecondsTimePoint(EXCEL_EPOCH_START.getTimeInMillis() + millisecondsSinceExcelEpochStart);
+            if (!time_ExcelEpoch.trim().isEmpty()) {
+                
+                BigDecimal timeStamp = new BigDecimal(time_ExcelEpoch);
+                long millisecondsSinceExcelEpochStart = timeStamp.multiply(MILLISECONDS_PER_DAY).longValue();
+    			this.timePoint = new MillisecondsTimePoint(EXCEL_EPOCH_START.getTimeInMillis() + millisecondsSinceExcelEpochStart);
+            }
 
 		}
 
@@ -89,13 +94,63 @@ public class WindLogParser {
 
 	}
 
-	private static final int CSV_INDEX_TIME_STAMP = 0;
-	private static final int CSV_INDEX_TRUE_WIND_SPEED = 5;
-	private static final int CSV_INDEX_TRUE_WIND_DIRECTION = 6;
-	private static final int CSV_INDEX_LAT = 37;
-	private static final int CSV_INDEX_LONG = 38;
+//	private static final int CSV_INDEX_TIME_STAMP = 0;
+//	private static final int CSV_INDEX_TRUE_WIND_SPEED = 5;
+//	private static final int CSV_INDEX_TRUE_WIND_DIRECTION = 6;
+//	private static final int CSV_INDEX_LAT = 37;
+//	private static final int CSV_INDEX_LONG = 38;
 	
-	private static final int CSV_MAX_INDEX = Collections.max(Arrays.asList(CSV_INDEX_TIME_STAMP, CSV_INDEX_TRUE_WIND_SPEED, CSV_INDEX_TRUE_WIND_DIRECTION, CSV_INDEX_LAT, CSV_INDEX_LONG));
+    private static final String COL_NAME_TIME_STAMP = "GPS Time";
+    private static final String COL_NAME_TRUE_WIND_SPEED = "Tws";
+    private static final String COL_NAME_TRUE_WIND_DIRECTION = "Twd";
+    private static final String COL_NAME_LAT = "Lat";
+    private static final String COL_NAME_LONG = "Lon";
+    
+    private static class ColumnNumbers {
+        
+        Integer gpsTime;
+        Integer trueWindSpeed;
+        Integer trueWindDirection;
+        Integer lat;
+        Integer lon;
+        final int max;
+
+        ColumnNumbers(String headerLine) {
+            if (headerLine == null) {
+                throw new NullPointerException("Empty header line.");
+            }
+            //supposed to be like Utc,Bsp,Awa,Aws,Twa,Tws,Twd,Rudder2,Leeway,Set,Drift,Hdg,AirTmp,SeaTmp,Baro,Depth,Heel,Trim,Rudder,Tab,Forestay,Downhaul,MastAng,FrstyLen,MastButt,LoadStbd,LoadPort,Rake,Volts,ROT,GpsQual,GpsPDOP,GpsNum,GpsAge,GpsAltitude,GpsGeoSep,GpsMode,Lat,Lon,Cog,Sog,DiffStn,Error,StbRunner,PrtRunner,Vang,Traveller,MainSheet,KeelAng,KeelHt,CanardH,OilPres,RPM1,RPM2,Dagger Bd,Boom Pos,DistToLn,RchTmToLn,RchDtToLn,GPS Time,Downhaul2,MkLat,MkLon,PortLat,PortLon,StbdLat,StbdLon,GPS HPE,RH,LeadPt,LeadSb,BkStay,User 0,User 1,User 2,User 3,User 4,User 5,User 6,User 7,User 8,User 9,User 10,User 11,User 12,User 13,User 14,User 15,User 16,User 17,User 18,User 19,User 20,User 21,User 22,User 23,User 24,User 25,User 26,User 27,User 28,User 29,User 30,User 31,TmToGun,TmToLn,TmToBurn,BelowLn,GunBlwLn,WvSigHt,WvSigPd,WvMaxHt,WvMaxPd,Slam,Motion
+            //there may be slight difference from one Expedition version to another, but so far not in the wind-related columns 
+            String[] columns = LINE_PATTERN.split(headerLine);
+            for (int i = 0; i < columns.length; i++) {
+                switch (columns[i]) {
+                case COL_NAME_TIME_STAMP:
+                    gpsTime = i;
+                    break;
+                case COL_NAME_TRUE_WIND_SPEED:
+                    trueWindSpeed = i;
+                    break;
+                case COL_NAME_TRUE_WIND_DIRECTION:
+                    trueWindDirection = i;
+                    break;
+                case COL_NAME_LAT:
+                    lat = i;
+                    break;
+                case COL_NAME_LONG:
+                    lon = i;
+                    break;
+                }
+
+            }
+
+            if (gpsTime == null || trueWindSpeed == null || trueWindDirection == null || lat == null || lon == null) {
+                throw new RuntimeException("Unexpected csv header for Expedition wind import: " + headerLine);
+            } else {
+                max = Collections.max(Arrays.asList(gpsTime, trueWindSpeed, trueWindDirection, lat, lon));
+            }
+            
+        }
+    }
 
 	private static final Pattern LINE_PATTERN = Pattern.compile(",");
 
@@ -108,18 +163,18 @@ public class WindLogParser {
 	public static List<Wind> importWind(InputStream windStream) throws IOException {
 		BufferedReader csvReader = new BufferedReader(new InputStreamReader(windStream));
 		String headerLine = csvReader.readLine();
-		assertHeaderFormat(headerLine);
+		ColumnNumbers columnNumbers = new ColumnNumbers(headerLine);
 
 		List<Wind> result = new ArrayList<Wind>();
 		String dataLine;
 		WindBuffer windBuffer = new WindBuffer();
 		while ((dataLine = csvReader.readLine()) != null) {
-			String[] data = parseDataLine(dataLine);
+			String[] data = parseDataLine(dataLine, columnNumbers.max);
 
 			if (data != null) {
-				windBuffer.updateTime(data[CSV_INDEX_TIME_STAMP]);
-				windBuffer.updateWindData(data[CSV_INDEX_TRUE_WIND_SPEED], data[CSV_INDEX_TRUE_WIND_DIRECTION]);
-				windBuffer.updatePosition(data[CSV_INDEX_LAT], data[CSV_INDEX_LONG]);
+				windBuffer.updateTime(data[columnNumbers.gpsTime]);
+				windBuffer.updateWindData(data[columnNumbers.trueWindSpeed], data[columnNumbers.trueWindDirection]);
+				windBuffer.updatePosition(data[columnNumbers.lat], data[columnNumbers.lon]);
 
 				Wind wind = windBuffer.createWindIfReady();
 				if (wind != null) {
@@ -131,29 +186,10 @@ public class WindLogParser {
 		return result;
 	}
 
-	private static void assertHeaderFormat(String headerLine) {
-		if (headerLine == null) {
-			throw new NullPointerException("Empty header line.");
-		}
-		//supposed to be Utc,Bsp,Awa,Aws,Twa,Tws,Twd,Rudder2,Leeway,Set,Drift,Hdg,AirTmp,SeaTmp,Baro,Depth,Heel,Trim,Rudder,Tab,Forestay,Downhaul,MastAng,FrstyLen,MastButt,LoadStbd,LoadPort,Rake,Volts,ROT,GpsQual,GpsPDOP,GpsNum,GpsAge,GpsAltitude,GpsGeoSep,GpsMode,Lat,Lon,Cog,Sog,DiffStn,Error,StbRunner,PrtRunner,Vang,Traveller,MainSheet,KeelAng,KeelHt,CanardH,OilPres,RPM1,RPM2,Dagger Bd,Boom Pos,DistToLn,RchTmToLn,RchDtToLn,GPS Time,Downhaul2,MkLat,MkLon,PortLat,PortLon,StbdLat,StbdLon,GPS HPE,RH,LeadPt,LeadSb,BkStay,User 0,User 1,User 2,User 3,User 4,User 5,User 6,User 7,User 8,User 9,User 10,User 11,User 12,User 13,User 14,User 15,User 16,User 17,User 18,User 19,User 20,User 21,User 22,User 23,User 24,User 25,User 26,User 27,User 28,User 29,User 30,User 31,TmToGun,TmToLn,TmToBurn,BelowLn,GunBlwLn,WvSigHt,WvSigPd,WvMaxHt,WvMaxPd,Slam,Motion
-		String[] columns = LINE_PATTERN.split(headerLine);
-		if ("Utc".equals(columns[CSV_INDEX_TIME_STAMP]) && "Tws".equals(columns[CSV_INDEX_TRUE_WIND_SPEED]) && "Twd".equals(columns[CSV_INDEX_TRUE_WIND_DIRECTION]) && "Lat".equals(columns[CSV_INDEX_LAT]) && "Lon".equals(columns[CSV_INDEX_LONG])) {
-			return;
-		} else {
-			throw new RuntimeException("Unexpected csv header for Expedition wind import: " + headerLine);
-		}
-	}
-
-	private static String[] parseDataLine(String dataLine) {
+	private static String[] parseDataLine(String dataLine, int minLength) {
 		String[] data = LINE_PATTERN.split(dataLine);
-		if (data.length >= CSV_MAX_INDEX) {
-			//        	if (!data[CSV_INDEX_TIME_STAMP].trim().isEmpty() 
-			//        			&& !data[CSV_INDEX_TRUE_WIND_SPEED].trim().isEmpty() 
-			//        			&& !data[CSV_INDEX_TRUE_WIND_DIRECTION].trim().isEmpty() 
-			//        			&& !data[CSV_INDEX_LAT].trim().isEmpty() 
-			//        			&& !data[CSV_INDEX_LONG].trim().isEmpty()) {
+		if (data.length >= minLength) {
 			return data;
-			//        	}
 		}
 		//else
 		return null;
