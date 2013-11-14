87e1e9743d456c8f9728d6da8722ee31e0e11b1c
Axel Uhl <axel.uhl@sap.com>
2013-11-14 13:43:35 +0100

    added structure for Igtimi connector

---
+++ java/com.sap.sailing.domain.igtimiadapter/src/com/sap/sailing/domain/igtimiadapter/IgtimiConnectorFactory.java
@@ -0,0 +1,7 @@
+package com.sap.sailing.domain.igtimiadapter;
+
+import com.sap.sailing.domain.igtimiadapter.impl.IgtimiConnectorFactoryImpl;
+
+public interface IgtimiConnectorFactory {
+    static IgtimiConnectorFactory INSTANCE = new IgtimiConnectorFactoryImpl();
+}
