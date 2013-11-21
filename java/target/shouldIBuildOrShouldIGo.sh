06da3870149d5212c80874be2279bf8e873f1ca7
Simon Pamies <spamsch@gmail.com>
2013-11-21 11:14:39 +0100

    Make sure to remove old files that could still be available

--- java/target/shouldIBuildOrShouldIGo.sh
+++ java/target/shouldIBuildOrShouldIGo.sh
@@ -50,6 +50,8 @@ find_project_home ()
 load_from_release_file ()
 {
     cd $USER_HOME/servers/server
+    rm -f ./$USE_ENVIRONMENT
+    rm -f ./$INSTALL_FROM_RELEASE.tar.gz*
     rm -rf plugins start stop status native-libraries org.eclipse.osgi
     echo "Loading from release file http://releases.sapsailing.com/$INSTALL_FROM_RELEASE/$INSTALL_FROM_RELEASE.tar.gz"
     `which wget` http://releases.sapsailing.com/$INSTALL_FROM_RELEASE/$INSTALL_FROM_RELEASE.tar.gz
