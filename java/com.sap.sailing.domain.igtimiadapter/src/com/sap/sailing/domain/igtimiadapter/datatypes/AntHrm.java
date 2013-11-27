847e2a24586782e6586c3b10f41fb16846a43a67
Axel Uhl <axel.uhl@sap.com>
2013-11-27 11:04:01 +0100

    seems that the ant_hrm message type 13 only provides one and not three values; request e-mail sent to Brent for clarification

--- java/com.sap.sailing.domain.igtimiadapter/src/com/sap/sailing/domain/igtimiadapter/datatypes/AntHrm.java
+++ java/com.sap.sailing.domain.igtimiadapter/src/com/sap/sailing/domain/igtimiadapter/datatypes/AntHrm.java
@@ -3,7 +3,6 @@ package com.sap.sailing.domain.igtimiadapter.datatypes;
 import java.util.Map;
 
 import com.sap.sailing.domain.common.TimePoint;
-import com.sap.sailing.domain.common.impl.MillisecondsTimePoint;
 import com.sap.sailing.domain.igtimiadapter.Sensor;
 
 /**
@@ -13,26 +12,11 @@ import com.sap.sailing.domain.igtimiadapter.Sensor;
  *
  */
 public class AntHrm extends Fix {
-    /**
-     * The ANT protocol specifies its own time stamp that may vary from the {@link #getTimePoint transmitter's time stamp}. 
-     */
-    private final TimePoint eventTime;
-    private final int heartBeatCount;
     private final int heartRate;
     
     public AntHrm(TimePoint timePoint, Sensor sensor, Map<Integer, Object> valuesPerSubindex) {
         super(sensor, timePoint);
-        eventTime = new MillisecondsTimePoint(((Number) valuesPerSubindex.get(1)).longValue());
-        heartBeatCount = ((Number) valuesPerSubindex.get(2)).intValue();
-        heartRate = ((Number) valuesPerSubindex.get(3)).intValue();
-    }
-
-    public TimePoint getEventTime() {
-        return eventTime;
-    }
-
-    public int getHeartBeatCount() {
-        return heartBeatCount;
+        heartRate = ((Number) valuesPerSubindex.get(1)).intValue();
     }
 
     public int getHeartRate() {
