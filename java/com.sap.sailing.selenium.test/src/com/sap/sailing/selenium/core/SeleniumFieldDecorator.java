b53305eccd5fb441039fa4ea116f9c509735f6d6
D049941 <riccardo.nimser@sap.com>
2013-11-30 13:55:24 +0100

    Indentation changed to use spaces only and line endings changed to Unix.

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/core/SeleniumFieldDecorator.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/core/SeleniumFieldDecorator.java
@@ -21,67 +21,66 @@ import org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler;
 import org.openqa.selenium.support.pagefactory.internal.LocatingElementListHandler;
 
 public class SeleniumFieldDecorator implements FieldDecorator {
-	private ElementLocatorFactory factory;
-	
-	public SeleniumFieldDecorator(ElementLocatorFactory factory) {
-	    this.factory = factory;
-	}
-	
-	@Override
-	public Object decorate(ClassLoader loader, Field field) {
-		ElementLocator locator = this.factory.createLocator(field);
-		
-	    if (locator == null) {
-	    	return null;
-	    }
+    private ElementLocatorFactory factory;
+    
+    public SeleniumFieldDecorator(ElementLocatorFactory factory) {
+        this.factory = factory;
+    }
+    
+    @Override
+    public Object decorate(ClassLoader loader, Field field) {
+        ElementLocator locator = this.factory.createLocator(field);
+        
+        if (locator == null) {
+        	return null;
+        }
+        
+        if (isDecoratableField(field)) {
+        	return proxyForElementLocator(loader, locator);
+        }
+        
+        if (isDecoratableList(field)) {
+        	return proxyForElementsLocator(loader, locator);
+        }
+        
+        return null;
+    }
+    
+    private boolean isDecoratableField(Field field) {
+        return WebElement.class.isAssignableFrom(field.getType());
+    }
+    
+    private boolean isDecoratableList(Field field) {
+        if (!List.class.isAssignableFrom(field.getType())) {
+          return false;
+        }
+        
+        // Type erasure in Java isn't complete. Attempt to discover the generic type of the list.
+        Type genericType = field.getGenericType();
+        if (!(genericType instanceof ParameterizedType)) {
+            return false;
+        }
+        
+        Type listType = ((ParameterizedType) genericType).getActualTypeArguments()[0];
+        
+        if (!WebElement.class.equals(listType)) {
+            return false;
+        }
+    
+        return true;
+      }
+    
+    private Object proxyForElementLocator(ClassLoader loader, ElementLocator locator) {
+        InvocationHandler handler = new LocatingElementHandler(locator);
+        Object proxy = Proxy.newProxyInstance( loader, new Class[] {WebElement.class, WrapsElement.class, Locatable.class}, handler);
+        
+        return proxy;
+    }
 
-	    if (isDecoratableField(field)) {
-	    	return proxyForElementLocator(loader, locator);
-	    }
-	    
-	    if (isDecoratableList(field)) {
-	    	return proxyForElementsLocator(loader, locator);
-	    }
-	    
-	    return null;
-	}
-	
-	private boolean isDecoratableField(Field field) {
-		return WebElement.class.isAssignableFrom(field.getType());
-	}
-	
-	private boolean isDecoratableList(Field field) {
-	    if (!List.class.isAssignableFrom(field.getType())) {
-	      return false;
-	    }
-
-	    // Type erasure in Java isn't complete. Attempt to discover the generic type of the list.
-	    Type genericType = field.getGenericType();
-	    if (!(genericType instanceof ParameterizedType)) {
-	    	return false;
-	    }
-
-	    Type listType = ((ParameterizedType) genericType).getActualTypeArguments()[0];
-
-	    if (!WebElement.class.equals(listType)) {
-	    	return false;
-	    }
-
-	    return true;
-	  }
-	
-	private Object proxyForElementLocator(ClassLoader loader, ElementLocator locator) {
-	    InvocationHandler handler = new LocatingElementHandler(locator);
-	    Object proxy = Proxy.newProxyInstance( loader, new Class[] {WebElement.class, WrapsElement.class, Locatable.class}, handler);
-	    
-	    return proxy;
-	}
-	
-	private Object proxyForElementsLocator(ClassLoader loader, ElementLocator locator) {
-		InvocationHandler handler = new LocatingElementListHandler(locator);
-		Object proxy = Proxy.newProxyInstance(loader, new Class[] {List.class}, handler);
-		
-    	return proxy;
-	}
-
+    private Object proxyForElementsLocator(ClassLoader loader, ElementLocator locator) {
+        InvocationHandler handler = new LocatingElementListHandler(locator);
+        Object proxy = Proxy.newProxyInstance(loader, new Class[] {List.class}, handler);
+        
+        return proxy;
+    }
 }
