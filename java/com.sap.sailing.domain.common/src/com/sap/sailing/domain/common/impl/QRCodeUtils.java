56c4b7ce0cc6ba922b599fd7f3a64785db3f6352
Lukas Niemeier <lukas.niemeier@sap.com>
2013-12-06 17:06:17 +0100

    # extract to local var

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/impl/QRCodeUtils.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/impl/QRCodeUtils.java
@@ -7,24 +7,24 @@ public class QRCodeUtils {
     private static final String fragmentKey = "identifier";
 
     public static String composeQRContent(String deviceIdentifier, String apkUrl) {
-        // poor man's uri fragment encoding: ' ' as '+'
+        // poor man's uri fragment encoding: ' ' as '%20'
         String encodedIdentifier = deviceIdentifier.replaceAll(" ", "%20");
         return apkUrl + "#" + fragmentKey + "=" + encodedIdentifier;
     }
 
     public static Pair<String, String> splitQRContent(String qrCodeContent) {
-        int index = qrCodeContent.lastIndexOf('#');
-        if (index == -1 || index == 0 || qrCodeContent.length() == index + 1) {
+        int fragmentIndex = qrCodeContent.lastIndexOf('#');
+        if (fragmentIndex == -1 || fragmentIndex == 0 || qrCodeContent.length() == fragmentIndex + 1) {
             throw new IllegalArgumentException("There is no server or identifier.");
         }
-        String deviceIdentifier = qrCodeContent.substring(qrCodeContent.lastIndexOf('#') + 1, qrCodeContent.length());
+        String deviceIdentifier = qrCodeContent.substring(fragmentIndex + 1, qrCodeContent.length());
         if (!deviceIdentifier.startsWith(fragmentKey + "=")) {
             throw new IllegalArgumentException("The identifier is malformed");
         }
         deviceIdentifier = deviceIdentifier.substring((fragmentKey + "=").length());
         deviceIdentifier = deviceIdentifier.replaceAll("%20", " ");
 
-        String apkUrl = qrCodeContent.substring(0, qrCodeContent.lastIndexOf('#'));
+        String apkUrl = qrCodeContent.substring(0, fragmentIndex);
         return new Pair<String, String>(deviceIdentifier, apkUrl);
     }
 
