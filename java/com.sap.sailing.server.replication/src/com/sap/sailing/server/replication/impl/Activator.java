bc6acb238f5d216ef2ccb8563cf51b84a5368338
Simon Pamies <spamsch@gmail.com>
2013-12-11 15:13:15 +0100

    Added automatic replication startup based on configuration from env.sh

--- java/com.sap.sailing.server.replication/src/com/sap/sailing/server/replication/impl/Activator.java
+++ java/com.sap.sailing.server.replication/src/com/sap/sailing/server/replication/impl/Activator.java
@@ -1,5 +1,7 @@
 package com.sap.sailing.server.replication.impl;
 
+import java.io.IOException;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.osgi.framework.BundleActivator;
@@ -15,6 +17,12 @@ public class Activator implements BundleActivator {
     
     public static final String REPLICATION_CHANNEL = "REPLICATION_CHANNEL";
     public static final String REPLICATION_HOST = "REPLICATION_HOST";
+    
+    public static final String REPLICATE_ON_START = "REPLICATE_ON_START";
+    public static final String REPLICATE_MASTER_SERVLET_HOST = "REPLICATE_MASTER_SERVLET_HOST";
+    public static final String REPLICATE_MASTER_SERVLET_PORT = "REPLICATE_MASTER_SERVLET_PORT";
+    public static final String REPLICATE_MASTER_QUEUE_HOST = "REPLICATE_MASTER_QUEUE_HOST";
+    public static final String REPLICATE_MASTER_QUEUE_PORT = "REPLICATE_MASTER_QUEUE_PORT";
 
     private ReplicationInstancesManager replicationInstancesManager;
     
@@ -42,6 +50,26 @@ public class Activator implements BundleActivator {
         ReplicationService serverReplicationMasterService = new ReplicationServiceImpl(exchangeName, exchangeHost, replicationInstancesManager);
         bundleContext.registerService(ReplicationService.class, serverReplicationMasterService, null);
         logger.info("Registered replication service "+serverReplicationMasterService+" using exchange name "+exchangeName+" on host "+exchangeHost);
+        checkIfAutomaticReplicationShouldStart(serverReplicationMasterService);
+    }
+    
+    private void checkIfAutomaticReplicationShouldStart(ReplicationService serverReplicationMasterService) {
+        if (System.getenv(REPLICATE_ON_START).equals("True")) {
+            logger.info("Configuration requested automatic replication. Starting up...");
+            ReplicationMasterDescriptorImpl master = new ReplicationMasterDescriptorImpl(
+                    System.getenv(REPLICATE_MASTER_QUEUE_HOST),
+                    System.getenv(REPLICATE_MASTER_SERVLET_HOST), 
+                    System.getenv(REPLICATION_CHANNEL), 
+                    Integer.valueOf(System.getenv(REPLICATE_MASTER_SERVLET_PORT).trim()), 
+                    Integer.valueOf(System.getenv(REPLICATE_MASTER_QUEUE_PORT).trim()), 
+                    serverReplicationMasterService.getServerIdentifier().toString());
+            try {
+                serverReplicationMasterService.startToReplicateFrom(master);
+                logger.info("Automatic replication has been started.");
+            } catch (ClassNotFoundException | IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
