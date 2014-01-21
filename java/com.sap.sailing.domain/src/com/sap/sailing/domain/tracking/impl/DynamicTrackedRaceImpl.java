a5083691a209217374b26b051d9ab601d54e6ffe
Axel Uhl <axel.uhl@sap.com>
2014-01-21 21:25:00 +0100

    fixing the fix of the previous commit which still caused and endless recursion

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/DynamicTrackedRaceImpl.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/DynamicTrackedRaceImpl.java
@@ -623,8 +623,8 @@ DynamicTrackedRace, GPSTrackListener<Competitor, GPSFixMoving> {
      * wind fixes that may have been loaded from the wind store for the new track.
      */
     @Override
-    public WindTrack getOrCreateWindTrack(WindSource windSource) {
-        WindTrack result = super.getOrCreateWindTrack(windSource);
+    protected WindTrack createWindTrack(WindSource windSource, long delayForWindEstimationCacheInvalidation) {
+        WindTrack result = super.createWindTrack(windSource, delayForWindEstimationCacheInvalidation);
         if (windSource.getType().canBeStored()) {
             // replicate all wind fixes that may have been loaded by the wind store
             result.lockForRead();
