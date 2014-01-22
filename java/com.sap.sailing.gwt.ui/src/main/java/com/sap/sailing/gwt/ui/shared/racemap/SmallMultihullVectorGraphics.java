d2da68e697d95e5ffbedde635ee9419b1f8b68af
fmittag <fmittag@gmx.net>
2014-01-22 17:46:44 +0100

    completed drawing of sails for small multihull boats

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/racemap/SmallMultihullVectorGraphics.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/racemap/SmallMultihullVectorGraphics.java
@@ -6,7 +6,7 @@ import com.google.gwt.canvas.dom.client.Context2d.LineJoin;
 public class SmallMultihullVectorGraphics extends BoatClassVectorGraphics {
     
     public SmallMultihullVectorGraphics(String... compatibleBoatClassNames) {
-        super("Multihull", 5.25, 2.6, 2.62, compatibleBoatClassNames);
+        super("Multihull", 7.1, 2.6, 2.62, compatibleBoatClassNames);
     }
 
     @Override
@@ -98,25 +98,126 @@ public class SmallMultihullVectorGraphics extends BoatClassVectorGraphics {
         ctx.setLineWidth(3.0);
 
         ctx.beginPath();
+        ctx.moveTo(707,132);
+        ctx.lineTo(266,132);
+        ctx.fill();
+        ctx.stroke();
 
+        ctx.beginPath();
+        ctx.moveTo(475,242);
+        ctx.lineTo(475,27.7);
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(529,21.7);
+        ctx.lineTo(709,132);
+        ctx.lineTo(529,242);
+        ctx.lineTo(529,242);
+        ctx.fill();
+        ctx.stroke();
     }
 
     @Override
     protected void drawDownwindPortTackSails(Context2d ctx) {
+        ctx.beginPath();
+        ctx.moveTo(336,132);
+        ctx.lineTo(90.6,182);
+        ctx.bezierCurveTo(90.6,182,246,212,291,192);
+        ctx.bezierCurveTo(339,162,336,132,336,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
 
+        ctx.beginPath();
+        ctx.moveTo(473,132);
+        ctx.lineTo(356,202);
+        ctx.bezierCurveTo(356,202,448,182,464,162);
+        ctx.bezierCurveTo(482,142,473,132,473,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(706,132);
+        ctx.lineTo(321,355);
+        ctx.bezierCurveTo(321,355,517,355,581,302);
+        ctx.bezierCurveTo(656,242,706,132,706,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
     }
 
     @Override
     protected void drawDownwindStarboardTackSails(Context2d ctx) {
+        ctx.beginPath();
+        ctx.moveTo(706,132);
+        ctx.lineTo(320,-88.3);
+        ctx.bezierCurveTo(320,-88.3,521,-88.3,585,-38.3);
+        ctx.bezierCurveTo(656,21.7,706,132,706,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
 
+        ctx.beginPath();
+        ctx.moveTo(473,132);
+        ctx.lineTo(355,61.7);
+        ctx.bezierCurveTo(355,61.7,448,81.7,464,102);
+        ctx.bezierCurveTo(482,122,473,132,473,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(336,132);
+        ctx.lineTo(90.6,91.7);
+        ctx.bezierCurveTo(90.6,91.7,246,61.7,291,81.7);
+        ctx.bezierCurveTo(339,112,336,132,336,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
     }
 
     @Override
     protected void drawUpwindPortTackSails(Context2d ctx) {
+        ctx.beginPath();
+        ctx.moveTo(473,132);
+        ctx.lineTo(337,152);
+        ctx.bezierCurveTo(337,152,431,162,455,152);
+        ctx.bezierCurveTo(482,152,473,132,473,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(336,132);
+        ctx.lineTo(87.6,152);
+        ctx.bezierCurveTo(87.6,152,239,192,288,172);
+        ctx.bezierCurveTo(336,152,336,132,336,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
     }
 
     @Override
     protected void drawUpwindStarboardTackSails(Context2d ctx) {   
+        ctx.beginPath();
+        ctx.moveTo(473,132);
+        ctx.lineTo(337,112);
+        ctx.bezierCurveTo(337,112,431,91.7,455,112);
+        ctx.bezierCurveTo(482,112,473,132,473,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(337,132);
+        ctx.lineTo(87.6,112);
+        ctx.bezierCurveTo(87.6,112,239,71.7,288,91.7);
+        ctx.bezierCurveTo(337,112,337,132,337,132);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
     }
 
     @Override
@@ -141,3 +242,4 @@ public class SmallMultihullVectorGraphics extends BoatClassVectorGraphics {
     protected void drawKillingSails(Context2d ctx) {
     }
 }
+
