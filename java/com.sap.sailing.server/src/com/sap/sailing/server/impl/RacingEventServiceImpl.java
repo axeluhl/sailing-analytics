a1833d51e9f8b2227791db0fd5377207aa574de3
Axel Uhl <axel.uhl@sap.com>
2014-01-25 00:03:29 +0100

    re-formatted comment

--- java/com.sap.sailing.server/src/com/sap/sailing/server/impl/RacingEventServiceImpl.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/impl/RacingEventServiceImpl.java
@@ -866,13 +866,10 @@ public class RacingEventServiceImpl implements RacingEventService, RegattaListen
                     }
                     trackers.add(tracker);
                 }
-                // TODO we assume here that the event name is unique which necessitates adding the boat class name to it
-                // in EventImpl constructor
+                // TODO we assume here that the event name is unique which necessitates adding the boat class name to it in EventImpl constructor
                 String regattaName = tracker.getRegatta().getName();
                 Regatta regattaWithName = regattasByName.get(regattaName);
-                // TODO we assume here that the event name is unique which necessitates adding the boat class name to it
-                // in
-                // EventImpl constructor
+                // TODO we assume here that the event name is unique which necessitates adding the boat class name to it in EventImpl constructor
                 if (regattaWithName != null) {
                     if (regattaWithName != tracker.getRegatta()) {
                         if (Util.isEmpty(regattaWithName.getAllRaces())) {
