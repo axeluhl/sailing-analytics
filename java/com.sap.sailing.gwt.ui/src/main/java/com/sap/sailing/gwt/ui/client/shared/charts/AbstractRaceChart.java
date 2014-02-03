522ef83a828955130afab843162d76576579610e
Axel Uhl <axel.uhl@sap.com>
2014-02-04 00:13:32 +0100

    fixing bug 1742 by removing the turboThreshold limit for Highcharts series

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/AbstractRaceChart.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/AbstractRaceChart.java
@@ -28,6 +28,14 @@ import com.sap.sailing.gwt.ui.client.Timer.PlayModes;
 
 public abstract class AbstractRaceChart extends SimplePanel implements RaceSelectionChangeListener,
     TimeListener, TimeZoomChangeListener, TimeRangeChangeListener {
+    /**
+     * Used as the turboThreshold for the Highcharts series; this is
+     * basically the maximum number of points in a series to be displayed.
+     * Setting it to 0 means there will be no limit. Default is 1000.
+     * See also bug 1742.
+     */
+    protected static final int MAX_SERIES_POINTS = 0;
+
     protected Chart chart;
     protected PlotLine timePlotLine;
 
