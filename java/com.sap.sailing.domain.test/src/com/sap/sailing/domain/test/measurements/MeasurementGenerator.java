0e0cda4a85992b95443c3abed3a19cf86b128c49
Axel Uhl <axel.uhl@sap.com>
2014-02-07 23:29:44 +0100

    use generator for TEST-*.xml files to generate measurement data

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/measurements/MeasurementGenerator.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/measurements/MeasurementGenerator.java
@@ -13,6 +13,8 @@ import com.sap.sailing.domain.common.impl.Util;
 /**
  * A utility for generating output files that contain an artificial test result with a &lt;system-out&gt; tag that has
  * &lt;measurement&gt; tags embedded which list named values for use by the Hudson/Jenkins Measurements Plot plug-in.
+ * The output is written by the {@link #writeMeasurementsToFile()} method, but only if a <code>bin/</code> directory
+ * exists as subdirectory of the current working directory.
  * 
  * @author Axel Uhl (D043530)
  * 
@@ -40,21 +42,25 @@ public class MeasurementGenerator {
     }
     
     public void writeMeasurementsToFile() throws IOException {
-        Writer w = new BufferedWriter(new FileWriter(new File(reportFileName)));
-        w.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-        w.write("<testsuite  name=\""+getSuiteName()+" tests=\""+getNumberOfTests()+"\" >\n");
-        for (MeasurementCase measurementCase : getCases()) {
-            w.write("<testcase   name=\""+measurementCase.getName()+"\" status=\"run\" time=\"0\" classname=\""+measurementCase.getName()+"\">\n");
-            w.write("<system-out>\n");
-            for (Measurement measurement : measurementCase.getMeasurements()) {
-                w.write("&lt;measurement&gt;&lt;name&gt;"+measurement.getName()+"&lt;/name&gt;&lt;value&gt;"+measurement.getValue()+
-                        "&lt;/value&gt;&lt;/measurement&gt;");
+        File binDir = new File("./bin");
+        if (binDir.exists() && binDir.isDirectory()) {
+            Writer w = new BufferedWriter(new FileWriter(new File(binDir, reportFileName)));
+            w.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            w.write("<testsuite name=\"" + getSuiteName() + " tests=\"" + getNumberOfTests() + "\" >\n");
+            for (MeasurementCase measurementCase : getCases()) {
+                w.write("<testcase name=\"" + measurementCase.getName() + "\" status=\"run\" time=\"0\" classname=\""
+                        + measurementCase.getName() + "\">\n");
+                w.write("<system-out>\n");
+                for (Measurement measurement : measurementCase.getMeasurements()) {
+                    w.write("&lt;measurement&gt;&lt;name&gt;" + measurement.getName() + "&lt;/name&gt;&lt;value&gt;"
+                            + measurement.getValue() + "&lt;/value&gt;&lt;/measurement&gt;");
+                }
+                w.write("</system-out>\n");
+                w.write("</testcase>\n");
             }
-            w.write("</system-out>\n");
-            w.write("</testcase>\n");
+            w.write("</testsuite>\n");
+            w.close();
         }
-        w.write("</testsuite>\n");
-        w.close();
     }
 
     private int getNumberOfTests() {
