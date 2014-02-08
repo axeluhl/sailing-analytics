c6ede3975022bb21ad27b187a0e268bc04eaba2a
Axel Uhl <axel.uhl@sap.com>
2014-02-08 11:19:34 +0100

    upgraded com.tractrac.clientmodule to TracAPI 3.0.0 preview; client code hasn't been adjusted yet and doesn't compile

---
+++ java/com.tractrac.clientmodule/src/com/tractrac/subscription/app/eventtest/Main.java
@@ -0,0 +1,86 @@
+package com.tractrac.subscription.app.eventtest;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.tractrac.model.lib.api.ModelLocator;
+import com.tractrac.model.lib.api.event.IEventFactory;
+import com.tractrac.model.lib.api.event.IRace;
+import com.tractrac.subscription.lib.api.IEventSubscriber;
+import com.tractrac.subscription.lib.api.IRaceSubscriber;
+import com.tractrac.subscription.lib.api.ISubscriberFactory;
+import com.tractrac.subscription.lib.api.SubscriberInitializationException;
+import com.tractrac.subscription.lib.api.SubscriptionLocator;
+
+/**
+ * 
+ * @author <a href="mailto:jorge@tractrac.dk">Jorge Piera Llodr&aacute</a>
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 *          the command line arguments
+	 */
+	public static void main(String[] args) throws URISyntaxException,
+			MalformedURLException, FileNotFoundException, IOException,
+			SubscriberInitializationException {
+
+		URI paramURI = parseArguments(args);
+
+		// Create the event object
+		IEventFactory eventFactory = ModelLocator.getEventFactory();
+		IRace race = eventFactory.createRace(paramURI);
+
+		// Create the subscriber
+		ISubscriberFactory subscriberFactory = SubscriptionLocator.getSusbcriberFactory();
+		IEventSubscriber eventSubscriber = subscriberFactory.createEventSubscriber(race.getEvent());
+
+		EventListener eventListener = new EventListener();
+		eventSubscriber.subscribeConnectionStatus(eventListener);		
+		eventSubscriber.subscribeEventMessages(eventListener);
+		
+		IRaceSubscriber raceSubscriber = subscriberFactory.createRaceSubscriber(race);
+		raceSubscriber.subscribeControlPositions(eventListener);
+		raceSubscriber.subscribePositions(eventListener);
+		raceSubscriber.subscribePositionsSnapped(eventListener);
+		raceSubscriber.subscribeControlPassings(eventListener);
+		raceSubscriber.subscribeRaceMessages(eventListener);
+		raceSubscriber.subscribeRaceMessages(eventListener);
+		raceSubscriber.subscribeRaceTimesChanges(eventListener);
+		raceSubscriber.subscribeRouteChanges(eventListener);
+		raceSubscriber.start();
+		
+		eventSubscriber.start();		
+		raceSubscriber.start();
+		
+		// Go ahead with GUI or other stuff in main thread
+		System.out.println("Press key to cancel live data stream");
+		System.in.read();
+		System.out.println("Cancelling data stream");
+		// Stop data streams
+		eventSubscriber.stop();		
+		raceSubscriber.stop();	
+	}
+
+	private static URI parseArguments(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Usage: java -jar TestTTCM.jar parametersfile");
+			System.exit(0);
+		}
+		try {
+			return new URI(args[0]);
+		} catch (URISyntaxException ex) {
+			System.out.println("Malformed URL " + ex.getMessage());
+			System.exit(0);
+		}
+		return null;
+	}
+}
---
+++ java/com.tractrac.clientmodule/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm test.jar
+rm -fr bin/*
+mkdir bin
+javac -cp src:lib/* -d bin src/com/tractrac/subscription/app/eventtest/*.java
+cd bin
+jar -cvfm ../test.jar ../Manifest.txt com/tractrac/subscription/app/eventtest/*.class
+cd ..
+java -jar test.jar "http://germanmaster.traclive.dk/events/event_20120928_Lidingpet/clientparams.php?event=event_20120928_Lidingpet&race=beed03b4-0a3a-11e2-a8d1-10bf48d758ce&ci=&minimize="
+
