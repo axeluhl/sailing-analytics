a4f6d5d9f00071751f76fab81b071fe0c9dd8a6c
Frederik Petersen <frederik.petersen@sap.com>
2014-02-24 09:36:01 +0100

    Added java doc to DataImportLockWithProgress

--- java/com.sap.sailing.server/src/com/sap/sailing/server/masterdata/DataImportLockWithProgress.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/masterdata/DataImportLockWithProgress.java
@@ -9,7 +9,30 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import com.sap.sailing.domain.common.DataImportProgress;
+import com.sap.sailing.server.operationaltransformation.CreateOrUpdateDataImportProgress;
+import com.sap.sailing.server.operationaltransformation.DataImportFailed;
+import com.sap.sailing.server.operationaltransformation.ImportMasterDataOperation;
+import com.sap.sailing.server.operationaltransformation.SetDataImportDeleteProgressFromMapTimer;
 
+/**
+ * This lock is used to allow only one master data import operation be performed at once. It also keeps information
+ * about the progress of the operation. It's important to keep in mind that there the {@link ImportMasterDataOperation}
+ * is run on every replica and that any http request from the client to obtain the progress can be directed to any of
+ * the servers by the load balancer. Thus, the progress needs to be maintained on every server. The first part of the
+ * import (the data transfer) only happens on the server that the initial request went to. The progress is created and
+ * updated on the other servers using a {@link CreateOrUpdateDataImportProgress} operation. If an error occurs it will
+ * be logged to the progress object using the {@link DataImportFailed} operation.
+ * 
+ * The {@link #progressPerId} entries are deleted
+ * {@value #TIME_TO_DELETE_PROGRESS_ENTRY_AFTER_OPERATION_FINISHED_IN_MILLIS} ms, after the operation finished on the
+ * server reached with the initial import request. The {@link SetDataImportDeleteProgressFromMapTimer} operation is
+ * replicated so that the map entry is also deleted on the other servers.
+ * 
+ * 
+ * 
+ * @author Frederik Petersen (D054528)
+ * 
+ */
 public class DataImportLockWithProgress extends ReentrantLock {
     /**
      * Defaults to 60s (60000ms).
