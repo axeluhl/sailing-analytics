0aef8d3bc7c41bdb925472b84d9efa67d26cd06d
Lennart Hensler <LennartHensler@aol.com>
2014-02-26 15:16:13 +0100

    Simplified the sum aggregation processor hierarchy

--- java/com.sap.sse.datamining/src/com/sap/sse/datamining/impl/components/aggregators/ParallelGroupedDoubleDataSumAggregationProcessor.java
+++ java/com.sap.sse.datamining/src/com/sap/sse/datamining/impl/components/aggregators/ParallelGroupedDoubleDataSumAggregationProcessor.java
@@ -1,28 +1,63 @@
 package com.sap.sse.datamining.impl.components.aggregators;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.Executor;
 
 import com.sap.sse.datamining.components.Processor;
+import com.sap.sse.datamining.impl.components.GroupedDataEntry;
 import com.sap.sse.datamining.shared.GroupKey;
 
 public class ParallelGroupedDoubleDataSumAggregationProcessor
-             extends AbstractParallelGroupedDataSumAggregationProcessor<Double, Double> {
+             extends AbstractStoringParallelAggregationProcessor<GroupedDataEntry<Double>, Map<GroupKey, Double>> {
+
+    private Map<GroupedDataEntry<Double>, Integer> elementAmountMap;
 
     public ParallelGroupedDoubleDataSumAggregationProcessor(Executor executor,
             Collection<Processor<Map<GroupKey, Double>>> resultReceivers) {
         super(executor, resultReceivers);
+        elementAmountMap = new HashMap<>();
     }
 
     @Override
-    protected Double multiply(Double element, Integer times) {
+    protected void storeElement(GroupedDataEntry<Double> element) {
+        if (!elementAmountMap.containsKey(element)) {
+            elementAmountMap.put(element, 0);
+        }
+        Integer currentAmount = elementAmountMap.get(element);
+        elementAmountMap.put(element, currentAmount + 1);
+    }
+
+    @Override
+    protected Map<GroupKey, Double> aggregateResult() {
+        Map<GroupKey, Double> result = new HashMap<>();
+        for (Entry<GroupedDataEntry<Double>, Integer> elementAmountEntry : elementAmountMap.entrySet()) {
+            Double element = elementAmountEntry.getKey().getDataEntry();
+            Integer times = elementAmountEntry.getValue();
+            Double multipliedElementValue = multiply(element, times);
+            
+            GroupKey groupKey = elementAmountEntry.getKey().getKey();
+            Double groupResult = result.get(groupKey);
+            result.put(groupKey, addToGroupResult(groupResult, multipliedElementValue));
+        }
+        return result;
+    }
+
+    private Double multiply(Double element, Integer times) {
         return element * times;
     }
 
-    @Override
-    protected Double add(Double firstSummand, Double secondSummand) {
+    private Double add(Double firstSummand, Double secondSummand) {
         return firstSummand + secondSummand;
+    }
+
+    private Double addToGroupResult(Double groupResult, Double multipliedElementValue) {
+        if (groupResult == null) {
+            return multipliedElementValue;
+        }
+        return add(groupResult, multipliedElementValue);
     }
 
 }
