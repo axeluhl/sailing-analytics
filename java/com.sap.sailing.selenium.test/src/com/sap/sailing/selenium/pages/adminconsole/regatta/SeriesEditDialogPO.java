3b069f5c40792ca0087455983c79cdb4457abdcf
D049941 <riccardo.nimser@sap.com>
2014-03-27 17:07:38 +0100

    New methods added to rename or delete existing races

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/regatta/SeriesEditDialogPO.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/regatta/SeriesEditDialogPO.java
@@ -1,12 +1,15 @@
 package com.sap.sailing.selenium.pages.adminconsole.regatta;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import com.sap.sailing.selenium.core.BySeleniumId;
 import com.sap.sailing.selenium.core.FindBy;
-
 import com.sap.sailing.selenium.pages.common.DataEntryDialogPO;
 
 public class SeriesEditDialogPO extends DataEntryDialogPO {
@@ -87,5 +90,39 @@ public class SeriesEditDialogPO extends DataEntryDialogPO {
         setToNumberOfRacesToAdd(to);
         setRaceNamePrefix(prefix);
         pressAddRaces();
+    }
+    
+    public void renameRace(String race, String newName) {
+        for(WebElement editorRow : getRaceNameEditors()) {
+            WebElement valueTextBox = findElementBySeleniumId(editorRow, "ValueTextBox");
+            String raceName = valueTextBox.getAttribute("value");
+            
+            if(raceName.equals(race)) {
+                valueTextBox.clear();
+                valueTextBox.sendKeys(newName);
+            }
+        }
+    }
+    
+    public void deleteRace(String race) {
+        deleteRaces(Arrays.asList(race));
+    }
+    
+    public void deleteRaces(List<String> races) {
+        for(WebElement editorRow : getRaceNameEditors()) {
+            WebElement valueTextBox = findElementBySeleniumId(editorRow, "ValueTextBox");
+            
+            if(races.contains(valueTextBox.getAttribute("value"))) {
+                WebElement removeButton = findElementBySeleniumId(editorRow, "RemoveButton");
+                removeButton.click();
+            }
+        }
+    }
+    
+    private List<WebElement> getRaceNameEditors() {
+        WebElement raceNamesEditor = findElementBySeleniumId("RaceNamesStringListEditorComposite");
+        WebElement raceNamesGrid = findElementBySeleniumId(raceNamesEditor, "ExpandedValuesGrid");
+        
+        return raceNamesGrid.findElements(By.tagName("tr"));
     }
 }
