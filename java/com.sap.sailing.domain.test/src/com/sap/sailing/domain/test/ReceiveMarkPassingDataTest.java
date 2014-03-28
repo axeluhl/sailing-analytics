9304a0dd75a78e46dc56321766d09b2192410833
Axel Uhl <axel.uhl@sap.com>
2014-03-28 15:51:43 +0100

    made ReceiveMarkPassingDataTest compile

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/ReceiveMarkPassingDataTest.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/ReceiveMarkPassingDataTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -24,10 +23,15 @@ import com.sap.sailing.domain.tractracadapter.DomainFactory;
 import com.sap.sailing.domain.tractracadapter.Receiver;
 import com.sap.sailing.domain.tractracadapter.ReceiverType;
 import com.sap.sailing.domain.tractracadapter.impl.ControlPointAdapter;
+import com.tractrac.model.lib.api.data.IControlPassing;
+import com.tractrac.model.lib.api.data.IControlPassings;
+import com.tractrac.model.lib.api.event.IRace;
+import com.tractrac.model.lib.api.event.IRaceCompetitor;
+import com.tractrac.subscription.lib.api.control.IControlPassingsListener;
 
 public class ReceiveMarkPassingDataTest extends AbstractTracTracLiveTest {
     final private Object semaphor = new Object();
-    final private MarkPassingsData[] firstData = new MarkPassingsData[1];
+    final private IControlPassings[] firstData = new IControlPassings[1];
     private RaceDefinition raceDefinition;
     
     public ReceiveMarkPassingDataTest() throws URISyntaxException,
@@ -43,15 +47,8 @@ public class ReceiveMarkPassingDataTest extends AbstractTracTracLiveTest {
      */
     @Before
     public void setupListener() {
-        final Race race = getTracTracEvent().getRaceList().iterator().next();
+        final IRace race = getTracTracEvent().getRaces().iterator().next();
         Receiver receiver = new Receiver() {
-            @Override
-            public Iterable<TypeController> getTypeControllersAndStart() {
-                final TypeController[] markPassingsListener = new TypeController[1];
-                markPassingsListener[0] = MarkPassingsData.subscribe();
-                return Collections.singleton(markPassingsListener[0]);
-            }
-
             @Override
             public void stopPreemptively() {
             }
@@ -71,17 +68,41 @@ public class ReceiveMarkPassingDataTest extends AbstractTracTracLiveTest {
             @Override
             public void stopAfterNotReceivingEventsForSomeTime(long timeoutInMilliseconds) {
             }
+
+            @Override
+            public void subscribe() {
+                getRaceSubscriber().subscribeControlPassings(new IControlPassingsListener() {
+                    private boolean first = true;
+                    
+                    @Override
+                    public void gotControlPassings(IRaceCompetitor raceCompetitor, IControlPassings controlPassings) {
+                        if (first) {
+                            synchronized (semaphor) {
+                                firstData[0] = controlPassings;
+                                semaphor.notifyAll();
+                                getRaceSubscriber().unsubscribeControlPassings(this);
+                            }
+                            first = false;
+                        }
+                    }
+                });
+            }
         };
         List<Receiver> receivers = new ArrayList<Receiver>();
         receivers.add(receiver);
         for (Receiver r : DomainFactory.INSTANCE.getUpdateReceivers(
-                new DynamicTrackedRegattaImpl(DomainFactory.INSTANCE.getOrCreateDefaultRegatta(EmptyRaceLogStore.INSTANCE, getTracTracEvent(), /* trackedRegattaRegistry */ null)),
-                getTracTracEvent(), EmptyWindStore.INSTANCE, /* startOfTracking */ null, /* endOfTracking */ null, /* delayToLiveInMillis */ 0l,
-                /* simulator */ null, new DynamicRaceDefinitionSet() {
+                new DynamicTrackedRegattaImpl(DomainFactory.INSTANCE.getOrCreateDefaultRegatta(
+                        EmptyRaceLogStore.INSTANCE, getTracTracEvent(), /* trackedRegattaRegistry */null)),
+                getTracTracEvent().getRaces().iterator().next(), getTracTracEvent(), EmptyWindStore.INSTANCE,
+                /* delayToLiveInMillis */ 0l,
+                /* simulator */null, new DynamicRaceDefinitionSet() {
                     @Override
-                    public void addRaceDefinition(RaceDefinition race, DynamicTrackedRace trackedRace) {}
+                    public void addRaceDefinition(RaceDefinition race, DynamicTrackedRace trackedRace) {
+                    }
                 },
-                /* trackedRegattaRegistry */ null, /*courseDesignUpdateURI*/ null, /*tracTracUsername*/ null, /*tracTracPassword*/ null, eventSubscriber, raceSubscriber, ReceiverType.RACECOURSE, ReceiverType.MARKPOSITIONS, ReceiverType.RACESTARTFINISH, ReceiverType.RAWPOSITIONS)) {
+                /* trackedRegattaRegistry */null, /* courseDesignUpdateURI */null, /* tracTracUsername */null, /* tracTracPassword */
+                null, getEventSubscriber(), getRaceSubscriber(), ReceiverType.RACECOURSE, ReceiverType.MARKPOSITIONS,
+                ReceiverType.RACESTARTFINISH, ReceiverType.RAWPOSITIONS)) {
             receivers.add(r);
         }
         addListenersForStoredDataAndStartController(receivers);
@@ -112,14 +133,14 @@ public class ReceiveMarkPassingDataTest extends AbstractTracTracLiveTest {
         }
         assertNotNull(firstData[0]);
         assertTrue(firstData[0].getPassings().size() > 0);
-        MarkPassingsData.Entry entry = firstData[0].getPassings().iterator().next();
+        IControlPassing entry = firstData[0].getPassings().iterator().next();
         assertNotNull(entry);
         // we expect to find the mark passings in order, so as we traverse the course for
         // its waypoints and compare their control points to the control point received,
         // the first waypoint is used
         boolean found = false;
         for (Waypoint waypoint : raceDefinition.getCourse().getWaypoints()) {
-            if (waypoint.getControlPoint() == DomainFactory.INSTANCE.getOrCreateControlPoint(new ControlPointAdapter(entry.getControlPoint()))) {
+            if (waypoint.getControlPoint() == DomainFactory.INSTANCE.getOrCreateControlPoint(new ControlPointAdapter(entry.getControl()))) {
                 found = true;
             }
         }
