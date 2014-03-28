d0fd83ff81b74a6aadc3a39647ac828ea50bfdd2
fmittag <fmittag@gmx.net>
2014-03-28 17:33:19 +0100

    corrected hello world sample

--- java/com.sap.sailing.gwt.home/src/main/java/com/sap/sailing/gwt/home/client/HomeEntryPoint.java
+++ java/com.sap.sailing.gwt.home/src/main/java/com/sap/sailing/gwt/home/client/HomeEntryPoint.java
@@ -1,15 +1,14 @@
 package com.sap.sailing.gwt.home.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class HomeEntryPoint implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		HelloWorld helloWorld = new HelloWorld("able", "baker", "charlie");
+        HelloWorld helloWorld = new HelloWorld("able", "baker", "charlie");
 
-		// Don't forget, this is DOM only; will not work with GWT widgets
-		Document.get().getBody().appendChild(helloWorld.getElement());
+        RootPanel.get().add(helloWorld);
 	}
 }
