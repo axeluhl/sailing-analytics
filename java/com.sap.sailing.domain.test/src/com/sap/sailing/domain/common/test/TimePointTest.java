d63e1ad1aefb2900e77d352e5fd88b76419be0a5
Axel Uhl <axel.uhl@sap.com>
2014-04-27 15:05:13 +0200

    fixed TimePoint comparator so it also works for extreme values

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/common/test/TimePointTest.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/common/test/TimePointTest.java
@@ -8,11 +8,10 @@ import com.sap.sailing.domain.common.TimePoint;
 import com.sap.sailing.domain.common.impl.MillisecondsTimePoint;
 
 public class TimePointTest {
-	@Test
-	public void compare() {
-		TimePoint one = new MillisecondsTimePoint(Long.MIN_VALUE);
-		TimePoint two = new MillisecondsTimePoint(Long.MAX_VALUE);
-		
-		assertTrue(one.before(two));
-	}
+    @Test
+    public void compare() {
+        TimePoint one = new MillisecondsTimePoint(Long.MIN_VALUE);
+        TimePoint two = new MillisecondsTimePoint(Long.MAX_VALUE);
+        assertTrue(one.before(two));
+    }
 }
