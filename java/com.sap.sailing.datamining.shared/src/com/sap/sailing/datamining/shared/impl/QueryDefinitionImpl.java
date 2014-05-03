afd2cca59009504e29bc78693d006ab56593f635
Lennart Hensler <lennarthensler@aol.com>
2014-05-03 18:33:01 +0200

    Opened the concrete QueryDefinition

--- java/com.sap.sailing.datamining.shared/src/com/sap/sailing/datamining/shared/impl/QueryDefinitionImpl.java
+++ java/com.sap.sailing.datamining.shared/src/com/sap/sailing/datamining/shared/impl/QueryDefinitionImpl.java
@@ -1,8 +1,10 @@
 package com.sap.sailing.datamining.shared.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gwt.dev.util.collect.HashMap;
 import com.sap.sailing.datamining.shared.QueryDefinition;
 import com.sap.sse.datamining.shared.components.AggregatorType;
 import com.sap.sse.datamining.shared.dto.FunctionDTO;
@@ -12,14 +14,23 @@ public class QueryDefinitionImpl implements QueryDefinition {
     private final String localeInfoName;
     private final Map<FunctionDTO, Iterable<?>> filterSelection;
     private final List<FunctionDTO> dimensionsToGroupBy;
+    private final FunctionDTO extractionFunction;
     private final AggregatorType aggregatorType;
 
-    public QueryDefinitionImpl(String localeInfoName, Map<FunctionDTO, Iterable<?>> filterSelection,
-            List<FunctionDTO> dimensionsToGroupBy, AggregatorType aggregatorType) {
+    public QueryDefinitionImpl(String localeInfoName, FunctionDTO extractionFunction, AggregatorType aggregatorType) {
         this.localeInfoName = localeInfoName;
-        this.filterSelection = filterSelection;
-        this.dimensionsToGroupBy = dimensionsToGroupBy;
+        this.filterSelection = new HashMap<>();
+        this.dimensionsToGroupBy = new ArrayList<>();
+        this.extractionFunction = extractionFunction;
         this.aggregatorType = aggregatorType;
+    }
+    
+    public void setFilterSelectionFor(FunctionDTO dimension, Iterable<?> selection) {
+        filterSelection.put(dimension, selection);
+    }
+    
+    public void appenDimensionToGroupBy(FunctionDTO dimensionToGroupBy) {
+        dimensionsToGroupBy.add(dimensionToGroupBy);
     }
 
     @Override
@@ -39,7 +50,7 @@ public class QueryDefinitionImpl implements QueryDefinition {
 
     @Override
     public FunctionDTO getExtractionFunction() {
-        return getExtractionFunction();
+        return extractionFunction;
     }
 
     @Override
