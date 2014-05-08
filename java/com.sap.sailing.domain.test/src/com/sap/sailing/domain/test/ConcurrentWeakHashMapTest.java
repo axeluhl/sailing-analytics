e4fe19f16455ef056a82afe9c50285f18c1726ad
Axel Uhl <axel.uhl@sap.com>
2014-05-08 10:39:26 +0200

    first test for concurrent weak hash map that tests purging

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/ConcurrentWeakHashMapTest.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/ConcurrentWeakHashMapTest.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.domain.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -18,5 +19,48 @@ public class ConcurrentWeakHashMapTest {
         ConcurrentWeakHashMap<String, String> m = new ConcurrentWeakHashMap<>();
         m.put("a", "b");
         assertEquals("b", m.get("a"));
+    }
+
+    @Test
+    public void testPutAndIterate() {
+        ConcurrentWeakHashMap<String, String> m = new ConcurrentWeakHashMap<>();
+        m.put("a", "b");
+        assertEquals("b", m.values().iterator().next());
+        assertEquals("a", m.keySet().iterator().next());
+        assertEquals("a", m.entrySet().iterator().next().getKey());
+    }
+
+    @Test
+    public void testPutAndGetWithSubsequentGC() {
+        Object key = createKeyObject();
+        ConcurrentWeakHashMap<Object, String> m = new ConcurrentWeakHashMap<>();
+        m.put(key, "b");
+        assertEquals("b", m.get(key));
+        key = null;
+        forceEnqueuableWeakReferencesToBeEnqueued();
+        assertTrue(m.isEmpty());
+    }
+
+    private void forceEnqueuableWeakReferencesToBeEnqueued() {
+        System.gc();
+        try {
+            long[] hugeArrayThatWontGetAllocated = new long[Integer.MAX_VALUE];
+            assertTrue(hugeArrayThatWontGetAllocated.length > 0);
+        } catch (OutOfMemoryError e) {
+            // Ignore OME
+        }
+        try {
+            Thread.sleep(50); // give GC some time to finish enqueuing and finalizing all weakly-referenced objects
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Use a separate method for this, assuming that the compiler won't put the object on the using method's stack
+     * @return
+     */
+    private Object createKeyObject() {
+        return new Object();
     }
 }
