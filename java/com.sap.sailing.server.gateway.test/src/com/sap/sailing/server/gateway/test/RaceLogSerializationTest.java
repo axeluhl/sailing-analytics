9647a607e51537e7fcaa9397edc937e538bdf5d9
Fredrik Teschke <f@ftes.de>
2014-05-08 18:26:50 +0200

    fixed further test concerning device id serialization and logic of placeholder identifier

--- java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/RaceLogSerializationTest.java
+++ java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/RaceLogSerializationTest.java
@@ -41,6 +41,7 @@ import com.sap.sailing.server.gateway.serialization.impl.DeviceIdentifierJsonSer
 import com.sap.sailing.server.gateway.serialization.racelog.impl.RaceLogEventSerializer;
 import com.sap.sailing.server.gateway.serialization.racelog.tracking.DeviceIdentifierJsonHandler;
 import com.sap.sailing.server.gateway.serialization.racelog.tracking.impl.PlaceHolderDeviceIdentifierJsonHandler;
+import com.sap.sailing.server.gateway.trackfiles.TrackFileImportDeviceIdentifierImpl;
 
 public class RaceLogSerializationTest {
     private JsonSerializer<RaceLogEvent> serializer;
@@ -110,7 +111,8 @@ public class RaceLogSerializationTest {
         
         setup(DeviceIdentifierJsonDeserializer.create(new SmartphoneImeiJsonHandler(), SmartphoneImeiIdentifier.TYPE),
                 new DeviceIdentifierJsonSerializer(onlyFallback));
-        DeviceIdentifier device = new SmartphoneImeiIdentifier("abc");
+        //track file device id can't be restored from string rep
+        DeviceIdentifier device = new TrackFileImportDeviceIdentifierImpl("file", "track", null, 1);
         DeviceCompetitorMappingEvent event = RaceLogEventFactory.INSTANCE.createDeviceCompetitorMappingEvent(
                 t(), author, device, competitor, 0, t(), t());
         
