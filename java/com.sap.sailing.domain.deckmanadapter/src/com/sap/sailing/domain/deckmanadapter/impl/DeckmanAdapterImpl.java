0e34dacb6199cafb93adcc4a4cc7e41791bdfc7a
Axel Uhl <axel.uhl@sap.com>
2014-05-10 19:49:37 +0200

    prepared record types and added a few lines of test data

--- java/com.sap.sailing.domain.deckmanadapter/src/com/sap/sailing/domain/deckmanadapter/impl/DeckmanAdapterImpl.java
+++ java/com.sap.sailing.domain.deckmanadapter/src/com/sap/sailing/domain/deckmanadapter/impl/DeckmanAdapterImpl.java
@@ -3,5 +3,5 @@ package com.sap.sailing.domain.deckmanadapter.impl;
 import com.sap.sailing.domain.deckmanadapter.DeckmanAdapter;
 
 public class DeckmanAdapterImpl implements DeckmanAdapter {
-
+    
 }
---
+++ java/com.sap.sailing.domain.deckmanadapter/src/com/sap/sailing/domain/deckmanadapter/impl/Record.java
@@ -0,0 +1,65 @@
+package com.sap.sailing.domain.deckmanadapter.impl;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+import com.sap.sailing.domain.common.SpeedWithBearing;
+import com.sap.sailing.domain.common.TimePoint;
+import com.sap.sailing.domain.common.impl.DegreeBearingImpl;
+import com.sap.sailing.domain.common.impl.DegreePosition;
+import com.sap.sailing.domain.common.impl.KnotSpeedWithBearingImpl;
+import com.sap.sailing.domain.common.impl.MillisecondsTimePoint;
+import com.sap.sailing.domain.tracking.GPSFixMoving;
+import com.sap.sailing.domain.tracking.Wind;
+import com.sap.sailing.domain.tracking.impl.GPSFixMovingImpl;
+import com.sap.sailing.domain.tracking.impl.WindImpl;
+
+public class Record {
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+    
+    private final TimePoint timePoint;
+    
+    private final GPSFixMoving gpsFix;
+    
+    private final Wind wind;
+    
+    private Map<FieldType, String> fieldsAsString;
+    
+    public Record(Map<FieldType, String> fieldsAsString) throws ParseException {
+        this.fieldsAsString = fieldsAsString;
+        timePoint = new MillisecondsTimePoint(dateFormat.parse(this.fieldsAsString.get(FieldType.date)));
+        final DegreePosition position = new DegreePosition(Double.valueOf(this.fieldsAsString.get(FieldType.latitude)),
+                Double.valueOf(this.fieldsAsString.get(FieldType.longitude)));
+        final SpeedWithBearing speed = new KnotSpeedWithBearingImpl(Double.valueOf(this.fieldsAsString.get(FieldType.sog)),
+                new DegreeBearingImpl(Double.valueOf(this.fieldsAsString.get(FieldType.cog))));
+        wind = new WindImpl(position, timePoint, new KnotSpeedWithBearingImpl(Double.valueOf(this.fieldsAsString
+                .get(FieldType.tws)), new DegreeBearingImpl(Double.valueOf(this.fieldsAsString.get(FieldType.twd)))));
+        gpsFix = new GPSFixMovingImpl(position, timePoint, speed);
+    }
+    
+    public String getField(FieldType fieldType) {
+        return fieldsAsString.get(fieldType);
+    }
+
+    public Map<FieldType, String> getFieldsAsString() {
+        return fieldsAsString;
+    }
+
+    public void setFieldsAsString(Map<FieldType, String> fieldsAsString) {
+        this.fieldsAsString = fieldsAsString;
+    }
+
+    public TimePoint getTimePoint() {
+        return timePoint;
+    }
+
+    public GPSFixMoving getGpsFix() {
+        return gpsFix;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+}
