61285ab7b993f19f005f2ba933ce8d60a4d662bb
Axel Uhl <axel.uhl@sap.com>
2014-05-10 19:09:38 +0200

    started a Deckman adapter that shall be able to read Deckman-exported CSV files and turn them into a TrackedRace

---
+++ java/com.sap.sailing.domain.deckmanadapter/src/com/sap/sailing/domain/deckmanadapter/DeckmanAdapter.java
@@ -0,0 +1,5 @@
+package com.sap.sailing.domain.deckmanadapter;
+
+public interface DeckmanAdapter {
+
+}
---
+++ java/com.sap.sailing.domain.deckmanadapter/src/com/sap/sailing/domain/deckmanadapter/impl/DeckmanAdapterImpl.java
@@ -0,0 +1,7 @@
+package com.sap.sailing.domain.deckmanadapter.impl;
+
+import com.sap.sailing.domain.deckmanadapter.DeckmanAdapter;
+
+public class DeckmanAdapterImpl implements DeckmanAdapter {
+
+}
