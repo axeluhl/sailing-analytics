5fdfe4d280d2c4447cf16208fa64d54ad1a7361c
Benjamin Ebling <benjamin.ebling@sap.com>
2014-05-12 10:32:37 +0200

    Added Logger

--- java/com.sap.sse.security/src/com/sap/sse/security/LoginServlet.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/LoginServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.shiro.SecurityUtils;
+
 public class LoginServlet extends HttpServlet {
 
     /**
@@ -16,6 +18,7 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
         resp.getWriter().append("Hello!");
     }
 
--- java/com.sap.sse.security/src/com/sap/sse/security/SecurityServiceImpl.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/SecurityServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sap.sse.security;
 
+import java.util.logging.Logger;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
@@ -12,6 +14,7 @@ public class SecurityServiceImpl implements SecurityService {
     public SecurityServiceImpl() {
         Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
         SecurityManager securityManager = factory.getInstance();
+        Logger.getLogger(SecurityServiceImpl.class.getName()).info("Created: " + securityManager);
         SecurityUtils.setSecurityManager(securityManager);
         this.securityManager = securityManager;
     }
