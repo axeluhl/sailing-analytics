f6a41cbb7ee2ccc9076fad26b4847f3d951f0243
Axel Uhl <axel.uhl@sap.com>
2014-05-19 22:16:59 +0200

    renamed RemoveSailingServer to RemoveRemoteSailingServerReference

--- java/com.sap.sailing.server/src/com/sap/sailing/server/operationaltransformation/RemoveRemoteSailingServerReference.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/operationaltransformation/RemoveRemoteSailingServerReference.java
@@ -10,11 +10,11 @@ import com.sap.sailing.server.RacingEventServiceOperation;
  * @author Frank Mittag (c5163874)
  *
  */
-public class RemoveSailingServer extends AbstractRacingEventServiceOperation<Void> {
+public class RemoveRemoteSailingServerReference extends AbstractRacingEventServiceOperation<Void> {
 	private static final long serialVersionUID = -1283971355735610505L;
 	private final String serverName; 
 	
-    public RemoveSailingServer(String serverName) {
+    public RemoveRemoteSailingServerReference(String serverName) {
         super();
         this.serverName = serverName;
     }
