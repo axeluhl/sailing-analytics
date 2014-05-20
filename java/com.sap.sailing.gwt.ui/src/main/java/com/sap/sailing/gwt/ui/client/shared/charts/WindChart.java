3ee490216b6c966b554f56deeee06cae5ff86d43
Axel Uhl <axel.uhl@sap.com>
2014-05-20 23:48:15 +0200

    use setMin(0) and setMaxPadding(0.05) for the wind speed axis to avoid errors with single value charts

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/WindChart.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/WindChart.java
@@ -171,8 +171,8 @@ public class WindChart extends AbstractRaceChart implements Component<WindChartS
                         return new Long(value < 0 ? value + 360 : value).toString();
                     }
                 }));
-        chart.getYAxis(1).setOpposite(true).setAxisTitleText(stringMessages.speed()+" ("+stringMessages.knotsUnit()+")")
-            .setStartOnTick(false).setGridLineWidth(0).setMinorGridLineWidth(0);
+        chart.getYAxis(1).setOpposite(true).setAxisTitleText(stringMessages.speed()+" ("+stringMessages.knotsUnit()+")").setMin(0)
+            .setMaxPadding(0.05).setStartOnTick(false).setGridLineWidth(0).setMinorGridLineWidth(0);
         if (compactChart) {
             chart.setSpacingBottom(10).setSpacingLeft(10).setSpacingRight(10).setSpacingTop(2)
                  .setOption("legend/margin", 2)
