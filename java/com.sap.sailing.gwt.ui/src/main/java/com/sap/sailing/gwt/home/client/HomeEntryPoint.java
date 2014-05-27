d6f0af3d4049e065505f74551e0a7e502720504a
Axel Uhl <axel.uhl@sap.com>
2014-05-27 11:37:19 +0200

    found a convention for how to split media queries out into separate CSS files and use them through a single resource in the .ui.xml files

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/HomeEntryPoint.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/HomeEntryPoint.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.gwt.home.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.StyleInjector;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.sap.sailing.gwt.home.client.app.ApplicationActivityMapper;
 import com.sap.sailing.gwt.home.client.app.ApplicationClientFactory;
@@ -16,5 +17,8 @@ public class HomeEntryPoint extends AbstractEntryPoint {
         EntryPointHelper.registerASyncService((ServiceDefTarget) clientFactory.getSailingService(), RemoteServiceMappingConstants.sailingServiceRemotePath);
         ApplicationHistoryMapper applicationHistoryMapper = GWT.create(ApplicationHistoryMapper.class);
         onModuleLoad(clientFactory, applicationHistoryMapper, new ApplicationActivityMapper(clientFactory));
+        HomeResources.INSTANCE.css().ensureInjected();
+        StyleInjector.injectAtEnd("@media (min-width: 25em) { "+HomeResources.INSTANCE.smallCss().getText()+"}");
+        StyleInjector.injectAtEnd("@media (min-width: 50em) { "+HomeResources.INSTANCE.mediumCss().getText()+"}");
     }
 }
