f7c3963bc793b736e0e1d6def4d5728bca1bf7b8
Axel Uhl <axel.uhl@sap.com>
2014-05-30 17:24:17 +0200

    renamed stringConstants variables to stringMessages

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/EventDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/EventDialog.java
@@ -23,7 +23,7 @@ import com.sap.sailing.gwt.ui.shared.VenueDTO;
 import com.sap.sse.gwt.client.dialog.DataEntryDialog;
 
 public abstract class EventDialog extends DataEntryDialog<EventDTO> {
-    protected StringMessages stringConstants;
+    protected StringMessages stringMessages;
     protected TextBox nameEntryField;
     protected TextBox venueEntryField;
     protected DateBox startDateBox;
@@ -36,11 +36,11 @@ public abstract class EventDialog extends DataEntryDialog<EventDTO> {
 
     protected static class EventParameterValidator implements Validator<EventDTO> {
 
-        private StringMessages stringConstants;
+        private StringMessages stringMessages;
         private ArrayList<EventDTO> existingEvents;
 
-        public EventParameterValidator(StringMessages stringConstants, Collection<EventDTO> existingEvents) {
-            this.stringConstants = stringConstants;
+        public EventParameterValidator(StringMessages stringMessages, Collection<EventDTO> existingEvents) {
+            this.stringMessages = stringMessages;
             this.existingEvents = new ArrayList<EventDTO>(existingEvents);
         }
 
@@ -73,22 +73,22 @@ public abstract class EventDialog extends DataEntryDialog<EventDTO> {
             // remark: startDate == null and endDate == null is valid
             if(startDate != null && endDate != null) {
                 if(startDate.after(endDate)) {
-                    datesErrorMessage = stringConstants.pleaseEnterStartAndEndDate(); 
+                    datesErrorMessage = stringMessages.pleaseEnterStartAndEndDate(); 
                 }
             } else if((startDate != null && endDate == null) || (startDate == null && endDate != null)) {
-                datesErrorMessage = stringConstants.pleaseEnterStartAndEndDate();
+                datesErrorMessage = stringMessages.pleaseEnterStartAndEndDate();
             }
             
             if(datesErrorMessage != null) {
                 errorMessage = datesErrorMessage;
             } else if (!nameNotEmpty) {
-                errorMessage = stringConstants.pleaseEnterAName();
+                errorMessage = stringMessages.pleaseEnterAName();
             } else if (!venueNotEmpty) {
-                errorMessage = stringConstants.pleaseEnterNonEmptyVenue();
+                errorMessage = stringMessages.pleaseEnterNonEmptyVenue();
             } else if (!courseAreaNotEmpty) {
-                errorMessage = stringConstants.pleaseEnterNonEmptyCourseArea();
+                errorMessage = stringMessages.pleaseEnterNonEmptyCourseArea();
             } else if (!unique) {
-                errorMessage = stringConstants.eventWithThisNameAlreadyExists();
+                errorMessage = stringMessages.eventWithThisNameAlreadyExists();
             }
 
             return errorMessage;
@@ -100,7 +100,7 @@ public abstract class EventDialog extends DataEntryDialog<EventDTO> {
             DialogCallback<EventDTO> callback) {
         super(stringConstants.event(), null, stringConstants.ok(), stringConstants.cancel(), validator,
                 callback);
-        this.stringConstants = stringConstants;
+        this.stringMessages = stringConstants;
         courseAreaNameEntryFields = new ArrayList<TextBox>();
         courseAreasGrid = new Grid(0, 0);
     }
@@ -151,22 +151,22 @@ public abstract class EventDialog extends DataEntryDialog<EventDTO> {
         Grid formGrid = new Grid(5, 2);
         panel.add(formGrid);
 
-        formGrid.setWidget(0,  0, new Label(stringConstants.name() + ":"));
+        formGrid.setWidget(0,  0, new Label(stringMessages.name() + ":"));
         formGrid.setWidget(0, 1, nameEntryField);
-        formGrid.setWidget(1, 0, new Label(stringConstants.venue() + ":"));
+        formGrid.setWidget(1, 0, new Label(stringMessages.venue() + ":"));
         formGrid.setWidget(1, 1, venueEntryField);
-        formGrid.setWidget(2, 0, new Label(stringConstants.startDate() + ":"));
+        formGrid.setWidget(2, 0, new Label(stringMessages.startDate() + ":"));
         formGrid.setWidget(2, 1, startDateBox);
-        formGrid.setWidget(3, 0, new Label(stringConstants.endDate() + ":"));
+        formGrid.setWidget(3, 0, new Label(stringMessages.endDate() + ":"));
         formGrid.setWidget(3, 1, endDateBox);
-        formGrid.setWidget(4, 0, new Label(stringConstants.isPublic() + ":"));
+        formGrid.setWidget(4, 0, new Label(stringMessages.isPublic() + ":"));
         formGrid.setWidget(4, 1, isPublicCheckBox);
 
-        panel.add(createHeadlineLabel(stringConstants.courseAreas()));
+        panel.add(createHeadlineLabel(stringMessages.courseAreas()));
         panel.add(courseAreasGrid);
         updateCourseAreasGrid(panel);
 
-        Button addCourseAreaButton = new Button(stringConstants.addCourseArea());
+        Button addCourseAreaButton = new Button(stringMessages.addCourseArea());
         addCourseAreaButton.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
@@ -185,7 +185,7 @@ public abstract class EventDialog extends DataEntryDialog<EventDTO> {
         int courseAreasCount = courseAreaNameEntryFields.size();
         courseAreasGrid = new Grid(courseAreasCount + 1, 1);
         courseAreasGrid.setCellSpacing(4);
-        courseAreasGrid.setHTML(0, 0, stringConstants.name());
+        courseAreasGrid.setHTML(0, 0, stringMessages.name());
         for(int i = 0; i < courseAreasCount; i++) {
             courseAreasGrid.setWidget(i+1, 0, courseAreaNameEntryFields.get(i));
         }
