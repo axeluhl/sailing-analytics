1e85d415596b5db042d59e06eeaf82b0eb45d0bf
Lennart Hensler <lennarthensler@aol.com>
2014-06-06 10:59:15 +0200

    Implemented tests for the functions to be registrated in productive use

--- java/com.sap.sailing.datamining.test/src/com/sap/sailing/datamining/impl/functions/TestSailingFunctionsRegistration.java
+++ java/com.sap.sailing.datamining.test/src/com/sap/sailing/datamining/impl/functions/TestSailingFunctionsRegistration.java
@@ -1,25 +1,34 @@
 package com.sap.sailing.datamining.impl.functions;
 
-import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.sap.sailing.datamining.data.HasGPSFixContext;
+import com.sap.sailing.datamining.data.HasTrackedLegOfCompetitorContext;
+import com.sap.sailing.datamining.data.HasTrackedRaceContext;
 import com.sap.sailing.datamining.impl.data.SailingDataMiningClassesWithFunctionsService;
 import com.sap.sailing.datamining.test.util.ConcurrencyTestsUtil;
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.Moving;
+import com.sap.sailing.domain.base.Nationality;
+import com.sap.sailing.domain.base.Team;
+import com.sap.sailing.domain.common.Named;
+import com.sap.sailing.domain.common.Speed;
 import com.sap.sse.datamining.factories.FunctionFactory;
 import com.sap.sse.datamining.functions.Function;
-import com.sap.sse.datamining.functions.FunctionProvider;
 import com.sap.sse.datamining.functions.FunctionRegistry;
 
 public class TestSailingFunctionsRegistration {
     
     private static FunctionRegistry functionRegistry;
-    private static FunctionProvider functionProvider;
     
     @BeforeClass
     public static void setUpFunctionRegistryAndProvider() {
@@ -27,30 +36,68 @@ public class TestSailingFunctionsRegistration {
         
         functionRegistry = FunctionFactory.createFunctionRegistry(ConcurrencyTestsUtil.getExecutor());
         functionRegistry.registerAllWithInternalFunctionPolicy(classesWithFunctionsService.getInternalClassesWithMarkedMethods());
-        functionProvider = FunctionFactory.createRegistryFunctionProvider(functionRegistry);
-    }
-
-    @Test
-    public void testRegisteredDimensionsWithoutTheTransitiveOnes() {
-        Collection<Function<?>> expectedDimensions = createExpectedRegisteredDimensionsWithoutTheTransitiveOnes();
-        assertThat(functionRegistry.getDimensions(), is(expectedDimensions));
-    }
-
-    private Collection<Function<?>> createExpectedRegisteredDimensionsWithoutTheTransitiveOnes() {
-        // TODO Auto-generated method stub
-        return null;
     }
     
     @Test
-    public void testImportantRegisteredFunctions() {
-        // This tests for the methods marked as SideEffectFreeValue, that will be used for the data extraction
-        Function<?>[] expectedFunctions = createExpectedImportantRegisteredFunctions();
-        assertThat(functionRegistry.getStatistics(), hasItems(expectedFunctions));
+    public void testImportantRegisteredDimensions() throws NoSuchMethodException, SecurityException {
+        Collection<Function<?>> expectedDimensions = createExpectedImportantRegisteredDimensions();
+        Collection<Function<?>> providedDimensions = functionRegistry.getDimensions();
+        for (Function<?> dimension : expectedDimensions) {
+            assertThat("The expected dimension '" + dimension + "' wasn't provided.",
+                       providedDimensions.contains(dimension), is(true));
+        }
     }
 
-    private Function<?>[] createExpectedImportantRegisteredFunctions() {
-        // TODO Auto-generated method stub
-        return null;
+    private Collection<Function<?>> createExpectedImportantRegisteredDimensions() throws NoSuchMethodException, SecurityException {
+        Collection<Function<?>> expectedDimensions = new HashSet<>();
+        
+        Method getRegattaMethod = HasTrackedRaceContext.class.getMethod("getRegatta", new Class<?>[0]);
+        Function<?> getRegatta = FunctionFactory.createMethodWrappingFunction(getRegattaMethod);
+        Method getNameMethod = Named.class.getMethod("getName", new Class<?>[0]);
+        Function<?> getName = FunctionFactory.createMethodWrappingFunction(getNameMethod);
+        expectedDimensions.add(FunctionFactory.createCompoundFunction(Arrays.asList(getRegatta, getName)));
+        
+        Method getYearMethod = HasTrackedRaceContext.class.getMethod("getYear", new Class<?>[0]);
+        expectedDimensions.add(FunctionFactory.createMethodWrappingFunction(getYearMethod));
+        
+        Method getCompetitorMethod = HasTrackedLegOfCompetitorContext.class.getMethod("getCompetitor", new Class<?>[0]);
+        Function<?> getCompetitor = FunctionFactory.createMethodWrappingFunction(getCompetitorMethod);
+        Method getTeamMethod = Competitor.class.getMethod("getTeam", new Class<?>[0]);
+        Function<?> getTeam = FunctionFactory.createMethodWrappingFunction(getTeamMethod);
+        Method getNationalityMethod = Team.class.getMethod("getNationality", new Class<?>[0]);
+        Function<?> getNationality = FunctionFactory.createMethodWrappingFunction(getNationalityMethod);
+        Method getAcronymMethod = Nationality.class.getMethod("getThreeLetterIOCAcronym", new Class<?>[0]);
+        Function<?> getAcronym = FunctionFactory.createMethodWrappingFunction(getAcronymMethod);
+        expectedDimensions.add(FunctionFactory.createCompoundFunction(Arrays.asList(getCompetitor, getTeam, getNationality, getAcronym)));
+        
+        return expectedDimensions;
+    }
+    
+    @Test
+    public void testImportantRegisteredStatistics() throws NoSuchMethodException, SecurityException {
+        Collection<Function<?>> expectedStatistics = createExpectedImportantRegisteredStatistics();
+        Collection<Function<?>> providedStatistics = functionRegistry.getStatistics();
+        for (Function<?> statistic : expectedStatistics) {
+            assertThat("The expected statistic '" + statistic + "' wasn't provided.",
+                       providedStatistics.contains(statistic), is(true));
+        }
+    }
+
+    private Collection<Function<?>> createExpectedImportantRegisteredStatistics() throws NoSuchMethodException, SecurityException {
+        Collection<Function<?>> expectedStatistics = new HashSet<>();
+        
+        Method getDistanceTraveledMethod = HasTrackedLegOfCompetitorContext.class.getMethod("getDistanceTraveled", new Class<?>[0]);
+        expectedStatistics.add(FunctionFactory.createMethodWrappingFunction(getDistanceTraveledMethod));
+        
+        Method getGPSFixMethod = HasGPSFixContext.class.getMethod("getGPSFix", new Class<?>[0]);
+        Function<?> getGPSFix = FunctionFactory.createMethodWrappingFunction(getGPSFixMethod);
+        Method getSpeedMethod = Moving.class.getMethod("getSpeed", new Class<?>[0]);
+        Function<?> getSpeed = FunctionFactory.createMethodWrappingFunction(getSpeedMethod);
+        Method getKnotsMethod = Speed.class.getMethod("getKnots", new Class<?>[0]);
+        Function<?> getKnots = FunctionFactory.createMethodWrappingFunction(getKnotsMethod);
+        expectedStatistics.add(FunctionFactory.createCompoundFunction(Arrays.asList(getGPSFix, getSpeed, getKnots)));
+        
+        return expectedStatistics;
     }
 
 }
