059c26aa9c3576f2735b722bd91f8eb5c77dfbbd
Axel Uhl <axel.uhl@sap.com>
2014-06-11 21:49:41 +0200

    fixed formatting

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/streamlets/Neighbors.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/streamlets/Neighbors.java
@@ -1,20 +1,19 @@
 package com.sap.sailing.gwt.ui.simulator.streamlets;
 
 public class Neighbors {
+    public int xBot;
+    public int xTop;
+    public double xMod;
+    public int yBot;
+    public int yTop;
+    public double yMod;
 
-	public int xBot;
-	public int xTop;
-	public double xMod;
-	public int yBot;
-	public int yTop;
-	public double yMod;
-
-	public Neighbors(int xTop, int yTop, int xBot, int yBot, double xMod, double yMod) {
-		this.xTop = xTop;
-		this.yTop = yTop;
-		this.xBot = xBot;
-		this.yBot = yBot;
-		this.xMod = xMod;
-		this.yMod = yMod;
-	}
+    public Neighbors(int xTop, int yTop, int xBot, int yBot, double xMod, double yMod) {
+        this.xTop = xTop;
+        this.yTop = yTop;
+        this.xBot = xBot;
+        this.yBot = yBot;
+        this.xMod = xMod;
+        this.yMod = yMod;
+    }
 }
