e1ca6db32228f4665b298b9fda41999a943a28db
Axel Uhl <axel.uhl@sap.com>
2014-06-11 21:56:58 +0200

    removed unused JSNI stuff

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/WindStreamletsCanvasOverlay.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/WindStreamletsCanvasOverlay.java
@@ -1,15 +1,7 @@
 package com.sap.sailing.gwt.ui.simulator;
 
 import java.util.Date;
-import java.util.logging.Logger;
 
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.json.client.JSONArray;
-import com.google.gwt.json.client.JSONNumber;
-import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.json.client.JSONString;
-import com.google.gwt.maps.client.base.LatLng;
-import com.sap.sailing.gwt.ui.shared.SimulatorWindDTO;
 import com.sap.sailing.gwt.ui.shared.WindFieldDTO;
 import com.sap.sailing.gwt.ui.shared.WindFieldGenParamsDTO;
 import com.sap.sailing.gwt.ui.simulator.racemap.FullCanvasOverlay;
@@ -36,8 +28,6 @@ public class WindStreamletsCanvasOverlay extends FullCanvasOverlay implements Ti
     private int nParticles;
     private Swarm swarm;
 
-    private static Logger logger = Logger.getLogger(WindStreamletsCanvasOverlay.class.getName());
-
     public WindStreamletsCanvasOverlay(SimulatorMap simulatorMap, int zIndex, final Timer timer,
             final WindFieldGenParamsDTO windParams) {
         super(simulatorMap.getMap(), zIndex);
@@ -57,92 +47,9 @@ public class WindStreamletsCanvasOverlay extends FullCanvasOverlay implements Ti
         return this.windParams;
     }
 
-    public JavaScriptObject getJSONRandomWindData() {
-
-        JSONObject jsonWindData = new JSONObject();
-
-        jsonWindData.put("timestamp", new JSONString("11:55 am on March 18, 2014"));
-
-        LatLng boundsSW = LatLng.newInstance(53.854617, 8.159124);
-        LatLng boundsNE = LatLng.newInstance(55.263922, 11.413824);
-
-        jsonWindData.put("x0", new JSONNumber(boundsSW.getLongitude()));
-        jsonWindData.put("y0", new JSONNumber(boundsSW.getLatitude()));
-        jsonWindData.put("x1", new JSONNumber(boundsNE.getLongitude()));
-        jsonWindData.put("y1", new JSONNumber(boundsNE.getLatitude()));
-
-        int maxSteps = 1;
-        int gridWidth = 100;
-        int gridHeight = 100;
-        jsonWindData.put("gridWidth", new JSONNumber(gridWidth));
-        jsonWindData.put("gridHeight", new JSONNumber(gridHeight));
-
-        JSONArray windField = new JSONArray();
-
-        for (int stp = 0; stp < maxSteps; stp++) {
-            JSONArray vectorField = new JSONArray();
-            for (int idx = 0; idx < (gridWidth * gridHeight * 2); idx++) {
-                vectorField.set(idx, new JSONNumber((Math.random() - 0.5) * 10.0 + 3.0));
-                // windField.set(idx, new JSONNumber(10.0));
-            }
-            windField.set(stp, vectorField);
-        }
-        jsonWindData.put("field", windField);
-
-        return jsonWindData.getJavaScriptObject();
-    }
-
-    public native boolean isSwarmDataExt() /*-{
-		if ($wnd.swarmDataExt) {
-			return true;
-		} else {
-			return false;
-		}
-    }-*/;
-
-    public native String getSwarmData() /*-{
-		return JSON.stringify($wnd.swarmData);
-    }-*/;
-
-    public native void setSwarmData(JavaScriptObject swarmData) /*-{
-        $wnd.swarmData = swarmData;
-    }-*/;
-
-    private native void getJSNIWind(WindStreamletsCanvasOverlay wsc) /*-{
-        $wnd.getWindfromSimulator = function(idx) {
-            return wsc.@com.sap.sailing.gwt.ui.simulator.WindStreamletsCanvasOverlay::getWind(I)(idx);
-        };
-    }-*/;
-
-    private native JavaScriptObject getWindInst(double x, double y) /*-{
-        return {
-            x : x,
-            y : y
-        };
-    }-*/;
-
-    public JavaScriptObject getWind(int idx) {
-        SimulatorWindDTO wind = this.windFieldDTO.getMatrix().get(idx);
-        double y = wind.trueWindSpeedInKnots * Math.cos(wind.trueWindBearingDeg * Math.PI / 180.0);
-        double x = wind.trueWindSpeedInKnots * Math.sin(wind.trueWindBearingDeg * Math.PI / 180.0);
-        return getWindInst(x, y);
-    }
-
-    public native void setWindDataJSON(String jsonField) /*-{
-        eval(jsonField);
-    }-*/;
-
     public WindStreamletsCanvasOverlay(SimulatorMap simulatorMap, int zIndex) {
         this(simulatorMap, zIndex, null, null);
     }
-
-    public native void setMapInstance(Object mapInstance) /*-{
-        $wnd.swarmMap = mapInstance;
-    }-*/;
-
-    public native void setCanvasProjectionInstance(Object instance) /*-{
-        $wnd.swarmCanvasProjection = instance;
-    }-*/;
 
     public void startStreamlets() {
         if (swarm == null) {
@@ -225,10 +132,6 @@ public class WindStreamletsCanvasOverlay extends FullCanvasOverlay implements Ti
 
     public Swarm getSwarm() {
         return this.swarm;
-    }
-
-    private void clear() {
-        this.stopStreamlets();
     }
 
     @Override
