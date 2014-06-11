6758adf44e73096c56a231e9239b10b7bf0dc5cb
Axel Uhl <axel.uhl@sap.com>
2014-06-11 18:38:16 +0200

    formatted Particle.java

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/streamlets/Particle.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/streamlets/Particle.java
@@ -2,13 +2,10 @@ package com.sap.sailing.gwt.ui.simulator.streamlets;
 
 import com.sap.sailing.domain.common.Position;
 
-
 public class Particle {
-
-	public Position pos;
-	public Vector pxOld;
-	public double speed;
-	public int age;
-	public Vector v;
-
+    public Position pos;
+    public Vector pxOld;
+    public double speed;
+    public int age;
+    public Vector v;
 }
