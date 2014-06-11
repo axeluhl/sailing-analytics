896f704345fa781a73c0ebe453184b17d55f5607
Axel Uhl <axel.uhl@sap.com>
2014-06-11 21:46:24 +0200

    formatted java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/streamlets/Index.java

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/streamlets/Index.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/simulator/streamlets/Index.java
@@ -1,12 +1,11 @@
 package com.sap.sailing.gwt.ui.simulator.streamlets;
 
 public class Index {
+    public long x;
+    public long y;
 
-	public long x;
-	public long y;
-
-	public Index(long x, long y) {
-		this.x = x;
-		this.y = y;
-	}
+    public Index(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
 }
