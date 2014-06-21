fa0ff723d33188c84c621490dc82be63c465115e
Axel Uhl <axel.uhl@sap.com>
2014-06-21 23:53:47 +0200

    enable CrossTrackErrorCache to clear the entire cache by using null as update interval which was so far not completely implemented

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/CrossTrackErrorCache.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/CrossTrackErrorCache.java
@@ -85,6 +85,15 @@ public class CrossTrackErrorCache extends AbstractRaceChangeListener {
 
         private static final long serialVersionUID = 4884868659665863604L;
         
+        public void deleteAll() {
+            lockForWrite();
+            try {
+                getInternalRawFixes().clear();
+            } finally {
+                unlockAfterWrite();
+            }
+        }
+        
         public void deleteAllLaterThan(TimePoint from) {
             // TODO use a specialized ArrayList in a specialized ArrayListNavigableSet and then make removeRange public
             lockForWrite();
@@ -171,7 +180,11 @@ public class CrossTrackErrorCache extends AbstractRaceChangeListener {
                             FromTimePointToEndUpdateInterval updateInterval) {
                         CrossTrackErrorSumAndNumberOfFixesTrack result;
                         if (oldValue != null) {
-                            oldValue.deleteAllLaterThan(updateInterval.getFrom());
+                            if (updateInterval == null) {
+                                oldValue.deleteAll();
+                            } else {
+                                oldValue.deleteAllLaterThan(updateInterval.getFrom());
+                            }
                             computedCacheUpdate.lockForRead();
                             try {
                                 for (CrossTrackErrorSumAndNumberOfFixes entry : computedCacheUpdate.getRawFixes()) {
@@ -446,7 +459,7 @@ public class CrossTrackErrorCache extends AbstractRaceChangeListener {
         final List<Competitor> shuffledCompetitors = new ArrayList<>(cachePerCompetitor.keySet());
         Collections.shuffle(shuffledCompetitors);
         for (Competitor competitor : shuffledCompetitors) {
-            invalidate(competitor, /* from the beginning of time */ null);
+            cachePerCompetitor.triggerUpdate(competitor, null /* meaning: from the beginning of time */);
         }
     }
 
