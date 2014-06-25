d156cfa7e9f4ae1161c1bebea783716f1e1225aa
Axel Uhl <axel.uhl@sap.com>
2014-06-25 23:22:10 +0200

    and here the aftermath to c2036cae1de457b65afba3a326309502b5e985c1 which was still missing an initial check for master's end of tracking time to be set

--- java/com.sap.sailing.server.replication.test/src/com/sap/sailing/server/replication/test/TrackRaceReplicationTest.java
+++ java/com.sap.sailing.server.replication.test/src/com/sap/sailing/server/replication/test/TrackRaceReplicationTest.java
@@ -152,10 +152,11 @@ public class TrackRaceReplicationTest extends AbstractServerReplicationTest {
     public void testRaceTimeReplication() throws InterruptedException, Exception {
         startTracking();
         // now wait at least until the start of tracking time has been received on the master copy
-        boolean receivedStartOfTracking = master.getTrackedRace(raceIdentifier).getStartOfTracking() != null;
-        while (!receivedStartOfTracking) { // relying on the Timeout rule for this test
+        boolean receivedStartAndEndOfTracking = master.getTrackedRace(raceIdentifier).getStartOfTracking() != null &&
+                master.getTrackedRace(raceIdentifier).getEndOfTracking() != null;
+        while (!receivedStartAndEndOfTracking) { // relying on the Timeout rule for this test
             Thread.sleep(10);
-            receivedStartOfTracking = master.getTrackedRace(raceIdentifier).getStartOfTracking() != null &&
+            receivedStartAndEndOfTracking = master.getTrackedRace(raceIdentifier).getStartOfTracking() != null &&
                     master.getTrackedRace(raceIdentifier).getEndOfTracking() != null;
         }
         Thread.sleep(2000); // kept failing several times for a 1000ms timeout
