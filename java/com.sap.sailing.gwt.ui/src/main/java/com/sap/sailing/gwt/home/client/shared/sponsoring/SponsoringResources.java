059276338d93e626af4c44e68ddaa64e220ef968
fmittag <fmittag@gmx.net>
2014-06-28 18:02:36 +0200

    yeah, made media queries and imported css styles work together in sponsoring example...

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/sponsoring/SponsoringResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/sponsoring/SponsoringResources.java
@@ -5,7 +5,7 @@ import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.CssResource.Import;
 import com.google.gwt.resources.client.CssResource.ImportedWithPrefix;
-import com.sap.sailing.gwt.home.client.shared.solutions.SolutionsResources.ImportedMainCss;
+import com.google.gwt.resources.client.CssResource.Shared;
 import com.sap.sailing.gwt.home.client.shared.sponsoring.GlobalResources.GlobalCss;
 
 public interface SponsoringResources extends ClientBundle {
@@ -23,18 +23,18 @@ public interface SponsoringResources extends ClientBundle {
     public interface MinWidth25emCss extends LocalCss {
     }
 
-    @Import(value = {ImportedMainCss.class})
+    @Import(value = {ImportedGlobalCss.class})
     @Source("com/sap/sailing/gwt/home/client/shared/sponsoring/SponsoringMedium.css")
     MinWidth25emCss mediumCss();
 
     public interface MinWidth50emCss extends LocalCss {
     }
 
-    @Import(value = {ImportedMainCss.class})
+    @Import(value = {ImportedGlobalCss.class})
     @Source("com/sap/sailing/gwt/home/client/shared/sponsoring/SponsoringLarge.css")
     MinWidth50emCss largeCss();
 
-    
+    @Shared
     public interface LocalCss extends CssResource {
         String localA();
     }
