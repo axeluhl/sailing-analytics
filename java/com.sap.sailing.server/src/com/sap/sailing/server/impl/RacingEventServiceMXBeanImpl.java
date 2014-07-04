51a44143e754c4d13f31b9b4a85c637f6a974f6c
Axel Uhl <axel.uhl@sap.com>
2014-07-04 13:31:23 +0200

    added a first JMX MXBean for the RacingEventService, rather intended as an example for future extensions

---
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/impl/RacingEventServiceMXBeanImpl.java
@@ -0,0 +1,22 @@
+package com.sap.sailing.server.impl;
+
+import com.sap.sailing.server.RacingEventService;
+import com.sap.sailing.server.RacingEventServiceMXBean;
+
+public class RacingEventServiceMXBeanImpl implements RacingEventServiceMXBean {
+    private final RacingEventService racingEventService;
+
+    protected RacingEventServiceMXBeanImpl(RacingEventService racingEventService) {
+        super();
+        this.racingEventService = racingEventService;
+    }
+    
+    private RacingEventService getRacingEventService() {
+        return racingEventService;
+    }
+
+    @Override
+    public int getNumberOfLeaderboards() {
+        return getRacingEventService().getLeaderboards().size();
+    }
+}
