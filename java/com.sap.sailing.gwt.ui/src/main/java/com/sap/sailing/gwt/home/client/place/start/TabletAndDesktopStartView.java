29627264f870775ee00e0ca8c82aaed902ca30a1
fmittag <fmittag@gmx.net>
2014-07-04 10:38:04 +0200

    removed 'sponsors and solutions teaser' from home page for now

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/start/TabletAndDesktopStartView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/start/TabletAndDesktopStartView.java
@@ -10,7 +10,6 @@ import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.home.client.app.PlaceNavigator;
 import com.sap.sailing.gwt.home.client.shared.mainevents.MainEvents;
 import com.sap.sailing.gwt.home.client.shared.mainmedia.MainMedia;
-import com.sap.sailing.gwt.home.client.shared.mainsponsors.MainSponsors;
 import com.sap.sailing.gwt.home.client.shared.socialfooter.SocialFooter;
 import com.sap.sailing.gwt.home.client.shared.stage.Stage;
 import com.sap.sailing.gwt.ui.shared.EventDTO;
@@ -22,13 +21,13 @@ public class TabletAndDesktopStartView extends Composite implements StartView {
     }
 
     @UiField Stage stage;
-    @UiField(provided=true) MainSponsors mainSponsors;
+//    @UiField(provided=true) MainSponsors mainSponsors;
     @UiField(provided=true) MainEvents mainEvents;
     @UiField(provided=true) MainMedia mainMedia;
     @UiField SocialFooter socialFooter;
 
     public TabletAndDesktopStartView(PlaceNavigator navigator) {
-        mainSponsors = new MainSponsors(navigator);
+//        mainSponsors = new MainSponsors(navigator);
         mainEvents = new MainEvents(navigator);
         mainMedia = new MainMedia(navigator);
         
