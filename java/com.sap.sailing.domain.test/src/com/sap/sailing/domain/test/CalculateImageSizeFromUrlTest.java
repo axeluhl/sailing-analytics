0231455482c1358aadd96eabbd9d76150f0c29c7
Benjamin Ebling <benjamin.ebling@sap.com>
2014-07-08 13:55:16 +0200

    Created test case for image size calculation from url.

---
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/CalculateImageSizeFromUrlTest.java
@@ -0,0 +1,58 @@
+package com.sap.sailing.domain.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Dimension;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+
+import org.junit.Test;
+
+public class CalculateImageSizeFromUrlTest {
+
+    @Test
+    public void calculateImageSizeFromUrl(){
+        try {
+            int width = 350;
+            int height = 150;
+            Dimension size = calculate("http://placehold.it/"+width+"x"+height);
+            assertTrue(size.getWidth()==width);
+            assertTrue(size.getHeight()==height);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    public Dimension calculate(String urlS) throws IOException{
+        ImageInputStream in = null;
+        try {
+            URL url = new URL(urlS);
+            URLConnection conn = url.openConnection();
+            in = ImageIO.createImageInputStream(conn.getInputStream());
+            final Iterator<ImageReader> readers = ImageIO.getImageReaders(in);
+            if (readers.hasNext()) {
+                ImageReader reader = readers.next();
+                try {
+                    reader.setInput(in);
+                    return new Dimension(reader.getWidth(0), reader.getHeight(0));
+                } finally {
+                    reader.dispose();
+                }
+            }
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (in != null) in.close();
+        }
+        return null;
+    }
+}
