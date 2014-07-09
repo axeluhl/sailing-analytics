d55a5c5b53872842d342f5f585c852aa6161b33d
fmittag <fmittag@gmx.net>
2014-07-09 20:45:40 +0200

    reimplemented photo gallery and recent videos on homepage

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/mainmedia/MainMedia.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/mainmedia/MainMedia.java
@@ -1,6 +1,9 @@
 package com.sap.sailing.gwt.home.client.shared.mainmedia;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
@@ -13,11 +16,11 @@ import com.sap.sailing.gwt.home.client.app.PlaceNavigator;
 import com.sap.sailing.gwt.ui.shared.EventDTO;
 
 public class MainMedia extends Composite {
-    @UiField HTMLPanel videoThumbsPanel;
-    @UiField HTMLPanel videoPanel;
-    @UiField HTMLPanel photoThumbsPanel;
+    @UiField HTMLPanel videosPanel;
+    @UiField DivElement videoLightBoxData;
 
-    @UiField DivElement photoUrl;
+    @UiField DivElement photoUrlsData;
+    @UiField DivElement currentPhotoUrl;
     
     interface MainMediaUiBinder extends UiBinder<Widget, MainMedia> {
     }
@@ -32,20 +35,38 @@ public class MainMedia extends Composite {
     }
 
     public void setRecentEvents(List<EventDTO> recentEvents) {
+        List<String> photoGalleryUrls = new ArrayList<String>();
+        String lightBoxData = "";
+        
         int videoCounter = 0;
         for(EventDTO event: recentEvents) {
-            if(event.getVideoURLs().size() > 0) {
-                MainMediaVideoThumb videoThumb = new MainMediaVideoThumb();
-                if(videoCounter <= 3) {
-                    videoThumbsPanel.add(videoThumb);
-                    videoCounter++;
-                }
+            photoGalleryUrls.addAll(event.getPhotoGalleryImageURLs());
+            
+            if(event.getVideoURLs().size() > 0 && videoCounter < 3) {
+                MainMediaVideo video = new MainMediaVideo(event);
+                lightBoxData += video.getDataForLightBox();  
+                videosPanel.add(video);
+                videoCounter++;
             }
         }
+
+        videoLightBoxData.setInnerText(lightBoxData);
         
-        photoUrl.setAttribute("data-url", "#1");
-//        String imagePath = "url(" + imageX + ")";
-//      photoUrl.getStyle().setBackgroundImage(imagePath);
+        // shuffle the image url list (Remark: Collections.shuffle() is not implemented in GWT)
+        int gallerySize = photoGalleryUrls.size();
+        Random random = new Random(gallerySize);  
+        for(int i = 0; i < gallerySize; i++) {  
+            Collections.swap(photoGalleryUrls, i, random.nextInt(gallerySize));  
+        }
+
+        currentPhotoUrl.setAttribute("data-url", "#1");
+        String imagePath = "url(" + photoGalleryUrls.get(0) + ")";
+        currentPhotoUrl.getStyle().setBackgroundImage(imagePath);
         
+        String slideShowData = "";
+        for(int i = 0; i < gallerySize; i++) {
+            slideShowData += photoGalleryUrls.get(0) + ",#" + (i+1) + ";";
+        }
+        photoUrlsData.setInnerText(slideShowData);
     }
 }
