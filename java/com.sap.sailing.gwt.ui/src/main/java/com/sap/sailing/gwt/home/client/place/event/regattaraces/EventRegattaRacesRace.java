f32bbd6158f3f7d3ae23eb74573c3431b0965de4
fmittag <fmittag@gmx.net>
2014-07-16 19:17:20 +0200

    made the shitty css work for the races! Hell!

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/regattaraces/EventRegattaRacesRace.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/regattaraces/EventRegattaRacesRace.java
@@ -1,19 +1,18 @@
 package com.sap.sailing.gwt.home.client.place.event.regattaraces;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.AnchorElement;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.dom.client.Style.Visibility;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.i18n.shared.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.EventListener;
+import com.google.gwt.user.client.ui.UIObject;
 import com.sap.sailing.domain.common.dto.FleetDTO;
 import com.sap.sailing.domain.common.dto.RaceColumnDTO;
 import com.sap.sailing.domain.common.dto.RaceDTO;
@@ -22,26 +21,26 @@ import com.sap.sailing.gwt.home.client.place.event.EventPageNavigator;
 import com.sap.sailing.gwt.ui.shared.StrippedLeaderboardDTO;
 import com.sap.sse.gwt.client.player.Timer;
 
-public class EventRegattaRacesRace extends Composite {
+public class EventRegattaRacesRace extends UIObject {
     private static EventRegattaRacesRaceUiBinder uiBinder = GWT.create(EventRegattaRacesRaceUiBinder.class);
 
     private enum SimpleRaceStates { NOT_TRACKED, TRACKED_AND_NOT_LIVE, TRACKED_AND_LIVE };
     
-    interface EventRegattaRacesRaceUiBinder extends UiBinder<Widget, EventRegattaRacesRace> {
+    interface EventRegattaRacesRaceUiBinder extends UiBinder<DivElement, EventRegattaRacesRace> {
     }
 
     @UiField DivElement fleetColor;
     @UiField SpanElement raceName;
-    @UiField SpanElement raceName2;
-    @UiField SpanElement raceState;
-    @UiField SpanElement raceTime;
+//    @UiField SpanElement raceName2;
+//    @UiField SpanElement raceState;
+//    @UiField SpanElement raceTime;
     @UiField SpanElement averageRaceWind;
     
     @UiField DivElement raceNotTrackedDiv;
     @UiField DivElement watchRaceDiv;
     @UiField DivElement analyzeRaceDiv;
-    @UiField Anchor watchRaceLink;
-    @UiField Anchor analyzeRaceLink;
+    @UiField AnchorElement watchRaceLink;
+    @UiField AnchorElement analyzeRaceLink;
     
     @UiField DivElement raceWinnerDiv;
     @UiField SpanElement raceWinner;
@@ -60,9 +59,8 @@ public class EventRegattaRacesRace extends Composite {
     
     // local_res.css.eventregattarace_featureunavailable
     
-    private final DateTimeFormat raceTimeFormat = DateTimeFormat.getFormat("EEE, h:mm a");
-
-    private final TextMessages textMessages = GWT.create(TextMessages.class);
+//    private final DateTimeFormat raceTimeFormat = DateTimeFormat.getFormat("EEE, h:mm a");
+//    private final TextMessages textMessages = GWT.create(TextMessages.class);
     
     private final StrippedLeaderboardDTO leaderboard;
     private final FleetDTO fleet;
@@ -80,29 +78,40 @@ public class EventRegattaRacesRace extends Composite {
         this.timerForClientServerOffset = timerForClientServerOffset;
         
         EventRegattaRacesResources.INSTANCE.css().ensureInjected();
-        initWidget(uiBinder.createAndBindUi(this));
-        
+        setElement(uiBinder.createAndBindUi(this));
+
         allConditionalElements = new Element[] {raceWinnerDiv, raceLeaderDiv, watchRaceDiv, analyzeRaceDiv, raceNotTrackedDiv,
                 raceFeaturesDiv, legProgressDiv, raceFlagDiv, windStatusDiv };
 
-        fleetColor.getStyle().setBackgroundColor(fleet.getColor().getAsHtml());
+        if(fleet.getColor() != null) {
+            fleetColor.getStyle().setBackgroundColor(fleet.getColor().getAsHtml());
+        }
         
         raceName.setInnerText(raceColumn.getName());
-        raceName2.setInnerText(raceColumn.getName());
-        
-//        {#if race.isFinished}
-//        <ui:text from='{i18n.eventRegattaRaceFinished}'/><span ui:field="endOfRaceTime" />
-//        {/if race.isFinished}
-//        {#if race.isLive}
-//        <ui:text from='{i18n.eventRegattaRaceStarted}'/> {race.startTime}
-//        {/if race.isLive}
-//        {#if race.isScheduled}
-//        <ui:text from='{i18n.eventRegattaRaceScheduled}'/> {race.startTime}
-//        {/if race.isScheduled}
-        
+//        raceName2.setInnerText(raceColumn.getName());
+
+        registerEvents();
         updateUI();    
     }
 
+    private void registerEvents() {
+        Event.sinkEvents(analyzeRaceLink, Event.ONCLICK);
+        Event.setEventListener(analyzeRaceLink, new EventListener() {
+            @Override
+            public void onBrowserEvent(Event event) {
+                openRaceViewer();
+            }
+        });
+
+        Event.sinkEvents(watchRaceLink, Event.ONCLICK);
+        Event.setEventListener(watchRaceLink, new EventListener() {
+            @Override
+            public void onBrowserEvent(Event event) {
+                openRaceViewer();
+            }
+        });
+    }
+    
     private SimpleRaceStates getSimpleRaceState() {
         SimpleRaceStates simpleRaceState = SimpleRaceStates.NOT_TRACKED;
         
@@ -142,16 +151,6 @@ public class EventRegattaRacesRace extends Composite {
         }
     }
     
-    @UiHandler("watchRaceLink")
-    public void watchRaceClicked(ClickEvent e) {
-        openRaceViewer();
-    }
-
-    @UiHandler("analyzeRaceLink")
-    public void analyzeRaceClicked(ClickEvent e) {
-        openRaceViewer();
-    }
-
     private void hideElement(Element el) {
         el.getStyle().setVisibility(Visibility.HIDDEN);
         el.getStyle().setDisplay(Display.NONE);
