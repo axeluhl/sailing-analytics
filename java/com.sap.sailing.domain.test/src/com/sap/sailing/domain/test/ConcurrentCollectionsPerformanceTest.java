6b06e205059468bfe95e12eff44b11d120aa0512
Axel Uhl <axel.uhl@sap.com>
2014-08-13 11:13:14 +0200

    started to create a performance test for the readers collection in NamedReentrantReadWriteLock which is a performance bottleneck

---
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/ConcurrentCollectionsPerformanceTest.java
@@ -0,0 +1,17 @@
+package com.sap.sailing.domain.test;
+
+import org.junit.Test;
+
+/**
+ * Measures the performance of different concurrent collections types and shows their add and remove behavior for
+ * different collection sizes and different numbers of concurrently accessing threads.
+ * 
+ * @author Axel Uhl (D043530)
+ *
+ */
+public class ConcurrentCollectionsPerformanceTest {
+    @Test
+    public void testConcurrentLinkedQueue() {
+        
+    }
+}
