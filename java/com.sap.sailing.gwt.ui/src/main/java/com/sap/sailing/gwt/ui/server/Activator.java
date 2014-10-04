505f6c36f249c055222e1c6bfb706f805d78847b
Axel Uhl <axel.uhl@sap.com>
2014-10-04 23:22:28 +0200

    improved loading strategy of security service in gwt.ui bundle, waiting for service to appear in separate thread

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/Activator.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/Activator.java
@@ -1,13 +1,17 @@
 package com.sap.sailing.gwt.ui.server;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.apache.shiro.SecurityUtils;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
 
 import com.sap.sse.security.SecurityService;
 
 public class Activator implements BundleActivator {
+    private static Logger logger = Logger.getLogger(Activator.class.getName());
     private static BundleContext context;
     private SailingServiceImpl sailingServiceToStopWhenStopping;
     private static Activator INSTANCE;
@@ -19,9 +23,21 @@ public class Activator implements BundleActivator {
     @Override
     public void start(BundleContext context) throws Exception {
         Activator.context = context;
-        ServiceReference<?> serviceReference = context.getServiceReference(SecurityService.class.getName());
-        SecurityService securityService = (SecurityService) context.getService(serviceReference);
-        SecurityUtils.setSecurityManager(securityService.getSecurityManager());
+        final ServiceTracker<SecurityService, SecurityService> tracker = new ServiceTracker<>(context, SecurityService.class, /* customizer */ null);
+        new Thread("ServiceTracker in bundle com.sap.sailing.gwt.ui waiting for SecurityService") {
+            @Override
+            public void run() {
+                try {
+                    logger.info("Waiting for SecurityService...");
+                    SecurityService securityService = tracker.waitForService(0);
+                    logger.info("Obtained SecurityService "+securityService+
+                            ". Setting it as SecurityUtils\' security manager.");
+                    SecurityUtils.setSecurityManager(securityService.getSecurityManager());
+                } catch (InterruptedException e) {
+                    logger.log(Level.SEVERE, "Interrupted while waiting for UserStore service", e);
+                }
+            }
+        }.start();
     }
 
     @Override
