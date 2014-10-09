8ba24416245a3a39f375ce680a6ab35af5b2d528
Axel Uhl <axel.uhl@sap.com>
2014-10-09 13:12:52 +0200

    reduced SecurityWebSessionManager to a modified cookie that uses Cookie.ROOT_PATH as its path

--- java/com.sap.sse.security/src/com/sap/sse/security/SessionCacheManager.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/SessionCacheManager.java
@@ -4,15 +4,20 @@ import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheException;
 import org.apache.shiro.cache.CacheManager;
 
+/**
+ * Obtains the singleton cache manager instance from the singleton {@link SecurityService} instance.
+ * 
+ * @see SecurityService#getCacheManager()
+ * 
+ * @author Benjamin Ebling
+ *
+ */
 public class SessionCacheManager implements CacheManager {
-    
-    SecurityService securityService;
-
+    private final SecurityService securityService;
     
     public SessionCacheManager() {
         securityService = Activator.getSecurityService();
     }
-
 
     @Override
     public <K, V> Cache<K, V> getCache(String name) throws CacheException {
@@ -21,6 +26,4 @@ public class SessionCacheManager implements CacheManager {
         }
         return securityService.getCacheManager().getCache(name);
     }
-
-    
 }
