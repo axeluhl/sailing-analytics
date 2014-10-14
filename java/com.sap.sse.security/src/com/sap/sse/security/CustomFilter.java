435a71a72e4d70b9c6b0c0dd85a5901ca349201e
Axel Uhl <axel.uhl@sap.com>
2014-10-14 10:12:43 +0200

    changed redirect such that the login page is redirected to with absolute context, enabling the entry point's re-use from the security.ui bundle

--- java/com.sap.sse.security/src/com/sap/sse/security/CustomFilter.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/CustomFilter.java
@@ -1,7 +1,9 @@
 package com.sap.sse.security;
 
 import java.io.IOException;
-import java.util.Map.Entry;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -9,25 +11,26 @@ import javax.servlet.ServletResponse;
 import org.apache.shiro.web.filter.authc.PassThruAuthenticationFilter;
 import org.apache.shiro.web.util.WebUtils;
 
+/**
+ * Forwards a set of query parameters that are useful for the login entry point, in particular <code>gwt.codesvr</code> which
+ * is important for GWT hosted mode debugging, and <code>locale</code> for i18n support.
+ * 
+ * @author Axel Uhl (D043530)
+ *
+ */
 public class CustomFilter extends PassThruAuthenticationFilter {
+    private final static Iterable<String> queryParametersToForwardToLogin = Arrays.asList("gwt.codesvr", "locale");
+    
     @Override
     protected void redirectToLogin(ServletRequest request, ServletResponse response) throws IOException {
-        String loginUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + getLoginUrl();
-        boolean first = true;
-        for (Entry<String, String[]> e : request.getParameterMap().entrySet()) {
-            if (first) {
-                loginUrl += "?" + e.getKey() + "=";
-            } else {
-                loginUrl += "&" + e.getKey() + "=";
-            }
-            for (int i = 0; i < e.getValue().length; i++) {
-                loginUrl += e.getValue()[i];
-                if (i < e.getValue().length - 2) {
-                    loginUrl += ",";
-                }
-
+        String loginUrl = getLoginUrl();
+        Map<String, String> queryParams = new HashMap<>();
+        for (String queryParameterToForwardToLogin : queryParametersToForwardToLogin) {
+            String value = request.getParameterMap().get(queryParameterToForwardToLogin) == null ? null : request.getParameterMap().get(queryParameterToForwardToLogin)[0];
+            if (value != null) {
+                queryParams.put(queryParameterToForwardToLogin, value);
             }
         }
-        WebUtils.issueRedirect(request, response, loginUrl);
+        WebUtils.issueRedirect(request, response, loginUrl, queryParams, /* contextRelative */ false);
     }
 }
