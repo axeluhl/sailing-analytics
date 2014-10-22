33c8fd6163ad128a5cfe76ae87b90f0cfeec20a6
Guy Saldanha <saldanha@gmx.de>
2014-10-22 14:56:43 +0200

    - moved QR code scan to separate view controller

---
+++ SAPTracker/HomeViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  SAPTracker
+//
+//  Created by computing on 17/10/14.
+//  Copyright (c) 2014 com.sap.sailing. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var trackingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addObservers()
+
+        // TODO: move someplace else, e.g. app delegate
+        DataManager.sharedManager
+    }
+
+    deinit {
+        removeObservers()
+    }
+    
+    func addObservers() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "trackingStarted:", name: LocationManager.NotificationType.trackingStartedNotificationKey, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "trackingStopped:", name: LocationManager.NotificationType.trackingStoppedNotificationKey, object: nil)
+    }
+    
+    func removeObservers() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    func trackingStarted(notification: NSNotification) {
+        println("trackingStarted")
+        trackingButton.setTitle("Stop Tracking", forState: UIControlState.Normal)
+    }
+    
+    func trackingStopped(notification: NSNotification) {
+        println("trackingStopped")
+        trackingButton.setTitle("Start Tracking", forState: UIControlState.Normal)
+    }
+
+    @IBAction func trackingButtonTap(sender: AnyObject) {
+        if LocationManager.sharedManager.isTracking {
+            LocationManager.sharedManager.stopTracking()
+        } else {
+            LocationManager.sharedManager.startTracking()
+        }
+    }
+  
+}
+
---
+++ SAPTracker/QRCodeViewController.swift
@@ -0,0 +1,71 @@
+//
+//  QRCodeViewController.swift
+//  SAPTracker
+//
+//  Created by computing on 22/10/14.
+//  Copyright (c) 2014 com.sap.sailing. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class QRCodeViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    struct NotificationType {
+        static let qrcodeScannedNotificationKey = "qrcode_scanned"
+    }
+
+    @IBOutlet weak var previewView: UIView!
+    var session: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startScanning()
+    }
+
+    @IBAction func dismiss(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func startScanning() {
+        var device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var writeError : NSError? = nil
+        var input = AVCaptureDeviceInput.deviceInputWithDevice(device, error: &writeError) as? AVCaptureDeviceInput
+        
+        var output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self,queue: dispatch_get_main_queue())
+        
+        session = AVCaptureSession()
+        session.canSetSessionPreset(AVCaptureSessionPresetHigh)
+        if session.canAddInput(input) {
+            session.addInput(input)
+        }
+        if session.canAddOutput(output) {
+            session.addOutput(output)
+        }
+        output.metadataObjectTypes = [AVMetadataObjectTypeEAN13Code,AVMetadataObjectTypeEAN8Code,AVMetadataObjectTypeQRCode]
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        previewLayer.frame = previewView.bounds
+        previewView.layer.addSublayer(previewLayer)
+        
+        session.startRunning()
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!)
+    {
+        if metadataObjects.count > 0 {
+            let metadataObject: AVMetadataMachineReadableCodeObject = metadataObjects[0] as AVMetadataMachineReadableCodeObject
+            println(metadataObject.stringValue)
+            
+            let notification = NSNotification(name: NotificationType.qrcodeScannedNotificationKey, object: self, userInfo:["value": metadataObject.stringValue])
+            NSNotificationQueue.defaultQueue().enqueueNotification(notification, postingStyle: NSPostingStyle.PostASAP)
+
+            session.stopRunning()
+            previewLayer.removeFromSuperlayer()
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+}
