db84b038971201f152293f87a63e1ce44e0fafd3
Peter Siegmund <siegmund@ubilabs.net>
2014-10-28 17:23:58 +0100

    unsent message are stored into a SQLite database

--- mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/data/database/BaseTable.java
+++ mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/data/database/BaseTable.java
@@ -4,12 +4,11 @@ import android.database.sqlite.SQLiteDatabase;
 
 public interface BaseTable {
 
-	public final static String _TIMESTAMP = "_timestamp";
+    public final static String _TIMESTAMP = "_timestamp";
 
-	public abstract void onCreate (SQLiteDatabase db);
+    public abstract void onCreate(SQLiteDatabase db);
 
-	public abstract void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion);
+    public abstract void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion);
 
-	public abstract void onDowngrade (SQLiteDatabase db, int oldVersion, int newVersion);
+    public abstract void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion);
 }
-
