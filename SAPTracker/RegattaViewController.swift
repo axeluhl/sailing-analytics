3c53f1afd79e62f92ca7de7f315c19cb78825a57
Guy Saldanha <saldanha@gmx.de>
2014-11-12 15:50:21 +0100

    - added regatta image picker

--- SAPTracker/RegattaViewController.swift
+++ SAPTracker/RegattaViewController.swift
@@ -9,26 +9,58 @@
 import Foundation
 import CoreLocation
 
-class RegattaViewController : UIViewController, UIActionSheetDelegate {
+class RegattaViewController : UIViewController, UIActionSheetDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    enum ActionSheet: Int {
+        case Menu, Image
+    }
+    var sourceTypes = [UIImagePickerControllerSourceType]()
+    var sourceTypeNames = [String]()
     
     // var regatta: Regatta
     
-    @IBAction func showActionSheet(sender: AnyObject) {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+    
+        // get image sources
+        super.viewDidLoad()
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            sourceTypes.append(UIImagePickerControllerSourceType.Camera)
+            sourceTypeNames.append("Camera")
+        }
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            sourceTypes.append(UIImagePickerControllerSourceType.PhotoLibrary)
+            sourceTypeNames.append("Photo Library")
+        }
+    }
+    
+    @IBAction func showMenuActionSheet(sender: AnyObject) {
         let actionSheet = UIActionSheet(title: nil, delegate: self, cancelButtonTitle: nil, destructiveButtonTitle: nil, otherButtonTitles: "Checkout", "Settings", "Edit Photo", "About", "Cancel")
+        actionSheet.tag = ActionSheet.Menu.rawValue
         actionSheet.cancelButtonIndex = 4
         actionSheet.showInView(self.view)
     }
     
     func actionSheet(actionSheet: UIActionSheet!, clickedButtonAtIndex buttonIndex: Int) {
-        switch buttonIndex{
-        case 1:
-            performSegueWithIdentifier("Settings", sender: actionSheet)
-            break
-        case 3:
-            performSegueWithIdentifier("About", sender: actionSheet)
-            break
-        default:
-            break
+        if actionSheet.tag == ActionSheet.Menu.rawValue {
+            switch buttonIndex{
+            case 1:
+                performSegueWithIdentifier("Settings", sender: actionSheet)
+                break
+            case 2:
+                showImageActionSheet(actionSheet)
+                break
+            case 3:
+                performSegueWithIdentifier("About", sender: actionSheet)
+                break
+            default:
+                break
+            }
+        } else if actionSheet.tag == ActionSheet.Image.rawValue {
+            if buttonIndex < actionSheet.cancelButtonIndex {
+                imagePicker(sourceTypes[buttonIndex])
+            }
         }
     }
     
@@ -42,4 +74,32 @@ class RegattaViewController : UIViewController, UIActionSheetDelegate {
         }
     }
     
+    // MARK:- Image picker
+    
+    @IBAction func showImageActionSheet(sender: AnyObject) {
+        if sourceTypes.count == 1 {
+            imagePicker(sourceTypes[0])
+        }
+        if sourceTypes.count == 2 {
+            let actionSheet = UIActionSheet(title: nil, delegate: self, cancelButtonTitle: nil, destructiveButtonTitle: nil, otherButtonTitles: sourceTypeNames[0], sourceTypeNames[1], "Cancel")
+            actionSheet.tag = ActionSheet.Image.rawValue
+            actionSheet.cancelButtonIndex = 2
+            actionSheet.showInView(self.view)
+        }
+    }
+    
+    func imagePicker(sourceType: UIImagePickerControllerSourceType) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = sourceType;
+        imagePickerController.mediaTypes = [kUTTypeImage];
+        imagePickerController.allowsEditing = false
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!) {
+        dismissViewControllerAnimated(true, completion: nil)
+        let selectedImage : UIImage = image
+        imageView.image = image
+    }
 }
