f49bc8a6d1cd027342cd4150ffeea63fc22ad0b7
Lukas Zielinski <lukas@lukaszielinski.de>
2014-11-12 16:40:08 +0100

    added TransmittingService

--- mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/services/TransmittingService.java
+++ mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/services/TransmittingService.java
@@ -2,12 +2,102 @@ package com.sap.sailing.android.tracking.app.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 
-public class TransmittingService extends Service {
+import com.sap.sailing.android.shared.logging.ExLog;
+import com.sap.sailing.android.tracking.app.R;
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+public class TransmittingService extends Service {
+	
+	private int UPDATE_INTERVAL_DEFAULT = 3000;
+	private int UPDATE_INTERVAL_POWERSAVE_MODE = 30000;
+	private int currentUpdateInterval = UPDATE_INTERVAL_DEFAULT;
+	
+	private Timer timer;
+	private Handler handler;
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		handler = new Handler();
+		
+		if (intent != null) {
+			if (intent.getAction() != null) {
+				if (intent.getAction().equals(getString(R.string.transmitting_service_start))) {
+					startTimer();
+				} else if (intent.getAction().equals(getString(R.string.transmitting_service_switch_to_power_saving_mode))) {
+					this.currentUpdateInterval = UPDATE_INTERVAL_POWERSAVE_MODE;
+				} else if (intent.getAction().equals(getString(R.string.transmitting_service_switch_to_default_power_mode))) {
+					this.currentUpdateInterval = UPDATE_INTERVAL_DEFAULT;
+				} else {
+					stopTimer();
+				}
+			}
+			else
+			{
+				stopTimer();
+			}
+		}
+		
+		return Service.START_STICKY;
+	}
+	
+
+	private void startTimer() {
+		timer = new Timer();
+		timer.start();
+		ExLog.i(this, "TIMER:", "Background update-timer start");
+	}
+
+	private void stopTimer() {
+		if (timer != null)
+		{
+			timer.stop();	
+			ExLog.i(this, "TIMER:", "Background update-timer stop");
+		}
+	}
+	
+	private void sendFixesToAPI() {
+		// IMPLEMENT ME
+	}
+
+	class Timer implements Runnable {
+		public Thread t;
+		public volatile boolean endExecution;
+		
+		@Override
+		public void run() {
+			while (!endExecution) {
+				try {
+					Thread.sleep(currentUpdateInterval);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				handler.post(new Runnable() {
+					@Override
+					public void run() {
+						sendFixesToAPI();
+					}
+				});
+			}
+		}
+
+		public void start() {
+			endExecution = false;
+			if (t == null) {
+				t = new Thread(this);
+				t.start();
+			}
+		}
+		
+		public void stop() {
+			endExecution = true;
+		}
+	}
 }
--- mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/ui/fragments/RegattaFragment.java
+++ mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/ui/fragments/RegattaFragment.java
@@ -13,35 +13,37 @@ import android.widget.Button;
 
 public class RegattaFragment extends BaseFragment implements OnClickListener {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
 
-        View view = inflater.inflate(R.layout.fragment_regatta, container, false);
+		View view = inflater.inflate(R.layout.fragment_regatta, container,
+				false);
 
-        Button startTracking = (Button) view.findViewById(R.id.startTracking);
-        startTracking.setOnClickListener(this);
-        Button stopTracking = (Button) view.findViewById(R.id.stopTracking);
-        stopTracking.setOnClickListener(this);
+		Button startTracking = (Button) view.findViewById(R.id.startTracking);
+		startTracking.setOnClickListener(this);
+		Button stopTracking = (Button) view.findViewById(R.id.stopTracking);
+		stopTracking.setOnClickListener(this);
 
-        return view;
-    }
+		return view;
+	}
 
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(getActivity(), TrackingService.class);
-        switch (view.getId()) {
-        case R.id.startTracking:
-            intent.setAction(getString(R.string.tracking_service_start));
-            break;
-            
-        case R.id.stopTracking:
-            intent.setAction(getString(R.string.tracking_service_stop));
-            break;
+	@Override
+	public void onClick(View view) {
+		Intent intent = new Intent(getActivity(), TrackingService.class);
+		switch (view.getId()) {
+		case R.id.startTracking:
+			intent.setAction(getString(R.string.tracking_service_start));
+			break;
 
-        default:
-            break;
-        }
-        getActivity().startService(intent);
-    }
+		case R.id.stopTracking:
+			intent.setAction(getString(R.string.tracking_service_stop));
+			break;
+
+		default:
+			break;
+		}
+		getActivity().startService(intent);
+	}
 }
