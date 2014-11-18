1adaa25dfd164dcd7d2a65daa3babc0d5cae2090
Fredrik Teschke <f@ftes.de>
2014-11-18 14:47:48 +0100

    implement IsSerializable instead of Serializable

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/DeviceMappingDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/DeviceMappingDTO.java
@@ -1,14 +1,13 @@
 package com.sap.sailing.gwt.ui.shared;
 
-import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
 import com.sap.sailing.domain.common.racelog.tracking.MappableToDevice;
 
-public class DeviceMappingDTO implements Serializable {
-    private static final long serialVersionUID = -3272980620254526040L;
+public class DeviceMappingDTO implements IsSerializable {
     public DeviceIdentifierDTO deviceIdentifier;
     public Date from;
     public Date to;
