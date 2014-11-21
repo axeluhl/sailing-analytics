3363864804f132f450084eac46230c9b0de3b10b
Axel Uhl <axel.uhl@sap.com>
2014-11-21 12:34:08 +0100

    added password validation to replication test

--- java/com.sap.sse.security.replication.test/src/com/sap/sse/security/replication/test/SimpleSecurityReplicationTest.java
+++ java/com.sap.sse.security.replication.test/src/com/sap/sse/security/replication/test/SimpleSecurityReplicationTest.java
@@ -4,15 +4,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import com.sap.sse.security.User;
 import com.sap.sse.security.operations.CreateUser;
+import com.sap.sse.security.shared.UserManagementException;
 
 public class SimpleSecurityReplicationTest extends AbstractSecurityReplicationTest {
     @Test
-    public void testSimpleReplication() throws InterruptedException {
+    public void testSimpleReplication() throws InterruptedException, UserManagementException {
         final String username = "Ernie";
         final String email = "ernie@sesame-street.com";
         final String password = "BertMyFriend";
@@ -22,6 +24,7 @@ public class SimpleSecurityReplicationTest extends AbstractSecurityReplicationTe
         User user = master.apply(createUser);
         assertNotNull(user);
         assertSame(user, master.getUserByName(username));
+        assertTrue(master.checkPassword(username, password));
         
         Thread.sleep(3000);
         
@@ -29,5 +32,6 @@ public class SimpleSecurityReplicationTest extends AbstractSecurityReplicationTe
         assertNotNull(replicatedErnie);
         assertEquals(username, replicatedErnie.getName());
         assertEquals(email, replicatedErnie.getEmail());
+        assertTrue(replica.checkPassword(username, password));
     }
 }
