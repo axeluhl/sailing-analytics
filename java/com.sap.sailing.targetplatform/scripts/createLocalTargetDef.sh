ec382dea4e0287bd5e6019d6c62601c43c970d6e
Axel Uhl <axel.uhl@sap.com>
2014-11-26 23:20:39 +0100

    extended patching of local p2 target path for Cygwin users

--- java/com.sap.sailing.targetplatform/scripts/createLocalTargetDef.sh
+++ java/com.sap.sailing.targetplatform/scripts/createLocalTargetDef.sh
@@ -3,7 +3,7 @@
 
 base="../definitions/race-analysis-p2"
 remote_repo="http://p2.sapsailing.com/p2/sailing/"
-local_repo="file://$(readlink -f ../../com.sap.sailing.targetplatform.base/target/repository/)"
+local_repo="file://`readlink -f ../../com.sap.sailing.targetplatform.base/target/repository/ | sed -e 's/\/cygdrive\/\(.\)/\/\1:/'`"
 
 # replace remote p2-repo URL with local repo URL
 sed -e "/^<repository location=\"/ s@$remote_repo@$local_repo@" $base-remote.target > $base-local.target
