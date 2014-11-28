47abca3054fe83c13e5d2a72270b05d8b447c1ee
Axel Uhl <axel.uhl@sap.com>
2014-11-28 17:34:47 +0100

    solving bug 2480 also for SecurityOperation implementations

--- java/com.sap.sse.security/src/com/sap/sse/security/operations/SecurityOperation.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/operations/SecurityOperation.java
@@ -4,4 +4,11 @@ import com.sap.sse.replication.OperationWithResult;
 import com.sap.sse.security.impl.ReplicableSecurityService;
 
 public interface SecurityOperation<ResultType> extends OperationWithResult<ReplicableSecurityService, ResultType> {
+    /**
+     * By default, the security-related operations will perform an update on the receiving replica that in turn
+     * will trigger replication of this change transitively.
+     */
+    default boolean isRequiresExplicitTransitiveReplication() {
+        return false;
+    }
 }
