236d01ab651eda65b0b27527978d3eb10a360d45
Fredrik Teschke <f@ftes.de>
2014-12-05 11:51:51 +0100

    change assertions to hamcrest

--- java/com.sap.sailing.domain.racelogtrackingadapter.test/src/com/sap/sailing/domain/racelogtracking/test/impl/TrackedRaceLoadsFixesTest.java
+++ java/com.sap.sailing.domain.racelogtrackingadapter.test/src/com/sap/sailing/domain/racelogtracking/test/impl/TrackedRaceLoadsFixesTest.java
@@ -1,6 +1,10 @@
 package com.sap.sailing.domain.racelogtracking.test.impl;
 
 import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -38,24 +42,26 @@ import com.sap.sailing.domain.tracking.impl.EmptyWindStore;
 
 public class TrackedRaceLoadsFixesTest extends AbstractGPSFixStoreTest {
     private final BoatClass boatClass = DomainFactory.INSTANCE.getOrCreateBoatClass("49er");
-    
+
     private int numberOfSwitchesBetweenLoadingRaceLogs = 0;
     private Integer previouslyLoading = null;
     private int numFixesReceived = 0;
-        
+
     @Test
-    public void areFixesStoredInDb() throws TransformationException, NoCorrespondingServiceRegisteredException, InterruptedException {
+    public void areFixesStoredInDb() throws TransformationException, NoCorrespondingServiceRegisteredException,
+            InterruptedException {
         Competitor comp2 = DomainFactory.INSTANCE.getOrCreateCompetitor("comp2", "comp2", null, null, null);
         Mark mark2 = DomainFactory.INSTANCE.getOrCreateMark("mark2");
         DeviceIdentifier device2 = new SmartphoneImeiIdentifier("imei2");
         DeviceIdentifier device3 = new SmartphoneImeiIdentifier("imei3");
-        Course course = new CourseImpl("course", Arrays.asList(new Waypoint[] {
-                new WaypointImpl(mark), new WaypointImpl(mark2)}));
-        RaceDefinition race = new RaceDefinitionImpl("race", course, boatClass, Arrays.asList(new Competitor[] {comp, comp2}));
+        Course course = new CourseImpl("course", Arrays.asList(new Waypoint[] { new WaypointImpl(mark),
+                new WaypointImpl(mark2) }));
+        RaceDefinition race = new RaceDefinitionImpl("race", course, boatClass, Arrays.asList(new Competitor[] { comp,
+                comp2 }));
 
         map(comp, device, 0, 20000);
         map(comp2, device2, 0, 600);
-        //reuse device for two marks
+        // reuse device for two marks
         map(mark, device3, 0, 600);
         map(mark2, device3, 0, 600);
 
@@ -64,17 +70,17 @@ public class TrackedRaceLoadsFixesTest extends AbstractGPSFixStoreTest {
         store.storeFix(device2, createFix(100, 10, 20, 30, 40));
         store.storeFix(device3, createFix(100, 10, 20, 30, 40));
         store.storeFix(device3, createFix(100, 10, 20, 30, 40));
-        
-        for (int i=0; i<10000; i++) {
-            store.storeFix(device, createFix(i+1000, 10, 20, 30, 40));
+
+        for (int i = 0; i < 10000; i++) {
+            store.storeFix(device, createFix(i + 1000, 10, 20, 30, 40));
         }
 
         TrackedRegatta regatta = new DynamicTrackedRegattaImpl(new RegattaImpl(EmptyRaceLogStore.INSTANCE,
-                EmptyRegattaLogStore.INSTANCE, RegattaImpl.getDefaultName("regatta", boatClass.getName()),
-                boatClass, /*startDate*/ null, /*endDate*/ null, null, null, "a", null));
-        DynamicTrackedRaceImpl trackedRace = new DynamicTrackedRaceImpl(regatta, race, Collections.<Sideline>emptyList(),
-                EmptyWindStore.INSTANCE, store, 0, 0, 0);
-        
+                EmptyRegattaLogStore.INSTANCE, RegattaImpl.getDefaultName("regatta", boatClass.getName()), boatClass, /* startDate */
+                null, /* endDate */null, null, null, "a", null));
+        DynamicTrackedRaceImpl trackedRace = new DynamicTrackedRaceImpl(regatta, race,
+                Collections.<Sideline> emptyList(), EmptyWindStore.INSTANCE, store, 0, 0, 0);
+
         trackedRace.attachRaceLog(raceLog);
         trackedRace.waitForLoadingFromGPSFixStoreToFinishRunning(raceLog);
 
@@ -83,9 +89,9 @@ public class TrackedRaceLoadsFixesTest extends AbstractGPSFixStoreTest {
         testLength(trackedRace.getOrCreateTrack(mark), 1);
         testLength(trackedRace.getOrCreateTrack(mark2), 1);
     }
-    
+
     @Test
-    public void metadataStoredInDb() throws TransformationException, NoCorrespondingServiceRegisteredException {    
+    public void metadataStoredInDb() throws TransformationException, NoCorrespondingServiceRegisteredException {
         assertEquals(0, store.getNumberOfFixes(device));
         assertEquals(null, store.getTimeRangeCoveredByFixes(device));
 
@@ -93,37 +99,38 @@ public class TrackedRaceLoadsFixesTest extends AbstractGPSFixStoreTest {
 
         store.storeFix(device, createFix(100, 10, 20, 30, 40));
         store.storeFix(device, createFix(200, 10, 20, 30, 40));
-        
+
         assertEquals(2, store.getNumberOfFixes(device));
         assertEquals(TimeRangeImpl.create(100, 200), store.getTimeRangeCoveredByFixes(device));
     }
-    
+
     @Test
-    public void attachTwoRaceLogsAndSeeIfLoadingIsSerializedNicely()
-            throws TransformationException, NoCorrespondingServiceRegisteredException, InterruptedException {
-        Course course = new CourseImpl("course", Collections.<Waypoint>emptyList());
-        RaceDefinition race = new RaceDefinitionImpl("race", course, boatClass, Arrays.asList(new Competitor[] {comp}));
+    public void attachTwoRaceLogsAndSeeIfLoadingIsSerializedNicely() throws TransformationException,
+            NoCorrespondingServiceRegisteredException, InterruptedException {
+        Course course = new CourseImpl("course", Collections.<Waypoint> emptyList());
+        RaceDefinition race = new RaceDefinitionImpl("race", course, boatClass,
+                Arrays.asList(new Competitor[] { comp }));
         RaceLog raceLog2 = new RaceLogImpl("raceLog 2");
-        
+
         final int numFixes = 10000;
-        map(comp, device, 0, numFixes/2);
-        map(raceLog2, comp, device, numFixes/2+1, numFixes);
-        
-        for (int i=0; i<numFixes; i++) {
+        map(comp, device, 0, numFixes / 2);
+        map(raceLog2, comp, device, numFixes / 2 + 1, numFixes);
+
+        for (int i = 0; i < numFixes; i++) {
             store.storeFix(device, createFix(i, 10, 20, 30, 40));
         }
 
         TrackedRegatta regatta = new DynamicTrackedRegattaImpl(new RegattaImpl(EmptyRaceLogStore.INSTANCE,
-                EmptyRegattaLogStore.INSTANCE, RegattaImpl.getDefaultName("regatta", boatClass.getName()),
-                boatClass, /*startDate*/ null, /*endDate*/ null, null, null, "a", null));
-        DynamicTrackedRaceImpl trackedRace = new DynamicTrackedRaceImpl(regatta, race, Collections.<Sideline>emptyList(),
-                EmptyWindStore.INSTANCE, store, 0, 0, 0);
-        
+                EmptyRegattaLogStore.INSTANCE, RegattaImpl.getDefaultName("regatta", boatClass.getName()), boatClass, /* startDate */
+                null, /* endDate */null, null, null, "a", null));
+        DynamicTrackedRaceImpl trackedRace = new DynamicTrackedRaceImpl(regatta, race,
+                Collections.<Sideline> emptyList(), EmptyWindStore.INSTANCE, store, 0, 0, 0);
+
         trackedRace.addListener(new AbstractRaceChangeListener() {
             @Override
             public void competitorPositionChanged(GPSFixMoving fix, Competitor competitor) {
                 numFixesReceived++;
-                int currentlyLoading = fix.getTimePoint().asMillis() <= numFixes/2 ? 1 : 2;
+                int currentlyLoading = fix.getTimePoint().asMillis() <= numFixes / 2 ? 1 : 2;
                 if (previouslyLoading == null) {
                     previouslyLoading = currentlyLoading;
                 } else if (previouslyLoading != currentlyLoading) {
@@ -138,8 +145,10 @@ public class TrackedRaceLoadsFixesTest extends AbstractGPSFixStoreTest {
 
         trackedRace.waitForLoadingFromGPSFixStoreToFinishRunning(raceLog);
         trackedRace.waitForLoadingFromGPSFixStoreToFinishRunning(raceLog2);
-        
-        assertEquals(1, numberOfSwitchesBetweenLoadingRaceLogs);
-        assertEquals(numFixes, numFixesReceived);
+
+        assertThat("switch at most once between the two race logs while loading",
+                numberOfSwitchesBetweenLoadingRaceLogs, lessThanOrEqualTo(1));
+        assertThat("number of fixes added to race equal to number of fixes in GPSFixStore", numFixesReceived,
+                equalTo(numFixes));
     }
 }
