f58a4c1e3efb449b8f958c62237be05da1b1c4af
Papick Garcia Taboada <pgt@pgt.de>
2014-12-10 14:30:56 +0100

    Randomize videos

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/mainmedia/MainMedia.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/mainmedia/MainMedia.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.gwt.home.client.shared.mainmedia;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -26,13 +27,14 @@ public class MainMedia extends Composite {
 
     @UiField
     HTMLPanel videosPanel;
+
     @UiField
     DivElement videoLightBoxData;
 
     @UiField
     ImageCarousel imageCarousel;
 
-    private int videoCounter;
+    private final HashSet<String> addedVideoUrls = new HashSet<String>(MAX_VIDEO_COUNT);
 
     interface MainMediaUiBinder extends UiBinder<Widget, MainMedia> {
     }
@@ -40,16 +42,15 @@ public class MainMedia extends Composite {
     private static MainMediaUiBinder uiBinder = GWT.create(MainMediaUiBinder.class);
 
     public MainMedia(PlaceNavigator navigator) {
-        videoCounter = 0;
-
         MainMediaResources.INSTANCE.css().ensureInjected();
         initWidget(uiBinder.createAndBindUi(this));
     }
 
     public void setFeaturedEvents(List<Pair<StageEventType, EventBaseDTO>> featuredEvents) {
         for (Pair<StageEventType, EventBaseDTO> featuredEventTypeAndEvent : featuredEvents) {
-            if (featuredEventTypeAndEvent.getB().getVideoURLs().size() > 0 && videoCounter < MAX_VIDEO_COUNT) {
-                addVideoToVideoPanel(featuredEventTypeAndEvent.getB());
+            if (featuredEventTypeAndEvent.getB().getVideoURLs().size() > 0 && addedVideoUrls.size() < MAX_VIDEO_COUNT) {
+                String youTubeRandomUrl = getRandomVideoURL(featuredEventTypeAndEvent.getB());
+                addVideoToVideoPanel(youTubeRandomUrl, featuredEventTypeAndEvent.getB());
             }
         }
     }
@@ -61,19 +62,41 @@ public class MainMedia extends Composite {
                 this.height = height;
                 this.width = width;
             }
-
             String url;
             int height;
             int width;
         }
-        List<Holder> photoGalleryUrls = new LinkedList<>();
+
+        final List<Holder> photoGalleryUrls = new LinkedList<>();
+        final List<Pair<String, EventBaseDTO>> videoCandidates = new LinkedList<>();
+
         for (EventBaseDTO event : recentEvents) {
             for (String url : event.getSailingLovesPhotographyImages()) {
                 ImageSize size = event.getImageSize(url);
                 photoGalleryUrls.add(new Holder(url, size.getHeight(), size.getWidth()));
             }
-            if (!event.getVideoURLs().isEmpty() && videoCounter < MAX_VIDEO_COUNT) {
-                addVideoToVideoPanel(event);
+            for (String videoUrl : event.getVideoURLs()) {
+                videoCandidates.add(new Pair<String, EventBaseDTO>(videoUrl, event));
+            }
+        }
+
+        final int numberOfCandidatesAvailable = videoCandidates.size();
+        if (numberOfCandidatesAvailable <= (MAX_VIDEO_COUNT - addedVideoUrls.size())) {
+            // add all we have, no randomize
+            for (Pair<String, EventBaseDTO> videoCandidateInfo : videoCandidates) {
+                addVideoToVideoPanel(videoCandidateInfo.getA(), videoCandidateInfo.getB());
+            }
+        } else {
+            // fill up the list randomly from videoCandidates
+            final Random videosRandomizer = new Random(numberOfCandidatesAvailable);
+            randomlyPick: for (int i = 0; i < numberOfCandidatesAvailable; i++) {
+                int nextVideoindex = videosRandomizer.nextInt(numberOfCandidatesAvailable);
+                final Pair<String, EventBaseDTO> videoCandidateInfo = videoCandidates.get(nextVideoindex);
+                final String youtubeUrl = videoCandidateInfo.getA();
+                addVideoToVideoPanel(youtubeUrl, videoCandidateInfo.getB());
+                if (addedVideoUrls.size() == MAX_VIDEO_COUNT) {
+                    break randomlyPick;
+                }
             }
         }
 
@@ -83,22 +106,21 @@ public class MainMedia extends Composite {
         for (int i = 0; i < gallerySize; i++) {
             Collections.swap(photoGalleryUrls, i, random.nextInt(gallerySize));
         }
-
         for (Holder holder : photoGalleryUrls) {
-
             imageCarousel.addImage(holder.url, holder.height, holder.width);
         }
-
     }
 
-    private void addVideoToVideoPanel(EventBaseDTO event) {
-        String youtubeUrl = getRandomVideoURL(event);
+    private void addVideoToVideoPanel(String youtubeUrl, EventBaseDTO event) {
+        if (addedVideoUrls.contains(youtubeUrl)) {
+            return;
+        }
         String eventName = event.getName();
         String youtubeId = YoutubeApi.getIdByUrl(youtubeUrl);
         if (youtubeId != null && !youtubeId.trim().isEmpty()) {
             MainMediaVideo video = new MainMediaVideo(eventName, youtubeId);
             videosPanel.add(video);
-            videoCounter++;
+            addedVideoUrls.add(youtubeUrl);
         }
     }
 
@@ -112,5 +134,7 @@ public class MainMedia extends Composite {
         }
         return result;
     }
+
+
 
 }
