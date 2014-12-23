3c86aaea47b0e23eaca808a47f66eec52e801f42
Fredrik Teschke <f@ftes.de>
2014-12-23 09:47:24 +0100

    only 1 activity in tracking app

--- mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/ui/activities/LaunchActivity.java
+++ mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/ui/activities/LaunchActivity.java
@@ -7,17 +7,21 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.sap.sailing.android.shared.logging.ExLog;
 import com.sap.sailing.android.shared.services.sending.MessageSendingService;
 import com.sap.sailing.android.tracking.app.R;
+import com.sap.sailing.android.tracking.app.services.TrackingService;
 import com.sap.sailing.domain.common.racelog.tracking.DeviceMappingConstants;
 import com.sap.sailing.domain.racelogtracking.DeviceIdentifier;
 import com.sap.sailing.domain.racelogtracking.impl.SmartphoneUUIDIdentifierImpl;
@@ -27,33 +31,89 @@ import com.sap.sse.common.impl.MillisecondsTimePoint;
 public class LaunchActivity extends BaseActivity {
     private static int requestCodeQRCode = 42471;
     private static final String TAG = LaunchActivity.class.getName();
+    private boolean isTrackingActive = false;
+    private Button toggleTrackingBtn;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launch_activity);
-
-        Button scanBarcode = (Button) findViewById(R.id.btnScanBarcode);
-        scanBarcode.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                requestQRCodeScan();
-            }
-        });
+        toggleTrackingBtn = (Button) findViewById(R.id.btnToggleTracking);
     }
     
-    protected boolean requestQRCodeScan() {
+    public void onScanQRCodeClicked(View view) {
         try {
             Intent intent = new Intent("com.google.zxing.client.android.SCAN");
             intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
             startActivityForResult(intent, requestCodeQRCode);
-            return true;
         } catch (Exception e) {    
             Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
             Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
             startActivity(marketIntent);
         }
-        return false;
+    }
+
+    public static class ErrorDialogFragment extends DialogFragment {
+        private Dialog dialog;
+
+        public ErrorDialogFragment() {
+            super();
+            dialog = null;
+        }
+
+        /**
+         * Set the dialog to display
+         *
+         * @param dialog
+         *            An error dialog
+         */
+        public void setDialog(Dialog dialog) {
+            this.dialog = dialog;
+        }
+
+        /*
+         * This method must return a Dialog to the DialogFragment.
+         */
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return dialog;
+        }
+    }
+
+    private boolean googlePLayServicesAvailable() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (ConnectionResult.SUCCESS == resultCode) {
+            ExLog.i(this, TAG, getString(R.string.play_services_available));
+            return true;
+        } else {
+            // Display an error dialog
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+            if (dialog != null) {
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                errorFragment.setDialog(dialog);
+                errorFragment.show(getSupportFragmentManager(), TAG);
+            }
+            return false;
+        }
+    }
+    
+    public void onToggleTrackingClicked(View view) {
+        if (!googlePLayServicesAvailable()) {
+            return;
+        }
+        
+        if (isTrackingActive == false){
+                Intent startTrackingIntent = new Intent(this, TrackingService.class);
+            getBaseContext().startService(startTrackingIntent);
+            toggleTrackingBtn.setText(getString(R.string.stop_tracking));
+            isTrackingActive = true;
+        } else {
+                Intent startTrackingIntent = new Intent(this, TrackingService.class);
+            getBaseContext().stopService(startTrackingIntent);
+            toggleTrackingBtn.setText(getString(R.string.start_tracking));
+            isTrackingActive = false;
+        }
     }
     
     @Override
@@ -73,6 +133,7 @@ public class LaunchActivity extends BaseActivity {
                 server += ":" + uri.getPort();
             }
             prefs.setServerURL(server);
+            
             String leaderboard = uri.getQueryParameter(DeviceMappingConstants.LEADERBOARD_NAME);
             String competitorIdAsString = uri.getQueryParameter(DeviceMappingConstants.COMPETITOR_ID_AS_STRING);
             String markIdAsString = uri.getQueryParameter(DeviceMappingConstants.MARK_ID_AS_STRING);
@@ -89,8 +150,10 @@ public class LaunchActivity extends BaseActivity {
                 if (competitorIdAsString != null) {
                     mappingStart.put(DeviceMappingConstants.PUSH_DEVICE_ID, "<push_device_id_not_known_on_android>");
                     mappingStart.put(DeviceMappingConstants.COMPETITOR_ID_AS_STRING, competitorIdAsString);
+                    itemType = "competitor";
                 } else if (markIdAsString != null) {
                     mappingStart.put(DeviceMappingConstants.MARK_ID_AS_STRING, markIdAsString);
+                    itemType = "mark";
                 }
             } catch (IllegalArgumentException e) {
                 //deserializing the competitor/mark on the server would fail if the idAsString were used
@@ -105,15 +168,13 @@ public class LaunchActivity extends BaseActivity {
                     UUID.randomUUID(), mappingStart.toString(), /* callbackClass */ null));
             ExLog.i(this, TAG, "Created mapping event");
             Toast.makeText(this, "Successfully created mapping between device and " + itemType, Toast.LENGTH_LONG).show();
-
-            startActivity(new Intent(this, StartTrackingActivity.class));
         } else {
             Toast.makeText(this, "Error scanning QRCode (" + resultCode + ")", Toast.LENGTH_LONG).show();
         }
     }
     
     private String getPostCheckinUrl(String serverBaseUrl, String leaderboardName) {
-        return serverBaseUrl+"/sailingserver/api/v1/leaderboards/"+leaderboardName+"/device_mappings/start";
+        return serverBaseUrl+"/sailingserver/api/v1/leaderboards/"+Uri.encode(leaderboardName)+"/device_mappings/start";
     }
 
     //FIXME: currently duplicate code with racecommitte app (GeneralPreferenceFragment), refactor once maven build is available and thus update mechanism can be created for tracking app see bugs 2398 2399
