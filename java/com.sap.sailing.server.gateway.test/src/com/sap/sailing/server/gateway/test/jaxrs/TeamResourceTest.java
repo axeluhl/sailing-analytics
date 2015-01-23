16f35c1b24adbbfaedad969b1e58ab452827d1b7
Fredrik Teschke <f@ftes.de>
2015-01-23 11:15:28 +0100

    added implementation of file storage service

---
+++ java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/jaxrs/TeamResourceTest.java
@@ -0,0 +1,62 @@
+package com.sap.sailing.server.gateway.test.jaxrs;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+
+import org.junit.Test;
+
+import com.sap.sailing.domain.base.impl.BoatClassImpl;
+import com.sap.sailing.domain.base.impl.BoatImpl;
+import com.sap.sailing.domain.base.impl.DynamicBoat;
+import com.sap.sailing.domain.base.impl.DynamicTeam;
+import com.sap.sailing.domain.base.impl.NationalityImpl;
+import com.sap.sailing.domain.base.impl.PersonImpl;
+import com.sap.sailing.domain.base.impl.TeamImpl;
+import com.sap.sailing.server.gateway.jaxrs.api.TeamResource;
+import com.sap.sse.filestorage.FileStorageService;
+import com.sap.sse.filestorage.impl.AmazonS3FileStorageServiceImpl;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+
+public class TeamResourceTest extends AbstractJaxRsApiTest {
+    private final String name = "Heiko KRÖGER";
+    private final String id = "af855a56-9726-4a9c-a77e-da955bd289be";
+    private final String boatClassName = "49er";
+    private final String sailID = "GER 1";
+    private final String nationality = "GER";
+    private final String teamImageFile = "Bundesliga2014_Regatta6_eventteaser.jpg";
+    
+    private final FileStorageService storageService = new AmazonS3FileStorageServiceImpl();
+
+    @Override
+    public void setUp() {
+        super.setUp();
+        racingEventService = spy(racingEventService);
+        doReturn(storageService).when(racingEventService).getFileStorageService();
+        DynamicTeam team = new TeamImpl(null, Collections.singleton(new PersonImpl(null, new NationalityImpl(
+                nationality), null, null)), null);
+        DynamicBoat boat = new BoatImpl(null, new BoatClassImpl(boatClassName, false), sailID);
+        racingEventService.getBaseDomainFactory().getOrCreateCompetitor(id, name, null, team, boat);
+    }
+
+    @Test
+    public void storeAndRemoveTeamImage() throws URISyntaxException {
+        //set team image
+        TeamResource r = new TeamResource();
+        long length = new File(new URI(getClass().getResource(teamImageFile).toString())).length();
+        System.out.println("Found file with size (bytes) " + length);
+        InputStream stream = getClass().getResourceAsStream(teamImageFile);
+
+        FormDataContentDisposition fileDetails = FormDataContentDisposition.name("file").size(length)
+                .fileName(teamImageFile).build();
+        r.setTeamImage(id, stream, fileDetails);
+        
+        //now download and compare
+        
+    }
+}
---
+++ java/com.sap.sse.filestorage/src/com/sap/sse/filestorage/impl/AmazonS3FileStorageServiceImpl.java
@@ -0,0 +1,96 @@
+package com.sap.sse.filestorage.impl;
+
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.sap.sse.filestorage.FileStorageService;
+
+/**
+ * For testing purposes configure the access credentials as follows:
+ * To link this service to an AWS account, create the following file: ~/.aws/credentials
+ * and add credentials to it (get the access id and secret key from
+ * https://console.aws.amazon.com/iam/home?#security_credential).
+ * 
+ * TODO configure credentials in AdminConsole
+ * TODO configure bucket name in AdminConsole
+ * 
+ * @author Fredrik Teschke
+ *
+ */
+public class AmazonS3FileStorageServiceImpl implements FileStorageService {
+    private static final Logger logger = Logger.getLogger(AmazonS3FileStorageServiceImpl.class.getName());
+    
+    private static final String baseUrl = "s3.amazonaws.com";
+    private static final String retrievalProtocol = "http";
+    private static final String bucketName = "ftes-sap-sailing";
+    private final AmazonS3 s3;
+    
+    public AmazonS3FileStorageServiceImpl() {
+        /*
+         * The ProfileCredentialsProvider will return your [default]
+         * credential profile by reading from the credentials file located at
+         * (~/.aws/credentials).
+         */
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+
+        s3 = new AmazonS3Client(credentials);
+    }
+    
+    private static String getKey(String originalFileName) {
+        String key = UUID.randomUUID().toString();
+        if (originalFileName != null) {
+            key += "/" + originalFileName;
+        }
+        return key;
+    }
+    
+    private static URI getUri(String key) {
+        try {
+            return new URI(retrievalProtocol, bucketName + "." + baseUrl, key, null);
+        } catch (URISyntaxException e) {
+            logger.log(Level.WARNING, "Could not create URI for uploaded file with key " + key, e);
+            return null;
+        }
+    }
+
+
+    @Override
+    public URI storeFile(InputStream is, String originalFileName, long lengthInBytes) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(lengthInBytes);
+        String key = getKey(originalFileName);
+        PutObjectRequest request = new PutObjectRequest(bucketName, key, is, metadata)
+            .withCannedAcl(CannedAccessControlList.PublicRead);
+        s3.putObject(request);
+        return getUri(key);
+    }
+
+    @Override
+    public void removeFile(URI uri) {
+        String key = uri.getPath();
+        s3.deleteObject(new DeleteObjectRequest(bucketName, key));
+    }
+
+}
