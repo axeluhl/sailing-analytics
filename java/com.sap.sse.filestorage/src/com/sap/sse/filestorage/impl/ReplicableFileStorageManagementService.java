1272b9a314f56b6c79b59a5b3e73237658ad4b77
Fredrik Teschke <f@ftes.de>
2015-01-29 16:58:53 +0100

    add documentation for replicable service

--- java/com.sap.sse.filestorage/src/com/sap/sse/filestorage/impl/ReplicableFileStorageManagementService.java
+++ java/com.sap.sse.filestorage/src/com/sap/sse/filestorage/impl/ReplicableFileStorageManagementService.java
@@ -1,15 +1,21 @@
 package com.sap.sse.filestorage.impl;
 
 import com.sap.sse.common.NoCorrespondingServiceRegisteredException;
+import com.sap.sse.filestorage.FileStorageManagementService;
 import com.sap.sse.filestorage.common.FileStorageService;
 
 /**
- * Defines internal operations that do are called only by
+ * Publishes those methods of {@link FileStorageManagementService} that are required by operations implemented as lambda
+ * expressions to fulfill their tasks. These operations should not be invoked by external service clients.
+ * {@link FileStorageManagementService} is the one registered with the OSGi registry and thus the publicly-visible
+ * interface (description copied from ReplicableSecurityService).
+ * 
  * @author Fredrik Teschke
  *
  */
 public interface ReplicableFileStorageManagementService {
     void internalSetFileStorageServiceProperty(String serviceName, String propertyName, String propertyValue)
             throws NoCorrespondingServiceRegisteredException, IllegalArgumentException;
+
     void internalSetActiveFileStorageService(FileStorageService service);
 }
