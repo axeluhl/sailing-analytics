ee7d433c30edf50361a57d843bee1ce0caab2b1f
Steffen Schaefer <steffen.schaefer@oio.de>
2015-02-02 11:41:21 +0100

    Added missing @alternate annotations for *.gss declarations

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/stage/StageResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/stage/StageResources.java
@@ -32,10 +32,7 @@ public interface StageResources extends ClientBundle {
         String stageteaser_content_band_text_name();
         String stageteaser_content_band_action();
         String swipercontainer();
-        String swiperwrapper();
         String swiperslide();
-        String swiperwp8horizontal();
-        String swiperwp8vertical();
         String stage();
         String stage_teasers();
         String stage_controls();
