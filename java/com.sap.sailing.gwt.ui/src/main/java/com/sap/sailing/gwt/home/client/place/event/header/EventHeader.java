04d434863b9b4a433e31e93bc73a0f391258d1e1
Steffen Schaefer <steffen.schaefer@oio.de>
2015-02-02 09:57:45 +0100

    Changed EventHeader.css to EventHeader.gss.

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/header/EventHeader.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/header/EventHeader.java
@@ -5,7 +5,6 @@ import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.ImageElement;
 import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.dom.client.Style.Display;
-import com.google.gwt.dom.client.StyleInjector;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -121,7 +120,6 @@ public class EventHeader extends AbstractEventComposite {
 
     private void initResources() {
         EventHeaderResources.INSTANCE.css().ensureInjected();
-        StyleInjector.injectAtEnd("@media (min-width: 50em) { "+EventHeaderResources.INSTANCE.largeCss().getText()+"}");
         
         initWidget(uiBinder.createAndBindUi(this));
     }
