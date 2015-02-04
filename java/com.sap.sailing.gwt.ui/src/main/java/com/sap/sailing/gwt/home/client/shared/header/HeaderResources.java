eccc032f31ab46e0b2336f48689451c751c1fa94
Steffen Schaefer <steffen.schaefer@oio.de>
2015-02-04 14:18:14 +0100

    Adaption of new Header styles

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/header/HeaderResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/header/HeaderResources.java
@@ -3,7 +3,6 @@ package com.sap.sailing.gwt.home.client.shared.header;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
-import com.google.gwt.resources.client.CssResource.Shared;
 import com.google.gwt.resources.client.ImageResource;
 
 public interface HeaderResources extends ClientBundle {
@@ -12,20 +11,17 @@ public interface HeaderResources extends ClientBundle {
     @Source("com/sap/sailing/gwt/home/client/shared/header/Header.gss")
     LocalCss css();
 
-    @Shared
     public interface LocalCss extends CssResource {
         String siteheader();
-        String siteheader_full();
-        String logo();
-        String logotitle();
+        String siteheader_logo();
+        String siteheader_logo_image();
+        String siteheader_logo_title();
         String sitenavigation();
         String sitenavigation_link();
         String sitenavigation_linkactive();
         String sitenavigation_search();
-        String sitenavigationexpanded();
-        String searchsmall();
-        String search_textfield();
         String search();
+        String search_textfield();
         String search_button();
         String submit();
     }
