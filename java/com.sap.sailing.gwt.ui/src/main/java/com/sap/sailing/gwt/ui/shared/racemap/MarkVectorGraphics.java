9218392a24a9f4338f7ebccb1c15f525281fd767
Axel Uhl <axel.uhl@sap.com>
2015-02-06 10:26:27 +0100

    emergency fix for bug 2620: drawing start and finish boats as simple marks as a fallback

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/racemap/MarkVectorGraphics.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/racemap/MarkVectorGraphics.java
@@ -469,9 +469,165 @@ public class MarkVectorGraphics {
     }
 
     protected void drawStartBoat(Context2d ctx, boolean isSelected, String color2) {
+        ctx.setStrokeStyle("rgba(0,0,0,0)");
+        
+        ctx.save();
+        ctx.setFillStyle(color);
+        ctx.transform(12.5,0,0,12.5,-578,22.2);
+        
+        ctx.beginPath();
+        ctx.arc(49.7,12.8,2.66,0,doublePi,false);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+        
+        ctx.save();
+        ctx.setFillStyle(DEFAULT_MARK_BG_COLOR);
+        ctx.transform(12.5,0,0,12.5,-578,22.2);
+        ctx.beginPath();
+        ctx.arc(49.7,11.3,3.37,0,doublePi,false);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+        
+        ctx.save();
+        ctx.setFillStyle(color);
+        ctx.transform(12.5,0,0,12.5,-578,22.2);
+        ctx.beginPath();
+        ctx.arc(49.6,10.6,2.66,0,doublePi,false);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+
+        ctx.save();
+        ctx.setFillStyle(DEFAULT_MARK_BG_COLOR);
+        ctx.transform(4.98,0,0,10.8,-205,50.4);
+        ctx.beginPath();
+        ctx.moveTo(50.8,9.84);
+        ctx.lineTo(48.5,9.84);
+        ctx.lineTo(48.5,1.44);
+        ctx.lineTo(50.8,1.9);
+        ctx.lineTo(50.8,9.84);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+
+        ctx.save();
+        ctx.transform(12.5,0,0,12.5,-570,22.2);
+        ctx.save();
+        ctx.setFillStyle(color);
+        ctx.beginPath();
+        ctx.moveTo(48.8,3.82);
+        ctx.lineTo(48.8,-1.32);
+        ctx.lineTo(57,1.05);
+        ctx.lineTo(48.8,3.82);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+        
+        ctx.save();
+        ctx.setFillStyle(DEFAULT_MARK_BG_COLOR);
+        ctx.beginPath();
+        ctx.moveTo(48.5,4.16);
+        ctx.lineTo(48.5,-1.65);
+        ctx.lineTo(57.8,1.03);
+        ctx.lineTo(48.5,4.16);
+        ctx.closePath();
+        ctx.moveTo(49,-0.994);
+        ctx.lineTo(49,3.47);
+        ctx.lineTo(56.2,1.07);
+        ctx.lineTo(49,-0.994);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+        ctx.restore();
     }
 
     protected void drawFinishBoat(Context2d ctx, boolean isSelected, String color2) {
+        ctx.setStrokeStyle("rgba(0,0,0,0)");
+        
+        ctx.save();
+        ctx.setFillStyle(color);
+        ctx.transform(12.5,0,0,12.5,-578,22.2);
+        
+        ctx.beginPath();
+        ctx.arc(49.7,12.8,2.66,0,doublePi,false);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+        
+        ctx.save();
+        ctx.setFillStyle(DEFAULT_MARK_BG_COLOR);
+        ctx.transform(12.5,0,0,12.5,-578,22.2);
+        ctx.beginPath();
+        ctx.arc(49.7,11.3,3.37,0,doublePi,false);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+        
+        ctx.save();
+        ctx.setFillStyle(color);
+        ctx.transform(12.5,0,0,12.5,-578,22.2);
+        ctx.beginPath();
+        ctx.arc(49.6,10.6,2.66,0,doublePi,false);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+
+        ctx.save();
+        ctx.setFillStyle(DEFAULT_MARK_BG_COLOR);
+        ctx.transform(4.98,0,0,10.8,-205,50.4);
+        ctx.beginPath();
+        ctx.moveTo(50.8,9.84);
+        ctx.lineTo(48.5,9.84);
+        ctx.lineTo(48.5,1.44);
+        ctx.lineTo(50.8,1.9);
+        ctx.lineTo(50.8,9.84);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+
+        ctx.save();
+        ctx.transform(12.5,0,0,12.5,-570,22.2);
+        ctx.save();
+        ctx.setFillStyle(color);
+        ctx.beginPath();
+        ctx.moveTo(48.8,3.82);
+        ctx.lineTo(48.8,-1.32);
+        ctx.lineTo(57,1.05);
+        ctx.lineTo(48.8,3.82);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+        
+        ctx.save();
+        ctx.setFillStyle(DEFAULT_MARK_BG_COLOR);
+        ctx.beginPath();
+        ctx.moveTo(48.5,4.16);
+        ctx.lineTo(48.5,-1.65);
+        ctx.lineTo(57.8,1.03);
+        ctx.lineTo(48.5,4.16);
+        ctx.closePath();
+        ctx.moveTo(49,-0.994);
+        ctx.lineTo(49,3.47);
+        ctx.lineTo(56.2,1.07);
+        ctx.lineTo(49,-0.994);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+        ctx.restore();
     }
 
     public double getMarkHeightInMeters() {
