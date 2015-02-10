c2b904ca8525f927a7d321f2016acb29bee9c844
Peter Siegmund <siegmund@ubilabs.net>
2015-02-10 09:45:24 +0100

    deleted Tracker app test-suite

--- mobile/com.sap.sailing.android.tracking.app.test-test/gen/com/sap/sailing/android/tracking/app/test/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.sap.sailing.android.tracking.app.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
