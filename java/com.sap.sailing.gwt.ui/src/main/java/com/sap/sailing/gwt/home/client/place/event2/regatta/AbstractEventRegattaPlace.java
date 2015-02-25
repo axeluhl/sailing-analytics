189d4865498fc5c2ecd3b488ebed3d9db4ca30ea
Steffen Schaefer <steffen.schaefer@oio.de>
2015-02-25 09:22:58 +0100

    Removed debug code

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event2/regatta/AbstractEventRegattaPlace.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event2/regatta/AbstractEventRegattaPlace.java
@@ -1,6 +1,5 @@
 package com.sap.sailing.gwt.home.client.place.event2.regatta;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.place.shared.PlaceTokenizer;
 import com.sap.sailing.gwt.home.client.place.event2.AbstractEventPlace;
 import com.sap.sailing.gwt.home.client.place.event2.tabs.EventContext;
@@ -9,18 +8,10 @@ public abstract class AbstractEventRegattaPlace extends AbstractEventPlace {
 
     public AbstractEventRegattaPlace(EventContext ctx) {
         super(ctx);
-        if(ctx.getRegattaId() == null || ctx.getRegattaId().isEmpty()) {
-            GWT.debugger();
-            throw new IllegalArgumentException();
-        }
     }
 
     public AbstractEventRegattaPlace(String eventUuidAsString, String regattaId) {
         super(eventUuidAsString);
-        if(regattaId == null || regattaId.isEmpty()) {
-            GWT.debugger();
-            throw new IllegalArgumentException();
-        }
         getCtx().withRegattaId(regattaId);
     }
 
