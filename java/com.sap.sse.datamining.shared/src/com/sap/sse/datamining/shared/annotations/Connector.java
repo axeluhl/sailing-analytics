83da840124c48d4463c334c47608ec447a7b6ea5
Lennart Hensler <LennartHensler@aol.com>
2015-03-02 12:40:48 +0100

    Documented the datamining annotations

--- java/com.sap.sse.datamining.shared/src/com/sap/sse/datamining/shared/annotations/Connector.java
+++ java/com.sap.sse.datamining.shared/src/com/sap/sse/datamining/shared/annotations/Connector.java
@@ -7,19 +7,39 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Methods marked with this annotation will be used as function for the data mining framework. Functions can be used by the user to
- * create new functions and dimensions.<br />
- * The marked method has to be <b>side effect free</b>.<br />
+ * Methods marked with this annotation indicate, that the result type contains marked methods.<br />
+ * The marked method has to match the following conditions or the data mining could fail:
+ * <ul>
+ *      <li>Has no parameters</li>
+ *      <li>The return type isn't <code>void</code></li>
+ *      <li>Is side effect free</li>
+ * </ul>
+ * 
+ * @author Lennart Hensler (D054527)
  */
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Connector {
     
+    /**
+     * The message key used for internationalization.
+     */
     public String messageKey() default "";
-    
+
+    /**
+     * The ordinal used for the sorting of functions. The default value is {@link Integer#MAX_VALUE}.<br />
+     * <br />
+     * The standard sorting with this ordinal is ascending (The function with ordinal 0 is the first and the
+     * function with <code>Integer.MAX_VALUE</code> is the last in the list).<br />
+     * If there are more than one ordinal in a function (e.g. if the function is an instance of ConcatenatingCompoundFunction),
+     * then the smallest ordinal is the used ordinal.
+     */
     public int ordinal() default Integer.MAX_VALUE;
     
+    /**
+     * If <code>false</code>, the connection won't be used to find {@link Statistic Statistics}.
+     */
     public boolean scanForStatistics() default true;
 
 }
