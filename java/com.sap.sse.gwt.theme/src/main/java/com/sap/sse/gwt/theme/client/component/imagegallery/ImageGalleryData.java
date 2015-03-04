69c109a46b16fdabf39f495612d8202f5a101e48
fmittag <fmittag@gmx.net>
2015-03-04 12:30:33 +0100

    repaired image and video gallery showcase

--- java/com.sap.sse.gwt.theme/src/main/java/com/sap/sse/gwt/theme/client/component/imagegallery/ImageGalleryData.java
+++ java/com.sap.sse.gwt.theme/src/main/java/com/sap/sse/gwt/theme/client/component/imagegallery/ImageGalleryData.java
@@ -1,9 +1,38 @@
 package com.sap.sse.gwt.theme.client.component.imagegallery;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface ImageGalleryData {
-    String getName();
+public class ImageGalleryData {
+    private String name;
+    private List<ImageDescriptor> images;
     
-    List<ImageDescriptor> getImages();
+    public ImageGalleryData() {
+        this("", new ArrayList<ImageDescriptor>());
+    }
+
+    public ImageGalleryData(String name) {
+        this(name, new ArrayList<ImageDescriptor>());
+    }
+
+    public ImageGalleryData(String name, List<ImageDescriptor> images) {
+        this.name = name;
+        this.images = images;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ImageDescriptor> getImages() {
+        return images;
+    }
+
+    public void setImages(List<ImageDescriptor> images) {
+        this.images = images;
+    }
 }
--- java/com.sap.sse.gwt.theme/src/main/java/com/sap/sse/gwt/theme/client/showcase/components/ImageGalleryShowcase.java
+++ java/com.sap.sse.gwt.theme/src/main/java/com/sap/sse/gwt/theme/client/showcase/components/ImageGalleryShowcase.java
@@ -6,6 +6,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.sap.sse.gwt.theme.client.component.imagegallery.ImageGallery;
+import com.sap.sse.gwt.theme.client.component.imagegallery.ImageGalleryData;
 
 public class ImageGalleryShowcase extends Composite {
 
@@ -14,10 +15,12 @@ public class ImageGalleryShowcase extends Composite {
     interface ImageGalleryShowcaseUiBinder extends UiBinder<Widget, ImageGalleryShowcase> {
     }
 
-    @UiField
+    @UiField(provided=true)
     ImageGallery imageGallery;
 
     public ImageGalleryShowcase() {
+        imageGallery = new ImageGallery(new ImageGalleryData("My image gallery"));
+        
         initWidget(uiBinder.createAndBindUi(this));
     }
 }
