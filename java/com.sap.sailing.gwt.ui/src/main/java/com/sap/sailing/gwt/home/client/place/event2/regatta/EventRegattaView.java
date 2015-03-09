898b6bcba7fe8d559aedc973d75a065002daa5e3
Papick Garcia Taboada <pgt@pgt.de>
2015-03-09 14:31:44 +0100

    Initial leaderboard and competitor charts

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event2/regatta/EventRegattaView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event2/regatta/EventRegattaView.java
@@ -3,11 +3,14 @@ package com.sap.sailing.gwt.home.client.place.event2.regatta;
 import com.sap.sailing.gwt.home.client.app.PlaceNavigation;
 import com.sap.sailing.gwt.home.client.place.event2.EventView;
 import com.sap.sailing.gwt.home.client.place.event2.regatta.tabs.RegattaRacesPlace;
+import com.sap.sse.gwt.client.ErrorReporter;
 
 public interface EventRegattaView extends EventView<AbstractEventRegattaPlace, EventRegattaView.Presenter> {
 
     public interface Presenter extends EventView.Presenter {
 
         PlaceNavigation<RegattaRacesPlace> getCurrentRegattaOverviewNavigation();
+
+        ErrorReporter getErrorReporter();
     }
 }
--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event2/regatta/tabs/RegattaCompetitorAnalyticsTabView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event2/regatta/tabs/RegattaCompetitorAnalyticsTabView.java
@@ -1,50 +1,117 @@
 package com.sap.sailing.gwt.home.client.place.event2.regatta.tabs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.sap.sailing.gwt.home.client.place.event.regattaanalytics.RegattaAnalytics;
+import com.sap.sailing.domain.common.DetailType;
+import com.sap.sailing.domain.common.RaceIdentifier;
+import com.sap.sailing.domain.common.RegattaNameAndRaceName;
+import com.sap.sailing.gwt.home.client.place.event.oldcompetitorcharts.OldCompetitorCharts;
+import com.sap.sailing.gwt.home.client.place.event.regattaanalytics.RegattaAnalyticsDataManager;
 import com.sap.sailing.gwt.home.client.place.event2.regatta.EventRegattaView;
 import com.sap.sailing.gwt.home.client.place.event2.regatta.EventRegattaView.Presenter;
 import com.sap.sailing.gwt.home.client.place.event2.regatta.RegattaTabView;
-import com.sap.sailing.gwt.ui.shared.eventview.EventViewDTO;
+import com.sap.sailing.gwt.home.client.shared.placeholder.Placeholder;
+import com.sap.sailing.gwt.ui.client.SailingServiceAsync;
+import com.sap.sailing.gwt.ui.leaderboard.LeaderboardSettings;
+import com.sap.sailing.gwt.ui.leaderboard.LeaderboardSettings.RaceColumnSelectionStrategies;
+import com.sap.sailing.gwt.ui.leaderboard.LeaderboardSettingsFactory;
+import com.sap.sailing.gwt.ui.leaderboard.LeaderboardUrlSettings;
+import com.sap.sse.gwt.client.ErrorReporter;
+import com.sap.sse.gwt.client.async.AsyncActionsExecutor;
+import com.sap.sse.gwt.client.player.Timer;
+import com.sap.sse.gwt.client.player.Timer.PlayModes;
+import com.sap.sse.gwt.client.player.Timer.PlayStates;
+import com.sap.sse.gwt.client.useragent.UserAgentDetails;
+import com.sap.sse.gwt.shared.GwtHttpRequestUtils;
 
 /**
  * Created by pgtaboada on 25.11.14.
  */
-public class RegattaCompetitorAnalyticsTabView extends Composite implements RegattaTabView<RegattaOverviewPlace> {
+public class RegattaCompetitorAnalyticsTabView extends Composite implements
+        RegattaTabView<RegattaCompetitorAnalyticsPlace> {
 
     public RegattaCompetitorAnalyticsTabView() {
 
     }
 
-    @UiField(provided = true)
-    protected RegattaAnalytics regattaAnalytics;
+    @UiField
+    protected OldCompetitorCharts competitorCharts;
+
+    private RegattaAnalyticsDataManager regattaAnalyticsManager;
+    private final UserAgentDetails userAgent = new UserAgentDetails(Window.Navigator.getUserAgent());
+    private final AsyncActionsExecutor asyncActionsExecutor = new AsyncActionsExecutor();
 
     @Override
-    public Class<RegattaOverviewPlace> getPlaceClassForActivation() {
-        return RegattaOverviewPlace.class;
+    public Class<RegattaCompetitorAnalyticsPlace> getPlaceClassForActivation() {
+        return RegattaCompetitorAnalyticsPlace.class;
     }
 
     @Override
-    public void start(RegattaOverviewPlace myPlace, AcceptsOneWidget contentArea) {
+    public void start(RegattaCompetitorAnalyticsPlace myPlace, AcceptsOneWidget contentArea) {
 
-        EventViewDTO event = myPlace.getCtx().getEventDTO();
+        contentArea.setWidget(new Placeholder());
+
         String regattaId = myPlace.getRegattaId();
 
-//        regattaAnalytics = new RegattaAnalytics(event, regattaId, RegattaNavigationTabs.CompetitorAnalytics,
-//                currentPresenter.getTimerForClientServerOffset(), currentPresenter.getHomePlaceNavigator());
-//
-//        // TODO: load leaderboard, as done in RegattaActivity
-//
-//        initWidget(ourUiBinder.createAndBindUi(this));
-//
-//        contentArea.setWidget(this);
-        contentArea.setWidget(new Label());
+        if (regattaId != null && !regattaId.isEmpty()) {
+
+            String leaderboardName = regattaId;
+
+            long delayBetweenAutoAdvancesInMilliseconds = 3000l;
+            Timer autoRefreshTimer = new Timer(PlayModes.Live, PlayStates.Paused,
+                    delayBetweenAutoAdvancesInMilliseconds);
+
+            ErrorReporter errorReporter = currentPresenter.getErrorReporter();
+
+            SailingServiceAsync sailingService = currentPresenter.getSailingService();
+
+            boolean autoExpandLastRaceColumn = GwtHttpRequestUtils.getBooleanParameter(
+                    LeaderboardUrlSettings.PARAM_AUTO_EXPAND_LAST_RACE_COLUMN, false);
+
+            final LeaderboardSettings leaderboardSettings = createLeaderboardSettingsFromURLParameters(Window.Location
+                    .getParameterMap());
+
+            final RaceIdentifier preselectedRace = getPreselectedRace(Window.Location.getParameterMap());
+
+            regattaAnalyticsManager = new RegattaAnalyticsDataManager( //
+                    sailingService, //
+                    asyncActionsExecutor, //
+                    autoRefreshTimer, //
+                    errorReporter, //
+                    userAgent);
+
+            regattaAnalyticsManager.createLeaderboardPanel( //
+                    leaderboardSettings, //
+                    preselectedRace, //
+                    "leaderboardGroupName", // TODO: keep using magic string? ask frank!
+                    leaderboardName, //
+                    true, // this information came from place, now hard coded. check with frank
+                    autoExpandLastRaceColumn);
+
+            List<DetailType> availableDetailsTypes = new ArrayList<DetailType>();
+            DetailType initialDetailType = DetailType.REGATTA_RANK;
+            availableDetailsTypes.add(DetailType.REGATTA_RANK);
+            availableDetailsTypes.add(DetailType.REGATTA_TOTAL_POINTS_SUM);
+            regattaAnalyticsManager.createMultiCompetitorChart(leaderboardName, initialDetailType);
+
+            initWidget(ourUiBinder.createAndBindUi(this));
+
+            competitorCharts.setChart(regattaAnalyticsManager.getMultiCompetitorChart(), availableDetailsTypes,
+                    initialDetailType);
+
+            regattaAnalyticsManager.showCompetitorChart(competitorCharts.getSelectedChartDetailType());
+            contentArea.setWidget(this);
+        }
     }
 
     @Override
@@ -59,8 +126,8 @@ public class RegattaCompetitorAnalyticsTabView extends Composite implements Rega
     private Presenter currentPresenter;
 
     @Override
-    public RegattaOverviewPlace placeToFire() {
-        return new RegattaOverviewPlace(currentPresenter.getCtx());
+    public RegattaCompetitorAnalyticsPlace placeToFire() {
+        return new RegattaCompetitorAnalyticsPlace(currentPresenter.getCtx());
     }
 
     @Override
@@ -68,5 +135,130 @@ public class RegattaCompetitorAnalyticsTabView extends Composite implements Rega
         this.currentPresenter = currentPresenter;
 
     }
+
+    private LeaderboardSettings createLeaderboardSettingsFromURLParameters(Map<String, List<String>> parameterMap) {
+        LeaderboardSettings result;
+        Long refreshIntervalMillis = parameterMap.containsKey(LeaderboardUrlSettings.PARAM_REFRESH_INTERVAL_MILLIS) ? Long
+                .valueOf(parameterMap.get(LeaderboardUrlSettings.PARAM_REFRESH_INTERVAL_MILLIS).get(0)) : null;
+        RaceColumnSelectionStrategies raceColumnSelectionStrategy;
+        final Integer numberOfLastRacesToShow;
+        if (parameterMap.containsKey(LeaderboardUrlSettings.PARAM_NAME_LAST_N)) {
+            raceColumnSelectionStrategy = RaceColumnSelectionStrategies.LAST_N;
+            numberOfLastRacesToShow = Integer
+                    .valueOf(parameterMap.get(LeaderboardUrlSettings.PARAM_NAME_LAST_N).get(0));
+        } else if (isSmallWidth()) {
+            raceColumnSelectionStrategy = RaceColumnSelectionStrategies.LAST_N;
+            int width = Window.getClientWidth();
+            numberOfLastRacesToShow = (width - 275) / 40;
+        } else {
+            raceColumnSelectionStrategy = RaceColumnSelectionStrategies.EXPLICIT;
+            numberOfLastRacesToShow = null;
+        }
+        if (parameterMap.containsKey(LeaderboardUrlSettings.PARAM_RACE_NAME)
+                || parameterMap.containsKey(LeaderboardUrlSettings.PARAM_RACE_DETAIL)
+                || parameterMap.containsKey(LeaderboardUrlSettings.PARAM_LEG_DETAIL)
+                || parameterMap.containsKey(LeaderboardUrlSettings.PARAM_MANEUVER_DETAIL)
+                || parameterMap.containsKey(LeaderboardUrlSettings.PARAM_OVERALL_DETAIL)
+                || parameterMap.containsKey(LeaderboardUrlSettings.PARAM_SHOW_ADDED_SCORES)
+                || parameterMap
+                        .containsKey(LeaderboardUrlSettings.PARAM_SHOW_OVERALL_COLUMN_WITH_NUMBER_OF_RACES_COMPLETED)) {
+            List<DetailType> maneuverDetails = getDetailTypeListFromParamValue(parameterMap
+                    .get(LeaderboardUrlSettings.PARAM_MANEUVER_DETAIL));
+            List<DetailType> raceDetails = getDetailTypeListFromParamValue(parameterMap
+                    .get(LeaderboardUrlSettings.PARAM_RACE_DETAIL));
+            List<DetailType> overallDetails = getDetailTypeListFromParamValue(parameterMap
+                    .get(LeaderboardUrlSettings.PARAM_OVERALL_DETAIL));
+            if (overallDetails.isEmpty()) {
+                overallDetails = Collections.singletonList(DetailType.REGATTA_RANK);
+            }
+            List<DetailType> legDetails = getDetailTypeListFromParamValue(parameterMap
+                    .get(LeaderboardUrlSettings.PARAM_LEG_DETAIL));
+            List<String> namesOfRacesToShow = getStringListFromParamValue(parameterMap
+                    .get(LeaderboardUrlSettings.PARAM_RACE_NAME));
+            boolean showAddedScores = parameterMap.containsKey(LeaderboardUrlSettings.PARAM_SHOW_ADDED_SCORES) ? Boolean
+                    .valueOf(parameterMap.get(LeaderboardUrlSettings.PARAM_SHOW_ADDED_SCORES).get(0)) : false;
+            boolean showOverallColumnWithNumberOfRacesSailedPerCompetitor = parameterMap
+                    .containsKey(LeaderboardUrlSettings.PARAM_SHOW_OVERALL_COLUMN_WITH_NUMBER_OF_RACES_COMPLETED) ? Boolean
+                    .valueOf(parameterMap.get(
+                            LeaderboardUrlSettings.PARAM_SHOW_OVERALL_COLUMN_WITH_NUMBER_OF_RACES_COMPLETED).get(0))
+                    : false;
+            boolean autoExpandPreSelectedRace = parameterMap
+                    .containsKey(LeaderboardUrlSettings.PARAM_AUTO_EXPAND_PRESELECTED_RACE) ? Boolean
+                    .valueOf(parameterMap.get(LeaderboardUrlSettings.PARAM_AUTO_EXPAND_PRESELECTED_RACE).get(0))
+                    : (namesOfRacesToShow != null && namesOfRacesToShow.size() == 1);
+            boolean showCompetitorSailIdColumn = true;
+            boolean showCompetitorFullNameColumn = true;
+            if (parameterMap.containsKey(LeaderboardUrlSettings.PARAM_SHOW_COMPETITOR_NAME_COLUMNS)) {
+                String value = parameterMap.get(LeaderboardUrlSettings.PARAM_SHOW_COMPETITOR_NAME_COLUMNS).get(0);
+                if (value.equals(LeaderboardUrlSettings.COMPETITOR_NAME_COLUMN_FULL_NAME)) {
+                    showCompetitorSailIdColumn = false;
+                } else if (value.equals(LeaderboardUrlSettings.COMPETITOR_NAME_COLUMN_SAIL_ID)) {
+                    showCompetitorFullNameColumn = false;
+                } else if (value.trim().equals("")) {
+                    showCompetitorFullNameColumn = false;
+                    showCompetitorSailIdColumn = false;
+                }
+            }
+            result = new LeaderboardSettings(maneuverDetails, legDetails, raceDetails, overallDetails,
+            /* namesOfRaceColumnsToShow */null, namesOfRacesToShow, numberOfLastRacesToShow, autoExpandPreSelectedRace,
+                    refreshIntervalMillis, /* sort by column */
+                    (namesOfRacesToShow != null && !namesOfRacesToShow.isEmpty()) ? namesOfRacesToShow.get(0) : null,
+                    /* ascending */true, /* updateUponPlayStateChange */raceDetails.isEmpty() && legDetails.isEmpty(),
+                    raceColumnSelectionStrategy, showAddedScores,
+                    showOverallColumnWithNumberOfRacesSailedPerCompetitor, showCompetitorSailIdColumn,
+                    showCompetitorFullNameColumn);
+
+        } else {
+            final List<DetailType> overallDetails = Collections.singletonList(DetailType.REGATTA_RANK);
+            result = LeaderboardSettingsFactory.getInstance()
+                    .createNewDefaultSettings(null, null,
+                    /* overallDetails */overallDetails, null,
+                    /* autoExpandFirstRace */false, refreshIntervalMillis, numberOfLastRacesToShow,
+                            raceColumnSelectionStrategy);
+        }
+        return result;
+    }
+
+    private List<DetailType> getDetailTypeListFromParamValue(List<String> list) {
+        List<DetailType> result = new ArrayList<DetailType>();
+        if (list != null) {
+            for (String entry : list) {
+                try {
+                    result.add(DetailType.valueOf(entry));
+                } catch (IllegalArgumentException e) {
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<String> getStringListFromParamValue(List<String> list) {
+        List<String> result = new ArrayList<String>();
+        if (list != null) {
+            result.addAll(list);
+        }
+        return result;
+    }
+
+    public boolean isSmallWidth() {
+        int width = Window.getClientWidth();
+        return width <= 720;
+    }
+
+    private RaceIdentifier getPreselectedRace(Map<String, List<String>> parameterMap) {
+        RaceIdentifier result;
+        if (parameterMap.containsKey(LeaderboardUrlSettings.PARAM_RACE_NAME)
+                && parameterMap.get(LeaderboardUrlSettings.PARAM_RACE_NAME).size() == 1
+                && parameterMap.containsKey(LeaderboardUrlSettings.PARAM_REGATTA_NAME)
+                && parameterMap.get(LeaderboardUrlSettings.PARAM_REGATTA_NAME).size() == 1) {
+            result = new RegattaNameAndRaceName(parameterMap.get(LeaderboardUrlSettings.PARAM_REGATTA_NAME).get(0),
+                    parameterMap.get(LeaderboardUrlSettings.PARAM_RACE_NAME).get(0));
+        } else {
+            result = null;
+        }
+        return result;
+    }
+
+
 
 }
