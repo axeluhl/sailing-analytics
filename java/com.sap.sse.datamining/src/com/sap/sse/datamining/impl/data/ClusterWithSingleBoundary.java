71efde05d2a2a6ee2d684475dfb933acc3f8709c
Lennart Hensler <lennarthensler@aol.com>
2015-03-09 10:06:12 +0100

    Javadoc for the clusters

--- java/com.sap.sse.datamining/src/com/sap/sse/datamining/impl/data/ClusterWithSingleBoundary.java
+++ java/com.sap.sse.datamining/src/com/sap/sse/datamining/impl/data/ClusterWithSingleBoundary.java
@@ -2,12 +2,22 @@ package com.sap.sse.datamining.impl.data;
 
 import java.util.Arrays;
 
+import com.sap.sse.datamining.data.Cluster;
 import com.sap.sse.datamining.data.ClusterBoundary;
 
 public class ClusterWithSingleBoundary<ElementType> extends AbstractCluster<ElementType> {
 
     private static final char INFINITE = '\u221e';
 
+    /**
+     * A {@link Cluster} with a single boundary. The other boundary will be interpreted as
+     * infinite or -infinite, depending on the {@link ComparisonStrategy} of the given
+     * {@link ClusterBoundary}. So this <code>Cluster</code> contains all elements, that are
+     * contained by the given <code>ClusterBoundary</code>.
+     * 
+     * @param messageKey the key used for internationalization
+     * @param boundary the boundary used to define the range
+     */
     public ClusterWithSingleBoundary(String messageKey, ClusterBoundary<ElementType> boundary) {
         super(messageKey, Arrays.asList(boundary));
     }
