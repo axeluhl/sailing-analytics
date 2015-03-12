d5baadf39a30d61d4c2fbb6569394bc598c325d7
Lennart Hensler <LennartHensler@aol.com>
2015-03-12 14:07:33 +0100

    Added decimals to the statistic relative rank in race

--- java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/data/HasRaceResultOfCompetitorContext.java
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/data/HasRaceResultOfCompetitorContext.java
@@ -16,7 +16,7 @@ public interface HasRaceResultOfCompetitorContext {
     /**
      * 0 means the competitor won the race, 1 means the competitor ranked last
      */
-    @Statistic(messageKey="RelativeRankInRace", ordinal=3)
+    @Statistic(messageKey="RelativeRankInRace", ordinal=3, resultDecimals=2)
     public double getRelativeRank();
 
     @Dimension(messageKey="WindSpeedInBeaufort")
