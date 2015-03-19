dee326c71c4434e8dfdf86d56fe43c4b46f9d2e6
Bennet Brunsen <brunsen@ubilabs.net>
2015-03-19 15:56:31 +0100

    Implemented StartActivity and partially HomeFragment
    Implemented StartActivity and partially HomeFragment. Additional
    extractions.

---
+++ mobile/com.sap.sailing.buoy.positioning/src/com/sap/sailing/android/buoy/positioning/app/ui/fragments/HomeFragment.java
@@ -0,0 +1,153 @@
+package com.sap.sailing.android.buoy.positioning.app.ui.fragments;
+
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.sap.sailing.android.buoy.positioning.app.R;
+import com.sap.sailing.android.buoy.positioning.app.adapter.RegattaAdapter;
+import com.sap.sailing.android.buoy.positioning.app.ui.activities.RegattaActivity;
+import com.sap.sailing.android.buoy.positioning.app.ui.activities.StartActivity;
+import com.sap.sailing.android.buoy.positioning.app.util.AppPreferences;
+import com.sap.sailing.android.shared.data.AbstractCheckinData;
+import com.sap.sailing.android.shared.logging.ExLog;
+import com.sap.sailing.android.shared.util.NetworkHelper.NetworkHelperError;
+import com.sap.sailing.android.shared.util.NetworkHelper.NetworkHelperFailureListener;
+import com.sap.sailing.android.shared.util.NetworkHelper.NetworkHelperSuccessListener;
+import com.sap.sailing.android.ui.fragments.AbstractHomeFragment;
+
+public class HomeFragment extends AbstractHomeFragment implements LoaderCallbacks<Cursor> {
+
+	private final static String TAG = HomeFragment.class.getName();
+
+	@SuppressLint("InflateParams")
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+
+		prefs = new AppPreferences(getActivity());
+
+		ListView listView = (ListView) view.findViewById(R.id.listRegatta);
+		if (listView != null) {
+			listView.addHeaderView(inflater.inflate(
+					R.layout.regatta_listview_header, null));
+
+			adapter = new RegattaAdapter(getActivity(),
+					R.layout.ragatta_listview_row, null, 0);
+			listView.setAdapter(adapter);
+			listView.setOnItemClickListener(new ItemClickListener());
+		}
+
+		getLoaderManager().initLoader(REGATTA_LOADER, null, this);
+
+		return view;
+	}
+	
+	@Override
+    public void onResume() {
+        super.onResume();
+        getLoaderManager().restartLoader(REGATTA_LOADER, null, this);
+
+        String lastQRCode = prefs.getLastScannedQRCode();
+        if (lastQRCode != null) {
+            handleQRCode(lastQRCode);
+        }
+    }
+
+	@Override
+	public void displayUserConfirmationScreen(AbstractCheckinData data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleScannedOrUrlMatchedUri(Uri uri) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+     * Start regatta activity.
+     * 
+     * @param checkinDigest
+     */
+    private void startRegatta(String checkinDigest) {
+        Intent intent = new Intent(getActivity(), RegattaActivity.class);
+        intent.putExtra(getString(R.string.checkin_digest), checkinDigest);
+        getActivity().startActivity(intent);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderId, Bundle bundle) {
+        switch (loaderId) {
+        case REGATTA_LOADER:
+            String[] projection = new String[] { "events.event_checkin_digest", "events.event_id", "events._id",
+                    "events.event_name", "events.event_server", "competitors.competitor_display_name",
+                    "competitors.competitor_id", "leaderboards.leaderboard_name",
+                    "competitors.competitor_country_code", "competitors.competitor_sail_id" };
+            return new CursorLoader(getActivity(), AnalyticsContract.EventLeaderboardCompetitorJoined.CONTENT_URI,
+                    projection, null, null, null);
+
+        default:
+            return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        switch (loader.getId()) {
+        case REGATTA_LOADER:
+            adapter.changeCursor(cursor);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        switch (loader.getId()) {
+        case REGATTA_LOADER:
+            adapter.changeCursor(null);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    private class ItemClickListener implements OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            if (position < 1) // tapped header
+            {
+                return;
+            }
+
+            // -1, because there's a header row
+            Cursor cursor = (Cursor) adapter.getItem(position - 1);
+
+            String checkinDigest = cursor.getString(cursor.getColumnIndex("event_checkin_digest"));
+            startRegatta(checkinDigest);
+        }
+    }
+
+}
---
+++ mobile/com.sap.sailing.buoy.positioning/src/com/sap/sailing/android/buoy/positioning/app/valueobjects/CheckinData.java
@@ -0,0 +1,34 @@
+package com.sap.sailing.android.buoy.positioning.app.valueobjects;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import com.sap.sailing.android.shared.data.AbstractCheckinData;
+import com.sap.sailing.android.shared.data.LeaderboardInfo;
+
+public class CheckinData extends AbstractCheckinData{
+	public String leaderboardName;
+	public String deviceUid;
+	public String uriString;
+	public String checkinDigest;
+
+	public void setCheckinDigestFromString(String checkinString)
+			throws UnsupportedEncodingException, NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(checkinString.getBytes("UTF-8"));
+		byte[] digest = md.digest();
+		StringBuffer buf = new StringBuffer();
+		for (byte byt : digest) {
+			buf.append(Integer.toString((byt & 0xff) + 0x100, 16).substring(1));
+		}
+		checkinDigest = buf.toString();
+	}
+	
+	public LeaderboardInfo getLeaderboard() {
+        LeaderboardInfo leaderboard = new LeaderboardInfo();
+        leaderboard.name = leaderboardName;
+        leaderboard.checkinDigest = checkinDigest;
+        return leaderboard;
+    }
+}
