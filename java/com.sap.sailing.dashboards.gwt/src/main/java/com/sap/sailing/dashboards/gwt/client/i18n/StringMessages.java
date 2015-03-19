cb2ab1b35c10035f2e6dc5ea7b78d7f254e0f9d6
fmittag <fmittag@gmx.net>
2015-03-19 17:29:27 +0100

    let dashboard stringmessages extends gwt.ui.client stringmessages

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/i18n/StringMessages.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/i18n/StringMessages.java
@@ -2,7 +2,7 @@ package com.sap.sailing.dashboards.gwt.client.i18n;
 
 import com.google.gwt.core.client.GWT;
 
-public interface StringMessages extends com.sap.sse.gwt.client.StringMessages {
+public interface StringMessages extends com.sap.sailing.gwt.ui.client.StringMessages {
     public static final StringMessages INSTANCE = GWT.create(StringMessages.class);
 
     String sampleText();
