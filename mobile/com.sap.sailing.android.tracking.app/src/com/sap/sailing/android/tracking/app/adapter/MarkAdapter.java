a0c366c429b892316922600b9be76ada31fd67e8
Bennet Brunsen <brunsen@ubilabs.net>
2015-03-19 11:12:51 +0100

    Created Project

--- mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/adapter/MarkAdapter.java
+++ mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/adapter/MarkAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.ResourceCursorAdapter;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.sap.sailing.android.tracking.app.R;
