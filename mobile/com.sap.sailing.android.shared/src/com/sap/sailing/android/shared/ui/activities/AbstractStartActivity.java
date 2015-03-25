a3403881a156ebb45c8bf3afdcf4d4444512e110
Axel Uhl <axel.uhl@sap.com>
2015-03-25 20:59:51 +0100

    removed Hockey dependency; removed Volley from pom.xml; added org.json.simple dependency

--- mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/activities/AbstractStartActivity.java
+++ mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/activities/AbstractStartActivity.java
@@ -1,6 +1,5 @@
 package com.sap.sailing.android.shared.ui.activities;
 
-import net.hockeyapp.android.CrashManager;
 import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
@@ -15,11 +14,11 @@ import com.sap.sailing.android.shared.data.AbstractCheckinData;
 import com.sap.sailing.android.shared.logging.ExLog;
 import com.sap.sailing.android.ui.fragments.AbstractHomeFragment;
 
-public abstract class AbstractStartActivity extends CheckinDataActivity{
+public abstract class AbstractStartActivity extends CheckinDataActivity {
 
-	private final static String TAG = AbstractStartActivity.class.getName();
-	
-	@Override
+    private final static String TAG = AbstractStartActivity.class.getName();
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_container);
@@ -31,56 +30,46 @@ public abstract class AbstractStartActivity extends CheckinDataActivity{
             toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
         }
     }
-	
-	@Override
-	public void onStart() {
-    	super.onStart();
 
-    	// get url if launched via url intent-filter
-       
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // get url if launched via url intent-filter
+
         Intent intent = getIntent();
         Uri uri = intent.getData();
 
-		if (uri != null) {
-			if (BuildConfig.DEBUG) {
-				ExLog.i(this, TAG, "Matched URL, handling scanned or matched URL.");
-			}
-			
-			getHomeFragment().handleScannedOrUrlMatchedUri(uri);
-			
-		}
-        
+        if (uri != null) {
+            if (BuildConfig.DEBUG) {
+                ExLog.i(this, TAG, "Matched URL, handling scanned or matched URL.");
+            }
+
+            getHomeFragment().handleScannedOrUrlMatchedUri(uri);
+
+        }
+
         intent.setData(null);
-	}
-	
-	@Override
+    }
+
+    @Override
     public void onCheckinDataAvailable(AbstractCheckinData data) {
-        if(data != null)
-        {
+        if (data != null) {
             getHomeFragment().displayUserConfirmationScreen(data);
         }
     }
-	
-	public abstract AbstractHomeFragment getHomeFragment();
-	
-	@Override
+
+    public abstract AbstractHomeFragment getHomeFragment();
+
+    @Override
     protected void onResume() {
-    	super.onResume();
-    	checkForCrashes();
-        //checkForUpdates();
-        
-    	int googleServicesResultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-    	if (googleServicesResultCode != ConnectionResult.SUCCESS)
-    	{
-    		Dialog dialog = GooglePlayServicesUtil.getErrorDialog(googleServicesResultCode, this, 0);
-    		dialog.show();
-    	}
+        super.onResume();
+        // checkForUpdates();
+
+        int googleServicesResultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (googleServicesResultCode != ConnectionResult.SUCCESS) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(googleServicesResultCode, this, 0);
+            dialog.show();
+        }
     }
-	
-    /**
-     * Hockeyapp integration method.
-     */
-	private void checkForCrashes() {
-		CrashManager.register(this, "060ff0c8a907638e3b31d3146091c87b");
-	}
 }
