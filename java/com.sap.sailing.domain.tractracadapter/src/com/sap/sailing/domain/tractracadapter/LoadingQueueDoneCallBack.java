0d360ace6bb8678bee883a98812ef110388c72fb
Frederik Petersen <frederik.petersen@sap.com>
2015-03-25 15:05:09 +0100

    More comments concerning new tractrac receiver callback

--- java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/LoadingQueueDoneCallBack.java
+++ java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/LoadingQueueDoneCallBack.java
@@ -1,8 +1,20 @@
 package com.sap.sailing.domain.tractracadapter;
 
+import com.sap.sailing.domain.tracking.TrackedRace;
 import com.sap.sailing.domain.tractracadapter.impl.AbstractReceiverWithQueue;
 import com.sap.sailing.domain.tractracadapter.impl.TracTracRaceTrackerImpl;
 
+/**
+ * Callback interface which is currently used, so that {@link Receiver}s can be told to callback when current queue
+ * content is worked through.
+ * 
+ * When loading -> tracking or loading -> finished would happen, we want to wait for all the current events in the queue
+ * to be handled before notifying the {@link TrackedRace}. This will ensure that caches, polar miner, etc. will not be
+ * resumed/started, as long as loading events are handled.
+ * 
+ * @author Frederik Petersen
+ *
+ */
 public interface LoadingQueueDoneCallBack {
 
     /**
