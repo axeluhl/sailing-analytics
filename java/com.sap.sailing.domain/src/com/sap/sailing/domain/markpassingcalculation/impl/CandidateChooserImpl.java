axeluhl/sailing-analytics

commit 7d3b339ad62e3e628d5bf799a48191d59b9c1ba8

introducing a cache for approximate distances between waypoints/control points to speed up the mark rounding inference algorithm; see bug 2745

java/com.sap.sailing.domain/src/com/sap/sailing/domain/markpassingcalculation/impl/CandidateChooserImpl.java
@@ -55,6 +55,7 @@ public class CandidateChooserImpl implements CandidateChooser {
     private Map<Competitor, NavigableSet<Candidate>> fixedPassings = new HashMap<>();
     private Map<Competitor, Integer> suppressedPassings = new HashMap<>();
     private TimePoint raceStartTime;
+    private final WaypointPositionAndDistanceCache waypointPositionAndDistanceCache;
     
     /**
      * An artificial proxy candidate that comes before the start mark passing. Its time point is set to
@@ -67,6 +68,7 @@ public class CandidateChooserImpl implements CandidateChooser {
 
     public CandidateChooserImpl(DynamicTrackedRace race) {
         this.race = race;
+        waypointPositionAndDistanceCache = new WaypointPositionAndDistanceCache(race, Duration.ONE_MINUTE);
         raceStartTime = race.getStartOfRace() != null ? race.getStartOfRace().minus(MILLISECONDS_BEFORE_STARTTIME) : null;
         start = new CandidateWithSettableTime(/* Index */0, raceStartTime, /* Probability */1, /* Waypoint */null);
         end = new CandidateWithSettableWaypointIndex(race.getRace().getCourse()
@@ -372,7 +374,7 @@ public class CandidateChooserImpl implements CandidateChooser {
             first = c1.getWaypoint();
         }
         final Waypoint second = c2.getWaypoint();
-        Distance totalGreatCircleDistance = getTotalGreatCircleDistanceBetweenWaypoints(first, second, middleOfc1Andc2);
+        Distance totalGreatCircleDistance = getApproximateTotalGreatCircleDistanceBetweenWaypoints(first, second, middleOfc1Andc2);
         Distance actualDistanceTraveled = race.getTrack(c).getDistanceTraveled(c1.getTimePoint(), c2.getTimePoint());
         result = getProbabilityOfActualDistanceGivenGreatCircleDistance(totalGreatCircleDistance, actualDistanceTraveled);
         return result;
@@ -412,7 +414,7 @@ public class CandidateChooserImpl implements CandidateChooser {
         return result;
     }
 
-    private Distance getTotalGreatCircleDistanceBetweenWaypoints(Waypoint first, final Waypoint second,
+    private Distance getApproximateTotalGreatCircleDistanceBetweenWaypoints(Waypoint first, final Waypoint second,
             final TimePoint timePoint) {
         Distance totalGreatCircleDistance = new MeterDistance(0);
         boolean legsAreBetweenCandidates = false;
@@ -425,7 +427,7 @@ public class CandidateChooserImpl implements CandidateChooser {
                 legsAreBetweenCandidates = true;
             }
             if (legsAreBetweenCandidates) {
-                totalGreatCircleDistance = totalGreatCircleDistance.add(leg.getGreatCircleDistance(timePoint));
+                totalGreatCircleDistance = waypointPositionAndDistanceCache.getApproximateDistance(from, leg.getLeg().getTo(), timePoint);
             }
         }
         return totalGreatCircleDistance;

java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/markpassing/impl/WaypointPositionAndDistanceCacheTest.java
@@ -1,0 +1,95 @@
+package com.sap.sailing.domain.markpassing.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.UUID;
+import java.util.function.Function;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sap.sailing.domain.base.ControlPointWithTwoMarks;
+import com.sap.sailing.domain.base.Course;
+import com.sap.sailing.domain.base.DomainFactory;
+import com.sap.sailing.domain.base.Mark;
+import com.sap.sailing.domain.base.RaceDefinition;
+import com.sap.sailing.domain.base.Regatta;
+import com.sap.sailing.domain.base.Waypoint;
+import com.sap.sailing.domain.base.impl.ControlPointWithTwoMarksImpl;
+import com.sap.sailing.domain.base.impl.CourseImpl;
+import com.sap.sailing.domain.base.impl.MarkImpl;
+import com.sap.sailing.domain.base.impl.WaypointImpl;
+import com.sap.sailing.domain.common.impl.DegreePosition;
+import com.sap.sailing.domain.markpassingcalculation.impl.WaypointPositionAndDistanceCache;
+import com.sap.sailing.domain.racelog.tracking.EmptyGPSFixStore;
+import com.sap.sailing.domain.tracking.DynamicTrackedRace;
+import com.sap.sailing.domain.tracking.TrackedRegatta;
+import com.sap.sailing.domain.tracking.impl.DynamicTrackedRaceImpl;
+import com.sap.sailing.domain.tracking.impl.EmptyWindStore;
+import com.sap.sailing.domain.tracking.impl.GPSFixImpl;
+import com.sap.sse.common.Duration;
+import com.sap.sse.common.TimePoint;
+import com.sap.sse.common.impl.MillisecondsTimePoint;
+
+public class WaypointPositionAndDistanceCacheTest {
+    private WaypointPositionAndDistanceCache cache;
+    private int recalculations;
+    private DynamicTrackedRace trackedRace;
+    private TimePoint now;
+    private Waypoint start;
+    private Waypoint windwardWaypoint;
+    private Waypoint finish;
+    
+    @Before
+    public void setUp() {
+        final RaceDefinition race = mock(RaceDefinition.class);
+        when(race.getCompetitors()).thenReturn(Collections.emptyList());
+        when(race.getBoatClass()).thenReturn(DomainFactory.INSTANCE.getOrCreateBoatClass("29er"));
+        Mark pinEnd = new MarkImpl("Pin End");
+        Mark startBoat = new MarkImpl("Start Boat");
+        Mark windward = new MarkImpl("Windward");
+        ControlPointWithTwoMarks startFinish = new ControlPointWithTwoMarksImpl(UUID.randomUUID(), pinEnd, startBoat, "Start/Finish");
+        start = new WaypointImpl(startFinish);
+        windwardWaypoint = new WaypointImpl(windward);
+        finish = new WaypointImpl(startFinish);
+        Course course = new CourseImpl("Course name", Arrays.asList(start, windwardWaypoint, finish));
+        when(race.getCourse()).thenReturn(course);
+        final TrackedRegatta trackedRegatta = mock(TrackedRegatta.class);
+        final Regatta regatta = mock(Regatta.class);
+        when(regatta.getName()).thenReturn("Regatta name");
+        when(trackedRegatta.getRegatta()).thenReturn(regatta);
+        trackedRace = new DynamicTrackedRaceImpl(trackedRegatta, race, Collections.emptyList(), EmptyWindStore.INSTANCE, EmptyGPSFixStore.INSTANCE,
+                /* delayToLiveInMillis */ 8000, /* millisecondsOverWhichToAverageWind */ 30000,
+                /* millisecondsOverWhichToAverageSpeed */ 15000, /* delayForCacheInvalidationOfWindEstimation */ 10000,
+                /* useInternalMarkPassingAlgorithm */ false);
+        now = MillisecondsTimePoint.now();
+        trackedRace.getOrCreateTrack(pinEnd).addGPSFix(new GPSFixImpl(new DegreePosition(0, -0.0000001), now));
+        trackedRace.getOrCreateTrack(startBoat).addGPSFix(new GPSFixImpl(new DegreePosition(0, 0.0000001), now));
+        trackedRace.getOrCreateTrack(windward).addGPSFix(new GPSFixImpl(new DegreePosition(1, 0), now));
+        recalculations = 0;
+        cache = new WaypointPositionAndDistanceCache(trackedRace, /* timeRangeResolution */ Duration.ONE_MINUTE) {
+            @Override
+            protected <R> R computeResult(Function<TimePoint, R> resultCalculator, TimePoint roundedToTimeRangeCenter) {
+                recalculations++;
+                return super.computeResult(resultCalculator, roundedToTimeRangeCenter);
+            }
+        };
+    }
+    
+    @Test
+    public void simpleTest() {
+        assertEquals(60, cache.getApproximateDistance(start, windwardWaypoint, now).getNauticalMiles(), 0.01);
+        assertEquals(3, recalculations); // one for the position of each waypoint, one for the distance
+    }
+
+    @Test
+    public void testBackAndForth() {
+        assertEquals(60, cache.getApproximateDistance(start, windwardWaypoint, now).getNauticalMiles(), 0.01);
+        assertEquals(60, cache.getApproximateDistance(windwardWaypoint, start, now).getNauticalMiles(), 0.01);
+        assertEquals(3, recalculations); // one for the position of each waypoint, one for the distance which creates two symmetrical cache entries
+    }
+}

java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/TrackTest.java
@@ -55,6 +55,7 @@ import com.sap.sailing.domain.tracking.impl.GPSFixMovingImpl;
 import com.sap.sailing.domain.tracking.impl.MaxSpeedCache;
 import com.sap.sailing.domain.tracking.impl.TrackImpl;
 import com.sap.sse.common.TimePoint;
+import com.sap.sse.common.TimeRange;
 import com.sap.sse.common.Util;
 import com.sap.sse.common.Util.Pair;
 import com.sap.sse.common.impl.MillisecondsTimePoint;
@@ -1069,10 +1070,10 @@ public class TrackTest {
                 speed.travel(firstFixSoFar.getTimePoint(), firstFixSoFar.getTimePoint().plus(timespan)));
         GPSFixMoving newFirstFix = new GPSFixMovingImpl(newPosition, slightlyBeforeFirstFix, speed);
         track.addGPSFix(newFirstFix);
-        com.sap.sse.common.Util.Pair<TimePoint, TimePoint> intervalAffected = track.getEstimatedPositionTimePeriodAffectedBy(newFirstFix);
+        TimeRange intervalAffected = track.getEstimatedPositionTimePeriodAffectedBy(newFirstFix);
         Position newPositionAtBeginningOfTime = track.getEstimatedPosition(beginningOfTime, /* extrapolate */false);
         assertFalse(newPositionAtBeginningOfTime.equals(positionAtBeginningOfTime));
-        assertTrue(!intervalAffected.getA().after(beginningOfTime));
+        assertTrue(!intervalAffected.from().after(beginningOfTime));
     }
 
     @Test
@@ -1088,10 +1089,10 @@ public class TrackTest {
                 speed.travel(lastFixSoFar.getTimePoint(), lastFixSoFar.getTimePoint().plus(timespan)));
         GPSFixMoving newLastFix = new GPSFixMovingImpl(newPosition, slightlyAfterLastFix, speed);
         track.addGPSFix(newLastFix);
-        com.sap.sse.common.Util.Pair<TimePoint, TimePoint> intervalAffected = track.getEstimatedPositionTimePeriodAffectedBy(newLastFix);
+        TimeRange intervalAffected = track.getEstimatedPositionTimePeriodAffectedBy(newLastFix);
         Position newPositionAtEndOfTime = track.getEstimatedPosition(endOfTime, /* extrapolate */false);
         assertFalse(newPositionAtEndOfTime.equals(positionAtEndOfTime));
-        assertTrue(!intervalAffected.getB().before(endOfTime));
+        assertTrue(!intervalAffected.to().before(endOfTime));
     }
 
     @Test
@@ -1106,13 +1107,13 @@ public class TrackTest {
         assertNull(positionAtEndOfTime);
         GPSFixMoving newFix = new GPSFixMovingImpl(new DegreePosition(12, 34), now, new KnotSpeedWithBearingImpl(12, new DegreeBearingImpl(123)));
         myTrack.addGPSFix(newFix);
-        com.sap.sse.common.Util.Pair<TimePoint, TimePoint> intervalAffected = myTrack.getEstimatedPositionTimePeriodAffectedBy(newFix);
+        TimeRange intervalAffected = myTrack.getEstimatedPositionTimePeriodAffectedBy(newFix);
         Position newPositionAtBeginningOfTime = myTrack.getEstimatedPosition(beginningOfTime, /* extrapolate */false);
         Position newPositionAtEndOfTime = myTrack.getEstimatedPosition(now, /* extrapolate */false);
         assertFalse(newPositionAtBeginningOfTime.equals(positionAtBeginningOfTime));
-        assertTrue(!intervalAffected.getA().after(beginningOfTime));
+        assertTrue(!intervalAffected.from().after(beginningOfTime));
         assertFalse(newPositionAtEndOfTime.equals(positionAtEndOfTime));
-        assertTrue(!intervalAffected.getB().before(endOfTime));
+        assertTrue(!intervalAffected.to().before(endOfTime));
     }
 
     @Test

java/com.sap.sailing.domain/src/com/sap/sailing/domain/markpassingcalculation/impl/CandidateFinderImpl.java
@@ -33,6 +33,7 @@ import com.sap.sailing.domain.tracking.GPSFix;
 import com.sap.sailing.domain.tracking.GPSFixMoving;
 import com.sap.sailing.domain.tracking.GPSFixTrack;
 import com.sap.sse.common.TimePoint;
+import com.sap.sse.common.TimeRange;
 import com.sap.sse.common.Util;
 
 /**
@@ -119,10 +120,10 @@ public class CandidateFinderImpl implements CandidateFinder {
         TimePoint end = null;
         for (Entry<Mark, List<GPSFix>> fixes : markFixes.entrySet()) {
             for (GPSFix fix : fixes.getValue()) {
-                Util.Pair<TimePoint, TimePoint> timePoints = race.getOrCreateTrack(fixes.getKey())
+                TimeRange timePoints = race.getOrCreateTrack(fixes.getKey())
                         .getEstimatedPositionTimePeriodAffectedBy(fix);
-                TimePoint newStart = timePoints.getA();
-                TimePoint newEnd = timePoints.getB();
+                TimePoint newStart = timePoints.from();
+                TimePoint newEnd = timePoints.to();
                 start = start == null || start.after(newStart) ? newStart : start;
                 end = end == null || end.before(newEnd) ? newEnd : end;
             }

java/com.sap.sailing.domain/src/com/sap/sailing/domain/markpassingcalculation/impl/WaypointPositionAndDistanceCache.java
@@ -1,0 +1,226 @@
+package com.sap.sailing.domain.markpassingcalculation.impl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import com.sap.sailing.domain.base.Course;
+import com.sap.sailing.domain.base.Mark;
+import com.sap.sailing.domain.base.Waypoint;
+import com.sap.sailing.domain.common.Distance;
+import com.sap.sailing.domain.common.Position;
+import com.sap.sailing.domain.tracking.GPSFix;
+import com.sap.sailing.domain.tracking.GPSFixTrack;
+import com.sap.sailing.domain.tracking.TrackedRace;
+import com.sap.sailing.domain.tracking.impl.AbstractRaceChangeListener;
+import com.sap.sse.common.Duration;
+import com.sap.sse.common.TimePoint;
+import com.sap.sse.common.TimeRange;
+import com.sap.sse.common.Util;
+import com.sap.sse.common.Util.Pair;
+import com.sap.sse.common.impl.MillisecondsTimePoint;
+import com.sap.sse.concurrent.ConcurrentWeakHashMap;
+
+/**
+ * A cache for approximate waypoint positions that has a configurable {@link TimeRange} resolution. Requests that fall
+ * into the same time range will be responded to using the previous response for the middle of that time range. Time
+ * ranges are arranged not to overlap for the same waypoint by putting the center of each time range to a whole multiple
+ * of the resolution, starting at Unix-time 0.
+ * <p>
+ * 
+ * The cache is weak for the {@link Waypoint} objects, so if a course change eliminates a waypoint, this cache will
+ * eventually let go of the position records for those waypoints.
+ * <p>
+ * 
+ * The cache listens for mark fixes, indicating new knowledge about a mark's position. All waypoints referencing this
+ * mark will have their cache entries removed for those time ranges that have an overlap with the time range affected by
+ * the new mark fix.
+ * 
+ * @author Axel Uhl (D043530)
+ *
+ */
+public class WaypointPositionAndDistanceCache {
+    /**
+     * Weak keys are the waypoints; values are time-ordered maps mapping the center of a time range
+     * to the approximate waypoint position at that time.
+     */
+    private final ConcurrentWeakHashMap<Waypoint, Map<TimePoint, Position>> waypointPositionCache;
+    
+    /**
+     * Caches distances between the positions cached in {@link #waypointPositionCache}. As soon as an entry
+     * is invalidated in {@link #waypointPositionCache}, it is also invalidated here. Keys always occur
+     * symmetrically under the map's monitor being held, i.e., if (w1, w2) is a cache key then so is
+     * (w2, w1), with equal value.
+     */
+    private final ConcurrentHashMap<Pair<Waypoint, Waypoint>, Map<TimePoint, Distance>> distanceCache;
+    
+    /**
+     * The duration of the time ranges whose center time points serve as keys for the {@link NavigableMap}s used as
+     * values in the {@link #waypointPositionCache} structure. Each cache entry extends half this duration into the past and into the
+     * future, or more formally: if <code>t</code> is the time point marking the center of the time range, the time
+     * range for the cache entry is defined as <code>[t-timeRangeResolution/2, t+timeRangeResolution/2]</code> if the
+     * millisecond representation of this duration is even, and
+     * <code>[t-timeRangeResolution/2-1, t+timeRangeResolution/2]</code> for odd durations.
+     */
+    private final Duration timeRangeResolution;
+
+    private final TrackedRace trackedRace;
+    
+    private final List<Waypoint> waypoints;
+    
+    public WaypointPositionAndDistanceCache(TrackedRace race, Duration timeRangeResolution) {
+        this.trackedRace = race;
+        this.waypointPositionCache = new ConcurrentWeakHashMap<>();
+        this.distanceCache = new ConcurrentHashMap<>();
+        this.timeRangeResolution = timeRangeResolution;
+        this.waypoints = Collections.synchronizedList(new ArrayList<>());
+        final Course course = this.trackedRace.getRace().getCourse();
+        course.lockForRead(); // obtain waypoints list copy and register listener atomically so as to not miss any updates
+        try {
+            Util.addAll(course.getWaypoints(), this.waypoints);
+            this.trackedRace.addListener(new AbstractRaceChangeListener() {
+                @Override
+                public void markPositionChanged(GPSFix fix, Mark mark, boolean firstInTrack) {
+                    invalidate(mark, fix);
+                }
+
+                @Override
+                public void waypointAdded(int zeroBasedIndex, Waypoint waypointThatGotAdded) {
+                    waypoints.add(zeroBasedIndex, waypointThatGotAdded);
+                }
+
+                @Override
+                public void waypointRemoved(int zeroBasedIndex, Waypoint waypointThatGotRemoved) {
+                    assert waypoints.get(zeroBasedIndex) == waypointThatGotRemoved;
+                    waypoints.remove(zeroBasedIndex);
+                }
+            });
+        } finally {
+            course.unlockAfterRead();
+        }
+    }
+    
+    public Position getApproximatePosition(Waypoint waypoint, TimePoint timePoint) {
+        return getApproximateResult(waypoint, timePoint, waypointPositionCache,
+                roundedToTimeRangeCenter->trackedRace.getApproximatePosition(waypoint, roundedToTimeRangeCenter),
+                /* alternateKeySupplier */ null);
+    }
+    
+    /**
+     * @param resultCalculator
+     *            function that computes the result to be cached, assuming the caller knows the key which it just passed
+     *            to this method
+     * @param alternateKeySupplier
+     *            if a cache entry was computed using <code>resultCalculator</code>, it can optionally be stored under
+     *            the additional key supplied by this object. If the supplier is <code>null</code> or supplies <code>null</code>,
+     *            no additional cache entry except one for the standard <code>key</code> will be produced
+     */
+    private <K, R> R getApproximateResult(K cacheKey, TimePoint timePoint, Map<K, Map<TimePoint, R>> cacheMap,
+            Function<TimePoint, R> resultCalculator, Supplier<K> alternateKeySupplier) {
+        Map<TimePoint, R> map = cacheMap.get(cacheKey);
+        if (map == null) {
+            map = Collections.synchronizedMap(new HashMap<TimePoint, R>());
+            cacheMap.put(cacheKey, map);
+        }
+        final TimePoint roundedToTimeRangeCenter = roundToResolution(timePoint);
+        final R cachedResult = map.get(roundedToTimeRangeCenter);
+        final R result;
+        if (cachedResult == null) {
+            result = computeResult(resultCalculator, roundedToTimeRangeCenter);
+            map.put(roundedToTimeRangeCenter, result);
+            K alternateKey;
+            if (alternateKeySupplier != null && (alternateKey=alternateKeySupplier.get()) != null) {
+                Map<TimePoint, R> mapForAlternateKey = cacheMap.get(alternateKey);
+                if (mapForAlternateKey == null) {
+                    mapForAlternateKey = Collections.synchronizedMap(new HashMap<TimePoint, R>());
+                    cacheMap.put(alternateKey, mapForAlternateKey);
+                }
+                mapForAlternateKey.put(roundedToTimeRangeCenter, result);
+            }
+        } else {
+            result = cachedResult;
+        }
+        return result;
+    }
+
+    /**
+     * This trivial method exists for the sake of testability. A test can subclass and redefine to easily intercept and
+     * take note that a re-calculation actually took place.
+     */
+    protected <R> R computeResult(Function<TimePoint, R> resultCalculator, final TimePoint roundedToTimeRangeCenter) {
+        return resultCalculator.apply(roundedToTimeRangeCenter);
+    }
+    
+    public Distance getApproximateDistance(Waypoint w1, Waypoint w2, TimePoint timePoint) {
+        return getApproximateResult(new Pair<>(w1, w2), timePoint, distanceCache,
+                roundedToTimeRangeCenter->getApproximatePosition(w1, roundedToTimeRangeCenter).getDistance(getApproximatePosition(w2, roundedToTimeRangeCenter)),
+                /* alternate key supplier */ ()->new Pair<>(w2, w1));
+    }
+    
+    /**
+     * Invalidates those elements of the cache affected by the position fix for the mark. This considers for which time
+     * range the {@link GPSFixTrack#getEstimatedPosition(TimePoint, boolean)} result will be affected by the fix and checks
+     * which cache entries overlap. It invalidates the cache entries for those waypoints using the <code>mark</code>.
+     */
+    private void invalidate(Mark mark, GPSFix fix) {
+        TimeRange affectedTimeRange = trackedRace.getOrCreateTrack(mark).getEstimatedPositionTimePeriodAffectedBy(fix);
+        synchronized (waypoints) { // it's a synchronized list, so grabbing the list's monitor will grant us exclusive access here
+            for (Waypoint waypoint : waypoints) {
+                if (Util.contains(waypoint.getMarks(), mark)) {
+                    invalidate(waypoint, affectedTimeRange);
+                }
+            }
+        }
+    }
+
+    private void invalidate(Waypoint waypoint, TimeRange affectedTimeRange) {
+        Map<TimePoint, Position> map = waypointPositionCache.get(waypoint);
+        if (map != null) {
+            for (TimePoint timePoint = roundToResolution(affectedTimeRange.from());
+                 !timePoint.after(affectedTimeRange.to());
+                 timePoint = timePoint.plus(timeRangeResolution)) {
+                assert timePoint.equals(roundToResolution(timePoint));
+                map.remove(timePoint);
+                synchronized (waypoints) {
+                    for (Waypoint otherWaypoint : waypoints) {
+                        if (otherWaypoint != waypoint) {
+                            final Map<TimePoint, Distance> distanceMap = distanceCache.get(new Pair<>(waypoint, otherWaypoint));
+                            if (distanceMap != null) {
+                                distanceMap.remove(timePoint);
+                            }
+                            final Map<TimePoint, Distance> otherDistanceMap = distanceCache.get(new Pair<>(otherWaypoint, waypoint));
+                            if (otherDistanceMap != null) {
+                                otherDistanceMap.remove(timePoint);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Rounds the <code>timePoint</code> to the next time range center which is then a valid key for a {@link #waypointPositionCache} value.<p>
+     * 
+     * Default scope for testability in fragment.
+     */
+    TimePoint roundToResolution(TimePoint timePoint) {
+        long mod = timePoint.asMillis() % timeRangeResolution.asMillis();
+        long div = timePoint.asMillis() / timeRangeResolution.asMillis();
+        // example with odd resolution 3ms; Intervals [2, 4], [5, 7], ... 
+        // example with even resolution 4ms; Intervals [2, 5], [6, 9], ...
+        final long roundedMillis;
+        if (mod <= (timeRangeResolution.asMillis()-1)/2) {
+            roundedMillis = div * timeRangeResolution.asMillis();
+        } else {
+            roundedMillis = (div+1) * timeRangeResolution.asMillis();
+        }
+        return new MillisecondsTimePoint(roundedMillis);
+    }
+}

java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/GPSFixTrack.java
@@ -10,6 +10,7 @@ import com.sap.sailing.domain.common.Speed;
 import com.sap.sailing.domain.common.SpeedWithBearing;
 import com.sap.sailing.domain.common.confidence.Weigher;
 import com.sap.sse.common.TimePoint;
+import com.sap.sse.common.TimeRange;
 import com.sap.sse.common.Util;
 
 /**
@@ -118,7 +119,7 @@ public interface GPSFixTrack<ItemType, FixType extends GPSFix> extends Track<Fix
      *         <code>new MillisecondsTimePoint(0)</code>). If no fix after <code>fix</code> is found, the second
      *         component is the end of time (<code>new MillisecondsTimePoint(Long.MAX_VALUE)</code>).
      */
-    Util.Pair<TimePoint, TimePoint> getEstimatedPositionTimePeriodAffectedBy(GPSFix fix);
+    TimeRange getEstimatedPositionTimePeriodAffectedBy(GPSFix fix);
 
     /**
      * Same as {@link #getEstimatedPosition(TimePoint, boolean)}, but produces an iterator for all {@link Timed} objects

java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/CrossTrackErrorCache.java
@@ -392,13 +392,13 @@ public class CrossTrackErrorCache extends AbstractRaceChangeListener {
 
     @Override
     public void competitorPositionChanged(GPSFixMoving fix, Competitor competitor) {
-        TimePoint from = owner.getTrack(competitor).getEstimatedPositionTimePeriodAffectedBy(fix).getA();
+        TimePoint from = owner.getTrack(competitor).getEstimatedPositionTimePeriodAffectedBy(fix).from();
         invalidate(competitor, from);
     }
 
     @Override
     public void markPositionChanged(GPSFix fix, Mark mark, boolean firstInTrack) {
-        TimePoint from = owner.getOrCreateTrack(mark).getEstimatedPositionTimePeriodAffectedBy(fix).getA();
+        TimePoint from = owner.getOrCreateTrack(mark).getEstimatedPositionTimePeriodAffectedBy(fix).from();
         final List<Competitor> shuffledCompetitors = new ArrayList<>(cachePerCompetitor.keySet());
         Collections.shuffle(shuffledCompetitors);
         for (Competitor competitor : shuffledCompetitors) {

commit 46cad9a69fdf3ad2bfcc6b0b2130434cf70738c4

fixed bug introduced in 7d3b339ad62e3e628d5bf799a48191d59b9c1ba8: distances along course need to be *added* leg by leg;
slight performance improvement for CandidateChooserImpl during initialization of end proxy candidate

java/com.sap.sailing.domain/src/com/sap/sailing/domain/markpassingcalculation/impl/CandidateChooserImpl.java
@@ -71,8 +71,7 @@ public class CandidateChooserImpl implements CandidateChooser {
         waypointPositionAndDistanceCache = new WaypointPositionAndDistanceCache(race, Duration.ONE_MINUTE);
         raceStartTime = race.getStartOfRace() != null ? race.getStartOfRace().minus(MILLISECONDS_BEFORE_STARTTIME) : null;
         start = new CandidateWithSettableTime(/* Index */0, raceStartTime, /* Probability */1, /* Waypoint */null);
-        end = new CandidateWithSettableWaypointIndex(race.getRace().getCourse()
-                .getIndexOfWaypoint(race.getRace().getCourse().getLastWaypoint()) + 2, /* TimePoint */null,
+        end = new CandidateWithSettableWaypointIndex(race.getRace().getCourse().getNumberOfWaypoints() + 1, /* TimePoint */null,
                 /* Probability */1, /* Waypoint */null);
         candidates = new HashMap<>();
         List<Candidate> startAndEnd = Arrays.asList(start, end);
@@ -427,7 +426,8 @@ public class CandidateChooserImpl implements CandidateChooser {
                 legsAreBetweenCandidates = true;
             }
             if (legsAreBetweenCandidates) {
-                totalGreatCircleDistance = waypointPositionAndDistanceCache.getApproximateDistance(from, leg.getLeg().getTo(), timePoint);
+                totalGreatCircleDistance = totalGreatCircleDistance.add(
+                        waypointPositionAndDistanceCache.getApproximateDistance(from, leg.getLeg().getTo(), timePoint));
             }
         }
         return totalGreatCircleDistance;