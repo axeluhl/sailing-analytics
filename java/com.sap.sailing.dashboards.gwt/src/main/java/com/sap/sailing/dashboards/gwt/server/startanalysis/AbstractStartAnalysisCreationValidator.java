fb7d36b7553a4447814997aadc309af46c37e130
Alexander Ries <alexander.ries@sap.com>
2015-04-01 13:44:18 +0200

    added new start analysis creation condition for gate starts

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/server/startanalysis/AbstractStartAnalysisCreationValidator.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/server/startanalysis/AbstractStartAnalysisCreationValidator.java
@@ -6,10 +6,12 @@ import com.sap.sailing.domain.base.Competitor;
 import com.sap.sailing.domain.base.Waypoint;
 import com.sap.sailing.domain.tracking.MarkPassing;
 import com.sap.sailing.domain.tracking.TrackedRace;
+import com.sap.sse.common.TimePoint;
 
 public abstract class AbstractStartAnalysisCreationValidator {
     
     private static int MINIMUM_NUMBER_COMPETITORS_FOR_STARTANALYSIS = 3;
+    private static long RACE_PROGRESSION_THREHOLD_AT_GATE_START_RACES_IN_MILLISECONDS = 6*60*1000;
     
     protected boolean threeCompetitorsPassedSecondWayPoint(TrackedRace trackedRace){
         Waypoint secondWaypoint = trackedRace.getRace().getCourse().getFirstLeg().getTo();
@@ -22,8 +24,31 @@ public abstract class AbstractStartAnalysisCreationValidator {
         return counter >= MINIMUM_NUMBER_COMPETITORS_FOR_STARTANALYSIS ? true : false;
     }
     
-    protected boolean competitorPassedSecondWayPoint(Competitor competitor, TrackedRace trackedRace){
+    private boolean competitorPassedSecondWayPoint(Competitor competitor, TrackedRace trackedRace){
         Waypoint secondWaypoint = trackedRace.getRace().getCourse().getFirstLeg().getTo();
         return trackedRace.getMarkPassing(competitor, secondWaypoint) == null ? false : true;
+    }
+    
+    private boolean gateStartGolfFlagIsDown(TrackedRace trackedRace){
+        if(trackedRace.getGateStartGolfDownTime() != 0){
+            return true;
+        }else{
+            TimePoint timeStartOfRace = trackedRace.getStartOfRace();
+            TimePoint timeNewestEvent = trackedRace.getTimePointOfNewestEvent();
+            if(timeStartOfRace != null && timeNewestEvent != null){
+                if(timeStartOfRace.asMillis()+RACE_PROGRESSION_THREHOLD_AT_GATE_START_RACES_IN_MILLISECONDS > timeNewestEvent.asMillis()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    protected boolean raceProgressedFarEnough(Competitor competitor, TrackedRace trackedRace){
+        if(trackedRace.isGateStart()){
+            return gateStartGolfFlagIsDown(trackedRace);
+        }else{
+            return competitorPassedSecondWayPoint(competitor, trackedRace);
+        }
     }
 }
