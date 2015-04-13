1881e11134aa3675b887948a62703ddf8b10494b
Steffen Schaefer <steffen.schaefer@oio.de>
2015-04-13 09:32:58 +0200

    StartActivity model update

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/start/EventStageDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/start/EventStageDTO.java
@@ -4,8 +4,26 @@ import com.sap.sailing.gwt.home.client.shared.stage.StageEventType;
 import com.sap.sailing.gwt.ui.shared.general.EventMetadataDTO;
 
 public class EventStageDTO extends EventMetadataDTO {
-    
+
     private StageEventType stageType;
+    private String baseURL;
+    private boolean isOnRemoteServer;
+
+    public String getBaseURL() {
+        return baseURL;
+    }
+
+    public void setBaseURL(String baseURL) {
+        this.baseURL = baseURL;
+    }
+
+    public boolean isOnRemoteServer() {
+        return isOnRemoteServer;
+    }
+
+    public void setOnRemoteServer(boolean isOnRemoteServer) {
+        this.isOnRemoteServer = isOnRemoteServer;
+    }
 
     public StageEventType getStageType() {
         return stageType;
