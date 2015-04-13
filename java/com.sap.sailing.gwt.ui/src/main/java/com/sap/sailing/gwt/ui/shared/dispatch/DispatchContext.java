0db3bff5f65f7e296433a5581ca19dc6c264cf06
Papick Garcia Taboada <pgt@pgt.de>
2015-04-13 12:06:17 +0200

    Fixed wrong import

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/dispatch/DispatchContext.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/dispatch/DispatchContext.java
@@ -1,9 +1,8 @@
 package com.sap.sailing.gwt.ui.shared.dispatch;
 
-import com.google.gwt.thirdparty.guava.common.annotations.GwtCompatible;
 import com.sap.sailing.server.RacingEventService;
 
-@GwtCompatible
+
 public interface DispatchContext {
 
     RacingEventService getRacingEventService();
