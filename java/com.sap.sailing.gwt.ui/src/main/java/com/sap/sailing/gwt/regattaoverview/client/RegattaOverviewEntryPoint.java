173fe1c3636193326adba0945a83b3d09218f3b4
Steffen Schaefer <steffen.schaefer@oio.de>
2015-04-16 12:33:23 +0200

    Fixed title

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/regattaoverview/client/RegattaOverviewEntryPoint.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/regattaoverview/client/RegattaOverviewEntryPoint.java
@@ -15,6 +15,7 @@ import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
+import com.sap.sailing.gwt.common.client.i18n.TextMessages;
 import com.sap.sailing.gwt.regattaoverview.client.RegattaRaceStatesComponent.EntryHandler;
 import com.sap.sailing.gwt.ui.client.AbstractSailingEntryPoint;
 import com.sap.sailing.gwt.ui.shared.RegattaOverviewEntryDTO;
@@ -55,7 +56,7 @@ public class RegattaOverviewEntryPoint extends AbstractSailingEntryPoint  {
                 && Window.Location.getParameter("embedded").equalsIgnoreCase("true");
         if (!embedded) {
 
-            SAPHeader logoAndTitlePanel = new SAPHeader(getStringMessages().eventOverview(), "", false);
+            SAPHeader logoAndTitlePanel = new SAPHeader(TextMessages.INSTANCE.headerLogo(), getStringMessages().eventOverview(), false);
             headerPanel.setHeaderWidget(logoAndTitlePanel);
         } else {
             RootPanel.getBodyElement().getStyle().setPadding(0, Unit.PX);
