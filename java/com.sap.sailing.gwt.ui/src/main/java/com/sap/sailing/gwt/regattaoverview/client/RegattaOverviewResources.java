02c6a67c4efb85d155d303afbbd55a8e6ed6fec6
Papick Garcia Taboada <pgt@pgt.de>
2015-04-16 11:17:39 +0200

    Disabled not strict on gss resources

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/regattaoverview/client/RegattaOverviewResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/regattaoverview/client/RegattaOverviewResources.java
@@ -3,13 +3,11 @@ package com.sap.sailing.gwt.regattaoverview.client;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
-import com.google.gwt.resources.client.CssResource.NotStrict;
 
 public interface RegattaOverviewResources extends ClientBundle {
     public static final RegattaOverviewResources INSTANCE = GWT.create(RegattaOverviewResources.class);
 
     @Source("RegattaOverview.gss")
-    @NotStrict
     LocalCss css();
 
     public interface LocalCss extends CssResource {
