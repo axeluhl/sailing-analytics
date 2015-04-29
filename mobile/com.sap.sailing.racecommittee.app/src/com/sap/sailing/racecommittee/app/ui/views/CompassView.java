0eb6863bb982182d7b81783a3838281472322400
Peter Siegmund <siegmund@ubilabs.net>
2015-04-29 09:53:36 +0200

    Bug 2776 - WindCompass performance makes using it impossible on a large number of the tablets we provide

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/views/CompassView.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/views/CompassView.java
@@ -2,13 +2,12 @@ package com.sap.sailing.racecommittee.app.ui.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.RotateAnimation;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -16,19 +15,12 @@ import com.sap.sailing.racecommittee.app.R;
 
 public class CompassView extends RelativeLayout {
 
-    public interface CompassDirectionListener {
-        void onDirectionChanged(float degree);
-    }
-
     private CompassDirectionListener changeListener = null;
     private RotateAnimation rotation = null;
     private ImageView needleView = null;
     private TextView degreeView = null;
     private float currentDegrees = 0.0f;
     private Float deferredToDegrees = null;
-
-    private float getNeedlePivotX() { return needleView.getMeasuredWidth() / 2; }
-    private float getNeedlePivotY() { return needleView.getMeasuredHeight() / 2; }
 
     public CompassView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -43,6 +35,14 @@ public class CompassView extends RelativeLayout {
     public CompassView(Context context) {
         super(context);
         inflate();
+    }
+
+    private float getNeedlePivotX() {
+        return needleView.getMeasuredWidth() / 2;
+    }
+
+    private float getNeedlePivotY() {
+        return needleView.getMeasuredHeight() / 2;
     }
 
     private void inflate() {
@@ -65,7 +65,7 @@ public class CompassView extends RelativeLayout {
 
         float toDegrees = (float) Math.toDegrees(Math.atan2(rotateY, rotateX));
         cancelAndStartAnimation(toDegrees, 0);
-        
+
         return true;
     }
 
@@ -76,16 +76,16 @@ public class CompassView extends RelativeLayout {
         doDeferredRotation();
     }
 
+    public float getDirection() {
+        return currentDegrees;
+    }
+
     public void setDirection(float toDegrees) {
         if (isNeedleReady()) {
             deferredToDegrees = toDegrees;
         } else {
             cancelAndStartAnimation(toDegrees, 500);
         }
-    }
-
-    public float getDirection() {
-        return currentDegrees;
     }
 
     private void cancelAndStartAnimation(float toDegrees, long duration) {
@@ -104,7 +104,7 @@ public class CompassView extends RelativeLayout {
             if (degree < 0) {
                 degree += 360;
             }
-            degreeView.setText(String.format("%.0f", degree));
+            degreeView.setText(String.format("%.0f°", degree));
         }
     }
 
@@ -121,7 +121,7 @@ public class CompassView extends RelativeLayout {
             changeListener.onDirectionChanged(degree);
         }
     }
-    
+
     private void doDeferredRotation() {
         if (deferredToDegrees != null) {
             float toDegrees = deferredToDegrees;
@@ -129,15 +129,19 @@ public class CompassView extends RelativeLayout {
             setDirection(toDegrees);
         }
     }
-    
+
     private boolean isNeedleReady() {
         return getNeedlePivotX() == 0.0;
     }
 
+    public interface CompassDirectionListener {
+        void onDirectionChanged(float degree);
+    }
+
     private class NeedleRotationListener implements AnimationListener {
-        
+
         private float targetDegrees;
-        
+
         public NeedleRotationListener(float toDegrees) {
             this.targetDegrees = toDegrees;
         }
