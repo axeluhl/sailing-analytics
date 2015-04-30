d564f86827a52c457a02c1c87d0855f8b2db0845
Peter Siegmund <siegmund@ubilabs.net>
2015-04-30 15:22:46 +0200

    Bug 2783 - Separate sending of results (photos/result list)

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/panels/FinishedSubmitFragment.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/panels/FinishedSubmitFragment.java
@@ -1,29 +1,15 @@
 package com.sap.sailing.racecommittee.app.ui.fragments.panels;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import com.sap.sailing.android.shared.util.AppUtils;
 import com.sap.sailing.android.shared.util.ViewHolder;
-import com.sap.sailing.domain.base.racegroup.RaceGroup;
 import com.sap.sailing.racecommittee.app.AppConstants;
-import com.sap.sailing.racecommittee.app.AppPreferences;
 import com.sap.sailing.racecommittee.app.R;
-import com.sap.sailing.racecommittee.app.ui.activities.RacingActivity;
-import com.sap.sailing.racecommittee.app.ui.fragments.dialogs.ProtestTimeDialogFragment;
-import com.sap.sailing.racecommittee.app.utils.CameraHelper;
-import com.sap.sailing.racecommittee.app.utils.MailHelper;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class FinishedSubmitFragment extends BasePanelFragment {
 
@@ -56,55 +42,6 @@ public class FinishedSubmitFragment extends BasePanelFragment {
             });
         }
 
-        Button submit = ViewHolder.get(layout, R.id.submit_button);
-        if (submit != null) {
-            submit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String recipient = AppPreferences.on(getActivity()).getMailRecipient();
-                    CharSequence subject = getSubject();
-                    CharSequence body = getBody();
-                    MailHelper.send(new String[] { recipient }, subject.toString(), body.toString(), getPhotos(), getActivity());
-                }
-            });
-        }
-
         return layout;
-    }
-
-    public ArrayList<Uri> getPhotos() {
-        ArrayList<Uri> retValue = new ArrayList<>();
-        CameraHelper cameraHelper = CameraHelper.on(getActivity());
-        File folder = cameraHelper.getOutputMediaFolder(cameraHelper.getSubFolder(getRace()));
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            if (file.getName().endsWith(".jpg") || file.getName().endsWith(".mp4")) {
-                retValue.add(Uri.fromFile(file));
-            }
-        }
-        Collections.sort(retValue, new Comparator<Uri>() {
-            @Override
-            public int compare(Uri lhs, Uri rhs) {
-                return lhs.getEncodedPath().compareTo(rhs.getEncodedPath());
-            }
-        });
-        return retValue;
-    }
-
-    public CharSequence getSubject() {
-        CharSequence subject = "";
-        RacingActivity activity = (RacingActivity) getActivity();
-        if (activity != null) {
-            ActionBar actionBar = activity.getSupportActionBar();
-            if (actionBar != null) {
-                subject = actionBar.getTitle();
-            }
-        }
-        return subject;
-    }
-
-    public CharSequence getBody() {
-        CharSequence body = "See the attachments\n\nSend from my RCApp - " + AppUtils.getBuildInfo(getActivity());
-        return body;
     }
 }
--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/raceinfo/PhotoListFragment.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/raceinfo/PhotoListFragment.java
@@ -12,14 +12,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 import com.sap.sailing.android.shared.logging.ExLog;
+import com.sap.sailing.android.shared.util.AppUtils;
 import com.sap.sailing.android.shared.util.ViewHolder;
+import com.sap.sailing.racecommittee.app.AppConstants;
+import com.sap.sailing.racecommittee.app.AppPreferences;
 import com.sap.sailing.racecommittee.app.R;
 import com.sap.sailing.racecommittee.app.ui.adapters.finishing.FinishListPhotoAdapter;
 import com.sap.sailing.racecommittee.app.ui.views.DividerItemDecoration;
 import com.sap.sailing.racecommittee.app.utils.CameraHelper;
+import com.sap.sailing.racecommittee.app.utils.MailHelper;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -30,11 +35,12 @@ import java.util.List;
 public class PhotoListFragment extends BaseFragment {
 
     private final static String TAG = PhotoListFragment.class.getName();
-    private final static int SHOOTPHOTO = 9000;
+    private final static int PHOTOSHOOTING = 9000;
 
     private ArrayList<Uri> mPhotos;
     private FinishListPhotoAdapter mAdapter;
     private RecyclerView mPhotoList;
+    private Button mSubmit;
 
     public PhotoListFragment() {
         mPhotos = new ArrayList<>();
@@ -56,14 +62,14 @@ public class PhotoListFragment extends BaseFragment {
                 @Override
                 public void onClick(View v) {
                     CameraHelper cameraHelper = CameraHelper.on(getActivity());
-                    Uri photoUri = cameraHelper.getOutputMediaFileUri(CameraHelper.MEDIA_TYPE_IMAGE, cameraHelper
-                        .getSubFolder(getRace()));
+                    Uri photoUri = cameraHelper
+                        .getOutputMediaFileUri(CameraHelper.MEDIA_TYPE_IMAGE, cameraHelper.getSubFolder(getRace()));
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     PackageManager manager = getActivity().getPackageManager();
                     List<ResolveInfo> activities = manager.queryIntentActivities(intent, 0);
                     if (activities.size() > 0) {
                         intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                        startActivityForResult(intent, SHOOTPHOTO);
+                        startActivityForResult(intent, PHOTOSHOOTING);
                     } else {
                         Toast.makeText(getActivity(), getString(R.string.no_camera), Toast.LENGTH_LONG).show();
                     }
@@ -76,8 +82,18 @@ public class PhotoListFragment extends BaseFragment {
             LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
             layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
             mPhotoList.setLayoutManager(layoutManager);
-//            mPhotoList.setHasFixedSize(true);
             mPhotoList.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+        }
+
+        mSubmit = ViewHolder.get(layout, R.id.submit_button);
+        if (mSubmit != null) {
+            mSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String recipient = AppPreferences.on(getActivity()).getMailRecipient();
+                    MailHelper.send(new String[] { recipient }, getSubject(), getBody(), getPhotos(), getActivity());
+                }
+            });
         }
         return layout;
     }
@@ -100,7 +116,7 @@ public class PhotoListFragment extends BaseFragment {
         }
 
         switch (requestCode) {
-        case SHOOTPHOTO:
+        case PHOTOSHOOTING:
             refreshPhotoList();
             ExLog.i(getActivity(), TAG, "Returned from Photo");
             break;
@@ -126,6 +142,50 @@ public class PhotoListFragment extends BaseFragment {
                 return lhs.getEncodedPath().compareTo(rhs.getEncodedPath());
             }
         });
+        if (mSubmit != null) {
+            mSubmit.setEnabled(mPhotos.size() != 0);
+        }
         mAdapter.notifyDataSetChanged();
+    }
+
+    private ArrayList<Uri> getPhotos() {
+        ArrayList<Uri> retValue = new ArrayList<>();
+        CameraHelper cameraHelper = CameraHelper.on(getActivity());
+        File folder = cameraHelper.getOutputMediaFolder(cameraHelper.getSubFolder(getRace()));
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            if (file.getName().endsWith(".jpg") || file.getName().endsWith(".mp4")) {
+                retValue.add(Uri.fromFile(file));
+            }
+        }
+        Collections.sort(retValue, new Comparator<Uri>() {
+            @Override
+            public int compare(Uri lhs, Uri rhs) {
+                return lhs.getEncodedPath().compareTo(rhs.getEncodedPath());
+            }
+        });
+        return retValue;
+    }
+
+    private String getSubject() {
+        return String.format(getString(R.string.results_mail_subject), getRaceName());
+    }
+
+    private String getBody() {
+        return String.format(getString(R.string.results_mail_body), getRaceName()) + "\n\nSend from my RCApp - " + AppUtils.getBuildInfo(getActivity());
+    }
+
+    private String getRaceName() {
+        String name = getRace().getRaceGroup().getName();
+
+        if (!getRace().getSeries().getName().equals(AppConstants.DEFAULT)) {
+            name += " - " + getRace().getSeries().getName();
+        }
+
+        if (!getRace().getFleet().getName().equals(AppConstants.DEFAULT)) {
+            name += " - " + getRace().getFleet().getName();
+        }
+
+        return name;
     }
 }
