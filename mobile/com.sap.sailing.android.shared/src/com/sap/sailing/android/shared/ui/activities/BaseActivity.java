ffc96c23081bcf1963e2c299a20a5d4a1b3fc740
Peter Siegmund <siegmund@ubilabs.net>
2015-05-06 08:57:19 +0200

    Removed BaseActivity from Android Shared

--- mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/activities/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package com.sap.sailing.android.shared.ui.activities;
-
-import android.support.v7.app.ActionBarActivity;
-
-public abstract class BaseActivity extends ActionBarActivity {
-
-    // TODO what is this planned for?
-    // private static final String TAG = BaseActivity.class.getName();
-}
