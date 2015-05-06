ed6a8e0522c83dde1f133f91ea7bc64525246114
Peter Siegmund <siegmund@ubilabs.net>
2015-05-06 10:28:42 +0200

    RCApp: images will now be saved in /sdcard/Android/data/<packageName>/files/pictures and will be deleted on app uninstall

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/utils/CameraHelper.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/utils/CameraHelper.java
@@ -2,7 +2,6 @@ package com.sap.sailing.racecommittee.app.utils;
 
 import android.content.Context;
 import android.net.Uri;
-import android.os.Environment;
 import android.support.annotation.Nullable;
 import com.sap.sailing.android.shared.logging.ExLog;
 import com.sap.sailing.racecommittee.app.domain.ManagedRace;
@@ -18,7 +17,7 @@ public class CameraHelper {
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
 
-    private static final String APP_NAME = "RCApp";
+    private static final String MAIN_SUB_FOLDER = "pictures";
     private static final String TAG = CameraHelper.class.getName();
 
     private Context mContext;
@@ -88,13 +87,7 @@ public class CameraHelper {
             subFolder += File.separator;
         }
 
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_PICTURES), APP_NAME + File.separator + subFolder);
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
+        File mediaStorageDir = new File(mContext.getExternalFilesDir(null), MAIN_SUB_FOLDER + File.separator + subFolder);
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists()) {
@@ -114,14 +107,12 @@ public class CameraHelper {
         File mediaStorageDir = getOutputMediaFolder(subFolder);
 
         // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", mContext.getResources().getConfiguration().locale).format(new Date());
         File mediaFile;
         if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_" + timeStamp + ".jpg");
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
         } else if (type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "VID_" + timeStamp + ".mp4");
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_" + timeStamp + ".mp4");
         } else {
             return null;
         }
