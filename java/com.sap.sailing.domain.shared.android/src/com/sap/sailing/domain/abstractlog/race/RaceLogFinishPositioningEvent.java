bd0f3c468cc9b76545e2735ba1a93a104017f08d
Jan Broß <mail@jbross.de>
2015-05-18 17:38:14 +0200

    Remove obsolete abstract keyword

--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/race/RaceLogFinishPositioningEvent.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/race/RaceLogFinishPositioningEvent.java
@@ -1,7 +1,7 @@
 package com.sap.sailing.domain.abstractlog.race;
 
 
-public abstract interface RaceLogFinishPositioningEvent extends RaceLogEvent {
+public interface RaceLogFinishPositioningEvent extends RaceLogEvent {
     
     /**
      * @return a triple holding the competitor ID, the competitor name and the {@link MaxPointReason} documenting the
