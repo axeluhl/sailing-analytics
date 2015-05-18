9825ea997fc0987456998acaab12bff4c9740c04
Alexander Ries <alexander.ries@sap.com>
2015-05-18 16:25:09 +0200

    RaceExecutionOrderProvider not extends WithId

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/RaceExecutionOrderProvider.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/RaceExecutionOrderProvider.java
@@ -1,12 +1,13 @@
 package com.sap.sailing.domain.tracking;
 
 import com.sap.sailing.domain.base.Regatta;
+import com.sap.sse.common.WithID;
 
 /**
  * @author Alexander Ries (D062114)
  *
  */
-public interface RaceExecutionOrderProvider {
+public interface RaceExecutionOrderProvider extends WithID{
 
     /**
      * Parameter <code>race</code> is a {@link TrackedRace} from which the method returns the previous {@link TrackedRace} in the execution order of a {@link Regatta}.
