e17290adeacf34782e2ad3457e59e8061e97d1b4
Benjamin Barth <benjamin.barth@oio.de>
2015-05-22 16:51:13 +0200

    Media page - Full screen viewer added ESC key support

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/media/FullscreenViewer.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/media/FullscreenViewer.java
@@ -3,11 +3,14 @@ package com.sap.sailing.gwt.home.client.shared.media;
 import java.util.List;
 
 import com.google.gwt.dom.client.Style.Overflow;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.PopupPanel.PositionCallback;
@@ -15,7 +18,13 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.sap.sailing.gwt.ui.shared.media.ImageMetadataDTO;
 
 public class FullscreenViewer {
-    private PopupPanel popup = new PopupPanel(true);
+    private PopupPanel popup = new PopupPanel(true) {
+        protected void onPreviewNativeEvent(NativePreviewEvent event) {
+            if (event.getTypeInt() == Event.ONKEYDOWN && event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ESCAPE) {
+                hide();
+            }
+        };
+    };
 
     public FullscreenViewer(ImageMetadataDTO selected, List<ImageMetadataDTO> images) {
         final GalleryPlayer viewer = new GalleryPlayer(selected, images);
@@ -38,7 +47,6 @@ public class FullscreenViewer {
         popup.setPopupPositionAndShow(new PositionCallback() {
             @Override
             public void setPosition(int offsetWidth, int offsetHeight) {
-
                 popup.center();
             }
         });
