4cc806704d979971084b1a1bb341d45851d8ca98
Alexander Ries <alexander.ries@sap.com>
2015-05-26 10:02:05 +0200

    Chaning now not only dashboard eventlogo but also window title according to given eventid parameter

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/eventlogo/EventLogo.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/eventlogo/EventLogo.java
@@ -3,6 +3,7 @@ package com.sap.sailing.dashboards.gwt.client.eventlogo;
 import java.util.UUID;
 
 import com.google.gwt.dom.client.Style.Visibility;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Image;
 import com.sap.sailing.gwt.ui.client.SailingServiceAsync;
@@ -22,12 +23,17 @@ public class EventLogo extends Image {
             sailingServiceAsync.getEventById(eventUUID, true, new AsyncCallback<EventDTO>() {
                 @Override
                 public void onSuccess(final EventDTO event) {
-                    final String logoUrl = event.getLogoImageURL();
-                            if (logoUrl != null) {
-                                eventLogo.getElement().setAttribute("src", logoUrl);
-                            } else {
-                                eventLogo.getElement().getStyle().setVisibility(Visibility.HIDDEN);
-                            }
+                    if (event != null) {
+                        final String logoUrl = event.getLogoImageURL();
+                        if (logoUrl != null) {
+                            eventLogo.getElement().setAttribute("src", logoUrl);
+                        } else {
+                            eventLogo.getElement().getStyle().setVisibility(Visibility.HIDDEN);
+                        }
+                        if (event.getName() != null) {
+                            Window.setTitle(event.getName() + " Dashboard");
+                        }
+                    }
                 }
 
                 @Override
