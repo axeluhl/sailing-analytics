dbd0fc92ee40117a28a36de4c0c333cf9ab41162
Jan Broß <mail@jbross.de>
2015-05-29 11:55:06 +0200

    Implements AndroidRaceLogResolver

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/AndroidRaceLogResolver.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/data/AndroidRaceLogResolver.java
@@ -10,14 +10,7 @@ public class AndroidRaceLogResolver implements RaceLogResolver {
     @Override
     public RaceLog resolve(SimpleRaceLogIdentifier identifier)
             throws RegataLikeNameOfIdentifierDoesntMatchActualRegattaLikeNameException {
-
-        //has to check against local as well as online racelog events
-        
-        //FIXME: somehow get the ManagedRace belonging to identifier in order to get the RaceLog
-        //InMemoryDataStore.INSTANCE.(new ManagedRaceIdentifierImpl(identifier.getRaceColumnName(), new FleetIdentifier(
-        
-        
-        return null;
+        return InMemoryDataStore.INSTANCE.getRace(identifier).getRaceLog();
     }
 
 }
