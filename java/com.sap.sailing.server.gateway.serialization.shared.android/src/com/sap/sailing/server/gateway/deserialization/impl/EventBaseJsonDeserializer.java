205062743304fb6a0279aa147af472d14dd39cc9
fmittag <fmittag@gmx.net>
2015-06-09 19:10:01 +0200

    added Json serializer and deserializer code for images and videos attribute of event

--- java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/deserialization/impl/EventBaseJsonDeserializer.java
+++ java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/deserialization/impl/EventBaseJsonDeserializer.java
@@ -17,7 +17,13 @@ import com.sap.sailing.domain.common.impl.ImageSizeImpl;
 import com.sap.sailing.server.gateway.deserialization.JsonDeserializationException;
 import com.sap.sailing.server.gateway.deserialization.JsonDeserializer;
 import com.sap.sailing.server.gateway.serialization.impl.EventBaseJsonSerializer;
+import com.sap.sse.common.TimePoint;
 import com.sap.sse.common.impl.MillisecondsTimePoint;
+import com.sap.sse.common.media.ImageDescriptor;
+import com.sap.sse.common.media.ImageDescriptorImpl;
+import com.sap.sse.common.media.MimeType;
+import com.sap.sse.common.media.VideoDescriptor;
+import com.sap.sse.common.media.VideoDescriptorImpl;
 
 public class EventBaseJsonDeserializer implements JsonDeserializer<EventBase> {
     private final JsonDeserializer<Venue> venueDeserializer;
@@ -28,22 +34,22 @@ public class EventBaseJsonDeserializer implements JsonDeserializer<EventBase> {
         this.leaderboardGroupDeserializer = leaderboardGroupDeserializer;
     }
 
-    public EventBase deserialize(JSONObject object) throws JsonDeserializationException {
-        UUID id = UUID.fromString((String) object.get(EventBaseJsonSerializer.FIELD_ID));
-        String name = (String) object.get(EventBaseJsonSerializer.FIELD_NAME);
-        String description = (String) object.get(EventBaseJsonSerializer.FIELD_DESCRIPTION);
-        String officialWebsiteURLAsString = (String) object.get(EventBaseJsonSerializer.FIELD_OFFICIAL_WEBSITE_URL);
-        String logoImageURLAsString = (String) object.get(EventBaseJsonSerializer.FIELD_LOGO_IMAGE_URL);
-        Number startDate = (Number) object.get(EventBaseJsonSerializer.FIELD_START_DATE);
-        Number endDate = (Number) object.get(EventBaseJsonSerializer.FIELD_END_DATE);
+    public EventBase deserialize(JSONObject eventJson) throws JsonDeserializationException {
+        UUID id = UUID.fromString((String) eventJson.get(EventBaseJsonSerializer.FIELD_ID));
+        String name = (String) eventJson.get(EventBaseJsonSerializer.FIELD_NAME);
+        String description = (String) eventJson.get(EventBaseJsonSerializer.FIELD_DESCRIPTION);
+        String officialWebsiteURLAsString = (String) eventJson.get(EventBaseJsonSerializer.FIELD_OFFICIAL_WEBSITE_URL);
+        String logoImageURLAsString = (String) eventJson.get(EventBaseJsonSerializer.FIELD_LOGO_IMAGE_URL);
+        Number startDate = (Number) eventJson.get(EventBaseJsonSerializer.FIELD_START_DATE);
+        Number endDate = (Number) eventJson.get(EventBaseJsonSerializer.FIELD_END_DATE);
         final Venue venue;
-        if (object.get(EventBaseJsonSerializer.FIELD_VENUE) != null) {
-            JSONObject venueObject = Helpers.getNestedObjectSafe(object, EventBaseJsonSerializer.FIELD_VENUE);
+        if (eventJson.get(EventBaseJsonSerializer.FIELD_VENUE) != null) {
+            JSONObject venueObject = Helpers.getNestedObjectSafe(eventJson, EventBaseJsonSerializer.FIELD_VENUE);
             venue = venueDeserializer.deserialize(venueObject);
         } else {
             venue = null;
         }
-        JSONArray leaderboardGroupsJson = (JSONArray) object.get(EventBaseJsonSerializer.FIELDS_LEADERBOARD_GROUPS);
+        JSONArray leaderboardGroupsJson = (JSONArray) eventJson.get(EventBaseJsonSerializer.FIELDS_LEADERBOARD_GROUPS);
         List<LeaderboardGroupBase> leaderboardGroups = new ArrayList<LeaderboardGroupBase>();
         if (leaderboardGroupsJson != null) {
             for (Object lgJson : leaderboardGroupsJson) {
@@ -67,28 +73,28 @@ public class EventBaseJsonDeserializer implements JsonDeserializer<EventBase> {
                 throw new JsonDeserializationException("Error deserializing logo image URL for event "+name, e);
             }
         }
-        if (object.get(EventBaseJsonSerializer.FIELD_IMAGE_URLS) != null) {
+        if (eventJson.get(EventBaseJsonSerializer.FIELD_IMAGE_URLS) != null) {
             try {
-                result.setImageURLs(getURLsFromStrings(Helpers.getNestedArraySafe(object, EventBaseJsonSerializer.FIELD_IMAGE_URLS)));
+                result.setImageURLs(getURLsFromStrings(Helpers.getNestedArraySafe(eventJson, EventBaseJsonSerializer.FIELD_IMAGE_URLS)));
             } catch (MalformedURLException e) {
                 throw new JsonDeserializationException("Error deserializing image URLs for event "+name, e);
             }
         }
-        if (object.get(EventBaseJsonSerializer.FIELD_VIDEO_URLS) != null) {
+        if (eventJson.get(EventBaseJsonSerializer.FIELD_VIDEO_URLS) != null) {
             try {
-                result.setVideoURLs(getURLsFromStrings(Helpers.getNestedArraySafe(object, EventBaseJsonSerializer.FIELD_VIDEO_URLS)));
+                result.setVideoURLs(getURLsFromStrings(Helpers.getNestedArraySafe(eventJson, EventBaseJsonSerializer.FIELD_VIDEO_URLS)));
             } catch (MalformedURLException e) {
                 throw new JsonDeserializationException("Error deserializing video URLs for event "+name, e);
             }
         }
-        if (object.get(EventBaseJsonSerializer.FIELD_SPONSOR_IMAGE_URLS) != null) {
+        if (eventJson.get(EventBaseJsonSerializer.FIELD_SPONSOR_IMAGE_URLS) != null) {
             try {
-                result.setSponsorImageURLs(getURLsFromStrings(Helpers.getNestedArraySafe(object, EventBaseJsonSerializer.FIELD_SPONSOR_IMAGE_URLS)));
+                result.setSponsorImageURLs(getURLsFromStrings(Helpers.getNestedArraySafe(eventJson, EventBaseJsonSerializer.FIELD_SPONSOR_IMAGE_URLS)));
             } catch (MalformedURLException e) {
                 throw new JsonDeserializationException("Error deserializing sponsor image URLs for event "+name, e);
             }
         }
-        JSONArray imageSizes = (JSONArray) object.get(EventBaseJsonSerializer.FIELD_IMAGE_SIZES);
+        JSONArray imageSizes = (JSONArray) eventJson.get(EventBaseJsonSerializer.FIELD_IMAGE_SIZES);
         if (imageSizes != null) {
             for (Object imageURLAndSizeObject : imageSizes) {
                 JSONObject imageURLAndSizeJson = (JSONObject) imageURLAndSizeObject;
@@ -103,9 +109,89 @@ public class EventBaseJsonDeserializer implements JsonDeserializer<EventBase> {
                 }
             }
         }
+        JSONArray imagesJson = (JSONArray) eventJson.get(EventBaseJsonSerializer.FIELD_IMAGES);
+        if (imagesJson != null) {
+            for (Object imageJson : imagesJson) {
+                ImageDescriptor imgaeDescriptor = loadImage((JSONObject) imageJson);
+                if (imgaeDescriptor != null) {
+                   result.addImage(imgaeDescriptor);
+                }
+            }            
+        }
+        JSONArray videosJson = (JSONArray) eventJson.get(EventBaseJsonSerializer.FIELD_VIDEOS);
+        if (videosJson != null) {
+            for (Object videoJson : videosJson) {
+                VideoDescriptor videoDescriptor = loadVideo((JSONObject) videoJson);
+                if (videoDescriptor != null) {
+                   result.addVideo(videoDescriptor);
+                }
+            }            
+        }
+
         return result;
     }
+
+    private ImageDescriptor loadImage(JSONObject imageJson) {
+        ImageDescriptor image = null;
+        URL imageURL = Helpers.getURLField(imageJson, EventBaseJsonSerializer.FIELD_SOURCE_URL);
+        if (imageURL != null) {
+            String title = (String) imageJson.get(EventBaseJsonSerializer.FIELD_TITLE);
+            String subtitle = (String) imageJson.get(EventBaseJsonSerializer.FIELD_SUBTITLE);
+            String copyright = (String) imageJson.get(EventBaseJsonSerializer.FIELD_COPYRIGHT);
+            Integer imageWidth = (Integer) imageJson.get(EventBaseJsonSerializer.FIELD_IMAGE_WIDTH_IN_PX);
+            Integer imageHeight = (Integer) imageJson.get(EventBaseJsonSerializer.FIELD_IMAGE_HEIGHT_IN_PX);
+            Long createdAtDateInMs = (Long) imageJson.get(EventBaseJsonSerializer.FIELD_CREATEDATDATE);
+            TimePoint createdAtDate = createdAtDateInMs != null ? new MillisecondsTimePoint(createdAtDateInMs) : null;
+            JSONArray tags = (JSONArray) imageJson.get(EventBaseJsonSerializer.FIELD_TAGS);
+            List<String> imageTags = new ArrayList<String>();
+            if (tags != null) {
+                for (Object tagObject : tags) {
+                    imageTags.add((String) tagObject);
+                }
+            }
+            image = new ImageDescriptorImpl(imageURL, createdAtDate);
+            image.setCopyright(copyright);
+            image.setTitle(title);
+            image.setSubtitle(subtitle);
+            image.setTags(imageTags);
+            if (imageWidth != null && imageHeight != null) {
+                image.setSize(imageWidth, imageHeight);
+            }
+        }
+        return image;
+    }
     
+    private VideoDescriptor loadVideo(JSONObject videoJson) {
+        VideoDescriptor video = null;
+        URL videoURL = Helpers.getURLField(videoJson, EventBaseJsonSerializer.FIELD_SOURCE_URL);
+        if(videoURL != null) {
+            String title = (String) videoJson.get(EventBaseJsonSerializer.FIELD_TITLE);
+            String subtitle = (String) videoJson.get(EventBaseJsonSerializer.FIELD_SUBTITLE);
+            String copyright = (String) videoJson.get(EventBaseJsonSerializer.FIELD_COPYRIGHT);
+            Object mimeTypeRaw = videoJson.get(EventBaseJsonSerializer.FIELD_MIMETYPE);
+            MimeType mimeType = mimeTypeRaw == null ? null : MimeType.valueOf((String) mimeTypeRaw);
+            Long createdAtDateInMs = (Long) videoJson.get(EventBaseJsonSerializer.FIELD_CREATEDATDATE);
+            TimePoint createdAtDate = createdAtDateInMs != null ? new MillisecondsTimePoint(createdAtDateInMs) : null;
+            JSONArray tags = (JSONArray) videoJson.get(EventBaseJsonSerializer.FIELD_TAGS);
+            Integer lengthInSeconds = (Integer) videoJson.get(EventBaseJsonSerializer.FIELD_VIDEO_LENGTH_IN_SECONDS);
+            URL thumbnailURL = Helpers.getURLField(videoJson, EventBaseJsonSerializer.FIELD_VIDEO_THUMBNAIL_URL);
+            List<String> videoTags = new ArrayList<String>();
+            if (tags != null) {
+                for (Object tagObject : tags) {
+                    videoTags.add((String) tagObject);
+                }
+            }
+            video = new VideoDescriptorImpl(videoURL, mimeType, createdAtDate);
+            video.setCopyright(copyright);
+            video.setTitle(title);
+            video.setSubtitle(subtitle);
+            video.setTags(videoTags);
+            video.setLengthInSeconds(lengthInSeconds);
+            video.setThumbnailURL(thumbnailURL);
+        }
+        return video;
+    }
+
     private Iterable<URL> getURLsFromStrings(JSONArray strings) throws MalformedURLException {
         List<URL> result = new ArrayList<URL>();
         if (strings != null) {
@@ -115,5 +201,4 @@ public class EventBaseJsonDeserializer implements JsonDeserializer<EventBase> {
         }
         return result;
     }
-
 }
--- java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/serialization/impl/EventBaseJsonSerializer.java
+++ java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/serialization/impl/EventBaseJsonSerializer.java
@@ -12,7 +12,9 @@ import com.sap.sailing.domain.base.EventBase;
 import com.sap.sailing.domain.base.LeaderboardGroupBase;
 import com.sap.sailing.domain.base.Venue;
 import com.sap.sailing.server.gateway.serialization.JsonSerializer;
+import com.sap.sse.common.media.ImageDescriptor;
 import com.sap.sse.common.media.ImageSize;
+import com.sap.sse.common.media.VideoDescriptor;
 
 public class EventBaseJsonSerializer implements JsonSerializer<EventBase> {
     private static final Logger logger = Logger.getLogger(EventBaseJsonSerializer.class.getName());
@@ -31,8 +33,25 @@ public class EventBaseJsonSerializer implements JsonSerializer<EventBase> {
     public static final String FIELD_IMAGE_URL = "imageURL";
     public static final String FIELD_IMAGE_WIDTH = "imageWidth";
     public static final String FIELD_IMAGE_HEIGHT = "imageHeight";
+    public static final String FIELD_VIDEO_URL = "videoURL";
     public static final String FIELD_OFFICIAL_WEBSITE_URL = "officialWebsiteURL";
     public static final String FIELDS_LEADERBOARD_GROUPS = "leaderboardGroups";
+
+    public static final String FIELD_SOURCE_URL = "sourceURL";
+    public static final String FIELD_TITLE = "title";
+    public static final String FIELD_SUBTITLE = "subtitle";
+    public static final String FIELD_MIMETYPE = "mimetype";
+    public static final String FIELD_COPYRIGHT = "copyright";
+    public static final String FIELD_CREATEDATDATE = "createdAtDate";
+    public static final String FIELD_TAGS = "tags";
+    // specific image fields
+    public static final String FIELD_IMAGES = "images";
+    public static final String FIELD_IMAGE_WIDTH_IN_PX = "widthInPixel";
+    public static final String FIELD_IMAGE_HEIGHT_IN_PX = "heightInPixel";
+    // specific video fields
+    public static final String FIELD_VIDEOS = "videos";
+    public static final String FIELD_VIDEO_THUMBNAIL_URL = "thumbnailURL";
+    public static final String FIELD_VIDEO_LENGTH_IN_SECONDS = "lengthInSeconds";
 
     private final JsonSerializer<Venue> venueSerializer;
     private final JsonSerializer<? super LeaderboardGroupBase> leaderboardGroupBaseSerializer;
@@ -71,7 +90,52 @@ public class EventBaseJsonSerializer implements JsonSerializer<EventBase> {
         for (URL sponsorImageURL : event.getSponsorImageURLs()) {
             addImageSize(sponsorImageURL, imageSizes, event);
         }
+        JSONArray jsonImages = new JSONArray();
+        for(ImageDescriptor imageDescriptor: event.getImages()) {
+            addImage(imageDescriptor, jsonImages);
+        }
+        result.put(FIELD_IMAGES, jsonImages);
+        JSONArray jsonVideos = new JSONArray();
+        for(VideoDescriptor videoDescriptor: event.getVideos()) {
+            addVideo(videoDescriptor, jsonVideos);
+        }
+        result.put(FIELD_VIDEOS, jsonVideos);
         return result;
+    }
+
+    private void addImage(ImageDescriptor image, JSONArray imagesJson) {
+        JSONObject imageJson = new JSONObject();
+        imageJson.put(FIELD_SOURCE_URL, image.getURL().toString());
+        imageJson.put(FIELD_TITLE, image.getTitle());
+        imageJson.put(FIELD_SUBTITLE, image.getSubtitle());
+        imageJson.put(FIELD_MIMETYPE, image.getMimeType());
+        imageJson.put(FIELD_COPYRIGHT, image.getCopyright());
+        imageJson.put(FIELD_IMAGE_WIDTH_IN_PX, image.getWidthInPx());
+        imageJson.put(FIELD_IMAGE_HEIGHT_IN_PX, image.getHeightInPx());
+        imageJson.put(FIELD_CREATEDATDATE, image.getCreatedAtDate().asMillis());
+        JSONArray tags = new JSONArray();
+        for (String tag : image.getTags()) {
+            tags.add(tag);
+        }
+        imageJson.put(FIELD_TAGS, tags);
+        imagesJson.add(imageJson);
+    }
+
+    private void addVideo(VideoDescriptor video, JSONArray videosJson) {
+        JSONObject videoJson = new JSONObject();
+        videoJson.put(FIELD_SOURCE_URL, video.getURL().toString());
+        videoJson.put(FIELD_VIDEO_THUMBNAIL_URL, video.getThumbnailURL() != null ? video.getThumbnailURL().toString() : null);
+        videoJson.put(FIELD_TITLE, video.getTitle());
+        videoJson.put(FIELD_SUBTITLE, video.getSubtitle());
+        videoJson.put(FIELD_MIMETYPE, video.getMimeType());
+        videoJson.put(FIELD_COPYRIGHT, video.getCopyright());
+        videoJson.put(FIELD_VIDEO_LENGTH_IN_SECONDS, video.getLengthInSeconds());
+        videoJson.put(FIELD_CREATEDATDATE, video.getCreatedAtDate().asMillis());
+        JSONArray tags = new JSONArray();
+        for (String tag : video.getTags()) {
+            tags.add(tag);
+        }
+        videosJson.add(videoJson);
     }
 
     /**
