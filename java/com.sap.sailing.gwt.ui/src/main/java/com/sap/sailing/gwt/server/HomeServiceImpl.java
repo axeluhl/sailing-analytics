f2b8a926c6304ce518a2c32eb350cfb5d381f7c4
Steffen Schaefer <steffen.schaefer@oio.de>
2015-06-11 09:03:15 +0200

    Using correct link for sailors info of Kieler Woche

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/server/HomeServiceImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/server/HomeServiceImpl.java
@@ -347,7 +347,7 @@ public class HomeServiceImpl extends ProxiedRemoteServiceServlet implements Home
         
         // TODO fmittag: put in correct event ID
         if("adbd871b-b9b3-4e18-bd79-d560e7b87c53".equals(event.getId().toString())) {
-            dto.setSailorsInfoURL("http://livecenter.kieler-woche.de/");
+            dto.setSailorsInfoURL("http://sailorsinfo.kieler-woche.de/");
         }
 
         return dto;
