0ef485c8d035c4c674d9ae6c3d5c7cae3ad620ec
Benjamin Barth <benjamin.barth@oio.de>
2015-06-15 15:57:56 +0200

    Event overview mobile - Initialy added SectionHeader partial

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/sectionHeader/SectionHeaderResources.java
@@ -0,0 +1,27 @@
+package com.sap.sailing.gwt.home.mobile.partials.sectionHeader;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface SectionHeaderResources extends ClientBundle {
+    public static final SectionHeaderResources INSTANCE = GWT.create(SectionHeaderResources.class);
+
+    @Source("SectionHeader.gss")
+    LocalCss css();
+
+    public interface LocalCss extends CssResource {
+        String sectionheader();
+        String sectionheaderstandalone();
+        String sectionheader_item();
+        String sectionheader_itemdoublerow();
+        String sectionheader_itemright();
+        String sectionheader_item_image();
+        String sectionheader_item_image_seperator();
+        String sectionheader_item_title();
+        String sectionheader_item_label();
+        String sectionheader_item_subtitle();
+        String sectionheader_item_infotext();
+        String sectionheader_item_arrow();
+    }
+}
