823797947ba3c2d8637ff2ab0474378105e02837
Papick Garcia Taboada <pgt@pgt.de>
2015-06-15 12:58:32 +0200

    Added missing history tokenizer for event

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/app/MobileHistoryMapper.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/app/MobileHistoryMapper.java
@@ -2,6 +2,7 @@ package com.sap.sailing.gwt.home.mobile.app;
 
 import com.google.gwt.place.shared.PlaceHistoryMapper;
 import com.google.gwt.place.shared.WithTokenizers;
+import com.sap.sailing.gwt.home.client.place.event.EventDefaultPlace;
 import com.sap.sailing.gwt.home.client.place.event.legacy.EventPlace;
 import com.sap.sailing.gwt.home.client.place.events.EventsPlace;
 import com.sap.sailing.gwt.home.client.place.start.StartPlace;
@@ -9,6 +10,7 @@ import com.sap.sailing.gwt.home.client.place.start.StartPlace;
 @WithTokenizers({ //
 EventPlace.Tokenizer.class,//
         EventsPlace.Tokenizer.class,//
+        EventDefaultPlace.Tokenizer.class,//
         StartPlace.Tokenizer.class
 })
 public interface MobileHistoryMapper extends PlaceHistoryMapper {
