b14033b5f38bb2188b9f9c52b87f8401d45051fa
Benjamin Barth <benjamin.barth@oio.de>
2015-06-15 18:06:13 +0200

    Event overview mobile - Implemented StatisticsBox partial

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/statisticsBox/StatisticsBox.java
@@ -0,0 +1,36 @@
+package com.sap.sailing.gwt.home.mobile.partials.statisticsBox;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.gwt.home.mobile.partials.sectionHeader.SectionHeaderResources;
+
+public class StatisticsBox extends Composite {
+    
+    public static final String ICON_REGATTAS_FOUGHT = "images/mobile/icon_regattasFought.svg";
+    public static final String ICON_COMPATITORS_COUNT = "images/mobile/icon_averageSpeed.svg";
+    public static final String ICON_RACES_COUNT = "images/mobile/icon_racesCount.svg";
+    public static final String ICON_TRACKED_COUNT = "images/mobile/icon_trackedCount.svg";
+    
+    private static StatisticsBoxUiBinder uiBinder = GWT.create(StatisticsBoxUiBinder.class);
+
+    interface StatisticsBoxUiBinder extends UiBinder<Widget, StatisticsBox> {
+    }
+
+    @UiField DivElement itemContainerUi;
+    
+    public StatisticsBox() {
+        StatisticsBoxResources.INSTANCE.css().ensureInjected();
+        SectionHeaderResources.INSTANCE.css().ensureInjected();
+        initWidget(uiBinder.createAndBindUi(this));
+    }
+    
+    public void addItem(String iconUrl, String name, Integer count) {
+        StatisticsBoxItem item = new StatisticsBoxItem(iconUrl, name, count);
+        itemContainerUi.appendChild(item.getElement());
+    }
+
+}
---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/statisticsBox/StatisticsBoxItem.java
@@ -0,0 +1,29 @@
+package com.sap.sailing.gwt.home.mobile.partials.statisticsBox;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.ImageElement;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Widget;
+
+class StatisticsBoxItem extends Widget {
+
+    private static StatisticsBoxItemUiBinder uiBinder = GWT.create(StatisticsBoxItemUiBinder.class);
+
+    interface StatisticsBoxItemUiBinder extends UiBinder<Element, StatisticsBoxItem> {
+    }
+    
+    @UiField ImageElement iconUi;
+    @UiField SpanElement nameUi;
+    @UiField SpanElement valueUi;
+
+    public StatisticsBoxItem(String iconUrl, String name, Integer value) {
+        setElement(uiBinder.createAndBindUi(this));
+        iconUi.setSrc(iconUrl);
+        nameUi.setInnerText(name);
+        valueUi.setInnerText(value == null ? "n/a" : String.valueOf(value));
+    }
+
+}
