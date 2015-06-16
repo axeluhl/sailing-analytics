719179fbcfde59681b87eae17464a83edaced127
Alexander Ries <alexander.ries@sap.com>
2015-06-16 22:45:13 +0200

    formatted parts of EventLogo

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/eventlogo/EventLogo.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/eventlogo/EventLogo.java
@@ -35,7 +35,6 @@ public class EventLogo extends Image {
                             Window.setTitle(event.getName() + " Dashboard");
                         }
                     }
-                    
                 }
 
                 @Override
