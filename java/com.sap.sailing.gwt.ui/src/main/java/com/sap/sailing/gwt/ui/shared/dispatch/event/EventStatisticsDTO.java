6ece53bb6d1127244fd5f29abc8c97ef199af54f
Axel Uhl <axel.uhl@sap.com>
2015-06-17 16:17:24 +0200

    added number of GPS fixes, wind fixes and max speed to EventStatisticsDTO

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/dispatch/event/EventStatisticsDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/dispatch/event/EventStatisticsDTO.java
@@ -1,6 +1,10 @@
 package com.sap.sailing.gwt.ui.shared.dispatch.event;
 
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.common.Speed;
 import com.sap.sailing.gwt.ui.shared.dispatch.DTO;
+import com.sap.sse.common.TimePoint;
+import com.sap.sse.common.Util.Triple;
 
 public class EventStatisticsDTO implements DTO {
 
@@ -8,21 +12,25 @@ public class EventStatisticsDTO implements DTO {
     private Integer competitorsCount;
     private Integer racesRunCount;
     private Integer trackedRacesCount;
+    private long numberOfGPSFixes;
+    private long numberOfWindFixes;
+    private Triple<Competitor, Speed, TimePoint> maxSpeed;
     
     @SuppressWarnings("unused")
     private EventStatisticsDTO() {
     }
     
     public EventStatisticsDTO(Integer regattasFoughtCount, Integer competitorsCount, Integer racesRunCount,
-            Integer trackedRacesCount) {
+            Integer trackedRacesCount, long numberOfGPSFixes, long numberOfWindFixes, Triple<Competitor, Speed, TimePoint> maxSpeed) {
         super();
         this.regattasFoughtCount = regattasFoughtCount;
         this.competitorsCount = competitorsCount;
         this.racesRunCount = racesRunCount;
         this.trackedRacesCount = trackedRacesCount;
+        this.numberOfGPSFixes = numberOfGPSFixes;
+        this.numberOfWindFixes = numberOfWindFixes;
+        this.maxSpeed = maxSpeed;
     }
-
-
 
     public Integer getRegattasFoughtCount() {
         return regattasFoughtCount;
@@ -54,6 +62,18 @@ public class EventStatisticsDTO implements DTO {
 
     public void setTrackedRacesCount(Integer trackedRacesCount) {
         this.trackedRacesCount = trackedRacesCount;
+    }
+
+    public long getNumberOfGPSFixes() {
+        return numberOfGPSFixes;
+    }
+
+    public long getNumberOfWindFixes() {
+        return numberOfWindFixes;
+    }
+
+    public Triple<Competitor, Speed, TimePoint> getMaxSpeed() {
+        return maxSpeed;
     }
 
 }
