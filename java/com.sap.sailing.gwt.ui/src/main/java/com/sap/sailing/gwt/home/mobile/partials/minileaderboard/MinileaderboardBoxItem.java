bd4cd9176343f921f05d348fdd75d0c159a054ba
Steffen Schaefer <steffen.schaefer@oio.de>
2015-06-18 08:52:05 +0200

    Corrected rendering of flag images

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/minileaderboard/MinileaderboardBoxItem.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/minileaderboard/MinileaderboardBoxItem.java
@@ -3,9 +3,12 @@ package com.sap.sailing.gwt.home.mobile.partials.minileaderboard;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.ImageElement;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.domain.common.dto.CompetitorDTO;
+import com.sap.sailing.gwt.ui.client.FlagImageResolver;
 import com.sap.sailing.gwt.ui.shared.dispatch.event.MiniLeaderboardItemDTO;
 
 public class MinileaderboardBoxItem extends Widget {
@@ -31,7 +34,21 @@ public class MinileaderboardBoxItem extends Widget {
         competitorCountryNameUi.setInnerText(String.valueOf(entry.getCompetitor().getCountryName()));
         competitorRankUi.setInnerText(String.valueOf(entry.getRank()));
         competitorPointsUi.setInnerText(String.valueOf(entry.getPoints()));
-        competitorFlagUi.setSrc(entry.getCompetitor().getFlagImageURL());
+        
+        CompetitorDTO competitor = entry.getCompetitor();
+        String flagImageURL = competitor.getFlagImageURL();
+
+        if (flagImageURL == null || flagImageURL.isEmpty()) {
+            String twoLetterIsoCountryCode = competitor.getTwoLetterIsoCountryCode();
+            final ImageResource flagImageResource;
+            if (twoLetterIsoCountryCode==null || twoLetterIsoCountryCode.isEmpty()) {
+                flagImageResource = FlagImageResolver.getEmptyFlagImageResource();
+            } else {
+                flagImageResource = FlagImageResolver.getFlagImageResource(twoLetterIsoCountryCode);
+            }
+            flagImageURL = flagImageResource.getSafeUri().asString();
+        }
+        competitorFlagUi.setSrc(flagImageURL);
         
     }
 
