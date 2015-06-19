46ecaee070e0498904f1e9253f4439baa6440f6b
Steffen Schaefer <steffen.schaefer@oio.de>
2015-06-19 15:33:01 +0200

    mobile video stage is now showing the video title

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/event/overview/EventOverviewStage.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/event/overview/EventOverviewStage.java
@@ -59,7 +59,7 @@ public class EventOverviewStage extends Composite implements RefreshableWidget<E
             }
             if (!(lastContent instanceof Video)
                     || ((Video) lastContent).shouldBeReplaced(videoStageDTO.getVideo().getSourceRef())) {
-                lastContent = new Video();
+                lastContent = new Video(true);
                 ((Video) lastContent).setData(videoStageDTO);
             } 
         } else if (data instanceof EventOverviewTickerStageDTO) {
