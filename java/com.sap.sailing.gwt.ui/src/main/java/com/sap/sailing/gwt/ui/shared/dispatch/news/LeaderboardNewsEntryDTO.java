229adaf03e1be0d64a27b38ff85ca918323c76ae
Frederik Petersen <frederik.petersen@sap.com>
2015-06-21 16:02:05 +0200

    Added conditional time formatting for UpdatesBoxItems

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/dispatch/news/LeaderboardNewsEntryDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/dispatch/news/LeaderboardNewsEntryDTO.java
@@ -1,5 +1,7 @@
 package com.sap.sailing.gwt.ui.shared.dispatch.news;
 
+import java.util.Date;
+
 import com.google.gwt.core.shared.GwtIncompatible;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sailing.news.impl.LeaderboardUpdateNewsItem;
@@ -19,8 +21,8 @@ public class LeaderboardNewsEntryDTO extends NewsEntryDTO {
     }
 
     @GwtIncompatible
-    public LeaderboardNewsEntryDTO(LeaderboardUpdateNewsItem item) {
-        super(item.getTitle(), item.getCreatedAtDate(), null);
+    public LeaderboardNewsEntryDTO(LeaderboardUpdateNewsItem item, Date currentTimestamp) {
+        super(item.getTitle(), item.getCreatedAtDate(), currentTimestamp, null);
         this.boatClassName = item.getBoatClass();
         this.type = Type.RESULTS_UPDATE;
         this.leaderboardName = item.getLeaderboardName();
