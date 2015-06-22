9cd1ab891952c16b43dff0ffa6245d7b5117b6df
Axel Uhl <axel.uhl@sap.com>
2015-06-22 14:59:10 +0200

    fix for bug 2995: require handicap functions to be serializable

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/ranking/TimeOnDistanceAllowancePerNauticalMileMap.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/ranking/TimeOnDistanceAllowancePerNauticalMileMap.java
@@ -4,6 +4,6 @@ import com.sap.sailing.domain.base.Competitor;
 import com.sap.sse.common.Duration;
 import com.sap.sse.util.SerializableFunction;
 
+@FunctionalInterface
 public interface TimeOnDistanceAllowancePerNauticalMileMap extends SerializableFunction<Competitor, Duration> {
-
 }
