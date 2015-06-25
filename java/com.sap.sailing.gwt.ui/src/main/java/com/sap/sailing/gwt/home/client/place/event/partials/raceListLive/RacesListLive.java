41efb83fbba489856a007ba421c983ab0ccb7d52
Benjamin Barth <benjamin.barth@oio.de>
2015-06-25 15:54:48 +0200

    Races lists - Refactored race list structure for easier reuse

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/partials/raceListLive/RacesListLive.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/partials/raceListLive/RacesListLive.java
@@ -1,29 +1,24 @@
 package com.sap.sailing.gwt.home.client.place.event.partials.raceListLive;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Display;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.home.client.place.event.EventView;
-import com.sap.sailing.gwt.home.client.place.event.partials.racelist.RaceList;
+import com.sap.sailing.gwt.home.client.place.event.partials.racelist.AbstractRaceList;
+import com.sap.sailing.gwt.home.client.place.event.partials.racelist.RaceListColumnFactory;
+import com.sap.sailing.gwt.home.client.place.event.partials.racelist.RaceListContainer;
+import com.sap.sailing.gwt.home.client.place.event.partials.racelist.SortableRaceListColumn;
 import com.sap.sailing.gwt.home.client.place.event.regatta.tabs.reload.RefreshableWidget;
+import com.sap.sailing.gwt.ui.client.StringMessages;
+import com.sap.sailing.gwt.ui.shared.dispatch.event.LiveRaceDTO;
 import com.sap.sailing.gwt.ui.shared.dispatch.event.LiveRacesDTO;
 
 public class RacesListLive extends Composite implements RefreshableWidget<LiveRacesDTO> {
 
-    private static RacesListLiveUiBinder uiBinder = GWT.create(RacesListLiveUiBinder.class);
-
-    interface RacesListLiveUiBinder extends UiBinder<Widget, RacesListLive> {
-    }
-
-    @UiField(provided = true) RaceList raceList;
+    private final RaceListLiveRaces raceList;
     
     public RacesListLive(EventView.Presenter presenter, boolean showRegattaDetails) {
-        RacesListLiveResources.INSTANCE.css().ensureInjected();
-        raceList = new RaceList(presenter, showRegattaDetails);
-        initWidget(uiBinder.createAndBindUi(this));
+        raceList = new RaceListLiveRaces(presenter, showRegattaDetails);
+        initWidget(new RaceListContainer<LiveRaceDTO>(StringMessages.INSTANCE.liveNow(), raceList));
         getElement().getStyle().setDisplay(Display.NONE);
     }
 
@@ -33,8 +28,49 @@ public class RacesListLive extends Composite implements RefreshableWidget<LiveRa
             getElement().getStyle().setDisplay(Display.NONE);
         } else {
             getElement().getStyle().clearDisplay();
-            this.raceList.setListData(data);
+            raceList.setListData(data);
         }
     }
+    
+    private class RaceListLiveRaces extends AbstractRaceList<LiveRaceDTO> {
 
+        private final SortableRaceListColumn<LiveRaceDTO, ?> regattaNameColumn = RaceListColumnFactory.getRegattaNameColumn();
+        private final SortableRaceListColumn<LiveRaceDTO, ?> flagsColumn = RaceListColumnFactory.getFlagsColumn();
+        private final SortableRaceListColumn<LiveRaceDTO, ?> windSpeedColumn = RaceListColumnFactory.getWindSpeedColumn();
+        private final SortableRaceListColumn<LiveRaceDTO, ?> windDirectionColumn = RaceListColumnFactory.getWindDirectionColumn();
+        private final SortableRaceListColumn<LiveRaceDTO, ?> raceViewStateColumn = RaceListColumnFactory.getRaceViewStateColumn();
+
+        public RaceListLiveRaces(EventView.Presenter presenter, boolean showRegattaDetails) {
+            super(presenter);
+            this.regattaNameColumn.setShowDetails(showRegattaDetails);
+        }
+
+        public void setListData(LiveRacesDTO data) {
+            boolean hasFleets = data.hasFleets();
+            this.fleetCornerColumn.setShowDetails(hasFleets);
+            this.fleetNameColumn.setShowDetails(hasFleets);
+            this.courseAreaColumn.setShowDetails(data.hasCourseAreas());
+            this.courseColumn.setShowDetails(data.hasCourses());
+            boolean hasWind = data.hasWind();
+            this.windSpeedColumn.setShowDetails(hasWind);
+            this.windDirectionColumn.setShowDetails(hasWind);
+            setTableData(data.getRaces());
+        }
+
+        @Override
+        protected void initTableColumns() {
+            add(fleetCornerColumn);
+            add(regattaNameColumn);
+            add(raceNameColumn);
+            add(fleetNameColumn);
+            add(startTimeColumn);
+            add(flagsColumn);
+            add(windSpeedColumn);
+            add(windDirectionColumn);
+            add(courseAreaColumn);
+            add(courseColumn);
+            add(raceViewStateColumn);
+            add(raceViewerButtonColumn);
+        }
+    }
 }
---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/partials/racelist/RaceListContainer.java
@@ -0,0 +1,43 @@
+package com.sap.sailing.gwt.home.client.place.event.partials.racelist;
+
+
+import java.util.List;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.HeadingElement;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.gwt.home.client.place.event.partials.raceListLive.RacesListLiveResources;
+import com.sap.sailing.gwt.ui.shared.race.RaceMetadataDTO;
+
+public class RaceListContainer<T extends RaceMetadataDTO> extends Composite {
+
+    private static RaceListContainerUiBinder uiBinder = GWT.create(RaceListContainerUiBinder.class);
+
+    interface RaceListContainerUiBinder extends UiBinder<Widget, RaceListContainer<?>> {
+    }
+    
+    @UiField HeadingElement titleUi;
+    @UiField DivElement infoUi;
+    @UiField(provided = true) AbstractRaceList<T> raceListUi;
+
+    public RaceListContainer(String title, AbstractRaceList<T> raceList) {
+        RacesListLiveResources.INSTANCE.css().ensureInjected();
+        this.raceListUi = raceList;
+        initWidget(uiBinder.createAndBindUi(this));
+        this.titleUi.setInnerText(title);
+    }
+    
+    public void setRaceListData(List<T> data) {
+        this.raceListUi.setTableData(data);
+    }
+    
+    public void setInfoText(String infoText) {
+        this.infoUi.setInnerText(infoText);
+        this.infoUi.getStyle().clearDisplay();
+    }
+
+}
