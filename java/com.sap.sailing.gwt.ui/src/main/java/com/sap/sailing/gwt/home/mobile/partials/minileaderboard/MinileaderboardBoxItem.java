8eb5e4b163508862760b834bbb1363b23b035fbd
Steffen Schaefer <steffen.schaefer@oio.de>
2015-07-06 13:31:36 +0200

    Using util to resolve the flag image URI

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/minileaderboard/MinileaderboardBoxItem.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/minileaderboard/MinileaderboardBoxItem.java
@@ -3,7 +3,7 @@ package com.sap.sailing.gwt.home.mobile.partials.minileaderboard;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.ImageElement;
-import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.safehtml.shared.SafeUri;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
@@ -37,19 +37,8 @@ public class MinileaderboardBoxItem extends Widget {
         competitorPointsUi.setInnerText(StringMessages.INSTANCE.pointsValue(entry.getPoints()));
         
         SimpleCompetitorDTO competitor = entry.getCompetitor();
-        String flagImageURL = competitor.getFlagImageURL();
-
-        if (flagImageURL == null || flagImageURL.isEmpty()) {
-            String twoLetterIsoCountryCode = competitor.getTwoLetterIsoCountryCode();
-            final ImageResource flagImageResource;
-            if (twoLetterIsoCountryCode==null || twoLetterIsoCountryCode.isEmpty()) {
-                flagImageResource = FlagImageResolver.getEmptyFlagImageResource();
-            } else {
-                flagImageResource = FlagImageResolver.getFlagImageResource(twoLetterIsoCountryCode);
-            }
-            flagImageURL = flagImageResource.getSafeUri().asString();
-        }
-        competitorFlagUi.setSrc(flagImageURL);
+        SafeUri imageUri = FlagImageResolver.getFlagImageResource(competitor.getFlagImageURL(), competitor.getTwoLetterIsoCountryCode());
+        competitorFlagUi.setSrc(imageUri.asString());
         
     }
 
