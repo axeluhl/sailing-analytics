6c3336c987468165e9ca5ed0b25d56a457090e35
Benjamin Barth <benjamin.barth@oio.de>
2015-07-08 14:16:34 +0200

    Regatta list - Adjusted temporary regatta progress layout

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/partials/multiRegattaList/MultiRegattaListStepsBody.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/partials/multiRegattaList/MultiRegattaListStepsBody.java
@@ -3,8 +3,10 @@ package com.sap.sailing.gwt.home.client.place.event.partials.multiRegattaList;
 import java.util.Map.Entry;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.dom.client.Style.FontWeight;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.UIObject;
@@ -20,18 +22,21 @@ public class MultiRegattaListStepsBody extends UIObject {
 
     public MultiRegattaListStepsBody(RegattaProgressSeriesDTO seriesProgress) {
         Element main = uiBinder.createAndBindUi(this);
-        main.appendChild(getDetailsItem(seriesProgress.getName()));
-        main.appendChild(getDetailsItem("Race count:" + seriesProgress.getTotalRaceCount()));
+        SpanElement nameElement = getDetailsItem(seriesProgress.getName());
+        nameElement.getStyle().setFontWeight(FontWeight.BOLD);
+        main.appendChild(nameElement);
+        main.appendChild(getDetailsItem("Total - " + seriesProgress.getTotalRaceCount()));
         for (Entry<FleetMetadataDTO, Integer> fleetState : seriesProgress.getFleetState().entrySet()) {
-            DivElement div = getDetailsItem(fleetState.getKey().getFleetName() + " - " + fleetState.getValue());
-            div.getStyle().setColor(fleetState.getKey().getFleetColor());
-            main.appendChild(div);
+            SpanElement element = getDetailsItem(fleetState.getKey().getFleetName() + " - " + fleetState.getValue());
+            element.getStyle().setColor(fleetState.getKey().getFleetColor());
+            main.appendChild(element);
         }
         setElement(main);
     }
     
-    private DivElement getDetailsItem(String text) {
-        DivElement detailsItem = DOM.createDiv().cast();
+    private SpanElement getDetailsItem(String text) {
+        SpanElement detailsItem = DOM.createSpan().cast();
+        detailsItem.getStyle().setPaddingRight(0.75, Unit.EM);
         detailsItem.setInnerText(text);
         return detailsItem;
     }
