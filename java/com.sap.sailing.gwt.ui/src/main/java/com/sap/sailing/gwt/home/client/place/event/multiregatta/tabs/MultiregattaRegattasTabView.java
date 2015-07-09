e1c498a5f4ae27e7691e07857ac27356146de84c
Benjamin Barth <benjamin.barth@oio.de>
2015-07-09 13:29:09 +0200

    Regatta list - Integrated filter into multi regattas regatta list

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/multiregatta/tabs/MultiregattaRegattasTabView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/multiregatta/tabs/MultiregattaRegattasTabView.java
@@ -1,5 +1,7 @@
 package com.sap.sailing.gwt.home.client.place.event.multiregatta.tabs;
 
+import java.util.Collection;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -10,17 +12,24 @@ import com.sap.sailing.gwt.common.client.controls.tabbar.TabView;
 import com.sap.sailing.gwt.home.client.place.event.multiregatta.EventMultiregattaView;
 import com.sap.sailing.gwt.home.client.place.event.multiregatta.EventMultiregattaView.Presenter;
 import com.sap.sailing.gwt.home.client.place.event.multiregatta.MultiregattaTabView;
+import com.sap.sailing.gwt.home.client.place.event.partials.listNavigation.DropdownFilter;
+import com.sap.sailing.gwt.home.client.place.event.partials.listNavigation.DropdownFilter.DropdownFilterList;
 import com.sap.sailing.gwt.home.client.place.event.partials.multiRegattaList.MultiRegattaList;
 import com.sap.sailing.gwt.home.client.place.event.partials.raceListLive.RacesListLive;
 import com.sap.sailing.gwt.home.client.place.event.regatta.tabs.reload.RefreshManager;
+import com.sap.sailing.gwt.home.client.place.event.regatta.tabs.reload.RefreshableWidget;
+import com.sap.sailing.gwt.ui.client.StringMessages;
+import com.sap.sailing.gwt.ui.shared.dispatch.SortedSetResult;
 import com.sap.sailing.gwt.ui.shared.dispatch.event.GetLiveRacesForEventAction;
 import com.sap.sailing.gwt.ui.shared.dispatch.event.GetRegattaListViewAction;
+import com.sap.sailing.gwt.ui.shared.dispatch.regatta.RegattaWithProgressDTO;
 
 /**
  * Created by pgtaboada on 25.11.14.
  */
 public class MultiregattaRegattasTabView extends Composite implements MultiregattaTabView<MultiregattaRegattasPlace> {
     
+    @UiField(provided = true) DropdownFilter<String> boatCategoryFilterUi;
     @UiField(provided = true) RacesListLive racesListLiveUi;
     @UiField(provided = true) MultiRegattaList regattaListUi;
     private Presenter currentPresenter;
@@ -46,12 +55,14 @@ public class MultiregattaRegattasTabView extends Composite implements Multiregat
 
     @Override
     public void start(final MultiregattaRegattasPlace myPlace, final AcceptsOneWidget contentArea) {
+        MultiRegattasTabViewRegattaFilterList regattaFilterList = new MultiRegattasTabViewRegattaFilterList();
+        boatCategoryFilterUi = new DropdownFilter<String>(StringMessages.INSTANCE.allBoatClasses(), regattaFilterList);
         racesListLiveUi = new RacesListLive(currentPresenter, true);
         regattaListUi = new MultiRegattaList(currentPresenter);
         initWidget(ourUiBinder.createAndBindUi(this));
         RefreshManager refreshManager = new RefreshManager(this, currentPresenter.getDispatch());
         refreshManager.add(racesListLiveUi, new GetLiveRacesForEventAction(currentPresenter.getCtx().getEventDTO().getId()));
-        refreshManager.add(regattaListUi, new GetRegattaListViewAction(currentPresenter.getCtx().getEventDTO().getId()));
+        refreshManager.add(regattaFilterList, new GetRegattaListViewAction(currentPresenter.getCtx().getEventDTO().getId()));
         contentArea.setWidget(this);
     }
 
@@ -67,6 +78,26 @@ public class MultiregattaRegattasTabView extends Composite implements Multiregat
     @Override
     public MultiregattaRegattasPlace placeToFire() {
         return new MultiregattaRegattasPlace(currentPresenter.getCtx());
+    }
+    
+    private class MultiRegattasTabViewRegattaFilterList implements DropdownFilterList<String>, RefreshableWidget<SortedSetResult<RegattaWithProgressDTO>> {
+        
+        @Override
+        public void setData(SortedSetResult<RegattaWithProgressDTO> data, long nextUpdate, int updateNo) {
+            regattaListUi.setData(data, nextUpdate, updateNo);
+            boatCategoryFilterUi.updateFilterValues();
+        }
+
+        @Override
+        public Collection<String> getSelectableValues() {
+            return regattaListUi.getSelectableBoatCategories();
+        }
+
+        @Override
+        public void onSelectFilter(String value) {
+            regattaListUi.setVisibleBoatCategory(value);
+        }
+        
     }
 
 }
