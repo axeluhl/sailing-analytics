83dd8d53baa1bbb5381456107ec4890774f2c97a
Steffen Schaefer <steffen.schaefer@oio.de>
2015-07-13 13:10:37 +0200

    Improved layouting of the regatta progress steps

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/partials/multiRegattaList/MultiRegattaListSteps.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/partials/multiRegattaList/MultiRegattaListSteps.java
@@ -1,8 +1,11 @@
 package com.sap.sailing.gwt.home.client.place.event.partials.multiRegattaList;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.AnchorElement;
 import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
@@ -18,6 +21,7 @@ public class MultiRegattaListSteps extends Composite {
     interface MultiRegattaListStepsUiBinder extends UiBinder<Widget, MultiRegattaListSteps> {
     }
     
+    @UiField DivElement progressContainerUi;
     @UiField DivElement stepsContainerUi;
     @UiField DivElement leaderboardButtonContainerUi;
     @UiField AnchorElement leaderboardButtonUi;
@@ -26,13 +30,19 @@ public class MultiRegattaListSteps extends Composite {
         MultiRegattaListResources.INSTANCE.css().ensureInjected();
         initWidget(uiBinder.createAndBindUi(this));
         for(RegattaProgressSeriesDTO seriesProgress : regattaProgress.getSeries()) {
-            stepsContainerUi.insertBefore(new MultiRegattaListStepsBody(seriesProgress).getElement(), leaderboardButtonContainerUi);
+            stepsContainerUi.appendChild(new MultiRegattaListStepsBody(seriesProgress).getElement());
         }
     }
     
     void setLeaderboardNavigation(PlaceNavigation<?> placeNavigation) {
         placeNavigation.configureAnchorElement(leaderboardButtonUi);
         leaderboardButtonUi.getStyle().clearDisplay();
+        Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+            @Override
+            public void execute() {
+                stepsContainerUi.getStyle().setMarginRight(leaderboardButtonUi.getOffsetWidth() + 20, Unit.PX);
+            }
+        });
     }
 
 }
