947ba07204017788faafa04d3b10536b036763ec
Alexander Ries <alexander.ries@sap.com>
2015-07-14 10:10:13 +0200

    addded distanceToRCBoatInMeters to StartLineAdvantageDTO

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/shared/dto/StartLineAdvantageDTO.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/shared/dto/StartLineAdvantageDTO.java
@@ -1,16 +1,14 @@
 package com.sap.sailing.dashboards.gwt.shared.dto;
 
-import java.io.Serializable;
-
+import com.google.gwt.user.client.rpc.IsSerializable;
 import com.sap.sailing.dashboards.gwt.client.startanalysis.StartlineAdvantageType;
 
-public class StartLineAdvantageDTO implements Serializable{
-    
-    private static final long serialVersionUID = 502350559068890424L;
+public class StartLineAdvantageDTO implements IsSerializable {
     
     public StartlineAdvantageType startLineAdvatageType;
     public double startLineAdvantage;
     public double startlineAdvantageAverage;
+    public double distanceToRCBoatInMeters;
     
     public StartLineAdvantageDTO(){}
 }
