dff8016da50c031f5747b84f9bcaabe8e9998179
Steffen Schaefer <steffen.schaefer@oio.de>
2015-07-14 09:40:40 +0200

    Split up in several actions with different refresh intervals and implemented auto refresh for race list

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/dispatch/event/RaceListDataCalculator.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/dispatch/event/RaceListDataCalculator.java
@@ -1,30 +1,25 @@
 package com.sap.sailing.gwt.ui.shared.dispatch.event;
 
-import java.util.UUID;
+import java.util.Collection;
+import java.util.TreeSet;
 
 import com.google.gwt.core.shared.GwtIncompatible;
-import com.sap.sailing.gwt.ui.shared.dispatch.DispatchContext;
-import com.sap.sailing.gwt.ui.shared.dispatch.ResultWithTTL;
 import com.sap.sailing.gwt.ui.shared.dispatch.event.EventActionUtil.RaceCallback;
 
 @GwtIncompatible
 public class RaceListDataCalculator implements RaceCallback {
-    private final RaceRefreshCalculator refreshCalculator = new RaceRefreshCalculator();
-//    private final RegattaProgressCalculator progressCalculator = new RegattaProgressCalculator();
-    private final RaceListViewDTO result = new RaceListViewDTO();
+    private final TreeSet<RaceListRaceDTO> races = new TreeSet<>();
 
     @Override
     public void doForRace(RaceContext context) {
-        refreshCalculator.doForRace(context);
-//        progressCalculator.doForRace(context);
-        result.add(context.getLiveRaceOrNull());
-        result.add(context.getFinishedRaceOrNull());
+        RaceListRaceDTO finishedRace = context.getFinishedRaceOrNull();
+        if(finishedRace != null) {
+            races.add(finishedRace);
+        }
     }
     
-    public ResultWithTTL<RaceListViewDTO> getResult(DispatchContext context, UUID eventId, String regattaId) {
-        LeaderboardContext leaderboardContext = EventActionUtil.getLeaderboardContext(context, eventId, regattaId);
-        result.setProgress(leaderboardContext.getRegattaWithProgress(context));
-        return new ResultWithTTL<>(EventActionUtil.getEventStateDependentTTL(context, eventId, refreshCalculator.getTTL()), result);
+    public Collection<RaceListRaceDTO> getResult() {
+        return races;
     }
 
 }
