ce09b3f4a95165b73e1ad53cb5f56e15df373ae9
Alexander Ries <alexander.ries@sap.com>
2015-07-14 15:39:30 +0200

    added color theme

---
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/resources/DashboardResources.java
@@ -0,0 +1,18 @@
+package com.sap.sailing.dashboards.gwt.client.resources;
+
+/**
+ * @author Alexander Ries (D062114)
+ *
+ */
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+ 
+public interface DashboardResources extends ClientBundle {
+    
+    interface Style extends CssResource {
+        // Your classes here
+    }
+    // We only need to source the theme file
+    @Source({"colors.gss", "Ribstyle.css"})
+    Style style();
+}
