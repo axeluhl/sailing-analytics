c31a50e100a7820ce7b51b1b88719f0fd8dd1569
Benjamin Barth <benjamin.barth@oio.de>
2015-07-15 11:14:18 +0200

    Regatta/races list - Added String messages for regatta overview link

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/StringMessages.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/StringMessages.java
@@ -1348,4 +1348,5 @@ public interface StringMessages extends com.sap.sse.gwt.client.StringMessages, c
     String miniLeeaderboardLegendText();
     String currentOfTotal(int current, int total);
     String noFinishedRaces();
+    String racesOverview();
 }
