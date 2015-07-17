466b093635e7190d5434e560eb0716017c9206dc
Frederik Petersen <frederik.petersen@sap.com>
2015-07-17 16:48:25 +0200

    Now calling new setMinAndMax combined method

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/TimePanel.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/TimePanel.java
@@ -396,13 +396,9 @@ public class TimePanel<T extends TimePanelSettings> extends SimplePanel implemen
         assert min != null && max != null;
                 
         boolean changed = false;
-        if (!max.equals(timeRangeProvider.getToTime())) {
+        if (!max.equals(timeRangeProvider.getToTime()) || !min.equals(timeRangeProvider.getFromTime())) {
             changed = true;
-            timeSlider.setMaxValue(new Double(max.getTime()), fireEvent);
-        }
-        if (!min.equals(timeRangeProvider.getFromTime())) {
-            changed = true;
-            timeSlider.setMinValue(new Double(min.getTime()), fireEvent);
+            timeSlider.setMinAndMaxValue(new Double(min.getTime()),new Double(max.getTime()), fireEvent);
         }
         if (changed) {
             if (!timeRangeProvider.isZoomed()) {
