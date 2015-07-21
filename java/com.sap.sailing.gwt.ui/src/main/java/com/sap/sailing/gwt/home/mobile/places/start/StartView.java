bac40991b7fd85a984aa106f9d44073419d87cca
Steffen Schaefer <steffen.schaefer@oio.de>
2015-07-21 11:35:39 +0200

    Added method to set the Quickfinder values

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/start/StartView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/start/StartView.java
@@ -1,10 +1,15 @@
 package com.sap.sailing.gwt.home.mobile.places.start;
 
+import java.util.Collection;
+
 import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.gwt.ui.shared.general.EventReferenceDTO;
 
 public interface StartView {
 
     Widget asWidget();
+    
+    void setQuickFinderValues(Collection<EventReferenceDTO> events);
     
     public interface Presenter {
         void gotoEvents();
--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/start/StartViewImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/start/StartViewImpl.java
@@ -1,9 +1,15 @@
 package com.sap.sailing.gwt.home.mobile.places.start;
 
+import java.util.Collection;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.gwt.home.mobile.partials.quickfinder.Quickfinder;
+import com.sap.sailing.gwt.home.mobile.places.QuickfinderPresenter;
+import com.sap.sailing.gwt.ui.shared.general.EventReferenceDTO;
 
 public class StartViewImpl extends Composite implements StartView {
     private static StartPageViewUiBinder uiBinder = GWT.create(StartPageViewUiBinder.class);
@@ -13,11 +19,14 @@ public class StartViewImpl extends Composite implements StartView {
 
     private Presenter currentPresenter;
 
+    @UiField Quickfinder quickFinderUi;
 
     public StartViewImpl(Presenter presenter) {
         this.currentPresenter = presenter;
         initWidget(uiBinder.createAndBindUi(this));
-
     }
     
+    @Override
+    public void setQuickFinderValues(Collection<EventReferenceDTO> events) {
+    }
 }
