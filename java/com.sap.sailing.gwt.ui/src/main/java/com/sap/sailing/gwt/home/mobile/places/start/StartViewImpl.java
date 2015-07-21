dded78c92748df34fae908bdcd3ee33c7e68892b
Papick Garcia Taboada <pgt@pgt.de>
2015-07-21 11:04:30 +0200

    Work in progress: start place

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/start/StartViewImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/start/StartViewImpl.java
@@ -1,11 +1,7 @@
 package com.sap.sailing.gwt.home.mobile.places.start;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -17,18 +13,11 @@ public class StartViewImpl extends Composite implements StartView {
 
     private Presenter currentPresenter;
 
-    @UiField
-    protected Anchor eventsLinkUi;
 
     public StartViewImpl(Presenter presenter) {
         this.currentPresenter = presenter;
         initWidget(uiBinder.createAndBindUi(this));
 
-    }
-
-    @UiHandler("eventsLinkUi")
-    public void gotoEvents(ClickEvent e) {
-        currentPresenter.gotoEvents();
     }
     
 }
