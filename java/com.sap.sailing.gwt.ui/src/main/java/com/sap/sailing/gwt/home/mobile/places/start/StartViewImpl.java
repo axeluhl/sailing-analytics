abd53b79ed1161a59b626901de5359acbb7da6c1
Steffen Schaefer <steffen.schaefer@oio.de>
2015-07-21 15:10:04 +0200

    Grouping events by recent and upcoming in StartActivity quickfinder

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/start/StartViewImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/start/StartViewImpl.java
@@ -8,12 +8,15 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.gwt.common.client.i18n.TextMessages;
 import com.sap.sailing.gwt.home.mobile.partials.quickfinder.Quickfinder;
 import com.sap.sailing.gwt.home.mobile.partials.stage.Stage;
 import com.sap.sailing.gwt.home.shared.app.PlaceNavigation;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sailing.gwt.ui.shared.dispatch.start.EventQuickfinderDTO;
 import com.sap.sailing.gwt.ui.shared.start.EventStageDTO;
+import com.sap.sse.common.TimePoint;
+import com.sap.sse.common.impl.MillisecondsTimePoint;
 
 public class StartViewImpl extends Composite implements StartView {
     private static StartPageViewUiBinder uiBinder = GWT.create(StartPageViewUiBinder.class);
@@ -46,9 +49,16 @@ public class StartViewImpl extends Composite implements StartView {
         // TODO change message: recent events
         quickFinderUi.addPlaceholderItem(i18n.events());
         
+        TimePoint now = MillisecondsTimePoint.now();
         for (EventQuickfinderDTO event : events) {
+            String group;
+            if(now.before(event.getStartTimePoint())) {
+                group = TextMessages.INSTANCE.seriesHeaderUpcoming();
+            } else {
+                group = TextMessages.INSTANCE.searchResultSortNewest();
+            }
             PlaceNavigation<?> eventPlaceNavigation = currentPresenter.getEventNavigation(event);
-            quickFinderUi.addItem(event.getDisplayName(), eventPlaceNavigation);
+            quickFinderUi.addItemToGroup(group, event.getDisplayName(), eventPlaceNavigation);
         }
     }
 }
