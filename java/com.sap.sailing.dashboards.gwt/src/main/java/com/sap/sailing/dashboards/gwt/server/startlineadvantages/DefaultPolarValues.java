5210ed645d0d0fdd9c24852017bbf58df04c4056
Alexander Ries <alexander.ries@sap.com>
2015-07-24 18:28:31 +0200

    introduced default polar data service

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/server/startlineadvantages/DefaultPolarValues.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/server/startlineadvantages/DefaultPolarValues.java
@@ -1,10 +1,23 @@
 package com.sap.sailing.dashboards.gwt.server.startlineadvantages;
 
+import com.sap.sailing.domain.common.ManeuverType;
+
 /**
  * @author Alexander Ries (D062114)
  *
  */
-public enum DefaultPolarValues {
+public class DefaultPolarValues {
 
+    private final static double TACKING_ANGLE = 90;
+    private final static double JIBING_ANGLE = 30;
     
+    public static double getManouvreAngle(ManeuverType maneuverType) {
+        double result = 0;
+        if (maneuverType.equals(ManeuverType.TACK)) {
+            result = TACKING_ANGLE * 2;
+        } else if (maneuverType.equals(ManeuverType.JIBE)) {
+            result = JIBING_ANGLE * 2;
+        }
+        return result;
+    }
 }
