4d561f664b353f3c07ade0b7c14b2d2638890cce
Steffen Schaefer <steffen.schaefer@oio.de>
2015-07-29 12:34:08 +0200

    Implemented up-navigation for logo of mobile series header

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/series/minileaderboard/SeriesMiniOverallLeaderboardView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/series/minileaderboard/SeriesMiniOverallLeaderboardView.java
@@ -6,6 +6,7 @@ import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.home.client.place.fakeseries.SeriesContext;
 import com.sap.sailing.gwt.home.mobile.places.SeriesLeaderboardNavigationProvider;
 import com.sap.sailing.gwt.home.mobile.places.series.OverallLeaderboardNavigationProvider;
+import com.sap.sailing.gwt.home.shared.app.PlaceNavigation;
 import com.sap.sailing.gwt.home.shared.dispatch.DispatchSystem;
 import com.sap.sailing.gwt.ui.shared.general.EventMetadataDTO;
 
@@ -15,6 +16,8 @@ public interface SeriesMiniOverallLeaderboardView {
         DispatchSystem getDispatch();
 
         SeriesContext getCtx();
+
+        PlaceNavigation<?> getSeriesNavigation();
     }
 
     Widget asWidget();
