c5c566f9da998586dce93ba1c68744a4e081bb44
Steffen Schaefer <steffen.schaefer@oio.de>
2015-07-29 15:48:48 +0200

    Added regatta progress to the single regatta event overview tab

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/regatta/tabs/RegattaOverviewTabView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/regatta/tabs/RegattaOverviewTabView.java
@@ -6,14 +6,20 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.sap.sailing.gwt.common.client.controls.tabbar.TabView;
 import com.sap.sailing.gwt.home.client.place.event.overview.EventOverviewStage;
+import com.sap.sailing.gwt.home.client.place.event.partials.multiRegattaList.MultiRegattaListItem;
+import com.sap.sailing.gwt.home.client.place.event.partials.multiRegattaList.MultiRegattaListStepsLegend;
 import com.sap.sailing.gwt.home.client.place.event.partials.raceListLive.RacesListLive;
 import com.sap.sailing.gwt.home.client.place.event.regatta.EventRegattaView;
 import com.sap.sailing.gwt.home.client.place.event.regatta.EventRegattaView.Presenter;
 import com.sap.sailing.gwt.home.client.place.event.regatta.RegattaTabView;
 import com.sap.sailing.gwt.home.client.place.event.regatta.tabs.reload.RefreshManager;
+import com.sap.sailing.gwt.home.client.place.event.regatta.tabs.reload.RefreshableWidget;
 import com.sap.sailing.gwt.ui.shared.dispatch.event.GetLiveRacesForRegattaAction;
+import com.sap.sailing.gwt.ui.shared.dispatch.event.GetRegattaWithProgressAction;
+import com.sap.sailing.gwt.ui.shared.dispatch.regatta.RegattaWithProgressDTO;
 import com.sap.sailing.gwt.ui.shared.eventview.EventViewDTO.EventType;
 
 /**
@@ -43,6 +49,12 @@ public class RegattaOverviewTabView extends Composite implements RegattaTabView<
         initWidget(ourUiBinder.createAndBindUi(this));
         
         RefreshManager refreshManager = new RefreshManager(this, currentPresenter.getDispatch());
+        refreshManager.add(new RefreshableWidget<RegattaWithProgressDTO>() {
+            @Override
+            public void setData(RegattaWithProgressDTO data) {
+                regattaInfoContainerUi.setWidget(new MultiRegattaListItem(data, true));
+            }
+        }, new GetRegattaWithProgressAction(myPlace.getCtx().getEventDTO().getId(), myPlace.getRegattaId()));
 
         stage.setupRefresh(refreshManager);
         refreshManager.add(racesListLive.getRefreshable(), new GetLiveRacesForRegattaAction(currentPresenter.getCtx().getEventDTO()
@@ -62,6 +74,8 @@ public class RegattaOverviewTabView extends Composite implements RegattaTabView<
     private static MyBinder ourUiBinder = GWT.create(MyBinder.class);
     private Presenter currentPresenter;
     
+    @UiField MultiRegattaListStepsLegend regattaProgressLegendUi;
+    @UiField SimplePanel regattaInfoContainerUi;
     @UiField(provided = true)
     RacesListLive racesListLive;
     @UiField(provided = true) EventOverviewStage stage;
