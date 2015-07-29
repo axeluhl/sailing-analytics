cbf50d1ff008ab65ce754c7225e7430d48204d79
Alexander Ries <alexander.ries@sap.com>
2015-07-29 15:57:38 +0200

    removed getBoatSpeedForWindAngleAndSpeed() from DefaultPolarValues

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/server/startlineadvantages/DefaultPolarValues.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/server/startlineadvantages/DefaultPolarValues.java
@@ -10,7 +10,6 @@ public class DefaultPolarValues {
 
     private final static double TACKING_ANGLE = 40;
     private final static double JIBING_ANGLE = 30;
-    private final static double BOAT_SPEED = 10;
     
     public static double getManouvreAngle(ManeuverType maneuverType) {
         double result = 0;
@@ -18,16 +17,6 @@ public class DefaultPolarValues {
             result = TACKING_ANGLE * 2;
         } else if (maneuverType.equals(ManeuverType.JIBE)) {
             result = JIBING_ANGLE * 2;
-        }
-        return result;
-    }
-    
-    public static double getBoatSpeedForWindAngleAndSpeed(double angle, double speed) {
-        double result = BOAT_SPEED;
-        if(angle <= 180) {
-            result = BOAT_SPEED*(angle/1000);
-        } if(angle > 180) {
-            result = BOAT_SPEED*((360-angle)/1000);
         }
         return result;
     }
