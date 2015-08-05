a5af499e6d6c466e5bd08b94d3c57797c5df1c0a
Benjamin Barth <benjamin.barth@oio.de>
2015-08-05 11:30:18 +0200

    Fullscreen Leaderboard - Extracted FullscreenLeaderboardDelegate to be able to reuse it

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/oldleaderboard/FullscreenViewerLeaderboardDelegate.java
@@ -0,0 +1,75 @@
+package com.sap.sailing.gwt.home.client.place.event.oldleaderboard;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.gwt.home.client.place.event.oldleaderboard.OldLeaderboard.LeaderboardDelegate;
+import com.sap.sailing.gwt.home.shared.partials.fullscreen.FullscreenViewer;
+import com.sap.sailing.gwt.ui.leaderboard.LeaderboardPanel;
+
+public class FullscreenViewerLeaderboardDelegate extends FullscreenViewer implements LeaderboardDelegate {
+    
+    private final Image autoRefreshControl = new Image("images/home/reload.png");
+    private final Image settingsControl = new Image("images/home/settings.png");
+    
+    private final Label lastScoringUpdateTime = new Label();
+    private final Label lastScoringUpdateText = new Label();
+    private final Label lastScoringComment = new Label();
+    private final Label scoringScheme = new Label();
+    private final Label hasLiveRace = new Label();
+    
+    public FullscreenViewerLeaderboardDelegate() {
+        setHeaderWidget(createPanel(lastScoringComment, hasLiveRace, scoringScheme));
+        addToolbarInfo(createPanel(lastScoringUpdateText, lastScoringUpdateTime));
+        addToolbarAction(autoRefreshControl);
+        addToolbarAction(settingsControl);
+    }
+    
+    private Widget createPanel(Widget... contentWidgets) {
+        FlowPanel panel = new FlowPanel();
+        for (Widget widget : contentWidgets) panel.add(widget);
+        return panel;
+    }
+    
+    @Override
+    public void setLeaderboard(LeaderboardPanel leaderboardPanel) {
+        showContent(leaderboardPanel);
+    }
+
+    @Override
+    public Widget getAutoRefreshControl() {
+        return autoRefreshControl;
+    }
+
+    @Override
+    public Widget getSettingsControl() {
+        return settingsControl;
+    }
+    
+    @Override
+    public Element getLastScoringUpdateTimeElement() {
+        return lastScoringUpdateTime.getElement();
+    }
+    
+    @Override
+    public Element getLastScoringUpdateTextElement() {
+        return lastScoringUpdateText.getElement();
+    }
+    
+    @Override
+    public Element getLastScoringCommentElement() {
+        return lastScoringComment.getElement();
+    }
+    
+    @Override
+    public Element getHasLiveRaceElement() {
+        return hasLiveRace.getElement();
+    }
+    
+    @Override
+    public Element getScoringSchemeElement() {
+        return scoringScheme.getElement();
+    }
+}
--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/regatta/tabs/RegattaLeaderboardTabView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/place/event/regatta/tabs/RegattaLeaderboardTabView.java
@@ -1,26 +1,21 @@
 package com.sap.sailing.gwt.home.client.place.event.regatta.tabs;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.domain.common.RaceIdentifier;
 import com.sap.sailing.domain.common.dto.LeaderboardDTO;
 import com.sap.sailing.domain.common.dto.RaceColumnDTO;
 import com.sap.sailing.gwt.common.client.controls.tabbar.TabView;
+import com.sap.sailing.gwt.home.client.place.event.oldleaderboard.FullscreenViewerLeaderboardDelegate;
 import com.sap.sailing.gwt.home.client.place.event.oldleaderboard.OldLeaderboard;
-import com.sap.sailing.gwt.home.client.place.event.oldleaderboard.OldLeaderboard.LeaderboardDelegate;
 import com.sap.sailing.gwt.home.client.place.event.regatta.EventRegattaView;
 import com.sap.sailing.gwt.home.client.place.event.regatta.EventRegattaView.Presenter;
 import com.sap.sailing.gwt.home.client.place.event.regatta.RegattaAnalyticsDataManager;
 import com.sap.sailing.gwt.home.client.shared.placeholder.Placeholder;
-import com.sap.sailing.gwt.home.shared.partials.fullscreen.FullscreenViewer;
 import com.sap.sailing.gwt.ui.leaderboard.LeaderboardPanel;
 import com.sap.sailing.gwt.ui.shared.general.EventState;
 
@@ -39,7 +34,7 @@ public class RegattaLeaderboardTabView extends SharedLeaderboardRegattaTabView<R
     protected OldLeaderboard leaderboard;
 
     public RegattaLeaderboardTabView() {
-        leaderboard = new OldLeaderboard(new FullscreenViewerLeaderBoardDelegate());
+        leaderboard = new OldLeaderboard(new FullscreenViewerLeaderboardDelegate());
     }
 
     @Override
@@ -107,68 +102,4 @@ public class RegattaLeaderboardTabView extends SharedLeaderboardRegattaTabView<R
         return new RegattaLeaderboardPlace(currentPresenter.getCtx());
     }
     
-    private class FullscreenViewerLeaderBoardDelegate extends FullscreenViewer implements LeaderboardDelegate {
-        
-        private final Image autoRefreshControl = new Image("images/home/reload.png");
-        private final Image settingsControl = new Image("images/home/settings.png");
-        
-        private final Label lastScoringUpdateTime = new Label();
-        private final Label lastScoringUpdateText = new Label();
-        private final Label lastScoringComment = new Label();
-        private final Label scoringScheme = new Label();
-        private final Label hasLiveRace = new Label();
-        
-        private FullscreenViewerLeaderBoardDelegate() {
-            setHeaderWidget(createPanel(lastScoringComment, hasLiveRace, scoringScheme));
-            addToolbarInfo(createPanel(lastScoringUpdateText, lastScoringUpdateTime));
-            addToolbarAction(autoRefreshControl);
-            addToolbarAction(settingsControl);
-        }
-        
-        private Widget createPanel(Widget... contentWidgets) {
-            FlowPanel panel = new FlowPanel();
-            for (Widget widget : contentWidgets) panel.add(widget);
-            return panel;
-        }
-        
-        @Override
-        public void setLeaderboard(LeaderboardPanel leaderboardPanel) {
-            showContent(leaderboardPanel);
-        }
-
-        @Override
-        public Widget getAutoRefreshControl() {
-            return autoRefreshControl;
-        }
-
-        @Override
-        public Widget getSettingsControl() {
-            return settingsControl;
-        }
-        
-        @Override
-        public Element getLastScoringUpdateTimeElement() {
-            return lastScoringUpdateTime.getElement();
-        }
-        
-        @Override
-        public Element getLastScoringUpdateTextElement() {
-            return lastScoringUpdateText.getElement();
-        }
-        
-        @Override
-        public Element getLastScoringCommentElement() {
-            return lastScoringComment.getElement();
-        }
-        
-        @Override
-        public Element getHasLiveRaceElement() {
-            return hasLiveRace.getElement();
-        }
-        
-        @Override
-        public Element getScoringSchemeElement() {
-            return scoringScheme.getElement();
-        }
-    }
 }
