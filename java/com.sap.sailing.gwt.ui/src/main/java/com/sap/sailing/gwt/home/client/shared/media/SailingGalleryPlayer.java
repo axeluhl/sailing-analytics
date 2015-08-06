2febb43691e40dd0fabc0e93328bb4615e62ed93
Steffen Schaefer <steffen.schaefer@oio.de>
2015-08-06 09:01:26 +0200

    Added method to correctly set the focus for the fullscreen gallery slider

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/media/SailingGalleryPlayer.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/client/shared/media/SailingGalleryPlayer.java
@@ -35,6 +35,10 @@ public class SailingGalleryPlayer extends ResizeComposite {
             subSliderUi.appendChild(createThumbImgElement(i));
         }
     }
+    
+    public void setFocusToSubSlider() {
+        subSliderUi.getFirstChildElement().focus();
+    }
 
     private ImageElement createThumbImgElement(SailingImageDTO i ) {
         ImageElement img = Document.get().createImageElement();
