cbd50476f0c51c58c5cd1d885f12f143c5d9fa42
Benjamin Barth <benjamin.barth@oio.de>
2015-08-10 16:48:29 +0200

    Refactored EventDTO structure

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/eventlist/EventListEventDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/eventlist/EventListEventDTO.java
@@ -1,10 +1,16 @@
 package com.sap.sailing.gwt.ui.shared.eventlist;
 
 import com.sap.sailing.gwt.ui.shared.general.EventMetadataDTO;
+import com.sap.sailing.gwt.ui.shared.general.EventState;
+import com.sap.sailing.gwt.ui.shared.start.StageEventType;
 
 public class EventListEventDTO extends EventMetadataDTO {
     private String baseURL;
     private boolean isOnRemoteServer;
+    
+    public StageEventType getStageType() {
+        return getState() == EventState.RUNNING ? StageEventType.RUNNING : StageEventType.POPULAR;
+    }
 
     public String getBaseURL() {
         return baseURL;
