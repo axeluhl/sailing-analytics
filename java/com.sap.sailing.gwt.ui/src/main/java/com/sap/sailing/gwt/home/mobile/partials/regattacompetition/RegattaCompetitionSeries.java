e5d18b47e4f77e3a064c5b800df93f95ff73ffa9
Benjamin Barth <benjamin.barth@oio.de>
2015-08-17 15:40:37 +0200

    Mobile Races - Refactored to use SectionHeaderContents collapsibility

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/regattacompetition/RegattaCompetitionSeries.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/regattacompetition/RegattaCompetitionSeries.java
@@ -3,14 +3,10 @@ package com.sap.sailing.gwt.home.mobile.partials.regattacompetition;
 import static com.sap.sailing.domain.common.LeaderboardNameConstants.DEFAULT_SERIES_NAME;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Widget;
-import com.sap.sailing.gwt.home.client.place.events.CollapseAnimation;
 import com.sap.sailing.gwt.home.mobile.partials.section.MobileSection;
 import com.sap.sailing.gwt.home.mobile.partials.sectionHeader.SectionHeaderContent;
 import com.sap.sailing.gwt.home.mobile.places.races.RacesView.Presenter;
@@ -27,9 +23,7 @@ public class RegattaCompetitionSeries extends Composite {
     interface RegattaCompetitionSeriesUiBinder extends UiBinder<MobileSection, RegattaCompetitionSeries> {
     }
     
-    private static final String ACCORDION_COLLAPSED_STYLE = RegattaCompetitionResources.INSTANCE.css().accordioncollapsed();
     private static final StringMessages I18N = StringMessages.INSTANCE;
-    
     
     @UiField SectionHeaderContent sectionHeaderUi;
     @UiField FlowPanel fleetContainerUi;
@@ -40,7 +34,7 @@ public class RegattaCompetitionSeries extends Composite {
         seriesUi.setEdgeToEdgeContent(true);
         sectionHeaderUi.setSectionTitle(DEFAULT_SERIES_NAME.equals(series.getSeriesName()) ? I18N.races() : series.getSeriesName());
         initSubtitle(series.getCompetitorCount(), series.getRaceCount());
-        initAnimation(sectionHeaderUi, fleetContainerUi, true);
+        sectionHeaderUi.initCollapsibility(fleetContainerUi.getElement(), true);
     }
     
     private void initSubtitle(int competitorCount, int raceCount) {
@@ -52,19 +46,6 @@ public class RegattaCompetitionSeries extends Composite {
         }
     }        
     
-    private void initAnimation(Widget trigger, final Widget content, boolean showInitial) {
-        final CollapseAnimation animation = new CollapseAnimation(content.getElement(), showInitial);
-        content.setStyleName(ACCORDION_COLLAPSED_STYLE, !showInitial);
-        trigger.addDomHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                boolean collapsed = content.getElement().hasClassName(ACCORDION_COLLAPSED_STYLE);
-                content.setStyleName(ACCORDION_COLLAPSED_STYLE, !collapsed);
-                animation.animate(collapsed);
-            }
-        }, ClickEvent.getType());
-    }
-
     public void addFleet(Presenter presenter, RaceCompetitionFormatFleetDTO fleet, int fleetCount) {
         RegattaCompetitionFleet competitionFleet = new RegattaCompetitionFleet(fleet, fleetCount);
         for (SimpleRaceMetadataDTO race : fleet.getRaces()) {
