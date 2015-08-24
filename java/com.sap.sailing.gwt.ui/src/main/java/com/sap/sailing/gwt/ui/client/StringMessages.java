837e8bcd4cd38e2aba85237314f24eaf26ee1b55
Benjamin Barth <benjamin.barth@oio.de>
2015-08-24 17:32:11 +0200

    Added addtional String message

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/StringMessages.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/StringMessages.java
@@ -1367,4 +1367,5 @@ public interface StringMessages extends com.sap.sse.gwt.client.StringMessages, c
     String choosePresentation();
     String openFullscreenView();
     String closeFullscreenView();
+    String progress();
 }
