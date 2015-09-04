b4728028c1e598b6175069617649bf5ee31db5ab
fmittag <fmittag@gmx.net>
2015-09-04 15:40:27 +0200

    Change inheritance hierarchy of MobileApplicationClientFactory to use more of the existing code (to prepare common usage of the standalone server configuration) Some code cleanup in MainView

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/app/MobileApplicationClientFactory.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/app/MobileApplicationClientFactory.java
@@ -4,11 +4,10 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
-import com.google.gwt.user.client.ui.AcceptsOneWidget;
-import com.google.gwt.user.client.ui.Widget;
 import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.event.shared.SimpleEventBus;
 import com.sap.sailing.gwt.home.client.place.start.StartPlace;
+import com.sap.sailing.gwt.home.desktop.app.ApplicationTopLevelView;
 import com.sap.sailing.gwt.home.mobile.places.MainView;
 import com.sap.sailing.gwt.home.shared.dispatch.DispatchSystem;
 import com.sap.sailing.gwt.home.shared.dispatch.DispatchSystemImpl;
@@ -16,39 +15,38 @@ import com.sap.sailing.gwt.ui.client.HomeService;
 import com.sap.sailing.gwt.ui.client.HomeServiceAsync;
 import com.sap.sailing.gwt.ui.client.RemoteServiceMappingConstants;
 import com.sap.sse.gwt.client.EntryPointHelper;
-import com.sap.sse.gwt.client.ErrorReporter;
+import com.sap.sse.security.ui.client.SecureClientFactoryImpl;
 
 /**
  * 
  * @author pgtaboada
  *
  */
-public class MobileApplicationClientFactory implements com.sap.sse.gwt.client.mvp.ClientFactory {
+public class MobileApplicationClientFactory extends SecureClientFactoryImpl {
     private final HomeServiceAsync homeService;
-    private final PlaceController placeController;
     private final MobilePlacesNavigator navigator;
-    private final SimpleEventBus eventBus;
     private final DispatchSystem dispatch = new DispatchSystemImpl();
-    private MainView mainView;
 
     public MobileApplicationClientFactory() {
         this(new SimpleEventBus());
     }
 
-    public MobileApplicationClientFactory(SimpleEventBus eventBus) {
+    private MobileApplicationClientFactory(SimpleEventBus eventBus) {
         this(eventBus, new PlaceController(eventBus));
     }
 
-    public MobileApplicationClientFactory(SimpleEventBus eventBus, PlaceController placeController) {
-        this.eventBus = eventBus;
-        this.placeController = placeController;
-        this.homeService = GWT.create(HomeService.class);
+    private MobileApplicationClientFactory(EventBus eventBus, PlaceController placeController) {
+        this(new MainView(new MobilePlacesNavigator(placeController), eventBus), eventBus, placeController);
+    }
+    
+    public MobileApplicationClientFactory(ApplicationTopLevelView root, EventBus eventBus,
+            PlaceController placeController) {
+        super(root, eventBus, placeController);
         this.navigator = new MobilePlacesNavigator(placeController);
-        mainView = new MainView(this);
+        this.homeService = GWT.create(HomeService.class);
         EntryPointHelper.registerASyncService((ServiceDefTarget) homeService,
                 RemoteServiceMappingConstants.homeServiceRemotePath);
     }
-
 
     public MobilePlacesNavigator getNavigator() {
         return navigator;
@@ -58,33 +56,8 @@ public class MobileApplicationClientFactory implements com.sap.sse.gwt.client.mv
         return homeService;
     }
 
-    public PlaceController getPlaceController() {
-        return placeController;
-    }
-
     public DispatchSystem getDispatch() {
         return dispatch;
-    }
-
-    @Override
-    public ErrorReporter getErrorReporter() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public EventBus getEventBus() {
-        return eventBus;
-    }
-
-    @Override
-    public Widget getRoot() {
-        return mainView;
-    }
-
-    @Override
-    public AcceptsOneWidget getContent() {
-        return mainView.getContent();
     }
 
     @Override
