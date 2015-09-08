2911bfd5c8a2ce4da4e830e51390659f59b3e1ad
Benjamin Barth <benjamin.barth@oio.de>
2015-09-08 11:52:24 +0200

    Refactoring - Moved shared Message partials to respective package

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/message/MessageResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/message/MessageResources.java
@@ -1,4 +1,4 @@
-package com.sap.sailing.gwt.home.client.place.event.partials.message;
+package com.sap.sailing.gwt.home.shared.partials.message;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
