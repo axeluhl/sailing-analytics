8a5877bf43085f84de5a724fcd1ad45961d290da
Benjamin Barth <benjamin.barth@oio.de>
2015-09-14 12:05:16 +0200

    Refactoring - Moved desktop SeriesHeader partials to respective package

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/seriesheader/SeriesHeader.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/seriesheader/SeriesHeader.java
@@ -1,4 +1,4 @@
-package com.sap.sailing.gwt.home.client.place.fakeseries.partials.header;
+package com.sap.sailing.gwt.home.desktop.partials.seriesheader;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
