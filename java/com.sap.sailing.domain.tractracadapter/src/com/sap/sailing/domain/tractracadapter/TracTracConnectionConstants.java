4b2bf746d8accedcf870c07b61400c86dc25917e
fmittag <fmittag@gmx.net>
2015-09-15 22:10:29 +0200

    reverted change of tractrac constants from event.tractrac.com back to germanmaster.traclive.dk actually germanmaster.traclive.dk should not be used anymore

--- java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/TracTracConnectionConstants.java
+++ java/com.sap.sailing.domain.tractracadapter/src/com/sap/sailing/domain/tractracadapter/TracTracConnectionConstants.java
@@ -2,7 +2,7 @@ package com.sap.sailing.domain.tractracadapter;
 
 public interface TracTracConnectionConstants {
     
-    final String HOST_NAME = "event.tractrac.com";
+    final String HOST_NAME = "germanmaster.traclive.dk";
     
     final int PORT_TUNNEL_LIVE = 1520;                          //For official usage use 4412;
     final int PORT_TUNNEL_STORED = PORT_TUNNEL_LIVE + 1;        //For official usage use 4413;
