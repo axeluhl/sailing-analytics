78f28a336b4bbd60bfc4f14408dc017d1b69eed6
Lennart Hensler <lennarthensler@aol.com>
2015-09-21 13:53:53 +0200

    The data mining settings are now fully functional

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/shared/components/CompositeValidator.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/shared/components/CompositeValidator.java
@@ -1,33 +1,22 @@
 package com.sap.sse.gwt.client.shared.components;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.sap.sse.common.Util;
 import com.sap.sse.common.settings.Settings;
 import com.sap.sse.gwt.client.dialog.DataEntryDialog.Validator;
 import com.sap.sse.gwt.client.shared.components.CompositeSettings.ComponentAndSettingsPair;
+import com.sap.sse.gwt.client.shared.components.CompositeTabbedSettingsDialogComponent.ComponentAndDialogComponent;
 
 public class CompositeValidator implements Validator<CompositeSettings> {
-    private static class ComponentAndValidator<SettingsType extends Settings> extends Util.Pair<SettingsDialogComponent<SettingsType>, Validator<SettingsType>> {
-        private static final long serialVersionUID = -4190322565836849861L;
-
-        public ComponentAndValidator(SettingsDialogComponent<SettingsType> a, Validator<SettingsType> b) {
-            super(a, b);
-        }
-    }
     
-    private final Iterable<ComponentAndValidator<?>> validators;
+    private final Map<Component<?>, Validator<?>> validatorsMappedByComponent;
 
-    public CompositeValidator(Iterable<SettingsDialogComponent<?>> dialogComponents) {
-        ArrayList<ComponentAndValidator<?>> v = new ArrayList<ComponentAndValidator<?>>();
-        for (SettingsDialogComponent<?> dialogComponent : dialogComponents) {
-            v.add(getComponentAndValidator(dialogComponent));
+    public CompositeValidator(Iterable<ComponentAndDialogComponent<?>> componentsAndDialogComponents) {
+        validatorsMappedByComponent = new HashMap<>();
+        for (ComponentAndDialogComponent<?> component : componentsAndDialogComponents) {
+            validatorsMappedByComponent.put(component.getA(), component.getB().getValidator());
         }
-        validators = v;
-    }
-
-    private <SettingsType extends Settings> ComponentAndValidator<SettingsType> getComponentAndValidator(SettingsDialogComponent<SettingsType> dialogComponents) {
-        return new ComponentAndValidator<SettingsType>(dialogComponents, dialogComponents.getValidator());
     }
 
     @Override
@@ -35,23 +24,24 @@ public class CompositeValidator implements Validator<CompositeSettings> {
         StringBuilder result = new StringBuilder();
         for (ComponentAndSettingsPair<?> componentAndSettings : valueToValidate.getSettingsPerComponent()) {
             final String errorMessage = getErrorMessage(componentAndSettings);
-            if (errorMessage != null) {
+            if (errorMessage != null && !errorMessage.isEmpty()) {
                 result.append(errorMessage);
-                result.append("; ");
             }
         }
         return result.toString();
     }
 
     private <SettingsType extends Settings> String getErrorMessage(ComponentAndSettingsPair<SettingsType> componentAndSettings) {
-        for (ComponentAndValidator<?> componentAndValidator : validators) {
-            if (componentAndValidator.getA() == componentAndSettings.getA()) {
-                @SuppressWarnings("unchecked")
-                final Validator<SettingsType> validator = (Validator<SettingsType>) componentAndValidator.getB();
-                return validator.getErrorMessage(componentAndSettings.getB());
+        String errorMessage = null;
+        @SuppressWarnings("unchecked")
+        Validator<SettingsType> validator = (Validator<SettingsType>) validatorsMappedByComponent.get(componentAndSettings.getA());
+        if (validator != null) {
+            errorMessage = validator.getErrorMessage(componentAndSettings.getB());
+            if (errorMessage != null && !errorMessage.isEmpty() && !getClass().equals(validator.getClass())) {
+                errorMessage += "; ";
             }
         }
-        return null;
+        return errorMessage;
     }
 
 }
