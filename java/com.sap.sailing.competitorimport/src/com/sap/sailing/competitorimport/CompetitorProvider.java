8865b031c1ecf39ab4b658c9e5b923ca2f355c04
Axel Uhl <axel.uhl@sap.com>
2015-09-23 17:01:14 +0200

    added bundle com.sap.sailing.competitorimport

---
+++ java/com.sap.sailing.competitorimport/src/com/sap/sailing/competitorimport/CompetitorProvider.java
@@ -0,0 +1,20 @@
+package com.sap.sailing.competitorimport;
+
+/**
+ * Competitor data may come from various sources, such as external regatta management systems (RMS),
+ * CSV files, Excel spreadsheets and the like. When creating a regatta that uses "RaceLog Tracking"
+ * (also known as Smartphone Tracking)---and be it only in order to manage results for the regatta
+ * with the race committee app---it is tedious having to enter all competitor data manually when
+ * that data is already available in electronic form.<p>
+ * 
+ * In its simplest form, a provider will just produce an otherwise unstructured list of competitor
+ * entries with no hint as to the event, regatta or race that the competitor pertains to. More
+ * sophisticated providers may structure competitor data into events, regattas or even individual
+ * races so that 
+ * 
+ * @author Axel Uhl (d043530)
+ *
+ */
+public class CompetitorProvider {
+
+}
