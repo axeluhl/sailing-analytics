23950d37890587ff2ef794b559f4bfc4f7be1cd1
Alexander Ries <alexander.ries@sap.com>
2015-09-24 11:35:34 +0200

    added logging for wind retrieving and displaying in dashdashboard

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/dataretriever/WindBotDataRetriever.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/dataretriever/WindBotDataRetriever.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.sap.sailing.domain.common.RegattaAndRaceIdentifier;
@@ -31,7 +33,9 @@ public class WindBotDataRetriever implements TimeListener, RaceSelectionChangeLi
     private final int ONE_HOUR_IN_MILLISECONDS = 1000*60*60;
     private final String LODA_WIND_CHART_DATA_CATEGORY = "loadWindChartData";
     private boolean didInitialLoading;
-
+    
+    private static final Logger logger = Logger.getLogger(WindBotDataRetriever.class.getName());
+    
     public WindBotDataRetriever(SailingServiceAsync sailingService) {
         this.sailingService = sailingService;
         currentLiveRace = null;
@@ -49,6 +53,7 @@ public class WindBotDataRetriever implements TimeListener, RaceSelectionChangeLi
             didInitialLoading = true;
             from = new Date(from.getTime()-ONE_HOUR_IN_MILLISECONDS);
         }
+        logger.log(Level.INFO, "Executing WindInfoAction with from "+from+" and to "+to);
         GetWindInfoAction getWindInfoAction = new GetWindInfoAction(sailingService, selectedRaceIdentifier, from, to,
                 WIND_CHART_RESOLUTION_IN_MILLISECONDS, windSourceTypeNames, /*
                                                               * onlyUpToNewestEvent==true because we don't want to
@@ -58,18 +63,22 @@ public class WindBotDataRetriever implements TimeListener, RaceSelectionChangeLi
                 new AsyncCallback<WindInfoForRaceDTO>() {
                     @Override
                     public void onSuccess(WindInfoForRaceDTO result) {
+                        logger.log(Level.INFO, "Received Response from WindInfoAction");
                         if (result != null) {
                             if(windBotIDsInLiveRace.size() != getWindBotIdsFrom(result).size()){
                                 windBotIDsInLiveRace = getWindBotIdsFrom(result);
                                 notifyListenersAboutNumberOfWindBotChange(windBotIDsInLiveRace);
+                                logger.log(Level.INFO, "Number of Windbots changed");
                             }
                             notifyWindBotDataRetrieverListeners(result);
+                        } else {
+                            logger.log(Level.INFO, "Response WindInfoForRaceDTO from WindInfoAction null");                            
                         }
                     }
 
                     @Override
                     public void onFailure(Throwable caught) {
-
+                        logger.log(Level.INFO, caught.getMessage().toString());
                     }
                 });
     }
