6121d2963840fb3b853b624e66cf020b81f9318b
Jan Broß <mail@jbross.de>
2015-09-29 10:33:41 +0200

    Remove AbstractOkCancelDialog

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/CreateDefaultRegattaLeaderboardDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/CreateDefaultRegattaLeaderboardDialog.java
@@ -1,6 +1,10 @@
 package com.sap.sailing.gwt.ui.adminconsole;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Panel;
 import com.sap.sailing.domain.common.RegattaIdentifier;
 import com.sap.sailing.domain.common.RegattaName;
 import com.sap.sailing.gwt.ui.client.SailingServiceAsync;
@@ -9,9 +13,10 @@ import com.sap.sailing.gwt.ui.shared.RegattaDTO;
 import com.sap.sailing.gwt.ui.shared.StrippedLeaderboardDTO;
 import com.sap.sse.gwt.client.ErrorReporter;
 
-public class CreateDefaultRegattaLeaderboardDialog extends AbstractOkCancelDialog{
+public class CreateDefaultRegattaLeaderboardDialog extends AbstractCancelableDialog{
 
     private RegattaDTO regatta;
+    private Button okButton;
     
     public CreateDefaultRegattaLeaderboardDialog(SailingServiceAsync sailingService, StringMessages stringMessages,
             ErrorReporter errorReporter, RegattaDTO regatta) {
@@ -19,8 +24,20 @@ public class CreateDefaultRegattaLeaderboardDialog extends AbstractOkCancelDialo
         this.regatta = regatta;
         setupUi();
     }   
-    
-    @Override
+
+    protected void addButtons(Panel buttonPanel) {
+        super.addButtons(buttonPanel);
+        okButton = new Button(stringMessages.ok());
+        okButton.setTitle(stringMessages.canOnlyBeEditedBeforeStartingTracking());
+        okButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                ok();
+            }
+        });
+        buttonPanel.add(okButton);
+    }
+
     protected void ok() {
         RegattaIdentifier regattaIdentifier = new RegattaName(regatta.getName());
         sailingService.createRegattaLeaderboard(regattaIdentifier, regatta.getName(), new int[]{},
