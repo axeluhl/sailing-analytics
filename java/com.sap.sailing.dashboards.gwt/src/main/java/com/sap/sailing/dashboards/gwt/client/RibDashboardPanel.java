6bc0b3df6841a791a4a7222b51a29499838d2b11
Alexander Ries <alexander.ries@sap.com>
2015-10-01 11:28:18 +0200

    removed unused logger

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/RibDashboardPanel.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/RibDashboardPanel.java
@@ -2,8 +2,6 @@ package com.sap.sailing.dashboards.gwt.client;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
@@ -45,7 +43,6 @@ public class RibDashboardPanel extends Composite implements RibDashboardDataRetr
     }
 
     private static RootUiBinder uiBinder = GWT.create(RootUiBinder.class);
-    private static final Logger logger = Logger.getLogger(RibDashboardPanel.class.getName());
 
     @UiField
     RibDashboardPanelStyle style;
