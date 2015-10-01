1ca0cc02534324178de40015f0a9002588283881
Axel Uhl <axel.uhl@sap.com>
2015-10-01 17:51:29 +0200

    fixing averse effects of ce7ac3c47dd47ad47e530bb4b9e9180a4a4f33f2 on serializability of PolarDataMiner

--- java/com.sap.sse.datamining/src/com/sap/sse/datamining/impl/data/AbstractClusterBoundary.java
+++ java/com.sap.sse.datamining/src/com/sap/sse/datamining/impl/data/AbstractClusterBoundary.java
@@ -1,9 +1,12 @@
 package com.sap.sse.datamining.impl.data;
 
+import java.io.Serializable;
+
 import com.sap.sse.datamining.data.ClusterBoundary;
 
-public abstract class AbstractClusterBoundary<ElementType> implements ClusterBoundary<ElementType> {
-
+public abstract class AbstractClusterBoundary<ElementType extends Serializable> implements ClusterBoundary<ElementType> {
+    private static final long serialVersionUID = 513488430319010656L;
+    
     private final ElementType boundaryValue;
     private final ComparisonStrategy strategy;
 
