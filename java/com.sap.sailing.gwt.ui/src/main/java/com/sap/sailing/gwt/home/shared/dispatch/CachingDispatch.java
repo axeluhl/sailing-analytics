a125116086da63c02d4187948687d16bd46d53ea
Papick Garcia Taboada <pgt@pgt.de>
2015-10-13 14:20:13 +0200

    Minor refactorings

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/dispatch/CachingDispatch.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/dispatch/CachingDispatch.java
@@ -11,18 +11,17 @@ import com.sap.sailing.gwt.ui.shared.dispatch.Result;
 import com.sap.sse.common.Duration;
 
 /**
+ * Delegating dispatch implementation that caches results if actions implement IsClientCache interface.
  * 
  * @author pgtaboada
- *
  */
 public final class CachingDispatch implements DispatchAsync {
 
     private static final Logger LOG = Logger.getLogger(CachingDispatch.class.getName());
-    private final DispatchAsync dispatch;
-    private final HashMap<String, ResultHolder> resultsCache = new HashMap<>();
     private final int DEFAULT_TIME_TO_LIVE_MILLIS = (int) Duration.ONE_SECOND.asMillis() * 30;
-
-    private final InvalidationTask invalidationTask = new InvalidationTask();
+    private final CacheCleanupTask invalidationTask = new CacheCleanupTask();
+    private final HashMap<String, ResultHolder> resultsCache = new HashMap<>();
+    private final DispatchAsync dispatch;
 
     public CachingDispatch(DispatchAsync service) {
         this.dispatch = service;
@@ -31,14 +30,13 @@ public final class CachingDispatch implements DispatchAsync {
     @SuppressWarnings("unchecked")
     @Override
     public <R extends Result, A extends Action<R>> void execute(A action, AsyncCallback<R> callback) {
-
         if (action instanceof IsClientCacheable) {
             final IsClientCacheable clientCacheableAction = (IsClientCacheable) action;
             final ResultHolder cachedResult = resultsCache.get(key(clientCacheableAction));
-            if (cachedResult != null ) {
+            if (cachedResult != null) {
                 if (cachedResult.isValid()) {
                     LOG.fine("Cache hit for " + action.getClass().getName());
-                    callback.onSuccess((R) cachedResult.payload);
+                    callback.onSuccess((R) cachedResult.getPayload());
                 } else {
                     executeAndCache(action, callback);
                 }
@@ -54,6 +52,7 @@ public final class CachingDispatch implements DispatchAsync {
 
     /**
      * Perform dispatch call with intercepting callback.
+     * 
      * @param action
      * @param callback
      */
@@ -81,20 +80,21 @@ public final class CachingDispatch implements DispatchAsync {
         @Override
         public void onSuccess(R result) {
             if (action instanceof IsClientCacheable) {
-                // we should cache the action
+                final IsClientCacheable clientCacheableAction = (IsClientCacheable) action;
+                final String instanceKey = key(clientCacheableAction);
                 int cacheTotalTimeToLiveMillis = DEFAULT_TIME_TO_LIVE_MILLIS;
                 if (result instanceof HasClientCacheTotalTimeToLive) {
                     cacheTotalTimeToLiveMillis = ((HasClientCacheTotalTimeToLive) result).cacheTotalTimeToLiveMillis();
                 } else if (action instanceof HasClientCacheTotalTimeToLive) {
                     cacheTotalTimeToLiveMillis = ((HasClientCacheTotalTimeToLive) action).cacheTotalTimeToLiveMillis();
                 }
-                IsClientCacheable clientCacheableAction = (IsClientCacheable) action;
-                LOG.finest("Added " + key(clientCacheableAction) + " to cache, ttl: "
-                        + cacheTotalTimeToLiveMillis + "ms");
                 resultsCache.put(key(clientCacheableAction), new ResultHolder(System.currentTimeMillis()
                         + cacheTotalTimeToLiveMillis, result));
-                // schedule cache cleanup
-                invalidationTask.schedule((int) (cacheTotalTimeToLiveMillis + Duration.ONE_SECOND.asMillis()));
+                LOG.finest("Added " + instanceKey + " to cache, ttl: " + cacheTotalTimeToLiveMillis + "ms");
+                if (!invalidationTask.isRunning()) {
+                    LOG.finest("Schedule cache cleanup");
+                    invalidationTask.schedule((int) Duration.ONE_MINUTE.asMillis());
+                }
             }
             callback.onSuccess(result);
         }
@@ -105,12 +105,24 @@ public final class CachingDispatch implements DispatchAsync {
         }
     }
 
+    /**
+     * Generate instance key for given action.
+     * 
+     * @param action
+     * @return
+     */
     private String key(IsClientCacheable action) {
         StringBuilder key = new StringBuilder(action.getClass().getName()).append("_");
         action.cacheInstanceKey(key);
         return key.toString();
     }
 
+    /**
+     * Holder class used to store results in cache.
+     * 
+     * @author pgtaboada
+     *
+     */
     private static class ResultHolder {
         final Result payload;
         final long invalidationTimestamp;
@@ -129,13 +141,26 @@ public final class CachingDispatch implements DispatchAsync {
         }
     }
 
-    private class InvalidationTask extends Timer {
+    /**
+     * Removes due elements from resultscache.
+     * 
+     * @author pgtaboada
+     *
+     */
+    private class CacheCleanupTask extends Timer {
         @Override
         public void run() {
-            ArrayList<String> keys = new ArrayList<String>(resultsCache.keySet());
-            for (String key : keys) {
-                if (!resultsCache.get(key).isValid()) {
-                    resultsCache.remove(key);
+            try {
+                LOG.finest("Start cache cleanup");
+                final ArrayList<String> keys = new ArrayList<String>(resultsCache.keySet());
+                for (String key : keys) {
+                    if (!resultsCache.get(key).isValid()) {
+                        resultsCache.remove(key);
+                    }
+                }
+            } finally {
+                if (!resultsCache.isEmpty()) {
+                    invalidationTask.schedule((int) Duration.ONE_MINUTE.asMillis());
                 }
             }
         }
