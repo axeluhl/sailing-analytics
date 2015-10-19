b75c4725d6538ef93c2d45f258e2329677bbc8ce
Christopher Ronnewinkel (D036654) <christopher.ronnewinkel@sap.com>
2015-10-19 19:53:24 +0200

    use java.util.Random for random number generation (used for wind pattern with gusts); remove library "ssj.jar";

---
+++ java/com.sap.sailing.simulator/src/com/sap/sailing/simulator/windfield/impl/BlastRandom.java
@@ -0,0 +1,29 @@
+package com.sap.sailing.simulator.windfield.impl;
+
+import java.util.Random;
+
+public class BlastRandom extends Random {
+
+    private static final long serialVersionUID = -6409398526247431352L;
+
+    // random number having uniform distribution
+    public double nextDouble(double min, double max) {
+        return (min + (max - min)*this.nextDouble());
+    }
+
+    // random number having Gaussian distribution
+    public double nextGaussian(double mean, double variance) {
+        return (mean + variance * this.nextGaussian());
+    }
+
+    // random number having exponential distribution
+    public double nextExponential(double lambda) {
+        return -Math.log(1.0 - this.nextDouble())/lambda;
+    }
+
+    // random number having geometric distribution
+    public int nextGeometric(double p) {
+        return (int)Math.floor(nextExponential(p));
+    }
+    
+}
