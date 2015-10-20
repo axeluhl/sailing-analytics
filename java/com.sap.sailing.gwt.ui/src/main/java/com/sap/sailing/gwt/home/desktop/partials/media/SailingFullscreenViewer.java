72e4865bd8b07ea6df98cb880c116a8ada56fc3e
Papick Garcia Taboada <pgt@pgt.de>
2015-10-20 15:11:10 +0200

    Enabled autoplay in fullscreen viewer

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/media/SailingFullscreenViewer.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/media/SailingFullscreenViewer.java
@@ -2,12 +2,36 @@ package com.sap.sailing.gwt.home.desktop.partials.media;
 
 import java.util.Collection;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Image;
 import com.sap.sailing.gwt.home.shared.partials.fullscreen.FullscreenContainer;
 import com.sap.sailing.gwt.ui.shared.media.SailingImageDTO;
 import com.sap.sse.gwt.client.controls.carousel.ImageCarousel.FullscreenViewer;
 
-public class SailingFullscreenViewer extends FullscreenContainer<SailingGalleryPlayer> implements FullscreenViewer<SailingImageDTO> {
+public class SailingFullscreenViewer extends FullscreenContainer<SailingGalleryPlayer> implements
+        FullscreenViewer<SailingImageDTO> {
+    private final Image autoRefreshControl = new Image("images/home/reload.svg");
+
+    public SailingFullscreenViewer() {
+        SailingFullscreenViewerResources.INSTANCE.css().ensureInjected();
+    }
+
     public void show(SailingImageDTO selected, Collection<SailingImageDTO> images) {
-        showContent(new SailingGalleryPlayer(selected, images));
+        final SailingGalleryPlayer player = new SailingGalleryPlayer(selected, images);
+        showContent(player);
+        autoRefreshControl.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                player.toggleAutoplay();
+                if (player.isAutoplaying()) {
+                    autoRefreshControl.addStyleName(SailingFullscreenViewerResources.INSTANCE.css().is_autoplaying());
+                } else {
+                    autoRefreshControl
+                            .removeStyleName(SailingFullscreenViewerResources.INSTANCE.css().is_autoplaying());
+                }
+            }
+        });
+        addToolbarAction(autoRefreshControl);
     }
 }
