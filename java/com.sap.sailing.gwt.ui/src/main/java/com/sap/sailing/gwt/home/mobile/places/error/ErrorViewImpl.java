c45297ac892220cdc4edd1cb8b818005380850d6
Benjamin Barth <benjamin.barth@oio.de>
2015-10-20 15:51:02 +0200

    Added a mobile ErrorView implementation

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/error/ErrorViewImpl.java
@@ -0,0 +1,38 @@
+package com.sap.sailing.gwt.home.mobile.places.error;
+
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.sap.sailing.gwt.home.mobile.resources.SharedResources;
+import com.sap.sailing.gwt.home.shared.partials.error.ErrorMessage;
+import com.sap.sse.gwt.client.mvp.ErrorView;
+
+public class ErrorViewImpl extends Composite implements ErrorView {
+
+    private static ErrorViewUiBinder uiBinder = GWT.create(ErrorViewUiBinder.class);
+    
+    interface ErrorViewUiBinder extends UiBinder<Widget, ErrorViewImpl> {
+    }
+    
+    @UiField(provided = true) ErrorMessage errorMessageUi;
+    
+    public ErrorViewImpl(String errorMessageDetail, Throwable errorReason, Command reloadCommand) {
+        this(new ErrorMessage(errorMessageDetail, errorReason, reloadCommand));
+    }
+    
+    public ErrorViewImpl(String errorMessage, String errorMessageDetail, Throwable errorReason, Command reloadCommand) {
+        this(new ErrorMessage(errorMessage, errorMessageDetail, errorReason, reloadCommand));
+    }
+    
+    private ErrorViewImpl(ErrorMessage errorMessage) {
+        errorMessageUi = errorMessage;
+        initWidget(uiBinder.createAndBindUi(this));
+        errorMessageUi.addReloadPageButtonStyleNames(SharedResources.INSTANCE.mainCss().button());
+        errorMessageUi.addReloadPageButtonStyleNames(SharedResources.INSTANCE.mediaCss().small12());
+    }
+
+}
+ 
