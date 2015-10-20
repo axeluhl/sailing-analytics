44900bc0cc4deee0f758bd2b2d70f82ec77101f1
Axel Uhl <axel.uhl@sap.com>
2015-10-20 10:25:24 +0200

    consolidated imports

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/RaceListFragment.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/RaceListFragment.java
@@ -10,7 +10,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.SortedSet;
 import java.util.TreeMap;
 
 import android.app.Activity;
