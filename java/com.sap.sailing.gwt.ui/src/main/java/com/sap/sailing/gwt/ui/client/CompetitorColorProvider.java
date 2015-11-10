969e0e3fb0c1e87c91bcb219c39565405f01cde7
Axel Uhl <axel.uhl@sap.com>
2015-11-10 16:21:06 +0100

    some reformatting of Javadoc

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/CompetitorColorProvider.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/CompetitorColorProvider.java
@@ -6,15 +6,17 @@ import com.sap.sse.common.Color;
 
 /**
  * 
- * @author Frank
- * A color provider for a competitor.
- * A color of a competitor can either be the same for all races of a regatta or it can be race specific.
- * Race specific can make sense e.g. when a competitor is changing it's boat during a regatta. 
+ * A color provider for a competitor. A color of a competitor can either be the same for all races of a regatta or it
+ * can be race specific. Race specific can make sense e.g. when a competitor is changing it's boat during a regatta.
+ * 
+ * @author Frank Mittag
  */
 public interface CompetitorColorProvider {
     Color getColor(CompetitorDTO competitor);
+
     Color getColor(CompetitorDTO competitor, RegattaAndRaceIdentifier raceIdentfier);
-    
+
     void addBlockedColor(Color color);
+
     void removeBlockedColor(Color color);
 }
