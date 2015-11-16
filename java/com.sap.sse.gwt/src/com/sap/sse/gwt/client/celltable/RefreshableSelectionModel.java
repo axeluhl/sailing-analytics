a4db261f5c6a36eb1f9a62964d5f37cd76f39cf9
Lukas Furmanek <lukas.furmanek@sap.com>
2015-11-16 15:59:08 +0100

    Added JavaDoc

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/celltable/RefreshableSelectionModel.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/celltable/RefreshableSelectionModel.java
@@ -3,10 +3,33 @@ package com.sap.sse.gwt.client.celltable;
 import java.util.Set;
 
 public interface RefreshableSelectionModel<T> {
+    /**
+     * @return Returns an instance of <code>HasEqualIdentity&ltT&gt</code>, to compare objects from the type
+     *         <code>&ltT&gt</code>.
+     */
     public HasEqualIdentity<T> getHasEqualIdentity();
+
+    /**
+     * Sets the <code>HasEqualIdentity&ltT&gt</code> field, to compare objects from the type <code>&ltT&gt</code>.
+     * 
+     * @param comp
+     *            When you set <code>null</code>, the <code>T.eqauls()</code> will be used.
+     */
     public void setHasEqualIdentity(HasEqualIdentity<T> comp);
+
     public Set<T> getSelectedSet();
+
     public void clear();
+
     public void setSelected(T item, boolean selected);
+
+    /**
+     * Refreshes the <code>RefreshableSelectionModel&ltT&gt</code> with the <code>newObjects</code>. If the selected
+     * objects are according to <code>HasEqualIdentity&ltT&gt.compare()</code> are the same, the selection will be
+     * reselected. If the object have no <code>HasEqualIdentity&ltT&gt</code>, this method will use the
+     * <code>&ltT&gt.eqauls()</code> method to compare.
+     * 
+     * @param newObjects
+     */
     public void refreshSelectionModel(Iterable<T> newObjects);
 }
