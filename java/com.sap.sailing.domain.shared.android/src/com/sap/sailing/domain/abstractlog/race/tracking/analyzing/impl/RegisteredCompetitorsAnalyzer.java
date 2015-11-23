34cb677b5e4812c599b09b346bce11fe54b4f40a
Jan Broß <mail@jbross.de>
2015-11-23 13:47:30 +0100

    Removes duplicate CompetitorInLogFinder COde

--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/race/tracking/analyzing/impl/RegisteredCompetitorsAnalyzer.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/race/tracking/analyzing/impl/RegisteredCompetitorsAnalyzer.java
@@ -1,15 +1,11 @@
 package com.sap.sailing.domain.abstractlog.race.tracking.analyzing.impl;
 
-import java.util.HashSet;
 import java.util.Set;
 
-import com.sap.sailing.domain.abstractlog.AbstractLog;
-import com.sap.sailing.domain.abstractlog.AbstractLogEvent;
-import com.sap.sailing.domain.abstractlog.BaseLogAnalyzer;
 import com.sap.sailing.domain.abstractlog.race.RaceLog;
 import com.sap.sailing.domain.abstractlog.race.analyzing.impl.RaceLogAnalyzer;
 import com.sap.sailing.domain.abstractlog.regatta.RegattaLog;
-import com.sap.sailing.domain.abstractlog.shared.events.RegisterCompetitorEvent;
+import com.sap.sailing.domain.abstractlog.shared.analyzing.CompetitorsInLogAnalyzer;
 import com.sap.sailing.domain.base.Competitor;
 
 /**
@@ -36,34 +32,6 @@ public class RegisteredCompetitorsAnalyzer extends RaceLogAnalyzer<Set<Competito
         } else {
             //get Events from RegattaLog
             return new CompetitorsInLogAnalyzer<>(regattaLog).analyze();
-        }
-    }
-    
-    
-    /**
-     * Uses the {@link RegisterCompetitorEvent}s in the provided {@link AbstractLog} to determine the competitors registered in the given log.
-     * 
-     * @author Fredrik Teschke
-     *
-     */
-    private class CompetitorsInLogAnalyzer<LogT extends AbstractLog<EventT, VisitorT>, EventT extends AbstractLogEvent<VisitorT>, VisitorT>
-            extends BaseLogAnalyzer<LogT, EventT, VisitorT, Set<Competitor>> {
-
-        public CompetitorsInLogAnalyzer(LogT log) {
-            super(log);
-        }
-
-        @Override
-        protected Set<Competitor> performAnalysis() {
-            Set<Competitor> result = new HashSet<Competitor>();
-
-            for (EventT event : getLog().getUnrevokedEvents()) {
-                if (event instanceof RegisterCompetitorEvent) {
-                    result.add(((RegisterCompetitorEvent<?>) event).getCompetitor());
-                }
-            }
-
-            return result;
         }
     }
 }
