9bceca739977c663142ed99237889d3f57e7bf66
Benjamin Barth <benjamin.barth@oio.de>
2015-12-02 10:11:04 +0100

    Moved default stage event teaser image to SharedHomeResources

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/resources/SharedHomeResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/resources/SharedHomeResources.java
@@ -1,9 +1,15 @@
 package com.sap.sailing.gwt.home.shared.resources;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 
 public interface SharedHomeResources extends ClientBundle {
+    
+    public static final SharedHomeResources INSTANCE = GWT.create(SharedHomeResources.class);
+    
+    @Source("default_stage_event_teaser.jpg")
+    ImageResource defaultStageEventTeaserImage();
     
     @Source("default_video_preview.jpg")
     ImageResource defaultVideoPreviewImage();
