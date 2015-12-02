e9293e0a73858e2c313d141f1d86139077120448
Benjamin Barth <benjamin.barth@oio.de>
2015-12-02 09:45:54 +0100

    Moved default video preview image to SharedHomeResources

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/resources/SharedHomeResources.java
@@ -0,0 +1,11 @@
+package com.sap.sailing.gwt.home.shared.resources;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface SharedHomeResources extends ClientBundle {
+    
+    @Source("default_video_preview.jpg")
+    ImageResource defaultVideoPreviewImage();
+    
+}
