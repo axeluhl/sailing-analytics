52272e29190226f098280bcc438e1bb97e0633e0
Bennet Brunsen <brunsen@ubilabs.net>
2015-12-07 14:32:46 +0100

    Show license dialog in tracking app

--- mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/ui/fragments/AboutFragment.java
+++ mobile/com.sap.sailing.android.tracking.app/src/com/sap/sailing/android/tracking/app/ui/fragments/AboutFragment.java
@@ -7,7 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.sap.sailing.android.shared.util.EulaHelper;
+import com.sap.sailing.android.shared.util.LicenseHelper;
 import com.sap.sailing.android.tracking.app.R;
+
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.model.Notices;
 
 public class AboutFragment extends com.sap.sailing.android.ui.fragments.BaseFragment {
 
@@ -25,7 +29,7 @@ public class AboutFragment extends com.sap.sailing.android.ui.fragments.BaseFrag
         view.findViewById(R.id.licence_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showLicenceDialog();
             }
         });
         view.findViewById(R.id.eula_button).setOnClickListener(new View.OnClickListener() {
@@ -35,5 +39,18 @@ public class AboutFragment extends com.sap.sailing.android.ui.fragments.BaseFrag
             }
         });
         return view;
+    }
+
+    private void showLicenceDialog() {
+        Notices notices = new Notices();
+        LicenseHelper licenseHelper = new LicenseHelper();
+        notices.addNotice(licenseHelper.getAndroidSupportNotice());
+        notices.addNotice(licenseHelper.getOpenSansNotice());
+        notices.addNotice(licenseHelper.getJsonSimpleNotice());
+        notices.addNotice(licenseHelper.getDialogNotice());
+        LicensesDialog.Builder builder = new LicensesDialog.Builder(getActivity());
+        builder.setTitle(getString(R.string.licence_information));
+        builder.setNotices(notices);
+        builder.build().show();
     }
 }
