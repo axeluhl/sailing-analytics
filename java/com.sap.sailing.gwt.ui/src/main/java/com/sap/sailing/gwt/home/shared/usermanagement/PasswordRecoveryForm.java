76d3a9ba5ea3f55f8db68a0b559e28c63307edcc
Benjamin Barth <benjamin.barth@sap.com>
2015-12-08 15:35:39 +0100

    Added base implementation for desktop password recovery form

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/PasswordRecoveryForm.java
@@ -0,0 +1,39 @@
+package com.sap.sailing.gwt.home.shared.usermanagement;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class PasswordRecoveryForm extends Composite {
+    
+    interface PasswordRecoveryFormUiBinder extends UiBinder<Widget, PasswordRecoveryForm> {
+    }
+    
+    private static PasswordRecoveryFormUiBinder uiBinder = GWT.create(PasswordRecoveryFormUiBinder.class);
+    
+    @UiField TextBox loginNameUi;
+    
+    @UiField DivElement formErrorUi;
+    
+    public PasswordRecoveryForm() {
+        initWidget(uiBinder.createAndBindUi(this));
+        setPlaceholder(loginNameUi, "TODO Username or Email");
+    }
+    
+    @UiHandler("resetPasswordUi")
+    void onResetPasswordUiControlClicked(ClickEvent event) {
+        // String login = loginNameUi.getValue();
+        // TODO
+    }
+    
+    private void setPlaceholder(Widget widget, String placeholderText) {
+        widget.getElement().setAttribute("placeholder", placeholderText);
+    }
+    
+}
