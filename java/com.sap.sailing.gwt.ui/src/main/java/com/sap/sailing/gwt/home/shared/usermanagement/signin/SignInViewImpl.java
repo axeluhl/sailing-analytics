1235a1bbf48e6417a078a85870a6020a71582185
Benjamin Barth <benjamin.barth@sap.com>
2015-12-09 18:08:42 +0100

    Adjusted default selection and key handling in sign in view

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/signin/SignInViewImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/signin/SignInViewImpl.java
@@ -3,6 +3,8 @@ package com.sap.sailing.gwt.home.shared.usermanagement.signin;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -50,6 +52,12 @@ public class SignInViewImpl extends Composite implements SignInView {
     @Override
     public void setErrorMessage(String errorMessage) {
         formErrorUi.setInnerText(errorMessage);
+        selectAll(passwordUi.getValue().isEmpty() ? loginNameUi : passwordUi);
+    }
+    
+    @Override
+    protected void onLoad() {
+        selectAll(loginNameUi);
     }
     
     @UiHandler("forgotPasswordUi")
@@ -64,7 +72,14 @@ public class SignInViewImpl extends Composite implements SignInView {
     
     @UiHandler("signInUi")
     void onSignInControlUiClicked(ClickEvent event) {
-         presenter.login(loginNameUi.getValue(), passwordUi.getValue());
+        triggerLogin();
+    }
+    
+    @UiHandler({"loginNameUi","passwordUi"}) 
+    void onSignInKeyPressed(KeyUpEvent event) {
+        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+            triggerLogin();
+        }
     }
     
     @UiHandler("loginFacebookUi")
@@ -77,8 +92,17 @@ public class SignInViewImpl extends Composite implements SignInView {
         presenter.loginWithGoogle(); 
     }
     
+    private void triggerLogin() {
+        presenter.login(loginNameUi.getValue(), passwordUi.getValue());
+    }
+    
     private void setPlaceholder(Widget widget, String placeholderText) {
         widget.getElement().setAttribute("placeholder", placeholderText);
+    }
+    
+    private void selectAll(TextBox textBox) {
+        textBox.setFocus(true);
+        textBox.selectAll();
     }
     
 }
