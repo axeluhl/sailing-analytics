77c035ec5f68b179917dac7a8f98f57a1a028571
Lukas Furmanek <lukas.furmanek@sap.com>
2015-12-09 10:43:22 +0100

    newSelectionModel: added Javadoc

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/controls/FlushableCellTable.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/controls/FlushableCellTable.java
@@ -4,6 +4,14 @@ import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ProvidesKey;
 
+/**
+ * This {@link FlushableCellTable} provides the {@link FlushableCellTable#flush()}-method for the
+ * {@link SelectionCheckboxColumn}. So the {@link SelectionCheckboxColumn} can ensure that the selection state is
+ * displayed correct.
+ * 
+ * @author D064976
+ * @param <T>
+ */
 public class FlushableCellTable<T> extends CellTable<T> implements Flushable {
     public FlushableCellTable() {
         super();
