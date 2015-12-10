41179f5b914cb9d40b5f8e833acad61d5224a334
Benjamin Barth <benjamin.barth@sap.com>
2015-12-10 16:23:24 +0100

    Implemented password recovery logic

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/recovery/PasswordRecoveryActivity.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/recovery/PasswordRecoveryActivity.java
@@ -1,32 +1,62 @@
 package com.sap.sailing.gwt.home.shared.usermanagement.recovery;
 
+import static com.sap.sse.security.shared.UserManagementException.CANNOT_RESET_PASSWORD_WITHOUT_VALIDATED_EMAIL;
+
+import java.util.HashMap;
+
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
-import com.sap.sailing.gwt.home.shared.app.ClientFactoryWithUserManagementService;
+import com.sap.sailing.gwt.home.desktop.app.TabletAndDesktopApplicationClientFactory;
+import com.sap.sailing.gwt.home.shared.usermanagement.UserManagementRequestEvent;
+import com.sap.sailing.gwt.home.shared.usermanagement.signin.SigInPlace;
+import com.sap.sse.security.shared.UserManagementException;
+import com.sap.sse.security.ui.client.EntryPointLinkFactory;
+import com.sap.sse.security.ui.client.i18n.StringMessages;
 
 public class PasswordRecoveryActivity extends AbstractActivity implements PasswordRecoveryView.Presenter {
 
-    private final ClientFactoryWithUserManagementService clientFactory;
+    private final TabletAndDesktopApplicationClientFactory clientFactory;
     private final PlaceController placeController;
+    private final PasswordRecoveryView view = new PasswordRecoveryViewImpl();
 
-    public PasswordRecoveryActivity(ClientFactoryWithUserManagementService clientFactory, PlaceController placeController) {
+    public PasswordRecoveryActivity(TabletAndDesktopApplicationClientFactory clientFactory, PlaceController placeController) {
         this.clientFactory = clientFactory;
         this.placeController = placeController;
     }
     
     @Override
     public void start(AcceptsOneWidget panel, EventBus eventBus) {
-        PasswordRecoveryView view = new PasswordRecoveryViewImpl();
         view.setPresenter(this);
         panel.setWidget(view);
     }
 
     @Override
-    public void resetPassword(String email, String username) {
-        // TODO Auto-generated method stub
+    public void resetPassword(String email, final String username) {
+        clientFactory.getUserManagement().resetPassword(username, email,
+                EntryPointLinkFactory.createPasswordResetLink(new HashMap<String, String>()), 
+                new AsyncCallback<Void>() {
+                    @Override
+                    public void onSuccess(Void result) {
+                        clientFactory.getEventBus().fireEvent(new UserManagementRequestEvent());
+                        placeController.goTo(new SigInPlace());
+                    }
+                    
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        StringMessages i18n = StringMessages.INSTANCE;
+                        if (caught instanceof UserManagementException) {
+                            if (CANNOT_RESET_PASSWORD_WITHOUT_VALIDATED_EMAIL.equals(caught.getMessage())) {
+                                view.setErrorMessage(i18n.cannotResetPasswordWithoutValidatedEmail(username));
+                            } else {
+                                view.setErrorMessage(i18n.errorResettingPassword(username, caught.getMessage()));
+                            }
+                        } else {
+                            view.setErrorMessage(i18n.errorDuringPasswordReset(caught.getMessage()));
+                        }
+                    }
+                });
     }
-    
-
 }
