0918815753e4ce45453c21302e69c0b2f94ea491
Benjamin Barth <benjamin.barth@sap.com>
2015-12-10 16:20:30 +0100

    Used the interface for views showing an error message again

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/recovery/PasswordRecoveryView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/recovery/PasswordRecoveryView.java
@@ -1,8 +1,9 @@
 package com.sap.sailing.gwt.home.shared.usermanagement.recovery;
 
 import com.google.gwt.user.client.ui.IsWidget;
+import com.sap.sailing.gwt.home.shared.usermanagement.ErrorMessageView;
 
-public interface PasswordRecoveryView extends IsWidget {
+public interface PasswordRecoveryView extends ErrorMessageView, IsWidget {
 
     void setPresenter(Presenter presenter);
     
