9666c38c8d9588caaf0862d2fcb2c0afc7c59e4c
Benjamin Barth <benjamin.barth@sap.com>
2015-12-15 18:05:24 +0100

    Marked user management places as mobile compatible

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/create/CreateAccountPlace.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/create/CreateAccountPlace.java
@@ -1,7 +1,8 @@
 package com.sap.sailing.gwt.home.shared.usermanagement.create;
 
 import com.google.gwt.place.shared.Place;
+import com.sap.sailing.gwt.home.shared.app.HasMobileVersion;
 
-public class CreateAccountPlace extends Place {
+public class CreateAccountPlace extends Place implements HasMobileVersion {
 
 }
---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/signin/SignInPlace.java
@@ -0,0 +1,8 @@
+package com.sap.sailing.gwt.home.shared.usermanagement.signin;
+
+import com.google.gwt.place.shared.Place;
+import com.sap.sailing.gwt.home.shared.app.HasMobileVersion;
+
+public class SignInPlace extends Place implements HasMobileVersion {
+
+}
