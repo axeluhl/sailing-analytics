42bc3497513c662c375ec6bb2b348e42ae46b7b6
Benjamin Barth <benjamin.barth@sap.com>
2015-12-17 11:11:53 +0100

    Refactoring of places and views for better reusability

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/AbstractUserManagementPlace.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/usermanagement/AbstractUserManagementPlace.java
@@ -1,9 +1,10 @@
 package com.sap.sailing.gwt.home.shared.usermanagement;
 
 import com.google.gwt.place.shared.Place;
+import com.sap.sailing.gwt.home.shared.app.HasLocationTitle;
 import com.sap.sailing.gwt.home.shared.places.start.StartPlace;
 
-public abstract class AbstractUserManagementPlace extends Place {
+public abstract class AbstractUserManagementPlace extends Place implements HasLocationTitle {
     
     private final Place nextTarget;
     
