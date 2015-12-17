84e46a348cedf04841d70149f90aedc1bf4e4659
Benjamin Barth <benjamin.barth@sap.com>
2015-12-17 13:38:49 +0100

    Added header text to mobile authentication workflow

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/user/authentication/AuthenticationPlace.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/user/authentication/AuthenticationPlace.java
@@ -3,10 +3,17 @@ package com.sap.sailing.gwt.home.mobile.places.user.authentication;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceTokenizer;
 import com.google.gwt.place.shared.Prefix;
+import com.sap.sailing.gwt.home.shared.app.HasLocationTitle;
 import com.sap.sailing.gwt.home.shared.app.HasMobileVersion;
 import com.sap.sailing.gwt.home.shared.places.PlaceTokenPrefixes;
+import com.sap.sse.security.ui.client.i18n.StringMessages;
 
-public class AuthenticationPlace extends Place implements HasMobileVersion {
+public class AuthenticationPlace extends Place implements HasMobileVersion, HasLocationTitle {
+    
+    @Override
+    public String getLocationTitle() {
+        return StringMessages.INSTANCE.user();
+    }
 
     @Prefix(PlaceTokenPrefixes.UserAuthentication)
     public static class Tokenizer implements PlaceTokenizer<AuthenticationPlace> {
