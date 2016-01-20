ddca44907fd50b73faa1a0958d0ece616abef33d
Steffen Schaefer <steffen.schaefer@sap.com>
2016-01-20 13:24:34 +0100

    Introduces SharedResources interface in sse.security.ui to make it possible to refactor parts of user management UI of Home to SSE

---
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/client/usermanagement/UserManagementSharedResources.java
@@ -0,0 +1,16 @@
+package com.sap.sse.security.ui.client.usermanagement;
+
+
+public interface UserManagementSharedResources {
+    
+    UserManagementMainCss mainCss();
+
+    UserManagementMediaCss mediaCss();
+    
+    public interface UserManagementMainCss {
+    }
+
+    public interface UserManagementMediaCss {
+    }
+
+}
