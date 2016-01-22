403c6b2e650f5090082ac26593bc0a40687a7049
Raimund Wege <raimundwege@gmail.com>
2016-01-22 14:58:49 +0100

    Changed about view controller from web view to a view with licenses and EULA button and added license view controller

---
+++ ios/SAPTracker/src/xcode/SAPTracker/LicenseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LicenseViewController.swift
+//  SAPTracker
+//
+//  Created by Raimund Wege on 22.01.16.
+//  Copyright © 2016 com.sap.sailing. All rights reserved.
+//
+
+import UIKit
+
+class LicenseViewController: UIViewController {
+
+}
