6c907c1ad1e1c46ccf3eabd79129fd9f0e154c57
Benjamin Barth <benjamin.barth@sap.com>
2016-01-28 14:16:24 +0100

    Adjusted validation logic of password reset form

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/user/passwordreset/PasswordResetView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/user/passwordreset/PasswordResetView.java
@@ -1,13 +1,22 @@
 package com.sap.sailing.gwt.home.shared.places.user.passwordreset;
 
+import com.google.gwt.user.client.ui.HasEnabled;
 import com.google.gwt.user.client.ui.IsWidget;
+import com.sap.sse.security.ui.authentication.ErrorMessageView;
 
-public interface PasswordResetView extends IsWidget {
+public interface PasswordResetView extends ErrorMessageView, IsWidget {
 
     void setPresenter(Presenter currentPresenter);
+    
+    HasEnabled getChangePasswordControl();
 
     public interface Presenter {
-        void resetPassword(String newPassword, String newPasswordConfirmation);
+        
+        void onChangePassword(String newValue);
+        
+        void onChangePasswordConfirmation(String newValue);
+        
+        void resetPassword();
     }
 
 }
