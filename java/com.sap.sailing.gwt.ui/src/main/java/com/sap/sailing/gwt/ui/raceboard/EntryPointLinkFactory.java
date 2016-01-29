6427cfeebf7217c3a35ff78b0df7bf014b40a5b1
Benjamin Barth <benjamin.barth@sap.com>
2016-01-29 15:58:39 +0100

    Adjusted link references for email validation and password reset

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/EntryPointLinkFactory.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/EntryPointLinkFactory.java
@@ -2,6 +2,8 @@ package com.sap.sailing.gwt.ui.raceboard;
 
 import java.util.HashMap;
 
+import com.google.gwt.http.client.UrlBuilder;
+import com.google.gwt.user.client.Window;
 import com.sap.sse.gwt.client.AbstractEntryPointLinkFactory;
 
 public class EntryPointLinkFactory extends AbstractEntryPointLinkFactory {
@@ -11,11 +13,20 @@ public class EntryPointLinkFactory extends AbstractEntryPointLinkFactory {
     }
     
     public static final String createEmailValidationLink() {
-        return createEntryPointLink("/gwt/Home.html#/user/confirmation/:MAIL_VERIFIED", new HashMap<String, String>());
+        return createFullQualifiedLink("/gwt/Home.html", "/user/confirmation/:MAIL_VERIFIED");
     }
     
     public static final String createPasswordResetLink() {
-        return createEntryPointLink("/gwt/Home.html#/user/passwordreset/:", new HashMap<String, String>());
+        return createFullQualifiedLink("/gwt/Home.html", "/user/passwordreset/:");
+    }
+    
+    private static String createFullQualifiedLink(String baseUrl, String hash) {
+        String path = createEntryPointLink(baseUrl, new HashMap<String, String>());
+        UrlBuilder urlBuilder = Window.Location.createUrlBuilder();
+        for (String parameter : Window.Location.getParameterMap().keySet()) {
+            urlBuilder.removeParameter(parameter);
+        }
+        return urlBuilder.setPath(path).setHash(hash).buildString();
     }
 
 }
