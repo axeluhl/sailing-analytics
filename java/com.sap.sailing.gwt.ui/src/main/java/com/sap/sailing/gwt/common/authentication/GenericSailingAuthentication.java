f9c00a04bb6bf2e6fba0af53c1986cb89c02c9ae
Steffen Schäfer <steffen.schaefer@oio.de>
2016-02-03 17:08:30 +0100

    Implemented a generic version of authentication for sailing modules

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/common/authentication/GenericSailingAuthentication.java
@@ -0,0 +1,36 @@
+package com.sap.sailing.gwt.common.authentication;
+
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
+import com.sap.sse.gwt.common.CommonSharedResources;
+import com.sap.sse.security.ui.authentication.AuthenticationCallback;
+import com.sap.sse.security.ui.authentication.AuthenticationClientFactory;
+import com.sap.sse.security.ui.authentication.AuthenticationClientFactoryImpl;
+import com.sap.sse.security.ui.authentication.AuthenticationManager;
+import com.sap.sse.security.ui.authentication.AuthenticationManagerImpl;
+import com.sap.sse.security.ui.authentication.AuthenticationPlaceManagementController;
+import com.sap.sse.security.ui.authentication.WrappedPlaceManagementController;
+import com.sap.sse.security.ui.authentication.view.AuthenticationMenuView;
+import com.sap.sse.security.ui.authentication.view.FlyoutAuthenticationPresenter;
+import com.sap.sse.security.ui.authentication.view.FlyoutAuthenticationView;
+import com.sap.sse.security.ui.client.UserService;
+
+public class GenericSailingAuthentication {
+    private static final CommonSharedResources res = CommonSharedResources.INSTANCE;
+    
+    public GenericSailingAuthentication(UserService userService, AuthenticationMenuView userManagementMenuView) {
+        res.mainCss().ensureInjected();
+        
+        EventBus eventBus = new SimpleEventBus();
+        FlyoutAuthenticationView display = new GenericSailingAuthenticationView(res);
+        AuthenticationManager manager = new AuthenticationManagerImpl(userService, eventBus,
+                EntryPointLinkFactory.createEmailValidationLink(), 
+                EntryPointLinkFactory.createPasswordResetLink());
+        AuthenticationClientFactory clientFactory = new AuthenticationClientFactoryImpl(manager, res);
+        WrappedPlaceManagementController userManagementController = null;
+        AuthenticationCallback callback = new AuthenticationCallbackImpl(userManagementController);
+        userManagementController = new AuthenticationPlaceManagementController(clientFactory, callback, display, eventBus);
+        new FlyoutAuthenticationPresenter(display, userManagementMenuView, userManagementController, eventBus, manager.getAuthenticationContext());
+    }
+
+}
---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/common/authentication/GenericSailingAuthenticationView.java
@@ -0,0 +1,20 @@
+package com.sap.sailing.gwt.common.authentication;
+
+import com.sap.sse.gwt.common.CommonSharedResources;
+import com.sap.sse.security.ui.authentication.view.AbstractFlyoutAuthenticationView;
+
+public class GenericSailingAuthenticationView extends AbstractFlyoutAuthenticationView {
+    private static final SAPHeaderWithAuthenticationResources res = SAPHeaderWithAuthenticationResources.INSTANCE;
+
+    public GenericSailingAuthenticationView(CommonSharedResources resources) {
+        super(resources);
+        res.css().ensureInjected();
+        
+        popupPanel.setStyleName(res.css().usermanagement_view());
+    }
+    
+    public void show() {
+        popupPanel.show();
+        getPresenter().onVisibilityChanged(true);
+    }
+}
