9cb2d3fbe259e9714f515ba2ca5346259a2af271
Alexander Ries <alexander.ries@sap.com>
2016-02-03 16:38:17 +0100

    changed dashboard_size_medium to 35px

--- java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/theme/Sizes.java
+++ java/com.sap.sailing.dashboards.gwt/src/main/java/com/sap/sailing/dashboards/gwt/client/theme/Sizes.java
@@ -8,12 +8,12 @@ public class Sizes {
     public static final String DASHBOARD_FONT_SIZE_VERY_SMALL = "10px";
     public static final String DASHBOARD_FONT_SIZE_SMALL = "13px";
     public static final String DASHBOARD_FONT_SIZE_MEDIUM = "14px";
-    public static final String DASHBOARD_FONT_SIZE_LARGE = "18px";
+    public static final String DASHBOARD_FONT_SIZE_LARGE = "16px";
     public static final String DASHBOARD_FONT_SIZE_VERY_LARGE = "33px";
     
     public static final String DASHBOARD_SPACE_VERY_SMALL = "10px";
     public static final String DASHBOARD_SPACE_SMALL = "30px";
-    public static final String DASHBOARD_SPACE_MEDIUM = "40px";
+    public static final String DASHBOARD_SPACE_MEDIUM = "35px";
     public static final String DASHBOARD_SPACE_LARGE = "50px";
     public static final String DASHBOARD_SPACE_VERY_LARGE = "70px";
     
