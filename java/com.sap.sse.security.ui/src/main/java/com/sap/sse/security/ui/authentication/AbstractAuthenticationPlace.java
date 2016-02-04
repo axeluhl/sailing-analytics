25f5602b6b4f74b3f563d8727c21f56bbb840192
Benjamin Barth <benjamin.barth@sap.com>
2016-02-04 09:25:01 +0100

    Added JavaDoc to AbstractAuthenticationPlace and AuthenticationContextEvent classes

--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/AbstractAuthenticationPlace.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/AbstractAuthenticationPlace.java
@@ -2,8 +2,16 @@ package com.sap.sse.security.ui.authentication;
 
 import com.google.gwt.place.shared.Place;
 
+/**
+ * Super class for {@link Place}s within the authentication management, defining a header text for each {@link Place}.
+ */
 public abstract class AbstractAuthenticationPlace extends Place {
     
+    /**
+     * Defines a header text for the {@link Place}
+     * 
+     * @return the header test
+     */
     public abstract String getHeaderText();
     
 }
