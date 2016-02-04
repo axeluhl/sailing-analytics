f76bdde3c775cb629ca2fd1f573e58e554992384
Benjamin Barth <benjamin.barth@sap.com>
2016-02-04 10:11:58 +0100

    Added JavaDoc to AbstractAuthenticationPlace and AuthenticationView classes

--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/AbstractAuthenticationPlace.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/AbstractAuthenticationPlace.java
@@ -1,9 +1,12 @@
 package com.sap.sse.security.ui.authentication;
 
 import com.google.gwt.place.shared.Place;
+import com.sap.sse.security.ui.authentication.view.AuthenticationView;
 
 /**
  * Super class for {@link Place}s within the authentication management, defining a header text for each {@link Place}.
+ * 
+ * @see AuthenticationView#setHeading(String)
  */
 public abstract class AbstractAuthenticationPlace extends Place {
     
