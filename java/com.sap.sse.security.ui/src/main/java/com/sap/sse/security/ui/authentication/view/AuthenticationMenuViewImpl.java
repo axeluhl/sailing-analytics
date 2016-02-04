02f0b244aa288536a821053f41f079b9ea15c247
Benjamin Barth <benjamin.barth@sap.com>
2016-02-04 10:59:07 +0100

    Added JavaDoc to AuthenticationMenuView interface and AuthenticationMenuViewImpl class

--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/view/AuthenticationMenuViewImpl.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/view/AuthenticationMenuViewImpl.java
@@ -5,6 +5,9 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Widget;
 
+/**
+ * Default implementation of {@link AuthenticationMenuView} based on an {@link Anchor} widget.
+ */
 public class AuthenticationMenuViewImpl implements AuthenticationMenuView {
     
     private Presenter presenter;
@@ -12,6 +15,16 @@ public class AuthenticationMenuViewImpl implements AuthenticationMenuView {
     private final String loggedInStyle;
     private final String openStyle;
 
+    /**
+     * Create a new {@link AuthenticationMenuViewImpl} instance with the given parameters.
+     * 
+     * @param anchor
+     *            the {@link Anchor} widget representing the menu item
+     * @param loggedInStyle
+     *            the style name to add to the widget, if there is an authenticated user
+     * @param openStyle
+     *            the style name to add to the widget, if the {@link FlyoutAuthenticationView} is open
+     */
     public AuthenticationMenuViewImpl(Anchor anchor, String loggedInStyle, String openStyle) {
         this.anchor = anchor;
         this.loggedInStyle = loggedInStyle;
