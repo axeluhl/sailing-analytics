6211f97c54909d486790cf2cd8dedf691edd0781
Steffen Schäfer <steffen.schaefer@oio.de>
2016-02-16 09:49:30 +0100

    Added more authentication framework related JavaDocs

--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/AuthenticationRequestEvent.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/AuthenticationRequestEvent.java
@@ -3,6 +3,9 @@ package com.sap.sse.security.ui.authentication;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
 
+/**
+ * Event to be fired if the user needs to see the authentication UI.
+ */
 public class AuthenticationRequestEvent extends GwtEvent<AuthenticationRequestEvent.Handler> {
     public static final Type<Handler> TYPE = new Type<AuthenticationRequestEvent.Handler>();
 
--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/AuthenticationSignOutRequestEvent.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/AuthenticationSignOutRequestEvent.java
@@ -3,6 +3,10 @@ package com.sap.sse.security.ui.authentication;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
 
+/**
+ * Event to be fired to trigger logout. This makes it possible for UI elements to request a logout without further
+ * integration with the authentication framework.
+ */
 public class AuthenticationSignOutRequestEvent extends GwtEvent<AuthenticationSignOutRequestEvent.Handler> {
     public static final Type<Handler> TYPE = new Type<AuthenticationSignOutRequestEvent.Handler>();
 
