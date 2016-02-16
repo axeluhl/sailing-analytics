87705abfd424fcf9c3f4bffd63e936ee593898c1
Steffen Schäfer <steffen.schaefer@oio.de>
2016-02-16 10:34:29 +0100

    Added more authentication framework related JavaDocs

--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/app/NeedsAuthenticationContext.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/app/NeedsAuthenticationContext.java
@@ -1,6 +1,14 @@
 package com.sap.sse.security.ui.authentication.app;
 
+/**
+ * Common interface for all classes that need to be informed whenever the authentication changes.
+ */
 public interface NeedsAuthenticationContext {
 
+    /**
+     * To be called when the authentication changes.
+     * 
+     * @param authenticationContext the new {@link AuthenticationContext}.
+     */
     void setAuthenticationContext(AuthenticationContext authenticationContext);
 }
--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/generic/GenericAuthentication.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/generic/GenericAuthentication.java
@@ -2,6 +2,7 @@ package com.sap.sse.security.ui.authentication.generic;
 
 import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.event.shared.SimpleEventBus;
+import com.sap.sse.gwt.client.mvp.ClientFactory;
 import com.sap.sse.gwt.common.CommonSharedResources;
 import com.sap.sse.security.ui.authentication.AuthenticationClientFactory;
 import com.sap.sse.security.ui.authentication.AuthenticationClientFactoryImpl;
@@ -13,6 +14,13 @@ import com.sap.sse.security.ui.authentication.view.FlyoutAuthenticationPresenter
 import com.sap.sse.security.ui.authentication.view.FlyoutAuthenticationView;
 import com.sap.sse.security.ui.client.UserService;
 
+/**
+ * Configures the authentication framework in a common way that is suitable for generic flyout based authentication.
+ * This can be used for cases when no specific handling of the authentication or deeper integration is necessary.
+ * 
+ * Typically you would wire the whole authentication mechanism in the {@link ClientFactory}. For simple modules there is
+ * potentially no {@link ClientFactory}, so using this is suitable.
+ */
 public class GenericAuthentication {
     private final EventBus eventBus = new SimpleEventBus();
     private final AuthenticationManager manager;
