555c33bbbd96d9750262d2e2696810ea29e2b16a
Steffen Schäfer <steffen.schaefer@oio.de>
2016-02-16 08:29:24 +0100

    Added NeedsAuthenticationContext interface to reduce duplication of setAuthenticationContext methods on several places

---
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/app/NeedsAuthenticationContext.java
@@ -0,0 +1,6 @@
+package com.sap.sse.security.ui.authentication.app;
+
+public interface NeedsAuthenticationContext {
+
+    void setAuthenticationContext(AuthenticationContext authenticationContext);
+}
