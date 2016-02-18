9dab47a64fe2d8048be56e22f1b1690b3101904c
Steffen Schäfer <steffen.schaefer@oio.de>
2016-02-18 10:53:35 +0100

    JavaDocs and code format for BreadcrumbPane widget

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/breadcrumb/BreadcrumbPane.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/breadcrumb/BreadcrumbPane.java
@@ -14,71 +14,78 @@ import com.google.gwt.user.client.ui.Widget;
 import com.sap.sse.gwt.client.LinkUtil;
 
 /**
- * @author zhorvath
+ * Widget that renders multiple clickable items with ">" as spacer.
+ * 
+ * Every item has:
+ * <ul>
+ * <li>a Text to show</li>
+ * <li>an action that is being triggered when doing a normal click</li>
+ * <li>a link that is used when the user clicks to open in a new tab</li>
+ * </ul>
  *
  */
 public class BreadcrumbPane extends Widget {
 
-	interface Resources extends ClientBundle {
-		@Source("Breadcrumb.css")
-		Style style();
-	}
+    interface Resources extends ClientBundle {
+        @Source("Breadcrumb.css")
+        Style style();
+    }
 
-	interface Style extends CssResource {
+    interface Style extends CssResource {
 
-		String event_header__breadcrumb();
+        String event_header__breadcrumb();
 
-		String event_header__breadcrumb__item();
+        String event_header__breadcrumb__item();
 
-	}
+    }
 
-	private static Resources resources;
+    private static Resources resources;
 
-	private static Resources getResources() {
-		if (resources == null) {
-			resources = GWT.create(Resources.class);
-			resources.style().ensureInjected();
-		}
-		return resources;
-	}
+    private static Resources getResources() {
+        if (resources == null) {
+            resources = GWT.create(Resources.class);
+            resources.style().ensureInjected();
+        }
+        return resources;
+    }
 
-	private final Style style = getResources().style();
+    private final Style style = getResources().style();
 
-	private final DivElement breadcrumDiv;
+    private final DivElement breadcrumDiv;
 
-	public BreadcrumbPane() {
-		breadcrumDiv = Document.get().createDivElement();
-		breadcrumDiv.addClassName(style.event_header__breadcrumb());
+    public BreadcrumbPane() {
+        breadcrumDiv = Document.get().createDivElement();
+        breadcrumDiv.addClassName(style.event_header__breadcrumb());
 
-		setElement(breadcrumDiv);
-	}
+        setElement(breadcrumDiv);
+    }
 
-	public void addBreadcrumbItem(String title, String link, final Runnable runnable) {
-	    addBreadcrumbItem(title, UriUtils.fromString(link), runnable);
-	}
-	
-	public void addBreadcrumbItem(String title, SafeUri link, final Runnable runnable) {
-		AnchorElement breadcrumItem = Document.get().createAnchorElement();
-		breadcrumItem.setHref(link);
-		breadcrumItem.setInnerText(title);
-		breadcrumItem.addClassName(style.event_header__breadcrumb__item());
+    public void addBreadcrumbItem(String title, String link, final Runnable runnable) {
+        addBreadcrumbItem(title, UriUtils.fromString(link), runnable);
+    }
 
-		breadcrumDiv.appendChild(breadcrumItem);
+    public void addBreadcrumbItem(String title, SafeUri link, final Runnable runnable) {
+        AnchorElement breadcrumItem = Document.get().createAnchorElement();
+        breadcrumItem.setHref(link);
+        breadcrumItem.setInnerText(title);
+        breadcrumItem.addClassName(style.event_header__breadcrumb__item());
 
-		Event.sinkEvents(breadcrumItem, Event.ONCLICK);
-		Event.setEventListener(breadcrumItem, new EventListener() {
-			@Override
-			public void onBrowserEvent(Event event) {
-				if (LinkUtil.handleLinkClick(event)) {
-					event.stopPropagation();
-					event.preventDefault();
-					runnable.run();
-				}
-			}
-		});
-	}
-	
-	public void clear() {
-	    breadcrumDiv.removeAllChildren();
-	}
+        breadcrumDiv.appendChild(breadcrumItem);
+
+        Event.sinkEvents(breadcrumItem, Event.ONCLICK);
+        Event.setEventListener(breadcrumItem, new EventListener() {
+            @Override
+            public void onBrowserEvent(Event event) {
+                if (LinkUtil.handleLinkClick(event)) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    runnable.run();
+                }
+            }
+        });
+    }
+
+    public void clear() {
+        breadcrumDiv.removeAllChildren();
+    }
 }
