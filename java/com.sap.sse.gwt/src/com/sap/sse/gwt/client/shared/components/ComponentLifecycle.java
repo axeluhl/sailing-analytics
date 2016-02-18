f2810fa7981d2018cce9cef2db20f41d29616326
fmittag <fmittag@gmx.net>
2016-02-18 17:39:39 +0100

    added some doc

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/shared/components/ComponentLifecycle.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/shared/components/ComponentLifecycle.java
@@ -4,7 +4,8 @@ import com.sap.sse.common.settings.Settings;
 
 /**
  * @author Frank
- *
+ * A ComponentLifecycle decouples the lifecycle of a component from the corresponding component instance.
+ * This way we can create e.g. settings for a component with a settings dialog without the need to instantiate the component.
  * @param <C> the component type
  * @param <S> the settings type
  * @param <SDC> the settings dialog component type
