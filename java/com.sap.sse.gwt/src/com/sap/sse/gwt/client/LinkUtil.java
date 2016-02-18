08b2a4d7d1588e75695633f2584a9956ba45f478
Steffen Schäfer <steffen.schaefer@oio.de>
2016-02-18 08:38:07 +0100

    Added JavaDocs to LinkUtil

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/LinkUtil.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/LinkUtil.java
@@ -5,14 +5,28 @@ import com.google.gwt.dom.client.AnchorElement;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.EventListener;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.impl.HyperlinkImpl;
 
+/**
+ * When just attaching a handler to an {@link Anchor} or {@link AnchorElement} to trigger a place change there are muiltiple disadvantages:
+ * <ul>
+ *   <li>Opening the page in a new tab via middle click or Ctrl+Click doesn't work</li>
+ *   <li>Clicks with any mouse key will trigger the handler</li>
+ * </ul>
+ * 
+ * This util helps to work around such problems.
+ */
 public final class LinkUtil {
     private static final HyperlinkImpl HYPERLINK_IMPL = GWT.create(HyperlinkImpl.class);
     
     private LinkUtil() {
     }
     
+    /**
+     * @param event a browser event to check if it is a "normal" click event
+     * @return true if the event is a "normal" click event, false otherwise
+     */
     public static boolean handleLinkClick(Event event) {
         if(event.getTypeInt() != Event.ONCLICK) {
             return false;
@@ -21,6 +35,9 @@ public final class LinkUtil {
     }
 
 
+    /**
+     * Configures click handling of the given {@link AnchorElement} to trigger the given action when a "normal" click was triggered.
+     */
     public static void configureForAction(AnchorElement anchor, final Runnable action) {
         DOM.sinkEvents(anchor, Event.ONCLICK);
         DOM.setEventListener(anchor, new EventListener() {
