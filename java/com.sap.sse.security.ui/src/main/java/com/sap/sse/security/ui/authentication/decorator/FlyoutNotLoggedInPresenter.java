94a1fa5c1001371676ffb48f96788621f9b4f175
Papick Garcia Taboada <papick.g.taboada@sap.com>
2016-02-24 10:14:15 +0100

    Removed wrong import/ javadoc

--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/decorator/FlyoutNotLoggedInPresenter.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/decorator/FlyoutNotLoggedInPresenter.java
@@ -1,14 +1,12 @@
 package com.sap.sse.security.ui.authentication.decorator;
 
-import org.apache.tools.ant.taskdefs.Javac.ImplementationSpecificArgument;
-
 import com.google.web.bindery.event.shared.EventBus;
 import com.sap.sse.security.ui.authentication.AuthenticationRequestEvent;
 
 /**
- * {@link NotLoggedInPresenter} {@link ImplementationSpecificArgument} that fires an {@link AuthenticationRequestEvent}
- * which is used by authentication UI implementations using a flyout to host the authentication UI. This event triggers
- * the flyout to be shown.
+ * {@link NotLoggedInPresenter} implementation that fires an {@link AuthenticationRequestEvent} which is used by
+ * authentication UI implementations using a flyout to host the authentication UI. This event triggers the flyout to be
+ * shown.
  *
  */
 public class FlyoutNotLoggedInPresenter implements NotLoggedInPresenter {
