988ee01760aea541837f16807149d17341bfbe67
fmittag <fmittag@gmx.net>
2016-03-01 17:29:51 +0100

    removed another unused class

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/shared/components/ComponentConstructorArgs.java
+++
@@ -1,7 +0,0 @@
-package com.sap.sse.gwt.client.shared.components;
-
-import com.sap.sse.common.settings.Settings;
-
-public interface ComponentConstructorArgs<C extends Component<S>, S extends Settings> {
-    C createComponent(S settings);
-}
