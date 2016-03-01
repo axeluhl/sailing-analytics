d6bded79dc2416beeb7533b8f678c036fc9ad4bf
fmittag <fmittag@gmx.net>
2016-03-01 17:11:29 +0100

    removed unused file

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/shared/perspective/PerspectiveConstructorArgs.java
+++
@@ -1,7 +0,0 @@
-package com.sap.sse.gwt.client.shared.perspective;
-
-import com.sap.sse.common.settings.Settings;
-import com.sap.sse.gwt.client.shared.components.ComponentConstructorArgs;
-
-public interface PerspectiveConstructorArgs<P extends Perspective<S>, S extends Settings> extends ComponentConstructorArgs<P, S> {
-}
