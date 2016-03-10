ffa30b06510cfba7eb5ede0f0ba6d263e02ce5a5
Benjamin Barth <benjamin.barth@sap.com>
2016-03-10 16:45:07 +0100

    Extended page object for event config to access list entries

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/event/EventConfigurationPanelPO.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/event/EventConfigurationPanelPO.java
@@ -2,14 +2,35 @@ package com.sap.sailing.selenium.pages.adminconsole.event;
 
 import java.util.Date;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.sap.sailing.selenium.core.BySeleniumId;
 import com.sap.sailing.selenium.core.FindBy;
 import com.sap.sailing.selenium.pages.PageArea;
+import com.sap.sailing.selenium.pages.common.ConfirmDialogPO;
+import com.sap.sailing.selenium.pages.gwt.CellTablePO;
+import com.sap.sailing.selenium.pages.gwt.DataEntryPO;
+import com.sap.sailing.selenium.pages.gwt.GenericCellTablePO;
 
 public class EventConfigurationPanelPO extends PageArea {
+    
+    public static class EventEntryPO extends DataEntryPO {
+
+        public EventEntryPO(CellTablePO<?> table, WebElement element) {
+            super(table, element);
+        }
+        
+        @Override
+        public String getIdentifier() {
+            return getColumnContent("Event");
+        }
+        
+        public String getEventURL() {
+            return getWebElement().findElement(By.xpath(".//td/div/a")).getAttribute("href");
+        }        
+    }
     
     @FindBy(how = BySeleniumId.class, using = "RefreshEventsButton")
     private WebElement refreshEventsButton;
@@ -19,15 +40,28 @@ public class EventConfigurationPanelPO extends PageArea {
     
     @FindBy(how = BySeleniumId.class, using = "RemoveEventsButton")
     private WebElement removeEventsButton;
+    
+    @FindBy(how = BySeleniumId.class, using = "EventsCellTable")
+    private WebElement eventsCellTable;
 
     public EventConfigurationPanelPO(WebDriver driver, WebElement element) {
         super(driver, element);
     }
     
-    public void createPublicEvent(String name, String venue, Date startDate, Date endDate) {
+    public void createSimplePublicEvent(String name, String venue, Date startDate, Date endDate) {
         createEventButton.click();
-        EventCreateDialogPO dialog = new EventCreateDialogPO(driver, findElementBySeleniumId(driver, "EventCreateDialog"));
-        dialog.createEvent(name, "", venue, startDate, endDate, true);
+        EventCreateDialogPO createDialog = getDialog(EventCreateDialogPO::new, "EventCreateDialog");
+        createDialog.createEvent(name, "", venue, startDate, endDate, true);
+        ConfirmDialogPO confirmDialog = getDialog(ConfirmDialogPO::new, "CreateDefaultLeaderboardGroupConfirmDialog");
+        confirmDialog.pressNo();
     }
-
+    
+    public EventEntryPO getEventEntry(String event) {
+        return getEventsTable().getEntry(event);
+    }
+    
+    private CellTablePO<EventEntryPO> getEventsTable() {
+        return new GenericCellTablePO<>(this.driver, this.eventsCellTable, EventEntryPO.class);
+    }
+    
 }
