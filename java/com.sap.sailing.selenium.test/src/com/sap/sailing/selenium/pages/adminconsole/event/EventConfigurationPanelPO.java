f096e6b820b3795552200e1e2f5abf4320388cc3
Benjamin Barth <benjamin.barth@sap.com>
2016-03-14 16:23:44 +0100

    Extended EventConfigurationPanelPO to create events with default leaderboard group and regatta with default leaderboard

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/event/EventConfigurationPanelPO.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/event/EventConfigurationPanelPO.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebElement;
 import com.sap.sailing.selenium.core.BySeleniumId;
 import com.sap.sailing.selenium.core.FindBy;
 import com.sap.sailing.selenium.pages.PageArea;
+import com.sap.sailing.selenium.pages.adminconsole.leaderboard.LeaderboardGroupCreateDialogPO;
+import com.sap.sailing.selenium.pages.adminconsole.regatta.RegattaCreateDialogPO;
 import com.sap.sailing.selenium.pages.common.ConfirmDialogPO;
 import com.sap.sailing.selenium.pages.gwt.CellTablePO;
 import com.sap.sailing.selenium.pages.gwt.DataEntryPO;
@@ -17,7 +19,12 @@ import com.sap.sailing.selenium.pages.gwt.GenericCellTablePO;
 public class EventConfigurationPanelPO extends PageArea {
     
     private static final String ID_EVENT_CREATE_DIALOG = "EventCreateDialog";
+    private static final String ID_REGATTA_CREATE_DIALOG = "RegattaCreateDialog";
+    private static final String ID_CREATE_DEFAULT_LEADERBOARD_GROUP_DIALOG = "LeaderboardGroupCreateDialog";
     private static final String ID_CREATE_DEFAULT_LEADERBOARD_GROUP_CONFIRM_DIALOG = "CreateDefaultLeaderboardGroupConfirmDialog";
+    private static final String ID_CREATE_DEFAULT_REGATTA_CONFIRM_DIALOG = "CreateDefaultRegattaDialog";
+    private static final String ID_CREATE_DEFAULT_REGATTA_LEADERBOARD_CONFIRM_DIALOG = "CreateDefaultRegattaLeaderboardDialog";
+    private static final String ID_LINK_LEADERBORAD_TO_GROUP_DIALOG = "LinkRegattaLeaderboardToLeaderboardGroupOfEventDialog";
     
     public static class EventEntryPO extends DataEntryPO {
 
@@ -52,11 +59,17 @@ public class EventConfigurationPanelPO extends PageArea {
     }
     
     public void createEmptyEvent(String name, String desc, String venue, Date startDate, Date endDate, boolean isPublic) {
-        createEventButton.click();
-        EventCreateDialogPO createDialog = getPO(EventCreateDialogPO::new, ID_EVENT_CREATE_DIALOG);
-        createDialog.createEvent(name, desc, venue, startDate, endDate, isPublic);
-        ConfirmDialogPO confirmDialog = getPO(ConfirmDialogPO::new, ID_CREATE_DEFAULT_LEADERBOARD_GROUP_CONFIRM_DIALOG);
-        confirmDialog.pressNo();
+        createEvent(name, desc, venue, startDate, endDate, isPublic).pressNo();
+    }
+    
+    public void createEventWithDefaultLeaderboardGroupRegattaAndDefaultLeaderboard(String eventName, String eventDesc,
+            String venue, Date eventStartDate, Date eventEndDate, boolean isPublic, String regattaName, String boatClass,
+            Date regattaStartDate, Date regattaEndDate) {
+        createEvent(eventName, eventDesc, venue, eventStartDate, eventEndDate, isPublic).pressYes();
+        getPO(LeaderboardGroupCreateDialogPO::new, ID_CREATE_DEFAULT_LEADERBOARD_GROUP_DIALOG).pressOk();
+        getPO(ConfirmDialogPO::new, ID_CREATE_DEFAULT_REGATTA_CONFIRM_DIALOG).pressYes();
+        createRegatta(regattaName, boatClass, regattaStartDate, regattaEndDate).pressYes();
+        getPO(ConfirmDialogPO::new, ID_LINK_LEADERBORAD_TO_GROUP_DIALOG).pressOk();
     }
     
     public EventEntryPO getEventEntry(String event) {
@@ -65,6 +78,21 @@ public class EventConfigurationPanelPO extends PageArea {
     
     private CellTablePO<EventEntryPO> getEventsTable() {
         return new GenericCellTablePO<>(this.driver, this.eventsCellTable, EventEntryPO.class);
+    }
+    
+    private ConfirmDialogPO createEvent(String name, String desc, String venue, Date start, Date end, boolean isPublic) {
+        createEventButton.click();
+        EventCreateDialogPO createDialog = getPO(EventCreateDialogPO::new, ID_EVENT_CREATE_DIALOG);
+        createDialog.setValues(name, desc, venue, start, end, isPublic);
+        createDialog.pressOk();
+        return getPO(ConfirmDialogPO::new, ID_CREATE_DEFAULT_LEADERBOARD_GROUP_CONFIRM_DIALOG);
+    }
+    
+    private ConfirmDialogPO createRegatta(String name, String boatClass, Date start, Date end) {
+        RegattaCreateDialogPO createDialog = getPO(RegattaCreateDialogPO::new, ID_REGATTA_CREATE_DIALOG);
+        createDialog.setValues(name, boatClass, start, end);
+        createDialog.pressOk();
+        return getPO(ConfirmDialogPO::new, ID_CREATE_DEFAULT_REGATTA_LEADERBOARD_CONFIRM_DIALOG);
     }
     
 }
