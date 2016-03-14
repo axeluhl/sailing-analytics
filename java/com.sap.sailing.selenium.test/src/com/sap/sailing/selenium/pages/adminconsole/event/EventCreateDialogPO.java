053549c87cc3b543c0f3d3a61dcecbb819282ee8
Benjamin Barth <benjamin.barth@sap.com>
2016-03-14 16:21:14 +0100

    Adjusted EventCreateDialogPO date and workflow handling

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/event/EventCreateDialogPO.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/event/EventCreateDialogPO.java
@@ -1,7 +1,5 @@
 package com.sap.sailing.selenium.pages.adminconsole.event;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.openqa.selenium.WebDriver;
@@ -16,8 +14,6 @@ import com.sap.sailing.selenium.pages.gwt.TextAreaPO;
 import com.sap.sailing.selenium.pages.gwt.TextBoxPO;
 
 public class EventCreateDialogPO extends DataEntryDialogPO {
-
-    private static final DateFormat DATE_TIME_BOX_FORMAT = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 
     @FindBy(how = BySeleniumId.class, using = "NameTextBox")
     private WebElement nameTextBox;
@@ -41,14 +37,13 @@ public class EventCreateDialogPO extends DataEntryDialogPO {
         super(driver, element);
     }
     
-    void createEvent(String name, String description, String venue, Date start, Date end, boolean isPublic) {
+    void setValues(String name, String description, String venue, Date start, Date end, boolean isPublic) {
         TextBoxPO.create(driver, nameTextBox).appendText(name);
         TextAreaPO.create(driver, descriptionTextArea).appendText(description);
         TextBoxPO.create(driver, venueTextBox).appendText(venue);
-        BetterDateTimeBoxPO.create(driver, startDateTimeBox).setText(DATE_TIME_BOX_FORMAT.format(start));
-        BetterDateTimeBoxPO.create(driver, endDateTimeBox).setText(DATE_TIME_BOX_FORMAT.format(end));
+        BetterDateTimeBoxPO.create(driver, startDateTimeBox).setDate(start);
+        BetterDateTimeBoxPO.create(driver, endDateTimeBox).setDate(end);
         CheckBoxPO.create(driver, isPublicCheckBox).setSelected(isPublic);
-        pressOk();
     }
 
 }
