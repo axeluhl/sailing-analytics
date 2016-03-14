d5dfc027b7ea73e6c05d4438a5e30e89cfc5b6f6
Benjamin Barth <benjamin.barth@sap.com>
2016-03-14 11:16:12 +0100

    Added verification to EventPage to ensure page is loaded

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/home/event/EventPage.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/home/event/EventPage.java
@@ -1,12 +1,17 @@
 package com.sap.sailing.selenium.pages.home.event;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.sap.sailing.selenium.core.BySeleniumId;
 import com.sap.sailing.selenium.pages.HostPage;
 import com.sap.sailing.selenium.pages.HostPageWithAuthentication;
 import com.sap.sailing.selenium.pages.PageObject;
 
 public class EventPage extends HostPageWithAuthentication {
+    
+    private static final String EVENT_HEADER_IDENTIFIER = "EventHeaderPanel";
     
     /**
      * Navigates to the given home URL and provides the corresponding {@link PageObject}.
@@ -23,8 +28,14 @@ public class EventPage extends HostPageWithAuthentication {
         super(driver);
     }
     
+    @Override
+    protected void verify() {
+        WebDriverWait wait = new WebDriverWait(driver, DEFAULT_WAIT_TIMEOUT_SECONDS);
+        wait.until(ExpectedConditions.presenceOfElementLocated(new BySeleniumId(EVENT_HEADER_IDENTIFIER)));
+    }
+    
     public EventHeaderPO getEventHeader() {
-        return getPO(EventHeaderPO::new, "EventHeaderPanel");
+        return getPO(EventHeaderPO::new, EVENT_HEADER_IDENTIFIER);
     }
 
 }
