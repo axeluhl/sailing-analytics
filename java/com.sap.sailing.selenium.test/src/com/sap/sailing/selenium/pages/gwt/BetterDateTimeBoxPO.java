e3b4016f71b6ff0c376cbfb667c7eeb8a0200198
Benjamin Barth <benjamin.barth@sap.com>
2016-03-14 13:40:09 +0100

    Added convenience method to BetterDateTimeBoxPO to set date objects

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/gwt/BetterDateTimeBoxPO.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/gwt/BetterDateTimeBoxPO.java
@@ -1,5 +1,9 @@
 package com.sap.sailing.selenium.pages.gwt;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -9,6 +13,8 @@ import com.sap.sailing.selenium.pages.PageObject;
  * {@link PageObject} implementation for better date time boxes.
  */
 public class BetterDateTimeBoxPO extends TextBoxPO {
+    
+    private static final DateFormat DATE_TIME_BOX_FORMAT = new SimpleDateFormat("dd/MM/yyyy hh:mm");
     
     /**
      * Factory method to create a {@link BetterDateTimeBoxPO}.
@@ -26,6 +32,19 @@ public class BetterDateTimeBoxPO extends TextBoxPO {
      */
     protected BetterDateTimeBoxPO(WebDriver driver, WebElement element) {
         super(driver, element);
+    }
+    
+    /**
+     * Sets the underlying {@link WebElement}s text by formatting the given {@link Date} using the internal date format
+     * (<code>dd/MM/yyyy hh:mm</code>).
+     * 
+     * @param date the {@link Date} object to set
+     * 
+     * @see DateFormat#format(Date)
+     * @see #setText(String)
+     */
+    public void setDate(Date date) {
+        this.setText(DATE_TIME_BOX_FORMAT.format(date));
     }
     
     @Override
