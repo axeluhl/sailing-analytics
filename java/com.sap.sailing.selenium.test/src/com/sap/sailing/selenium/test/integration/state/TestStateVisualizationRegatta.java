5d1d573dda91c4bb3debf202e266b4c8848976e7
Benjamin Barth <benjamin.barth@sap.com>
2016-03-15 17:52:36 +0100

    Adjusted test cases for a live regatta within a live event

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/test/integration/state/TestStateVisualizationRegatta.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/test/integration/state/TestStateVisualizationRegatta.java
@@ -12,6 +12,9 @@ import org.junit.Test;
 
 import com.sap.sailing.selenium.pages.adminconsole.AdminConsolePage;
 import com.sap.sailing.selenium.pages.adminconsole.event.EventConfigurationPanelPO;
+import com.sap.sailing.selenium.pages.adminconsole.leaderboard.LeaderboardConfigurationPanelPO;
+import com.sap.sailing.selenium.pages.adminconsole.leaderboard.LeaderboardDetailsPanelPO;
+import com.sap.sailing.selenium.pages.adminconsole.leaderboard.SetStartTimeDialogPO;
 import com.sap.sailing.selenium.pages.common.LabelTypePO;
 import com.sap.sailing.selenium.pages.home.event.RegattaListItemPO;
 import com.sap.sailing.selenium.pages.home.event.regatta.RegattaEventPage;
@@ -27,7 +30,9 @@ public class TestStateVisualizationRegatta extends AbstractSeleniumTest {
     
     @Test
     public void testStateFlagOfUpcomingRegatta() {
-        testStateFlagOfRegatta("TestRegatta", getFutureDate(3), getFutureDate(7), LabelTypePO::isUpcoming);
+        TestCase testCase = new TestCase();
+        testCase.createTestEventWithRegatta("TestRegatta", getFutureDate(3), getFutureDate(7));
+        testCase.assertStateFlagOfRegatta("TestRegatta", LabelTypePO::isUpcoming);
     }
 
     // @Test
@@ -35,27 +40,45 @@ public class TestStateVisualizationRegatta extends AbstractSeleniumTest {
         // TODO
     }
 
-    // @Test
+    @Test
     public void testStateFlagOfLiveRegatta() {
-        // TODO
+        TestCase testCase = new TestCase();
+        testCase.createTestEventWithRegatta("TestLiveRegatta", getPastDate(3), getFutureDate(3));
+        testCase.setLeaderboardsRaceStartTime("TestLiveRegatta", "R1", getPastDate(1));
+        testCase.assertStateFlagOfRegatta("TestLiveRegatta", LabelTypePO::isLive);
     }
 
     @Test
     public void testStateFlagOfFinishedRegatta() {
-        testStateFlagOfRegatta("TestRegatta", getPastDate(7), getPastDate(3), LabelTypePO::isFinished);
+        TestCase testCase = new TestCase();
+        testCase.createTestEventWithRegatta("TestRegatta", getPastDate(3), getPastDate(7));
+        testCase.assertStateFlagOfRegatta("TestRegatta", LabelTypePO::isFinished);
     }
     
-    private void testStateFlagOfRegatta(String regattaName, Date regattaStartDate, Date regattaEndDate,
-            Predicate<LabelTypePO> expectedLabelType) {
-        AdminConsolePage adminConsolePage = AdminConsolePage.goToPage(getWebDriver(), getContextRoot());
-        EventConfigurationPanelPO eventConfigurationPanel = adminConsolePage.goToEvents();
-        final String eventName = "RegattaStateTestEvent", venue = "Anywhere", boatClass = "J-22";
-        eventConfigurationPanel.createEventWithDefaultLeaderboardGroupRegattaAndDefaultLeaderboard(eventName, null,
-                venue, getPastDate(3), getFutureDate(3), true, regattaName, boatClass, regattaStartDate, regattaEndDate);
-        String eventUrl = eventConfigurationPanel.getEventEntry(eventName).getEventURL();
-        RegattaEventPage regattaEventPage = RegattaEventPage.goToRegattaEventUrl(getWebDriver(), eventUrl);
-        RegattaListItemPO regattaListItem = regattaEventPage.getRegattaListItem(regattaName);
-        Assert.assertTrue(expectedLabelType.test(regattaListItem.getRegattaHeader().getRegattaStateLabel()));
+    private class TestCase {
+        private final AdminConsolePage adminConsolePage = AdminConsolePage.goToPage(getWebDriver(), getContextRoot());
+        private String eventUrl; 
+        
+        private void createTestEventWithRegatta(String regattaName, Date regattaStartDate, Date regattaEndDate) {
+            EventConfigurationPanelPO eventConfigPanel = adminConsolePage.goToEvents();
+            final String eventName = "RegattaStateTestEvent", venue = "Anywhere", boatClass = "J-22";
+            eventConfigPanel.createEventWithDefaultLeaderboardGroupRegattaAndDefaultLeaderboard(eventName, null, venue,
+                    getPastDate(3), getFutureDate(3), true, regattaName, boatClass, regattaStartDate, regattaEndDate);
+            this.eventUrl = eventConfigPanel.getEventEntry(eventName).getEventURL();
+        }
+        
+        private void setLeaderboardsRaceStartTime(String leaderboard, String race, Date startTime) {
+            LeaderboardConfigurationPanelPO leaderboardConfiguration = adminConsolePage.goToLeaderboardConfiguration();
+            LeaderboardDetailsPanelPO leaderboardDetails = leaderboardConfiguration.getLeaderboardDetails(leaderboard);
+            SetStartTimeDialogPO startTimeDialog = leaderboardDetails.getRacesTable().getEntry(race).clickSetStartTime();
+            startTimeDialog.setStartTimeValue(startTime);
+            startTimeDialog.pressSetStartTime();
+        }
+        
+        private void assertStateFlagOfRegatta(String regattaName, Predicate<LabelTypePO> expectedLabelType) {
+            RegattaEventPage regattaEventPage = RegattaEventPage.goToRegattaEventUrl(getWebDriver(), eventUrl);
+            RegattaListItemPO regattaListItem = regattaEventPage.getRegattaListItem(regattaName);
+            Assert.assertTrue(expectedLabelType.test(regattaListItem.getRegattaHeader().getRegattaStateLabel()));
+        }
     }
-    
 }
