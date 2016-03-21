fab845dd899b562ae726540289da0d64f4adea04
Jan Broß <jan.bross@sap.com>
2016-03-21 15:09:15 +0100

    Add toggle button to Start/StopTracking Dialog

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/AdminConsoleResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/AdminConsoleResources.java
@@ -69,6 +69,9 @@ interface AdminConsoleResources extends ClientBundle {
 
     @Source("com/sap/sailing/gwt/ui/client/images/start_racelog_tracking.png")
     ImageResource startRaceLogTracking();
+    
+    @Source("com/sap/sailing/gwt/ui/client/images/stop_racelog_tracking.png")
+    ImageResource stopRaceLogTracking();
 
     @Source("com/sap/sailing/gwt/ui/client/images/competitor_registrations.png")
     ImageResource competitorRegistrations();
