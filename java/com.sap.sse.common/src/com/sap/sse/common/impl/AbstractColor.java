a003165cfc70562911ad8d10af1c86ad0d9fe07b
Charlotte Proeller <charlotte.proeller@sap.com>
2016-04-03 17:22:53 +0200

    bug 3549: added all 140 official CSS named colors as constants in Color interface

--- java/com.sap.sse.common/src/com/sap/sse/common/impl/AbstractColor.java
+++ java/com.sap.sse.common/src/com/sap/sse/common/impl/AbstractColor.java
@@ -1,11 +1,17 @@
 package com.sap.sse.common.impl;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import com.google.gwt.dev.util.collect.HashMap;
 import com.sap.sse.common.Color;
 import com.sap.sse.common.Util;
 
 public abstract class AbstractColor implements Color {
     private static final long serialVersionUID = 7758884012281863458L;
 
+    private static final Map<String,Color> colorNameToColor = createColorMap();
+    
     @Override
     public String getAsHtml() {
         Util.Triple<Integer, Integer, Integer> asRGB = getAsRGB();
@@ -13,6 +19,22 @@ public abstract class AbstractColor implements Color {
                 + toBrowserHexValue(asRGB.getC());
     }
     
+    private static Map<String, Color> createColorMap() {
+        HashMap<String,Color> map = new HashMap<String, Color>();
+        String name;
+        for(Field field: Color.class.getFields()){
+            if(field.getClass().isAssignableFrom(Color.class)){
+                name = field.getName().toLowerCase().replace("_", "");
+                try {
+                    map.put(name, (Color) field.get(null));
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                   //ignore this case, and don't add something to the map.
+                }
+            }
+        }
+        return map;
+    }
+
     private static String toBrowserHexValue(int number) {
         StringBuilder builder = new StringBuilder(Integer.toHexString(number & 0xff));
         while (builder.length() < 2) {
@@ -22,36 +44,6 @@ public abstract class AbstractColor implements Color {
     }
 
     public static Color getColorByLowercaseNameStatic(String lowercaseColorName) {
-        final Color result;
-        if ("white".equals(lowercaseColorName)) {
-            result = WHITE;
-        } else if ("light_gray".equals(lowercaseColorName)) {
-            result = LIGHT_GRAY;
-        } else if ("gray".equals(lowercaseColorName)) {
-            result = GRAY;
-        } else if ("dark_gray".equals(lowercaseColorName)) {
-            result = DARK_GRAY;
-        } else if ("black".equals(lowercaseColorName)) {
-            result = BLACK;
-        } else if ("red".equals(lowercaseColorName)) {
-            result = RED;
-        } else if ("pink".equals(lowercaseColorName)) {
-            result = PINK;
-        } else if ("orange".equals(lowercaseColorName)) {
-            result = ORANGE;
-        } else if ("yellow".equals(lowercaseColorName)) {
-            result = YELLOW;
-        } else if ("green".equals(lowercaseColorName)) {
-            result = GREEN;
-        } else if ("magenta".equals(lowercaseColorName)) {
-            result = MAGENTA;
-        } else if ("cyan".equals(lowercaseColorName)) {
-            result = CYAN;
-        } else if ("blue".equals(lowercaseColorName)) {
-            result = BLUE;
-        } else {
-            result = null;
-        }
-        return result;
+            return colorNameToColor.get(lowercaseColorName);
     }
 }
