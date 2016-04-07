0982bd23ef8b18b359aae9f4ffefd9dfc5214c75
Benjamin Barth <benjamin.barth@sap.com>
2016-04-07 15:58:03 +0200

    Moved SensorFixMapperFactory interface and impl class to separate package

--- java/com.sap.sailing.domain.racelogtrackingadapter/src/com/sap/sailing/domain/racelogsensortracking/SensorFixMapperFactory.java
+++ java/com.sap.sailing.domain.racelogtrackingadapter/src/com/sap/sailing/domain/racelogsensortracking/SensorFixMapperFactory.java
@@ -1,7 +1,8 @@
-package com.sap.sailing.domain.racelog.tracking;
+package com.sap.sailing.domain.racelogsensortracking;
 
 import com.sap.sailing.domain.abstractlog.regatta.events.RegattaLogDeviceCompetitorMappingEvent;
 import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.racelog.tracking.SensorFixMapper;
 import com.sap.sailing.domain.tracking.Track;
 import com.sap.sse.common.Timed;
 
@@ -12,7 +13,7 @@ public interface SensorFixMapperFactory {
     
     <FixT extends Timed, TrackT extends Track<?>> SensorFixMapper<FixT, TrackT, Competitor> 
             createCompetitorMapper(Class<?> eventType);
-    
+
 //    <FixT extends Timed, TrackT extends Track<?>> SensorFixMapper<FixT, TrackT, Mark> 
 //            createMarkMapper(RegattaLogDeviceMarkMappingEvent event);
 }
--- java/com.sap.sailing.domain.racelogtrackingadapter/src/com/sap/sailing/domain/racelogsensortracking/impl/SensorFixMapperFactoryImpl.java
+++ java/com.sap.sailing.domain.racelogtrackingadapter/src/com/sap/sailing/domain/racelogsensortracking/impl/SensorFixMapperFactoryImpl.java
@@ -1,15 +1,23 @@
-package com.sap.sailing.domain.racelogtracking.impl;
+package com.sap.sailing.domain.racelogsensortracking.impl;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
 
 import com.sap.sailing.domain.abstractlog.regatta.events.RegattaLogDeviceCompetitorMappingEvent;
-import com.sap.sailing.domain.abstractlog.regatta.events.RegattaLogDeviceCompetitorSensorDataMappingEvent;
 import com.sap.sailing.domain.base.Competitor;
 import com.sap.sailing.domain.racelog.tracking.SensorFixMapper;
-import com.sap.sailing.domain.racelog.tracking.SensorFixMapperFactory;
+import com.sap.sailing.domain.racelogsensortracking.SensorFixMapperFactory;
 import com.sap.sailing.domain.tracking.Track;
 import com.sap.sse.common.Timed;
 
 public class SensorFixMapperFactoryImpl implements SensorFixMapperFactory {
+    
+    private final ServiceTracker<SensorFixMapper<?, ?, ?>, SensorFixMapper<?, ?, ?>> tracker;
 
+    public SensorFixMapperFactoryImpl(ServiceTracker<SensorFixMapper<?, ?, ?>, SensorFixMapper<?, ?, ?>> tracker) {
+        this.tracker = tracker;
+    }
+    
     @Override
     public <FixT extends Timed, TrackT extends Track<?>> SensorFixMapper<FixT, TrackT, Competitor>
             createCompetitorMapper(RegattaLogDeviceCompetitorMappingEvent event) {
@@ -17,11 +25,14 @@ public class SensorFixMapperFactoryImpl implements SensorFixMapperFactory {
     }
     
     @Override
+    @SuppressWarnings("unchecked")
     public <FixT extends Timed, TrackT extends Track<?>> SensorFixMapper<FixT, TrackT, Competitor>
             createCompetitorMapper(Class<?> eventType) {
-        // TODO use more concrete interfaces to determine event type
-        if (RegattaLogDeviceCompetitorSensorDataMappingEvent.class.isAssignableFrom(eventType)) {
-            return (SensorFixMapper<FixT, TrackT, Competitor>) new BravoDataFixMapper();
+        for (ServiceReference<SensorFixMapper<?, ?, ?>> serviceReference : tracker.getServiceReferences()) {
+            SensorFixMapper<?, ?, ?> service = tracker.getService(serviceReference);
+            if (service != null && service.isResponsibleFor(eventType)) {
+                return (SensorFixMapper<FixT, TrackT, Competitor>) service;
+            }
         }
         throw new RuntimeException("No SensorFixMapper found for event type: " + eventType);
     }
