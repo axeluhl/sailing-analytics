d2f5ffa92993a1fc86e5c8f2d11c89da566e6ae5
Benjamin Barth <benjamin.barth@sap.com>
2016-04-07 11:55:09 +0200

    Fill natural sort information for finished races

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/server/RaceListDataCalculator.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/server/RaceListDataCalculator.java
@@ -10,11 +10,13 @@ import com.sap.sailing.gwt.home.server.EventActionUtil.RaceCallback;
 @GwtIncompatible
 public class RaceListDataCalculator implements RaceCallback {
     private final TreeSet<RaceListRaceDTO> races = new TreeSet<>();
+    private int naturalOrder = 0;
 
     @Override
     public void doForRace(RaceContext context) {
         RaceListRaceDTO finishedRace = context.getFinishedRaceOrNull();
         if(finishedRace != null) {
+            finishedRace.setNaturalOrder(naturalOrder++);
             races.add(finishedRace);
         }
     }
