72daf65d7c85f221bea11d24bad8aeefa150973f
fmittag <fmittag@gmx.net>
2016-04-09 17:37:52 +0200

    removed bundle com.sap.sailing.yachtscoring -> not needed

--- java/com.sap.sailing.yachtscoring/src/com/sap/sailing/yachtscoring/YachtscoringEventResultsParser.java
+++
@@ -1,8 +0,0 @@
-package com.sap.sailing.yachtscoring;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-public interface YachtscoringEventResultsParser {
-    public EventResultDescriptor getEventResult(InputStream is) throws IOException;
-}
