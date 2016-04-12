21d9532e0dc396e42409a4db9e9080e30bae6ef1
Benjamin Barth <benjamin.barth@sap.com>
2016-04-12 17:08:25 +0200

    Added Serializable interface to TrackListenerCollection

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/TrackListenerCollection.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/TrackListenerCollection.java
@@ -3,6 +3,7 @@ package com.sap.sailing.domain.tracking.impl;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ConcurrentModificationException;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,8 +11,8 @@ import java.util.Set;
 import com.sap.sailing.domain.tracking.TrackListener;
 import com.sap.sse.common.Timed;
 
-public class TrackListenerCollection<ItemType, FixType extends Timed, ListenerType extends TrackListener> {
-
+public class TrackListenerCollection<ItemType, FixType extends Timed, ListenerType extends TrackListener> implements Serializable {
+    private static final long serialVersionUID = -7324146114278369375L;
     private Set<ListenerType> listeners;
     
     public TrackListenerCollection() {
