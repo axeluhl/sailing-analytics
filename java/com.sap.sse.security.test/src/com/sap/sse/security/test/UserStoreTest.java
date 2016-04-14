426a944f3be0c0433b8ddb55590db63d9575e486
Axel Uhl <axel.uhl@sap.com>
2016-04-14 18:30:55 +0200

    first failing test case that shows that the UserStore doesn't clear the userByAccessToken structure in its clear() method
    Change-Id: Id93bdbcc16a6f94c08547fa596685dfbe04cdb8a

--- java/com.sap.sse.security.test/src/com/sap/sse/security/test/UserStoreTest.java
+++ java/com.sap.sse.security.test/src/com/sap/sse/security/test/UserStoreTest.java
@@ -6,21 +6,24 @@ import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
 import com.sap.sse.security.UserStore;
+import com.sap.sse.security.shared.UserManagementException;
 import com.sap.sse.security.userstore.mongodb.UserStoreImpl;
 
 public class UserStoreTest {
     @Test
-    public void testClear() {
+    public void testClear() throws UserManagementException {
         final UserStore userStore = new UserStoreImpl();
         final String username = "abc";
+        userStore.deleteUser(username);
+        final String email = "e@mail.com";
         final String accessToken = "ak";
+        userStore.createUser(username, email);
         userStore.setAccessToken(username, accessToken);
         final String prefKey = "pk";
         final String prefValue = "pv";
         userStore.setPreference(username, prefKey, prefValue);
         assertEquals(prefValue, userStore.getPreference(username, prefKey));
         assertEquals(username, userStore.getUserByAccessToken(accessToken).getName());
-        assertEquals(accessToken, userStore.getAccessToken(username));
         
         userStore.clear();
         assertNull(userStore.getPreference(username, prefKey));
