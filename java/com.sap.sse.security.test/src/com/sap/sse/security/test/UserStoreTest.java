8781eefbeb6fa630e128feb65a26d752079b2c82
Axel Uhl <axel.uhl@sap.com>
2016-04-14 18:41:33 +0200

    added second fialing test case for user store
    Change-Id: Iaaa502113b89689aac04267fe5ea158230d5cee4

--- java/com.sap.sse.security.test/src/com/sap/sse/security/test/UserStoreTest.java
+++ java/com.sap.sse.security.test/src/com/sap/sse/security/test/UserStoreTest.java
@@ -3,6 +3,7 @@ package com.sap.sse.security.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.sap.sse.security.UserStore;
@@ -10,24 +11,35 @@ import com.sap.sse.security.shared.UserManagementException;
 import com.sap.sse.security.userstore.mongodb.UserStoreImpl;
 
 public class UserStoreTest {
-    @Test
-    public void testClear() throws UserManagementException {
-        final UserStore userStore = new UserStoreImpl();
-        final String username = "abc";
-        userStore.deleteUser(username);
-        final String email = "e@mail.com";
-        final String accessToken = "ak";
+    private final UserStore userStore = new UserStoreImpl(null, null);
+    private final String username = "abc";
+    private final String email = "e@mail.com";
+    private final String accessToken = "ak";
+    private final String prefKey = "pk";
+    private final String prefValue = "pv";
+    
+    @Before
+    public void setUp() throws UserManagementException {
         userStore.createUser(username, email);
         userStore.setAccessToken(username, accessToken);
-        final String prefKey = "pk";
-        final String prefValue = "pv";
         userStore.setPreference(username, prefKey, prefValue);
+    }
+    
+    @Test
+    public void testClear() throws UserManagementException {
         assertEquals(prefValue, userStore.getPreference(username, prefKey));
         assertEquals(username, userStore.getUserByAccessToken(accessToken).getName());
-        
         userStore.clear();
         assertNull(userStore.getPreference(username, prefKey));
         assertNull(userStore.getUserByAccessToken(accessToken));
         assertNull(userStore.getAccessToken(username));
+    }
+
+    @Test
+    public void testUpdate() throws UserManagementException {
+        UserStore newUserStore = new UserStoreImpl(null, null);
+        newUserStore.replaceContentsFrom(userStore);
+        assertEquals(prefValue, newUserStore.getPreference(username, prefKey));
+        assertEquals(username, newUserStore.getUserByAccessToken(accessToken).getName());
     }
 }
