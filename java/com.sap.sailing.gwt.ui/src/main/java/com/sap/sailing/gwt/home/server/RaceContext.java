4d01dffa9a8aa67a542bf6c27f9629035d9f8f91
Axel Uhl <axel.uhl@sap.com>
2016-04-27 17:39:08 +0200

    use TrackedRace.getEndOfRace() in RaceContext.getWinnerOrNull() to ensure score corrections are applied;
    this fixes bug 3634 which so far had been preferring RaceState.getFinishedTime() which is based on the blue flag down
    event which, however, may be before TrackedRace.getEndOfRace(). We may consider to use RaceState.getFinishedTime() as
    a default for TrackedRace.getEndOfRace(), particularly in doubtful mark passing situations, and maybe always, since
    it would correctly ignore much delayed mark passings outside of the finishing window
    
    Change-Id: I710f9dbcb5a73f6397759be2db8b41311910dbd7

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/server/RaceContext.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/server/RaceContext.java
@@ -436,8 +436,8 @@ public class RaceContext {
         }
         // TODO do not calculate the winner if the blue flag is currently shown.
         try {
-            TimePoint finishTime = getFinishTime();
-            if(finishTime == null) {
+            TimePoint finishTime = trackedRace.getEndOfRace();
+            if (finishTime == null) {
                 finishTime = HomeServiceUtil.getLiveTimePoint();
             }
             List<Competitor> competitors = leaderboard.getCompetitorsFromBestToWorst(raceColumn, finishTime);
