84f8b7ee5ba052157dfe9a9a11ac9a5f4211377f
Axel Uhl <axel.uhl@sap.com>
2016-05-10 14:09:39 +0200

    report a Shiro UnauthenticatedException through RPC as such
    Change-Id: I17e13210be8bb5b5fdb2d652587113a8273917e8

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/dispatch/servlets/ProxiedRemoteServiceServlet.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/dispatch/servlets/ProxiedRemoteServiceServlet.java
@@ -1,7 +1,12 @@
 package com.sap.sse.gwt.dispatch.servlets;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gwt.user.server.rpc.RPCServletUtils;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.google.gwt.user.server.rpc.SerializationPolicy;
 
@@ -29,6 +34,35 @@ public abstract class ProxiedRemoteServiceServlet extends RemoteServiceServlet {
         }
 
         return super.doGetSerializationPolicy(request, moduleBaseURL, strongName);
+    }
+
+    @Override
+    protected void doUnexpectedFailure(Throwable e) {
+        if (e.getCause().getClass().getName().equals("org.apache.shiro.authz.UnauthenticatedException")) {
+            final HttpServletResponse servletResponse = getThreadLocalResponse();
+            try {
+                servletResponse.reset();
+            } catch (IllegalStateException ex) {
+                throw new RuntimeException("Unable to report failure", e);
+            }
+            ServletContext servletContext = getServletContext();
+            RPCServletUtils.writeResponseForUnexpectedFailure(servletContext, servletResponse, e);
+            servletContext.log("Exception while dispatching incoming RPC call", e.getCause());
+            try {
+                servletResponse.setContentType("text/plain");
+                servletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                try {
+                    servletResponse.getOutputStream().write(e.getCause().getLocalizedMessage().getBytes("UTF-8"));
+                } catch (IllegalStateException ex) {
+                    // Handle the (unexpected) case where getWriter() was previously used
+                    servletResponse.getWriter().write("Couldn't write exception");
+                }
+            } catch (IOException ex) {
+                servletContext.log(
+                        "respondWithUnexpectedFailure failed while sending the previous failure to the client", ex);
+            }
+        } else {
+            super.doUnexpectedFailure(e);
+        }
     }    
-    
 }
