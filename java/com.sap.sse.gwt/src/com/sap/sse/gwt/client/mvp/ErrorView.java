2ce5c676bf58018d3e30be8a7f7b805ba39c1646
Steffen Schaefer <steffen.schaefer@sap.com>
2016-06-21 14:57:59 +0200

    Added JavaDocs to BusyView and ErrorView

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/mvp/ErrorView.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/mvp/ErrorView.java
@@ -2,6 +2,9 @@ package com.sap.sse.gwt.client.mvp;
 
 import com.google.gwt.user.client.ui.IsWidget;
 
+/**
+ * View that shows an application error.
+ */
 public interface ErrorView extends IsWidget {
 
 }
