3564470b6a5f2038a458ad99be5c113cbbc0d42c
Steffen Schaefer <steffen.schaefer@sap.com>
2016-06-23 14:23:59 +0200

    Added JavaDocs for SensorFixStore

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/racelog/tracking/SensorFixStore.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/racelog/tracking/SensorFixStore.java
@@ -11,11 +11,29 @@ import com.sap.sse.common.TimeRange;
 import com.sap.sse.common.Timed;
 
 
+/**
+ * Store abstraction for persistence of Fixes (e.g. GPSFix).
+ */
 public interface SensorFixStore {
     
+    /**
+     * Loads fixes for a device in a given time range.
+     * 
+     * @param consumer will be called for each loaded fix. Must not be <code>null</code>.
+     * @param deviceIdentifier the device to load the fixes for. Must not be <code>null</code>.
+     * @param start the lower bound of the time range to load. Must not be <code>null</code>.
+     * @param end the upper bound of the time range to load. Must not be <code>null</code>.
+     * @param inclusive true if fixes exactly at the bounds of the time range should be loaded, false otherwise.
+     */
     <FixT extends Timed> void loadFixes(Consumer<FixT> consumer, DeviceIdentifier deviceIdentifier, TimePoint start, TimePoint end, boolean inclusive) throws NoCorrespondingServiceRegisteredException,
     TransformationException;
 
+    /**
+     * Saves a fix for the given device and informs all registered listeners about the new fix.
+     * 
+     * @param device the device to store the fix for. Must not be <code>null</code>.
+     * @param fix The fix to store. Must not be <code>null</code>.
+     */
     <FixT extends Timed> void storeFix(DeviceIdentifier device, FixT fix);
 
     /**
