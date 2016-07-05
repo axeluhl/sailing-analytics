4edc91f10b859f5673f3bc4f6ff901b797d495c6
Steffen Schaefer <steffen.schaefer@sap.com>
2016-07-05 13:34:02 +0200

    Added JavaDoc to PreferenceObjectListener

--- java/com.sap.sse.security/src/com/sap/sse/security/PreferenceObjectListener.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/PreferenceObjectListener.java
@@ -1,5 +1,10 @@
 package com.sap.sse.security;
 
+/**
+ * Listener used by UserStore to notify about changes of stored preference objects.
+ *
+ * @param <PrefT> The type of preference object this Listener handles.
+ */
 public interface PreferenceObjectListener<PrefT> {
     void preferenceObjectChanged(String username, String key, PrefT oldPreference, PrefT newPreference);
 }
