8059be9fb725c34a567851e295b582bfc62bb009
Steffen Schaefer <steffen.schaefer@sap.com>
2016-07-06 11:40:28 +0200

    Implemented specific setting to handle BoatClassMasterdata

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/preferences/BoatClassNotificationPreference.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/preferences/BoatClassNotificationPreference.java
@@ -1,34 +1,34 @@
 package com.sap.sailing.domain.common.preferences;
 
+import com.sap.sailing.domain.common.BoatClassMasterdata;
 import com.sap.sse.common.settings.generic.AbstractGenericSerializableSettings;
 import com.sap.sse.common.settings.generic.BooleanSetting;
-import com.sap.sse.common.settings.generic.StringSetting;
 
 public class BoatClassNotificationPreference extends AbstractGenericSerializableSettings {
     private static final long serialVersionUID = -6510535362114348707L;
     
-    private transient StringSetting boatClassName;
+    private transient BoatClassSetting boatClass;
     private transient BooleanSetting notifyAboutUpcomingRaces;
     private transient BooleanSetting notifyAboutResults;
     
     public BoatClassNotificationPreference() {
     }
     
-    public BoatClassNotificationPreference(String boatClassName, boolean notifyAboutRaces, boolean notifyAboutResults) {
-        this.boatClassName.setValue(boatClassName);
+    public BoatClassNotificationPreference(BoatClassMasterdata boatClass, boolean notifyAboutRaces, boolean notifyAboutResults) {
+        this.boatClass.setValue(boatClass);
         this.notifyAboutUpcomingRaces.setValue(notifyAboutRaces);
         this.notifyAboutResults.setValue(notifyAboutResults);
     }
     
     @Override
     protected void addChildSettings() {
-        boatClassName = new StringSetting("boatClassName", this);
+        boatClass = new BoatClassSetting("boatClass", this);
         notifyAboutUpcomingRaces = new BooleanSetting("notifyAboutUpcomingRaces", this, false);
         notifyAboutResults = new BooleanSetting("notifyAboutResults", this, false);
     }
     
-    public String getBoatClassName() {
-        return boatClassName.getValue();
+    public BoatClassMasterdata getBoatClass() {
+        return boatClass.getValue();
     }
     
     public boolean isNotifyAboutUpcomingRaces() {
