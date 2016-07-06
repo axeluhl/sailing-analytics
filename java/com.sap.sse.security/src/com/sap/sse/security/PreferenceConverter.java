e5d38e7455bc0f4c8a394f88415d47c3c7b5b4ea
Steffen Schaefer <steffen.schaefer@sap.com>
2016-07-06 09:16:08 +0200

    Added JavaDocs to PreferenceConverter

--- java/com.sap.sse.security/src/com/sap/sse/security/PreferenceConverter.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/PreferenceConverter.java
@@ -1,5 +1,11 @@
 package com.sap.sse.security;
 
+/**
+ * Converter to be used by {@link UserStore} to convert preference objects to/from Strings.
+ * @see UserStore#registerPreferenceConverter(String, PreferenceConverter)
+ *
+ * @param <PREF> The type of preference object, this converter handles.
+ */
 public interface PreferenceConverter<PREF> {
     String toString(PREF preference);
 
