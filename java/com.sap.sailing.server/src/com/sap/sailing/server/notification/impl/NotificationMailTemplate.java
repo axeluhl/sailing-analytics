aade87ce946f32a76e9dea18d55104077ae21539
Papick Garcia Taboada <papick.g.taboada@sap.com>
2016-07-19 17:55:36 +0200

    Added sapsailing site look and feel to html mail

--- java/com.sap.sailing.server/src/com/sap/sailing/server/notification/impl/NotificationMailTemplate.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/notification/impl/NotificationMailTemplate.java
@@ -1,5 +1,6 @@
 package com.sap.sailing.server.notification.impl;
 
+import java.net.URL;
 import java.util.Arrays;
 
 import com.sap.sse.common.Util.Pair;
@@ -9,19 +10,27 @@ public class NotificationMailTemplate {
     private final String subject;
     private final String title;
     private final String text;
+    private final String serverBaseUrl;
     private final Iterable<Pair<String, String>> labelsAndLinkUrls;
     
     @SafeVarargs
-    public NotificationMailTemplate(String subject, String text, Pair<String, String>... labelsAndLinkUrls) {
-        this(subject, null, text, labelsAndLinkUrls);
+    public NotificationMailTemplate(String subject, String text, URL serverBaseUrl,
+            Pair<String, String>... labelsAndLinkUrls) {
+        this(subject, null, text, serverBaseUrl, labelsAndLinkUrls);
     }
 
     @SafeVarargs
-    public NotificationMailTemplate(String subject, String title, String text, Pair<String, String>... labelsAndLinkUrls) {
+    public NotificationMailTemplate(String subject, String title, String text, URL serverBaseUrl,
+            Pair<String, String>... labelsAndLinkUrls) {
         this.subject = subject;
         this.title = title;
         this.text = text;
+        this.serverBaseUrl = serverBaseUrl.toString();
         this.labelsAndLinkUrls = Arrays.asList(labelsAndLinkUrls);
+    }
+
+    public String getServerBaseUrl() {
+        return serverBaseUrl;
     }
 
     public String getSubject() {
