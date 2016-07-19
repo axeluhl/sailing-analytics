39473822514a58dae899466541bd94d0d1f1789c
Peter Siegmund <siegmund@ubilabs.net>
2016-07-19 08:40:10 +0200

    fixed "No QR Code" alertDialog (positivButton was invisible)

--- mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/activities/AbstractBaseActivity.java
+++ mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/activities/AbstractBaseActivity.java
@@ -1,11 +1,11 @@
 package com.sap.sailing.android.shared.ui.activities;
 
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 
 import com.sap.sailing.android.shared.logging.ExLog;
 
@@ -54,18 +54,17 @@ public abstract class AbstractBaseActivity extends SendingServiceAwareActivity {
     }
 
     public void showErrorPopup(String title, String message) {
-        AlertDialog dialog = new AlertDialog.Builder(this).setTitle(title).setMessage(message)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+        new AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
                         dialog.dismiss();
                     }
-                }).create();
-
-        dialog.show();
+            }).show();
     }
 
-    public void showErrorPopup(int string1Id, int string2Id) {
-        showErrorPopup(getString(string1Id), getString(string2Id));
+    public void showErrorPopup(int title, int message) {
+        showErrorPopup(getString(title), getString(message));
     }
 }
