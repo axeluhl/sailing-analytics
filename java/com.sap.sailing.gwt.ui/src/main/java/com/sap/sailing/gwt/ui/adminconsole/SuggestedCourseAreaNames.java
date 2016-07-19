2d30f1941563098628afa71d7bcb0199b51f05e7
Axel Uhl <axel.uhl@sap.com>
2016-07-20 01:12:06 +0200

    added "Juliet" in addition to "Juliett" as possible course area name
    Change-Id: Ice3d56a6f1f63718d31e389be68707d95884be59

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/SuggestedCourseAreaNames.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/SuggestedCourseAreaNames.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public interface SuggestedCourseAreaNames {
     static final List<String> suggestedCourseAreaNames = Arrays.asList("Alpha", "Alfa", "Bravo", "Charlie", "Delta", "Echo",
-            "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Stadium", "Offshore", "TV", "Media");
+            "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Juliet", "Kilo", "Stadium", "Offshore", "TV", "Media");
 
 }
