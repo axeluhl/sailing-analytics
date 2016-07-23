bcb521cac066d9e8c9f65e895e0acc9c8274f8a7
Axel Uhl <axel.uhl@sap.com>
2016-07-23 16:14:51 +0200

    fixing bug 3813 by letting the UpdateTrackedRaceStatus operation use setStatus immediately instead of the onStatusChanged
    Change-Id: I4b6a05f483908ce0c1f86c1ca329e026e76872aa

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/mock/MockedTrackedRace.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/mock/MockedTrackedRace.java
@@ -812,6 +812,10 @@ public class MockedTrackedRace implements DynamicTrackedRace {
     }
 
     @Override
+    public void setStatus(TrackedRaceStatus newStatus) {
+    }
+
+    @Override
     public void onStatusChanged(TrackingDataLoader loader, TrackedRaceStatus status) {
     }
 
