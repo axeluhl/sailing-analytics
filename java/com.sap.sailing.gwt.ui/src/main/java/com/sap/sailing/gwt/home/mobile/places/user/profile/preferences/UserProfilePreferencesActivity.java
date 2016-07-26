533945c22213b214bff7a0b730c6773cc7efb458
Benjamin Barth <benjamin.barth@sap.com>
2016-07-26 14:49:09 +0200

    Added abstraction for mobile user profile activities

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/user/profile/preferences/UserProfilePreferencesActivity.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/places/user/profile/preferences/UserProfilePreferencesActivity.java
@@ -1,26 +1,23 @@
 package com.sap.sailing.gwt.home.mobile.places.user.profile.preferences;
 
-import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.sap.sailing.gwt.home.mobile.app.MobileApplicationClientFactory;
-import com.sap.sailing.gwt.home.shared.app.PlaceNavigation;
-import com.sap.sailing.gwt.home.shared.places.user.profile.AbstractUserProfilePlace;
+import com.sap.sailing.gwt.home.mobile.places.user.profile.AbstractUserProfileActivity;
 import com.sap.sailing.gwt.home.shared.places.user.profile.preferences.UserPreferencesPresenter;
 import com.sap.sailing.gwt.home.shared.places.user.profile.preferences.UserPreferencesView;
 import com.sap.sailing.gwt.home.shared.places.user.profile.preferences.UserPreferencesView.Presenter;
 import com.sap.sailing.gwt.home.shared.places.user.profile.preferences.UserProfilePreferencesPlace;
 import com.sap.sse.security.ui.authentication.AuthenticationContextEvent;
 
-public class UserProfilePreferencesActivity extends AbstractActivity implements UserProfilePreferencesView.Presenter {
+public class UserProfilePreferencesActivity extends AbstractUserProfileActivity implements UserProfilePreferencesView.Presenter {
 
-    private final MobileApplicationClientFactory clientFactory;
     private final UserPreferencesView.Presenter userPreferencesPresenter;
     private final UserProfilePreferencesView currentView;
     
     public UserProfilePreferencesActivity(UserProfilePreferencesPlace place,
             MobileApplicationClientFactory clientFactory) {
-        this.clientFactory = clientFactory;
+        super(clientFactory);
         this.userPreferencesPresenter = new UserPreferencesPresenter<MobileApplicationClientFactory>(clientFactory);
         this.currentView = new UserProfilePreferencesViewImpl(this);
     }
@@ -45,18 +42,4 @@ public class UserProfilePreferencesActivity extends AbstractActivity implements 
         return userPreferencesPresenter;
     }
     
-    @Override
-    public void doTriggerLoginForm() {
-        clientFactory.getNavigator().getSignInNavigation().goToPlace();
-    }
-    
-    @Override
-    public PlaceNavigation<? extends AbstractUserProfilePlace> getUserProfileNavigation() {
-        return clientFactory.getNavigator().getUserProfileNavigation();
-    }
-    
-    @Override
-    public PlaceNavigation<? extends AbstractUserProfilePlace> getUserPreferencesNavigation() {
-        return clientFactory.getNavigator().getUserPreferencesNavigation();
-    }
 }
