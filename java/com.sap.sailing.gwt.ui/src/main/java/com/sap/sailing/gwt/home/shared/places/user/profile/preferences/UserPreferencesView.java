9b754d7bea3c471ad1310827567a4c8634f4977c
Benjamin Barth <benjamin.barth@sap.com>
2016-07-26 11:34:42 +0200

    Added reusable UserPreferencesView to avoid duplicated code in desktop and mobile versions

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/user/profile/preferences/UserPreferencesView.java
@@ -0,0 +1,16 @@
+package com.sap.sailing.gwt.home.shared.places.user.profile.preferences;
+
+import com.google.gwt.user.client.ui.IsWidget;
+import com.sap.sailing.gwt.home.shared.partials.multiselection.SuggestedMultiSelectionBoatClassDataProvider;
+import com.sap.sailing.gwt.home.shared.partials.multiselection.SuggestedMultiSelectionCompetitorDataProvider;
+
+public interface UserPreferencesView extends IsWidget {
+    
+    public void setEdgeToEdge(boolean edgeToEdge);
+    
+    public interface Presenter {
+        SuggestedMultiSelectionBoatClassDataProvider getFavoriteBoatClassesDataProvider();
+        SuggestedMultiSelectionCompetitorDataProvider getFavoriteCompetitorsDataProvider();
+    }
+
+}
