31be537930f174b99ab7d133f4f87854bd456438
Steffen Schaefer <steffen.schaefer@sap.com>
2016-08-09 14:28:23 +0200

    Added some JavaDocs to UrlBuilderUtil

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/settings/UrlBuilderUtil.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/settings/UrlBuilderUtil.java
@@ -7,10 +7,23 @@ import java.util.logging.Logger;
 import com.google.gwt.http.client.UrlBuilder;
 import com.google.gwt.user.client.Window;
 
+/**
+ * Helps to construct pre-parameterized {@link UrlBuilder} instances based on the current location/url.
+ * The created {@link UrlBuilder} instances are constructed in a way so that "locale" and "gwt.codesvr" parameters are preserved,
+ * but all other parameters as well as the fragment/hash part are being erased from the resulting UrlBuilder.
+ */
 public class UrlBuilderUtil {
     
     private static final Logger log = Logger.getLogger(UrlBuilderUtil.class.getName());
 
+    /**
+     * Creates an {@link UrlBuilder} based on the current location but with clean parameters and fragment/hash.
+     * <p>
+     * 
+     * Example:
+     * Current location: http://www.sapsailing.com/gwt/Home.html?locale=de&some_param=some_value#abc123
+     * Returned UrlBuilder: http://www.sapsailing.com/gwt/Home.html?locale=de
+     */
     public static UrlBuilder createUrlBuilderFromCurrentLocationWithCleanParameters() {
         final UrlBuilder urlBuilder = Window.Location.createUrlBuilder();
         for (String parameterName : Window.Location.getParameterMap().keySet()) {
@@ -22,6 +35,24 @@ public class UrlBuilderUtil {
         return urlBuilder;
     }
 
+    /**
+     * Creates an {@link UrlBuilder} based on the current location but with clean parameters and fragment/hash.
+     * In addition the base URL and path are being set specifically.
+     * <p>
+     * 
+     * Example1:
+     * Current location: http://www.sapsailing.com/gwt/Home.html?locale=de&some_param=some_value#abc123
+     * baseUrl: http://kielerwoche2016.sapsailing.com
+     * path: /gwt/Leaderboard.html
+     * Returned UrlBuilder: http://kielerwoche2016.sapsailing.com/gwt/Leaderboard.html?locale=de
+     * <p>
+     * 
+     * Example2:
+     * Current location: http://www.sapsailing.com/gwt/AdminConsole.html?gwt.codesvr=127.0.0.1:9997&some_param=some_value#abc123
+     * baseUrl: https://ess2017.sapsailing.com
+     * path: /gwt/Home.html#/some/place/:key=value
+     * Returned UrlBuilder: https://ess2017.sapsailing.com/gwt/Home.html?gwt.codesvr=127.0.0.1:9997#/some/place/:key=value
+     */
     public static UrlBuilder createUrlBuilderFromBaseURLAndPathWithCleanParameters(String baseUrl, String path) {
         final int colonIndex = baseUrl.indexOf(':');
         final String protocol = colonIndex >= 0 ? baseUrl.substring(0, colonIndex) : "http";
@@ -62,6 +93,16 @@ public class UrlBuilderUtil {
         return urlBuilder;
     }
 
+    /**
+     * Creates an {@link UrlBuilder} based on the current location but with clean parameters and fragment/hash.
+     * In addition, the path and hash parts can specifically being set.
+     * <p>
+     * 
+     * Example:
+     * Current location: http://www.sapsailing.com/gwt/Home.html?locale=de&some_param=some_value#abc123
+     * pathAndHash: /gwt/SomePage.html#/some/place/:key=value
+     * Returned UrlBuilder: http://www.sapsailing.com/gwt/SomePage.html?locale=de#/some/place/:key=value
+     */
     public static UrlBuilder createUrlBuilderFromCurrentLocationWithCleanParametersAndPath(String pathAndHash) {
         final UrlBuilder urlBuilder = createUrlBuilderFromCurrentLocationWithCleanParameters();
         String[] documentAndFragment = pathAndHash.split("#", 2);
@@ -72,6 +113,17 @@ public class UrlBuilderUtil {
         return urlBuilder;
     }
 
+    /**
+     * Creates an {@link UrlBuilder} based on the current location but with clean parameters and fragment/hash.
+     * In addition, the path and hash parts as well as URL parameters can specifically being set.
+     * <p>
+     * 
+     * Example:
+     * Current location: http://www.sapsailing.com/gwt/Home.html?locale=de&some_param=some_value#abc123
+     * path: /gwt/SomePage.html
+     * parameters: {p1: [abc], p2: [xyz, 123]}
+     * Returned UrlBuilder: http://www.sapsailing.com/gwt/SomePage.html?locale=de&p1=abc&p2=xyz&p2=123
+     */
     public static UrlBuilder createUrlBuilderWithPathAndParameters(String path, Map<String, String> parameters) {
         final UrlBuilder urlBuilder = createUrlBuilderFromCurrentLocationWithCleanParametersAndPath(path);
         for (Map.Entry<String, String> entry : parameters.entrySet()) {
