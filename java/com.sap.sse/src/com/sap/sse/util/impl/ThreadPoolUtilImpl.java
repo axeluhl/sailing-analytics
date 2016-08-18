541ac164f05a3a972307119c079907df4f20dc70
EC2 Server <axel.uhl@sap.com>
2016-08-18 15:06:41 +0000

    increased the thread pool sizes again after it turned out that we only get ~50% CPU utilization

--- java/com.sap.sse/src/com/sap/sse/util/impl/ThreadPoolUtilImpl.java
+++ java/com.sap.sse/src/com/sap/sse/util/impl/ThreadPoolUtilImpl.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import com.sap.sse.util.ThreadPoolUtil;
 
 public class ThreadPoolUtilImpl implements ThreadPoolUtil {
-    private static final int REASONABLE_THREAD_POOL_SIZE = Math.max(Runtime.getRuntime().availableProcessors()/2, 3);
+    private static final int REASONABLE_THREAD_POOL_SIZE = Math.max(Runtime.getRuntime().availableProcessors()-1, 3);
 
     private final ScheduledExecutorService defaultBackgroundTaskThreadPoolExecutor;
     private final ScheduledExecutorService defaultForegroundTaskThreadPoolExecutor;
