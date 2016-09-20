3f5006e5285aca8fbebffb30b667e5ba9d596fe5
Axel Uhl <axel.uhl@sap.com>
2016-09-20 16:55:38 +0200

    single point of exit; formatting
    Change-Id: I40c5169d0864a781c68d7d6fe3c545a652d13a15

--- mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/ui/fragments/AbstractHomeFragment.java
+++ mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/ui/fragments/AbstractHomeFragment.java
@@ -54,18 +54,18 @@ public abstract class AbstractHomeFragment extends BaseFragment {
     }
 
     private boolean requestQRCodeScan() {
+        boolean result = true;
         try {
             Intent intent = new Intent("com.google.zxing.client.android.SCAN");
             intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
             startActivityForResult(intent, requestCodeQRCode);
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
             Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
             startActivity(marketIntent);
-            return false;
+            result = false;
         }
-        return true;
+        return result;
     }
 
     @Override
