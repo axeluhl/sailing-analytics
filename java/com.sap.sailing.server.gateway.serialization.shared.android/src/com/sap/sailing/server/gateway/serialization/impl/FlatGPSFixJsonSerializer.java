3dd274b98111a5a7e2435fa7ef2426a05a88364f
Axel Uhl <axel.uhl@sap.com>
2016-10-13 10:43:21 +0200

    using NOT_AVAILABLE_THROUGH_SERVER constant in FlatGPSFixJsonSerializer
    Change-Id: I297db7cf7fa60bae1d795ab5491bdfdf4be908ed

--- java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/serialization/impl/FlatGPSFixJsonSerializer.java
+++ java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/serialization/impl/FlatGPSFixJsonSerializer.java
@@ -18,7 +18,7 @@ public class FlatGPSFixJsonSerializer implements JsonSerializer<GPSFix> {
         result.put(FIELD_LON_DEG, object.getPosition().getLngDeg());
         result.put(FIELD_LAT_DEG, object.getPosition().getLatDeg());
         result.put(FIELD_TIME_MILLIS, object.getTimePoint().asMillis());
-        result.put(FIELD_ACCURACY, -1);
+        result.put(FIELD_ACCURACY, NOT_AVAILABLE_THROUGH_SERVER);
         return result;
     }
 }
