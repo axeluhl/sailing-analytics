3f4dc5103e4b57061e1e99e934d377bd59f3dd1f
Axel Uhl <axel.uhl@sap.com>
2016-10-14 08:45:28 +0200

    added Javadoc
    Change-Id: I3e905a0dd0e572cb74962d6442a0b298f638ce62

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/racelist/DefaultRaceListColumnComparator.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/racelist/DefaultRaceListColumnComparator.java
@@ -3,7 +3,10 @@ package com.sap.sailing.gwt.home.desktop.partials.racelist;
 import com.sap.sailing.gwt.home.communication.race.RaceMetadataDTO;
 
 public class DefaultRaceListColumnComparator<T extends RaceMetadataDTO<?>> extends RaceListColumnComparator<T, String> {
-
+    /**
+     * Returns {@code null} for all objects passed, forcing the {@link #compare(RaceMetadataDTO, RaceMetadataDTO)} method
+     * to resort to {@link RaceMetadataDTO#getNaturalOrder()} for comparison.
+     */
     @Override
     public String getValue(T object) {
         return null;
