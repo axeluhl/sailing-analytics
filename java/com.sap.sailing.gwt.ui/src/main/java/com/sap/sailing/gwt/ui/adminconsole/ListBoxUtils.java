3ee385c1fa3e71143c9ae8ae5b33ee3142a7bfa6
fmittag <fmittag@gmx.net>
2016-12-02 12:21:07 +0100

    Little improvement for listbox with start mode flags for SWC procedure

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/ListBoxUtils.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/ListBoxUtils.java
@@ -68,7 +68,12 @@ public final class ListBoxUtils {
         setupFlagsListBox(box, selectedFlags, Flags.validValues());
     }
 
-    public static void setupStartmodeFlagsListBox(ListBox box, List<Flags> selectedFlags) {
+    public static void setupRRS26StartmodeFlagsListBox(ListBox box, List<Flags> selectedFlags) {
         setupFlagsListBox(box, selectedFlags, Flags.PAPA, Flags.BLACK, Flags.INDIA, Flags.INDIA_ZULU, Flags.UNIFORM);            
     }
+    
+    public static void setupSWCStartmodeFlagsListBox(ListBox box, List<Flags> selectedFlags) {
+        setupFlagsListBox(box, selectedFlags, Flags.BLACK, Flags.UNIFORM);            
+    }
+
 }
