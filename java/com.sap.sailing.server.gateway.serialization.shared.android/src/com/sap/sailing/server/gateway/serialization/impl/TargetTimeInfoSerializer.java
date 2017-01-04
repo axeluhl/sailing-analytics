51cea7a46d80554d80ef506784d4c7648c00b937
Axel Uhl <axel.uhl@sap.com>
2017-01-04 10:57:08 +0100

    added redundant but useful output to TargetTime JSON format (total duration and leg angle to wind)
    Change-Id: I69ce77ce365e27e350e164216f7e38cf2dc6153f

--- java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/serialization/impl/TargetTimeInfoSerializer.java
+++ java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/serialization/impl/TargetTimeInfoSerializer.java
@@ -11,10 +11,12 @@ import com.sap.sailing.server.gateway.serialization.JsonSerializer;
 public class TargetTimeInfoSerializer implements JsonSerializer<TargetTimeInfo> {
     
     public static final String LEGS = "legs";
+    public static final String DURATION_MILLIS = "durationMillis";
     public static final String LEG_START_TIME_MILLIS = "legStartMillis";
     public static final String LEG_DURATION_MILLIS = "legDurationMillis";
     public static final String LEG_DISTANCE_METERS = "legDistanceMeters";
     public static final String LEG_BEARING_DEGREES = "legBearingDegrees";
+    public static final String LEG_TRUE_WIND_ANGLE_TO_LEG_DEGREES = "legTrueWindAngleToLegDegrees";
     public static final String LEG_WIND = "legWind";
     public static final String LEG_TYPE = "legType";
     
@@ -29,6 +31,8 @@ public class TargetTimeInfoSerializer implements JsonSerializer<TargetTimeInfo> 
     public JSONObject serialize(TargetTimeInfo object) {
         final JSONArray legsAsJson = new JSONArray();
         final JSONObject result = new JSONObject();
+        // redundant, but possibly useful to clients other than the deserializer:
+        result.put(DURATION_MILLIS, object.getExpectedDuration().asMillis());
         result.put(LEGS, legsAsJson);
         for (final LegTargetTimeInfo legInfo : object.getLegTargetTimes()) {
             final JSONObject legAsJson = new JSONObject();
@@ -36,8 +40,11 @@ public class TargetTimeInfoSerializer implements JsonSerializer<TargetTimeInfo> 
             legAsJson.put(LEG_DURATION_MILLIS, legInfo.getExpectedDuration().asMillis());
             legAsJson.put(LEG_DISTANCE_METERS, legInfo.getDistance().getMeters());
             legAsJson.put(LEG_BEARING_DEGREES, legInfo.getLegBearing().getDegrees());
+            // redundant, but possibly useful to clients other than the deserializer:
+            legAsJson.put(LEG_TRUE_WIND_ANGLE_TO_LEG_DEGREES, legInfo.getTrueWindAngleToLeg().getDegrees());
             legAsJson.put(LEG_TYPE, legInfo.getLegType().name());
             legAsJson.put(LEG_WIND, windSerializer.serialize(legInfo.getWind()));
+            legsAsJson.add(legAsJson);
         }
         return result;
     }
