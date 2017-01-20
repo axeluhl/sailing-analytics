4af582421bad712df046869339634c81e66eb8b4
Axel Uhl <axel.uhl@sap.com>
2017-01-20 10:35:14 +0100

    added a few more GRIB sample files and some documentation
    Change-Id: I6b3d869e99ba40f0fbb39118c07bf2eb1f8896a5

--- java/com.sap.sailing.grib.test/src/com/sap/sailing/grib/test/SimpleGridFileReadingTest.java
+++ java/com.sap.sailing.grib.test/src/com/sap/sailing/grib/test/SimpleGridFileReadingTest.java
@@ -8,8 +8,12 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import ucar.ma2.Array;
+import com.sap.sailing.domain.common.Position;
+import com.sap.sailing.domain.common.impl.DegreePosition;
+
+import ucar.ma2.ArrayFloat.D2;
 import ucar.nc2.VariableSimpleIF;
+import ucar.nc2.dt.GridCoordSystem;
 import ucar.nc2.dt.GridDatatype;
 import ucar.nc2.dt.grid.GeoGrid;
 import ucar.nc2.dt.grid.GridDataset;
@@ -20,31 +24,58 @@ import ucar.unidata.geoloc.LatLonRect;
 public class SimpleGridFileReadingTest {
     private GridDataset dataSet;
 
-    @Before
-    public void setUp() throws IOException {
-        dataSet = GridDataset.open("resources/Drake.wind.grb");
-    }
-    
     @Test
-    public void testBoundingBox() {
+    public void testBoundingBox() throws IOException {
+        dataSet = GridDataset.open("resources/Drake.wind.grb");
         final LatLonRect boundingBox = dataSet.getBoundingBox();
         System.out.println(boundingBox);
     }
 
     @Test
     public void testWindContent() throws IOException {
+        dataSet = GridDataset.open("resources/Drake.wind.grb");
         final List<GridDatatype> grids = dataSet.getGrids();
         for (GridDatatype grid : grids) {
             assertTrue(grid instanceof GeoGrid);
         }
         GeoGrid grid0 = (GeoGrid) grids.get(0);
-        final Array volumeDataAtTime0 = grid0.readVolumeData(0);
+        final D2 volumeDataAtTime0 = (D2) grid0.readVolumeData(0);
         final List<VariableSimpleIF> dataVariables = dataSet.getDataVariables();
+        final GridCoordSystem coordinateSystem = grid0.getCoordinateSystem();
+        final LatLonRect latLngBoundingBox = coordinateSystem.getLatLonBoundingBox();
+        final Position sw = new DegreePosition(latLngBoundingBox.getLowerLeftPoint().getLatitude(), latLngBoundingBox.getLowerLeftPoint().getLongitude());
+        final Position ne = new DegreePosition(latLngBoundingBox.getUpperRightPoint().getLatitude(), latLngBoundingBox.getUpperRightPoint().getLongitude());
+        final Position middle = sw.translateGreatCircle(sw.getBearingGreatCircle(ne), sw.getDistance(ne).scale(0.5));
+        final int[] coordinateIndices = coordinateSystem.findXYindexFromLatLon(middle.getLatDeg(), middle.getLngDeg(), new int[2]);
+        final float dataAtMiddle = volumeDataAtTime0.get(coordinateIndices[0], coordinateIndices[1]);
         System.out.println(dataVariables);
+        System.out.println(dataAtMiddle);
     }
     
     @Test
-    public void testDateRange() {
+    public void testWindContent2() throws IOException {
+        dataSet = GridDataset.open("resources/wind-Atlantic.24hr.grb.bz2");
+        final List<GridDatatype> grids = dataSet.getGrids();
+        for (GridDatatype grid : grids) {
+            assertTrue(grid instanceof GeoGrid);
+        }
+        GeoGrid grid0 = (GeoGrid) grids.get(0);
+        final D2 volumeDataAtTime0 = (D2) grid0.readVolumeData(0);
+        final List<VariableSimpleIF> dataVariables = dataSet.getDataVariables();
+        final GridCoordSystem coordinateSystem = grid0.getCoordinateSystem();
+        final LatLonRect latLngBoundingBox = coordinateSystem.getLatLonBoundingBox();
+        final Position sw = new DegreePosition(latLngBoundingBox.getLowerLeftPoint().getLatitude(), latLngBoundingBox.getLowerLeftPoint().getLongitude());
+        final Position ne = new DegreePosition(latLngBoundingBox.getUpperRightPoint().getLatitude(), latLngBoundingBox.getUpperRightPoint().getLongitude());
+        final Position middle = sw.translateGreatCircle(sw.getBearingGreatCircle(ne), sw.getDistance(ne).scale(0.5));
+        final int[] coordinateIndices = coordinateSystem.findXYindexFromLatLon(middle.getLatDeg(), middle.getLngDeg(), new int[2]);
+        final float dataAtMiddle = volumeDataAtTime0.get(coordinateIndices[0], coordinateIndices[1]);
+        System.out.println(dataVariables);
+        System.out.println(dataAtMiddle);
+    }
+    
+    @Test
+    public void testDateRange() throws IOException {
+        dataSet = GridDataset.open("resources/Drake.wind.grb");
         final CalendarDateRange dateRange = dataSet.getCalendarDateRange();
         final CalendarDate start = dateRange.getStart();
         final CalendarDate end = dateRange.getEnd();
