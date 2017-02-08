9f34d655be79b6d54168c6433ef67c94300ee8b6
Axel Uhl <axel.uhl@sap.com>
2017-02-08 17:46:13 +0100

    added @Statistic annotations on BravoFix
    Change-Id: I9523a1236dd47fdc348f840cbb04c968993cd7e1

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/tracking/BravoFix.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/tracking/BravoFix.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.domain.common.tracking;
 
 import com.sap.sailing.domain.common.Distance;
+import com.sap.sse.datamining.annotations.Statistic;
 
 /**
  * Specific {@link SensorFix} for bravo devices (http://www.bravosystems.es/).
@@ -14,15 +15,21 @@ public interface BravoFix extends SensorFix {
      * 
      * @return the ride height
      */
+    @Statistic(messageKey="rideHeight", resultDecimals=1)
     Distance getRideHeight();
 
+    @Statistic(messageKey="rideHeightPort", resultDecimals=1)
     Distance getRideHeightPortHull();
 
+    @Statistic(messageKey="rideHeightStarboard", resultDecimals=1)
     Distance getRideHeightStarboardHull();
 
+    @Statistic(messageKey="pitch", resultDecimals=1)
     double getPitch();
 
+    @Statistic(messageKey="yaw", resultDecimals=1)
     double getYaw();
 
+    @Statistic(messageKey="roll", resultDecimals=1)
     double getRoll();
 }
