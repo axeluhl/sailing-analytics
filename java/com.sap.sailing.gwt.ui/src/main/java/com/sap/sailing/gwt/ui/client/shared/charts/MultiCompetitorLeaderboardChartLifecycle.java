8ad604df99f7345949c52e256ba5b4d1194d9bd1
Kai Boernert <kai.boernert@oio.de>
2017-02-14 10:53:54 +0100

    added lifecycle for multileaderboardchart

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/MultiCompetitorLeaderboardChartLifecycle.java
@@ -0,0 +1,51 @@
+package com.sap.sailing.gwt.ui.client.shared.charts;
+
+import com.sap.sailing.gwt.ui.client.StringMessages;
+import com.sap.sse.gwt.client.shared.components.ComponentLifecycle;
+
+public class MultiCompetitorLeaderboardChartLifecycle implements
+        ComponentLifecycle<MultiCompetitorLeaderboardChartSettings, MultiCompetitorLeaderboardChartSettingsDialogComponent> {
+    
+    public static final String ID = "MultiCompetitorLeaderboardChart";
+
+    public MultiCompetitorLeaderboardChartLifecycle() {
+    }
+
+    @Override
+    public MultiCompetitorLeaderboardChartSettingsDialogComponent getSettingsDialogComponent(
+            MultiCompetitorLeaderboardChartSettings settings) {
+        return new MultiCompetitorLeaderboardChartSettingsDialogComponent(settings);
+    }
+
+    @Override
+    public MultiCompetitorLeaderboardChartSettings createDefaultSettings() {
+        return new MultiCompetitorLeaderboardChartSettings(null);
+    }
+
+    @Override
+    public String getLocalizedShortName() {
+        return StringMessages.INSTANCE.competitorCharts();
+    }
+
+    @Override
+    public String getComponentId() {
+        return ID;
+    }
+
+    @Override
+    public boolean hasSettings() {
+        return true;
+    }
+
+    @Override
+    public MultiCompetitorLeaderboardChartSettings extractGlobalSettings(
+            MultiCompetitorLeaderboardChartSettings settings) {
+        return settings;
+    }
+
+    @Override
+    public MultiCompetitorLeaderboardChartSettings extractContextSettings(
+            MultiCompetitorLeaderboardChartSettings settings) {
+        return createDefaultSettings();
+    }
+}
