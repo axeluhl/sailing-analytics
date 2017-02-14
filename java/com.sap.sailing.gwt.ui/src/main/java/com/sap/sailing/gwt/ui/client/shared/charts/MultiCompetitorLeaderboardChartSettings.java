222f4c3b69de7cfa76f70522c2985e076a365ae9
Kai Boernert <kai.boernert@oio.de>
2017-02-14 11:12:52 +0100

    competitor chart settings element adjusted

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/MultiCompetitorLeaderboardChartSettings.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/MultiCompetitorLeaderboardChartSettings.java
@@ -1,16 +1,30 @@
 package com.sap.sailing.gwt.ui.client.shared.charts;
 
 import com.sap.sailing.domain.common.DetailType;
-import com.sap.sse.common.settings.AbstractSettings;
+import com.sap.sse.common.settings.generic.AbstractGenericSerializableSettings;
+import com.sap.sse.common.settings.generic.EnumSetting;
+import com.sap.sse.common.settings.generic.StringToEnumConverter;
 
-public class MultiCompetitorLeaderboardChartSettings extends AbstractSettings {
-    private final DetailType detailType;
+public class MultiCompetitorLeaderboardChartSettings extends AbstractGenericSerializableSettings {
+    private static final long serialVersionUID = 2456555424331709738L;
+
+    private EnumSetting<DetailType> detailType;
 
     public MultiCompetitorLeaderboardChartSettings(DetailType detailType) {
-        this.detailType = detailType;
+        this.detailType.setValue(detailType);
+    }
+
+    @Override
+    protected void addChildSettings() {
+        detailType = new EnumSetting<>("detailType", this, new StringToEnumConverter<DetailType>() {
+            @Override
+            public DetailType fromString(String stringValue) {
+                return DetailType.valueOf(stringValue);
+            }
+        });
     }
 
     public DetailType getDetailType() {
-        return detailType;
+        return detailType.getValue();
     }
 }
