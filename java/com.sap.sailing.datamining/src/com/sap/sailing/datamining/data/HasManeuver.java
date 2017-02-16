2a22240137f73a6fcb049e3378a5bc89d95bd2fa
Lennart Hensler <LennartHensler@aol.com>
2017-02-16 16:05:01 +0100

    Cleaned the sailing data mining domain

--- java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/data/HasManeuver.java
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/data/HasManeuver.java
@@ -5,8 +5,11 @@ import com.sap.sailing.domain.common.Positioned;
 import com.sap.sailing.domain.tracking.Maneuver;
 import com.sap.sse.common.TimePoint;
 import com.sap.sse.common.Timed;
+import com.sap.sse.datamining.annotations.Connector;
 
 public interface HasManeuver extends Positioned, Timed {
+    
+    @Connector
     Maneuver getManeuver();
 
     @Override
