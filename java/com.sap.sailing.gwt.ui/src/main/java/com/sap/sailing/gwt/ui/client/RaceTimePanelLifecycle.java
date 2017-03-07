d8b8927e800be20ca00dbe22364e70e0a54439db
Vladislav Chumak <vladislav.chumak@googlemail.com>
2017-03-07 18:25:43 +0100

    Revert "bug3529 Removed storable settings support from RaceTimePanel"
    This reverts commit 18dcc7258701a8dcb97049cf6011d45458129f57.

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/RaceTimePanelLifecycle.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/RaceTimePanelLifecycle.java
@@ -19,7 +19,7 @@ public class RaceTimePanelLifecycle implements
 
     @Override
     public RaceTimePanelSettings createDefaultSettings() {
-        return new RaceTimePanelSettings(1000);
+        return new RaceTimePanelSettings();
     }
 
     @Override
@@ -39,7 +39,7 @@ public class RaceTimePanelLifecycle implements
 
     @Override
     public RaceTimePanelSettings extractGlobalSettings(RaceTimePanelSettings settings) {
-        return createDefaultSettings();
+        return settings;
     }
 
     @Override
