1c7c5ea9d5e1272c86787949272377081dce2dca
Axel Uhl <axel.uhl@sap.com>
2017-03-20 17:46:02 +0100

    Introduced null-safety for file storage service; useful during boot-up phase
    Change-Id: I0396d5055cee2ab316b365ad0219df713018bc9d

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
@@ -468,6 +468,7 @@ import com.sap.sse.common.impl.MillisecondsTimePoint;
 import com.sap.sse.common.impl.TimeRangeImpl;
 import com.sap.sse.common.mail.MailException;
 import com.sap.sse.common.media.MimeType;
+import com.sap.sse.filestorage.FileStorageManagementService;
 import com.sap.sse.filestorage.FileStorageService;
 import com.sap.sse.filestorage.InvalidPropertiesException;
 import com.sap.sse.gwt.client.ServerInfoDTO;
@@ -5993,9 +5994,12 @@ public class SailingServiceImpl extends ProxiedRemoteServiceServlet implements S
     @Override
     public FileStorageServiceDTO[] getAvailableFileStorageServices(String localeInfoName) {
         List<FileStorageServiceDTO> serviceDtos = new ArrayList<>();
-        for (FileStorageService s : getService().getFileStorageManagementService().getAvailableFileStorageServices()) {
-            serviceDtos.add(FileStorageServiceDTOUtils.convert(s, getLocale(localeInfoName)));
-        }
+        final FileStorageManagementService fileStorageManagementService = getService().getFileStorageManagementService();
+        if (fileStorageManagementService != null) {
+			for (FileStorageService s : fileStorageManagementService.getAvailableFileStorageServices()) {
+				serviceDtos.add(FileStorageServiceDTOUtils.convert(s, getLocale(localeInfoName)));
+			}
+		}
         return serviceDtos.toArray(new FileStorageServiceDTO[0]);
     }
 
