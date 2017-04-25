c8450801896144e344d8e0b7b5b3beaba8a8f08e
Kai Boernert <kai.boernert@oio.de>
2017-04-25 19:44:12 +0200

    added tokenizers

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/autoplay/client/app/classic/AutoPlayHistoryMapperDesktopImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/autoplay/client/app/classic/AutoPlayHistoryMapperDesktopImpl.java
@@ -2,9 +2,12 @@ package com.sap.sailing.gwt.autoplay.client.app.classic;
 
 import com.google.gwt.place.shared.PlaceHistoryMapper;
 import com.google.gwt.place.shared.WithTokenizers;
+import com.sap.sailing.gwt.autoplay.client.places.screens.idleloop.leaderboard.LeaderboardPlace;
+import com.sap.sailing.gwt.autoplay.client.places.screens.liveraceloop.raceboard.LiveRaceWithRaceboardPlace;
 import com.sap.sailing.gwt.autoplay.client.places.startclassic.old.PlayerPlace;
 import com.sap.sailing.gwt.autoplay.client.places.startup.classic.ClassicConfigPlace;
 
-@WithTokenizers({ ClassicConfigPlace.Tokenizer.class, PlayerPlace.Tokenizer.class })
+@WithTokenizers({ ClassicConfigPlace.Tokenizer.class, PlayerPlace.Tokenizer.class,
+        LiveRaceWithRaceboardPlace.Tokenizer.class, LeaderboardPlace.Tokenizer.class })
 public interface AutoPlayHistoryMapperDesktopImpl extends PlaceHistoryMapper {
 }
