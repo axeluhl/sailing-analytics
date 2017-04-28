b9b9fb1c795c50eb61f60cd0ec2b6472501c2551
Axel Uhl <axel.uhl@sap.com>
2017-04-28 10:21:31 +0200

    added ZIPped NMEA test data and added a first test case frame for it
    Change-Id: I0eb7148017232fc5d0b07be0de4531a227b400af

---
+++ java/com.sap.sailing.nmeaconnector.test/src/com/sap/sailing/nmeaconnector/test/NmeaFromZipImportTest.java
@@ -0,0 +1,34 @@
+package com.sap.sailing.nmeaconnector.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sap.sailing.domain.common.Wind;
+import com.sap.sailing.nmeaconnector.NmeaFactory;
+import com.sap.sse.common.Util;
+
+public class NmeaFromZipImportTest {
+    private ZipInputStream zipInputStream;
+
+    @Before
+    public void setUp() throws FileNotFoundException {
+        NmeaFactory.INSTANCE.getUtil().registerAdditionalParsers();
+        zipInputStream = new ZipInputStream(new FileInputStream("resources/LogSS.txt.zip"));
+    }
+    
+    @Test
+    public void testReadOneZipEntry() throws IOException, InterruptedException {
+        ZipEntry entry;
+        while ((entry=zipInputStream.getNextEntry()) != null && entry.getName().toLowerCase().endsWith(".txt"));
+        Iterable<Wind> windFixes = NmeaFactory.INSTANCE.readWind(zipInputStream);
+        assertTrue(!Util.isEmpty(windFixes));
+    }
+}
