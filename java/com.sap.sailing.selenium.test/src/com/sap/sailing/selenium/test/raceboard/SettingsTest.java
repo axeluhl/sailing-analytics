28982e93a37bb8fffdac802bd5a4cdb67efb0941
Vladislav Chumak <vladislav.chumak@googlemail.com>
2017-05-05 15:51:03 +0200

    bug4100 Removed START_ANALYSIS Mode testing code from raceboard selenium test

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/test/raceboard/SettingsTest.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/test/raceboard/SettingsTest.java
@@ -213,16 +213,17 @@ public class SettingsTest extends AbstractSeleniumTest {
         // verify that document settings are able to override custom user settings by a system default value
         Assert.assertFalse(mapSettings.isTransparentHoverlines());
 
-        // verify that custom document settings override mode settings of other modes
-        raceboard = RaceBoardPage.goToRaceboardUrl(getWebDriver(), getContextRoot(), BMW_CUP_REGATTA, BMW_CUP_REGATTA,
-                String.format(BMW_RACE, 1), "START_ANALYSIS");
+        // // verify that custom document settings override mode settings of other modes
+        // raceboard = RaceBoardPage.goToRaceboardUrl(getWebDriver(), getContextRoot(), BMW_CUP_REGATTA,
+        // BMW_CUP_REGATTA,
+        // String.format(BMW_RACE, 1), "START_ANALYSIS");
         // leaderboardSettingsDialog = raceboard.openLeaderboardSettingsDialog();
         // leaderboardSettingsPanelPO = leaderboardSettingsDialog.getLeaderboardSettingsPanelPO();
         // selectedDetails = leaderboardSettingsPanelPO.getSelectedDetails();
         // Assert.assertArrayEquals(detailsToSelect, selectedDetails);
-
-        mapSettings = raceboard.openMapSettings();
-        Assert.assertFalse(mapSettings.isWindUp());
+        //
+        // mapSettings = raceboard.openMapSettings();
+        // Assert.assertFalse(mapSettings.isWindUp());
     }
 
     private void initTrackingForBmwCupRace(AdminConsolePage adminConsole) {
