95d652a0b7c088baff9614fc951b3681b8f9d2f2
Axel Uhl <axel.uhl@sap.com>
2017-05-22 18:32:21 +0200

    also register the "inner" regatta leaderboard with the RacingEventService during test set-up
    Change-Id: I373ae50b0379a4989d815ef04778d0e23b667b8c

--- java/com.sap.sailing.server.test/src/com/sap/sailing/server/test/ApplyScoresFromRaceLogWithEliminationsTest.java
+++ java/com.sap.sailing.server.test/src/com/sap/sailing/server/test/ApplyScoresFromRaceLogWithEliminationsTest.java
@@ -1,13 +1,18 @@
 package com.sap.sailing.server.test;
 
+import java.security.Provider.Service;
+
 import com.sap.sailing.domain.base.Regatta;
+import com.sap.sailing.domain.leaderboard.RegattaLeaderboard;
 import com.sap.sailing.domain.leaderboard.RegattaLeaderboardWithEliminations;
 import com.sap.sailing.domain.leaderboard.impl.DelegatingRegattaLeaderboardWithCompetitorElimination;
 
 public class ApplyScoresFromRaceLogWithEliminationsTest extends ApplyScoresFromRaceLogTest {
     @Override
     protected RegattaLeaderboardWithEliminations createLeaderboard(Regatta regatta, int[] discardingThresholds) {
+        final RegattaLeaderboard regattaLeaderboard = super.createLeaderboard(regatta, discardingThresholds);
+        service.addLeaderboard(regattaLeaderboard);
         return new DelegatingRegattaLeaderboardWithCompetitorElimination(
-                ()->super.createLeaderboard(regatta, discardingThresholds), "Test leaderboard with elimination");
+                ()->regattaLeaderboard, "Test leaderboard with elimination");
     }
 }
