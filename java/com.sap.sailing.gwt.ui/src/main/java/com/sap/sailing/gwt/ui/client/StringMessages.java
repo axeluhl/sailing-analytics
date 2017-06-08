40f37d776aeb837021d0192ade5d7cea8102aa57
Leon Radeck <leon.radeck@sap.com>
2017-06-08 11:26:28 +0200

    Show message when removing series.

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/StringMessages.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/StringMessages.java
@@ -1811,4 +1811,5 @@ public interface StringMessages extends com.sap.sse.gwt.client.StringMessages,
     String unknownLeaderboardType(String name);
     String selectCompetitorsToEliminate();
     String trackedRaceAlreadyLinked();
+    String reallyRemoveSeries(String seriesName);
 }
