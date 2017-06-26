2e185f1405e41866b828ed1d072597a996ac3e0c
Steffen Schaefer <steffen.schaefer@sap.com>
2017-06-26 09:20:23 +0200

    Bug 4148: UserProfileSettingsPlace is now marked as HasMobileVersion to not switch to the desktop page anymore

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/user/profile/settings/UserProfileSettingsPlace.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/user/profile/settings/UserProfileSettingsPlace.java
@@ -1,11 +1,12 @@
 package com.sap.sailing.gwt.home.shared.places.user.profile.settings;
 
 import com.google.gwt.place.shared.Prefix;
+import com.sap.sailing.gwt.home.shared.app.HasMobileVersion;
 import com.sap.sailing.gwt.home.shared.places.PlaceTokenPrefixes;
 import com.sap.sailing.gwt.home.shared.places.user.profile.AbstractUserProfilePlace;
 
 
-public class UserProfileSettingsPlace extends AbstractUserProfilePlace /* TODO implements HasMobileVersion */ {
+public class UserProfileSettingsPlace extends AbstractUserProfilePlace implements HasMobileVersion {
 
     @Prefix(PlaceTokenPrefixes.UserProfileSettings)
     public static class Tokenizer extends AbstractUserProfilePlace.Tokenizer<UserProfileSettingsPlace> {
