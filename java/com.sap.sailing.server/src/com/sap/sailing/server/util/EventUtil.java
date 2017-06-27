8bbd4b0e6039131ae1579685121e53b5f4cdb441
Benjamin Barth <benjamin.barth@oio.de>
2017-06-27 14:23:04 +0200

    bug2059 Fixed EventUtil interface implementation

--- java/com.sap.sailing.server/src/com/sap/sailing/server/util/EventUtil.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/util/EventUtil.java
@@ -1,14 +1,21 @@
 package com.sap.sailing.server.util;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.time.ZoneOffset;
 
 import com.sap.sailing.domain.base.EventBase;
 
 public interface EventUtil {
-        
+
+    /**
+     * Obtains the year of the provided {@link EventBase event}'s start date.
+     * 
+     * @param event
+     *            the {@link EventBase event} to get the year for
+     * @return the year of the {@link EventBase event}'s start date
+     */
     public static Integer getYearOfEvent(EventBase event) {
-        LocalDateTime dateTime = LocalDateTime.ofEpochSecond(event.getStartDate().asMillis(), 0, ZoneOffset.UTC);
-        return dateTime.getYear();
+        Instant instant = Instant.ofEpochMilli(event.getStartDate().asMillis());
+        return instant.atOffset(ZoneOffset.UTC).getYear();
     }
 }
