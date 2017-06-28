ee4224a0d0b5201ac0680fdcf11f175fc77d8fb6
Steffen Schaefer <steffen.schaefer@sap.com>
2017-06-28 10:16:08 +0200

    Bug 2059: Adjusted statistics UI to support different implementations by supporting desktop an mobile views using composition instead of inheritance

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/statistics/DesktopStatisticsBoxView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/statistics/DesktopStatisticsBoxView.java
@@ -4,43 +4,33 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.UListElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
-import com.sap.sailing.gwt.home.communication.event.statistics.EventStatisticsDTO;
-import com.sap.sailing.gwt.home.shared.partials.statistics.AbstractStatisticsBox;
+import com.sap.sailing.gwt.home.shared.partials.statistics.StatisticsBoxView;
 
-public class StatisticsBox extends AbstractStatisticsBox {
+public class DesktopStatisticsBoxView extends Composite implements StatisticsBoxView {
 
     private static StatisticsBoxUiBinder uiBinder = GWT.create(StatisticsBoxUiBinder.class);
 
-    interface StatisticsBoxUiBinder extends UiBinder<Widget, StatisticsBox> {
+    interface StatisticsBoxUiBinder extends UiBinder<Widget, DesktopStatisticsBoxView> {
     }
     
     @UiField UListElement itemContainerUi;
-
-    public StatisticsBox() {
-        this(true);
-    }
     
-    public StatisticsBox(boolean showRegattaInformation) {
-        super(showRegattaInformation);
+    public DesktopStatisticsBoxView() {
         StatisticsBoxResources.INSTANCE.css().ensureInjected();
         initWidget(uiBinder.createAndBindUi(this));
         setVisible(false);
     }
-    
-    @Override
-    public void setData(EventStatisticsDTO statistics) {
-        super.setData(statistics);
-        setVisible(true);
-    }
 
     @Override
-    protected void clear() {
+    public void clear() {
+        setVisible(true);
         itemContainerUi.removeAllChildren();
     }
     
     @Override
-    protected void addItem(String iconUrl, String name, Object payload) {
+    public void addItem(String iconUrl, String name, Object payload) {
         itemContainerUi.appendChild(new StatisticsBoxItem(iconUrl, name, payload).getElement());
     }
 
--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/statistics/EventStatisticsBoxPresenter.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/statistics/EventStatisticsBoxPresenter.java
@@ -1,12 +1,10 @@
 package com.sap.sailing.gwt.home.shared.partials.statistics;
 
-import com.google.gwt.i18n.client.NumberFormat;
-import com.google.gwt.user.client.ui.Composite;
 import com.sap.sailing.gwt.home.communication.event.statistics.EventStatisticsDTO;
 import com.sap.sailing.gwt.home.shared.refresh.RefreshableWidget;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 
-public abstract class AbstractStatisticsBox extends Composite implements RefreshableWidget<EventStatisticsDTO> {
+public class EventStatisticsBoxPresenter extends AbstractStatisticsBoxPresenter implements RefreshableWidget<EventStatisticsDTO> {
     
     private static final StringMessages MSG = StringMessages.INSTANCE;
     public static final String ICON_REGATTAS_FOUGHT = "images/mobile/icon_regattasFought.svg";
@@ -18,30 +16,11 @@ public abstract class AbstractStatisticsBox extends Composite implements Refresh
     public static final String ICON_WIND_FIX = "images/mobile/strongest_wind.svg";
     public static final String ICON_SUM_MILES = "images/mobile/sum_miles.svg";
     
-    private NumberFormat simpleFormat = NumberFormat.getFormat("#0.0");
-    
     private final boolean showRegattaInformation;
 
-    public AbstractStatisticsBox(boolean showRegattaInformation) {
+    public EventStatisticsBoxPresenter(boolean showRegattaInformation, StatisticsBoxView view) {
+        super(view);
         this.showRegattaInformation = showRegattaInformation;
-    }
-
-    public void addItemWithCompactFormat(String iconUrl, String name, Double payload) {
-        if (payload != null && payload != 0) {
-            addItem(iconUrl, name, compactNumber(payload));
-        }
-    }
-    
-    public void addItemWithCompactFormat(String iconUrl, String name, long payload) {
-        if (payload != 0) {
-            addItem(iconUrl, name, compactNumber(payload));
-        }
-    }
-
-    public void addItemIfNotNull(String iconUrl, String name, Object payload) {
-        if (payload != null || payload instanceof Number && ((Number) payload).longValue() != 0) {
-            addItem(iconUrl, name, payload);
-        }
     }
 
     @Override
@@ -59,32 +38,4 @@ public abstract class AbstractStatisticsBox extends Composite implements Refresh
         addItemWithCompactFormat(ICON_FASTEST_SAILOR, MSG.fastestSailor() + ": "
                 + statistics.getCompetitorInfo(), statistics.getCompetitorSpeed());
     }
-    
-    private String compactNumber(double value) {
-        if(value < 100.0) {
-            return simpleFormat.format(value);
-        }
-        if(value < 100_000.0) {
-            return "" + Double.valueOf(value).intValue();
-        }
-        if(value < 100_000_000.0) {
-            return StringMessages.INSTANCE.millionValue(value / 1_000_000.0);
-        }
-        return StringMessages.INSTANCE.billionValue(value / 1_000_000_000.0);
-    }
-    
-    private String compactNumber(long value) {
-        if(value < 100_000l) {
-            return "" + value;
-        }
-        if(value < 100_000_000l) {
-            return StringMessages.INSTANCE.millionValue(value / 1_000_000.0);
-        }
-        return StringMessages.INSTANCE.billionValue(value / 1_000_000_000.0);
-    }
-    
-    protected abstract void clear();
-    
-    protected abstract void addItem(String iconUrl, String name, Object payload);
-
 }
