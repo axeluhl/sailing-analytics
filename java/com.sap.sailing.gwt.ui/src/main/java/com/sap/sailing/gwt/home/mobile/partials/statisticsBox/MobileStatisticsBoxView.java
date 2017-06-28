6d0ea3db87b4811b80c6c0f1a25219806b560704
Steffen Schaefer <steffen.schaefer@sap.com>
2017-06-28 15:33:20 +0200

    Bug 2059: Added yearly statistics in mobile race list

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/statisticsBox/MobileStatisticsBoxView.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/statisticsBox/MobileStatisticsBoxView.java
@@ -4,20 +4,28 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.home.mobile.partials.section.MobileSection;
+import com.sap.sailing.gwt.home.mobile.partials.sectionHeader.SectionHeaderContent;
 import com.sap.sailing.gwt.home.shared.partials.statistics.StatisticsBoxView;
+import com.sap.sailing.gwt.ui.client.StringMessages;
 
 public class MobileStatisticsBoxView extends Composite implements StatisticsBoxView {
     private static StatisticsBoxUiBinder uiBinder = GWT.create(StatisticsBoxUiBinder.class);
     
-    interface StatisticsBoxUiBinder extends UiBinder<Widget, MobileStatisticsBoxView> {
+    interface StatisticsBoxUiBinder extends UiBinder<MobileSection, MobileStatisticsBoxView> {
     }
 
     @UiField MobileSection itemContainerUi;
+    @UiField SectionHeaderContent headerUi;
     
     public MobileStatisticsBoxView() {
+        this(StringMessages.INSTANCE.statistics());
+    }
+    
+    public MobileStatisticsBoxView(String title) {
+        StatisticsBoxResources.INSTANCE.css().ensureInjected();
         initWidget(uiBinder.createAndBindUi(this));
+        headerUi.setSectionTitle(title);
     }
 
     @Override
