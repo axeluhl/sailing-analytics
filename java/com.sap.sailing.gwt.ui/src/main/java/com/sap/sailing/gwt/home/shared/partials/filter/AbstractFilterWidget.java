9ddc5736b1f443555c07e4586b50864205dd9e63
Benjamin Barth <benjamin.barth@oio.de>
2017-07-18 14:44:11 +0200

    bug4205 Added some JavaDocs to interfaces

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/filter/AbstractFilterWidget.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/filter/AbstractFilterWidget.java
@@ -7,8 +7,18 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Composite;
 import com.sap.sse.common.filter.Filter;
 
+/**
+ * Abstract implementation of {@link FilterWidget} holding a list of
+ * {@link #addFilterValueChangeHandler(FilterValueChangeHandler) added} {@link FilterValueChangeHandler handlers} and
+ * providing a {@link #notifyValueChangeHandlers() convenience method} to notify them.
+ * 
+ * @param <T>
+ *            the actual type of values to be filtered
+ * @param <C>
+ *            the actual type of values to select
+ */
 public abstract class AbstractFilterWidget<T, C> extends Composite implements FilterWidget<T, C> {
-    
+
     private final List<FilterValueChangeHandler<T>> valueChangeHandlers = new ArrayList<>();
 
     @Override
@@ -16,7 +26,11 @@ public abstract class AbstractFilterWidget<T, C> extends Composite implements Fi
         valueChangeHandlers.add(handler);
         return () -> valueChangeHandlers.remove(handler);
     }
-    
+
+    /**
+     * Convenience method to notify all {@link #addFilterValueChangeHandler(FilterValueChangeHandler) added}
+     * {@link FilterValueChangeHandler handler}s.
+     */
     protected void notifyValueChangeHandlers() {
         Filter<T> filter = getFilter();
         valueChangeHandlers.forEach(valueChangeHandler -> valueChangeHandler.onFilterValueChanged(filter));
