8819c57079a7ce5417bdf5ca58ca60034b8bff4e
Steffen Schaefer <steffen.schaefer@sap.com>
2017-08-17 14:22:28 +0200

    Bug 4228: explicitly configured a place prefix for MoreLoginInformationPlace

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/morelogininformation/MoreLoginInformationPlace.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/morelogininformation/MoreLoginInformationPlace.java
@@ -2,9 +2,12 @@ package com.sap.sailing.gwt.home.shared.places.morelogininformation;
 
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceTokenizer;
+import com.google.gwt.place.shared.Prefix;
 import com.sap.sailing.gwt.home.shared.app.HasMobileVersion;
+import com.sap.sailing.gwt.home.shared.places.PlaceTokenPrefixes;
 
 public class MoreLoginInformationPlace extends Place implements HasMobileVersion {
+    @Prefix(PlaceTokenPrefixes.AboutAccount)
     public static class Tokenizer implements PlaceTokenizer<MoreLoginInformationPlace> {
         @Override
         public String getToken(MoreLoginInformationPlace place) {
