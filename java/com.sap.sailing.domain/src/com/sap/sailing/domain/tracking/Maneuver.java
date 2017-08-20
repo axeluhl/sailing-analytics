0575fa52ab8543d97586dd6c75605dbc32e07632
Vladislav Chumak <v.chumak@sap.com>
2017-08-20 15:30:11 +0800

    Fixed recent merge with compile errors

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/Maneuver.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/Maneuver.java
@@ -2,6 +2,7 @@ package com.sap.sailing.domain.tracking;
 
 import com.sap.sailing.domain.common.Distance;
 import com.sap.sailing.domain.common.ManeuverType;
+import com.sap.sailing.domain.common.SpeedWithBearing;
 import com.sap.sailing.domain.common.Tack;
 import com.sap.sailing.domain.common.tracking.GPSFix;
 import com.sap.sse.common.TimePoint;
@@ -13,6 +14,10 @@ public interface Maneuver extends GPSFix {
 
     @Dimension(messageKey="Tack", ordinal=13)
     Tack getNewTack();
+    
+    SpeedWithBearing getSpeedWithBearingBefore();
+
+    SpeedWithBearing getSpeedWithBearingAfter();
 
     @Statistic(messageKey="DirectionChange", resultDecimals=2, ordinal=2)
     double getDirectionChangeInDegrees();
