f36fac1adce8e941b2e95ec75a693f94c6665d87
Raimund Wege <raimundwege@gmail.com>
2017-08-21 14:16:46 +0200

    Added training and create training view controller

---
+++ ios/SAPTracker/src/xcode/SAPTracker/CreateTrainingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateTrainingViewController.swift
+//  SAPTracker
+//
+//  Created by Raimund Wege on 21.08.17.
+//  Copyright © 2017 com.sap.sailing. All rights reserved.
+//
+
+import UIKit
+
+class CreateTrainingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
