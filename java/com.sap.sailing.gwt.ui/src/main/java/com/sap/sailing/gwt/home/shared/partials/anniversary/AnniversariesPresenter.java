71cb5881a570300b1329d95bca1cf9d9a275ec82
Benjamin Barth <benjamin.barth@oio.de>
2017-08-22 17:55:09 +0200

    bug4227 Integrated TTL calculation and hiding if no anniversary is available

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/anniversary/AnniversariesPresenter.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/anniversary/AnniversariesPresenter.java
@@ -1,13 +1,15 @@
 package com.sap.sailing.gwt.home.shared.partials.anniversary;
 
+import com.sap.sailing.gwt.home.communication.anniversary.AnniversariesDTO;
 import com.sap.sailing.gwt.home.communication.anniversary.AnniversaryDTO;
+import com.sap.sailing.gwt.home.shared.refresh.RefreshableWidget;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 
-public class AnniversariesPresenter {
+public class AnniversariesPresenter implements RefreshableWidget<AnniversariesDTO> {
 
     private static final String COUNTDOWN_MAJOR_ICON = "images/mobile/icon_trackedCount.svg";
-    private static final String COUNTDOWN_REPDIGIT_ICON = "images/mobile/icon_trackedCount.svg";
-    private static final String ANNOUNCEMENT_ICON = "images/mobile/icon_trackedCount.svg";
+    // private static final String COUNTDOWN_REPDIGIT_ICON = "images/mobile/icon_trackedCount.svg";
+    // private static final String ANNOUNCEMENT_ICON = "images/mobile/icon_trackedCount.svg";
 
     private AnniversariesView view;
 
@@ -15,7 +17,14 @@ public class AnniversariesPresenter {
         this.view = view;
     }
 
-    public void addAnniversary(AnniversaryDTO anniversary) {
+    @Override
+    public void setData(AnniversariesDTO data) {
+        this.view.clearAnniversaries();
+        this.view.asWidget().setVisible(!data.isEmpty());
+        data.getValues().forEach(this::addAnniversary);
+    }
+
+    private void addAnniversary(AnniversaryDTO anniversary) {
         final int countdown = anniversary.getCountDown(), target = anniversary.getTarget();
         final String teaser = StringMessages.INSTANCE.anniversaryMajorCountdownTeaser(countdown, target);
         final String description = StringMessages.INSTANCE.anniversaryMajorCountdownDescription(target);
