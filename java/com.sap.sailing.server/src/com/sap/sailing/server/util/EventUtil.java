a49360a9333c749148f7a6bc47ef73269b65d2c5
Steffen Schaefer <steffen.schaefer@sap.com>
2017-09-12 11:46:11 +0200

    Bug 4227: moved isFakeSeries method from HomeServiceUtil to EventUtil to make it possible to reuse this functionality in RacingEventService for the calculation of race details used by anniversary race calculation

--- java/com.sap.sailing.server/src/com/sap/sailing/server/util/EventUtil.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/util/EventUtil.java
@@ -2,8 +2,10 @@ package com.sap.sailing.server.util;
 
 import java.time.Instant;
 import java.time.ZoneOffset;
+import java.util.Iterator;
 
 import com.sap.sailing.domain.base.EventBase;
+import com.sap.sailing.domain.base.LeaderboardGroupBase;
 
 public interface EventUtil {
 
@@ -16,5 +18,20 @@ public interface EventUtil {
      */
     public static Integer getYearOfEvent(EventBase event) {
         return event.getStartDate() == null ? null : Instant.ofEpochMilli(event.getStartDate().asMillis()).atOffset(ZoneOffset.UTC).getYear();
+    }
+    
+    /**
+     * Returns {@code true} if the given event is part of an event series.
+     */
+    public static boolean isFakeSeries(EventBase event) {
+        final Iterator<? extends LeaderboardGroupBase> lgIter = event.getLeaderboardGroups().iterator();
+        if (!lgIter.hasNext()) {
+            return false;
+        }
+        final LeaderboardGroupBase lg = lgIter.next();
+        if (lgIter.hasNext()) {
+            return false;
+        }
+        return lg.hasOverallLeaderboard();
     }
 }
