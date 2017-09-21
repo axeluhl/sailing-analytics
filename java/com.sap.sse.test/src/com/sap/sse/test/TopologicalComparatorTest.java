121d3e0d278fcd78eddb2a1d967afbb8177a710b
Axel Uhl <axel.uhl@sap.com>
2017-09-22 00:59:14 +0200

    started adding tests for graph sort
    Change-Id: I90ffac1aa67deb841f277e54406081ac70190e29

---
+++ java/com.sap.sse.test/src/com/sap/sse/test/TopologicalComparatorTest.java
@@ -0,0 +1,56 @@
+package com.sap.sse.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.sap.sse.util.graph.DirectedEdge;
+import com.sap.sse.util.graph.DirectedGraph;
+import com.sap.sse.util.graph.impl.DirectedEdgeImpl;
+import com.sap.sse.util.graph.impl.DirectedGraphImpl;
+import com.sap.sse.util.topologicalordering.TopologicalComparator;
+
+public class TopologicalComparatorTest {
+    private final Set<String> nodes = new HashSet<>();
+    private final Set<DirectedEdge<String>> edges = new HashSet<>();
+    private DirectedGraph<String> graph;
+    
+    private void addLessThan(String lesser, String greater) {
+        nodes.add(lesser);
+        nodes.add(greater);
+        edges.add(new DirectedEdgeImpl<String>(lesser, greater));
+    }
+    
+    private void singleRoot(String root) {
+        nodes.add(root);
+    }
+    
+    private void createGraph() {
+        graph = new DirectedGraphImpl<>(nodes, edges);
+    }
+    
+    @Test
+    public void testSimpleSort() {
+        addLessThan("A", "B"); addLessThan("B", "C"); addLessThan("C", "D");
+        addLessThan("E", "F"); addLessThan("F", "G"); addLessThan("G", "H");
+        createGraph();
+        TopologicalComparator<String> comparator = new TopologicalComparator<>(graph);
+        assertEquals(0, comparator.compare("A", "E"));
+        assertEquals(0, comparator.compare("B", "F"));
+        assertEquals(0, comparator.compare("C", "G"));
+        assertEquals(0, comparator.compare("D", "H"));
+        assertChain(comparator, "A", "B", "C", "D");
+        assertChain(comparator, "E", "F", "G", "H");
+    }
+    
+    private void assertChain(TopologicalComparator<String> comparator, String...strings) {
+        for (int i=0; i<strings.length; i++) {
+            for (int j=0; j<strings.length; j++) {
+                assertEquals(Integer.compare(i, j), comparator.compare(strings[i], strings[j]));
+            }
+        }
+    }
+}
