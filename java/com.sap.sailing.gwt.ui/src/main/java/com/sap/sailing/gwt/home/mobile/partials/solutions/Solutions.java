0fe4eda923159eea737dfda050c80758c7985651
Benjamin Barth <benjamin.barth@oio.de>
2017-09-27 16:49:31 +0200

    bug4311 Refactored mobile solution partial to use accordion and item class

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/solutions/Solutions.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/solutions/Solutions.java
@@ -2,20 +2,14 @@ package com.sap.sailing.gwt.home.mobile.partials.solutions;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.AnchorElement;
-import com.google.gwt.dom.client.DivElement;
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.ImageElement;
 import com.google.gwt.safehtml.shared.UriUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Event;
-import com.google.gwt.user.client.EventListener;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.home.desktop.places.whatsnew.WhatsNewPlace.WhatsNewNavigationTabs;
 import com.sap.sailing.gwt.home.mobile.app.MobilePlacesNavigator;
-import com.sap.sailing.gwt.home.shared.utils.CollapseAnimation;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 
 public class Solutions extends Composite {
@@ -26,14 +20,7 @@ public class Solutions extends Composite {
     private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
 
     @UiField StringMessages i18n;
-    @UiField DivElement sapInSailingDiv;
-    @UiField DivElement sapSailingAnalyticsUi;
-    @UiField DivElement raceCommitteeAppUi;
-    @UiField DivElement inSightAppUi;
-    @UiField DivElement buoyPingerAppUi;
-    @UiField DivElement postRaceAnalyticsUi;
-    @UiField DivElement strategySimulatorUi;
-    
+
     @UiField AnchorElement sailingAnalyticsDetailsAnchor;
     @UiField AnchorElement raceManagerAppDetailsAnchor;
     @UiField AnchorElement sailInSightAppDetailsAnchor;
@@ -49,7 +36,6 @@ public class Solutions extends Composite {
     @UiField ImageElement inSightAppStoreImgUi;
     
     public Solutions(MobilePlacesNavigator placesNavigator) {
-        SolutionsResources.INSTANCE.css().ensureInjected();
         initWidget(uiBinder.createAndBindUi(this));
         String playstorebadgeSrc = UriUtils.fromString(i18n.playstoreBadge()).asString();
         String insightAppstoreSrc = UriUtils.fromString("images/home/appstore" + i18n.appstoreBadgeSuffix() + ".svg")
@@ -62,55 +48,16 @@ public class Solutions extends Composite {
         inSightPlayStoreImgUi.setSrc(playstorebadgeSrc);
         buoyPingerPlayStoreImgUi.setSrc(playstorebadgeSrc);
         inSightAppStoreImgUi.setSrc(insightAppstoreSrc);
-        
+
         initWhatsNewLink(placesNavigator, WhatsNewNavigationTabs.SailingAnalytics, sailingAnalyticsDetailsAnchor);
         initWhatsNewLink(placesNavigator, WhatsNewNavigationTabs.RaceManagerApp, raceManagerAppDetailsAnchor);
         initWhatsNewLink(placesNavigator, WhatsNewNavigationTabs.InSightApp, sailInSightAppDetailsAnchor);
         initWhatsNewLink(placesNavigator, WhatsNewNavigationTabs.BuoyPingerApp, buoyPingerAppDetailsAnchor);
         initWhatsNewLink(placesNavigator, WhatsNewNavigationTabs.SailingSimulator, simulatorAppDetailsAnchor);
-        
-        initAnimation(sapInSailingDiv, true);
-        initAnimation(sapSailingAnalyticsUi, false);
-        initAnimation(raceCommitteeAppUi, false);
-        initAnimation(inSightAppUi, false);
-        initAnimation(buoyPingerAppUi, false);
-        initAnimation(postRaceAnalyticsUi, false);
-        initAnimation(strategySimulatorUi, false);
     }
     
     private void initWhatsNewLink(MobilePlacesNavigator placesNavigator, WhatsNewNavigationTabs tab, AnchorElement anchor) {
         placesNavigator.getWhatsNewNavigation(tab).configureAnchorElement(anchor);
     }
 
-    private void initAnimation(final DivElement rootElement, boolean showInitial) {
-        final CollapseAnimation animation = new CollapseAnimation(getContent(rootElement), showInitial);
-        setClassName(rootElement, SolutionsResources.INSTANCE.css().accordioncollapsed(), !showInitial);
-        Element header = getHeader(rootElement);
-        DOM.sinkEvents(header, Event.ONCLICK);
-        DOM.setEventListener(header, new EventListener() {
-            @Override
-            public void onBrowserEvent(Event event) {
-                boolean collapsed = isCollapsed(rootElement);
-                setClassName(rootElement, SolutionsResources.INSTANCE.css().accordioncollapsed(), !collapsed);
-                animation.animate(collapsed);
-            }
-        });
-    }
-    
-    private void setClassName(Element element, String className, boolean add) {
-        if (add) element.addClassName(className);
-        else element.removeClassName(className);
-    }
-    
-    private boolean isCollapsed(Element rootElement) {
-        return rootElement.hasClassName(SolutionsResources.INSTANCE.css().accordioncollapsed());
-    }
-    
-    private Element getHeader(Element rootElement) {
-        return rootElement.getFirstChildElement();
-    }
-    
-    private Element getContent(Element rootElement) {
-        return rootElement.getFirstChildElement().getNextSiblingElement();
-    }
 }
