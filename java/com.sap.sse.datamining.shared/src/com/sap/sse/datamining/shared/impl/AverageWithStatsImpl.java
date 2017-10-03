2a353732f82978655ac875fa8ed6cd9ad292ea53
Axel Uhl <axel.uhl@sap.com>
2017-10-03 11:51:11 +0200

    started to adjust tests to AverageWithStats idea;
    as min/max/median etc. are not computed properly yet and as not
    all expected aspects (standard deviation) are prepared properly,
    the tests regarding AverageWithStats still fail
    
    Change-Id: I3cb18439e820269e9f5a8924ba0cf933d7d2b60e

--- java/com.sap.sse.datamining.shared/src/com/sap/sse/datamining/shared/impl/AverageWithStatsImpl.java
+++ java/com.sap.sse.datamining.shared/src/com/sap/sse/datamining/shared/impl/AverageWithStatsImpl.java
@@ -57,4 +57,68 @@ public class AverageWithStatsImpl<T> implements AverageWithStats<T> {
     public String getResultType() {
         return resultType;
     }
+
+    @Override
+    public String toString() {
+        return "AverageWithStatsImpl [average=" + average + ", min=" + min + ", max=" + max + ", median=" + median
+                + ", standardDeviation=" + standardDeviation + ", count=" + count + ", resultType=" + resultType + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((average == null) ? 0 : average.hashCode());
+        result = prime * result + (int) (count ^ (count >>> 32));
+        result = prime * result + ((max == null) ? 0 : max.hashCode());
+        result = prime * result + ((median == null) ? 0 : median.hashCode());
+        result = prime * result + ((min == null) ? 0 : min.hashCode());
+        result = prime * result + ((resultType == null) ? 0 : resultType.hashCode());
+        result = prime * result + ((standardDeviation == null) ? 0 : standardDeviation.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AverageWithStatsImpl<?> other = (AverageWithStatsImpl<?>) obj;
+        if (average == null) {
+            if (other.average != null)
+                return false;
+        } else if (!average.equals(other.average))
+            return false;
+        if (count != other.count)
+            return false;
+        if (max == null) {
+            if (other.max != null)
+                return false;
+        } else if (!max.equals(other.max))
+            return false;
+        if (median == null) {
+            if (other.median != null)
+                return false;
+        } else if (!median.equals(other.median))
+            return false;
+        if (min == null) {
+            if (other.min != null)
+                return false;
+        } else if (!min.equals(other.min))
+            return false;
+        if (resultType == null) {
+            if (other.resultType != null)
+                return false;
+        } else if (!resultType.equals(other.resultType))
+            return false;
+        if (standardDeviation == null) {
+            if (other.standardDeviation != null)
+                return false;
+        } else if (!standardDeviation.equals(other.standardDeviation))
+            return false;
+        return true;
+    }
 }
--- java/com.sap.sse.datamining.test/src/com/sap/sse/datamining/impl/components/aggregators/TestParallelAggregationProcessors.java
+++ java/com.sap.sse.datamining.test/src/com/sap/sse/datamining/impl/components/aggregators/TestParallelAggregationProcessors.java
@@ -1,27 +1,19 @@
 package com.sap.sse.datamining.impl.components.aggregators;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.sap.sse.datamining.components.Processor;
 import com.sap.sse.datamining.impl.components.GroupedDataEntry;
 import com.sap.sse.datamining.shared.GroupKey;
-import com.sap.sse.datamining.shared.data.AverageWithStats;
 import com.sap.sse.datamining.shared.impl.GenericGroupKey;
 import com.sap.sse.datamining.test.util.ConcurrencyTestsUtil;
-import com.sap.sse.datamining.test.util.components.NullProcessor;
 
-public class TestParallelAggregationProcessors {
+public class TestParallelAggregationProcessors extends AbstractTestParallelAveragingProcessors<Number> {
     
-    private Collection<Processor<Map<GroupKey, AverageWithStats<?>>, ?>> receivers;
-    private Map<GroupKey, Number> receivedAggregations;
-
     @Test
     public void testSumAggregationProcessor() throws InterruptedException {
         Processor<GroupedDataEntry<Number>, Map<GroupKey, Number>> sumAggregationProcessor = ParallelGroupedNumberDataSumAggregationProcessor.getDefinition().construct(ConcurrencyTestsUtil.getExecutor(), receivers);
@@ -31,56 +23,6 @@ public class TestParallelAggregationProcessors {
         sumAggregationProcessor.finish();
         Map<GroupKey, Number> expectedReceivedAggregations = computeExpectedSumAggregations(elements);
         ConcurrencyTestsUtil.verifyResultData(receivedAggregations, expectedReceivedAggregations);
-    }
-
-    private Map<GroupKey, Number> computeExpectedSumAggregations(Collection<GroupedDataEntry<Number>> elements) {
-        Map<GroupKey, Number> expectedSumAggregations = new HashMap<>();
-        for (GroupedDataEntry<Number> element : elements) {
-            GroupKey key = element.getKey();
-            if (!expectedSumAggregations.containsKey(key)) {
-                expectedSumAggregations.put(key, 0.0);
-            }
-            Number currentValue = expectedSumAggregations.get(key);
-            expectedSumAggregations.put(key, currentValue.doubleValue() + element.getDataEntry().doubleValue());
-        }
-        return expectedSumAggregations;
-    }
-
-    @Test
-    public void testAverageAggregationProcessor() throws InterruptedException {
-        Processor<GroupedDataEntry<Number>, Map<GroupKey, Number>> averageAggregationProcessor = ParallelGroupedNumberDataAverageAggregationProcessor
-                .getDefinition().construct(ConcurrencyTestsUtil.getExecutor(), receivers);
-        Collection<GroupedDataEntry<Number>> elements = createElements();
-        ConcurrencyTestsUtil.processElements(averageAggregationProcessor, elements);
-        
-        averageAggregationProcessor.finish();
-        Map<GroupKey, Number> expectedReceivedAggregations = computeExpectedAverageAggregations(elements);
-        ConcurrencyTestsUtil.verifyResultData(receivedAggregations, expectedReceivedAggregations);
-    }
-
-    private Map<GroupKey, Number> computeExpectedAverageAggregations(Collection<GroupedDataEntry<Number>> elements) {
-        Map<GroupKey, Number> result = new HashMap<>();
-        
-        Map<GroupKey, Number> sumAggregations = computeExpectedSumAggregations(elements);
-        Map<GroupKey, Double> elementAmountPerKey = countElementAmountPerKey(elements);
-        for (Entry<GroupKey, Number> sumAggregationEntry : sumAggregations.entrySet()) {
-            GroupKey key = sumAggregationEntry.getKey();
-            result.put(key, sumAggregationEntry.getValue().doubleValue() / elementAmountPerKey.get(key));
-        }
-        return result;
-    }
-
-    private Map<GroupKey, Double> countElementAmountPerKey(Collection<GroupedDataEntry<Number>> elements) {
-        Map<GroupKey, Double> elementAmountPerKey = new HashMap<>();
-        for (GroupedDataEntry<?> element : elements) {
-            GroupKey key = element.getKey();
-            if (!elementAmountPerKey.containsKey(key)) {
-                elementAmountPerKey.put(key, 0.0);
-            }
-            Double currentAmount = elementAmountPerKey.get(key);
-            elementAmountPerKey.put(key, currentAmount + 1.0);
-        }
-        return elementAmountPerKey;
     }
 
     @Test
@@ -94,14 +36,6 @@ public class TestParallelAggregationProcessors {
         ConcurrencyTestsUtil.verifyResultData(receivedAggregations, expectedReceivedAggregations);
     }
 
-    private Map<GroupKey, Number> computeExpectedMedianAggregations() {
-        Map<GroupKey, Number> result = new HashMap<>();
-        result.put(new GenericGroupKey<Integer>(1), 7.0);
-        result.put(new GenericGroupKey<Integer>(2), 6.0);
-        result.put(new GenericGroupKey<Integer>(3), 5.0);
-        return result;
-    }
-
     @Test
     public void testMaxAggregationProcessor() throws InterruptedException {
         Processor<GroupedDataEntry<Number>, Map<GroupKey, Number>> maxAggregationProcessor = ParallelGroupedNumberDataMaxAggregationProcessor.getDefinition().construct(ConcurrencyTestsUtil.getExecutor(), receivers);
@@ -111,14 +45,6 @@ public class TestParallelAggregationProcessors {
         maxAggregationProcessor.finish();
         Map<GroupKey, Number> expectedReceivedAggregations = computeExpectedMaxAggregations();
         ConcurrencyTestsUtil.verifyResultData(receivedAggregations, expectedReceivedAggregations);
-    }
-
-    private Map<GroupKey, Number> computeExpectedMaxAggregations() {
-        Map<GroupKey, Number> result = new HashMap<>();
-        result.put(new GenericGroupKey<Integer>(1), 10.0);
-        result.put(new GenericGroupKey<Integer>(2), 7.0);
-        result.put(new GenericGroupKey<Integer>(3), 5.0);
-        return result;
     }
 
     @Test
@@ -132,21 +58,12 @@ public class TestParallelAggregationProcessors {
         ConcurrencyTestsUtil.verifyResultData(receivedAggregations, expectedReceivedAggregations);
     }
 
-    private Map<GroupKey, Number> computeExpectedMinAggregations() {
-        Map<GroupKey, Number> result = new HashMap<>();
-        result.put(new GenericGroupKey<Integer>(1), 5.0);
-        result.put(new GenericGroupKey<Integer>(2), 3.0);
-        result.put(new GenericGroupKey<Integer>(3), 5.0);
-        return result;
-    }
-
     @Test
     public void testCountAggregationProcessor() throws InterruptedException {
         Processor<GroupedDataEntry<Object>, Map<GroupKey, Number>> countAggregationProcessor = ParallelGroupedDataCountAggregationProcessor.getDefinition().construct(ConcurrencyTestsUtil.getExecutor(), receivers);
         @SuppressWarnings("unchecked")
         Collection<GroupedDataEntry<Object>> elements = (Collection<GroupedDataEntry<Object>>)(Collection<?>) createElements();
         ConcurrencyTestsUtil.processElements(countAggregationProcessor, elements);
-        
         countAggregationProcessor.finish();
         Map<GroupKey, Number> expectedReceivedAggregations = computeExpectedCountAggregations();
         ConcurrencyTestsUtil.verifyResultData(receivedAggregations, expectedReceivedAggregations);
@@ -159,41 +76,4 @@ public class TestParallelAggregationProcessors {
         result.put(new GenericGroupKey<Integer>(3), 3.0);
         return result;
     }
-
-    private Collection<GroupedDataEntry<Number>> createElements() {
-        Collection<GroupedDataEntry<Number>> elements = new ArrayList<>();
-        
-        GroupKey firstGroupKey = new GenericGroupKey<Integer>(1);
-        elements.add(new GroupedDataEntry<Number>(firstGroupKey, 5.0));
-        elements.add(new GroupedDataEntry<Number>(firstGroupKey, 10.0));
-        elements.add(new GroupedDataEntry<Number>(firstGroupKey, 7.0));
-        
-        GroupKey secondGroupKey = new GenericGroupKey<Integer>(2);
-        elements.add(new GroupedDataEntry<Number>(secondGroupKey, 5.0));
-        elements.add(new GroupedDataEntry<Number>(secondGroupKey, 3.0));
-        elements.add(new GroupedDataEntry<Number>(secondGroupKey, 7.0));
-        elements.add(new GroupedDataEntry<Number>(secondGroupKey, 7.0));
-        
-        GroupKey thirdGroupKey = new GenericGroupKey<Integer>(3);
-        elements.add(new GroupedDataEntry<Number>(thirdGroupKey, 5.0));
-        elements.add(new GroupedDataEntry<Number>(thirdGroupKey, 5.0));
-        elements.add(new GroupedDataEntry<Number>(thirdGroupKey, 5.0));
-        
-        return elements;
-    }
-    
-    @Before
-    public void initializeResultReceivers() {
-        @SuppressWarnings("unchecked")
-        Processor<Map<GroupKey, Number>, Void> receiver = new NullProcessor<Map<GroupKey, Number>, Void>((Class<Map<GroupKey, Number>>)(Class<?>) Map.class, Void.class) {
-            @Override
-            public void processElement(Map<GroupKey, Number> element) {
-                receivedAggregations = element;
-            }
-        };
-        
-        receivers = new ArrayList<>();
-        receivers.add(receiver);
-    }
-
 }
