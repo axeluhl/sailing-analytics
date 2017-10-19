fa337a91069f9d65ff946541caa021a5eff1cb25
Raphael Müßeler <raphael.muesseler@sap.com>
2017-10-19 15:08:48 +0200

    PairingList Implementation (attributes, constructor and getters and setters)

---
+++ java/com.sap.sse/src/com/sap/sse/pairinglist/impl/PairingListImpl.java
@@ -0,0 +1,30 @@
+package com.sap.sse.pairinglist.impl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import com.sap.sse.pairinglist.PairingList;
+
+public class PairingListImpl implements PairingList {
+  
+    private ArrayList<ArrayList<ArrayList<Integer>>> pList;
+    private double standardDev;
+    
+    
+    public PairingListImpl(ArrayList<ArrayList<ArrayList<Integer>>> pList, double standardDev) {
+        this.pList = pList;
+        this.standardDev = standardDev;
+    }
+
+    public void setPList(ArrayList<ArrayList<ArrayList<Integer>>> pList) {
+        this.pList = pList;
+    }
+    
+    public ArrayList<ArrayList<ArrayList<Integer>>> getPList() {
+        return this.pList;
+    }
+
+    public double getStandardDev() {
+        return standardDev;
+    }  
+    
+}
