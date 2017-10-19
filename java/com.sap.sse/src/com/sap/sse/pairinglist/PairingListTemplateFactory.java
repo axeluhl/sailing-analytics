f6e601b1849930629c3faba96433476bd8a70bec
Kuck <robin.kuck@sap.com>
2017-10-19 15:35:19 +0200

    bug3484 PairingListTemplateFactoryImpl, PairingListTemplateImpl created

--- java/com.sap.sse/src/com/sap/sse/pairinglist/PairingListTemplateFactory.java
+++ java/com.sap.sse/src/com/sap/sse/pairinglist/PairingListTemplateFactory.java
@@ -1,5 +1,11 @@
 package com.sap.sse.pairinglist;
 
-public interface PairingListTemplateFactory {
+import com.sap.sse.pairinglist.impl.PairingListImpl;
+import com.sap.sse.pairinglist.impl.PairingListTemplateFactoryImpl;
 
+public interface PairingListTemplateFactory {
+    
+    public PairingListTemplateFactoryImpl INSTANCE = new PairingListTemplateFactoryImpl();
+    
+    public PairingListTemplate createPairingListTemplate(int competitors, int flights);
 }
