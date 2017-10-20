2e00f3490ef75d4a8f37f7ce866f43ea78a8f322
Raphael Müßeler <raphael.muesseler@sap.com>
2017-10-20 15:02:09 +0200

    bug3484 implemented calcStandardDev()

--- java/com.sap.sse/src/com/sap/sse/pairinglist/impl/PairingListTemplateImpl.java
+++ java/com.sap.sse/src/com/sap/sse/pairinglist/impl/PairingListTemplateImpl.java
@@ -6,6 +6,13 @@ import com.sap.sse.pairinglist.PairingFrameProvider;
 import com.sap.sse.pairinglist.PairingList;
 import com.sap.sse.pairinglist.PairingListTemplate;
 
+/**
+ * @author D070307
+ *
+ * @param <Flight>
+ * @param <Group>
+ * @param <Competitor>
+ */
 public class PairingListTemplateImpl<Flight,Group,Competitor> implements PairingListTemplate<Flight,Group,Competitor> {
     
     private int[][] pairingListTemplate;
@@ -44,5 +51,45 @@ public class PairingListTemplateImpl<Flight,Group,Competitor> implements Pairing
             }
         }
         
+    }
+    
+    /**
+     * @param associations: association describes a 2 dimensional array of integers, which contains the information 
+     *                      about how often the teams play against each other
+     * @return standardDev: returns how much the association values deviate from each other
+     */
+    
+    private double calcStandardDev(int[][] associations) {
+        double standardDev = Double.POSITIVE_INFINITY;
+        double expectedValue = 0;
+        
+        /** 
+         * hist shows how often a specific value of one association occurs.
+         * A value specifies how often one team plays against another.
+         */
+        ArrayList<Integer> hist = new ArrayList<>();     
+        
+        // filling hist
+        for (int[] key: associations) {
+            for (int value: key) {
+                hist.add(value, (hist.get(value) + 1));
+            }
+        }
+        
+        // calculating the expected value of hist
+        for (int i = 0; i < hist.size(); i++) {
+            expectedValue = (hist.get(i) / hist.size()) * i; 
+        }
+        
+        // calculating standard deviation from value and expectedValue
+        for (Integer val: hist) {
+            standardDev = standardDev + Math.pow((val - expectedValue), 2);
+        }
+        
+        if (standardDev > 0) {
+            standardDev = Math.sqrt(standardDev / hist.size());
+        }
+        
+        return standardDev;
     }
 }
