cf3ea719e73a2fdbf9199ac2afe5f61cf76059da
Raphael Müßeler <raphael.muesseler@sap.com>
2017-10-25 09:27:03 +0200

    bug3484 created test method for 'getAssociationPairingList'

---
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/PairingListRegattaAdapter.java
@@ -0,0 +1,9 @@
+package com.sap.sailing.domain.base;
+
+import com.sap.sse.pairinglist.PairingFrameProvider;
+
+public interface PairingListRegattaAdapter<Flight, Group, Competitor> extends PairingFrameProvider<Flight, Group, Competitor> {
+
+    
+    
+}
--- java/com.sap.sse.test/src/com/sap/see/pairinglist/test/PairingListTest.java
+++ java/com.sap.sse.test/src/com/sap/see/pairinglist/test/PairingListTest.java
@@ -1,29 +1,52 @@
 package com.sap.see.pairinglist.test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
+import java.security.acl.Group;
 
-
+import org.junit.Before;
 import org.junit.Test;
 
-
 import com.sap.sse.pairinglist.impl.PairingListTemplateImpl;
+
+import junit.framework.Assert;
 
 
 public class PairingListTest<Flight, Group, Competitor> {
     PairingListTemplateImpl<Flight, Group, Competitor> aImpl;
+    
+    @Before
+    public void init() {
+        this.aImpl = new PairingListTemplateImpl<Flight, Group, Competitor>();
+        
+        // creating pairing list template
+        this.aImpl.create(15, 3, 18);
+    }
   
   
-   @Test
-   public void testPairingListCreation() {
-           aImpl=new PairingListTemplateImpl<>();
-           int[][] temp=aImpl.create(15, 3, 18);
-           assertNotNull(temp);
-           for(int[] i:temp){
-               for (int z: i){
-                   if(z<=0) fail("Problem in .create!");
-               }
-           }
-   }
+    @Test
+    public void testPairingListCreation() {
+        int[][] plTemplate = this.aImpl.getPairingListTemplate();
+        
+        Assert.assertNotNull(plTemplate);
+        for(int[] i:plTemplate){
+            for (int z: i){
+                if(z<=0) Assert.fail("Problem in .create!");
+            }
+        }
+    }
    
+    @Test 
+    public void testTeamAssociationCreation() {
+        int[][] plTemplate = this.aImpl.getPairingListTemplate();
+        int[][] associations = new int[18][18];
+        
+        this.aImpl.getAssociationsFromPairingList(plTemplate, associations);
+        
+        for (int x = 0; x < associations.length; x++) {
+            for (int y = 0; y < associations[0].length; y++) {
+                if ((x == y) && (associations[x][y] != -1)) {
+                    Assert.fail("In 'getAssociationsFromPairingList' the diagonal of association matrix has to be -1.");
+                }
+            }
+        }
+    }
 }
