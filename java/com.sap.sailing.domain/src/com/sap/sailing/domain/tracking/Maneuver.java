257e4cd22d93dacaff7fb2e34ba99dc5db8cc161
Vladislav Chumak <v.chumak@sap.com>
2017-10-26 15:32:39 +0200

    Completed javadoc of Maneuver interface

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/Maneuver.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/Maneuver.java
@@ -34,48 +34,67 @@ import com.sap.sse.datamining.annotations.Statistic;
  *
  */
 public interface Maneuver extends GPSFix {
+    /**
+     * Gets the type of this maneuver, e.g. whether its a tack, jibe and etc. The maneuver type is determined
+     * considering the boat's course change, wind bearing and marks.
+     * 
+     * @return The type of maneuver
+     */
     ManeuverType getType();
 
+    /**
+     * Gets the new tack applied after the maneuver. A tack in sailing is defined as the side of the boat (starboard, or
+     * port) from which the wind is blowing.
+     * 
+     * @return The new tack after the performed maneuver
+     */
     @Dimension(messageKey = "Tack", ordinal = 13)
     Tack getNewTack();
-    
+
     /**
-     * Gets the the maneuver loss of this maneuver which is the distance projected onto the average course between entering and exiting the
-     * maneuver that the boat lost compared to not having maneuvered.
+     * Gets the the maneuver loss of this maneuver which is the distance projected onto the average course between
+     * entering and exiting the maneuver that the boat lost compared to not having maneuvered. The maneuver loss is
+     * calculated considering the maneuver curve, which was performed between {@link #getTimePointBefore()} and
+     * {@link #getTimePointAfter()}.
      */
     Distance getManeuverLoss();
 
     /**
-     * Gets the time point of the corresponding maneuver. The time point refers to a position within
-     * the main curve of maneuver with the highest course change recorded toward the direction of maneuver.
+     * Gets the time point of the corresponding maneuver. The time point refers to a position within the main curve of
+     * maneuver with the highest course change recorded toward the direction of maneuver.
      * 
-     * @return The computed maneuver time point
+     * @return The maneuver time point with the highest course change
      */
     TimePoint getTimePoint();
-    
+
     /**
-     * Gets the time point of maneuver start.
+     * Gets the time point of maneuver start. The time point refers to a position before the main curve where the boat
+     * has started to lose speed due to preparations for turning.
      * 
      * @return The time point of maneuver start
+     * @see Maneuver
      */
     TimePoint getTimePointBefore();
 
     /**
-     * Gets the computed time point of maneuver end.
+     * Gets the time point of maneuver end. The time point refers to a position after the main curve where the boat has
+     * accelerated to its target speed after turning.
      * 
      * @return The time point of maneuver end
+     * @see Maneuver
      */
     TimePoint getTimePointAfter();
 
     /**
-     * Gets the speed with bearing at maneuver start.
+     * Gets the speed with bearing at maneuver start, which is at {@link #getTimePointBefore()}.
      * 
      * @return The speed with bearing at maneuver start
+     * 
      */
     SpeedWithBearing getSpeedWithBearingBefore();
 
     /**
-     * Gets the speed with bearing at maneuver end.
+     * Gets the speed with bearing at maneuver end, which is at {@link #getTimePointAfter()}.
      * 
      * @return The speed with bearing at maneuver end
      */
@@ -83,22 +102,36 @@ public interface Maneuver extends GPSFix {
 
     /**
      * Gets the total course change performed within maneuver between {@link #getTimePointBefore()} and
-     * {@link #getTimePointAfter()} in degrees. The port side course changes are negative. The value may exceed 360
-     * degrees if the performed maneuver is a penalty circle.
+     * {@link #getTimePointAfter()} in degrees. The port side course changes produce a negative value. The value may
+     * exceed 360 degrees if the performed maneuver is a penalty circle.
      * 
      * @return The total course change within the whole maneuver in degrees
      */
     @Statistic(messageKey = "DirectionChange", resultDecimals = 2, ordinal = 2)
     double getDirectionChangeInDegrees();
 
+    /**
+     * Gets the time point of main curve start, where the boat starts to perform the main turn of the maneuver towards
+     * the direction of maneuver.
+     * 
+     * @return The time point of the main curve start
+     * @see Maneuver
+     */
     TimePoint getTimePointBeforeMainCurve();
 
+    /**
+     * Gets the time point of main curve end, , where the boat finishes to perform the main turn of the maneuver towards
+     * the direction of maneuver.
+     * 
+     * @return The time point of the main curve end
+     * @see Maneuver
+     */
     TimePoint getTimePointAfterMainCurve();
-    
+
     /**
      * Gets the total course change performed within the main curve of maneuver between
      * {@link #getTimePointBeforeMainCurve()} and {@link #getTimePointAfterMainCurve()} in degrees. The port side course
-     * changes are negative. The value may exceed 360 degrees if the performed maneuver is a penalty circle.
+     * changes produce a negative value. The value may exceed 360 degrees if the performed maneuver is a penalty circle.
      * 
      * @return The total course change with the main curve in degrees
      */
