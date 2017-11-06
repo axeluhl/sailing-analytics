cce9a34a8194dbace4953b44e3b896bf762580d4
Steffen Schaefer <steffen.schaefer@sap.com>
2017-11-06 14:21:38 +0100

    Bug 4346: Changed social sharing icons to be used via ImageResource

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/sharing/SharingButtonsResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/sharing/SharingButtonsResources.java
@@ -3,12 +3,22 @@ package com.sap.sailing.gwt.home.desktop.partials.sharing;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
 
 public interface SharingButtonsResources extends ClientBundle {
     public static final SharingButtonsResources INSTANCE = GWT.create(SharingButtonsResources.class);
 
     @Source("SharingButtons.gss")
     LocalCss css();
+    
+    @Source("email-icon.png")
+    ImageResource email();
+    
+    @Source("facebook-icon.png")
+    ImageResource facebook();
+    
+    @Source("twitter-icon.png")
+    ImageResource twitter();
 
     public interface LocalCss extends CssResource {
         String eventheader_sharing();
