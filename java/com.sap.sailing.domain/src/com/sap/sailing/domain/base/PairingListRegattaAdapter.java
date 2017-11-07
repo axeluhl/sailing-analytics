da301ed57f026d4be53771feb20a15d7334792cd
Kuck <robin.kuck@sap.com>
2017-11-07 10:21:55 +0100

    bug3484 PairingListRegattaAdapter fixed

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/PairingListRegattaAdapter.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/PairingListRegattaAdapter.java
@@ -1,15 +1,6 @@
 package com.sap.sailing.domain.base;
-
-<<<<<<< HEAD
-import com.sap.sse.pairinglist.CompetitionFormat;
+import com.sap.sse.pairinglist.CompetitionFormat; 
 
 public interface PairingListRegattaAdapter<Flight, Group, Competitor> extends CompetitionFormat<Flight, Group, Competitor> {
-=======
-import com.sap.sse.pairinglist.PairingFrameProvider;
 
-public interface PairingListRegattaAdapter<Flight, Group, Competitor> extends PairingFrameProvider {
->>>>>>> c6840c6f8abe1d8de70e395003205c11e79c2495
-
-    
-    
 }
