a003a80c563d1459c680b22d9ac671ef46e56a63
Steffen Schaefer <steffen.schaefer@sap.com>
2017-11-07 13:07:58 +0100

    Bug 4346: Changed Quickfinder arrow icon to be used via DataResource

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/quickfinder/QuickfinderResources.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/quickfinder/QuickfinderResources.java
@@ -3,6 +3,8 @@ package com.sap.sailing.gwt.home.mobile.partials.quickfinder;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.DataResource;
+import com.google.gwt.resources.client.DataResource.MimeType;
 
 public interface QuickfinderResources extends ClientBundle {
 
@@ -10,6 +12,10 @@ public interface QuickfinderResources extends ClientBundle {
 
     @Source("Quickfinder.gss")
     LocalCss css();
+    
+    @Source("quickfinder-arrow.svg")
+    @MimeType("image/svg+xml")
+    DataResource arrow();
 
     public interface LocalCss extends CssResource {
         String quickfinder();
