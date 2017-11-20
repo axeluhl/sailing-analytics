8b799e36b833f24c924021732c955cee3ed02bde
Steffen Schaefer <steffen.schaefer@sap.com>
2017-11-20 16:36:57 +0100

    Bug 4346: Changed solutions images to be used via ImageResource

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/solutions/SolutionsResources.java
@@ -0,0 +1,20 @@
+package com.sap.sailing.gwt.home.mobile.partials.solutions;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.sap.sailing.gwt.home.shared.partials.solutions.SolutionsSharedResources;
+
+public interface SolutionsResources extends SolutionsSharedResources {
+    
+    @Source("solutions-sap-trimmed.png")
+    ImageResource sapTrimmed();
+    
+    @Source("solutions-race.png")
+    ImageResource race();
+    
+    @Source("solutions-post-trimmed.png")
+    ImageResource postTrimmed();
+    
+    @Source("solutions-simulator-trimmed.png")
+    ImageResource simulatorTrimmed();
+
+}
