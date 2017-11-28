f2e542fc80dd30ddfb8e1edbae69eec7002d4944
Axel Uhl <axel.uhl@sap.com>
2017-11-28 14:46:42 +0100

    fixed type parameters of PairingListRegattaAdapter
    Change-Id: I6df729768924e0e4e84b5d28495a4bcecb582aa4

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/PairingListRegattaAdapter.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/PairingListRegattaAdapter.java
@@ -1,6 +1,5 @@
 package com.sap.sailing.domain.base;
 import com.sap.sse.pairinglist.CompetitionFormat; 
 
-public interface PairingListRegattaAdapter<Flight, Group, Competitor> extends CompetitionFormat<Flight, Group, Competitor> {
-
+public interface PairingListRegattaAdapter extends CompetitionFormat<RaceColumn, Fleet, Competitor> {
 }
