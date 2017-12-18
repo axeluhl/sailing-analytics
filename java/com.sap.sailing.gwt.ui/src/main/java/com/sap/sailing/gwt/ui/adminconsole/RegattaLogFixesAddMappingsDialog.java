a0329998f4a62c1f7f8c325a759852fb52b5feb7
Steffen Schaefer <steffen.schaefer@sap.com>
2017-12-18 11:06:34 +0100

    Bug 4371: Refactored device mapping dialogs to make it possible to open those with existing device IDs to make the integration in the expedition import process possible

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/RegattaLogFixesAddMappingsDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/RegattaLogFixesAddMappingsDialog.java
@@ -5,16 +5,17 @@ import java.util.Collection;
 import com.sap.sailing.gwt.ui.client.SailingServiceAsync;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sailing.gwt.ui.shared.DeviceMappingDTO;
+import com.sap.sailing.gwt.ui.shared.TrackFileImportDeviceIdentifierDTO;
 import com.sap.sse.gwt.client.ErrorReporter;
 
-public class RegattaLogImportFixesAndAddMappingsDialog extends AbstractRegattaLogFixesAddMappingsDialog {
+public class RegattaLogFixesAddMappingsDialog extends AbstractRegattaLogFixesAddMappingsDialog {
     TrackFileImportWidget importWidget;
 
-    public RegattaLogImportFixesAndAddMappingsDialog(SailingServiceAsync sailingService,
+    public RegattaLogFixesAddMappingsDialog(SailingServiceAsync sailingService,
             final ErrorReporter errorReporter, final StringMessages stringMessages, String leaderboardName,
+            Collection<TrackFileImportDeviceIdentifierDTO> importedDeviceIds,
             DialogCallback<Collection<DeviceMappingDTO>> callback) {
         super(sailingService, errorReporter, stringMessages, leaderboardName, callback);
-        importWidget = new TrackFileImportWidget(deviceIdTable, stringMessages, sailingService, errorReporter);
-        setImportWidget(importWidget);
+        deviceIdTable.getDataProvider().getList().addAll(importedDeviceIds);
     }
 }
