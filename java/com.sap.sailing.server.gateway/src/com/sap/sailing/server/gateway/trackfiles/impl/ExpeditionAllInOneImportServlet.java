87f91079c95e4876105aedd8bdafb1c86de6ae6a
Steffen Schaefer <steffen.schaefer@sap.com>
2017-12-19 07:48:14 +0100

    Bug 4371: Fixed automatic denotation in expedition all in one importer

--- java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/trackfiles/impl/ExpeditionAllInOneImportServlet.java
+++ java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/trackfiles/impl/ExpeditionAllInOneImportServlet.java
@@ -3,14 +3,19 @@ package com.sap.sailing.server.gateway.trackfiles.impl;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
 import org.json.simple.JSONObject;
+import org.osgi.util.tracker.ServiceTracker;
 
+import com.sap.sailing.domain.racelogtracking.RaceLogTrackingAdapterFactory;
 import com.sap.sailing.server.gateway.impl.AbstractFileUploadServlet;
 import com.sap.sailing.server.gateway.trackfiles.impl.ExpeditionAllInOneImporter.ImporterResult;
+import com.sap.sse.util.ServiceTrackerFactory;
 
 /**
  * Import servlet for sensor data files. Importers are located through the OSGi service registry and matched against the
@@ -19,6 +24,14 @@ import com.sap.sailing.server.gateway.trackfiles.impl.ExpeditionAllInOneImporter
 public class ExpeditionAllInOneImportServlet extends AbstractFileUploadServlet {
     private static final long serialVersionUID = 1120226743039934620L;
     // private static final Logger logger = Logger.getLogger(ExpeditionAllInOneImportServlet.class.getName());
+    private ServiceTracker<RaceLogTrackingAdapterFactory, RaceLogTrackingAdapterFactory> raceLogTrackingAdapterTracker;
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        raceLogTrackingAdapterTracker = ServiceTrackerFactory.createAndOpen(getContext(),
+                RaceLogTrackingAdapterFactory.class);
+    }
 
     /**
      * Process the uploaded file items.
@@ -38,7 +51,8 @@ public class ExpeditionAllInOneImportServlet extends AbstractFileUploadServlet {
             if (fileItem == null) {
                 throw new RuntimeException("No file to import");
             }
-            final ImporterResult importerResult = new ExpeditionAllInOneImporter(getService(), /* TODO */ null,
+            final ImporterResult importerResult = new ExpeditionAllInOneImporter(getService(),
+                    raceLogTrackingAdapterTracker.getService().getAdapter(getService().getBaseDomainFactory()),
                     getServiceFinderFactory(), getContext()).importFiles(fileName, fileItem);
             resp.setContentType("text/html;charset=UTF-8");
             this.toJSON(importerResult).writeJSONString(resp.getWriter());
