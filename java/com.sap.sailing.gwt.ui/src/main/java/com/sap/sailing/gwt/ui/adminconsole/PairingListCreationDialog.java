03e6597f1ec4079af3b6695982bd403e04101fd2
Axel Uhl <axel.uhl@sap.com>
2017-12-23 15:30:52 +0100

    use more specific link to "Quality" explanation in Wiki
    Change-Id: I5620782e234624963adf461106a49d119dd92d21

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/PairingListCreationDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/PairingListCreationDialog.java
@@ -105,7 +105,7 @@ public class PairingListCreationDialog extends DataEntryDialog<PairingListTempla
         qualityHelpImage.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                Window.open("https://wiki.sapsailing.com/wiki/howto/eventmanagers/Pairing-Lists", "", "");
+                Window.open("https://wiki.sapsailing.com/wiki/howto/eventmanagers/Pairing-Lists#pairing-list_general_quality", "", "");
             }
         });
         formGrid.setWidget(4, 0, qualityPanel);
