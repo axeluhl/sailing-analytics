fdd2b84fbe232fc2f471bdf22cd195b1f0dcdb0f
Axel Uhl <axel.uhl@sap.com>
2017-12-23 15:30:13 +0100

    use radio buttons instead of checkboxes for naming configuration
    Change-Id: Iafb4d602a92e7328e04a00775201d738b0909cd5

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/ChooseNameDenoteEventDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/ChooseNameDenoteEventDialog.java
@@ -4,9 +4,9 @@ import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -14,34 +14,32 @@ import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sailing.gwt.ui.shared.StrippedLeaderboardDTO;
 
 public class ChooseNameDenoteEventDialog extends AbstractChooseNameDenoteEventDialog<String> {
-    
-    private final CheckBox defaultName,ownPrefix;
+    private final RadioButton defaultName;
+    private final RadioButton ownPrefix;
     private final TextBox name;
     private final Label example;
     private final StrippedLeaderboardDTO strippedLeaderboardDTO;
     
     public ChooseNameDenoteEventDialog(StringMessages stringMessages,StrippedLeaderboardDTO leaderboard,DialogCallback<String> callback){
-        super("Choose a name", stringMessages, null, callback);
+        super(stringMessages.chooseAName(), stringMessages, null, callback);
+        final String NAME_TYPE_RADIO_BUTTON_GROUP = "namegroup";
         this.strippedLeaderboardDTO=leaderboard;
-        this.defaultName=createCheckbox(stringMessages.defaultName());
+        this.defaultName=createRadioButton(NAME_TYPE_RADIO_BUTTON_GROUP, stringMessages.defaultName());
         this.defaultName.setValue(true);
-        this.ownPrefix=createCheckbox(stringMessages.ownPrefix());
+        this.ownPrefix=createRadioButton(NAME_TYPE_RADIO_BUTTON_GROUP, stringMessages.ownPrefix());
         this.name=createTextBox("R", 5);
         this.name.setEnabled(false);
         this.example=new Label();
         this.updateExample();
         this.ownPrefix.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-            
             @Override
             public void onValueChange(ValueChangeEvent<Boolean> event) {
                 defaultName.setValue(false);
                 name.setEnabled(true);
                 updateExample();
-                
             }
         });
         this.defaultName.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-
             @Override
             public void onValueChange(ValueChangeEvent<Boolean> event) {
                 name.setEnabled(false);
@@ -50,23 +48,24 @@ public class ChooseNameDenoteEventDialog extends AbstractChooseNameDenoteEventDi
             }
         });
         this.name.addChangeHandler(new ChangeHandler() {
-            
             @Override
             public void onChange(ChangeEvent event) {
               updateExample();
-                
             }
         });
         this.getOkButton().setFocus(true);
     }
+
     protected void updateExample() {
-       if(this.defaultName.getValue()){
-           this.example.setText(stringMessages.exampleTextForName()+" "+strippedLeaderboardDTO.getName()+" "+ strippedLeaderboardDTO.getRaceList().get(0).getName()+" "+strippedLeaderboardDTO.getRaceList().get(0).getFleets().get(0).getName());
-       }else if(this.ownPrefix.getValue()){
-           this.example.setText(stringMessages.exampleTextForName()+" "+name.getValue()+" "+1);
-       }
-        
+        if (this.defaultName.getValue()) {
+            this.example.setText(stringMessages.exampleTextForName() + " " + strippedLeaderboardDTO.getName() + " "
+                    + strippedLeaderboardDTO.getRaceList().get(0).getName() + " "
+                    + strippedLeaderboardDTO.getRaceList().get(0).getFleets().get(0).getName());
+        } else if (this.ownPrefix.getValue()) {
+            this.example.setText(stringMessages.exampleTextForName() + " " + name.getValue() + 1);
+        }
     }
+    
     @Override
     protected Widget getAdditionalWidget(){
         final VerticalPanel panel = new VerticalPanel();
@@ -78,12 +77,15 @@ public class ChooseNameDenoteEventDialog extends AbstractChooseNameDenoteEventDi
         panel.add(formGrid);
         return panel;
     }
+    
     @Override
     protected String getResult() {
-        if(this.ownPrefix.getValue()){
-            return name.getValue();
-        }else{
-        return null;
+        final String result;
+        if (this.ownPrefix.getValue()) {
+            result = name.getValue();
+        } else {
+            result = null;
         }
+        return result;
     }
 }
