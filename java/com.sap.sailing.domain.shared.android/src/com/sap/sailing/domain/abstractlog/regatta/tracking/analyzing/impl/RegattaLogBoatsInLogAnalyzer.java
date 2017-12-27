6f1b629afecc71d03b175e9018948fe538ee14fd
Frank Mittag <fmittag@gmx.net>
2017-12-27 20:36:50 +0100

    fixed javadoc

--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/regatta/tracking/analyzing/impl/RegattaLogBoatsInLogAnalyzer.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/regatta/tracking/analyzing/impl/RegattaLogBoatsInLogAnalyzer.java
@@ -13,7 +13,7 @@ import com.sap.sailing.domain.base.Boat;
  * This class searches for RegisterBoat events in the given log.
  * 
  * Boats alone in a regatta log are not enough to be used in a real race.
- * Instead a competitor and a boat together must be registered through a {@link RegisterCompetitorAndBoat} event. 
+ * Instead a competitor and a boat together must be registered through a {@link RegisterCompetitorEvent} event. 
  *
  */
 public class RegattaLogBoatsInLogAnalyzer<LogT extends AbstractLog<EventT, VisitorT>, EventT extends AbstractLogEvent<VisitorT>, VisitorT>
