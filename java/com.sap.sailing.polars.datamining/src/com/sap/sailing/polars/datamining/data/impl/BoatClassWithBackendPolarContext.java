0f62511a76e08be47356c32ea4a44623003ac9fb
Vladislav Chumak <v.chumak@sap.com>
2018-01-17 01:46:13 +0100

    Added a rudimentary mining for target TWAs based on BackendPolars

--- java/com.sap.sailing.polars.datamining/src/com/sap/sailing/polars/datamining/data/impl/BoatClassWithBackendPolarContext.java
+++ java/com.sap.sailing.polars.datamining/src/com/sap/sailing/polars/datamining/data/impl/BoatClassWithBackendPolarContext.java
@@ -1,6 +1,12 @@
 package com.sap.sailing.polars.datamining.data.impl;
 
+import java.util.Set;
+
 import com.sap.sailing.domain.base.BoatClass;
+import com.sap.sailing.domain.base.SpeedWithBearingWithConfidence;
+import com.sap.sailing.domain.common.LegType;
+import com.sap.sailing.domain.common.Tack;
+import com.sap.sailing.domain.common.impl.KnotSpeedImpl;
 import com.sap.sailing.domain.polars.PolarDataService;
 import com.sap.sailing.polars.datamining.data.HasBackendPolarBoatClassContext;
 
@@ -22,6 +28,32 @@ public class BoatClassWithBackendPolarContext implements HasBackendPolarBoatClas
     @Override
     public PolarDataService getPolarDataService() {
         return polarDataService;
+    }
+    
+    public Double getTargetBeatAngle() {
+        Set<SpeedWithBearingWithConfidence<Void>> set = polarDataService.getAverageTrueWindSpeedAndAngleCandidates(boatClass, new KnotSpeedImpl(15), LegType.UPWIND, Tack.STARBOARD);
+        Double bestConfidence = -1.0;
+        Double bestBeatAngle = null;
+        for (SpeedWithBearingWithConfidence<Void> element : set) {
+            if(element.getConfidence() > bestConfidence) {
+                bestBeatAngle = element.getObject().getBearing().getDegrees();
+                bestConfidence = element.getConfidence();
+            }
+        }
+        return bestBeatAngle;
+    }
+    
+    public Double getTargetRunawayAngle() {
+        Set<SpeedWithBearingWithConfidence<Void>> set = polarDataService.getAverageTrueWindSpeedAndAngleCandidates(boatClass, new KnotSpeedImpl(15), LegType.DOWNWIND, Tack.PORT);
+        Double bestConfidence = -1.0;
+        Double bestBeatAngle = null;
+        for (SpeedWithBearingWithConfidence<Void> element : set) {
+            if(element.getConfidence() > bestConfidence) {
+                bestBeatAngle = element.getObject().getBearing().getDegrees();
+                bestConfidence = element.getConfidence();
+            }
+        }
+        return bestBeatAngle;
     }
 
     @Override
