cc247f7978487303aa0f8b0ed25e97441994fc36
Steffen Schaefer <steffen.schaefer@sap.com>
2018-02-05 16:52:46 +0100

    Bug 4380: Added JavaDocs

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/ChartZoomChangedEvent.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/ChartZoomChangedEvent.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
 
+/**
+ * Event to be fired by {@link AbstractRaceChart} when a user zooms into the chart.
+ */
 public class ChartZoomChangedEvent extends GwtEvent<ChartZoomChangedEvent.Handler>{    
     public static final Type<Handler> TYPE = new Type<>();
     private final Date rangeStart;
--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/ChartZoomResetEvent.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/ChartZoomResetEvent.java
@@ -3,6 +3,9 @@ package com.sap.sailing.gwt.ui.client.shared.charts;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
 
+/**
+ * Event to be fired by {@link AbstractRaceChart} when a user resets the zoom to show the whole time range again.
+ */
 public class ChartZoomResetEvent extends GwtEvent<ChartZoomResetEvent.Handler> {
     public static final Type<Handler> TYPE = new Type<>();
 
