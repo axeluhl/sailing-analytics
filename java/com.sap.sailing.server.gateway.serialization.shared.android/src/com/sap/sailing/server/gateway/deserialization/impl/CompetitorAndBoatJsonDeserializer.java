6a7d33bda8eff57669a1dd9482b1ba5d77d22037
Frank Mittag <fmittag@gmx.net>
2018-02-09 14:23:47 +0100

    Implemented a CompetitorAndBoatJsonSerializer and CompetitorAndBoatJsonDeserializer to implement a /rc/competitorsAndBoats service call to transfer Competitor/Boat pairs to the rcApp

---
+++ java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/deserialization/impl/CompetitorAndBoatJsonDeserializer.java
@@ -0,0 +1,32 @@
+package com.sap.sailing.server.gateway.deserialization.impl;
+
+import org.json.simple.JSONObject;
+
+import com.sap.sailing.domain.base.Boat;
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.SharedDomainFactory;
+import com.sap.sailing.domain.base.impl.DynamicBoat;
+import com.sap.sailing.server.gateway.deserialization.JsonDeserializationException;
+import com.sap.sailing.server.gateway.deserialization.JsonDeserializer;
+import com.sap.sse.common.Util.Pair;
+
+public class CompetitorAndBoatJsonDeserializer implements JsonDeserializer<Pair<Competitor, Boat>> {
+    private final JsonDeserializer<Competitor> competitorDeserializer;
+    private final JsonDeserializer<DynamicBoat> boatDeserializer;
+    
+    public static CompetitorAndBoatJsonDeserializer create(SharedDomainFactory baseDomainFactory) {
+        return new CompetitorAndBoatJsonDeserializer(CompetitorJsonDeserializer.create(baseDomainFactory), BoatJsonDeserializer.create(baseDomainFactory));
+    }
+
+    public CompetitorAndBoatJsonDeserializer(JsonDeserializer<Competitor> competitorDeserializer, JsonDeserializer<DynamicBoat> boatDeserializer) {
+        this.competitorDeserializer = competitorDeserializer;
+        this.boatDeserializer = boatDeserializer;
+    }
+
+    @Override
+    public Pair<Competitor, Boat> deserialize(JSONObject competitorAndBoatJsonObject) throws JsonDeserializationException {
+        Competitor competitor = competitorDeserializer.deserialize(Helpers.getNestedObjectSafe(competitorAndBoatJsonObject, "competitor"));
+        Boat boat = boatDeserializer.deserialize(Helpers.getNestedObjectSafe(competitorAndBoatJsonObject, "boat"));
+        return new Pair<Competitor, Boat>(competitor, boat);
+    }
+}
