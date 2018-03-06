cefc5e96b76f5ede04522e53e2e7b3f3032c6def
Benjamin Barth <benjamin.barth@oio.de>
2018-03-06 16:51:42 +0100

    bug4436 Fixed unit of maneuver loss in ManeuverTablePanel

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/racemap/maneuver/ManeuverTablePanel.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/racemap/maneuver/ManeuverTablePanel.java
@@ -204,7 +204,8 @@ public class ManeuverTablePanel extends AbstractCompositeComponent<ManeuverTable
 
             @Override
             public String getValue(SingleManeuverDTO object) {
-                return object.loss == null ? "" : towDigitAccuracy.format(object.loss) + stringMessages.meters();
+                return object.loss == null ? ""
+                        : (towDigitAccuracy.format(object.loss) + " " + stringMessages.metersUnit());
             }
         };
     }
