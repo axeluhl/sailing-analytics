38af28afed5d7f24e51acd24e6d515e6982b7569
Frank Mittag <fmittag@gmx.net>
2018-03-07 12:28:50 +0100

    fixed missing registration of BoatsResource in RestApiApplication

--- java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/jaxrs/api/RestApiApplication.java
+++ java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/jaxrs/api/RestApiApplication.java
@@ -16,6 +16,7 @@ public class RestApiApplication extends Application {
         classes.add(LeaderboardGroupsResource.class);
         classes.add(EventsResource.class);
         classes.add(RegattasResource.class);
+        classes.add(BoatsResource.class);
         classes.add(LeaderboardsResource.class);
         classes.add(LeaderboardsResourceV2.class);
         classes.add(PolarResource.class);
