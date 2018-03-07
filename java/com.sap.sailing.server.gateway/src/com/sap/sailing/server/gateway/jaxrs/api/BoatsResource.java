48518547124fb7f019c49b07dae6772f93ab8995
Frank Mittag <fmittag@gmx.net>
2018-03-07 15:25:40 +0100

    Changed BoatsResource to use the standard BoatJsonSerializer

--- java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/jaxrs/api/BoatsResource.java
+++ java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/jaxrs/api/BoatsResource.java
@@ -9,25 +9,14 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.lang.StringEscapeUtils;
-import org.json.simple.JSONObject;
 
 import com.sap.sailing.domain.base.Boat;
-import com.sap.sailing.domain.common.tracking.impl.BoatJsonConstants;
 import com.sap.sailing.server.gateway.jaxrs.AbstractSailingServerResource;
+import com.sap.sailing.server.gateway.serialization.impl.BoatJsonSerializer;
 
 @Path("/v1/boats")
 public class BoatsResource extends AbstractSailingServerResource {
     
-    public static JSONObject getBoatJSON(Boat boat) {
-        JSONObject json = new JSONObject();
-        json.put(BoatJsonConstants.FIELD_ID, boat.getId().toString());
-        json.put(BoatJsonConstants.FIELD_NAME, boat.getName());
-        json.put(BoatJsonConstants.FIELD_SAIL_ID, boat.getSailID());
-        json.put(BoatJsonConstants.FIELD_BOAT_CLASS_NAME, boat.getBoatClass().getName());
-        json.put(BoatJsonConstants.FIELD_COLOR, boat.getColor() != null ? boat.getColor().getAsHtml() : null);
-        return json;
-    }
-
     @GET
     @Produces("application/json;charset=UTF-8")
     @Path("{boatId}")
@@ -39,7 +28,8 @@ public class BoatsResource extends AbstractSailingServerResource {
                     .entity("Could not find a boat with id '" + StringEscapeUtils.escapeHtml(boatIdAsString) + "'.")
                     .type(MediaType.TEXT_PLAIN).build();
         } else {
-            String jsonString = getBoatJSON(boat).toJSONString();
+            BoatJsonSerializer boatJsonSerializer = BoatJsonSerializer.create();
+            String jsonString = boatJsonSerializer.serialize(boat).toJSONString();
             response = Response.ok(jsonString).header("Content-Type", MediaType.APPLICATION_JSON + ";charset=UTF-8").build();
         }
         return response;
