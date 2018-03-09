951e4f38d83a8999eb7ce0ec421d31b981e9d2f6
Frank Mittag <fmittag@gmx.net>
2018-03-09 10:48:31 +0100

    fixed and improved BoatResourceTest to use now also the BoatJsonDeserializer

--- java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/jaxrs/BoatResourceTest.java
+++ java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/jaxrs/BoatResourceTest.java
@@ -9,7 +9,8 @@ import org.junit.Test;
 
 import com.sap.sailing.domain.base.BoatClass;
 import com.sap.sailing.domain.base.impl.BoatClassImpl;
-import com.sap.sailing.domain.common.tracking.impl.BoatJsonConstants;
+import com.sap.sailing.domain.base.impl.DynamicBoat;
+import com.sap.sailing.server.gateway.deserialization.impl.BoatJsonDeserializer;
 import com.sap.sailing.server.gateway.deserialization.impl.Helpers;
 import com.sap.sailing.server.gateway.jaxrs.api.BoatsResource;
 
@@ -18,11 +19,11 @@ public class BoatResourceTest extends AbstractJaxRsApiTest {
     private final String boatName = "My boat";
     private final String boatClassName = "49er";
     private final String sailId = "GER 1";
+    private final BoatClass boatClass = new BoatClassImpl(boatClassName, true);
 
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        BoatClass boatClass = new BoatClassImpl(boatClassName, true);
         racingEventService.getBaseDomainFactory().getOrCreateBoat(id, boatName, boatClass, sailId, null);
     }
 
@@ -31,11 +32,15 @@ public class BoatResourceTest extends AbstractJaxRsApiTest {
         BoatsResource resource = spyResource(new BoatsResource());
         String jsonString = resource.getBoat(id).getEntity().toString();
         
-        JSONObject json = Helpers.toJSONObjectSafe(JSONValue.parse(jsonString));
-        assertTrue(json.get(BoatJsonConstants.FIELD_ID).equals(id));
-        assertTrue(json.get(BoatJsonConstants.FIELD_NAME).equals(boatName));
-        assertTrue(json.get(BoatJsonConstants.FIELD_BOAT_CLASS_NAME).equals(boatClassName));
-        assertTrue(json.get(BoatJsonConstants.FIELD_SAIL_ID).equals(sailId));
+        BoatJsonDeserializer boatJsonDeserializer = BoatJsonDeserializer.create(racingEventService.getBaseDomainFactory());
+        JSONObject jsonObject = Helpers.toJSONObjectSafe(JSONValue.parse(jsonString));
+        DynamicBoat boat = boatJsonDeserializer.deserialize(jsonObject);
+        
+        assertTrue(boat.getId().equals(id));
+        assertTrue(boat.getName().equals(boatName));
+        assertTrue(boat.getSailID().equals(sailId));
+        assertTrue(boat.getSailID().equals(sailId));
+        assertTrue(boat.getBoatClass().getName().equals(boatClassName));
     }
 
 }
