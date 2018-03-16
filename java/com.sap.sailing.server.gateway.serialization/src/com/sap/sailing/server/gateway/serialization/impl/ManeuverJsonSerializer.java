431aad65b57ea6b401204cb2ad0878d662ecc207
Vladislav Chumak <v.chumak@sap.com>
2018-03-16 17:54:33 +0100

    Included mark passing flag into REST-API

--- java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/serialization/impl/ManeuverJsonSerializer.java
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/serialization/impl/ManeuverJsonSerializer.java
@@ -17,6 +17,7 @@ public class ManeuverJsonSerializer implements JsonSerializer<Maneuver> {
     public static final String POSITION_AND_TIME = "positionAndTime";
     public static final String MAX_ANGULAR_VELOCITY_IN_DEGREES_PER_SECOND = "maxAngularVelocityInDegreesPerSecond";
     public static final String LOWEST_SPEED_IN_KNOTS = "lowestSpeedInKnots";
+    public static final String MARK_PASSING = "markPassing";
 
     private final GPSFixJsonSerializer gpsFixSerializer;
     private final DistanceJsonSerializer distanceSerializer;
@@ -46,6 +47,7 @@ public class ManeuverJsonSerializer implements JsonSerializer<Maneuver> {
         result.put(POSITION_AND_TIME, gpsFixSerializer.serialize(maneuver));
         result.put(MAX_ANGULAR_VELOCITY_IN_DEGREES_PER_SECOND, maneuver.getMaxAngularVelocityInDegreesPerSecond());
         result.put(LOWEST_SPEED_IN_KNOTS, maneuver.getLowestSpeed().getKnots());
+        result.put(MARK_PASSING, maneuver.isMarkPassing());
         return result;
     }
 }
