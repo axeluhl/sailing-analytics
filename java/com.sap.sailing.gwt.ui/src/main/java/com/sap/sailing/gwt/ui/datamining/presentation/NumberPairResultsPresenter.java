c481b75c237402647fa28d96aed9c6a7dabb0bf9
Leon Radeck <leon.radeck@sap.com>
2018-04-17 14:50:47 +1000

    Removed legend for more space. Gave points groupkey name. Always use circle as a marker for now.

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/datamining/presentation/NumberPairResultsPresenter.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/datamining/presentation/NumberPairResultsPresenter.java
@@ -1,6 +1,10 @@
 package com.sap.sailing.gwt.ui.datamining.presentation;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.moxieapps.gwt.highcharts.client.Chart;
@@ -10,14 +14,19 @@ import org.moxieapps.gwt.highcharts.client.Credits;
 import org.moxieapps.gwt.highcharts.client.Exporting;
 import org.moxieapps.gwt.highcharts.client.Point;
 import org.moxieapps.gwt.highcharts.client.Series;
-import org.moxieapps.gwt.highcharts.client.Series.Type;
+import org.moxieapps.gwt.highcharts.client.plotOptions.Marker;
+import org.moxieapps.gwt.highcharts.client.plotOptions.Marker.Symbol;
+import org.moxieapps.gwt.highcharts.client.plotOptions.SeriesPlotOptions;
 
+import com.google.gwt.maps.client.overlays.Circle;
 import com.google.gwt.user.client.ui.SimpleLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sse.common.settings.Settings;
 import com.sap.sse.datamining.shared.GroupKey;
 import com.sap.sse.datamining.shared.data.AveragePairWithStats;
+import com.sap.sse.datamining.shared.impl.CompoundGroupKey;
+import com.sap.sse.datamining.shared.impl.GenericGroupKey;
 import com.sap.sse.datamining.shared.impl.dto.QueryResultDTO;
 import com.sap.sse.gwt.client.shared.components.Component;
 import com.sap.sse.gwt.client.shared.components.SettingsDialogComponent;
@@ -27,12 +36,13 @@ public class NumberPairResultsPresenter extends AbstractResultsPresenter<Setting
     private QueryResultDTO<?> result;
     private final SimpleLayoutPanel chartPanel;
     private final Chart chart;
-    private final ArrayList<Point> series;
+    private final Map<GroupKey, Series> seriesMappedByGroupKey;
+    private final GroupKey simpleResultSeriesKey;
 
 
     public NumberPairResultsPresenter(Component<?> parent, ComponentContext<?> context, StringMessages stringMessages) {
         super(parent, context, stringMessages);
-  
+        seriesMappedByGroupKey = new HashMap<>();
         chartPanel = new SimpleLayoutPanel() {
             @Override
             public void onResize() {
@@ -40,10 +50,9 @@ public class NumberPairResultsPresenter extends AbstractResultsPresenter<Setting
                 chart.redraw();
             }
         };
+        simpleResultSeriesKey = new GenericGroupKey<>(stringMessages.results());
         chart = createChart();
         chartPanel.setWidget(chart);
-        series = new ArrayList<>();
-
     }
     
     private Chart createChart() {
@@ -67,22 +76,48 @@ public class NumberPairResultsPresenter extends AbstractResultsPresenter<Setting
     protected Widget getPresentationWidget() {
         return chartPanel;
     }
+    
+    private GroupKey groupKeyToSeriesKey(GroupKey groupKey) {
+        if (groupKey.hasSubKeys()) {
+            List<? extends GroupKey> subKeys = GroupKey.Util.getSubKeys(groupKey);
+            return subKeys.size() == 1 ? subKeys.get(0) : new CompoundGroupKey(subKeys);
+        } else {
+            return groupKey;
+        }
+    }
+    
+    private void createAndAddSeriesToChart() {
+        for (GroupKey groupKey : getCurrentResult().getResults().keySet()) {
+            GroupKey seriesKey = groupKeyToSeriesKey(groupKey);
+            if (!seriesMappedByGroupKey.containsKey(seriesKey)) {
+                seriesMappedByGroupKey.put(seriesKey, chart.createSeries().setPlotOptions(new SeriesPlotOptions().setShowInLegend(false).setMarker(new Marker().setSymbol(Symbol.CIRCLE))).setName(seriesKey.asString()));
+            }
+        }
+        List<GroupKey> sortedSeriesKeys = new ArrayList<>(seriesMappedByGroupKey.keySet());
+        Collections.sort(sortedSeriesKeys);
+        for (GroupKey seriesKey : sortedSeriesKeys) {
+            chart.addSeries(seriesMappedByGroupKey.get(seriesKey), false, false);
+        }
+    }
 
 
     @Override
     protected void internalShowResults(QueryResultDTO<?> r) {
         result = r;
+        createAndAddSeriesToChart();
         for (Entry<GroupKey, ?> resultEntry : result.getResults().entrySet()) {
             @SuppressWarnings("unchecked")
             AveragePairWithStats<Number> value = (AveragePairWithStats<Number>) resultEntry.getValue();
-            
             Point point = new Point(value.getAverage().getA(), value.getAverage().getB());
             point.setName(resultEntry.getKey().asString());
-            series.add(point);
+            seriesMappedByGroupKey.get(groupKeyToSeriesKey(resultEntry.getKey()))
+            .addPoint(point, false, false, false);
+
         }
         chart.getXAxis().setAxisTitleText(result.getResultSignifier());
         
-        chart.addSeries(chart.createSeries().setName("Test").setType(Type.SCATTER).setPoints(series.toArray(new Point[series.size()])));
+//        chart.addSeries(chart.createSeries().setName("Test").setType(Type.SCATTER).setPoints(series.toArray(new Point[series.size()])));
+        chart.redraw();
     }
 
 
