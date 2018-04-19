39d9f78e1323f18be94bed84784497846ca2c9c4
Benjamin Barth <benjamin.barth@oio.de>
2018-04-19 15:41:59 +0200

    bug4436 Added some JavaDoc to GetManeuversForCompetitorsAction

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/actions/GetManeuversForCompetitorsAction.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/actions/GetManeuversForCompetitorsAction.java
@@ -11,6 +11,10 @@ import com.sap.sailing.gwt.ui.shared.ManeuverDTO;
 import com.sap.sse.common.TimeRange;
 import com.sap.sse.gwt.client.async.AsyncAction;
 
+/**
+ * {@link AsyncAction Asynchronous action} to load {@link ManeuverDTO maneuver information} for a number of
+ * {@link CompetitorWithBoatDTO competitors} within the respectively associated {@link TimeRange time ranges}.
+ */
 public class GetManeuversForCompetitorsAction implements AsyncAction<Map<CompetitorWithBoatDTO, List<ManeuverDTO>>> {
 
     private final SailingServiceAsync sailingService;
