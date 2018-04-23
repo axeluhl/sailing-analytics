70683976415052887465a09f47ab921ab85b4b5a
Kai Börnert <kai.boernert@oio.de>
2018-04-23 14:04:09 +0200

    bug4523 initial video flipping logic

--- java/com.sap.sse.common/src/com/sap/sse/common/media/MimeType.java
+++ java/com.sap.sse.common/src/com/sap/sse/common/media/MimeType.java
@@ -6,7 +6,7 @@ public enum MimeType {
             MediaSubType.quicktime), mp3(MediaType.audio, MediaSubType.mpeg), ogg(MediaType.audio, MediaSubType.ogg), aac(
             MediaType.audio, MediaSubType.aac), webm(MediaType.video, MediaSubType.webm), youtube(MediaType.video,
             MediaSubType.youtube), vimeo(MediaType.video, MediaSubType.vimeo), image(MediaType.image,
-            MediaSubType.unknown), unknown(MediaType.unknown, MediaSubType.unknown), mp4panorama(MediaType.video, MediaSubType.mp4);
+            MediaSubType.unknown), unknown(MediaType.unknown, MediaSubType.unknown), mp4panorama(MediaType.video, MediaSubType.mp4), mp4panoramaflip(MediaType.video, MediaSubType.mp4);
 
     public final MediaType mediaType;
     public final MediaSubType mediaSubType;
@@ -17,7 +17,11 @@ public enum MimeType {
     }
     
     public boolean isPanorama(){
-        return this == mp4panorama;
+        return this == mp4panorama || this == mp4panoramaflip;
+    }
+    
+    public boolean isFlippedPanorama(){
+        return this == mp4panoramaflip;
     }
 
     @Override
