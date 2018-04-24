c038d4628ca64bf9e4daea60f2fadfa70465d56c
Steffen Schaefer <steffen.schaefer@sap.com>
2018-04-24 16:43:13 +0200

    Bug 4339: The Leaderboard settings button can potentially scroll away

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/HostPage.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/HostPage.java
@@ -3,6 +3,7 @@ package com.sap.sailing.selenium.pages;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -79,5 +80,9 @@ public abstract class HostPage extends PageObject {
     
     public String getCurrentUrl() {
         return driver.getCurrentUrl();
+    }
+
+    protected void scrollToTop() {
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0);");
     }
 }
