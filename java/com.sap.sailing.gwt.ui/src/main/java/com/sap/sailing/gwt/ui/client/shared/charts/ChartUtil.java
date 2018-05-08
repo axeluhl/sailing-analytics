5cfe71eb0b9fdc939a6778585aad95cb0ae482f4
Kai Börnert <kai.boernert@oio.de>
2018-05-08 11:28:24 +0200

    bug4562 made text clickable again, cleaned up handler code

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/ChartUtil.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/charts/ChartUtil.java
@@ -5,8 +5,6 @@ import org.moxieapps.gwt.highcharts.client.Legend;
 import org.moxieapps.gwt.highcharts.client.Series;
 import org.moxieapps.gwt.highcharts.client.events.SeriesCheckboxClickEvent;
 import org.moxieapps.gwt.highcharts.client.events.SeriesCheckboxClickEventHandler;
-import org.moxieapps.gwt.highcharts.client.events.SeriesLegendItemClickEvent;
-import org.moxieapps.gwt.highcharts.client.events.SeriesLegendItemClickEventHandler;
 import org.moxieapps.gwt.highcharts.client.plotOptions.PlotOptions;
 import org.moxieapps.gwt.highcharts.client.plotOptions.SeriesPlotOptions;
 
@@ -20,33 +18,16 @@ public class ChartUtil {
      */
     static protected void useCheckboxesToShowAndHide(final Chart chart) {
         chart.setLegend(new Legend().setEnabled(true).setBorderWidth(0).setSymbolPadding(25)); // make room for checkbox
-        chart.setSeriesPlotOptions(new SeriesPlotOptions().setSeriesCheckboxClickEventHandler(new SeriesCheckboxClickEventHandler() {
+        chart.setSeriesPlotOptions(new SeriesPlotOptions().setShowCheckbox(true)
+                .setSeriesCheckboxClickEventHandler(new SeriesCheckboxClickEventHandler() {
                     @Override
                     public boolean onClick(SeriesCheckboxClickEvent seriesCheckboxClickEvent) {
+                        // we want to change the visiblity not the selection state, so own handler is necessary
                         Series series = chart.getSeries(seriesCheckboxClickEvent.getSeriesId());
-                        if (seriesCheckboxClickEvent.isChecked()) {
-                            ChartUtil.onSeriesSelectionChanged(series, true);
-                        } else {
-                            ChartUtil.onSeriesSelectionChanged(series, false);
-                        }
+                        series.setVisible(seriesCheckboxClickEvent.isChecked());
                         return false; // don't toggle the select state of the series
                     }
-                }).setShowCheckbox(true).
-                setSeriesLegendItemClickEventHandler(new SeriesLegendItemClickEventHandler() {
-                    @Override
-                    public boolean onClick(SeriesLegendItemClickEvent seriesLegendItemClickEvent) {
-                        // disable toggling visibility by clicking the legend item; force user to use checkbox instead
-                        return false;
-                    }
                 }));
-    }
-
-    static protected void onSeriesSelectionChanged(Series series, boolean selected) {
-        if (selected) {
-            series.show();
-        } else {
-            series.hide();
-        }
     }
 
 }
