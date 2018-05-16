dce75b2f4acfc7ef4a3bdbec288cd56e0dc15f5c
Axel Uhl <axel.uhl@sap.com>
2018-05-16 14:23:00 +0200

    bug 4448: cleaned up AbstractRoles, removing regatta-specific stuff. Race-specific stuff may remain
    Change-Id: I2b1dd2f09502fb06423dd4b773e766761b753b2c

--- java/com.sap.sse.security.common/src/com/sap/sse/security/shared/AbstractRoles.java
+++ java/com.sap.sse.security.common/src/com/sap/sse/security/shared/AbstractRoles.java
@@ -8,34 +8,6 @@ import java.util.UUID;
 public enum AbstractRoles {
     spectator(UUID.fromString("244cb84c-2b8a-4557-b175-db963072cfbc"), "spectator", Collections.<String>emptyList()),
     moderator(UUID.fromString("91c88a11-c977-4c24-b0a6-15bac0525764"), "moderator", asList("race:live_replay")),
-    sailor(UUID.fromString("eb774a5c-9350-4094-97d5-6b15ab49feac"), "sailor", Collections.<String>emptyList()),
-    coach(UUID.fromString("8e72e67d-cc65-4de4-a985-03a7a3c48e93"), "coach", Collections.<String>emptyList()),
-    eventmanager(UUID.fromString("7920a761-1378-4c98-9129-0fbe586c5cd7"), "eventmanager",
-            asList(
-                "race_board:*",
-                
-                /* AdminConsole:
-                Permission.MANAGE_ALL_COMPETITORS,
-                Permission.MANAGE_COURSE_LAYOUT,
-                Permission.MANAGE_DEVICE_CONFIGURATION,
-                Permission.MANAGE_EVENTS,
-                Permission.MANAGE_IGTIMI_ACCOUNTS,
-                Permission.MANAGE_LEADERBOARD_GROUPS,
-                Permission.MANAGE_LEADERBOARDS,
-                Permission.MANAGE_LEADERBOARD_RESULTS,
-                Permission.MANAGE_MEDIA,
-                Permission.MANAGE_RACELOG_TRACKING,
-                Permission.MANAGE_REGATTAS,
-                Permission.MANAGE_RESULT_IMPORT_URLS,
-                Permission.MANAGE_STRUCTURE_IMPORT_URLS,
-                Permission.MANAGE_TRACKED_RACES,
-                Permission.MANAGE_WIND,*/
-                
-                // back-end:
-                "event:*",
-                "regatta:*",
-                "leaderboard:*",
-                "leaderboard_group:*")),
     mediaeditor(UUID.fromString("2e22583b-c9c1-4ee4-8231-e8e320a8a411"), "mediaeditor", asList("media:*"));
     
     private final UUID id;
