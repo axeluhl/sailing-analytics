1205a02356f3d3210d27534438247ac092c502b1
Maximilian Groß <maximilian.gross@sap.com>
2018-06-14 12:20:24 +0200

    refactoring to match the project

---
+++ java/com.sap.sse.datamining.ui.test/test/com/sap/sse/datamining/ui/test/TestSSEDataMiningUiSuite.java
@@ -0,0 +1,14 @@
+package com.sap.sse.datamining.ui.test;
+
+import com.sap.sse.datamining.ui.test.client.TestSSEDataMiningUi;
+import com.google.gwt.junit.tools.GWTTestSuite;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class TestSSEDataMiningUiSuite extends GWTTestSuite {
+  public static Test suite() {
+    TestSuite suite = new TestSuite("Tests for TestSSEDataMiningUi");
+    suite.addTestSuite(TestSSEDataMiningUi.class);
+    return suite;
+  }
+}
--- java/com.sap.sse.datamining.ui.test/test/com/sap/sse/datamining/ui/test/client/TestSSEDataMiningUi.java
+++ java/com.sap.sse.datamining.ui.test/test/com/sap/sse/datamining/ui/test/client/TestSSEDataMiningUi.java
@@ -5,13 +5,13 @@ import com.google.gwt.junit.client.GWTTestCase;
 /**
  * GWT JUnit tests must extend GWTTestCase.
  */
-public class SSEDataMiningUiTest extends GWTTestCase {
+public class TestSSEDataMiningUi extends GWTTestCase {
 
     /**
      * Must refer to a valid module that sources this class.
      */
     public String getModuleName() {
-        return "com.sap.sse.datamining.ui.test.SSEDataMiningUiJUnit";
+        return "com.sap.sse.datamining.ui.test.TestSSEDataMiningUiJUnit";
     }
 
     public void testRegisterResultsPresenter() {
