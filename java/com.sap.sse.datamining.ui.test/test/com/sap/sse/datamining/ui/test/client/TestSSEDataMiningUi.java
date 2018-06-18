e6c00db523e7cea71ded2d48be77341bcad3d438
Maximilian Groß <maximilian.gross@sap.com>
2018-06-18 13:41:57 +0200

    added test cases for the registration of ResultsPresenter and their correct call mechanism

--- java/com.sap.sse.datamining.ui.test/test/com/sap/sse/datamining/ui/test/client/TestSSEDataMiningUi.java
+++ java/com.sap.sse.datamining.ui.test/test/com/sap/sse/datamining/ui/test/client/TestSSEDataMiningUi.java
@@ -1,6 +1,15 @@
 package com.sap.sse.datamining.ui.test.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.junit.client.GWTTestCase;
+import com.sap.sse.datamining.ui.client.presentation.MultiResultsPresenter;
+import com.sap.sse.datamining.ui.client.presentation.PlainResultsPresenter;
+import com.sap.sse.datamining.ui.client.presentation.ResultsChart;
+import com.sap.sse.datamining.ui.client.presentation.TabbedResultsPresenter;
+import com.sap.sse.datamining.ui.test.client.presentation.DummyQueryResultDTO;
+import com.sap.sse.datamining.ui.test.client.presentation.DummyResultsPresenter;
 
 /**
  * GWT JUnit tests must extend GWTTestCase.
@@ -14,7 +23,46 @@ public class TestSSEDataMiningUi extends GWTTestCase {
         return "com.sap.sse.datamining.ui.test.TestSSEDataMiningUiJUnit";
     }
 
-    public void testRegisterResultsPresenter() {
-        assertTrue(true);
+    /**
+     * Tests the mechanism {@link TabbedResultsPresenter#registerResultPresenter(Class, ResultsPresenter)}.
+     */
+    public void testAvoidDuplicateResultTypeRegistration() {
+        TabbedResultsPresenter presenter = new TabbedResultsPresenter(null, null, null);
+
+        presenter.registerResultPresenter(String.class, new PlainResultsPresenter(null, null));
+        presenter.registerResultPresenter(Double.class, new MultiResultsPresenter(null, null, null));
+        try {
+            presenter.registerResultPresenter(String.class, new ResultsChart(null, null, true, null));
+            fail();
+        } catch (IllegalStateException e) {
+            assertTrue(true);
+        }
+    }
+
+    /**
+     * Tests the mechanism {@link TabbedResultsPresenter#showResult(QueryResultDTO)}.
+     */
+    public void testShowResultWithCorrectResultsPresenter() {
+        TabbedResultsPresenter presenter = new TabbedResultsPresenter(null, null, null);
+        List<DummyResultsPresenter> executedPresenter = new ArrayList<>();
+
+        DummyResultsPresenter stringResultsPresenter = new DummyResultsPresenter(executedPresenter);
+        DummyResultsPresenter doubleResultsPresenter = new DummyResultsPresenter(executedPresenter);
+
+        presenter.registerResultPresenter(String.class, stringResultsPresenter);
+        presenter.registerResultPresenter(Double.class, doubleResultsPresenter);
+
+        presenter.showResult(new DummyQueryResultDTO<>(String.class));
+        assertTrue(executedPresenter.size() == 1);
+        assertEquals(executedPresenter.get(0), stringResultsPresenter);
+
+        presenter.showResult(new DummyQueryResultDTO<>(Double.class));
+        assertTrue(executedPresenter.size() == 2);
+        assertEquals(executedPresenter.get(1), doubleResultsPresenter);
+
+        presenter.showResult(new DummyQueryResultDTO<>(Number.class));
+        assertTrue(executedPresenter.size() == 2);
+        assertEquals(executedPresenter.get(0), stringResultsPresenter);
+        assertEquals(executedPresenter.get(1), doubleResultsPresenter);
     }
 }
