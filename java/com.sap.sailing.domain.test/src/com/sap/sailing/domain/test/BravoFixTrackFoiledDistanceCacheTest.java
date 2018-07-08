ad698cdf6a02a2557c59f7e074fb5d27ccd90333
Axel Uhl <axel.uhl@sap.com>
2018-07-09 00:41:49 +0200

    an always failing test case for bug4629
    Change-Id: I83cb2529fcb0fa31ab4292d2e34da19838a4e1b9

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/BravoFixTrackFoiledDistanceCacheTest.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/BravoFixTrackFoiledDistanceCacheTest.java
@@ -7,6 +7,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,6 +30,7 @@ import com.sap.sailing.domain.tracking.DynamicBravoFixTrack;
 import com.sap.sailing.domain.tracking.impl.BravoFixTrackImpl;
 import com.sap.sailing.domain.tracking.impl.DynamicGPSFixMovingTrackImpl;
 import com.sap.sailing.domain.tracking.impl.TimeRangeCache;
+import com.sap.sse.common.Distance;
 import com.sap.sse.common.TimePoint;
 import com.sap.sse.common.impl.DegreeBearingImpl;
 import com.sap.sse.common.impl.MillisecondsTimePoint;
@@ -56,6 +61,9 @@ public class BravoFixTrackFoiledDistanceCacheTest {
         private static Map<String, TimeRangeCacheWithParallelTestSupport<?>> caches = new HashMap<>();
         private int callsToCache;
         private int callsToInvalidateAllAtOrLaterThan;
+        private CyclicBarrier cacheBarrier;
+        private CyclicBarrier invalidateBarrier;
+        private CyclicBarrier cacheInformBarrier;
         
         public TimeRangeCacheWithParallelTestSupport(String nameForLockLogging) {
             super(nameForLockLogging);
@@ -72,10 +80,27 @@ public class BravoFixTrackFoiledDistanceCacheTest {
         public void invalidateAllAtOrLaterThan(TimePoint timePoint) {
             super.invalidateAllAtOrLaterThan(timePoint);
             callsToInvalidateAllAtOrLaterThan++;
+            if (invalidateBarrier != null) {
+                try {
+                    invalidateBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
 
         @Override
         public void cache(TimePoint from, TimePoint to, T result) {
+            try {
+                if (cacheInformBarrier != null) {
+                    cacheInformBarrier.await();
+                }
+                if (cacheBarrier != null) {
+                        cacheBarrier.await();
+                }
+            } catch (InterruptedException | BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
             super.cache(from, to, result);
             callsToCache++;
         }
@@ -86,6 +111,33 @@ public class BravoFixTrackFoiledDistanceCacheTest {
 
         public int getCallsToInvalidateAllAtOrLaterThan() {
             return callsToInvalidateAllAtOrLaterThan;
+        }
+
+        public void waitForCacheInvalidation() throws InterruptedException, BrokenBarrierException {
+            invalidateBarrier.await();
+            invalidateBarrier = null;
+        }
+
+        public void allowWaitingForCacheInvalidation() {
+            invalidateBarrier = new CyclicBarrier(2);
+        }
+
+        public void letFoilingDistanceCacheContinueWithCaching() throws InterruptedException, BrokenBarrierException {
+            cacheBarrier.await();
+            cacheBarrier = null;
+        }
+
+        public void letFoilingDistanceCacheWaitBeforeCaching() {
+            cacheBarrier = new CyclicBarrier(2);
+        }
+
+        public void letFoilingDistanceCacheInformUsBeforeCaching() {
+            cacheInformBarrier = new CyclicBarrier(2);
+        }
+        
+        public void waitForCacheToBeEntered() throws InterruptedException, BrokenBarrierException {
+            cacheInformBarrier.await();
+            cacheInformBarrier = null;
         }
     }
     
@@ -111,7 +163,7 @@ public class BravoFixTrackFoiledDistanceCacheTest {
     }
     
     @Test
-    public void testDistanceSpentFoiling() {
+    public void testDistanceSpentFoiling() throws InterruptedException, ExecutionException, BrokenBarrierException {
         assertEquals(new NauticalMileDistance(2./3600.).getMeters(), track.getDistanceSpentFoiling(t(1000l), t(3000l)).getMeters(), 0.01);
         assertEquals(1, foilingDistanceCache.getCallsToCache());
         assertEquals(6, foilingDistanceCache.getCallsToInvalidateAllAtOrLaterThan()); // the three sensor and three GPS fixes
@@ -121,6 +173,32 @@ public class BravoFixTrackFoiledDistanceCacheTest {
         assertEquals(7, foilingDistanceCache.getCallsToInvalidateAllAtOrLaterThan()); // now one more sensor fix
         assertEquals(new NauticalMileDistance(2./3600.).getMeters(), track.getDistanceSpentFoiling(t(1000l), t(3000l)).getMeters(), 0.01);
         assertEquals(2, foilingDistanceCache.getCallsToCache()); // had to be re-calculated and then was expected to be put to cache
+        gpsTrack.add(createGPSFix(4000l, 3./3600./60., 0, 0, 1));
+        assertEquals(8, foilingDistanceCache.getCallsToInvalidateAllAtOrLaterThan()); // now one more GPS fix
+        
+        // now modify the cache such that it will stop before updating the cache
+        foilingDistanceCache.letFoilingDistanceCacheWaitBeforeCaching();
+        foilingDistanceCache.letFoilingDistanceCacheInformUsBeforeCaching();
+        FutureTask<Distance> getDistanceFuture = new FutureTask<>(()->track.getDistanceSpentFoiling(t(1000l), t(4000l)));
+        new Thread(getDistanceFuture).start();
+        foilingDistanceCache.waitForCacheToBeEntered();
+        // now insert another sensor fix at t(4000l) that will have to invalidate the result of the previous request;
+        // adding the fix will trigger a cache invalidation; the TimeRangeCache.cache(...) call caused by the query above
+        // is still blocked:
+        FutureTask<Boolean> addFuture = new FutureTask<>(()->track.add(createFix(4000l, /* rideHeightPort */ 0.6, /* rideHeightStarboard */ 0.6, /* heel */ 10., /* pitch */ 5.)));
+        foilingDistanceCache.allowWaitingForCacheInvalidation();
+        new Thread(addFuture).start();
+        foilingDistanceCache.waitForCacheInvalidation();
+        // now that the cache invalidation has happened synchronously, let the request from above continue with its call to cache(...):
+        foilingDistanceCache.letFoilingDistanceCacheContinueWithCaching();
+        // wait until the caching has completed:
+        getDistanceFuture.get();
+        // and until adding the fixes has completed
+        addFuture.get();
+        // Now for the getDistanceFuture, either it has delivered the new value already because it was passed by
+        // the addition of the fix, or it delivered the old value, but then the cache entry will have been invalidated.
+        // Now ask again; if the invalidation worked correctly, we should get a greater result now:
+        assertEquals(new NauticalMileDistance(3./3600.).getMeters(), track.getDistanceSpentFoiling(t(1000l), t(4000l)).getMeters(), 0.01);
     }
 
     private BravoExtendedFixImpl createFix(long timePointAsMillis, Double rideHeightPort, Double rideHeightStarboard, Double heel, Double pitch) {
