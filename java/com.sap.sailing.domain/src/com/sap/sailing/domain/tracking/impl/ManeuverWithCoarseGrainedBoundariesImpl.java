653f391f03310fd2b93b9fc92b814a3f6f853948
Axel Uhl <axel.uhl@sap.com>
2018-07-09 22:40:52 +0200

    added comments to null parameter values
    Change-Id: Ibbceb30d2ad2007a00eb97b36fbbc5da2b3387ab

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/ManeuverWithCoarseGrainedBoundariesImpl.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/impl/ManeuverWithCoarseGrainedBoundariesImpl.java
@@ -26,7 +26,9 @@ public class ManeuverWithCoarseGrainedBoundariesImpl extends ManeuverImpl {
 
     public ManeuverWithCoarseGrainedBoundariesImpl(ManeuverType type, Tack newTack, Position position,
             TimePoint timePoint, ManeuverCurveBoundaries maneuverBoundaries) {
-        super(type, newTack, position, timePoint, maneuverBoundaries, maneuverBoundaries, Math.abs(maneuverBoundaries.getDirectionChangeInDegrees()), null, null);
+        super(type, newTack, position, timePoint, maneuverBoundaries, maneuverBoundaries,
+                Math.abs(maneuverBoundaries.getDirectionChangeInDegrees()), /* mark passing */ null,
+                /* maneuver loss */ null);
     }
 
     @Override
