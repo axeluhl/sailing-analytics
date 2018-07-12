f63e5923dcc7de1793f51668cfa491c0c46b754f
D067890 <j.rendl@sap.com>
2018-07-12 10:25:15 +0200

    bug4668: updated deserializer and serializer to turn tests green

--- java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/serialization/racelog/impl/RaceLogEventSerializer.java
+++ java/com.sap.sailing.server.gateway.serialization.shared.android/src/com/sap/sailing/server/gateway/serialization/racelog/impl/RaceLogEventSerializer.java
@@ -104,8 +104,8 @@ public class RaceLogEventSerializer implements JsonSerializer<RaceLogEvent>, Rac
     private final JsonSerializer<RaceLogEvent> suppressedMarkPassingsEventSerializer;
     private final JsonSerializer<RaceLogEvent> dependentStartTimeEventSerializer;
     private final JsonSerializer<RaceLogEvent> startOfTrackingEventSerializer;
-    private final JsonSerializer<RaceLogEvent> endOfTrackingEventSerializer;
     private final JsonSerializer<RaceLogEvent> useCompetitorsFromRaceLogEventSerializer;
+    private final JsonSerializer<RaceLogEvent> endOfTrackingEventSerializer;
 
     private JsonSerializer<RaceLogEvent> chosenSerializer;
 
@@ -132,8 +132,8 @@ public class RaceLogEventSerializer implements JsonSerializer<RaceLogEvent>, Rac
             JsonSerializer<RaceLogEvent> suppressedMarkPassingsSerializer,
             JsonSerializer<RaceLogEvent> dependentStartTimeEventSerializer,
             JsonSerializer<RaceLogEvent> startOfTrackingEventSerializer,
-            JsonSerializer<RaceLogEvent> endOfTrackingEventSerializer,
-            JsonSerializer<RaceLogEvent> useCompetitorsFromRaceLogEventSerializer) {
+            JsonSerializer<RaceLogEvent> useCompetitorsFromRaceLogEventSerializer,
+            JsonSerializer<RaceLogEvent> endOfTrackingEventSerializer) {
         this.flagEventSerializer = flagEventSerializer;
         this.startTimeSerializer = startTimeSerializer;
         this.raceStatusSerializer = raceStatusSerializer;
