e206133d90d290f100d8e57aa75d5117cb525dec
Benjamin Barth <benjamin.barth@oio.de>
2018-07-17 17:11:13 +0200

    bug4652 Adjusted shown "locations" in the header of series pages

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/seriesheader/SeriesHeader.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/seriesheader/SeriesHeader.java
@@ -3,8 +3,6 @@ package com.sap.sailing.gwt.home.desktop.partials.seriesheader;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.SpanElement;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Event;
@@ -13,15 +11,15 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Widget;
-import com.sap.sailing.gwt.home.communication.event.EventMetadataDTO;
+import com.sap.sailing.gwt.home.communication.event.EventAndLeaderboardReferenceWithStateDTO;
 import com.sap.sailing.gwt.home.communication.event.EventState;
 import com.sap.sailing.gwt.home.communication.fakeseries.EventSeriesViewDTO;
 import com.sap.sailing.gwt.home.desktop.partials.sharing.SharingButtons;
 import com.sap.sailing.gwt.home.desktop.partials.sharing.SharingMetadataProvider;
+import com.sap.sailing.gwt.home.desktop.places.event.regatta.overviewtab.RegattaOverviewPlace;
 import com.sap.sailing.gwt.home.desktop.places.fakeseries.SeriesView;
 import com.sap.sailing.gwt.home.desktop.places.fakeseries.SeriesView.Presenter;
 import com.sap.sailing.gwt.home.shared.app.PlaceNavigation;
-import com.sap.sailing.gwt.home.shared.places.event.EventDefaultPlace;
 import com.sap.sailing.gwt.home.shared.resources.SharedHomeResources;
 import com.sap.sailing.gwt.home.shared.utils.LabelTypeUtil;
 import com.sap.sailing.gwt.ui.client.StringMessages;
@@ -78,25 +76,23 @@ public class SeriesHeader extends Composite {
         eventName.setInnerText(series.getDisplayName());
         LabelTypeUtil.renderLabelType(eventState, series.getState().getStateMarker());
         
-        for (EventMetadataDTO eventOfSeries : series.getEventsAscending()) {
+        for (EventAndLeaderboardReferenceWithStateDTO eventOfSeries : series.getEventsAndRegattasOfSeriesAscending()) {
             if (eventOfSeries.getState() == EventState.PLANNED) {
-                InlineLabel eventLabel = new InlineLabel(eventOfSeries.getLocationOrVenue());
+                InlineLabel eventLabel = new InlineLabel(eventOfSeries.getDisplayName());
                 eventLabel.addStyleName(SeriesHeaderResources.INSTANCE.css().eventheader_intro_details_item());
                 eventLabel.addStyleName(SeriesHeaderResources.INSTANCE.css().eventheader_intro_details_item_inactive());
                 venues.add(eventLabel);
             } else {
-                Anchor eventAnchor = new Anchor(eventOfSeries.getLocationOrVenue());
+                Anchor eventAnchor = new Anchor(eventOfSeries.getDisplayName());
                 eventAnchor.addStyleName(SeriesHeaderResources.INSTANCE.css().eventheader_intro_details_item());
-                final PlaceNavigation<EventDefaultPlace> eventNavigation = presenter.getEventNavigation(eventOfSeries.getId());
-                eventAnchor.setHref(eventNavigation.getTargetUrl());
+                final PlaceNavigation<RegattaOverviewPlace> regattaNavigation = presenter
+                        .getRegattaNavigation(eventOfSeries.getId(), eventOfSeries.getLeaderboardName());
+                eventAnchor.setHref(regattaNavigation.getTargetUrl());
                 venues.add(eventAnchor);
-                eventAnchor.addClickHandler(new ClickHandler() {
-                    @Override
-                    public void onClick(ClickEvent event) {
-                        if(LinkUtil.handleLinkClick(Event.as(event.getNativeEvent()))) {
-                            event.preventDefault();
-                            eventNavigation.goToPlace();
-                        }
+                eventAnchor.addClickHandler(event -> {
+                    if (LinkUtil.handleLinkClick(Event.as(event.getNativeEvent()))) {
+                        event.preventDefault();
+                        regattaNavigation.goToPlace();
                     }
                 });
             }
