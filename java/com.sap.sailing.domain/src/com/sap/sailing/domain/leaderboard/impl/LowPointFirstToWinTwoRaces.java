2580435a5247a441de95d7e2261d69c9615bc4b9
Steffen Schaefer <steffen.schaefer@sap.com>
2018-07-19 10:36:45 +0200

    Bug 4661: Extended the JavaDocs of LowPointFirstToWinTwoRaces to explain all special rules compared to the default LowPoint rules

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/leaderboard/impl/LowPointFirstToWinTwoRaces.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/leaderboard/impl/LowPointFirstToWinTwoRaces.java
@@ -4,9 +4,19 @@ import com.sap.sailing.domain.base.RaceColumn;
 import com.sap.sailing.domain.common.ScoringSchemeType;
 
 /**
- * This class is a derivation of the LowPointScoring scheme. The first participant in the medal series, that wins two
- * races is ranked as the first. The participant that has the score 1 in the preSeries carry column only must reach one
- * additional victory. This Scheme will change the default scoring factor for medal races to 1, usually 2 is used
+ * This class is a derivation of the LowPointScoring scheme. The following additional rules apply:
+ * <ol>
+ * <li>The first participant in the medal series, that wins two races is ranked as the first.</li>
+ * <li>Having 1 point in the carry forward column of the medal series explicitly counts as an additional win which
+ * requires the winner of the qualification to only need one additional win in the medals.</li>
+ * <li>If none of the participants in the medal series reached the target amount of 2 wins yet, the ranking is done
+ * based on the points only.</li>
+ * <li>If there is a carry forward column in the medal series, the carry forward points are used as the primary tie
+ * breaking criteria in case two participants in the medal series have the same overall score.</li>
+ * <li>If there is no carry forward column in the medal series or the carry forward score is not sufficient, the points
+ * of the last medal race are used as a secondary tie breaking criteria.</li>
+ * <li>This Scheme will change the default scoring factor for medal races to 1, usually 2 is used</li>
+ * <ol>
  */
 public class LowPointFirstToWinTwoRaces extends LowPoint {
     private static final long serialVersionUID = 7072175334160798617L;
