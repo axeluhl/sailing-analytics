c044382a21101511c44ff10be36408b3d5fba8b5
Kai Börnert <kai.boernert@oio.de>
2018-07-30 16:05:37 +0200

    bug4682 improved deprecation explanation

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/media/MediaTrack.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/media/MediaTrack.java
@@ -26,6 +26,10 @@ public class MediaTrack implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Deprecated
+    /**
+     * Deprecated, as the server only used undefined in all cases. Left in, to not break serialisation/storage
+     *
+     */
     public enum Status {
         UNDEFINED('?'), CANNOT_PLAY('-'), NOT_REACHABLE('#'), REACHABLE('+');
 
