9e58407d674137f444690fd075e98d5e4139a636
Steffen Jacobs <steffen.jacobs@oio.de>
2018-08-03 10:21:27 +0200

    Bug 4202: fixed logical error which resulted in visual glitches

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/racemap/RaceMap.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/racemap/RaceMap.java
@@ -1224,7 +1224,7 @@ public class RaceMap extends AbstractCompositeComponent<RaceMapSettings> impleme
     private void updateBoatPositions(final Date newTime, final long transitionTimeInMillis,
             final Map<CompetitorDTO, Boolean> hasTailOverlapForCompetitor,
             final Iterable<CompetitorDTO> competitorsToShow, Map<CompetitorDTO, List<GPSFixDTOWithSpeedWindTackAndLegType>> boatData, boolean updateTailsOnly) {
-        if (zoomingAnimationsInProgress == 0 || currentlyDragging) {
+        if (zoomingAnimationsInProgress == 0 && !currentlyDragging) {
             fixesAndTails.updateFixes(boatData, hasTailOverlapForCompetitor, RaceMap.this, transitionTimeInMillis);
             showBoatsOnMap(newTime, transitionTimeInMillis,
                     /* re-calculate; it could have changed since the asynchronous request was made: */
@@ -1296,7 +1296,7 @@ public class RaceMap extends AbstractCompositeComponent<RaceMapSettings> impleme
     }
        
     private void showCourseMarksOnMap(CoursePositionsDTO courseDTO, long transitionTimeInMillis) {
-        if (zoomingAnimationsInProgress == 0 || currentlyDragging) {
+        if (zoomingAnimationsInProgress == 0 && !currentlyDragging) {
             if (map != null && courseDTO != null) {
                 WaypointDTO endWaypointForCurrentLegNumber = null;
                 if (courseDTO.currentLegNumber > 0 && courseDTO.currentLegNumber <= courseDTO.totalLegsCount) {
