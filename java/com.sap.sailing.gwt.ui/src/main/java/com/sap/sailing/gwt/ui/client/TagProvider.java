97f12c288c342ae92b517a9d6b5c34ca9376a6dd
D067890 <j.rendl@sap.com>
2018-08-07 13:34:59 +0200

    bug4104: added java doc for tag provider

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/TagProvider.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/TagProvider.java
@@ -5,24 +5,45 @@ import java.util.List;
 import com.sap.sailing.gwt.ui.shared.TagDTO;
 import com.sap.sse.common.filter.Filter;
 import com.sap.sse.common.filter.FilterSet;
-
+/**
+ * 
+ * @author D067890
+ * Used to store tags and tagFilterSets
+ *
+ */
 public interface TagProvider {
 
+    /**
+     * adds new tags, call {@link #updateFilteredTags() updateFilteredTags} afterwards so list of filtered tags can contain new tags
+     * @param list of tags which shall be added
+     */
     void addTags(List<TagDTO> tags);
-
+    
+    
+    /**
+     * adds a new tag, call {@link #updateFilteredTags() updateFilteredTags} afterwards so list of filtered tag can contain new tags
+     * @param tag which shall be added
+     */
     void addTag(TagDTO tag);
 
+    /**
+     * @return all filtered tags
+     */
     List<TagDTO> getFilteredTags();
 
     int getFilteredTagsListSize();
 
     void setTagsFilterSet(FilterSet<TagDTO, Filter<TagDTO>> tagsFilterSet);
 
-    FilterSet<TagDTO, Filter<TagDTO>> getTagsFilterSet();
-
+    FilterSet<TagDTO, Filter<TagDTO>> getTagsFilterSet();  
+    
+    /**
+     * filter all tags by tagsFilterSet
+     */
     void updateFilteredTags();
 
+    /**
+     * @return all tags without filtering
+     */
     List<TagDTO> getAllTags();
-
-
 }
