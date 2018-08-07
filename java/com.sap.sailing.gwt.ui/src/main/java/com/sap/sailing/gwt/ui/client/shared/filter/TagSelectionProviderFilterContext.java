fc410e78b397a5332c0501fa532e63bb1b85abf3
D067890 <j.rendl@sap.com>
2018-08-07 11:30:50 +0200

    bug4104: removed unused tag filter

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/shared/filter/TagSelectionProviderFilterContext.java
+++
@@ -1,9 +0,0 @@
-package com.sap.sailing.gwt.ui.client.shared.filter;
-
-import com.sap.sailing.gwt.ui.client.TagProvider;
-
-public interface TagSelectionProviderFilterContext {
-    TagProvider getTagSelectionProvider();
-    
-    void setTagSelectionProvider(TagProvider tagSelectionProvider); 
-}
