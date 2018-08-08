2a041c008e5ce6861a63a2af2108efc22d268b3e
Steffen Jacobs <steffen.jacobs@oio.de>
2018-08-08 19:03:50 +0200

    Bug 4211: added EditableCompetitorSuggestedMultiSelection widget

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/user/profile/sailorprofile/EditSailorProfile.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/places/user/profile/sailorprofile/EditSailorProfile.java
@@ -1,23 +1,16 @@
 package com.sap.sailing.gwt.home.shared.places.user.profile.sailorprofile;
 
-import java.util.Collection;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasEnabled;
 import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.common.client.SharedResources;
-import com.sap.sailing.gwt.home.communication.event.SimpleCompetitorWithIdDTO;
-import com.sap.sailing.gwt.home.shared.partials.multiselection.SuggestedMultiSelection;
-import com.sap.sailing.gwt.home.shared.partials.multiselection.SuggestedMultiSelection.NotificationCallback;
-import com.sap.sailing.gwt.home.shared.partials.multiselection.SuggestedMultiSelectionCompetitorDataProvider;
+import com.sap.sailing.gwt.home.shared.partials.editable.EditableCompetitorSuggestedMultiSelection;
 import com.sap.sailing.gwt.home.shared.places.user.profile.sailorprofile.domain.SailorProfileEntry;
 import com.sap.sailing.gwt.ui.client.FlagImageResolver;
-import com.sap.sailing.gwt.ui.client.StringMessages;
 
 /**
  * Implementation of {@link SharedSailorProfileView} where users can change their preferred selections and
@@ -39,13 +32,12 @@ public class EditSailorProfile extends Composite implements SharedSailorProfileV
     @UiField
     SharedResources res;
     @UiField(provided = true)
-    SuggestedMultiSelection<SimpleCompetitorWithIdDTO> competitorSelectionUi;
+    EditableCompetitorSuggestedMultiSelection competitorSelectionUi;
     @UiField
-    DivElement title;
+    DivElement titleUi;
 
     public EditSailorProfile(SharedSailorProfileView.Presenter presenter, FlagImageResolver flagImageResolver) {
-        competitorSelectionUi = new CompetitorDisplayImpl(presenter.getFavoriteCompetitorsDataProvider(),
-                flagImageResolver).selectionUi;
+        competitorSelectionUi = new EditableCompetitorSuggestedMultiSelection(presenter, flagImageResolver);
         initWidget(uiBinder.createAndBindUi(this));
         // TODO hide notificationsTextUi if the user's mail address is already verified
     }
@@ -55,36 +47,8 @@ public class EditSailorProfile extends Composite implements SharedSailorProfileV
         competitorSelectionUi.getElement().getParentElement().removeClassName(res.mediaCss().column());
     }
 
-    private class CompetitorDisplayImpl implements SuggestedMultiSelectionCompetitorDataProvider.Display {
-        private final SuggestedMultiSelection<SimpleCompetitorWithIdDTO> selectionUi;
-        private final HasEnabled notifyAboutResultsUi;
-
-        private CompetitorDisplayImpl(final SuggestedMultiSelectionCompetitorDataProvider dataProvider,
-                FlagImageResolver flagImageResolver) {
-            selectionUi = SuggestedMultiSelection.forCompetitors(dataProvider, StringMessages.INSTANCE.competitors(),
-                    flagImageResolver);
-            notifyAboutResultsUi = selectionUi.addNotificationToggle(new NotificationCallback() {
-                @Override
-                public void onNotificationToggled(boolean enabled) {
-                    dataProvider.setNotifyAboutResults(enabled);
-                }
-            }, StringMessages.INSTANCE.notificationAboutNewResults());
-            dataProvider.addDisplay(this);
-        }
-
-        @Override
-        public void setSelectedItems(Collection<SimpleCompetitorWithIdDTO> selectedItems) {
-            selectionUi.setSelectedItems(selectedItems);
-        }
-
-        @Override
-        public void setNotifyAboutResults(boolean notifyAboutResults) {
-            notifyAboutResultsUi.setEnabled(notifyAboutResults);
-        }
-    }
-
     public void setEntry(SailorProfileEntry entry) {
         competitorSelectionUi.setSelectedItems(entry.getCompetitors());
-        title.setInnerText(entry.getName());
+        titleUi.setInnerText(entry.getName());
     }
 }
