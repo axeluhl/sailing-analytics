283471a30ae6d3d578a18f44ea4369a343a15aec
D067890 <j.rendl@sap.com>
2018-08-08 14:04:48 +0200

    bug4104: Improved TagCreationInputPanel

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/TagCreationInputPanel.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/raceboard/TagCreationInputPanel.java
@@ -1,27 +1,38 @@
 package com.sap.sailing.gwt.ui.raceboard;
 
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 
 public class TagCreationInputPanel extends VerticalPanel{
-    private TextBox tagTextBox, commentTextBox, urlTextBox;
+    private TextBox tagTextBox, urlTextBox;
+    private TextArea commentTextBox; 
     
     public TagCreationInputPanel(StringMessages stringMessages) {
-        tagTextBox = new TextBox();
-        tagTextBox.setTitle("Tag");
-        tagTextBox.getElement().setPropertyString("placeholder", stringMessages.tagLabelTag());
-        add(tagTextBox);
+        VerticalPanel mainPanel = new VerticalPanel();
+        HorizontalPanel subPanel = new HorizontalPanel();
         
-        commentTextBox = new TextBox();
-        commentTextBox.setTitle("Comment");
-        commentTextBox.getElement().setPropertyString("placeholder", stringMessages.tagLabelComment());
-        add(commentTextBox);
+        tagTextBox = new TextBox();
+        tagTextBox.setTitle(stringMessages.tagLabelTag());
+        tagTextBox.getElement().setPropertyString("placeholder", stringMessages.tagLabelTag());
+        subPanel.add(tagTextBox);
         
         urlTextBox = new TextBox();
-        urlTextBox.setTitle("Image URL");
+        urlTextBox.setTitle(stringMessages.tagLabelImageURL());
         urlTextBox.getElement().setPropertyString("placeholder", stringMessages.tagLabelImageURL());
-        add(urlTextBox);
+        subPanel.add(urlTextBox);
+        
+        mainPanel.add(subPanel);
+        commentTextBox = new TextArea();
+        commentTextBox.setWidth("100%");
+        commentTextBox.setVisibleLines(4);
+        commentTextBox.setTitle(stringMessages.tagLabelComment());
+        commentTextBox.getElement().setPropertyString("placeholder", stringMessages.tagLabelComment());
+        mainPanel.add(commentTextBox);
+        add(mainPanel);
+        
     }
     
     public String getTagValue() {
