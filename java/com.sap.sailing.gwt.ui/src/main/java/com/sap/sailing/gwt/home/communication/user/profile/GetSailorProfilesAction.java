1c729b0f903f00624833cafb073040e9a1aaefe9
Steffen Jacobs <steffen.jacobs@oio.de>
2018-08-14 11:17:01 +0200

    Bug 4211: added boatclass retrival to GetSailorProfileAction

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/communication/user/profile/GetSailorProfilesAction.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/communication/user/profile/GetSailorProfilesAction.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import com.google.gwt.core.shared.GwtIncompatible;
+import com.sap.sailing.domain.base.BoatClass;
 import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.CompetitorAndBoatStore;
 import com.sap.sailing.domain.common.dto.BoatClassDTO;
 import com.sap.sailing.gwt.home.communication.SailingAction;
 import com.sap.sailing.gwt.home.communication.SailingDispatchContext;
@@ -43,21 +45,33 @@ public class GetSailorProfilesAction implements SailingAction<SailorProfileEntri
         List<SailorProfileEntry> list = new ArrayList<>();
         StreamSupport.stream(preferences.getSailorProfiles().spliterator(), false)
                 .filter(e -> uuid == null || (uuid != null && uuid.equals(e.getUuid())))
-                .forEach(s -> list.add(convertToDto(s)));
+                .forEach(s -> list.add(convertToDto(s, ctx.getRacingEventService().getCompetitorAndBoatStore())));
         result = new SailorProfileEntries(list);
         return result;
     }
 
     @GwtIncompatible
-    private SailorProfileEntry convertToDto(SailorProfilePreference pref) {
+    private SailorProfileEntry convertToDto(final SailorProfilePreference pref, final CompetitorAndBoatStore store) {
         return new SailorProfileEntry(pref.getUuid(), pref.getName(), convertCompetitors(pref.getCompetitors()),
-                new ArrayList<BadgeDTO>(), new ArrayList<BoatClassDTO>());
+                new ArrayList<BadgeDTO>(), convertBoats(pref.getCompetitors(), store));
     }
 
     @GwtIncompatible
-    private List<SimpleCompetitorWithIdDTO> convertCompetitors(Iterable<Competitor> comps) {
+    private List<SimpleCompetitorWithIdDTO> convertCompetitors(final Iterable<Competitor> comps) {
         return StreamSupport.stream(comps.spliterator(), false).filter(c -> c != null)
                 .map(c -> new SimpleCompetitorWithIdDTO(c)).collect(Collectors.toList());
+    }
+
+    @GwtIncompatible
+    private List<BoatClassDTO> convertBoats(Iterable<Competitor> comps, final CompetitorAndBoatStore store) {
+        return StreamSupport.stream(comps.spliterator(), false).filter(c -> c != null)
+                .map(c -> convertBoatClass(store.getExistingCompetitorWithBoatById(c.getId()).getBoat().getBoatClass()))
+                .collect(Collectors.toList());
+    }
+
+    @GwtIncompatible
+    private BoatClassDTO convertBoatClass(BoatClass bc) {
+        return new BoatClassDTO(bc.getName(), bc.getDisplayName(), bc.getHullLength(), bc.getHullBeam());
     }
 
 }
