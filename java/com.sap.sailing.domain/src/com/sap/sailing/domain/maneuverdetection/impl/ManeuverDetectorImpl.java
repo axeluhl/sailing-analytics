dca0f244477fcf77f3e0d230f6b257e3a413ef24
Vladislav Chumak <v.chumak@sap.com>
2018-08-16 22:17:22 +0200

    Changed course change limit for curve extension (testing hudson)

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/maneuverdetection/impl/ManeuverDetectorImpl.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/maneuverdetection/impl/ManeuverDetectorImpl.java
@@ -974,7 +974,7 @@ public class ManeuverDetectorImpl extends AbstractManeuverDetectorImpl {
         if (curveBoundaryExtension == null) {
             return false;
         }
-        return Math.abs(maneuverMainCurveDetails.getDirectionChangeInDegrees()) / 2.0 < Math
+        return Math.abs(maneuverMainCurveDetails.getDirectionChangeInDegrees()) / 2.5 < Math
                 .abs(curveBoundaryExtension.getCourseChangeInDegreesWithinExtensionArea());
     }
 
