6b240dd215368c023c2d7ca76f254c35f7a8b6ec
Alessandro Stoltenberg <alessandro.stoltenberg@sap.com>
2018-08-17 12:30:32 +0200

    bug4134_2: Added more testing to ColorMapperTest

--- java/com.sap.sailing.gwt.ui.test/src/com/sap/sailing/gwt/ui/test/ColorMapperTest.java
+++ java/com.sap.sailing.gwt.ui.test/src/com/sap/sailing/gwt/ui/test/ColorMapperTest.java
@@ -1,24 +1,81 @@
 package com.sap.sailing.gwt.ui.test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
+
 import com.sap.sailing.gwt.ui.simulator.streamlets.ColorMapper;
+import com.sap.sailing.gwt.ui.simulator.streamlets.ColorMapperChangedListener;
 import com.sap.sailing.gwt.ui.simulator.streamlets.ValueRangeFlexibleBoundaries;
 
 public class ColorMapperTest {
-    ValueRangeFlexibleBoundaries valueRange = new ValueRangeFlexibleBoundaries(-5.0, 5.0, 0.0);
-    ColorMapper colorMapperTestColored = new ColorMapper(valueRange, false);
-    ColorMapper colorMapperTestGrey = new ColorMapper(valueRange, true);
-    double testValue1 = -5.0;
-    String expectedResult1colored = "hsl(240, 100%, 50%)";
-    String expectedResult1grey = "rgba(255,255,255,0.0)";
-    double testValue2 = 5.0;
-    String expectedResult2colored = "hsl(0, 100%, 50%)";
-    String expectedResult2grey = "rgba(255,255,255,1.0)";
+    ColorMapper colorMapper;
+    ColorMapperChangedListener listener;
+    ValueRangeFlexibleBoundaries valueRange;
+    Set<Integer> valueSet;
+    Set<String> colorSet;
+
+    @Before
+    public void setUp() {
+        valueRange = new ValueRangeFlexibleBoundaries(-5.0, 5.0, 0.0);
+        colorMapper = new ColorMapper(valueRange, false);
+        listener = mock(ColorMapperChangedListener.class);
+        colorMapper.addListener(listener);
+        valueSet = new HashSet<>();
+        updateValueSet(valueSet);
+        colorSet = new HashSet<>();
+        updateColorSet(colorSet);
+    }
+
+    public void updateColorSet(Set<String> colorSet) {
+        colorSet.clear();
+        for (int i : valueSet) {
+            double h = (1 - (i - valueRange.getMinLeft()) / (valueRange.getMaxRight() - valueRange.getMinLeft())) * 240;
+            colorSet.add("hsl(" + Math.round(h) + ", 100%, 50%)");
+        }
+    }
+    public void updateValueSet(Set<Integer> valueSet) {
+        valueSet.clear();
+        for (int i = (int) Math.round(valueRange.getMinLeft()); i <= (int) Math.round(valueRange.getMaxRight()); i++) {
+            valueSet.add(i);
+        }
+    }
+
     @Test
-    public void testGetColor() {
-        assertEquals(expectedResult1colored, colorMapperTestColored.getColor(testValue1));
-        assertEquals(expectedResult1grey, colorMapperTestGrey.getColor(testValue1));
-        assertEquals(expectedResult2colored, colorMapperTestColored.getColor(testValue2));
-        assertEquals(expectedResult2grey, colorMapperTestGrey.getColor(testValue2));
+    public void testExceptionForValueOutOfBoundaries() {
+        try {
+            colorMapper.getColor(-10.0);
+            fail("Expected illegal argument exception");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+    }
+
+    @Test
+    public void testGetColorColored() {
+        // first set the color and verify listener will be notified
+        colorMapper.setGrey(false);
+        verify(listener, times(1)).onColorMappingChanged();
+        // Check for each value in valueSet that getColor() returns a color within the colorSet.
+        for (int i : valueSet) {
+            assertTrue(colorSet.contains(colorMapper.getColor(i)));
+        }
+    }
+    @Test
+    public void changeValueRangeAndTestGetColor() {
+        valueRange.setMinMax(12.0, 26.0);
+        //verify Listener
+        verify(listener, times(1)).onColorMappingChanged();
+        updateValueSet(valueSet);
+        updateColorSet(colorSet);
+        //check if new values are within the colorSet
+        for (int i : valueSet) {
+            assertTrue(colorSet.contains(colorMapper.getColor(i)));
+        }
     }
 }
