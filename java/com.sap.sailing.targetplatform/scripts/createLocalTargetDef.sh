c7a170d084038108ba4fe4014ff27e1983506c42
RobinFleige <robin.fleige@sap.com>
2018-08-20 13:23:21 +0200

    Fixes URL which still had http instead of https

--- java/com.sap.sailing.targetplatform/scripts/createLocalTargetDef.sh
+++ java/com.sap.sailing.targetplatform/scripts/createLocalTargetDef.sh
@@ -2,7 +2,7 @@
 # generate a target definition pointing to the locally built p2 repo from the race-analysis-p2-remote target defintion
 
 base="../definitions/race-analysis-p2"
-remote_repo="http://p2.sapsailing.com/p2/sailing/"
+remote_repo="https://p2.sapsailing.com/p2/sailing/"
 local_repo="file://`readlink -f ../../com.sap.sailing.targetplatform.base/target/repository/ | sed -e 's/\/cygdrive\/\(.\)/\/\1:/'`"
 
 # replace remote p2-repo URL with local repo URL
