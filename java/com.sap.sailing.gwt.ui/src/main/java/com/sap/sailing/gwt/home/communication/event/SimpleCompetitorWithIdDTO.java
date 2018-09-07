84d55873db1774a1fe360b1b3d74d4d9b976aeb3
Kai Börnert <kai.boernert@oio.de>
2018-09-07 15:23:36 +0200

    bug4211 fixed issue with statistics due to wrongly implemented equals in dto

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/communication/event/SimpleCompetitorWithIdDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/communication/event/SimpleCompetitorWithIdDTO.java
@@ -42,4 +42,29 @@ public class SimpleCompetitorWithIdDTO extends SimpleCompetitorDTO {
                 ? this.idAsString.compareTo(((SimpleCompetitorWithIdDTO) obj).idAsString) : compareTo;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((idAsString == null) ? 0 : idAsString.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SimpleCompetitorWithIdDTO other = (SimpleCompetitorWithIdDTO) obj;
+        if (idAsString == null) {
+            if (other.idAsString != null)
+                return false;
+        } else if (!idAsString.equals(other.idAsString))
+            return false;
+        return true;
+    }
+
 }
