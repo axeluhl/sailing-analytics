8253ec0a1ebf082fd5a375441c8988b08c183144
Steffen Jacobs <steffen.jacobs@oio.de>
2018-09-27 16:06:08 +0200

    Bug 4211: added ConfirmDialog

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/shared/partials/dialog/DialogResources.java
@@ -0,0 +1,30 @@
+package com.sap.sailing.gwt.home.shared.partials.dialog;
+
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface DialogResources extends ClientBundle {
+
+    public static final DialogResources INSTANCE = GWT.create(DialogResources.class);
+
+    @Source("Dialog.gss")
+    DialogCss css();
+
+    public interface DialogCss extends CssResource {
+
+        String dialog();
+
+        String message();
+
+        String yesButton();
+
+        String noButton();
+
+        String button();
+
+        String buttonPanel();
+
+        String backgroundPanel();
+    }
+}
