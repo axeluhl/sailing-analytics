585bbc65df1f1086d0cc38f25a1511e2e085e027
Kai Börnert <kai.boernert@oio.de>
2018-10-04 14:05:31 +0200

    bug4766 added javadoc to FixReceivedListener.fixReceived

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/racelog/tracking/FixReceivedListener.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/racelog/tracking/FixReceivedListener.java
@@ -7,8 +7,19 @@ import com.sap.sse.common.Timed;
 /**
  * Listener to be informed about new fixes by {@link SensorFixStore}.
  *
- * @param <FixT> the type of fixes this listener can consume.
+ * @param <FixT>
+ *            the type of fixes this listener can consume.
  */
 public interface FixReceivedListener<FixT extends Timed> {
+    /**
+     * 
+     * @param device
+     *            the device that recorded the fix. Cannot be <code>null</code>.
+     * @param fix
+     *            The fix that was stored. Cannot be <code>null</code>.
+     * @return An Iterable with RegattaAndRaceIdentifier is returned, that will contain races with new maneuvers, which
+     *         were not available at the last time the given device stored a fix, the Iterable can be empty. It can also
+     *         contain multiple identifiers, if the devicemapping is currently ambiguous
+     */
     Iterable<RegattaAndRaceIdentifier> fixReceived(DeviceIdentifier device, FixT fix);
 }
