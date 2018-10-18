8a40a87e6a0f6b8d304de6d3a82851748785ce33
Kai Börnert <kai.boernert@oio.de>
2018-10-18 14:02:43 +0200

    added migration for swisstiming

--- java/com.sap.sailing.domain.swisstimingadapter/src/com/sap/sailing/domain/swisstimingadapter/impl/SwissTimingArchiveConfigurationImpl.java
+++ java/com.sap.sailing.domain.swisstimingadapter/src/com/sap/sailing/domain/swisstimingadapter/impl/SwissTimingArchiveConfigurationImpl.java
@@ -3,6 +3,7 @@ package com.sap.sailing.domain.swisstimingadapter.impl;
 import com.sap.sailing.domain.swisstimingadapter.SwissTimingArchiveConfiguration;
 
 public class SwissTimingArchiveConfigurationImpl implements SwissTimingArchiveConfiguration {
+    private static final long serialVersionUID = 1L;
     private final String jsonUrl;
     
     public SwissTimingArchiveConfigurationImpl(String jsonUrl) {
