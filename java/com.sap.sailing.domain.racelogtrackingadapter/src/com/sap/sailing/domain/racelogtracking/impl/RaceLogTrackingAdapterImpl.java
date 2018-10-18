5084b78c926e3d31ce1914915f31f7596d629a63
Steffen Schäfer <steffen.schaefer@oio.de>
2018-10-18 14:54:44 +0200

    Bug 4720: Invitation mail changes for Branch.io integration

--- java/com.sap.sailing.domain.racelogtrackingadapter/src/com/sap/sailing/domain/racelogtracking/impl/RaceLogTrackingAdapterImpl.java
+++ java/com.sap.sailing.domain.racelogtrackingadapter/src/com/sap/sailing/domain/racelogtracking/impl/RaceLogTrackingAdapterImpl.java
@@ -296,13 +296,11 @@ public class RaceLogTrackingAdapterImpl implements RaceLogTrackingAdapter {
                             DeviceMappingConstants.URL_COMPETITOR_ID_AS_STRING, competitor.getId().toString(),
                             NonGwtUrlHelper.INSTANCE);
                     final RaceLogTrackingInvitationMailBuilder mail = new RaceLogTrackingInvitationMailBuilder(locale)
-                            .withSubject(competitor.getName()) //
-                            .addEventLogo(event) //
-                            .addHeadline(event, leaderboard) //
-                            .addSailInSightIntroductoryText(competitor.getName()) //
-                            .addQrCodeImage(url) //
-                            .addOpenInAppTextAndLinks(url, iOSAppUrl, androidAppUrl) //
-                            .addInstallAppTextAndLinks(iOSAppUrl, androidAppUrl);
+                            .withSubject(competitor.getName())
+                            .addEventLogo(event)
+                            .addHeadline(event, leaderboard)
+                            .addSailInSightIntroductoryText(competitor.getName())
+                            .addSailInsightDeeplink(url);
                     getMailService().sendMail(toAddress, mail.getSubject(), mail.getMultipartSupplier());
                 } catch (MessagingException | MailException | IOException e) {
                     logger.log(Level.SEVERE, "Error while trying to send invitation mail to competitor"
@@ -328,13 +326,11 @@ public class RaceLogTrackingAdapterImpl implements RaceLogTrackingAdapter {
             try {
                 final String buoyTender = RaceLogTrackingI18n.buoyTender(locale);
                 final RaceLogTrackingInvitationMailBuilder mail = new RaceLogTrackingInvitationMailBuilder(locale)
-                        .withSubject(buoyTender) //
-                        .addEventLogo(event) //
-                        .addHeadline(event, leaderboard) //
-                        .addBuoyPingerIntroductoryText(buoyTender) //
-                        .addQrCodeImage(url) //
-                        .addOpenInAppTextAndLinks(url, iOSAppUrl, androidAppUrl) //
-                        .addInstallAppTextAndLinks(iOSAppUrl, androidAppUrl);
+                        .withSubject(buoyTender)
+                        .addEventLogo(event)
+                        .addHeadline(event, leaderboard)
+                        .addBuoyPingerIntroductoryText(buoyTender)
+                        .addBuoyPingerDeeplink(url);
                 getMailService().sendMail(toAddress, mail.getSubject(), mail.getMultipartSupplier());
             } catch (MessagingException | MailException | IOException e) {
                 logger.log(Level.SEVERE, "Error while trying to send invitation mail to buoy tender "
