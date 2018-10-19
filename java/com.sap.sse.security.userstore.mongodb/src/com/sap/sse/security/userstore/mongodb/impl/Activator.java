46baa504c8a1effe1405f38c85b2bd3bc1e3e4d0
Kai Börnert <kai.boernert@oio.de>
2018-10-19 10:10:29 +0200

    added migration for User and UserGroup

--- java/com.sap.sse.security.userstore.mongodb/src/com/sap/sse/security/userstore/mongodb/impl/Activator.java
+++ java/com.sap.sse.security.userstore.mongodb/src/com/sap/sse/security/userstore/mongodb/impl/Activator.java
@@ -1,21 +1,27 @@
 package com.sap.sse.security.userstore.mongodb.impl;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
 
 import com.sap.sse.ServerStartupConstants;
 import com.sap.sse.mongodb.MongoDBService;
 import com.sap.sse.security.AccessControlStore;
 import com.sap.sse.security.PreferenceConverterRegistrationManager;
+import com.sap.sse.security.SecurityService;
 import com.sap.sse.security.UserStore;
+import com.sap.sse.security.shared.User;
+import com.sap.sse.security.shared.UserGroup;
+import com.sap.sse.security.shared.impl.SecuredSecurityTypes;
 import com.sap.sse.security.userstore.mongodb.AccessControlStoreImpl;
 import com.sap.sse.security.userstore.mongodb.UserStoreImpl;
 
 public class Activator implements BundleActivator {
-
+    private static final Logger logger = Logger.getLogger(Activator.class.getName());
     private static BundleContext context;
     private ServiceRegistration<?> accessControlStoreRegistration;
     private ServiceRegistration<?> userStoreRegistration;
@@ -33,7 +39,7 @@ public class Activator implements BundleActivator {
     public void start(BundleContext bundleContext) throws Exception {
         Activator.context = bundleContext;
         final String defaultTenantName = System.getProperty(UserStore.DEFAULT_TENANT_NAME_PROPERTY_NAME, ServerStartupConstants.SERVER_NAME);
-        UserStoreImpl userStore = new UserStoreImpl(defaultTenantName);
+        final UserStoreImpl userStore = new UserStoreImpl(defaultTenantName);
         AccessControlStoreImpl accessControlStore = new AccessControlStoreImpl(userStore);
         accessControlStoreRegistration = context.registerService(AccessControlStore.class.getName(),
                 accessControlStore, null);
@@ -44,6 +50,31 @@ public class Activator implements BundleActivator {
         for (CollectionNames name : CollectionNames.values()) {
             MongoDBService.INSTANCE.registerExclusively(CollectionNames.class, name.name());
         }
+
+        new Thread(getClass().getName() + " migration handler") {
+            public void run() {
+                try {
+                    final ServiceTracker<SecurityService, SecurityService> securityServiceServiceTracker = new ServiceTracker<>(
+                            context, SecurityService.class, null);
+                    securityServiceServiceTracker.open();
+                    final SecurityService securityService = securityServiceServiceTracker.waitForService(0);
+                    for (User user : userStore.getUsers()) {
+                        securityService.migrateOwnership(user, SecuredSecurityTypes.getAllInstances());
+                    }
+                    for (UserGroup group : userStore.getUserGroups()) {
+                        securityService.migrateOwnership(group, SecuredSecurityTypes.getAllInstances());
+                    }
+                    securityService.assumeOwnershipMigrated(SecuredSecurityTypes.ACCESS_CONTROL_LIST.getName(),
+                            SecuredSecurityTypes.getAllInstances());
+                    securityService.assumeOwnershipMigrated(SecuredSecurityTypes.ROLE_DEFINITION.getName(),
+                            SecuredSecurityTypes.getAllInstances());
+                    securityService.assumeOwnershipMigrated(SecuredSecurityTypes.SERVER.getName(),
+                            SecuredSecurityTypes.getAllInstances());
+                } catch (InterruptedException e) {
+                    logger.log(Level.SEVERE, "Error in migration", e);
+                }
+            }
+        }.start();
     }
 
     /*
