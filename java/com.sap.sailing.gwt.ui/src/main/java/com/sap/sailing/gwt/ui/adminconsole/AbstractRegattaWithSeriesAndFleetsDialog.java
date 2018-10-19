d376789c102a6d013a2ba49da3497a40be54ba6f
Thomas Wiese <mail@thomaswiese.de>
2018-10-19 09:05:21 +0200

    init enable button for registration link config correctly

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/AbstractRegattaWithSeriesAndFleetsDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/AbstractRegattaWithSeriesAndFleetsDialog.java
@@ -148,6 +148,7 @@ public abstract class AbstractRegattaWithSeriesAndFleetsDialog<T> extends DataEn
                         }).show();
             }
         });
+        registrationLinkWithQRCodeOpenButton.setEnabled(regatta.competitorRegistrationType.isOpen());
     }
 
     /**
