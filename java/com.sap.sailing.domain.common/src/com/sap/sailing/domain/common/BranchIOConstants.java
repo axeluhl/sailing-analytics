f7cc55433a31a7e67a7410c00e0f53bb7781490d
Thomas Wiese <mail@thomaswiese.de>
2018-11-05 12:58:29 +0100

    initial set open regatta registration link for branch.io add constants

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/BranchIOConstants.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/BranchIOConstants.java
@@ -5,6 +5,7 @@ public class BranchIOConstants {
     public static final String SAILINSIGHT_APP_BRANCHIO_PATH = "checkinUrl";
     public static final String BUOYPINGER_APP_BRANCHIO = "https://buoypinger-app.sapsailing.com/invite";
     public static final String BUOYPINGER_APP_BRANCHIO_PATH = "checkinUrl";
+    public static final String OPEN_REGATTA_APP_BRANCHIO = "https://sailinsight-app.sapsailing.com/register";
 
     private BranchIOConstants() {
     };
