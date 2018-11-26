2520170ef13c07bbb8709ef5b6a2b90994169ce2
Vladislav Chumak <v.chumak@sap.com>
2018-11-26 11:46:22 +0100

    Unified package naming in windestimation.lab

--- java/com.sap.sailing.windestimation.lab/src/com/sap/sailing/windestimation/classifier/maneuver/PersistedManeuverClassifiersScorePrinter.java
+++ java/com.sap.sailing.windestimation.lab/src/com/sap/sailing/windestimation/classifier/maneuver/PersistedManeuverClassifiersScorePrinter.java
@@ -1,4 +1,4 @@
-package com.sap.sailing.windestimation.maneuverclassifier;
+package com.sap.sailing.windestimation.classifier.maneuver;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
