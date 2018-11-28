160482b8406070598c78ce973380ccb60700de46
Benjamin Barth <benjamin.barth@oio.de>
2018-11-28 11:25:13 +0100

    bug4763 Added new AccessControlledButtonPanel to secure create/remove buttons

---
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/client/component/AccessControlledButtonPanel.java
@@ -0,0 +1,103 @@
+package com.sap.sse.security.ui.client.component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.sap.sse.security.shared.HasPermissions;
+import com.sap.sse.security.ui.client.UserService;
+
+/**
+ * Panel where several buttons can be added which are either {@link #addUnsecuredAction(String, Command) unsecured} or
+ * restricted for users with {@link #addCreateAction(String, Command) create} and /or
+ * {@link #addRemoveAction(String, Command) remove} permissions. The visibility of secured buttons depend on the
+ * permissions of the currently logged-in user and changes dynamically.
+ */
+public class AccessControlledButtonPanel extends Composite {
+
+    private final HorizontalPanel panel = new HorizontalPanel();
+    private final Map<Button, Supplier<Boolean>> buttonToPermissions = new HashMap<>();
+
+    private final Supplier<Boolean> createPermissionCheck, removePermissionCheck;
+    private final BiConsumer<Button, Supplier<Boolean>> visibilityUpdater = (btn, check) -> btn.setVisible(check.get());
+
+    /**
+     * Creates an {@link AccessControlledButtonPanel} instance for the given {@link HasPermissions type} using the
+     * provided {@link UserService} to check permissions and register for user status changes.
+     * 
+     * @param userService
+     *            the {@link UserService} to check permissions and register for user status changes
+     * @param type
+     *            the {@link HasPermissions} representing the type of objects to be secured by this panel
+     */
+    public AccessControlledButtonPanel(final UserService userService, final HasPermissions type) {
+        this.createPermissionCheck = () -> userService.hasCurrentUserPermissionToCreateObjectOfType(type);
+        this.removePermissionCheck = () -> userService.hasCurrentUserPermissionToDeleteAnyObjectOfType(type);
+        userService.addUserStatusEventHandler((user, preAuth) -> buttonToPermissions.forEach(visibilityUpdater), true);
+        initWidget(panel);
+    }
+
+    /**
+     * Adds an unsecured action button, which's visibility is independent from the current user's permissions.
+     * 
+     * @param text
+     *            the {@link String text} to show on the button
+     * @param callback
+     *            the {@link Command callback} to execute on button click
+     * @return the created {@link Button} instance
+     */
+    public Button addUnsecuredAction(final String text, final Command callback) {
+        return addAction(text, () -> true, callback);
+    }
+
+    /**
+     * Adds an secured action button, which is only visible if the current user has the
+     * {@link UserService#hasCurrentUserPermissionToCreateObjectOfType(HasPermissions) create permission} for the
+     * {@link HasPermissions type} provided in this {@link AccessControlledButtonPanel}'s constructor.
+     * 
+     * @param text
+     *            the {@link String text} to show on the button
+     * @param callback
+     *            the {@link Command callback} to execute on button click, if permission is granted
+     * @return the created {@link Button} instance
+     */
+    public Button addCreateAction(final String text, final Command callback) {
+        return addAction(text, createPermissionCheck, callback);
+    }
+
+    /**
+     * Adds an secured action button, which is only visible if the current user has any
+     * {@link UserService#hasCurrentUserPermissionToDeleteAnyObjectOfType(HasPermissions) delete permission} for the
+     * {@link HasPermissions type} provided in this {@link AccessControlledButtonPanel}'s constructor.
+     * 
+     * @param text
+     *            the {@link String text} to show on the button
+     * @param callback
+     *            the {@link Command callback} to execute on button click, if permission is granted
+     * @return the created {@link Button} instance
+     */
+    public Button addRemoveAction(final String text, final Command callback) {
+        return addAction(text, removePermissionCheck, callback);
+    }
+
+    private Button addAction(final String text, final Supplier<Boolean> permissionCheck, final Command callback) {
+        final Button button = new Button(text, (ClickHandler) event -> {
+            if (permissionCheck.get()) {
+                callback.execute();
+            }
+        });
+        this.buttonToPermissions.put(button, permissionCheck);
+        button.getElement().getStyle().setMarginRight(5, Unit.PX);
+        this.panel.add(button);
+        this.visibilityUpdater.accept(button, permissionCheck);
+        return button;
+    }
+
+}
