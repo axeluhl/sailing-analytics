a19efbd5239a32c74695069f885f4fe50489ba9e
Benjamin Barth <benjamin.barth@oio.de>
2018-12-06 13:47:39 +0100

    Fixed NPE during deserialization of AccessControlListDTO

--- java/com.sap.sse.security.common/src/com/sap/sse/security/shared/dto/AccessControlListDTO.java
+++ java/com.sap.sse.security.common/src/com/sap/sse/security/shared/dto/AccessControlListDTO.java
@@ -1,17 +1,18 @@
 package com.sap.sse.security.shared.dto;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
 import com.sap.sse.security.shared.impl.AbstractAccessControlList;
 
 public class AccessControlListDTO extends AbstractAccessControlList<StrippedUserGroupDTO, StrippedUserDTO> {
-    private static final long serialVersionUID = 1L;
 
-    @Deprecated
-    // gwt only
+    private static final long serialVersionUID = 2878270565599189728L;
+
+    @Deprecated // gwt only
     AccessControlListDTO() {
-        super(null);
+        super(Collections.<StrippedUserGroupDTO, Set<String>> emptyMap());
     }
 
     public AccessControlListDTO(Map<StrippedUserGroupDTO, Set<String>> permissionMap) {
