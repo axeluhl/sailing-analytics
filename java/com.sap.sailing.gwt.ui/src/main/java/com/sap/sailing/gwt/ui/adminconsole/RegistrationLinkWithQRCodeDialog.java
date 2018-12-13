ff580cccd655e370a8bc3469e3644e59234da25d
Thomas Wiese <mail@thomaswiese.de>
2018-12-13 14:11:45 +0100

    add server url to open regatta registration url

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/RegistrationLinkWithQRCodeDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/RegistrationLinkWithQRCodeDialog.java
@@ -157,6 +157,7 @@ public class RegistrationLinkWithQRCodeDialog extends DataEntryDialog<Registrati
         Map<String, String> parameters = new HashMap<String, String>();
         parameters.put("regatta_name", regattaName);
         parameters.put("secret", secretTextBox.getValue());
+        parameters.put("server", baseUrl);
         String deeplinkUrl = BranchIO.generateLink(BranchIOConstants.OPEN_REGATTA_APP_BRANCHIO, parameters,
                 URL::encodeQueryString);
         urlTextBox.setText(deeplinkUrl);
