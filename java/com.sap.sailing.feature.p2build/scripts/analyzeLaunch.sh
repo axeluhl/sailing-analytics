19af798ed3c41c2e212d62c99ca7c9a5a499319b
Axel Uhl <axel.uhl@sap.com>
2018-12-14 13:09:36 +0100

    fixed launch config patching another time; added missing org.eclipse.osgi.util bundle to sse runtime feature
    Change-Id: I0d93cf604d72c07e23c997d860d756052a1f8826

--- java/com.sap.sailing.feature.p2build/scripts/analyzeLaunch.sh
+++ java/com.sap.sailing.feature.p2build/scripts/analyzeLaunch.sh
@@ -2,7 +2,7 @@
 target_bundles=$(cat "$1" | grep 'key="target_bundles"' | sed -e 's/^.* value="\([^"]*\)"\/>/\1/' | sed -e 's/,/\n/g' | sed -e 's/^\(.*\)@\(.*\):\(.*\)$/\1 \2 \3/')
 workspace_bundles=$(cat "$1" | grep 'key="workspace_bundles"' | sed -e 's/^.* value="\([^"]*\)"\/>/\1/' | sed -e 's/,/\n/g' | sed -e 's/^\(.*\)@\(.*\):\(.*\)$/\1 \2 \3/')
 echo " *** Target Bundles ***"
-echo "$target_bundles"
+echo "$target_bundles" | sort
 echo " *** Workspace Bundles ***"
-echo "$workspace_bundles"
+echo "$workspace_bundles" | sort
 
