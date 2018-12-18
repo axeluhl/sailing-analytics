aa16714fd5770d7a68f7dcfec70565173f399466
Steffen Schaefer <steffen.schaefer@oio.de>
2018-12-18 08:34:41 +0100

    Implemented custom serialization for UserImpl again. This should fix deserialization of object cycles (UserImpl -> HashSet -> RoleImpl -> UserImpl) where uninitialized objects are inserted into a HashSet.

--- java/com.sap.sse.security.common/src/com/sap/sse/security/shared/dto/StrippedUserDTO.java
+++ java/com.sap.sse.security.common/src/com/sap/sse/security/shared/dto/StrippedUserDTO.java
@@ -1,21 +1,35 @@
 package com.sap.sse.security.shared.dto;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.sap.sse.common.Util;
 import com.sap.sse.security.shared.WildcardPermission;
 import com.sap.sse.security.shared.impl.SecurityUserImpl;
 
 public class StrippedUserDTO extends SecurityUserImpl<RoleDefinitionDTO, RoleDTO, StrippedUserGroupDTO> {
     private static final long serialVersionUID = 1L;
-
+    
+    private Set<RoleDTO> roles;
+    
     @Deprecated
     private StrippedUserDTO() {
         super(null);
     }
 
     public StrippedUserDTO(String name, Iterable<RoleDTO> roles, Iterable<WildcardPermission> permissions) {
-        super(name, roles, permissions);
+        super(name, permissions);
+        roles = new HashSet<>();
+        Util.addAll(roles, this.getRolesInternal());
     }
 
     public StrippedUserDTO(String name) {
-        super(name);
+        this(name, Collections.<RoleDTO>emptySet(), Collections.<WildcardPermission>emptySet());
+    }
+    
+    @Override
+    protected Set<RoleDTO> getRolesInternal() {
+        return roles;
     }
 }
