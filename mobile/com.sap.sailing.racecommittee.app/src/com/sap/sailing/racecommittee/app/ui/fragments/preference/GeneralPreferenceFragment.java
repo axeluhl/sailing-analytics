axeluhl/sailing-analytics

commit 4ad86839159266f68648c9a0ebe4671d69e05d3c

switched external qrscanner out for integrated scanner aligning with ios

mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/preference/GeneralPreferenceFragment.java
@@ -1,5 +1,8 @@
 package com.sap.sailing.racecommittee.app.ui.fragments.preference;
 
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.sap.sailing.android.shared.ui.activities.BarcodeCaptureActivity;
 import com.sap.sailing.android.shared.ui.fragments.preference.BasePreferenceFragment;
 import com.sap.sailing.android.shared.ui.views.EditSetPreference;
 import com.sap.sailing.racecommittee.app.AppPreferences;
@@ -9,9 +12,7 @@ import com.sap.sailing.racecommittee.app.data.DataManager;
 import com.sap.sailing.racecommittee.app.utils.QRHelper;
 import com.sap.sailing.racecommittee.app.utils.autoupdate.AutoUpdater;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.CheckBoxPreference;
@@ -174,29 +175,20 @@ public class GeneralPreferenceFragment extends BasePreferenceFragment {
         });
     }
 
-    private boolean requestQRCodeScan() {
-        try {
-            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-            startActivityForResult(intent, requestCodeQRCode);
-            return true;
-        } catch (Exception e) {
-            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
-            Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
-            startActivity(marketIntent);
-        }
-        return false;
+    private void requestQRCodeScan() {
+        Intent intent = new Intent(getContext(), BarcodeCaptureActivity.class);
+        startActivityForResult(intent, requestCodeQRCode);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode != requestCodeQRCode) {
+        if (requestCode == requestCodeQRCode) {
             super.onActivityResult(requestCode, resultCode, data);
             return;
         }
-
-        if (resultCode == Activity.RESULT_OK) {
-            QRHelper.with(getActivity()).saveData(data.getStringExtra("SCAN_RESULT"));
+        if (resultCode == CommonStatusCodes.SUCCESS && data != null) {
+            Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+            QRHelper.with(getActivity()).saveData(barcode.displayValue);
 
             final AppPreferences appPreferences = AppPreferences.on(getActivity());
             appPreferences.setNeedConfigRefresh(true);

mobile/com.sap.sailing.android.shared/build.gradle
@@ -40,9 +40,10 @@ android {
 
 dependencies {
     /* support libraries */
+    api "com.android.support:support-v4:${rootProject.ext.support}"
+    api "com.android.support:support-v13:${rootProject.ext.support}"
     api "com.android.support:appcompat-v7:${rootProject.ext.support}"
     api "com.android.support:preference-v14:${rootProject.ext.support}"
-    api "com.android.support:support-v13:${rootProject.ext.support}"
 
     /* local dependencies */
     api project(':java:com.sap.sailing.domain.common')
@@ -54,6 +55,7 @@ dependencies {
 
     /* Google Play Services */
     api "com.google.android.gms:play-services-maps:${rootProject.ext.play_services}"
+    api "com.google.android.gms:play-services-vision:${rootProject.ext.play_services}"
 
     /* license dialog */
     api "de.psdev.licensesdialog:licensesdialog:${rootProject.ext.license_dialog}"

mobile/com.sap.sailing.android.shared/res/layout/activity_barcode_capture.xml
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout
+        xmlns:android="http://schemas.android.com/apk/res/android"
+        xmlns:app="http://schemas.android.com/apk/res-auto"
+        xmlns:tools="http://schemas.android.com/tools"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:id="@+id/barcodeCaptureLayout"
+        android:keepScreenOn="true"
+        tools:context=".ui.activities.BarcodeCaptureActivity"
+        android:layout_gravity="center">
+
+    <com.sap.sailing.android.shared.ui.utils.CameraSourcePreview
+            android:id="@+id/barcode_preview"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:gravity="center" />
+
+</LinearLayout>

mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/activities/BarcodeCaptureActivity.java
@@ -1,0 +1,285 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sap.sailing.android.shared.ui.activities;
+
+import java.io.IOException;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.sap.sailing.android.shared.R;
+import com.sap.sailing.android.shared.logging.ExLog;
+import com.sap.sailing.android.shared.ui.utils.BarcodeGraphicTracker;
+import com.sap.sailing.android.shared.ui.utils.BarcodeTrackerFactory;
+import com.sap.sailing.android.shared.ui.utils.CameraSourcePreview;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class BarcodeCaptureActivity extends AppCompatActivity implements BarcodeGraphicTracker.BarcodeUpdateListener {
+
+    private final static String TAG = BarcodeCaptureActivity.class.getName();
+
+    // intent request code to handle updating play services if needed.
+    private static final int RC_HANDLE_GMS = 9001;
+
+    // permission request codes need to be < 256
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
+
+    // constants used to pass extra data in the intent
+    public static final String BarcodeObject = "Barcode";
+
+    private CameraSource mCameraSource;
+    private CameraSourcePreview mPreview;
+
+    /**
+     * Initializes the UI and creates the detector pipeline.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_capture);
+
+        mPreview = findViewById(R.id.barcode_preview);
+
+        // Check for the camera permission before accessing the camera.  If the
+        // permission is not granted yet, request permission.
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            createCameraSource();
+        } else {
+            requestCameraPermission();
+        }
+    }
+
+    /**
+     * Handles the requesting of the camera permission.  This includes
+     * showing a "Snackbar" message of why the permission is needed then
+     * sending the request.
+     */
+    private void requestCameraPermission() {
+        ExLog.w(this, TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions, RC_HANDLE_CAMERA_PERM);
+            }
+        };
+
+        findViewById(R.id.barcodeCaptureLayout).setOnClickListener(listener);
+    }
+
+    /**
+     * Creates and starts the camera.  Note that this uses a higher resolution in comparison
+     * to other detection examples to enable the barcode detector to detect small barcodes
+     * at long distances.
+     *
+     * Suppressing InlinedApi since there is a check that the minimum version is met before using
+     * the constant.
+     */
+    @SuppressLint("InlinedApi")
+    private void createCameraSource() {
+        Context context = getApplicationContext();
+
+        // A barcode detector is created to track barcodes.  An associated multi-processor instance
+        // is set to receive the barcode detection results, track the barcodes, and maintain
+        // graphics for each barcode on screen.  The factory is used by the multi-processor to
+        // create a separate tracker instance for each barcode.
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(context).build();
+        BarcodeTrackerFactory barcodeFactory = new BarcodeTrackerFactory(this);
+        barcodeDetector.setProcessor(new MultiProcessor.Builder<>(barcodeFactory).build());
+
+        if (!barcodeDetector.isOperational()) {
+            // Note: The first time that an app using the barcode or face API is installed on a
+            // device, GMS will download a native libraries to the device in order to do detection.
+            // Usually this completes before the app is run for the first time.  But if that
+            // download has not yet completed, then the above call will not detect any barcodes
+            // and/or faces.
+            //
+            // isOperational() can be used to check if the required native libraries are currently
+            // available.  The detectors will automatically become operational once the library
+            // downloads complete on device.
+            ExLog.w(this, TAG, "Detector dependencies are not yet available.");
+
+            // Check for low storage.  If there is low storage, the native library will not be
+            // downloaded, so detection will not become operational.
+            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+            if (hasLowStorage) {
+                Toast.makeText(this, "storage low", Toast.LENGTH_LONG).show();
+                ExLog.w(this, TAG, "storage low!");
+            }
+        }
+
+        // Creates and starts the camera.  Note that this uses a higher resolution in comparison
+        // to other detection examples to enable the barcode detector to detect small barcodes
+        // at long distances.
+        CameraSource.Builder builder = new CameraSource.Builder(getApplicationContext(), barcodeDetector)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(15.0f);
+
+        // make sure that auto focus is an available option
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            builder.setAutoFocusEnabled(true);
+        }
+
+        mCameraSource = builder.build();
+    }
+
+    /**
+     * Restarts the camera.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startCameraSource();
+    }
+
+    /**
+     * Stops the camera.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mPreview != null) {
+            mPreview.stop();
+        }
+    }
+
+    /**
+     * Releases the resources associated with the camera source, the associated detectors, and the
+     * rest of the processing pipeline.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPreview != null) {
+            mPreview.release();
+        }
+    }
+
+    /**
+     * Callback for the result from requesting permissions. This method
+     * is invoked for every call on {@link #requestPermissions(String[], int)}.
+     * <p>
+     * <strong>Note:</strong> It is possible that the permissions request interaction
+     * with the user is interrupted. In this case you will receive empty permissions
+     * and results arrays which should be treated as a cancellation.
+     * </p>
+     *
+     * @param requestCode  The request code passed in {@link #requestPermissions(String[], int)}.
+     * @param permissions  The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *                     which is either {@link PackageManager#PERMISSION_GRANTED}
+     *                     or {@link PackageManager#PERMISSION_DENIED}. Never null.
+     * @see #requestPermissions(String[], int)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+            ExLog.i(this, TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            ExLog.i(this, TAG, "Camera permission granted - initialize the camera source");
+            // we have permission, so create the camerasource
+            createCameraSource();
+            return;
+        }
+
+        ExLog.e(this, TAG, "Permission not granted: results len = " + grantResults.length +
+                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppTheme_AlertDialog);
+        builder.setMessage("can't use camera")
+            .setPositiveButton(R.string.ok, listener)
+            .show();
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists.  If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    @SuppressWarnings("MissingPermission")
+    private void startCameraSource() throws SecurityException {
+        // check that the device has play services available.
+        int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getApplicationContext());
+        if (code != ConnectionResult.SUCCESS) {
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS);
+            dialog.show();
+        }
+
+        if (mCameraSource != null) {
+            try {
+                mPreview.start(mCameraSource);
+            } catch (IOException e) {
+                ExLog.e(this, TAG, "Unable to start camera source." + e);
+                mCameraSource.release();
+                mCameraSource = null;
+            }
+        }
+    }
+
+    @Override
+    public void onBarcodeDetected(Barcode barcode) {
+        ExLog.i(this, TAG, barcode.displayValue);
+        //do something with barcode data returned
+        Intent data = new Intent();
+        data.putExtra(BarcodeObject, barcode);
+        setResult(CommonStatusCodes.SUCCESS, data);
+        finish();
+    }
+}

mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/fragments/AbstractHomeFragment.java
@@ -11,19 +11,16 @@ import com.sap.sailing.android.shared.ui.adapters.AbstractRegattaAdapter;
 import com.sap.sailing.android.shared.util.BaseAppPreferences;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 public abstract class AbstractHomeFragment extends BaseFragment {
     private final static String TAG = AbstractHomeFragment.class.getName();
@@ -57,26 +54,22 @@ public abstract class AbstractHomeFragment extends BaseFragment {
                 R.string.no_qr_code_popup_message);
     }
 
-    private boolean requestQRCodeScan() {
+    private void requestQRCodeScan() {
         Intent intent = new Intent(getContext(), BarcodeCaptureActivity.class);
-        intent.putExtra(BarcodeCaptureActivity.AutoFocus, true);
         startActivityForResult(intent, requestCodeQRCode);
-
-        return true;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == requestCodeQRCode) {
-            if (resultCode == CommonStatusCodes.SUCCESS) {
-                Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
-                handleQRCode(barcode.displayValue);
-            } else {
-                ExLog.e(getActivity(), TAG, CommonStatusCodes.getStatusCodeString(resultCode));
-            }
-        }
-        else {
+        if (requestCode != requestCodeQRCode) {
             super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        if (resultCode == CommonStatusCodes.SUCCESS && data != null) {
+            Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+            handleQRCode(barcode.displayValue);
+        } else {
+            ExLog.e(getActivity(), TAG, CommonStatusCodes.getStatusCodeString(resultCode));
         }
     }
 

mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/utils/BarcodeGraphicTracker.java
@@ -1,0 +1,59 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sap.sailing.android.shared.ui.utils;
+
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import android.content.Context;
+import android.support.annotation.UiThread;
+
+/**
+ * Generic tracker which is used for tracking or reading a barcode (and can really be used for
+ * any type of item).  This is used to receive newly detected items, add a graphical representation
+ * to an overlay, update the graphics as the item changes, and remove the graphics when the item
+ * goes away.
+ */
+public class BarcodeGraphicTracker extends Tracker<Barcode> {
+
+    private BarcodeUpdateListener mBarcodeUpdateListener;
+
+    /**
+     * Consume the item instance detected from an Activity or Fragment level by implementing the
+     * BarcodeUpdateListener interface method onBarcodeDetected.
+     */
+    public interface BarcodeUpdateListener {
+        @UiThread
+        void onBarcodeDetected(Barcode barcode);
+    }
+
+    BarcodeGraphicTracker(Context context) {
+        if (context instanceof BarcodeUpdateListener) {
+            this.mBarcodeUpdateListener = (BarcodeUpdateListener) context;
+        } else {
+            throw new RuntimeException("Hosting activity must implement BarcodeUpdateListener");
+        }
+    }
+
+    /**
+     * Start tracking the detected item instance within the item overlay.
+     */
+    @Override
+    public void onNewItem(int id, Barcode item) {
+        mBarcodeUpdateListener.onBarcodeDetected(item);
+    }
+}

mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/utils/BarcodeTrackerFactory.java
@@ -1,0 +1,25 @@
+package com.sap.sailing.android.shared.ui.utils;
+
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import android.content.Context;
+
+/**
+ * Factory for creating a tracker to be associated with a new barcode. The
+ * multi-processor uses this factory to create barcode trackers as needed -- one for each barcode.
+ */
+public class BarcodeTrackerFactory implements MultiProcessor.Factory<Barcode> {
+    private Context mContext;
+
+    public BarcodeTrackerFactory(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public Tracker<Barcode> create(Barcode barcode) {
+        return new BarcodeGraphicTracker(mContext);
+    }
+}
+

mobile/com.sap.sailing.android.shared/src/com/sap/sailing/android/shared/ui/utils/CameraSourcePreview.java
@@ -1,0 +1,170 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sap.sailing.android.shared.ui.utils;
+
+import java.io.IOException;
+
+import com.google.android.gms.common.images.Size;
+import com.google.android.gms.vision.CameraSource;
+import com.sap.sailing.android.shared.logging.ExLog;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.RequiresPermission;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+
+public class CameraSourcePreview extends ViewGroup {
+    private static final String TAG = CameraSourcePreview.class.getName();
+
+    private Context mContext;
+    private SurfaceView mSurfaceView;
+    private boolean mStartRequested;
+    private boolean mSurfaceAvailable;
+    private CameraSource mCameraSource;
+
+    public CameraSourcePreview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        mStartRequested = false;
+        mSurfaceAvailable = false;
+
+        mSurfaceView = new SurfaceView(context);
+        mSurfaceView.getHolder().addCallback(new SurfaceCallback());
+        addView(mSurfaceView);
+    }
+
+    @RequiresPermission(Manifest.permission.CAMERA)
+    public void start(CameraSource cameraSource) throws IOException, SecurityException {
+        if (cameraSource == null) {
+            stop();
+        }
+
+        mCameraSource = cameraSource;
+
+        if (mCameraSource != null) {
+            mStartRequested = true;
+            startIfReady();
+        }
+    }
+
+    public void stop() {
+        if (mCameraSource != null) {
+            mCameraSource.stop();
+        }
+    }
+
+    public void release() {
+        if (mCameraSource != null) {
+            mCameraSource.release();
+            mCameraSource = null;
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.CAMERA)
+    private void startIfReady() throws IOException, SecurityException {
+        if (mStartRequested && mSurfaceAvailable) {
+            mCameraSource.start(mSurfaceView.getHolder());
+            mStartRequested = false;
+        }
+    }
+
+    private class SurfaceCallback implements SurfaceHolder.Callback {
+        @Override
+        public void surfaceCreated(SurfaceHolder surface) {
+            mSurfaceAvailable = true;
+            try {
+                startIfReady();
+            } catch (SecurityException se) {
+                ExLog.e(getContext(), TAG,"Do not have permission to start the camera" + se);
+            } catch (IOException e) {
+                ExLog.e(getContext(), TAG, "Could not start camera source." + e);
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surface) {
+            mSurfaceAvailable = false;
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int width = 320;
+        int height = 240;
+        if (mCameraSource != null) {
+            Size size = mCameraSource.getPreviewSize();
+            if (size != null) {
+                width = size.getWidth();
+                height = size.getHeight();
+            }
+        }
+
+        // Swap width and height sizes when in portrait, since it will be rotated 90 degrees
+        if (isPortraitMode()) {
+            int tmp = width;
+            //noinspection SuspiciousNameCombination
+            width = height;
+            height = tmp;
+        }
+
+        final int layoutWidth = right - left;
+        final int layoutHeight = bottom - top;
+
+        // Computes height and width for potentially doing fit width.
+        int childWidth = layoutWidth;
+        int childHeight = (int)(((float) layoutWidth / (float) width) * height);
+
+        // If height is too tall using fit width, does fit height instead.
+        if (childHeight > layoutHeight) {
+            childHeight = layoutHeight;
+            childWidth = (int)(((float) layoutHeight / (float) height) * width);
+        }
+
+        for (int i = 0; i < getChildCount(); ++i) {
+            getChildAt(i).layout(0, 0, childWidth, childHeight);
+        }
+
+        try {
+            startIfReady();
+        } catch (SecurityException se) {
+            ExLog.e(getContext(), TAG,"Do not have permission to start the camera" + se);
+        } catch (IOException e) {
+            ExLog.e(getContext(), TAG, "Could not start camera source." + e);
+        }
+    }
+
+    private boolean isPortraitMode() {
+        int orientation = mContext.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            return false;
+        }
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            return true;
+        }
+
+        ExLog.i(getContext(), TAG, "isPortraitMode returning false by default");
+        return false;
+    }
+}

commit a13a94a0087c973d7e0ef2a94156c5e35f6c0c89

bug 4842: reverted barcode scanner problem introduced by 4ad86839159266f68648c9a0ebe4671d69e05d3c:
while introducing the use of the built-in barcode/QR-code scanner, a
comparison of the requestCode with the requestCodeQRCode was changed for
no obvious reason. Changing it back to the old comparison made this work
again for me.

Change-Id: I9fe7268d8a9c12a2877568157a4752dd96b77756

mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/fragments/preference/GeneralPreferenceFragment.java
@@ -185,7 +185,7 @@ public class GeneralPreferenceFragment extends BasePreferenceFragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == requestCodeQRCode) {
+        if (requestCode != requestCodeQRCode) {
             super.onActivityResult(requestCode, resultCode, data);
             return;
         }