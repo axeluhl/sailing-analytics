447a3b7bd31fb718329edef7b3b4c91dd75c0f30
Robin Kuck <robin.kuck@sap.com>
2019-01-05 18:14:34 +0100

    bug4469 edited info text of pairinglist dialog + added javadoc

--- java/com.sap.sse/src/com/sap/sse/pairinglist/PairingListTemplateFactory.java
+++ java/com.sap.sse/src/com/sap/sse/pairinglist/PairingListTemplateFactory.java
@@ -10,6 +10,8 @@ public interface PairingListTemplateFactory {
      * 
      * @param pairingFrameProvider interface that has all necessary parameters
      * @param flightMultiplier specifies how often the flights will be repeated within a single {@link PairingList}
+     * @param boatChangeFactor specifies the priority of well distributed assignment of competitors to boats (smallest factor) or minimization of boat changes within a {@link PairingList} (highest factor);
+     * valid factors are 0 - competitors/groups 
      * @return PairingListTemplate correlates with the new {@link PairingListTemplate}
      */
     PairingListTemplate createPairingListTemplate(PairingFrameProvider pairingFrameProvider, int flightMultiplier, int boatChangeFactor);
