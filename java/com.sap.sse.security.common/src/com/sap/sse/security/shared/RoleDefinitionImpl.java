f6979e0dbca723192ee54c28d57c852dc82065ac
Kai Börnert <kai.boernert@oio.de>
2019-01-10 15:21:14 +0100

    removed obsolete possibly wrong code

--- java/com.sap.sse.security.common/src/com/sap/sse/security/shared/RoleDefinitionImpl.java
+++ java/com.sap.sse.security.common/src/com/sap/sse/security/shared/RoleDefinitionImpl.java
@@ -35,15 +35,7 @@ public class RoleDefinitionImpl implements RoleDefinition {
     }
 
     public static RoleDefinition create(UUID id, String name, Iterable<WildcardPermission> permissions) {
-        final RoleDefinition result;
-        if (Util.equalsWithNull(id, AdminRole.getInstance().getId())) {
-            result = AdminRole.getInstance();
-        } else if (Util.equalsWithNull(id, UserRole.getInstance().getId())) {
-            result = UserRole.getInstance();
-        } else {
-            result = new RoleDefinitionImpl(id, name, permissions);
-        }
-        return result;
+        return new RoleDefinitionImpl(id, name, permissions);
     }
 
     @Override
