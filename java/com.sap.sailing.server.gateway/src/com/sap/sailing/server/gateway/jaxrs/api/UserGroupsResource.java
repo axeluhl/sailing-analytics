293c29ab06c836bf931b1214e66004157ae66d23
Kai Börnert <kai.boernert@oio.de>
2019-01-30 16:02:50 +0100

    added rest calls to allow adding roles to groups, remove them and query what roles a group has

--- java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/jaxrs/api/UserGroupsResource.java
+++ java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/jaxrs/api/UserGroupsResource.java
@@ -1,8 +1,15 @@
 package com.sap.sailing.server.gateway.jaxrs.api;
 
+import java.util.Map.Entry;
+import java.util.UUID;
+
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 import org.json.simple.JSONArray;
@@ -11,6 +18,7 @@ import org.json.simple.parser.ParseException;
 
 import com.sap.sailing.server.gateway.deserialization.JsonDeserializationException;
 import com.sap.sailing.server.gateway.jaxrs.AbstractSailingServerResource;
+import com.sap.sse.security.shared.RoleDefinition;
 import com.sap.sse.security.shared.impl.User;
 import com.sap.sse.security.shared.impl.UserGroup;
 
@@ -33,6 +41,72 @@ public class UserGroupsResource extends AbstractSailingServerResource {
                 groups.add(groupJson);
             }
             response = Response.ok(root.toJSONString()).build();
+        } else {
+            response = Response.status(401).build();
+        }
+        return response;
+    }
+
+    @GET
+    @Path("{userGroupId}/roles")
+    @Produces("application/json;charset=UTF-8")
+    public Response getUserGroup(@PathParam("userGroupId") UUID userGroupId)
+            throws ParseException, JsonDeserializationException {
+        Response response = null;
+        UserGroup userGroup = getService().getSecurityService().getUserGroup(userGroupId);
+        getSecurityService().checkCurrentUserReadPermission(userGroup);
+        if (userGroup != null) {
+            JSONObject root = new JSONObject();
+            root.put("uuid", userGroup.getId().toString());
+            root.put("name", userGroup.getName());
+            JSONArray roles = new JSONArray();
+            for (Entry<RoleDefinition, Boolean> roleDef : userGroup.getRoleDefinitionMap().entrySet()) {
+                JSONObject role = new JSONObject();
+                role.put("roleId", roleDef.getKey().getId().toString());
+                role.put("name", roleDef.getKey().getName());
+                role.put("all", roleDef.getValue());
+                roles.add(role);
+            }
+            root.put("roles", roles);
+            response = Response.ok(root.toJSONString()).build();
+        } else {
+            response = Response.status(401).build();
+        }
+        return response;
+    }
+    
+    @DELETE
+    @Path("{userGroupId}/roles")
+    @Produces("application/json;charset=UTF-8")
+    public Response getUserGroup(@PathParam("userGroupId") UUID userGroupId, @QueryParam("roleId") UUID roleId)
+            throws ParseException, JsonDeserializationException {
+        Response response = null;
+        UserGroup userGroup = getService().getSecurityService().getUserGroup(userGroupId);
+        RoleDefinition roleDefinitions = getSecurityService().getRoleDefinition(roleId);
+        if (userGroup != null && roleDefinitions != null) {
+            getSecurityService().checkCurrentUserUpdatePermission(userGroup);
+            // do not check read on role, to allow removal of not anymore visible roles
+            getSecurityService().removeRoleDefintionFromUserGroup(userGroup, roleDefinitions);
+            response = Response.ok().build();
+        } else {
+            response = Response.status(401).build();
+        }
+        return response;
+    }
+
+    @PUT
+    @Path("{userGroupId}/roles")
+    @Produces("application/json;charset=UTF-8")
+    public Response addRoleToUserGroup(@PathParam("userGroupId") UUID userGroupId, @QueryParam("roleId") UUID roleId,
+            @QueryParam("forAll") Boolean forAll) throws ParseException, JsonDeserializationException {
+        Response response = null;
+        UserGroup userGroup = getService().getSecurityService().getUserGroup(userGroupId);
+        RoleDefinition roleDefinitions = getSecurityService().getRoleDefinition(roleId);
+        if (userGroup != null && roleDefinitions != null) {
+            getSecurityService().checkCurrentUserUpdatePermission(userGroup);
+            getSecurityService().checkCurrentUserReadPermission(roleDefinitions);
+            getSecurityService().putRoleDefinitionToUserGroup(userGroup, roleDefinitions, forAll);
+            response = Response.ok().build();
         } else {
             response = Response.status(401).build();
         }
