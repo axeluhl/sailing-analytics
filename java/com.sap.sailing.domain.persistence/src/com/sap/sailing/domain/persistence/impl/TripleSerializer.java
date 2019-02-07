4ff49ea1e735e2aca6d6e0fa4ab53421b748fb67
Axel Uhl <axel.uhl@sap.com>
2019-02-07 12:50:24 +0100

    changed TripleSerializer from DBObject to Document
    Change-Id: Icb839a64f8484431da4200efdcf244b08677e32a

--- java/com.sap.sailing.domain.persistence/src/com/sap/sailing/domain/persistence/impl/TripleSerializer.java
+++ java/com.sap.sailing.domain.persistence/src/com/sap/sailing/domain/persistence/impl/TripleSerializer.java
@@ -1,12 +1,12 @@
 package com.sap.sailing.domain.persistence.impl;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import org.bson.Document;
+
 import com.sap.sse.common.Util;
 
 public class TripleSerializer {
-    public static DBObject serialize(Util.Triple<String, String, String> value) {
-        DBObject container = new BasicDBObject();
+    public static Document serialize(Util.Triple<String, String, String> value) {
+        Document container = new Document();
         container.put("a", value.getA());
         container.put("b", value.getB());
         container.put("c", value.getC());
