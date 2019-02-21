8db23f6d0f4d6e682fef965e9cc1b20fdfb998c1
Steffen Schaefer <steffen.schaefer@oio.de>
2019-02-21 10:47:21 +0100

    Moderator now also has DETAIL_TIMER permission

--- java/com.sap.sse.security.common/src/com/sap/sse/security/shared/PredefinedRoles.java
+++ java/com.sap.sse.security.common/src/com/sap/sse/security/shared/PredefinedRoles.java
@@ -14,7 +14,7 @@ import java.util.UUID;
  */
 public enum PredefinedRoles {
     spectator(UUID.fromString("244cb84c-2b8a-4557-b175-db963072cfbc"), "spectator", Collections.<String>emptyList()),
-    moderator(UUID.fromString("91c88a11-c977-4c24-b0a6-15bac0525764"), "moderator", asList("TRACKED_RACE:CAN_REPLAY_DURING_LIVE_RACES")),
+    moderator(UUID.fromString("91c88a11-c977-4c24-b0a6-15bac0525764"), "moderator", asList("TRACKED_RACE:CAN_REPLAY_DURING_LIVE_RACES,DETAIL_TIMER")),
     mediaeditor(UUID.fromString("2e22583b-c9c1-4ee4-8231-e8e320a8a411"), "mediaeditor", asList("MEDIA_TRACK:*"));
     
     private final UUID id;
