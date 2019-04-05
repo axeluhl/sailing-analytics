330bbb940018a1f1c24807839e5ed5955328c3ba
Steffen Jacobs <steffen.jacobs@oio.de>
2019-04-05 14:08:59 +0200

    Bug 4972: fixed problem with broken change-password dialog by introducing an existance-check for username

--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/client/component/ChangePasswordDialog.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/client/component/ChangePasswordDialog.java
@@ -1,14 +1,14 @@
 package com.sap.sse.security.ui.client.component;
 
-import com.sap.sse.security.shared.dto.UserDTO;
 import com.google.gwt.user.client.ui.FocusWidget;
+import com.sap.sse.security.shared.dto.UserDTO;
 import com.sap.sse.security.ui.client.UserManagementServiceAsync;
 import com.sap.sse.security.ui.client.i18n.StringMessages;
 
 public class ChangePasswordDialog extends AbstractUserDialog {
     public ChangePasswordDialog(StringMessages stringMessages, UserManagementServiceAsync userManagementService, UserDTO user,
             com.sap.sse.gwt.client.dialog.DataEntryDialog.DialogCallback<UserData> callback) {
-        super(stringMessages, stringMessages.changePassword(), userManagementService, user, callback);
+        super(stringMessages, stringMessages.changePassword(), userManagementService, user, callback, false);
         getNameBox().setEnabled(false);
         getEmailBox().setEnabled(false);
     }
