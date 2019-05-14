9786aae8e2d9e91d30d2d60a47a672d32ee0671a
Axel Uhl <axel.uhl@sap.com>
2019-05-14 12:36:02 +0200

    removed fixed FIXME
    Change-Id: I74444dff89954e942d124af0ca25db51c73b32a1

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/markpassingcalculation/impl/StationarySequenceBasedFilter.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/markpassingcalculation/impl/StationarySequenceBasedFilter.java
@@ -494,9 +494,6 @@ public class StationarySequenceBasedFilter {
      * sequence is started, adding all remaining candidates. Any sequence resulting from such a split and having only
      * one candidate left is removed. The candidates immediately left and right of the split now pass the filter and
      * therefore are added to the {@link #allEdges graph}.</li>
-     * FIXME if the fix replaces another one within a sequence, the bounding box may need to shrink. Otherwise, later
-     * additions that expand the bounding box seemingly beyond thresholds may fail to re-evaluate all fixes of the
-     * sequence. A simple fix may be to re-evaluate the bounding box entirely upon replacements.
      * <li>A GPS fix was added outside any stationary sequence. Nothing changes because no bounding box would get
      * smaller by a new fix added.</li>
      * <li>A GPS fix <em>replaces</em> an existing one outside of any stationary sequence. In this case it is possible
