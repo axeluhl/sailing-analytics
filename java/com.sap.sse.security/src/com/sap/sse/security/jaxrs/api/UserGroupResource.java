10c09fb990f3ae8c4279ce9e0f8ddbafb29deaa4
Steffen Jacobs <steffen.jacobs@oio.de>
2019-05-22 17:20:36 +0200

    Bug 4892: added PUT /restsecurity/usergroup to create a new usergroup

--- java/com.sap.sse.security/src/com/sap/sse/security/jaxrs/api/UserGroupResource.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/jaxrs/api/UserGroupResource.java
@@ -3,6 +3,7 @@ package com.sap.sse.security.jaxrs.api;
 import java.util.UUID;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -12,8 +13,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.sap.sse.security.jaxrs.AbstractSecurityResource;
+import com.sap.sse.security.shared.UserGroupManagementException;
+import com.sap.sse.security.shared.impl.SecuredSecurityTypes;
 import com.sap.sse.security.shared.impl.User;
 import com.sap.sse.security.shared.impl.UserGroup;
+import com.sap.sse.security.shared.impl.UserGroupImpl;
 import com.sun.jersey.api.client.ClientResponse.Status;
 
 @Path("/restsecurity/usergroup")
@@ -29,21 +33,57 @@ public class UserGroupResource extends AbstractSecurityResource {
             response = Response.status(Status.BAD_REQUEST).entity("Usergroup with this id does not exist.").build();
         } else {
             if (getService().hasCurrentUserReadPermission(usergroup)) {
-                final JSONObject jsonResult = new JSONObject();
-                jsonResult.put("id", usergroup.getId());
-                jsonResult.put("groupname", usergroup.getName());
-
-                final JSONArray jsonUsersInGroup = new JSONArray();
-                for (final User user : usergroup.getUsers()) {
-                    // filter users
-                    if (getService().hasCurrentUserReadPermission(user)) {
-                        jsonUsersInGroup.add(user.getId());
-                    }
-                }
-                jsonResult.put("users", jsonUsersInGroup);
+                final JSONObject jsonResult = convertUserGroupToJson(usergroup);
                 response = Response.ok(jsonResult.toJSONString()).build();
             } else {
                 response = Response.status(Status.UNAUTHORIZED).build();
+            }
+        }
+
+        return response;
+    }
+
+    private JSONObject convertUserGroupToJson(final UserGroup usergroup) {
+        final JSONObject jsonResult = new JSONObject();
+        jsonResult.put("id", usergroup.getId().toString());
+        jsonResult.put("groupname", usergroup.getName());
+
+        final JSONArray jsonUsersInGroup = new JSONArray();
+        for (final User user : usergroup.getUsers()) {
+            // filter users
+            if (getService().hasCurrentUserReadPermission(user)) {
+                jsonUsersInGroup.add(user.getId());
+            }
+        }
+        jsonResult.put("users", jsonUsersInGroup);
+        System.out.println(jsonResult.toJSONString());
+        return jsonResult;
+    }
+
+    @PUT
+    @Produces("application/json;charset=UTF-8")
+    public Response createUserGroup(@QueryParam("groupName") String groupName) {
+        final Response response;
+        final UserGroup usergroup = getService().getUserGroupByName(groupName);
+        if (usergroup != null) {
+            response = Response.status(Status.BAD_REQUEST).entity("Usergroup with this name already exists.").build();
+        } else {
+            UUID newTenantId = UUID.randomUUID();
+            UserGroup group = getService().setOwnershipWithoutCheckPermissionForObjectCreationAndRevertOnError(
+                    SecuredSecurityTypes.USER_GROUP, UserGroupImpl.getTypeRelativeObjectIdentifier(newTenantId),
+                    groupName, () -> {
+                        UserGroup userGroup;
+                        try {
+                            userGroup = getService().createUserGroup(newTenantId, groupName);
+                        } catch (UserGroupManagementException e) {
+                            throw new UserGroupManagementException(e.getMessage());
+                        }
+                        return userGroup;
+                    });
+            if (group == null) {
+                response = Response.status(Status.INTERNAL_SERVER_ERROR).entity("Could not create user group.").build();
+            } else {
+                response = Response.status(Status.CREATED).entity(convertUserGroupToJson(group).toJSONString()).build();
             }
         }
 
