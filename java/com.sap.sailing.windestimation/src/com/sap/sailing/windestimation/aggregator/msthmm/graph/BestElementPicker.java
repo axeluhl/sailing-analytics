57ca82c2272b98019c0e2a7cbe4d6b2428ed2c9b
Axel Uhl <axel.uhl@sap.com>
2019-07-12 23:49:02 +0200

    more interfaces for graph solving
    Change-Id: Id560729019d398a66cf051dc65cb1be9159f62b2

---
+++ java/com.sap.sailing.windestimation/src/com/sap/sailing/windestimation/aggregator/msthmm/graph/BestElementPicker.java
@@ -0,0 +1,15 @@
+package com.sap.sailing.windestimation.aggregator.msthmm.graph;
+
+/**
+ * An algorithm that, given a tree of element groups, from each group picks an element considered "best" according to
+ * two metrics: the {@link ElementWithQuality#getQuality() quality} of the element itself, and the
+ * {@link ElementAdjacencyQualityMetric} that evaluates the quality of the neighborhood of two elements in the
+ * {@link Tree}.
+ * 
+ * @author Axel Uhl (D043530)
+ *
+ * @param <T>
+ */
+public interface BestElementPicker<T extends ElementWithQuality> {
+    Iterable<T> getBestElements(Tree<GroupOutOfWhichToPickTheBestElement<T>> tree);
+}
