1086ebbe2b7238247df90543d0b9584cceebb47b
Steffen Schaefer <steffen.schaefer@oio.de>
2019-07-17 16:45:47 +0200

    Bug 5060: Added SIMULATOR action of TRACKED_RACE type to the initial sailing_viewer role definition / Removed READ permission for SIMULATOR type from sailing_viewer role definition

--- java/com.sap.sailing.server/src/com/sap/sailing/server/security/SailingViewerRole.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/security/SailingViewerRole.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.server.security;
 
 import com.sap.sailing.domain.common.security.SecuredDomainType;
+import com.sap.sailing.domain.common.security.SecuredDomainType.TrackedRaceActions;
 import com.sap.sse.security.shared.HasPermissions.DefaultActions;
 import com.sap.sse.security.shared.RolePrototype;
 import com.sap.sse.security.shared.WildcardPermission;
@@ -19,9 +20,11 @@ public class SailingViewerRole extends RolePrototype {
         super("sailing_viewer", "c42948df-517b-45cb-9fa9-d1e79f18e115",
                 WildcardPermission.builder().withTypes(SecuredDomainType.EVENT, SecuredDomainType.LEADERBOARD_GROUP,
                         SecuredDomainType.LEADERBOARD, SecuredDomainType.REGATTA, SecuredDomainType.TRACKED_RACE,
-                        SecuredDomainType.SIMULATOR, SecuredDomainType.MEDIA_TRACK)
-                        .withActions(DefaultActions.READ).build(), WildcardPermission.builder().withTypes(SecuredDomainType.COMPETITOR, SecuredDomainType.BOAT)
-                        .withActions(SecuredSecurityTypes.PublicReadableActions.READ_PUBLIC).build());
+                        SecuredDomainType.MEDIA_TRACK).withActions(DefaultActions.READ).build(),
+                        WildcardPermission.builder().withTypes(SecuredDomainType.COMPETITOR, SecuredDomainType.BOAT)
+                        .withActions(SecuredSecurityTypes.PublicReadableActions.READ_PUBLIC).build(),
+                        WildcardPermission.builder().withTypes(SecuredDomainType.TRACKED_RACE)
+                        .withActions(TrackedRaceActions.SIMULATOR).build());
     }
 
     public static SailingViewerRole getInstance() {
