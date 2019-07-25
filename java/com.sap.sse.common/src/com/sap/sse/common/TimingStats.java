4805906dcb6031d7d497e0993f98c3ee7b7414ee
Axel Uhl <axel.uhl@sap.com>
2019-07-25 16:22:43 +0200

    added TimingStats with tests
    Change-Id: I29557531d1b144fe9b9af86f0aaeff99db6f54f3

--- java/com.sap.sse.common/src/com/sap/sse/common/TimingStats.java
+++ java/com.sap.sse.common/src/com/sap/sse/common/TimingStats.java
@@ -1,9 +1,9 @@
 package com.sap.sse.common;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -11,14 +11,20 @@ import com.sap.sse.common.impl.MillisecondsDurationImpl;
 import com.sap.sse.common.impl.MillisecondsTimePoint;
 
 /**
- * Keep timing statistics for a service, an object, or some access method.
- * Each access is expected to be notified to this object. In turn, this
- * object will maintain averages about access timings for one or more
- * durations from "now" backwards into the past.<p>
+ * Keep timing statistics for a service, an object, or some access method. Each access is expected to be notified to
+ * this object by calling {@link #recordTiming(TimePoint, Duration)}. In turn, this object will maintain averages about
+ * access timings for one or more durations from "now" backwards into the past, grouped by a set of "ages" that can
+ * be specified at {@link #TimingStats(Duration...) construction}. These averages can be obtained by calling
+ * {@link #getAverageDurations()} for the current time.
+ * <p>
  * 
- * For example, an object of this type can be configured to keep statistics
- * about the last 10s, 30s, 1min, and 5min, for each keeping the average,
- * minimum and maximum time.
+ * For example, an object of this type can be configured to keep statistics about the last 10s, 30s, 1min, and 5min, for
+ * each keeping the average, minimum and maximum time.
+ * <p>
+ * 
+ * The boundary durations are inclusive, meaning that an entry {@link #recordTiming(TimePoint, Duration) recorded} is
+ * considered part of the range represented by the age passed to the constructor if it is exactly of that age at the
+ * time of recording.
  * 
  * @author Axel Uhl (d043530)
  *
@@ -55,12 +61,19 @@ public class TimingStats {
             }
             return result;
         }
+        @Override
+        public String toString() {
+            return "LogEntry [when=" + when + ", howLong=" + howLong + ", id=" + id + "]";
+        }
     }
     
     /**
-     * The durations for which to maintain averages; sorted from short to long
+     * Keys are the durations for which to maintain averages; the values are maintained transactionally; the elements in
+     * represent the sum of all {@link LogEntry#getHowLong() durations} of all {@link #logEntries} that are
+     * no older (considering the time point when {@link #updateStatsBasedOnNewNow()} was called last) than
+     * the corresponding key tells.
      */
-    private final Duration[] agesForAverages;
+    private final Map<Duration, AtomicLong> agesForAveragesAndTheirDurationSumsInMillis;
     
     /**
      * All log entries, with those removed by {@link #updateStatsBasedOnNewNow()} that are more than
@@ -71,14 +84,6 @@ public class TimingStats {
      */
     private final ConcurrentSkipListSet<LogEntry> logEntries;
 
-    /**
-     * Maintained transactionally, the elements in this array represent the sum of all
-     * {@link LogEntry#getHowLong() durations} of all {@link #logEntries} that are no older
-     * (considering the time point when {@link #updateStatsBasedOnNewNow()} was called last)
-     * than {@link #agesForAverages} for the respective array index.
-     */
-    private final AtomicLong[] durationsSumsInMillis;
-    
     private AtomicLong idCounter;
     
     /**
@@ -88,54 +93,89 @@ public class TimingStats {
     private TimePoint lastNow;
 
     public TimingStats(Duration... agesForAverages) {
-        lastNow = MillisecondsTimePoint.now();
-        idCounter = new AtomicLong(0);
-        this.agesForAverages = Arrays.copyOf(agesForAverages, agesForAverages.length);
-        Arrays.sort(this.agesForAverages);
-        logEntries = new ConcurrentSkipListSet<>();
-        durationsSumsInMillis = new AtomicLong[agesForAverages.length];
-        for (int i=0; i<agesForAverages.length; i++) {
-            durationsSumsInMillis[i] = new AtomicLong(0);
+        this(MillisecondsTimePoint.now(), agesForAverages);
+    }
+    
+    TimingStats(TimePoint now, Duration... agesForAverages) {
+        if (agesForAverages.length == 0) {
+            throw new IllegalArgumentException("At least one age must be provided");
         }
+        lastNow = now;
+        idCounter = new AtomicLong(0);
+        agesForAveragesAndTheirDurationSumsInMillis = new HashMap<>();
+        for (final Duration ageForAverages : agesForAverages) {
+            agesForAveragesAndTheirDurationSumsInMillis.put(ageForAverages, new AtomicLong(0));
+        }
+        logEntries = new ConcurrentSkipListSet<>();
     }
     
     public void recordTiming(TimePoint when, Duration howLong) {
         final TimePoint newNow = MillisecondsTimePoint.now();
+        recordTiming(newNow, when, howLong);
+    }
+    
+    void recordTiming(TimePoint newNow, TimePoint when, Duration howLong) {
         updateStatsBasedOnNewNow(newNow);
-        final Duration ageOfNewEntry = newNow.until(when);
+        final Duration ageOfNewEntry = when.until(newNow);
         logEntries.add(new LogEntry(when, howLong, idCounter.getAndIncrement()));
-        for (int i=0; i<agesForAverages.length && agesForAverages[i].compareTo(ageOfNewEntry) >= 0; i++) {
-            durationsSumsInMillis[i].addAndGet(howLong.asMillis());
+        for (final Entry<Duration, AtomicLong> e : agesForAveragesAndTheirDurationSumsInMillis.entrySet()) {
+            if (ageOfNewEntry.compareTo(e.getKey()) <= 0) {
+                e.getValue().addAndGet(howLong.asMillis());
+            }
         }
     }
 
     /**
      * Purges aged events from their old slots and updates the {@link #durationsSumsInMillis} accordingly. When the
-     * method returns, {@link #durationsSumsInMillis} is consistent with {@link #logEntries} based on {@link #lastNow}
-     * for determining the {@link LogEntry}'s ages. Entries with a {@link LogEntry#getWhen()} more than the greatest
-     * {@link #agesForAverages} before {@code newNow} will be removed from {@link #logEntries}, and of course from their
-     * respective {@link #durationsSumsInMillis}.
+     * method returns, the durations stored as values in {@link #agesForAveragesAndTheirDurationSumsInMillis} are
+     * consistent with {@link #logEntries} based on {@link #lastNow} for determining the {@link LogEntry}'s ages.
+     * Entries with a {@link LogEntry#getWhen()} more than the greatest key in
+     * {@link #agesForAveragesAndTheirDurationSumsInMillis} before {@code newNow} will be removed from
+     * {@link #logEntries}, and of course from their respective {@link #durationsSumsInMillis}.
      * <p>
      * 
-     * Analyzes all {@link #logEntries} from oldest to newest and computes each entry's {@link LogEntry#getWhen()} age
-     * based on the {@code newNow} and on the {@link #lastNow}. It then maps the two ages to the
-     * {@link #agesForAverages} groups. If both fall into the same group, there is no change for the event, and we can
-     * proceed to the next group. Otherwise, the event dropped out of its old group because of increased age and now has
-     * to be added to a new group or has to be eliminated if older than the maximum age of the oldest group. The
-     * previous group's {@link #durationsSumsInMillis} is reduced by the entry's {@link LogEntry#getHowLong() duration},
-     * and if the event ends up in a new group instead of dropping out of this object's stats, the new group's
-     * {@link #durationsSumsInMillis} is increased by the entry's duration.
+     * For each key in {@link #agesForAveragesAndTheirDurationSumsInMillis} and based on the {@link #lastNow} time point and
+     * {@code newNow}, finds all {@link LogEntry entries} in {@link #logEntries} that aged beyond the key duration.
+     * For each such entry their {@link LogEntry#getHowLong()} duration is subtracted from the duration stored in
+     * the value of {@link #agesForAveragesAndTheirDurationSumsInMillis}.<p>
+     * 
+     * Eventually, the {@link LogEntry entries} older than the greatest key from {@link #agesForAveragesAndTheirDurationSumsInMillis}
+     * are removed from {@link #logEntries}.<p>
+     * 
+     * @param newNow if earlier than {@link #lastNow}, this call is a no-op and the method returns immediately
      */
     private void updateStatsBasedOnNewNow(final TimePoint newNow) {
         // TODO synchronization / locking?
-        Iterator<LogEntry> i = logEntries.descendingIterator();
-        if (i.hasNext()) {
-            final LogEntry latestEntry = i.next();
-            
+        if (newNow.after(lastNow)) {
+            Duration oldestAge = Duration.NULL;
+            for (final Entry<Duration, AtomicLong> e : agesForAveragesAndTheirDurationSumsInMillis.entrySet()) {
+                if (e.getKey().compareTo(oldestAge) > 0) {
+                    oldestAge = e.getKey();
+                }
+                // An entry was considered in e's duration sum if and only if at lastNow its age,
+                // which is LogEntry.getWhen().until(lastNow) was less than or equal to e's key duration.
+                // It shall no longer be in e's duration sum if LogEntry().getWhen().until(newNow) is greater than
+                // e's key duration.
+                // Therefore, in order to find exactly those LogEntry elements that were but no longer are in the
+                // right time interval we need to obtain all LogEntry objects whose getWhen() is at or after
+                // lastNow.minus(e.getKey()) and before newNow.minus(e.getKey()).
+                final TimePoint removeIntervalStart = lastNow.minus(e.getKey());
+                final TimePoint removeIntervalEnd = newNow.minus(e.getKey());
+                if (removeIntervalStart.before(removeIntervalEnd)) {
+                    for (final LogEntry entrySkippingToNext : logEntries.subSet(
+                            timePointLogEntry(removeIntervalStart), /* fromInclusive */ true,
+                            timePointLogEntry(removeIntervalEnd), /* toInclusive */ false)) {
+                        e.getValue().addAndGet(-entrySkippingToNext.getHowLong().asMillis());
+                    }
+                }
+            }
+            final Iterator<LogEntry> removingIter = logEntries.headSet(timePointLogEntry(newNow.minus(oldestAge)), /* inclusive */ false).iterator();
+            while (removingIter.hasNext()) {
+                removingIter.next();
+                removingIter.remove();
+            }
+            lastNow = newNow;
         }
-            
-        // TODO Auto-generated method stub
-        lastNow = newNow;
     }
     
     /**
@@ -146,14 +186,18 @@ public class TimingStats {
      */
     public Map<Duration, Duration> getAverageDurations() {
         final TimePoint now = MillisecondsTimePoint.now();
+        return getAverageDurations(now);
+    }
+    
+    Map<Duration, Duration> getAverageDurations(TimePoint now) {
+        updateStatsBasedOnNewNow(now);
         final LogEntry dummyLogEntryForNow = timePointLogEntry(now);
         final Map<Duration, Duration> result = new HashMap<>();
-        updateStatsBasedOnNewNow(now);
-        for (int i=0; i<agesForAverages.length; i++) {
+        for (final Entry<Duration, AtomicLong> e : agesForAveragesAndTheirDurationSumsInMillis.entrySet()) {
             final int eventCount = logEntries.subSet(
-                    timePointLogEntry(now.minus(agesForAverages[i])), /* toInclusive */ false,
-                    dummyLogEntryForNow, /* fromInclusive */ true).size();
-            result.put(agesForAverages[i], new MillisecondsDurationImpl(durationsSumsInMillis[i].get() / eventCount));
+                    timePointLogEntry(now.minus(e.getKey())), /* fromInclusive */ true,
+                    dummyLogEntryForNow, /* toInclusive */ true).size();
+            result.put(e.getKey(), eventCount == 0 ? null : new MillisecondsDurationImpl(e.getValue().get() / eventCount));
         }
         return result;
     }
