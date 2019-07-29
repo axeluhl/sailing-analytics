245d9862c6fc77c1f9a805eb41cfca9b4a7ded33
Steffen Jacobs <steffen.jacobs@oio.de>
2019-07-29 17:45:46 +0200

    Bug 5085: fixed compile error in CourseAndMarkMappingFactoryImpl

--- java/com.sap.sailing.server/src/com/sap/sailing/server/impl/CourseAndMarkMappingFactoryImpl.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/impl/CourseAndMarkMappingFactoryImpl.java
@@ -1,7 +1,9 @@
 package com.sap.sailing.server.impl;
 
 import com.sap.sailing.domain.base.Course;
+import com.sap.sailing.domain.base.Regatta;
 import com.sap.sailing.domain.coursetemplate.CourseTemplate;
+import com.sap.sailing.domain.coursetemplate.CourseTemplateMapping;
 import com.sap.sailing.server.interfaces.CourseAndMarkMappingFactory;
 
 public class CourseAndMarkMappingFactoryImpl implements CourseAndMarkMappingFactory {
@@ -16,6 +18,19 @@ public class CourseAndMarkMappingFactoryImpl implements CourseAndMarkMappingFact
     public void updateCourse(Course courseToUpdate, CourseTemplate courseTemplate, int numberOfLaps) {
         // TODO Auto-generated method stub
 
+    }
+
+    @Override
+    public CourseTemplateMapping createMappingForCourseTemplate(Regatta regatta, CourseTemplate courseTemplate) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Course createCourseFromMappingAndDefineMarksAsNeeded(Regatta regatta,
+            CourseTemplateMapping courseTemplateMapping, int numberOfLaps) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
