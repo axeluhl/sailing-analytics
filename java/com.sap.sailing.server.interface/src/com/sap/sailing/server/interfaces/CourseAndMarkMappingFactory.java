24e5a71b2dbc4172b60b8f733918980de5e961a2
Steffen Jacobs <steffen.jacobs@oio.de>
2019-07-29 17:50:59 +0200

    Bug 5085: linked new CourseTemplateWithMarktemplateMappings in CourseAndMarkMappingFactory

--- java/com.sap.sailing.server.interface/src/com/sap/sailing/server/interfaces/CourseAndMarkMappingFactory.java
+++ java/com.sap.sailing.server.interface/src/com/sap/sailing/server/interfaces/CourseAndMarkMappingFactory.java
@@ -5,6 +5,7 @@ import com.sap.sailing.domain.base.CourseBase;
 import com.sap.sailing.domain.base.Regatta;
 import com.sap.sailing.domain.coursetemplate.CourseTemplate;
 import com.sap.sailing.domain.coursetemplate.CourseTemplateMapping;
+import com.sap.sailing.domain.coursetemplate.CourseTemplateWithMarkTemplateMappings;
 
 /**
  * From a {@link CourseTemplate} constructs or updates a {@link CourseBase}.
@@ -20,9 +21,10 @@ public interface CourseAndMarkMappingFactory {
     CourseTemplateMapping createMappingForCourseTemplate(Regatta regatta, CourseTemplate courseTemplate);
 
     // TODO Define Course based on CourseTemplateMapping that internally references MarkTemplateMappings
-
+    CourseTemplateWithMarkTemplateMappings createCourseTemplateMappingFromMapping(Regatta regatta,
+            CourseTemplateMapping courseTemplateMapping, int numberOfLaps);
     // TODO Do we need to loosely couple creation of DefineMarkEvents for the Regatta
-    Course createCourseFromMappingAndDefineMarksAsNeeded(Regatta regatta, CourseTemplateMapping courseTemplateMapping,
-            int numberOfLaps);
+    Course createCourseFromMappingAndDefineMarksAsNeeded(Regatta regatta,
+            CourseTemplateWithMarkTemplateMappings courseTemplateMappingWithMarkTemplateMappings);
 
 }
