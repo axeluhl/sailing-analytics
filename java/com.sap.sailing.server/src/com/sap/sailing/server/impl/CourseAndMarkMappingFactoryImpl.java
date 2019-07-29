5db7fcd7753a01250dc48e3bb4afd7bf9b7c6e56
Steffen Jacobs <steffen.jacobs@oio.de>
2019-07-29 17:51:46 +0200

    Bug 5085: Adjusted CourseAndMarkMappingFactoryImpl

--- java/com.sap.sailing.server/src/com/sap/sailing/server/impl/CourseAndMarkMappingFactoryImpl.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/impl/CourseAndMarkMappingFactoryImpl.java
@@ -4,6 +4,7 @@ import com.sap.sailing.domain.base.Course;
 import com.sap.sailing.domain.base.Regatta;
 import com.sap.sailing.domain.coursetemplate.CourseTemplate;
 import com.sap.sailing.domain.coursetemplate.CourseTemplateMapping;
+import com.sap.sailing.domain.coursetemplate.CourseTemplateWithMarkTemplateMappings;
 import com.sap.sailing.server.interfaces.CourseAndMarkMappingFactory;
 
 public class CourseAndMarkMappingFactoryImpl implements CourseAndMarkMappingFactory {
@@ -27,8 +28,15 @@ public class CourseAndMarkMappingFactoryImpl implements CourseAndMarkMappingFact
     }
 
     @Override
-    public Course createCourseFromMappingAndDefineMarksAsNeeded(Regatta regatta,
+    public CourseTemplateWithMarkTemplateMappings createCourseTemplateMappingFromMapping(Regatta regatta,
             CourseTemplateMapping courseTemplateMapping, int numberOfLaps) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Course createCourseFromMappingAndDefineMarksAsNeeded(Regatta regatta,
+            CourseTemplateWithMarkTemplateMappings courseTemplateMappingWithMarkTemplateMappings) {
         // TODO Auto-generated method stub
         return null;
     }
