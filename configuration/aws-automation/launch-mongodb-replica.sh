e2f48b6f6e93b6101039af2dca26c1d8427b4e2b
Axel Uhl <axel.uhl@sap.com>
2019-07-30 09:39:56 +0200

    introduced parameters to launch-mongodb-replica.sh

--- configuration/aws-automation/launch-mongodb-replica.sh
+++ configuration/aws-automation/launch-mongodb-replica.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-aws ec2 run-instances --placement AvailabilityZone=eu-west-1c --instance-type i2.xlarge --security-group-ids sg-0a9bc2fb61f10a342 --image-id ami-0e7e1e80586782638 --count 1 --user-data "REPLICA_SET_NAME=live
-REPLICA_SET_PRIMARY=mongo0.internal.sapsailing.com:27017" --ebs-optimized --key-name Axel
+# Usage:
+#   launch-mongodb-replica.sh <replica-set-name> <primary-host:primary-port> <priority>
+#
+# Example:
+#   launch-mongodb-replica.sh live dbserver.internal.sapsailing.com:10202 1
+#
+aws ec2 run-instances --placement AvailabilityZone=eu-west-1c --instance-type i2.xlarge --security-group-ids sg-0a9bc2fb61f10a342 --image-id ami-0e7e1e80586782638 --count 1 --user-data "REPLICA_SET_NAME=$1
+REPLICA_SET_PRIMARY=$2" --ebs-optimized --key-name Axel
