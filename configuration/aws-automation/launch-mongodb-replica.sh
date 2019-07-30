eb26cf32d6870e06b317a30eff7ed51637033022
Axel Uhl <axel.uhl@sap.com>
2019-07-30 11:09:54 +0200

    improved Wiki description of MongoDB setup and replica launch script
    Change-Id: Ia8eedbfae1a3da40b2e644805d698694e2d5121f

--- configuration/aws-automation/launch-mongodb-replica.sh
+++ configuration/aws-automation/launch-mongodb-replica.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Usage:
-#   launch-mongodb-replica.sh <replica-set-name> <primary-host:primary-port> <priority>
+#   launch-mongodb-replica.sh [ <replica-set-name> [ <primary-host:primary-port> [ <priority> ] ] ]
 #
-# Example:
-#   launch-mongodb-replica.sh live dbserver.internal.sapsailing.com:10202 1
+# The defaults are: live, mongo0.internal.sapsailing.com:27017, 1
+#
+# Examples:
+#   launch-mongodb-replica.sh
+#        connects to the "live" replica set through mongo0.internal.sapsailing.com:27017 with priority 1
+#   launch-mongodb-replica.sh archive dbserver.internal.sapsailing.com:10201 0
+#        connects to the "archive" replica set at dbserver.internal.sapsailing.com:10201 and ensures
+#        that the new replica never becomes PRIMARY by setting the priority to 0
 #
 aws ec2 run-instances --placement AvailabilityZone=eu-west-1c --instance-type i2.xlarge --security-group-ids sg-0a9bc2fb61f10a342 --image-id ami-0e7e1e80586782638 --count 1 --user-data "REPLICA_SET_NAME=$1
-REPLICA_SET_PRIMARY=$2" --ebs-optimized --key-name Axel
+REPLICA_SET_PRIMARY=$2
+REPLICA_SET_PRIORITY=$3" --ebs-optimized --key-name Axel
