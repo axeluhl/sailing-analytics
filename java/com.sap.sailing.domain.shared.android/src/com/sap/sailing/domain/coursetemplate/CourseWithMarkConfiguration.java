b1412107aea9e97a9a2548ce12bf720dfa130da0
Steffen Schaefer <steffen.schaefer@oio.de>
2019-08-01 13:32:15 +0200

    Bug 5085: Added JavaDocs

--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/coursetemplate/CourseWithMarkConfiguration.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/coursetemplate/CourseWithMarkConfiguration.java
@@ -1,8 +1,15 @@
 package com.sap.sailing.domain.coursetemplate;
 
-import com.sap.sse.common.NamedWithID;
+import com.sap.sailing.domain.abstractlog.regatta.RegattaLog;
+import com.sap.sailing.domain.base.Mark;
 
-public interface CourseWithMarkConfiguration extends NamedWithID {
+/**
+ * A course that does not consist of {@link Mark Marks} existing in a regatta but instead is defined by
+ * {@link MarkConfiguration MarkConfigurations}. This means changes to the model are easily possible without requiring
+ * to pollute the {@link RegattaLog}. The effective Marks will then be created upon Course creation based on the
+ * {@link MarkConfiguration MarkConfigurations}.
+ */
+public interface CourseWithMarkConfiguration {
 
     Iterable<MarkConfiguration> getMarkMappings();
 
