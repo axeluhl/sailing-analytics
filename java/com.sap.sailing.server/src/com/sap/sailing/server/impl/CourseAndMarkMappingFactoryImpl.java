fcae4700f44e5a5e96874d5bb3754a8403324ad7
Steffen Schaefer <steffen.schaefer@oio.de>
2019-08-05 13:12:38 +0200

    Bug 5085: Fixed compiler errors

--- java/com.sap.sailing.server/src/com/sap/sailing/server/impl/CourseAndMarkMappingFactoryImpl.java
+++ java/com.sap.sailing.server/src/com/sap/sailing/server/impl/CourseAndMarkMappingFactoryImpl.java
@@ -3,7 +3,9 @@ package com.sap.sailing.server.impl;
 import java.util.List;
 import java.util.function.Predicate;
 
+import com.sap.sailing.domain.abstractlog.AbstractLogEventAuthor;
 import com.sap.sailing.domain.base.Course;
+import com.sap.sailing.domain.base.CourseBase;
 import com.sap.sailing.domain.base.Regatta;
 import com.sap.sailing.domain.coursetemplate.CourseTemplate;
 import com.sap.sailing.domain.coursetemplate.CourseTemplateConfiguration;
@@ -25,41 +27,46 @@ public class CourseAndMarkMappingFactoryImpl implements CourseAndMarkMappingFact
     @Override
     public void updateCourse(Course courseToUpdate, CourseTemplate courseTemplate, int numberOfLaps) {
         // TODO Auto-generated method stub
+        
+    }
 
+    @Override
+    public CourseTemplate resolveCourseTemplate(Course course) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public MarkTemplate getOrCreateMarkTemplate(MarkConfiguration markConfiguration) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public CourseWithMarkConfiguration createCourseTemplateAndUpdatedConfiguration(String name,
+            CourseWithMarkConfiguration courseWithMarkConfiguration) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public CourseTemplateConfiguration createMappingForCourseTemplate(Regatta regatta, CourseTemplate courseTemplate,
-            Predicate<MarkProperties> markPropertiesFilter, Iterable<String> tagsToFilterFor) {
+            Predicate<MarkProperties> markPropertiesFilter, Iterable<String> tagsToFilterMarkProperties) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Course createCourseFromMappingAndDefineMarksAsNeeded(Regatta regatta,
-            CourseWithMarkConfiguration courseTemplateMappingWithMarkTemplateMappings,
-            TimePoint timePointForDefinitionOfMarksAndDeviceMappings) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
-    public Course createCourseFromMappingAndDefineMarksAsNeededAndExportCourseTemplate(Regatta regatta,
-            CourseWithMarkConfiguration courseTemplateMappingWithMarkTemplateMappings,
-            TimePoint timePointForDefinitionOfMarksAndDeviceMappings, String courseTemplateName) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
     public List<MarkConfiguration> createSuggestionsForMarkTemplate(Regatta regatta, MarkTemplate markTemplate,
-            Predicate<MarkProperties> markPropertiesFilter, Iterable<String> tagsToFilterFor) {
+            Predicate<MarkProperties> markPropertiesFilter, Iterable<String> tagsToFilterMarkProperties) {
         // TODO Auto-generated method stub
         return null;
     }
-    
+
     @Override
-    public CourseTemplate resolveCourseTemplate(Course course) {
+    public CourseBase createCourseFromMappingAndDefineMarksAsNeeded(Regatta regatta,
+            CourseWithMarkConfiguration courseTemplateMappingWithMarkTemplateMappings, int lapCount,
+            TimePoint timePointForDefinitionOfMarksAndDeviceMappings, AbstractLogEventAuthor author) {
         // TODO Auto-generated method stub
         return null;
     }
