aa237aab3af8103d2af4ff976116f51b32cfc026
Thomas Wiese <mail@thomaswiese.de>
2019-08-14 13:22:53 +0200

    Bug 5085: add fields and asserts to createMarkProperties API test

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/coursetemplate/MarkProperties.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/coursetemplate/MarkProperties.java
@@ -1,13 +1,57 @@
 package com.sap.sailing.selenium.api.coursetemplate;
 
+import java.util.UUID;
+
 import org.json.simple.JSONObject;
 
+import com.sap.sailing.domain.common.MarkType;
 import com.sap.sailing.selenium.api.core.JsonWrapper;
 
 public class MarkProperties extends JsonWrapper {
+
+    private static final String FIELD_ID = "id";
+    private static final String FIELD_NAME = "name";
+    private static final String FIELD_SHORTNAME = "shortName";
+    private static final String FIELD_HAS_DEVICEUUID = "hasDeviceUuid";
+    private static final String FIELD_COLOR = "color";
+    private static final String FIELD_SHAPE = "shape";
+    private static final String FIELD_PATTERN = "pattern";
+    private static final String FIELD_MARKTYPE = "markType";
 
     public MarkProperties(JSONObject json) {
         super(json);
     }
 
+    public UUID getId() {
+        return UUID.fromString(get(FIELD_ID));
+    }
+
+    public String getName() {
+        return get(FIELD_NAME);
+    }
+
+    public String getShortName() {
+        return get(FIELD_SHORTNAME);
+    }
+
+    public String getColor() {
+        return get(FIELD_COLOR);
+    }
+
+    public String getShape() {
+        return get(FIELD_SHAPE);
+    }
+
+    public String getPattern() {
+        return get(FIELD_PATTERN);
+    }
+
+    public MarkType getMarkType() {
+        String markType = get(FIELD_MARKTYPE);
+        return markType != null ? MarkType.valueOf(markType) : null;
+    }
+
+    public Boolean hasDevice() {
+        return get(FIELD_HAS_DEVICEUUID);
+    }
 }
--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/test/MarkPropertiesTest.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/test/MarkPropertiesTest.java
@@ -3,6 +3,8 @@ package com.sap.sailing.selenium.api.test;
 import static com.sap.sailing.selenium.api.core.ApiContext.SERVER_CONTEXT;
 import static com.sap.sailing.selenium.api.core.ApiContext.createAdminApiContext;
 import static java.util.UUID.randomUUID;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.UUID;
 
@@ -10,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.sap.sailing.selenium.api.core.ApiContext;
+import com.sap.sailing.selenium.api.coursetemplate.MarkProperties;
 import com.sap.sailing.selenium.api.coursetemplate.MarkPropertiesApi;
 import com.sap.sailing.selenium.test.AbstractSeleniumTest;
 
@@ -25,8 +28,15 @@ public class MarkPropertiesTest extends AbstractSeleniumTest {
     @Test
     public void createMarkPropertyWithDeviceUuidTest() {
         final ApiContext ctx = createAdminApiContext(getContextRoot(), SERVER_CONTEXT);
+        final String markPropertiesName = "testname";
+        final String markPropertiesShortName = "testshortname";
         final UUID deviceUuid = randomUUID();
-        markPropertiesApi.createMarkProperties(ctx, "test", "testshort", deviceUuid.toString(), "#FF0000", "shape",
-                "pattern", "STARTBOAT");
+        MarkProperties markProperties = markPropertiesApi.createMarkProperties(ctx, markPropertiesName,
+                markPropertiesShortName, deviceUuid.toString(), "#FF0000", "shape", "pattern", "STARTBOAT");
+        assertNotNull("read: no MarkProperties returnded", markProperties);
+        assertNotNull("read: MarkProperties.id is missing");
+        assertEquals("read: MarkProperties.name is different", markPropertiesName, markProperties.getName());
+        assertEquals("read: MarkProperties.shortName is different", markPropertiesShortName,
+                markProperties.getShortName());
     }
 }
