39d05f48db717529b4b9e02f0b9e6406235a2354
Steffen Schaefer <steffen.schaefer@oio.de>
2019-08-19 10:41:51 +0200

    Bug 5085: Added the associated tags to the JSON representation of MarkProperties

--- java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/serialization/impl/MarkPropertiesJsonSerializer.java
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/serialization/impl/MarkPropertiesJsonSerializer.java
@@ -1,5 +1,6 @@
 package com.sap.sailing.server.gateway.serialization.impl;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.sap.sailing.domain.coursetemplate.MarkProperties;
@@ -10,6 +11,7 @@ public class MarkPropertiesJsonSerializer implements JsonSerializer<MarkProperti
     private static final String FIELD_ID = "id";
     private static final String FIELD_NAME = "name";
     private static final String FIELD_SHORTNAME = "shortName";
+    private static final String FIELD_TAGS = "tags";
     private static final String FIELD_HAS_DEVICEUUID = "hasDeviceUuid";
     private static final String FIELD_COLOR = "color";
     private static final String FIELD_SHAPE = "shape";
@@ -24,6 +26,11 @@ public class MarkPropertiesJsonSerializer implements JsonSerializer<MarkProperti
         result.put(FIELD_ID, markProperties.getId().toString());
         result.put(FIELD_NAME, markProperties.getName());
         result.put(FIELD_SHORTNAME, markProperties.getShortName());
+        
+        final JSONArray tags = new JSONArray();
+        markProperties.getTags().forEach(tags::add);
+        result.put(FIELD_TAGS, tags);
+        
         result.put(FIELD_COLOR, markProperties.getColor() != null ? markProperties.getColor().getAsHtml() : null);
         result.put(FIELD_SHAPE, markProperties.getShape());
         result.put(FIELD_PATTERN, markProperties.getPattern());
