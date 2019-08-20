808b43249d72956bb630f74b82a5d5e45a5b1d7d
Steffen Schaefer <steffen.schaefer@oio.de>
2019-08-20 15:36:15 +0200

    Bug 5085: Added more test cases

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/test/CourseTemplateTest.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/test/CourseTemplateTest.java
@@ -3,6 +3,8 @@ package com.sap.sailing.selenium.api.test;
 import static com.sap.sailing.selenium.api.core.ApiContext.SERVER_CONTEXT;
 import static com.sap.sailing.selenium.api.core.ApiContext.createAdminApiContext;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,48 +23,80 @@ import com.sap.sailing.selenium.api.coursetemplate.MarkTemplateApi;
 import com.sap.sailing.selenium.api.coursetemplate.WaypointTemplate;
 import com.sap.sailing.selenium.test.AbstractSeleniumTest;
 import com.sap.sse.common.Util;
+import com.sap.sse.common.Util.Pair;
 
 public class CourseTemplateTest extends AbstractSeleniumTest {
 
     private final MarkTemplateApi markTemplateApi = new MarkTemplateApi();
     private final CourseTemplateApi courseTemplateApi = new CourseTemplateApi();
+    private ApiContext ctx;
+    private MarkTemplate sb;
+    private MarkTemplate pe;
+    private MarkTemplate b1;
+    private MarkTemplate b4s;
+    private MarkTemplate b4p;
+    private MarkTemplate spare;
+    private List<WaypointTemplate> waypointSequence;
 
     @Before
     public void setUp() {
         clearState(getContextRoot(), /* headless */ true);
-    }
 
-    @Test
-    public void createCourseTemplateTest() {
-        final ApiContext ctx = createAdminApiContext(getContextRoot(), SERVER_CONTEXT);
-        MarkTemplate sb = markTemplateApi.createMarkTemplate(ctx, "Startboat", "sb",
-                null, null, null, MarkType.STARTBOAT.name());
-        MarkTemplate pe = markTemplateApi.createMarkTemplate(ctx, "Pinend", "pe",
-                "#000000", null, null, MarkType.BUOY.name());
-        MarkTemplate b1 = markTemplateApi.createMarkTemplate(ctx, "blue", "blue",
-                "#0000FF", null, null, MarkType.BUOY.name());
-        MarkTemplate b4s = markTemplateApi.createMarkTemplate(ctx, "red", "red",
-                "#FF0000", null, null, MarkType.BUOY.name());
-        MarkTemplate b4p = markTemplateApi.createMarkTemplate(ctx, "green", "green",
-                "#00FF00", null, null, MarkType.BUOY.name());
-        MarkTemplate spare = markTemplateApi.createMarkTemplate(ctx, "spare", "spare",
-                "#00FFFF", null, null, MarkType.BUOY.name());
-        
-        final List<WaypointTemplate> waypointSequence = Arrays.asList(new WaypointTemplate("Start/End", PassingInstruction.Line, Arrays.asList(sb, pe)),
+        ctx = createAdminApiContext(getContextRoot(), SERVER_CONTEXT);
+        sb = markTemplateApi.createMarkTemplate(ctx, "Startboat", "sb", null, null, null, MarkType.STARTBOAT.name());
+        pe = markTemplateApi.createMarkTemplate(ctx, "Pinend", "pe", "#000000", null, null, MarkType.BUOY.name());
+        b1 = markTemplateApi.createMarkTemplate(ctx, "blue", "blue", "#0000FF", null, null, MarkType.BUOY.name());
+        b4s = markTemplateApi.createMarkTemplate(ctx, "red", "red", "#FF0000", null, null, MarkType.BUOY.name());
+        b4p = markTemplateApi.createMarkTemplate(ctx, "green", "green", "#00FF00", null, null, MarkType.BUOY.name());
+        spare = markTemplateApi.createMarkTemplate(ctx, "spare", "spare", "#00FFFF", null, null, MarkType.BUOY.name());
+
+        waypointSequence = Arrays.asList(
+                new WaypointTemplate("Start/End", PassingInstruction.Line, Arrays.asList(sb, pe)),
                 new WaypointTemplate(null, PassingInstruction.Port, Arrays.asList(b1)),
                 new WaypointTemplate(null, PassingInstruction.Gate, Arrays.asList(b4p, b4s)),
                 new WaypointTemplate("Start/End", PassingInstruction.Line, Arrays.asList(sb, pe)));
-        
-        final CourseTemplate courseTemplateToSave = new CourseTemplate("my-special-course-template", Arrays.asList(sb, pe, b1, b4s, b4p, spare),
-                Collections.emptyMap(),
-                waypointSequence,
-                null, Collections.emptySet(), null);
-        
-        final CourseTemplate createdCourseTemplate = courseTemplateApi.createCourseTemplate(ctx,
-                courseTemplateToSave);
-        
+    }
+
+    @Test
+    public void createSimpleCourseTemplateTest() {
+        final CourseTemplate courseTemplateToSave = constructCourseTemplateWithoutRepeatablePart();
+
+        final CourseTemplate createdCourseTemplate = courseTemplateApi.createCourseTemplate(ctx, courseTemplateToSave);
+
         assertEquals(courseTemplateToSave.getName(), createdCourseTemplate.getName());
         assertEquals(waypointSequence.size(), Util.size(createdCourseTemplate.getWaypoints()));
+    }
+
+    @Test
+    public void createCourseTemplateWithRepeatablePartTest() {
+        final Pair<Integer, Integer> repeatablePart = new Pair<>(1, 2);
+        final CourseTemplate courseTemplateToSave = constructCourseTemplate(repeatablePart);
         
+        final CourseTemplate createdCourseTemplate = courseTemplateApi.createCourseTemplate(ctx, courseTemplateToSave);
+
+        assertEquals(repeatablePart, createdCourseTemplate.getOptionalRepeatablePart());
+    }
+    
+    @Test
+    public void createCourseTemplateWithInvalidRepeatablePartTest() {
+        final Pair<Integer, Integer> repeatablePart = new Pair<>(1, 5);
+        final CourseTemplate courseTemplateToSave = constructCourseTemplate(repeatablePart);
+        
+        try {
+            courseTemplateApi.createCourseTemplate(ctx, courseTemplateToSave);
+            fail();
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("Repeatable part (1, 5) is out of range for sequence of length 4"));
+        }
+    }
+    
+    private CourseTemplate constructCourseTemplateWithoutRepeatablePart() {
+        return constructCourseTemplate(null);
+    }
+    
+    private CourseTemplate constructCourseTemplate(Pair<Integer, Integer> optionalRepeatablePart) {
+        return new CourseTemplate("my-special-course-template",
+                Arrays.asList(sb, pe, b1, b4s, b4p, spare), Collections.emptyMap(), waypointSequence, optionalRepeatablePart,
+                Collections.emptySet(), null);
     }
 }
