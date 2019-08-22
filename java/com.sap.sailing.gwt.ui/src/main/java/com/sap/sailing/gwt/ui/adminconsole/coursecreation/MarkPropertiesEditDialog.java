345c4f166490a1fc5aa404d8cd901e1e432f7d49
Steffen Jacobs <steffen.jacobs@oio.de>
2019-08-22 15:34:24 +0200

    Bug 5085: added ValueListBoxes for pattern and shape and display them only when mark type BUOY is selected (MarkTemplateEditDialog, MarkPropertiesEditDialog)

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/coursecreation/MarkPropertiesEditDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/coursecreation/MarkPropertiesEditDialog.java
@@ -17,6 +17,8 @@ import com.sap.sailing.domain.common.impl.RadianPosition;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sailing.gwt.ui.shared.DeviceIdentifierDTO;
 import com.sap.sailing.gwt.ui.shared.courseCreation.MarkPropertiesDTO;
+import com.sap.sailing.gwt.ui.shared.racemap.Pattern;
+import com.sap.sailing.gwt.ui.shared.racemap.Shape;
 import com.sap.sse.common.Color;
 import com.sap.sse.common.impl.RGBColor;
 import com.sap.sse.gwt.client.dialog.DataEntryDialog;
@@ -25,10 +27,12 @@ public class MarkPropertiesEditDialog extends DataEntryDialog<MarkPropertiesDTO>
     private final TextBox nameTextBox;
     private final TextBox displayColorTextBox;
     private final TextBox shortNameTextBox;
-    private final TextBox shapeTextBox;
-    private final TextBox patternTextBox;
     private final ValueListBox<MarkType> markTypeValueListBox;
+    private final ValueListBox<Pattern> patternValueListBox;
+    private final ValueListBox<Shape> shapeValueListBox;
     private final StringMessages stringMessages;
+    private Label labelShape;
+    private Label labelPattern;
 
     public MarkPropertiesEditDialog(final StringMessages stringMessages, MarkPropertiesDTO markPropertiesToEdit,
             DialogCallback<MarkPropertiesDTO> callback) {
@@ -51,7 +55,6 @@ public class MarkPropertiesEditDialog extends DataEntryDialog<MarkPropertiesDTO>
         this.stringMessages = stringMessages;
 
         this.markTypeValueListBox = new ValueListBox<>(new Renderer<MarkType>() {
-
             @Override
             public String render(MarkType object) {
                 return object == null ? "" : object.name();
@@ -64,23 +67,65 @@ public class MarkPropertiesEditDialog extends DataEntryDialog<MarkPropertiesDTO>
             }
         });
 
+        labelPattern = new Label(stringMessages.pattern());
+        this.patternValueListBox = new ValueListBox<>(new Renderer<Pattern>() {
+            @Override
+            public String render(Pattern object) {
+                return object != null ? object.name() : "";
+            }
+
+            @Override
+            public void render(Pattern object, Appendable appendable) throws IOException {
+                appendable.append(render(object));
+            }
+        });
+
+        labelShape = new Label(stringMessages.shape());
+        this.shapeValueListBox = new ValueListBox<>(new Renderer<Shape>() {
+            @Override
+            public String render(Shape object) {
+                return object != null ? object.name() : "";
+            }
+
+            @Override
+            public void render(Shape object, Appendable appendable) throws IOException {
+                appendable.append(render(object));
+            }
+        });
+
         this.nameTextBox = createTextBox(markPropertiesToEdit.getName());
         this.shortNameTextBox = createTextBox(markPropertiesToEdit.getCommonMarkProperties().getShortName());
-        this.shapeTextBox = createTextBox(markPropertiesToEdit.getCommonMarkProperties().getShape());
-        this.patternTextBox = createTextBox(markPropertiesToEdit.getCommonMarkProperties().getPattern());
         this.markTypeValueListBox.setValue(markPropertiesToEdit.getCommonMarkProperties().getType() != null
                 ? markPropertiesToEdit.getCommonMarkProperties().getType()
                 : MarkType.values()[0]);
         markTypeValueListBox.setAcceptableValues(Arrays.asList(MarkType.values()));
-
-        markTypeValueListBox.addValueChangeHandler(v -> validateAndUpdate());
+        markTypeValueListBox.addValueChangeHandler(v -> handleMarkTypeChange());
 
         if (markPropertiesToEdit.getCommonMarkProperties().getColor() != null) {
-            this.displayColorTextBox = createTextBox(markPropertiesToEdit.getCommonMarkProperties().getColor() == null ? ""
-                    : markPropertiesToEdit.getCommonMarkProperties().getColor().getAsHtml());
+            this.displayColorTextBox = createTextBox(
+                    markPropertiesToEdit.getCommonMarkProperties().getColor() == null ? ""
+                            : markPropertiesToEdit.getCommonMarkProperties().getColor().getAsHtml());
         } else {
             this.displayColorTextBox = createTextBox("");
         }
+
+        shapeValueListBox.setValue(null);
+        shapeValueListBox.setAcceptableValues(Arrays.asList(Shape.values()));
+        patternValueListBox.setValue(null);
+        patternValueListBox.setAcceptableValues(Arrays.asList(Pattern.values()));
+
+        handleMarkTypeChange();
+    }
+
+    private void handleMarkTypeChange() {
+        final boolean isBuoy = markTypeValueListBox.getValue() == MarkType.BUOY;
+
+        shapeValueListBox.setVisible(isBuoy);
+        patternValueListBox.setVisible(isBuoy);
+        labelShape.setVisible(isBuoy);
+        labelPattern.setVisible(isBuoy);
+
+        validateAndUpdate();
     }
 
     @Override
@@ -139,7 +184,9 @@ public class MarkPropertiesEditDialog extends DataEntryDialog<MarkPropertiesDTO>
         // TODO: tags, device identifier, position
         MarkPropertiesDTO markProperties = new MarkPropertiesDTO(UUID.randomUUID(), nameTextBox.getValue(),
                 new ArrayList<String>(), new DeviceIdentifierDTO(null, null), new RadianPosition(0, 0),
-                shortNameTextBox.getValue(), color, shapeTextBox.getText(), patternTextBox.getText(),
+                shortNameTextBox.getValue(), color,
+                shapeValueListBox.getValue() == null ? "" : shapeValueListBox.getValue().name(),
+                patternValueListBox.getValue() == null ? "" : patternValueListBox.getValue().name(),
                 markTypeValueListBox.getValue());
         return markProperties;
     }
@@ -153,10 +200,10 @@ public class MarkPropertiesEditDialog extends DataEntryDialog<MarkPropertiesDTO>
         result.setWidget(1, 1, shortNameTextBox);
         result.setWidget(2, 0, new Label(stringMessages.color()));
         result.setWidget(2, 1, displayColorTextBox);
-        result.setWidget(3, 0, new Label(stringMessages.shape()));
-        result.setWidget(3, 1, shapeTextBox);
-        result.setWidget(4, 0, new Label(stringMessages.pattern()));
-        result.setWidget(4, 1, patternTextBox);
+        result.setWidget(3, 0, labelShape);
+        result.setWidget(3, 1, shapeValueListBox);
+        result.setWidget(4, 0, labelPattern);
+        result.setWidget(4, 1, patternValueListBox);
         result.setWidget(5, 0, new Label(stringMessages.type()));
         result.setWidget(5, 1, markTypeValueListBox);
         return result;
