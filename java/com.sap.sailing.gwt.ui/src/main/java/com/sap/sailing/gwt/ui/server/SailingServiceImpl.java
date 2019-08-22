396ee743cb2182c6ed472809d80830074e33a471
Steffen Jacobs <steffen.jacobs@oio.de>
2019-08-22 13:18:44 +0200

    Bug 5085: added security information to CourseTemplateDTO objects before sending them back to the FE

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
@@ -9442,9 +9442,13 @@ public class SailingServiceImpl extends ResultCachingProxiedRemoteServiceServlet
         final List<WaypointTemplateDTO> convertedWaypointTemplates = StreamSupport
                 .stream(courseTemplate.getWaypointTemplates().spliterator(), false)
                 .map(this::convertToWaypointTemplateDTO).collect(Collectors.toList());
-        return new CourseTemplateDTO(courseTemplate.getId(), courseTemplate.getName(), convertedMarkTemplates,
+        final CourseTemplateDTO result = new CourseTemplateDTO(courseTemplate.getId(), courseTemplate.getName(),
+                convertedMarkTemplates,
                 convertedWaypointTemplates, convertedAssociatedRoles, "" + courseTemplate.getOptionalImageURL(),
                 courseTemplate.getTags(), convertToRepeatablePartDTO(courseTemplate.getRepeatablePart()));
+
+        SecurityDTOUtil.addSecurityInformation(getSecurityService(), result, courseTemplate.getIdentifier());
+        return result;
     }
 
     private RepeatablePartDTO convertToRepeatablePartDTO(RepeatablePart repeatablePart) {
