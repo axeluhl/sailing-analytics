d592a697a4c91461d208d128c4c5afa9dac18d76
Daniel Lisunkin <daniel.lisunkin@sap.com>
2019-08-23 16:55:18 +0200

    Started implementing certificate assignment regatta log event.

--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/orc/ORCCertificateAssignmentEvent.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/orc/ORCCertificateAssignmentEvent.java
@@ -1,9 +1,12 @@
 package com.sap.sailing.domain.abstractlog.orc;
 
-import com.sap.sailing.domain.abstractlog.regatta.RegattaLogEvent;
+import com.sap.sailing.domain.abstractlog.regatta.events.RegattaLogSetCompetitorHandicapInfoEvent;
+import com.sap.sailing.domain.common.orc.ORCCertificate;
 
 // TODO Here is needed a solution/concept to use this kind of events in race and regatta logs.
 // Additionally the logic shouldn't be implemented at two different places.
-public interface ORCCertificateAssignmentEvent{
+public interface ORCCertificateAssignmentEvent extends RegattaLogSetCompetitorHandicapInfoEvent {
 
+    public ORCCertificate getCertificate();
+    
 }
--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/orc/impl/ORCCertificateAssignmentEventImpl.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/orc/impl/ORCCertificateAssignmentEventImpl.java
@@ -5,22 +5,46 @@ import java.io.Serializable;
 import com.sap.sailing.domain.abstractlog.AbstractLogEventAuthor;
 import com.sap.sailing.domain.abstractlog.impl.AbstractLogEventImpl;
 import com.sap.sailing.domain.abstractlog.orc.ORCCertificateAssignmentEvent;
+import com.sap.sailing.domain.abstractlog.regatta.RegattaLogEventVisitor;
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.CompetitorAndBoat;
 import com.sap.sailing.domain.common.orc.ORCCertificate;
 import com.sap.sse.common.TimePoint;
 
 public class ORCCertificateAssignmentEventImpl extends AbstractLogEventImpl<ORCCertificate> implements ORCCertificateAssignmentEvent{
 
+    private ORCCertificate certificate;
+    private Competitor competitor;
+    
     public ORCCertificateAssignmentEventImpl(TimePoint createdAt, TimePoint logicalTimePoint,
-            AbstractLogEventAuthor author, Serializable pId) {
+            AbstractLogEventAuthor author, Serializable pId, ORCCertificate certificate, Competitor competitor) {
         super(createdAt, logicalTimePoint, author, pId);
+        this.certificate = certificate;
+        this.competitor = competitor;
     }
 
     private static final long serialVersionUID = -3186019736784868848L;
 
     @Override
+    public void accept(RegattaLogEventVisitor visitor) {
+        visitor.visit(this);
+    }
+
+    @Override
+    public ORCCertificate getCertificate() {
+        return certificate;
+    }
+
+    @Override
     public void accept(ORCCertificate visitor) {
         // TODO Auto-generated method stub
         
+    }
+
+    @Override
+    public Competitor getCompetitor() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
