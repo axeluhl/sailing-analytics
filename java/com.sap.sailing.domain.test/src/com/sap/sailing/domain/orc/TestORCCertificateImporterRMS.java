11831e03c991e17a4ce05d98e2844667880e7ae8
Daniel Lisunkin <daniel.lisunkin@sap.com>
2019-09-03 17:51:37 +0200

    Clean up.

--- java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/orc/TestORCCertificateImporterRMS.java
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/orc/TestORCCertificateImporterRMS.java
@@ -2,12 +2,9 @@ package com.sap.sailing.domain.orc;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 
@@ -16,25 +13,10 @@ import org.junit.Test;
 import com.sap.sailing.domain.common.orc.ORCCertificate;
 import com.sap.sailing.domain.common.orc.impl.ORCCertificateImpl;
 import com.sap.sailing.domain.orc.impl.ORCCertificateImporterRMS;
-import com.sap.sailing.domain.orc.impl.ORCCertificateImporterRMS.ORCCertificateValues;
 
 public class TestORCCertificateImporterRMS {
     private static final String RESOURCES = "resources/orc/";
 
-    /*
-    @Test
-    public void testSimpleRMSFileRead() throws FileNotFoundException, IOException {
-        final File gerRms = new File(RESOURCES + "GER2016.rms");
-        ORCCertificateImporterRMS gerRmsFile = new ORCCertificateImporterRMS(new FileReader(gerRms));
-        assertNotNull(gerRmsFile);
-        assertTrue(gerRmsFile.getFileIds().contains("GER152487GER884"));
-        final ORCCertificateValues ger884 = gerRmsFile.getValuesForFileId("GER152487GER884");
-        assertNotNull(ger884);
-        assertEquals("DE DOOD", ger884.getValue("BUILDER"));
-        assertEquals("498.0", ger884.getValue("R9012"));
-    }
-    */
-    
     @Test
     public void testSimpleLocalRMSFileRead() throws IOException {
         File fileGER = new File(RESOURCES + "GER2019.rms");
