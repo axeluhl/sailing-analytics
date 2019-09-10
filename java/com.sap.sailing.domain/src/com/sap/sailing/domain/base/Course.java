9866f89f638887bed810a47e102137bf19b9e42f
Steffen Schaefer <steffen.schaefer@oio.de>
2019-09-10 15:02:32 +0200

    Bug 5085: Removed unused method

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/Course.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/base/Course.java
@@ -1,6 +1,5 @@
 package com.sap.sailing.domain.base;
 
-import java.util.UUID;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import com.sap.sailing.domain.common.PassingInstruction;
@@ -47,9 +46,4 @@ public interface Course extends CourseBase, Renamable {
     int getNumberOfWaypoints();
 
     Leg getLeg(int zeroBasedIndexOfWaypoint);
-
-    default UUID getCourseTemplateIdOrNull() {
-        // FIXME implement handling of course template references
-        return null;
-    }
 }
