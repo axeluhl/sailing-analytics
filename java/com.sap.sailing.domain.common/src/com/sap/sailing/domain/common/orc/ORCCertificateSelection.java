e0ed1fe369f068a9f9ce8b5cbd6929025be19439
Axel Uhl <axel.uhl@sap.com>
2019-09-20 09:10:26 +0200

    added missing ORCCertificateSelection interface
    Change-Id: Ieb7b9edc5b0a2c17bd240c2a07658d9701b5011b

---
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/orc/ORCCertificateSelection.java
@@ -0,0 +1,21 @@
+package com.sap.sailing.domain.common.orc;
+
+/**
+ * ORC certificates can be obtained from various sources, coming in two forms: upload or download. Uploads happen
+ * through a file upload servlet; downloads may happen from the ORC certificate database or from general URLs delivering
+ * the certificate document. The solution typically accepts the common RMS and JSON formats for the certificates.
+ * <p>
+ * 
+ * Once provided from a document source, the zero or more certificates extracted from that source may be used to assign
+ * certificates to boats. The boats are identified by their ID, and no more than one ID must be provided per boat ID.
+ * <p>
+ * 
+ * The certificates within the documents are identified by the {@code RefNo} field in case of a JSON document, or the
+ * {@code NATCERTN.FILE_ID} field in case of an RMS document, respectively.
+ * 
+ * @author Axel Uhl (d043530)
+ *
+ */
+public interface ORCCertificateSelection {
+    
+}
