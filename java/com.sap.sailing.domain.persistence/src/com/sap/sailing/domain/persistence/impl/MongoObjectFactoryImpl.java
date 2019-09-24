d071ce582073a0b6a97e5858d0e8ed67b64e3cc1
Axel Uhl <axel.uhl@sap.com>
2019-09-24 17:37:00 +0200

    fixed persistence of ORC PCS certificate assignment event in regatta log
    Change-Id: I72fcfd66db1202b179492e9c08ba6d002ccb413f

--- java/com.sap.sailing.domain.persistence/src/com/sap/sailing/domain/persistence/impl/MongoObjectFactoryImpl.java
+++ java/com.sap.sailing.domain.persistence/src/com/sap/sailing/domain/persistence/impl/MongoObjectFactoryImpl.java
@@ -1858,7 +1858,7 @@ public class MongoObjectFactoryImpl implements MongoObjectFactory {
     public void storeRegattaLogEvent(RegattaLikeIdentifier regattaLikeIdentifier, RegattaLogORCCertificateAssignmentEvent event) {
         Document document = createBasicRegattaLogEventDBObject(event);
         document.put(FieldNames.REGATTA_LOG_EVENT_CLASS.name(), RegattaLogORCCertificateAssignmentEvent.class.getSimpleName());
-        document.append(FieldNames.COMPETITOR_ID.name(), event.getBoatId());
+        document.append(FieldNames.RACE_LOG_BOAT_ID.name(), event.getBoatId());
         document.append(FieldNames.ORC_CERTIFICATE.name(), createORCCertificateObject(event.getCertificate()));
         storeRegattaLogEvent(regattaLikeIdentifier, document);
     }
