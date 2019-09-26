9716c3f6585873393ca0d8b8304c02c24be25f98
Daniel Lisunkin <daniel.lisunkin@sap.com>
2019-09-26 17:35:46 +0200

    Added CertificatesTableWrapper class. Added comments, cleaned up code.

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/BoatCertificateAssignmentDialog.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/BoatCertificateAssignmentDialog.java
@@ -11,13 +11,7 @@ import com.sap.sse.gwt.client.dialog.DataEntryDialog;
 import com.sap.sse.security.ui.client.UserService;
 
 public class BoatCertificateAssignmentDialog extends DataEntryDialog<List<BoatDTO>> {
-    /*
-    private final SailingServiceAsync sailingService;
-    private final UserService userService;
-    private final StringMessages stringMessages;
-    private final ErrorReporter errorReporter;
-    private final String regattaName;
-    */
+    
     private final BoatCertificatesPanel panel;
 
     protected static class BoatValidator implements Validator<List<BoatDTO>> {
@@ -34,13 +28,7 @@ public class BoatCertificateAssignmentDialog extends DataEntryDialog<List<BoatDT
     public BoatCertificateAssignmentDialog(final SailingServiceAsync sailingService, UserService userService, String regattaName, final StringMessages stringMessages,
             final ErrorReporter errorReporter, DialogCallback<List<BoatDTO>> callback) {
         super(stringMessages.actionEditCompetitors(), stringMessages.actionEditCompetitors(), stringMessages.save(), stringMessages.cancel(), new BoatValidator(), callback);
-        /*
-        this.sailingService = sailingService;
-        this.userService = userService;
-        this.stringMessages = stringMessages;
-        this.errorReporter = errorReporter;
-        this.regattaName = regattaName;
-        */
+
         
         this.panel = new BoatCertificatesPanel(sailingService, userService, regattaName, stringMessages, errorReporter);
     }
--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/BoatWithCertificateTableWrapper.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/adminconsole/BoatWithCertificateTableWrapper.java
@@ -17,7 +17,9 @@ import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
+import com.sap.sailing.domain.base.Boat;
 import com.sap.sailing.domain.common.dto.BoatDTO;
+import com.sap.sailing.domain.common.orc.ORCCertificate;
 import com.sap.sailing.gwt.ui.client.SailingServiceAsync;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sse.common.Util;
@@ -34,17 +36,26 @@ import com.sap.sse.security.shared.HasPermissions.DefaultActions;
 import com.sap.sse.security.ui.client.UserService;
 import com.sap.sse.security.ui.client.component.AccessControlledActionsColumn;
 
+/**
+ * This Object is a more simplified version of the {@link BoatTableWrapper} to be used when matching {@link ORCCertificate}s to {@link Boat}s.
+ * The shown information is stripped down to the sail number, boat name, boat class and some actions including linking/unlinking.
+ * 
+ * @author Daniel Lisunkin (i505543)
+ * 
+ */
 public class BoatWithCertificateTableWrapper<S extends RefreshableSelectionModel<BoatDTO>> extends TableWrapper<BoatDTO, S> {
     private final LabeledAbstractFilterablePanel<BoatDTO> filterField;
     
-    public BoatWithCertificateTableWrapper(SailingServiceAsync sailingService, final UserService userService, StringMessages stringMessages,
-            ErrorReporter errorReporter, boolean multiSelection, boolean enablePager, boolean allowActions) {
+    public BoatWithCertificateTableWrapper(SailingServiceAsync sailingService, final UserService userService,
+            StringMessages stringMessages, ErrorReporter errorReporter, boolean multiSelection, boolean enablePager,
+            boolean allowActions) {
         this(sailingService, userService, stringMessages, errorReporter, multiSelection, enablePager, DEFAULT_PAGING_SIZE,
                 allowActions);
     }
 
-    public BoatWithCertificateTableWrapper(SailingServiceAsync sailingService, final UserService userService, StringMessages stringMessages, ErrorReporter errorReporter,
-            boolean multiSelection, boolean enablePager, int pagingSize, boolean allowActions) {
+    public BoatWithCertificateTableWrapper(SailingServiceAsync sailingService, final UserService userService,
+            StringMessages stringMessages, ErrorReporter errorReporter, boolean multiSelection, boolean enablePager,
+            int pagingSize, boolean allowActions) {
         super(sailingService, stringMessages, errorReporter, multiSelection, enablePager, pagingSize,
                 new EntityIdentityComparator<BoatDTO>() {
                     @Override
@@ -132,6 +143,7 @@ public class BoatWithCertificateTableWrapper<S extends RefreshableSelectionModel
         boatActionColumn.addAction(BoatConfigImagesBarCell.ACTION_UPDATE, HasPermissions.DefaultActions.UPDATE,
                 this::openEditBoatDialog);
         boatActionColumn.addAction(BoatConfigImagesBarCell.ACTION_REFRESH, this::allowUpdate);
+        // TODO Add Link/Unlink Button
 
         mainPanel.insert(filterField, 0);
         table.addColumnSortHandler(boatColumnListHandler);
