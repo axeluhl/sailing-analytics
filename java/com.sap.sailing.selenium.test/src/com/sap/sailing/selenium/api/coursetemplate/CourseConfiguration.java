7b1a7c8465420c04641f2e14363fd08c477ec0f1
Steffen Schaefer <steffen.schaefer@oio.de>
2019-09-27 11:51:03 +0200

    Bug 5085: Implemented handling of numberOfLaps in CourseConfiguration test API

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/coursetemplate/CourseConfiguration.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/coursetemplate/CourseConfiguration.java
@@ -11,6 +11,7 @@ public class CourseConfiguration extends JsonWrapper {
 
     private static final String FIELD_MARKCONFIGURATIONS = "markConfigurations";
     private static final String FIELD_WAYPOINTS = "waypoints";
+    public static final String FIELD_NUMBER_OF_LAPS = "numberOfLaps";
 
     public CourseConfiguration(JSONObject json) {
         super(json);
@@ -33,5 +34,9 @@ public class CourseConfiguration extends JsonWrapper {
         JSONArray waypointsJson = (JSONArray) getJson().get(FIELD_WAYPOINTS);
         return waypointsJson.stream().map(w -> (JSONObject) w).map(WaypointWithMarkConfiguration::new)
                 .collect(Collectors.toList());
+    }
+    
+    public void setNumberOfLaps(Integer numberOfLaps) {
+        getJson().put(FIELD_NUMBER_OF_LAPS, numberOfLaps);
     }
 }
