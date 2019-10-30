99cb732442061c078fe59383a087f408859d5d06
Axel Uhl <axel.uhl@sap.com>
2019-10-30 09:48:55 +0100

    revert to SailInsight1 default as SailInsight2 was removed from the stores
    Change-Id: Ief190f734106b959fb7c905948c7de8e4e4a5955

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/server/SailingServiceImpl.java
@@ -9179,7 +9179,7 @@ public class SailingServiceImpl extends ResultCachingProxiedRemoteServiceServlet
     @Override
     public MailInvitationType getMailType() {
         MailInvitationType type = MailInvitationType
-                .valueOf(System.getProperty(MAILTYPE_PROPERTY, MailInvitationType.SailInsight2.name()));
+                .valueOf(System.getProperty(MAILTYPE_PROPERTY, MailInvitationType.SailInsight1.name()));
         return type;
     }
 
