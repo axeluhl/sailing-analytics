57f9ae6aba2f8fcc8e70ffba2ea0ca4f674300d4
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2019-11-13 15:04:23 +0100

    added temporary "trackedBy" logo to EventHeader

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/eventheader/EventHeader.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/eventheader/EventHeader.java
@@ -60,6 +60,7 @@ public class EventHeader extends Composite {
     @UiField DivElement courseAreaUi;
     @UiField FlowPanel dropdownContent;
     @UiField SharingButtons sharing;
+    @UiField AnchorElement dataByContainer;
 
     private EventViewDTO event;
     private Presenter presenter;
@@ -148,6 +149,9 @@ public class EventHeader extends Composite {
                 hide(eventLink);
             }
             hide(competitors, races, courseAreaUi, eventCategory);
+        }
+        if(!event.isTrackedByTracTrac()) {
+            hide(dataByContainer);
         }
         initTitleAndSelection(nameToShow);
     }
