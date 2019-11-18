9010ea3698de6c7166206e0afadd2cf32fb7c1d2
Steffen Schaefer <steffen.schaefer@oio.de>
2019-11-18 10:03:27 +0100

    Bug 5085: Introduced CourseConfigurationBase as intermediate implementation of CourseConfiguration. This will be used to distinct different cases when referencing IsMarkRole or MarkRole objects in the CourseConfiguration model.

---
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/coursetemplate/CourseConfigurationBase.java
@@ -0,0 +1,70 @@
+package com.sap.sailing.domain.coursetemplate;
+
+import java.util.Map;
+
+import com.sap.sailing.domain.abstractlog.regatta.RegattaLog;
+import com.sap.sailing.domain.base.Mark;
+import com.sap.sse.common.Named;
+
+/**
+ * A course that does not consist of {@link Mark}s existing in a regatta but instead is defined by
+ * {@link MarkConfiguration}s. This means changes to the model are easily possible without requiring to pollute the
+ * {@link RegattaLog}. The effective Marks will then be created upon Course creation based on the
+ * {@link MarkConfiguration}s.
+ * <p>
+ * 
+ * This is the model represented in a course editor which deals with editing the waypoint sequence as well as changing
+ * how marks are mapped to {@link MarkTemplate}s and how marks are generally configured in terms of their base
+ * properties and tracking/positioning information. Any change that is incompatible with a non-{@code null}
+ * {@link CourseTemplate} returned by {@link #getOptionalCourseTemplate()} leads to breaking this connection
+ * such that {@link #getOptionalCourseTemplate()} from that point on will return {@code null}.<p>
+ * 
+ * The configuration for all marks are returned by {@link #getAllMarks()} because when pushing
+ * this to a Regatta the marks for all these configurations have to be established.
+ */
+public interface CourseConfigurationBase extends WithOptionalRepeatablePart, Named {
+    /**
+     * If this course configuration has been obtained from a {@link CourseTemplate} without modifying the sequence of
+     * waypoints and their mapping to {@link MarkTemplate}s, this method will return that {@link CourseTemplate}. In all
+     * other cases it will return {@code null}. If not {@code null}, the methods of {@link WithOptionalRepeatablePart}
+     * delegate to the {@link CourseTemplate} returned by this method.
+     */
+    CourseTemplate getOptionalCourseTemplate();
+    
+    /**
+     * Returns all marks that are part of this course configuration. This consists of:
+     * <ul>
+     * <li>All {@link MarkConfiguration} that are used by at least one {@link WaypointWithMarkConfiguration}.</li>
+     * <li>Any additional {@link MarkConfiguration} that is intended to be used as a spare mark.</li>
+     * </ul>
+     */
+    Iterable<MarkConfiguration> getAllMarks();
+
+    /**
+     * The waypoint sequence, without lap repetitions, with {@link MarkConfiguration configuration information}
+     * for the marks used. All {@link MarkConfiguration} objects referenced are part of the result of
+     * {@link #getAllMarks()}.
+     */
+    Iterable<WaypointWithMarkConfiguration> getWaypoints();
+    
+    /**
+     * Returns all {@link MarkConfiguration MarkConfigurations} that are part of the {@link CourseConfigurationBase} and
+     * have a role name explicitly associated.
+     */
+    Map<MarkConfiguration, String> getAssociatedRoles();
+    
+    /**
+     * A {@link CourseConfigurationBase} having a {@link RepeatablePart} can optionally also specify a number of laps.
+     * Depending on the use-case this can be omitted (saving a course template) or is required (creating a regatta
+     * course).
+     */
+    Integer getNumberOfLaps();
+
+    /**
+     * The waypoint sequence with lap count applied. If this course configuration has no {@link #hasRepeatablePart()
+     * repeatable part}, the result is the same as that of {@link #getWaypoints()}. The result has
+     * {@link MarkConfiguration configuration information} for the marks used. All {@link MarkConfiguration} objects
+     * referenced are part of the result of {@link #getAllMarks()}.
+     */
+    Iterable<WaypointWithMarkConfiguration> getWaypoints(int numberOfLaps);
+}
--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/coursetemplate/impl/CourseConfigurationImpl.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/coursetemplate/impl/CourseConfigurationImpl.java
@@ -6,83 +6,20 @@ import java.util.Set;
 
 import com.sap.sailing.domain.coursetemplate.CourseConfiguration;
 import com.sap.sailing.domain.coursetemplate.CourseTemplate;
+import com.sap.sailing.domain.coursetemplate.IsMarkRole;
 import com.sap.sailing.domain.coursetemplate.MarkConfiguration;
 import com.sap.sailing.domain.coursetemplate.RepeatablePart;
 import com.sap.sailing.domain.coursetemplate.WaypointWithMarkConfiguration;
 
-public class CourseConfigurationImpl implements CourseConfiguration {
-    private static final long serialVersionUID = -41810982418755899L;
-    
-    private final CourseTemplate optionalCourseTemplate;
-    // TODO decide if we should combine markConfigurations and roleMapping to one field
-    private final Set<MarkConfiguration> markConfigurations;
-    private final Map<MarkConfiguration, String> associatedRoles;
-    private final List<WaypointWithMarkConfiguration> waypoints;
-    private final RepeatablePart optionalRepeatablePart;
-    private final String name;
+public class CourseConfigurationImpl extends CourseConfigurationBaseImpl implements CourseConfiguration {
 
-    private Integer numberOfLaps;
-    
+    private static final long serialVersionUID = 4471259140392773170L;
+
     public CourseConfigurationImpl(CourseTemplate optionalCourseTemplate, Set<MarkConfiguration> markConfigurations,
             Map<MarkConfiguration, String> associatedRoles, List<WaypointWithMarkConfiguration> waypoints,
             RepeatablePart optionalRepeatablePart, Integer numberOfLaps, String name) {
-        super();
-        this.optionalCourseTemplate = optionalCourseTemplate;
-        this.markConfigurations = markConfigurations;
-        this.associatedRoles = associatedRoles;
-        this.waypoints = waypoints;
-        this.optionalRepeatablePart = optionalRepeatablePart;
-        this.numberOfLaps = numberOfLaps;
-        this.name = name;
-    }
-
-    @Override
-    public RepeatablePart getRepeatablePart() {
-        return optionalRepeatablePart;
-    }
-
-    @Override
-    public CourseTemplate getOptionalCourseTemplate() {
-        return optionalCourseTemplate;
-    }
-
-    @Override
-    public Iterable<MarkConfiguration> getAllMarks() {
-        return markConfigurations;
-    }
-
-    @Override
-    public Iterable<WaypointWithMarkConfiguration> getWaypoints() {
-        return waypoints;
-    }
-
-    @Override
-    public Map<MarkConfiguration, String> getAssociatedRoles() {
-        return associatedRoles;
-    }
-
-    @Override
-    public Integer getNumberOfLaps() {
-        return numberOfLaps;
-    }
-
-    @Override
-    public Iterable<WaypointWithMarkConfiguration> getWaypoints(int numberOfLaps) {
-        final Iterable<WaypointWithMarkConfiguration> result;
-        if (hasRepeatablePart()) {
-            if (numberOfLaps < 1) {
-                throw new IllegalArgumentException("The course template "+this+" has a repeatable part, hence the number of laps needs to be at least 1.");
-            }
-            result = optionalRepeatablePart.createSequence(numberOfLaps, waypoints);
-        } else {
-            result = waypoints;
-        }
-        return result;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
+        super(optionalCourseTemplate, markConfigurations, associatedRoles, waypoints, optionalRepeatablePart,
+                numberOfLaps, name);
     }
 
 }
