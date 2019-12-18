8019903b83c8fc8156737c498fde6112e80292dc
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2019-12-18 11:10:31 +0100

    Added equals and hashCode to TrackingConnectorInfoDTO

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/TrackingConnectorInfoDTO.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/shared/TrackingConnectorInfoDTO.java
@@ -20,4 +20,35 @@ public class TrackingConnectorInfoDTO implements IsSerializable {
     public String getWebUrl() {
         return webUrl;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((trackedBy == null) ? 0 : trackedBy.hashCode());
+        result = prime * result + ((webUrl == null) ? 0 : webUrl.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TrackingConnectorInfoDTO other = (TrackingConnectorInfoDTO) obj;
+        if (trackedBy == null) {
+            if (other.trackedBy != null)
+                return false;
+        } else if (!trackedBy.equals(other.trackedBy))
+            return false;
+        if (webUrl == null) {
+            if (other.webUrl != null)
+                return false;
+        } else if (!webUrl.equals(other.webUrl))
+            return false;
+        return true;
+    }
 }
