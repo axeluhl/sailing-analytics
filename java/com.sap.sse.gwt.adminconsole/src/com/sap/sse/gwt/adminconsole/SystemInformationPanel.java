f894201ae6c44a1804b96559301aec20ce9f1018
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2020-01-20 16:18:22 +0100

    version info now collapsable

--- java/com.sap.sse.gwt.adminconsole/src/com/sap/sse/gwt/adminconsole/SystemInformationPanel.java
+++ java/com.sap.sse.gwt.adminconsole/src/com/sap/sse/gwt/adminconsole/SystemInformationPanel.java
@@ -1,28 +1,69 @@
 package com.sap.sse.gwt.adminconsole;
 
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.sap.sse.gwt.client.ErrorReporter;
 import com.sap.sse.gwt.client.ServerInfoDTO;
 
 public class SystemInformationPanel extends FlowPanel {
 
+    private final FlowPanel panel;
     private final Label buildVersionText;
-    
-    public SystemInformationPanel(final ServerInfoDTO serverInfo, final ErrorReporter errorReporter, final StringMessages stringMessages) {
+    private final String fullVersionText;
+    private final Button collapseButton;
+    private boolean expanded;
+
+    public SystemInformationPanel(final ServerInfoDTO serverInfo, final ErrorReporter errorReporter,
+            final StringMessages stringMessages) {
         super();
+        fullVersionText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, "
+                + "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna "
+                + "aliquyam erat, sed diam voluptua. At vero eos et accusam et justo "
+                + "duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata "
+                + "sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, "
+                + "consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt "
+                + "ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero "
+                + "eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, "
+                + "no sea takimata sanctus est Lorem ipsum dolor sit amet.";
         buildVersionText = new Label("");
-        addFloatingWidget(buildVersionText);
-        if (serverInfo != null) {
-            buildVersionText.setText(stringMessages.version(serverInfo.getBuildVersion()));
-        } else {
-            buildVersionText.setText(stringMessages.version(stringMessages.unknown()));
-        }
+        collapseButton = new Button("+");
+        collapseButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                toggleExpand();
+            }
+        });
+        panel = new FlowPanel();
+        panel.add(collapseButton);
+        panel.add(buildVersionText);
+        
+        addFloatingWidget(panel);
+        buildVersionText.setText("Version information is hidden (click to expand)");
+        // if (serverInfo != null) {
+        // buildVersionText.setText(stringMessages.version(serverInfo.getBuildVersion()));
+        // } else {
+        // buildVersionText.setText(stringMessages.version(stringMessages.unknown()));
+        // }
     }
 
-    private void addFloatingWidget(Widget w) { 
+    protected void toggleExpand() {
+        if (expanded) {
+            buildVersionText.setText("Version information is hidden (click to expand)");
+            collapseButton.setText("+");
+        } else {
+            buildVersionText.setText(fullVersionText);
+            collapseButton.setText("-");
+        }
+        expanded = !expanded;
+    }
+
+    private void addFloatingWidget(Widget w) {
         w.getElement().getStyle().setFloat(Style.Float.LEFT);
         add(w);
     }
