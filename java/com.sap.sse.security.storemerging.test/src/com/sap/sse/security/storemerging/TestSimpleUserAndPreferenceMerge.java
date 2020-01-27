08ac982d8c026c1990a8423a29e256d956324dfd
Axel Uhl <axel.uhl@sap.com>
2020-01-27 16:37:26 +0100

    bug4006: added more tests for user store merging

--- java/com.sap.sse.security.storemerging.test/src/com/sap/sse/security/storemerging/TestSimpleUserAndPreferenceMerge.java
+++ java/com.sap.sse.security.storemerging.test/src/com/sap/sse/security/storemerging/TestSimpleUserAndPreferenceMerge.java
@@ -9,7 +9,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.StreamSupport;
 
 import org.bson.Document;
 import org.junit.After;
@@ -26,6 +28,7 @@ import com.sap.sse.security.SecurityService;
 import com.sap.sse.security.interfaces.AccessControlStore;
 import com.sap.sse.security.interfaces.UserImpl;
 import com.sap.sse.security.interfaces.UserStore;
+import com.sap.sse.security.shared.RoleDefinition;
 import com.sap.sse.security.shared.UserGroupManagementException;
 import com.sap.sse.security.shared.UserManagementException;
 import com.sap.sse.security.shared.impl.User;
@@ -138,8 +141,12 @@ public class TestSimpleUserAndPreferenceMerge {
         assertNotNull(targetUserStore.getUserByName("YCaT"));
         assertNotNull(targetUserStore.getUserByName("YCaT-member1"));
         assertNotNull(targetUserStore.getUserByName("YCaT-member2"));
-        assertNotNull(targetUserStore.getUserByName("uhl3"));
+        final User uhl5InTarget = targetUserStore.getUserByName("uhl5");
+        assertNotNull(uhl5InTarget); // an equal-named user also exists in source, but with different e-mail
+        final User uhl3InTargetUserStore = targetUserStore.getUserByName("uhl3");
+        assertNotNull(uhl3InTargetUserStore);
         assertNull(targetUserStore.getUserByName("uhl4"));
+        assertNull(targetUserStore.getUserGroupByName("test-server"));
         assertEquals(1, Util.size(targetUserStore.getUserGroupByName("uhl-tenant").getUsers()));
         assertSame(targetUserStore.getUserByName("uhl"), targetUserStore.getUserGroupByName("uhl-tenant").getUsers().iterator().next());
         final AccessControlStore targetAccessControlStore = merger.getTargetAccessControlStore();
@@ -151,10 +158,18 @@ public class TestSimpleUserAndPreferenceMerge {
         assertNotNull(sourceUserStore.getUserByName("<all>"));
         assertNotNull(sourceUserStore.getUserByName("uhl"));
         assertNotNull(sourceUserStore.getUserByName("uhl3"));
+        assertEquals(targetUserStore.getUserByName("uhl3").getEmail(), sourceUserStore.getUserByName("uhl3").getEmail());
         assertNotNull(sourceUserStore.getUserByName("axel.uhl"));
         assertNotNull(sourceUserStore.getUserByName("uhl4"));
+        assertNotNull(sourceUserStore.getUserByName("uhl5")); // an equal-named user also exists in target, but with different e-mail
+        assertTrue(StreamSupport
+                .stream(sourceUserStore.getUserByName("uhl5").getRoles().spliterator(), /* parallel */ false)
+                .filter(r -> r.getName().equals("user") && r.getQualifiedForTenant() != null &&
+                r.getQualifiedForTenant().getName().equals("uhl4-tenant"))
+                .findAny().isPresent());
+        assertNotNull(sourceUserStore.getUserGroupByName("test-server"));
         // check result in target user store:
-        assertNotNull(targetUserStore.getUserByName("uhl3"));
+        assertSame(uhl3InTargetUserStore, targetUserStore.getUserByName("uhl3")); // merged, so no new user in this case
         assertNotNull(targetUserStore.getUserByName("uhl4")); // in particular new user uhl4 is expected to be present now
         // the uhl-tenant group is expected to have been merged: the uhl4 user is part of that group in source1
         assertEquals(3, Util.size(targetUserStore.getUserGroupByName("uhl-tenant").getUsers()));
@@ -166,5 +181,21 @@ public class TestSimpleUserAndPreferenceMerge {
         final User uhl4InTarget = targetUserStore.getUserByName("uhl4");
         assertTrue(Util.contains(targetUserStore.getUserGroupByName("uhl-tenant").getUsers(), uhl4InTarget));
         assertSame(uhl4InTarget, targetAccessControlStore.getOwnership(uhl4InTarget.getIdentifier()).getAnnotation().getUserOwner());
+        final UserGroup testServerGroupInTargetAfterMerge = targetUserStore.getUserGroupByName("test-server");
+        assertNotNull(testServerGroupInTargetAfterMerge);
+        final Map<RoleDefinition, Boolean> rolesOnTestServerGroup = testServerGroupInTargetAfterMerge.getRoleDefinitionMap();
+        final Optional<RoleDefinition> sailingViewerRoleInTarget = StreamSupport.stream(targetUserStore.getRoleDefinitions().spliterator(), /* parallel */ false).
+                filter(rd->rd.getName().equals("sailing_viewer")).findAny();
+        assertTrue(sailingViewerRoleInTarget.isPresent());
+        assertTrue(rolesOnTestServerGroup.containsKey(sailingViewerRoleInTarget.get()));
+        assertTrue(rolesOnTestServerGroup.get(sailingViewerRoleInTarget.get()));
+        assertSame(uhl5InTarget, targetUserStore.getUserByName("uhl5")); // still the same
+        // Now ensure that no merge took place, particularly by validating that no "user" role with uhl4-tenant as group
+        // qualification was merged
+        assertFalse(StreamSupport
+                .stream(targetUserStore.getUserByName("uhl5").getRoles().spliterator(), /* parallel */ false)
+                .filter(r -> r.getName().equals("user") && r.getQualifiedForTenant() != null &&
+                r.getQualifiedForTenant().getName().equals("uhl4-tenant"))
+                .findAny().isPresent());
     }
 }
