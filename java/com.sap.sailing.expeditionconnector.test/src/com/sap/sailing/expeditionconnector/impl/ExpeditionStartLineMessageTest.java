2eb03cb8b068023d700a688e6cb8e1f2c71ec9e6
Axel Uhl <axel.uhl@sap.com>
2020-02-20 17:17:19 +0100

    added an Expedition message parsing/checksumming test

--- java/com.sap.sailing.expeditionconnector.test/src/com/sap/sailing/expeditionconnector/impl/ExpeditionStartLineMessageTest.java
+++ java/com.sap.sailing.expeditionconnector.test/src/com/sap/sailing/expeditionconnector/impl/ExpeditionStartLineMessageTest.java
@@ -1,5 +1,18 @@
 package com.sap.sailing.expeditionconnector.impl;
 
-public class ExpeditionStartLineMessageTest {
+import static org.junit.Assert.assertEquals;
 
+import java.net.SocketException;
+
+import org.junit.Test;
+
+import com.sap.sailing.expeditionconnector.UDPExpeditionReceiver;
+
+public class ExpeditionStartLineMessageTest {
+    @Test
+    public void testCreatePingStartLineMessages() throws SocketException {
+        final int checksum = new ExpeditionMessageParser(new UDPExpeditionReceiver(/* listening on port */ 0))
+                .computeChecksum("#0,4,133.9,5,2.12,6,348.5,94,349.8,95,2.41*09");
+        assertEquals(9, checksum);
+    }
 }
