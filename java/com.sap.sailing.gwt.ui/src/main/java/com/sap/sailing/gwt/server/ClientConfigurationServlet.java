5263a1fb6ea7b1a67de3d89e330be6a57b3afbd9
Georg Herdt <herdt@agima-it.de>
2020-02-21 16:43:34 +0100

    switch to servlet instead of filter 5153

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/server/ClientConfigurationServlet.java
@@ -0,0 +1,56 @@
+package com.sap.sailing.gwt.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+public class ClientConfigurationServlet extends HttpServlet {
+
+    /** serial version uid */
+    private static final long serialVersionUID = -2228462977010198686L;
+    
+    private final ConcurrentHashMap<String, byte[]> cache = new ConcurrentHashMap<>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        final String servletPath = req.getServletPath();
+        byte[] value = null;
+        if ((value = cache.get(servletPath)) != null) {
+            IOUtils.write(value, resp.getOutputStream());
+            return;
+        }
+
+        boolean deBrandingActive = Boolean.valueOf(System.getProperty("com.sap.sailing.debranding", "false"));
+
+        String title = "";
+        String faviconPath = "images/whitelabel.ico";
+        String appiconPath = "images/sailing-app-icon.png";
+        if (!deBrandingActive) {
+            title = "SAP ";
+            faviconPath = "images/sap.ico";
+            appiconPath = "images/sap-sailing-app-icon.png";
+        }
+
+        try (InputStream in = this.getServletContext().getResourceAsStream(servletPath);) {
+            byte[] buffer = IOUtils.toByteArray(in);
+
+            String content = new String(buffer);
+            String replaced = content.replace("${SAP}", title).replace("${faviconPath}", faviconPath)
+                    .replace("${appiconPath}", appiconPath)
+                    .replace("${debrandingActive}", Boolean.toString(deBrandingActive));
+
+            byte[] bytes = replaced.getBytes();
+            IOUtils.write(bytes, resp.getOutputStream());
+            cache.computeIfAbsent(servletPath, key -> bytes);
+        }
+    }
+
+}
