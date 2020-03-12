774da23e7786400d2536ddccada7526140ba8660
Thomas Wiese <mail@thomaswiese.de>
2020-03-12 15:20:37 +0100

    Bug 5214: fix test, added a nullcheck and some commenting

--- java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/test/DefaultTenantApiTest.java
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/api/test/DefaultTenantApiTest.java
@@ -21,9 +21,11 @@ import com.sap.sailing.selenium.test.AbstractSeleniumTest;
 public class DefaultTenantApiTest extends AbstractSeleniumTest {
 
     private static final String TENANT_GROUP_NAME = "NewGroup";
+    private static final String EVENT_NAME = "TestEvent2";
     private final SecurityApi securityApi = new SecurityApi();
     private final UserGroupApi userGroupApi = new UserGroupApi();
     private ApiContext adminSecurityCtx;
+    private ApiContext userSecurityCtx;
     private AdminConsolePage adminConsole;
     private UUID tenantGroupId;
 
@@ -33,24 +35,23 @@ public class DefaultTenantApiTest extends AbstractSeleniumTest {
         super.setUp();
         adminSecurityCtx = createAdminApiContext(getContextRoot(), SECURITY_CONTEXT);
         securityApi.createUser(adminSecurityCtx, "donald", "Donald Duck", null, "daisy0815");
+        userSecurityCtx = ApiContext.createApiContext(getContextRoot(), SECURITY_CONTEXT, "donald", "daisy0815");
         adminConsole = goToPage(getWebDriver(), getContextRoot());
         adminConsole.goToLocalServerPanel().setSelfServiceServer(true);
-        tenantGroupId = userGroupApi.createUserGroup(adminSecurityCtx, TENANT_GROUP_NAME).getGroupId();
+        tenantGroupId = userGroupApi.createUserGroup(userSecurityCtx, TENANT_GROUP_NAME).getGroupId();
     }
 
     @Test
     public void testEventApiWithUserTenant() {
-        final String eventName = "TestEvent2";
-        EventApi.create(eventName, "GC 32", CompetitorRegistrationType.CLOSED, "somewhere")
-                .auth("donald", "daisy0815").run();
-        assertEquals("donald-tenant", adminConsole.goToEvents().getEventEntry(eventName).getColumnContent("Group"));
+        EventApi.create(EVENT_NAME, "GC 32", CompetitorRegistrationType.CLOSED, "somewhere").auth("donald", "daisy0815")
+                .run();
+        assertEquals("donald-tenant", adminConsole.goToEvents().getEventEntry(EVENT_NAME).getColumnContent("Group"));
     }
 
     @Test
     public void testEventApiWithDefaultTenant() {
-        EventApi.create("testEvent2", "GC 32", CompetitorRegistrationType.CLOSED, "somewhere")
+        EventApi.create(EVENT_NAME, "GC 32", CompetitorRegistrationType.CLOSED, "somewhere")
                 .header("tenantGroupId", tenantGroupId.toString()).auth("donald", "daisy0815").run();
-        assertEquals(TENANT_GROUP_NAME,
-                adminConsole.goToEvents().getEventEntry("testEvent2").getColumnContent("Group"));
+        assertEquals(TENANT_GROUP_NAME, adminConsole.goToEvents().getEventEntry(EVENT_NAME).getColumnContent("Group"));
     }
 }
--- java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/impl/DefaultTenantRequestFilter.java
+++ java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/impl/DefaultTenantRequestFilter.java
@@ -2,6 +2,7 @@ package com.sap.sailing.server.gateway.impl;
 
 import java.io.IOException;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -18,19 +19,37 @@ import org.osgi.util.tracker.ServiceTracker;
 import com.sap.sailing.server.gateway.jaxrs.RestServletContainer;
 import com.sap.sse.security.SecurityService;
 
+/**
+ * Request filter to allow setting a divergent tenant group, for overriding the group used otherwise for created or
+ * updated domain objects. The divergent tenant group is passed by its UUID on a request by the header value
+ * <b>tenantGroupId</b>.
+ *
+ */
 @Provider
 public class DefaultTenantRequestFilter implements Filter {
+
+    private static final Logger logger = Logger.getLogger(DefaultTenantRequestFilter.class.getName());
+
+    private static final String DEFAULT_TENANT_GROUP_ID_HEADER_KEY = "tenantGroupId";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
+    /**
+     * Checks the headers of a http request for an existing key <b>tenantGroupId<b>. If available the header value is
+     * set as divergent default tenant group for this request.
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        final String defaultTenantGroupId = httpRequest.getHeader("tenantGroupId");
-        setDefaultTenantForRequest(request.getServletContext(), defaultTenantGroupId);
+        final String defaultTenantGroupId = httpRequest.getHeader(DEFAULT_TENANT_GROUP_ID_HEADER_KEY);
+        if (defaultTenantGroupId != null) {
+            setDefaultTenantForRequest(request.getServletContext(), defaultTenantGroupId);
+            logger.info("executing request " + httpRequest.getRequestURI() + " with divergent tenant group "
+                    + defaultTenantGroupId);
+        }
         chain.doFilter(request, response);
     }
 
