d45ecada96c7fcf25961ace6ef9d28b68c19ec0b
Georg Herdt <herdt@agima-it.de>
2020-03-23 16:20:24 +0100

    prepare entry point for sailingservice write #5221

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/AbstractSailingWriteEntryPoint.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/client/AbstractSailingWriteEntryPoint.java
@@ -1,28 +1,25 @@
 package com.sap.sailing.gwt.ui.client;
 
-import com.sap.sailing.domain.common.security.SecuredDomainType;
 import com.sap.sailing.gwt.common.communication.routing.ProvidesLeaderboardRouting;
-import com.sap.sse.security.ui.client.AbstractSecureEntryPoint;
+import com.sap.sse.gwt.client.ServiceRoutingProvider;
 
-public abstract class AbstractSailingEntryPoint<T> extends AbstractSecureEntryPoint<StringMessages> {
-    protected T sailingService;
-
-    @Override
-    protected void doOnModuleLoad() {
-        super.doOnModuleLoad();
-        getUserService().addKnownHasPermissions(SecuredDomainType.getAllInstances());
-    }
-
+public class AbstractSailingWriteEntryPoint extends AbstractSailingEntryPoint<SailingServiceWriteAsync> {
+    
     /**
      * Lazily initialize sailing service. Concrete entry point subclasses may influence sailing service creation by
      * implementing a given routing information providing interface {@link ProvidesLeaderboardRouting}. The routing
      * information provided via {@link ProvidesLeaderboardRouting#getLeaderboardName()} needs to be available when this
      * method is called for the first time. Repetitive calls will not cause a new leaderboard name to take effect.
      */
-    abstract protected T getSailingService();
-    
     @Override
-    protected StringMessages createStringMessages() {
-        return StringMessages.INSTANCE;
+    protected SailingServiceWriteAsync getSailingService() {
+        if (sailingService == null) {
+            if (this instanceof ServiceRoutingProvider) {
+                sailingService = SailingServiceHelper.createSailingServiceWriteInstance((ServiceRoutingProvider)this);
+            } else {
+                sailingService = SailingServiceHelper.createSailingServiceWriteInstance();
+            }
+        }
+        return sailingService;
     }
 }
