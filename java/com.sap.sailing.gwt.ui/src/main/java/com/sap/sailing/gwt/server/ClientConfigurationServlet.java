9300c5f81be6918201351d0440967cbcfd6d5edd
Georg Herdt <herdt@agima-it.de>
2020-03-27 13:01:16 +0100

    implement more generic way for replacement #5153

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/server/ClientConfigurationServlet.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/server/ClientConfigurationServlet.java
@@ -2,6 +2,8 @@ package com.sap.sailing.gwt.server;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.ServletException;
@@ -25,37 +27,51 @@ public class ClientConfigurationServlet extends HttpServlet {
 
         final boolean deBrandingActive = Boolean.valueOf(System.getProperty(DEBRANDING_PROPERTY_NAME, "false"));
         final String servletPath = req.getServletPath();
-        byte[] value = null;
-        if ((value = cache.get(generateKey(servletPath, deBrandingActive))) != null) {
-            IOUtils.write(value, resp.getOutputStream());
+        byte[] cachedPage = null;
+        if ((cachedPage = cache.get(generateKey(servletPath, deBrandingActive))) != null) {
+            IOUtils.write(cachedPage, resp.getOutputStream());
             return;
-        }
-        
-        String title = "";
-        String faviconPath = "images/whitelabel.ico";
-        String appiconPath = "images/sailing-app-icon.png";
-        if (!deBrandingActive) {
-            title = "SAP ";
-            faviconPath = "images/sap.ico";
-            appiconPath = "images/sap-sailing-app-icon.png";
         }
 
         try (InputStream in = this.getServletContext().getResourceAsStream(servletPath);) {
             byte[] buffer = IOUtils.toByteArray(in);
 
             String content = new String(buffer);
-            String replaced = content.replace("${SAP}", title).replace("${faviconPath}", faviconPath)
-                    .replace("${appiconPath}", appiconPath)
-                    .replace("${debrandingActive}", Boolean.toString(deBrandingActive));
-
-            byte[] bytes = replaced.getBytes();
+            for (Map.Entry<String, String> item : createReplacementMap(deBrandingActive).entrySet()) {
+                content = content.replace("${" + item.getKey() + "}", item.getValue());
+            }
+            
+            byte[] bytes = content.getBytes();
             IOUtils.write(bytes, resp.getOutputStream());
             cache.computeIfAbsent(generateKey(servletPath, deBrandingActive), key -> bytes);
+        } catch (RuntimeException e) {
+            
         }
     }
 
     private String generateKey(String servletPath, boolean active) {
         return servletPath + "_" + active;
     }
-    
+
+    private Map<String,String> createReplacementMap(boolean deBrandingActive) {
+        Map<String,String> map = new HashMap<>();
+        String title = "";
+        String faviconPath = "images/whitelabel.ico";
+        String appiconPath = "images/sailing-app-icon.png";
+        String saplogoBrowserInfo = "";
+        if (!deBrandingActive) {
+            title = "SAP ";
+            faviconPath = "images/sap.ico";
+            appiconPath = "images/sap-sailing-app-icon.png";
+            saplogoBrowserInfo = "<a class=\"sapLogo\" href=\"http://www.sap.com\"><img class=\"sapLogoImage\" src=\"/images/logo-small@2x.png\" alt=\"SAP Website\"/></a>\r\n";
+        }
+
+        map.put("SAP", title);
+        map.put("faviconPath", faviconPath);
+        map.put("appiconPath", appiconPath);
+        map.put("debrandingActive", Boolean.toString(deBrandingActive));
+        map.put("saplogoBrowserInfo", saplogoBrowserInfo);
+
+        return map;
+    }
 }
