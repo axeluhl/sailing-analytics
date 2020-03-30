03fcff1c8cc6a95a389208da82b3c723b275595c
Axel Uhl <axel.uhl@sap.com>
2020-03-30 12:31:48 +0200

    fixed finding of open-ended device mapping

--- java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/regatta/impl/OpenEndedDeviceMappingFinder.java
+++ java/com.sap.sailing.domain.shared.android/src/com/sap/sailing/domain/abstractlog/regatta/impl/OpenEndedDeviceMappingFinder.java
@@ -1,12 +1,16 @@
 package com.sap.sailing.domain.abstractlog.regatta.impl;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 import com.sap.sailing.domain.abstractlog.BaseLogAnalyzer;
 import com.sap.sailing.domain.abstractlog.regatta.RegattaLog;
 import com.sap.sailing.domain.abstractlog.regatta.RegattaLogEvent;
 import com.sap.sailing.domain.abstractlog.regatta.RegattaLogEventVisitor;
-import com.sap.sailing.domain.abstractlog.regatta.events.RegattaLogDeviceMappingEvent;
+import com.sap.sailing.domain.abstractlog.regatta.events.RegattaLogCloseOpenEndedDeviceMappingEvent;
+import com.sap.sailing.domain.abstractlog.regatta.tracking.analyzing.impl.RegattaLogDeviceMappingFinder;
+import com.sap.sailing.domain.racelogtracking.DeviceMappingWithRegattaLogEvent;
 import com.sap.sse.common.WithID;
 
 
@@ -17,28 +21,39 @@ import com.sap.sse.common.WithID;
  * If no corresponding event has been found, {@code null} is returned.
  */
 public class OpenEndedDeviceMappingFinder extends BaseLogAnalyzer<RegattaLog, RegattaLogEvent, RegattaLogEventVisitor, Serializable> {
-
     private final WithID mappedTo;
     private final Serializable deviceUuidAsString;
     
+    /**
+     * Combines revocations, range closing events and regular mapping events
+     */
+    private final RegattaLogDeviceMappingFinder<WithID> mappingFinder;
+    
     public OpenEndedDeviceMappingFinder(RegattaLog log, WithID mappedTo, String deviceUuidAsString) {
         super(log);
+        this.mappingFinder = new RegattaLogDeviceMappingFinder<>(log);
         this.mappedTo = mappedTo;
         this.deviceUuidAsString = deviceUuidAsString;
     }
 
     @Override
     protected Serializable performAnalysis() {
-        for (RegattaLogEvent regattaLogEvent : log.getUnrevokedEvents()) {
-            if (regattaLogEvent instanceof RegattaLogDeviceMappingEvent<?>) {
-                RegattaLogDeviceMappingEvent<?> deviceMappingEvent = (RegattaLogDeviceMappingEvent<?>) regattaLogEvent;
-                if (deviceMappingEvent.getMappedTo().equals(mappedTo) &&
-                        deviceMappingEvent.getDevice().getStringRepresentation().equals(deviceUuidAsString)) {
-                    return deviceMappingEvent.getId();
+        final Map<WithID, List<DeviceMappingWithRegattaLogEvent<WithID>>> mappings = mappingFinder.analyze();
+        final List<DeviceMappingWithRegattaLogEvent<WithID>> mappingsForObject = mappings.get(mappedTo);
+        Serializable result;
+        if (mappingsForObject != null) {
+            result = null;
+            for (final DeviceMappingWithRegattaLogEvent<WithID> mappingEvent : mappingsForObject) {
+                if (mappingEvent.getDevice().getStringRepresentation().equals(deviceUuidAsString) &&
+                    mappingEvent.getTimeRange().hasOpenEnd()) {
+                    result = mappingEvent.getRegattaLogEvent().getId();
+                    break;
                 }
             }
+        } else {
+            result = null;
         }
-        return null;
+        return result;
     }
 
 }
