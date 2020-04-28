7fa577ece43911a9e0e10ec0769e4d4dd601aaca
Axel Uhl <axel.uhl@sap.com>
2020-04-28 18:34:04 +0200

    make sure the DummyCompetitorsResource has the correct racingEventService set

--- java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/jaxrs/CompetitorsResourceTest.java
+++ java/com.sap.sailing.server.gateway.test/src/com/sap/sailing/server/gateway/test/jaxrs/CompetitorsResourceTest.java
@@ -1,6 +1,6 @@
 package com.sap.sailing.server.gateway.test.jaxrs;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Collections;
 
@@ -34,9 +34,9 @@ public class CompetitorsResourceTest extends AbstractJaxRsApiTest {
     public void testGetCompetitorAsJson() throws Exception {
         String jsonString = StreamingOutputUtil.getEntityAsString(competitorsResource.getCompetitor(id, null, null).getEntity());
         JSONObject json = Helpers.toJSONObjectSafe(JSONValue.parse(jsonString));
-        assertTrue(json.get("id").equals(id));
-        assertTrue(json.get("name").equals(name));
-        assertTrue(json.get("nationality").equals(nationality));
-        assertTrue(json.get("countryCode").equals(countryCode));
+        assertEquals(id, json.get("id"));
+        assertEquals(name, json.get("name"));
+        assertEquals(nationality, json.get("nationality"));
+        assertEquals(countryCode, json.get("countryCode"));
     }
 }
