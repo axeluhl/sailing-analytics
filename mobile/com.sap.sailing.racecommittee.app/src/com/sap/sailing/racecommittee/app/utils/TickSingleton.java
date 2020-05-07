f91ed675068511d6d23ddf958c41d75900fd8f94
Robert Benedikt <robert.benedikt@sap.com>
2020-05-07 14:37:46 +0200

    refactor: singleton for tick on each second to ensure that it ticks at the beginning of a second

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/utils/TickSingleton.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/utils/TickSingleton.java
@@ -1,45 +1,52 @@
 package com.sap.sailing.racecommittee.app.utils;
 
-import java.util.HashSet;
-import java.util.Set;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.sap.sse.common.TimePoint;
 import com.sap.sse.common.impl.MillisecondsTimePoint;
 
-import android.os.Handler;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Reports a tick every second as a singleton
- * 
- * TODO: make it possible to report ticks in other times (e.g. 2s...)
+ * Reports a tick every second as a singleton.
  */
-public enum TickSingleton implements Runnable {
+public enum TickSingleton {
     INSTANCE;
 
-    private Set<TickListener> listeners = new HashSet<>();
-    private Handler handler;
+    private final List<TickListener> listeners = new ArrayList<>();
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            TimePoint now = MillisecondsTimePoint.now();
+            final long delay = now.asMillis() / 1000 * 1000 + 1000 - now.asMillis();
+            handler.postDelayed(this, delay);
+            //Notify all listeners
+            for (TickListener listener : listeners) {
+                listener.notifyTick(now);
+            }
+        }
+    };
 
-    private TickSingleton() {
-        handler = new Handler();
-        handler.post(this);
-    }
-
+    /**
+     * If the {@link TickListener} has been registered, a new tick will be invoked immediately
+     * to ensure that {@link TickListener#notifyTick(TimePoint)} is called as soon as possible.
+     *
+     * @param toRegister The listener to register
+     */
     public void registerListener(TickListener toRegister) {
-        listeners.add(toRegister);
+        if (listeners.add(toRegister)) {
+            handler.removeCallbacks(runnable);
+            handler.post(runnable);
+        }
     }
 
     public void unregisterListener(TickListener toUnregister) {
         listeners.remove(toUnregister);
-    }
-
-    public void run() {
-        TimePoint now = MillisecondsTimePoint.now();
-        for (TickListener item : listeners) {
-            if (item != null) {
-                item.notifyTick(now);
-            }
+        if (listeners.isEmpty()) {
+            handler.removeCallbacks(runnable);
         }
-        handler.removeCallbacks(this);
-        handler.postDelayed(this, 200);
     }
 }
