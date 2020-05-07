43c2124e0fc2e59ce0d2090a4caa15533e4843b3
Robert Benedikt <robert.benedikt@sap.com>
2020-05-07 14:35:31 +0200

    feat: improve custom view for displaying the current flag and its timer

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/views/FlagTimeView.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/views/FlagTimeView.java
@@ -3,15 +3,15 @@ package com.sap.sailing.racecommittee.app.ui.views;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.sap.sailing.android.shared.logging.ExLog;
 import com.sap.sailing.android.shared.util.BitmapHelper;
+import com.sap.sailing.domain.abstractlog.race.RaceLogEventVisitor;
+import com.sap.sailing.domain.abstractlog.race.RaceLogFlagEvent;
+import com.sap.sailing.domain.abstractlog.race.impl.BaseRaceLogEventVisitor;
 import com.sap.sailing.domain.abstractlog.race.state.RaceState;
 import com.sap.sailing.domain.abstractlog.race.state.racingprocedure.FlagPoleState;
 import com.sap.sailing.domain.abstractlog.race.state.racingprocedure.RacingProcedure;
@@ -30,14 +30,17 @@ import java.util.List;
 
 public class FlagTimeView extends LinearLayout implements TickListener {
 
-    private static final String TAG = FlagTimeView.class.getSimpleName();
-
     private final ImageView imageView;
     private final TextView textView;
 
     private final int flagSize;
 
     private RaceState state;
+    private RaceLogRaceStatus status;
+    private RacingProcedure procedure;
+    private TimePoint startTime;
+    private TimePoint finishingTime;
+    private TimePoint nextTime;
 
     public FlagTimeView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -53,108 +56,134 @@ public class FlagTimeView extends LinearLayout implements TickListener {
         }
     }
 
+    private final RaceLogEventVisitor raceLogFlagEventListener = new BaseRaceLogEventVisitor() {
+        @Override
+        public void visit(RaceLogFlagEvent event) {
+            final TimePoint now = MillisecondsTimePoint.now();
+            setVisibility(VISIBLE);
+            setProcedureFlag(now);
+            notifyTick(now);
+        }
+    };
+
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         TickSingleton.INSTANCE.registerListener(this);
+        if (state != null) {
+            state.getRaceLog().addListener(raceLogFlagEventListener);
+        }
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         TickSingleton.INSTANCE.unregisterListener(this);
+        if (state != null) {
+            state.getRaceLog().removeListener(raceLogFlagEventListener);
+        }
     }
 
     public void setRaceState(RaceState state) {
+        final TimePoint now = MillisecondsTimePoint.now();
         this.state = state;
-        notifyTick(MillisecondsTimePoint.now());
+        state.getRaceLog().addListener(raceLogFlagEventListener);
+        status = state.getStatus();
+        procedure = state.getTypedRacingProcedure();
+        startTime = state.getStartTime();
+        finishingTime = state.getFinishingTime();
+        switch (status) {
+            case SCHEDULED:
+            case STARTPHASE:
+                setVisibility(VISIBLE);
+                setProcedureFlag(now);
+                notifyTick(now);
+                break;
+            case RUNNING:
+                if (procedure.isIndividualRecallDisplayed() && now.before(procedure.getIndividualRecallRemovalTime())) {
+                    setVisibility(VISIBLE);
+                    setProcedureFlag(now);
+                    notifyTick(now);
+                }
+                break;
+            case FINISHING:
+                setVisibility(VISIBLE);
+                setFinishingFlag(now);
+                notifyTick(now);
+                break;
+            default:
+                setVisibility(GONE);
+                clear();
+        }
     }
 
     @Override
     public void notifyTick(TimePoint now) {
-        if (state == null || state.getStartTime() == null || state.getFinishedTime() != null) {
-            setVisibility(View.GONE);
-            return;
+        switch (status) {
+            case SCHEDULED:
+            case STARTPHASE:
+            case RUNNING:
+                if (nextTime != null && !now.before(nextTime)) {
+                    setProcedureFlag(now);
+                }
+                if (nextTime != null) {
+                    textView.setText(TimeUtils.formatDuration(now, nextTime, false));
+                }
+                break;
+            case FINISHING:
+                textView.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+                textView.setText(TimeUtils.formatDurationSince(now.minus(finishingTime.asMillis()).asMillis(), false));
+                break;
         }
+    }
 
-        RacingProcedure procedure = state.getTypedRacingProcedure();
-        LayerDrawable flag = null;
+    private void setProcedureFlag(TimePoint now) {
+        Drawable flag = null;
         Drawable arrow = null;
-        String duration = null;
-        if (!procedure.isIndividualRecallDisplayed()) {
-            FlagPoleState poleState = state.getRacingProcedure().getActiveFlags(state.getStartTime(), now);
-            List<FlagPole> currentState = poleState.getCurrentState();
-            List<FlagPole> upcoming = poleState.computeUpcomingChanges();
-            FlagPole nextPole = FlagPoleState.getMostInterestingFlagPole(upcoming);
-            TimePoint change = poleState.getNextStateValidFrom();
-            Flags currentFlag;
-
-            if (change != null) {
-                for (FlagPole pole : currentState) {
-                    int isNext = 0;
-
-                    currentFlag = pole.getUpperFlag();
-                    if (isNextFlag(currentFlag, nextPole)) {
-                        isNext = 1;
-                    } else {
-                        currentFlag = pole.getLowerFlag();
-                        if (!Flags.NONE.equals(currentFlag)) {
-                            if (isNextFlag(currentFlag, nextPole)) {
-                                isNext = 2;
-                            }
-                        }
-                    }
-
-                    if (isNext != 0) {
-                        flag = FlagsResources.getFlagDrawable(getContext(), currentFlag.name(), flagSize);
-                        switch (isNext) {
-                            case 1:
-                                if (nextPole.isDisplayed()) {
-                                    arrow = BitmapHelper.getAttrDrawable(getContext(), R.attr.arrow_up);
-                                } else {
-                                    arrow = BitmapHelper.getAttrDrawable(getContext(), R.attr.arrow_down);
-                                }
-                                break;
-
-                            case 2:
-                                arrow = BitmapHelper.getAttrDrawable(getContext(), R.attr.arrow_up);
-                                break;
-
-                            default:
-                                ExLog.i(getContext(), TAG, "unknown flag");
-                        }
-                        duration = TimeUtils.formatDuration(now, poleState.getNextStateValidFrom());
-                    }
-                }
-            } else if (state.getStatus() == RaceLogRaceStatus.FINISHING) {
-                if (!currentState.isEmpty()) {
-                    flag = FlagsResources.getFlagDrawable(getContext(), currentState.get(0).getUpperFlag().name(),
-                            flagSize);
-                }
-                arrow = null;
-                duration = TimeUtils.formatDurationSince(now.minus(state.getFinishingTime().asMillis()).asMillis(), false);
-            }
-        } else {
-            TimePoint flagDown = procedure.getIndividualRecallRemovalTime();
-            if (now.before(flagDown)) {
-                flag = FlagsResources.getFlagDrawable(getContext(), Flags.XRAY.name(), flagSize);
-                arrow = BitmapHelper.getAttrDrawable(getContext(), R.attr.arrow_down);
-                duration = TimeUtils.formatDuration(now, flagDown);
-            }
-        }
-        if (duration != null) {
-            duration = duration.replace("-", "");
-        }
-
-        if (flag == null) {
-            setVisibility(View.GONE);
+        final FlagPoleState poleState = procedure.getActiveFlags(startTime, now);
+        nextTime = poleState.getNextStateValidFrom();
+        if (nextTime == null) {
+            setVisibility(GONE);
+            clear();
             return;
         }
-
-        setVisibility(View.VISIBLE);
+        final List<FlagPole> poles = poleState.getCurrentState();
+        final FlagPole nextPole = FlagPoleState.getMostInterestingFlagPole(poleState.computeUpcomingChanges());
+        for (FlagPole pole : poles) {
+            final Flags upperFlag = pole.getUpperFlag();
+            if (isNextFlag(upperFlag, nextPole)) {
+                flag = FlagsResources.getFlagDrawable(getContext(), upperFlag.name(), flagSize);
+                arrow = BitmapHelper.getAttrDrawable(getContext(), nextPole.isDisplayed() ? R.attr.arrow_up : R.attr.arrow_down);
+            } else {
+                final Flags lowerFlag = pole.getLowerFlag();
+                if (lowerFlag != Flags.NONE) {
+                    if (isNextFlag(lowerFlag, nextPole)) {
+                        flag = FlagsResources.getFlagDrawable(getContext(), lowerFlag.name(), flagSize);
+                        arrow = BitmapHelper.getAttrDrawable(getContext(), R.attr.arrow_up);
+                    }
+                }
+            }
+        }
         imageView.setImageDrawable(flag);
         textView.setCompoundDrawablesWithIntrinsicBounds(arrow, null, null, null);
-        textView.setText(duration);
+    }
+
+    private void setFinishingFlag(TimePoint now) {
+        final FlagPoleState poleState = procedure.getActiveFlags(startTime, now);
+        final List<FlagPole> poles = poleState.getCurrentState();
+        if (poles.isEmpty()) {
+            clear();
+            return;
+        }
+        final Drawable flag = FlagsResources.getFlagDrawable(getContext(), poles.get(0).getUpperFlag().name(), flagSize);
+        imageView.setImageDrawable(flag);
+        textView.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+    }
+
+    private void clear() {
+        imageView.setImageDrawable(null);
+        textView.setCompoundDrawables(null, null, null, null);
+        textView.setText(null);
     }
 
     private boolean isNextFlag(Flags flag, FlagPole pole) {
