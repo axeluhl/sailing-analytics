fadb126787fce3149ec2f106f1a48055e03b9835
Alexandr Timoshenko <thick.tav@gmail.com>
2020-05-11 17:23:07 +0200

    limit integer prefs to numbers only

--- mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/views/IntegerEditTextPreference.java
+++ mobile/com.sap.sailing.racecommittee.app/src/com/sap/sailing/racecommittee/app/ui/views/IntegerEditTextPreference.java
@@ -4,18 +4,23 @@ import android.content.Context;
 import android.support.v7.preference.EditTextPreference;
 import android.util.AttributeSet;
 
+import com.sap.sailing.racecommittee.app.R;
+
 public class IntegerEditTextPreference extends EditTextPreference {
 
     public IntegerEditTextPreference(Context context) {
         super(context);
+        setDialogLayoutResource(R.layout.integer_preference_dialog);
     }
 
     public IntegerEditTextPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setDialogLayoutResource(R.layout.integer_preference_dialog);
     }
 
     public IntegerEditTextPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setDialogLayoutResource(R.layout.integer_preference_dialog);
     }
 
     @Override
