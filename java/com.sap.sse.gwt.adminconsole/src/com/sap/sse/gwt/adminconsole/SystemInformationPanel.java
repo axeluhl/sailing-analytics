8f3b7e59bb9b8c4e072df292dd101db95bf1cc56
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2020-05-26 18:10:01 +0200

    bug5177: removed pointer styling

--- java/com.sap.sse.gwt.adminconsole/src/com/sap/sse/gwt/adminconsole/SystemInformationPanel.java
+++ java/com.sap.sse.gwt.adminconsole/src/com/sap/sse/gwt/adminconsole/SystemInformationPanel.java
@@ -1,12 +1,10 @@
 package com.sap.sse.gwt.adminconsole;
 
-import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.Widget;
 import com.sap.sse.gwt.client.ErrorReporter;
 import com.sap.sse.gwt.client.ServerInfoDTO;
 
@@ -32,26 +30,20 @@ public class SystemInformationPanel extends FlowPanel {
         } else {
             buildVersionText.setText(stringMessages.version(stringMessages.unknown()));
         }
-        addFloatingWidget(buildVersionText);
-        if(buildVersionCut) {
+        add(buildVersionText);
+        if (buildVersionCut) {
             additionalInformation = new Anchor(stringMessages.additionalInformation());
             additionalInformation.addClickHandler(new ClickHandler() {
                 @Override
                 public void onClick(ClickEvent event) {
                     AdditionalInformationDialog additionalInformationDialog = new AdditionalInformationDialog(
-                            stringMessages.additionalInformation(), stringMessages.version(fullVersionText), stringMessages.ok(),
-                            stringMessages.cancel(), /* validator */ null, /* animationEnabled */ false,
-                            /* callback */null);
+                            stringMessages.additionalInformation(), stringMessages.version(fullVersionText),
+                            stringMessages.ok(), stringMessages.cancel(), /* validator */ null,
+                            /* animationEnabled */ false, /* callback */null);
                     additionalInformationDialog.show();
                 }
             });
-            addFloatingWidget(additionalInformation);
+            add(additionalInformation);
         }
-    }
-
-    private void addFloatingWidget(Widget w) {
-        w.getElement().getStyle().setFloat(Style.Float.LEFT);
-        w.getElement().getStyle().setCursor(Style.Cursor.POINTER);
-        add(w);
     }
 }
