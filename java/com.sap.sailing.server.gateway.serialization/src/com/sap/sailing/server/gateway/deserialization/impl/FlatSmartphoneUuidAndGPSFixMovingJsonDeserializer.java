373d8f0adf4f449a3cf5226754b7532b3c0a23a7
Georg Herdt <herdt@agima-it.de>
2020-06-02 19:30:55 +0200

    add comment regarding possible timezone issue bug5293

--- java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/impl/FlatSmartphoneUuidAndGPSFixMovingJsonDeserializer.java
+++ java/com.sap.sailing.server.gateway.serialization/src/com/sap/sailing/server/gateway/deserialization/impl/FlatSmartphoneUuidAndGPSFixMovingJsonDeserializer.java
@@ -55,7 +55,8 @@ public class FlatSmartphoneUuidAndGPSFixMovingJsonDeserializer implements
         } else if (fixObject.get(FlatSmartphoneUuidAndGPSFixMovingJsonSerializer.TIME_ISO) != null) {
             String strIsoTimestamp = fixObject.get(FlatSmartphoneUuidAndGPSFixMovingJsonSerializer.TIME_ISO)
                     .toString();
-            DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_DATE_TIME;
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_DATE_TIME;  
+            // There might be a timezone issue, please see unit test and compare TZ used in to string between millis and ISO result.
             OffsetDateTime offsetDateTime = OffsetDateTime.parse(strIsoTimestamp, timeFormatter);
             timeMillis = offsetDateTime.toInstant().toEpochMilli();
         } else {
