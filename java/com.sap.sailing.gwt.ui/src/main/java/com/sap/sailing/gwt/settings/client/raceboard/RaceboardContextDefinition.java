fc4cf151449faa528328d8a5f8ab7a524ba72435
Axel Uhl <axel.uhl@sap.com>
2020-06-26 19:30:53 +0200

    bug5287: yet a better fix for ensuring valid ID parameter suppresses name param

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/settings/client/raceboard/RaceboardContextDefinition.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/settings/client/raceboard/RaceboardContextDefinition.java
@@ -21,9 +21,15 @@ public class RaceboardContextDefinition extends AbstractGenericSerializableSetti
     public RaceboardContextDefinition() {
     }
 
+    /**
+     * If the {@code leaderboardGroupId} is provided as a non-{@code null} value, the {@code leaderboardGroupName}
+     * parameter is ignored and not stored. Otherwise, the {@code leaderboardGroupName} is remembered.
+     */
     public RaceboardContextDefinition(String regattaName, String raceName, String leaderboardName,
             String leaderboardGroupName, UUID leaderboardGroupId, UUID eventId, String mode) {
-        this(regattaName, raceName, leaderboardName, leaderboardGroupName, leaderboardGroupId, eventId, mode, null);
+        this(regattaName, raceName, leaderboardName,
+             // use the leaderboardGroupId if not null; only resort to leaderboardGroupName if ID is not provided
+             leaderboardGroupId == null ? leaderboardGroupName : null, leaderboardGroupId, eventId, mode, null);
     }
 
     public RaceboardContextDefinition(String regattaName, String raceName, String leaderboardName,
