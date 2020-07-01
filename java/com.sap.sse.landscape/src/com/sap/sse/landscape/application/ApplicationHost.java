e466a0f94c02cf1237f79d03ba4bc8ec190df857
Axel Uhl <axel.uhl@sap.com>
2020-07-01 11:41:36 +0200

    bug4811: more landscape modeling

--- java/com.sap.sse.landscape/src/com/sap/sse/landscape/application/ApplicationHost.java
+++ java/com.sap.sse.landscape/src/com/sap/sse/landscape/application/ApplicationHost.java
@@ -9,5 +9,10 @@ import com.sap.sse.landscape.Host;
  *
  */
 public interface ApplicationHost extends Host {
-
+    /**
+     * Launches an application on this host, using the configuration parameters requested. Throws an exception
+     * in case the configuration is incompatible with the host, for example, if it requests a port to be used
+     * that is already taken.
+     */
+    <ShardingKey, MetricsT extends ApplicationProcessMetrics> ApplicationProcess<ShardingKey, MetricsT> launchApplication(ApplicationProcessConfiguration<ShardingKey, MetricsT> configuration);
 }
