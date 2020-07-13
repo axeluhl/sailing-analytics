6990fdd7d64627265914715fb254d3cf81b162b4
Axel Uhl <axel.uhl@sap.com>
2020-07-13 10:59:44 +0200

    bug4811: added equals/hashCode/toString() to AwsRegion

--- java/com.sap.sse.landscape.aws/src/com/sap/sse/landscape/aws/impl/AwsRegion.java
+++ java/com.sap.sse.landscape.aws/src/com/sap/sse/landscape/aws/impl/AwsRegion.java
@@ -33,4 +33,33 @@ public class AwsRegion implements Region {
         return null;
     }
 
+    @Override
+    public String toString() {
+        return "AwsRegion [id=" + id + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AwsRegion other = (AwsRegion) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
