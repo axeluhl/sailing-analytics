b762aaa2016e542f5744dc018d7b2fd9dcbeb404
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2020-08-13 17:18:45 +0200

    bug5239: Added UserGroupUserPanelPO

---
+++ java/com.sap.sailing.selenium.test/src/com/sap/sailing/selenium/pages/adminconsole/usergroups/UserGroupUserPanelPO.java
@@ -0,0 +1,69 @@
+package com.sap.sailing.selenium.pages.adminconsole.usergroups;
+
+import org.openqa.selenium.ElementNotSelectableException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.sap.sailing.selenium.core.BySeleniumId;
+import com.sap.sailing.selenium.core.FindBy;
+import com.sap.sailing.selenium.pages.PageArea;
+import com.sap.sailing.selenium.pages.gwt.CellTablePO;
+import com.sap.sailing.selenium.pages.gwt.DataEntryPO;
+import com.sap.sailing.selenium.pages.gwt.GenericCellTablePO;
+import com.sap.sailing.selenium.pages.gwt.SuggestBoxPO;
+
+public class UserGroupUserPanelPO extends PageArea {
+    private static final String TABLE_PERMISSION_COLUMN = "Username";
+    @FindBy(how = BySeleniumId.class, using = "AddUserButton")
+    private WebElement addButton;
+    @FindBy(how = BySeleniumId.class, using = "UserSuggestion")
+    private WebElement permissionInput;
+    @FindBy(how = BySeleniumId.class, using = "UserGroupUserDTOTable")
+    private WebElement permissionTable;
+    
+    public UserGroupUserPanelPO(WebDriver driver, WebElement element) {
+        super(driver, element);
+    }
+
+    private CellTablePO<DataEntryPO> getUserTable() {
+        return new GenericCellTablePO<>(this.driver, this.permissionTable, DataEntryPO.class);
+    }
+
+    public DataEntryPO findPermission(final String permissionName) {
+        final CellTablePO<DataEntryPO> table = getUserTable();
+        for (DataEntryPO entry : table.getEntries()) {
+            final String name = entry.getColumnContent(TABLE_PERMISSION_COLUMN);
+            if (permissionName.equals(name)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+    
+    public void addUser(String name) {
+        enterNewUser(name);
+        clickAddButtonOrThrow();
+        waitUntil(() -> findPermission(name) != null);
+    }
+
+    public void enterNewUser(String name) {
+        SuggestBoxPO.create(driver, permissionInput).appendText(name);
+    }
+    
+    public void clickAddButtonOrThrow() {
+        if (!addButton.isEnabled()) {
+            throw new ElementNotSelectableException("Add Button was disabled");
+        } else {
+            addButton.click();
+        }
+    }
+    
+    public void clickAddButtonAndExpectPermissionError() {
+        if (!addButton.isEnabled()) {
+            throw new ElementNotSelectableException("Add Button was disabled");
+        } else {
+            addButton.click();
+        }
+        waitForAlertContainingMessageAndAccept("Not permitted to add user");
+    }
+}
