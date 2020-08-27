db07b41e70c360989f2a8c711734bb60fc0b4060
Tim Hessenmüller <tim.hessenmueller@sap.com>
2020-08-27 18:39:29 +0200

    bug5259: Removed debug code

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/async/TimeRangeResultCache.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/client/async/TimeRangeResultCache.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.sap.sse.common.TimeRange;
 import com.sap.sse.common.Util.Pair;
@@ -74,7 +73,6 @@ public class TimeRangeResultCache<SubResult> {
             this.callback = callback;
             this.action = action;
             addChildren(childrenList);
-            GWT.log("Request created: " + childrenSet.size() + " children, " + Math.round(trimmedTimeRange != null ? trimmedTimeRange.getDuration().divide(timeRange.getDuration()) * 100d : 0d) + "% length");
         }
 
         /**
