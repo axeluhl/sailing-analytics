ee60c50797f047d1f9d3cbfe1a13e60870d03ae2
Steffen Schaefer <steffen.schaefer@oio.de>
2020-09-14 16:03:28 +0200

    Bug 5239: Removed flags to deactivate negative ACLs

--- java/com.sap.sse.security/src/com/sap/sse/security/impl/SecurityServiceImpl.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/impl/SecurityServiceImpl.java
@@ -177,9 +177,6 @@ public class SecurityServiceImpl implements ReplicableSecurityService, ClearStat
     private static final Logger logger = Logger.getLogger(SecurityServiceImpl.class.getName());
 
     private static final String ADMIN_DEFAULT_PASSWORD = "admin";
-    
-    // TODO Bug 5239: remove, once we allow denied ACLs again
-    private static final boolean supportDeniedActions = true;
 
     private final Set<String> migratedHasPermissionTypes = new ConcurrentSkipListSet<>();
 
@@ -571,16 +568,6 @@ public class SecurityServiceImpl implements ReplicableSecurityService, ClearStat
     @Override
     public AccessControlList overrideAccessControlList(QualifiedObjectIdentifier idOfAccessControlledObject,
             Map<UserGroup, Set<String>> permissionMap, String displayNameOfAccessControlledObject) {
-        // TODO Bug 5239: remove, once we allow denied ACLs again
-        if (!supportDeniedActions) {
-            for (Set<String> actions : permissionMap.values()) {
-                for (String action : actions) {
-                    if (SecurityAccessControlList.isDeniedAction(action)) {
-                        throw new IllegalArgumentException("Adding denied actions to an ACL is not allowed");
-                    }
-                }
-            }
-        }
         setEmptyAccessControlList(idOfAccessControlledObject, displayNameOfAccessControlledObject);
         for (Map.Entry<UserGroup, Set<String>> entry : permissionMap.entrySet()) {
             final UserGroup userGroup = entry.getKey();
@@ -610,14 +597,9 @@ public class SecurityServiceImpl implements ReplicableSecurityService, ClearStat
     /*
      * @param name The name of the user group to add
      */
-    @SuppressWarnings("unused")
     @Override
     public AccessControlList addToAccessControlList(QualifiedObjectIdentifier idOfAccessControlledObject,
             UserGroup group, String action) {
-        // TODO Bug 5239: remove, once we allow denied ACLs again
-        if (!supportDeniedActions && SecurityAccessControlList.isDeniedAction(action)) {
-            throw new IllegalArgumentException("Adding denied actions to an ACL is not allowed");
-        }
         if (getAccessControlList(idOfAccessControlledObject) == null) {
             setEmptyAccessControlList(idOfAccessControlledObject);
         }
