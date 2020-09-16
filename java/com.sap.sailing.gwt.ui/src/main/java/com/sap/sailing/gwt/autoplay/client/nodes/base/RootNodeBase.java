dde00b2ec7708e8615563b107c0b1e038db79464
Axel Uhl <axel.uhl@sap.com>
2020-09-16 09:58:20 +0200

    fixed log message in AutoPlay node

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/autoplay/client/nodes/base/RootNodeBase.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/autoplay/client/nodes/base/RootNodeBase.java
@@ -114,7 +114,7 @@ public abstract class RootNodeBase extends BaseCompositeNode {
                                 log("Received different live race, hard switching to AFTER_LIVE race");
                                 setCurrentState(isPreLiveRace, loadedLiveRace, RootNodeState.AFTER_LIVE, currentState);
                             } else {
-                                log("New " + (isPreLiveRace ? "live " : "pre live") + " race found: " + loadedLiveRace
+                                log("New " + (isPreLiveRace ? "live" : "pre live") + " race found: " + loadedLiveRace
                                         + " starting in " + (timeToRaceStartInMs / 1000) + "s");
                                 setCurrentState(isPreLiveRace, loadedLiveRace,
                                         isPreLiveRace ? RootNodeState.PRE_RACE : RootNodeState.LIVE, currentState);
