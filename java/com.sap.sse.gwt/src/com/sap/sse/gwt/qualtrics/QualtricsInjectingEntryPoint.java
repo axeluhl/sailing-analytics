17e0c3f4320d9b3386e809de2793752c313b8a70
Axel Uhl <axel.uhl@sap.com>
2020-09-29 15:03:47 +0200

    qualtrics: don't explicitly trigger Intercepts after a timeout but let the Intercept config do that

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/qualtrics/QualtricsInjectingEntryPoint.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/qualtrics/QualtricsInjectingEntryPoint.java
@@ -2,20 +2,11 @@ package com.sap.sse.gwt.qualtrics;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Timer;
-import com.sap.sse.common.Duration;
 
 public class QualtricsInjectingEntryPoint implements EntryPoint {
     @Override
     public void onModuleLoad() {
         GWT.log("Inject Qualtrics");
         Qualtrics.ensureInjected();
-        // we'd like to trigger the "count-down" intercepts always when loading the page
-        new Timer() {
-            @Override
-            public void run() {
-                Qualtrics.triggerIntercepts();
-            }
-        }.schedule((int) Duration.ONE_MINUTE.asMillis());
     }
 }
