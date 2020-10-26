548124cf348d17f26b4d755dd98321f4ddfc6645
Udo Wessels <mail@udo-wessels.de>
2020-10-26 13:04:59 +0100

    bug5108: fix compile problem (missing default constructor in exception class)

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/racelog/tracking/MarkAlreadyUsedInRaceException.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/racelog/tracking/MarkAlreadyUsedInRaceException.java
@@ -3,8 +3,14 @@ package com.sap.sailing.domain.common.racelog.tracking;
 import java.io.Serializable;
 
 public class MarkAlreadyUsedInRaceException extends Exception implements Serializable {
-    private static final long serialVersionUID = 963948633131929332L;
     
+    private static final long serialVersionUID = 2040461905745135256L;
+    
+    public MarkAlreadyUsedInRaceException() {
+        super();
+        this.raceNames = "";
+    }
+
     public MarkAlreadyUsedInRaceException(String message, String raceNames) {
         super(message);
         this.raceNames = raceNames;
