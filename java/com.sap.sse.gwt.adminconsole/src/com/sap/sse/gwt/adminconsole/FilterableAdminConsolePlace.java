a81b47cd7c2e805b17cbdf5ed0c45ed533d7e43a
Axel Uhl <axel.uhl@sap.com>
2020-11-24 10:15:45 +0100

    bug5288: Javadoc formatting

--- java/com.sap.sse.gwt.adminconsole/src/com/sap/sse/gwt/adminconsole/FilterableAdminConsolePlace.java
+++ java/com.sap.sse.gwt.adminconsole/src/com/sap/sse/gwt/adminconsole/FilterableAdminConsolePlace.java
@@ -3,12 +3,12 @@ package com.sap.sse.gwt.adminconsole;
 import com.sap.sse.gwt.client.panels.FilterParameter;
 
 /**
- * Places that have filterable tables and should be filterable by URL parameters must implement this interface.
- * The panel that is rendered when the place is called must implement FilterablePanelProvider.
- * Otherwise the filter-parameters will be ignored when the place is called.
+ * Places that have filterable tables and should be filterable by URL parameters must implement this interface. The
+ * panel that is rendered when the place is called must implement FilterablePanelProvider. Otherwise the
+ * filter-parameters will be ignored when the place is called.
+ * 
  * @see com.sap.sse.gwt.adminconsole.FilterablePanelProvider
  */
 public interface FilterableAdminConsolePlace {
-    
-    public FilterParameter getFilterParameter();
+    FilterParameter getFilterParameter();
 }
