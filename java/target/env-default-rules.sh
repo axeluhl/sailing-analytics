10536df42d004426a58cb92ddbd8358b61238d8a
Axel Uhl <axel.uhl@sap.com>
2020-11-25 20:26:33 +0100

    bug4811: added missing spaces between closing double-quote and closing bracket in env-default-rules.sh

--- java/target/env-default-rules.sh
+++ java/target/env-default-rules.sh
@@ -7,22 +7,22 @@
 # the optional environment appended from http://releases.sapsailing.com/environments,
 # and the optional user data from the EC2 environment, default values may be computed
 # which may use other variable values that *have* been set.
-if [ -z "${SERVER_NAME}"]; then
+if [ -z "${SERVER_NAME}" ]; then
   SERVER_NAME=MASTER
 fi
 
 # Message Queue hostname where to
 # send messages for replicas (this server is master)
-if [ -z "${REPLICATION_HOST}"]; then
+if [ -z "${REPLICATION_HOST}" ]; then
   REPLICATION_HOST=rabbit.internal.sapsailing.com
 fi
 # For the port, use 0 for the RabbitMQ default or a specific port that your RabbitMQ server is listening on
-if [ -z "${REPLICATION_PORT}"]; then
+if [ -z "${REPLICATION_PORT}" ]; then
   REPLICATION_PORT=0
 fi
 # The name of the message queuing fan-out exchange that this server will use in its role as replication master.
 # Make sure this is unique so that no other master is writing to this exchange at any time.
-if [ -z "${REPLICATION_CHANNEL}"]; then
+if [ -z "${REPLICATION_CHANNEL}" ]; then
   if [ -n "$AUTO_REPLICATE" ]; then
     # This seems to be a replica; use a dedicated outbound channel for "transitive replication"
     REPLICATION_CHANNEL=${SERVER_NAME}-${INSTANCE_NAME}
@@ -33,48 +33,48 @@ if [ -z "${REPLICATION_CHANNEL}"]; then
   fi
 fi
 
-if [ -z "${TELNET_PORT}"]; then
+if [ -z "${TELNET_PORT}" ]; then
   TELNET_PORT=14888
 fi
-if [ -z "${SERVER_PORT}"]; then
+if [ -z "${SERVER_PORT}" ]; then
   SERVER_PORT=8888
 fi
 if [ -z "${MONGODB_NAME}" ]; then
   MONGODB_NAME=${SERVER_NAME}
 fi
-if [ -z "${MONGODB_PORT}"]; then
+if [ -z "${MONGODB_PORT}" ]; then
   MONGODB_PORT=27017
 fi
 if [ -z "${MONGODB_HOST}" -a -z "${MONGODB_URI}" ]; then
   MONGODB_URI="mongodb://mongo0.internal.sapsailing.com,mongo1.internal.sapsailing.com/${MONGODB_NAME}?replicaSet=live&retryWrites=true&readPreference=nearest"
 fi
-if [ -z "${EXPEDITION_PORT}"]; then
+if [ -z "${EXPEDITION_PORT}" ]; then
   EXPEDITION_PORT=2010
 fi
-if [ -z "${REPLICATE_MASTER_SERVLET_HOST}"]; then
+if [ -z "${REPLICATE_MASTER_SERVLET_HOST}" ]; then
   REPLICATE_MASTER_SERVLET_HOST=${SERVER_NAME}.sapsailing.com
 fi
-if [ -z "${REPLICATE_MASTER_SERVLET_PORT}"]; then
+if [ -z "${REPLICATE_MASTER_SERVLET_PORT}" ]; then
   REPLICATE_MASTER_SERVLET_PORT=443
 fi
 # Host where RabbitMQ is running 
-if [ -z "${REPLICATE_MASTER_QUEUE_HOST}"]; then
+if [ -z "${REPLICATE_MASTER_QUEUE_HOST}" ]; then
   REPLICATE_MASTER_QUEUE_HOST=rabbit.internal.sapsailing.com
 fi
 # Port that RabbitMQ is listening on (normally something like 5672); use 0 to connect to RabbitMQ's default port
-if [ -z "${REPLICATE_MASTER_QUEUE_PORT}"]; then
+if [ -z "${REPLICATE_MASTER_QUEUE_PORT}" ]; then
   REPLICATE_MASTER_QUEUE_PORT=0
 fi
 # Exchange name that the master from which to auto-replicate is using as
 # its REPLICATION_CHANNEL variable, mapping to the master's replication.exchangeName
 # system property.
 #
-if [ -z "${REPLICATE_MASTER_EXCHANGE_NAME}"]; then
+if [ -z "${REPLICATE_MASTER_EXCHANGE_NAME}" ]; then
   REPLICATE_MASTER_EXCHANGE_NAME=${SERVER_NAME}
 fi
-if [-z "${BUILD_COMPLETE_NOTIFY}"]; then
+if [-z "${BUILD_COMPLETE_NOTIFY}" ]; then
   export BUILD_COMPLETE_NOTIFY=simon.marcel.pamies@sap.com
 fi
-if [ -z "${SERVER_STARTUP_NOTIFY}"]; then
+if [ -z "${SERVER_STARTUP_NOTIFY}" ]; then
   export SERVER_STARTUP_NOTIFY=simon.marcel.pamies@sap.com
 fi
