0d91636f5424c8ec52f61f5b74b635c16b67673b
Simon Pamies <s.pamies@appweeve.com>
2020-11-27 08:40:50 +0100

    Refactored configuration into interface

--- java/com.sap.sailing.ingestion/src/main/java/com/sap/sailing/ingestion/Configuration.java
+++ java/com.sap.sailing.ingestion/src/main/java/com/sap/sailing/ingestion/Configuration.java
@@ -2,14 +2,14 @@ package com.sap.sailing.ingestion;
 
 import software.amazon.awssdk.regions.Region;
 
-public class Configuration {
+public interface Configuration {
     /**
      * This host is only reachable through VPC and therefore one needs to ensure that the Lambda has all relevant
      * permissions
      */
-    protected static final String MEMCACHED_ENDPOINT_HOST = "fixingestion.cvoblp.cfg.euw2.cache.amazonaws.com";
-    protected static final int MEMCACHED_ENDPOINT_PORT = 11211;
+    String MEMCACHED_ENDPOINT_HOST = "fixingestion.cvoblp.cfg.euw2.cache.amazonaws.com";
+    int MEMCACHED_ENDPOINT_PORT = 11211;
 
-    protected static final Region S3_REGION = Region.EU_WEST_2;
-    protected static final String S3_BUCKET_NAME = "sapsailing-gps-fixes";
+    Region S3_REGION = Region.EU_WEST_2;
+    String S3_BUCKET_NAME = "sapsailing-gps-fixes";
 }
