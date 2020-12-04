1cec6af2cd28d1dbf00ec848b4128800f64f600c
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2020-12-04 17:06:22 +0100

    bug5201: Extracting common YachtScoringProvider logic to abstract class

---
+++ java/com.sap.sailing.yachtscoring.resultimport/src/com/sap/sailing/yachtscoring/resultimport/AbstractYachtScoringProvider.java
@@ -0,0 +1,44 @@
+package com.sap.sailing.yachtscoring.resultimport;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.sap.sailing.domain.resultimport.ResultUrlProvider;
+import com.sap.sailing.resultimport.AbstractResultUrlProvider;
+import com.sap.sailing.resultimport.ResultUrlRegistry;
+import com.sap.sailing.xrr.resultimport.ParserFactory;
+
+public abstract class AbstractYachtScoringProvider extends AbstractResultUrlProvider implements ResultUrlProvider {
+    private static final long serialVersionUID = 629585353618956893L;
+    
+    public static final String NAME = "YachtScoring XRR Result Importer";
+    public static final String EVENT_ID_REGEX = "^\\d+$";
+    public static final String EVENT_ID_TEMPLATE = "http://www.yachtscoring.com/results_xrr_auto.cfm?eid=%s";
+    protected final ParserFactory parserFactory;
+    
+    protected AbstractYachtScoringProvider(ParserFactory parserFactory, ResultUrlRegistry resultUrlRegistry) {
+        super(resultUrlRegistry);
+        this.parserFactory = parserFactory;
+    }
+    
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public URL resolveUrl(String url) throws MalformedURLException {
+        String completedUrl = url;
+        if (url.matches(EVENT_ID_REGEX)) {
+            completedUrl = String.format(EVENT_ID_TEMPLATE, url);
+        }
+        return new URL(completedUrl); //TODO Find a better way to check if a URL is valid
+    }
+
+    @Override
+    public String getOptionalSampleURL() {
+        return "http://www.yachtscoring.com/results_xrr_auto.cfm?eid=1220";
+    }
+
+}
