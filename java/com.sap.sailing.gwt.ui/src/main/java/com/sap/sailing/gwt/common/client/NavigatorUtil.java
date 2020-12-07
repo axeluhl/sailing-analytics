b541a77afdddc36b0359c9fc2aab456124fe05aa
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2020-12-07 14:59:32 +0100

    bug5190: New utility class to support native "navigator" browser functionalities.

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/common/client/NavigatorUtil.java
@@ -0,0 +1,63 @@
+package com.sap.sailing.gwt.common.client;
+
+import com.google.gwt.core.client.GWT;
+
+public class NavigatorUtil {
+    /**
+     * Note that this method will check whether the browser does support the copyToClipboard feature first 
+     * and will do nothing if it does not to prevent JS error messages in the console.
+     * @param String text: Text to share.
+     */
+    public static void copyToClipboard(String text) {
+        if(nativeClientHasNavigatorCopyToClipboardSupport()) {
+            nativeCopyToClipboard(text);
+        }else {
+            GWT.log("This browser does not support copying to clipboard");
+        }
+    }
+    
+    /**
+     * Note that this method will check whether the browser does support the sharing feature first 
+     * and will do nothing if it does not to prevent JS error messages in the console.
+     * @param String url: URL to share.
+     * @param String text: Text to share.
+     */
+    public static void shareUrlAndText(String url, String text) {
+        if(nativeClientHasNavigatorShareSupport()) {
+            nativeShare(url, text);
+        }else {
+            GWT.log("This browser does not support native sharing");
+        }
+    }
+    
+    private static native void nativeCopyToClipboard(String text) /*-{
+        window.focus();
+        navigator.clipboard.writeText(text);
+    }-*/;
+
+    private static native void nativeShare(String url, String text) /*-{
+        window.focus();
+        navigator.share({
+            url : url,
+            text : text
+        });
+    }-*/;
+
+    private static native boolean nativeClientHasNavigatorShareSupport() /*-{
+        window.focus();
+        if (navigator.share) {
+            return true;
+        } else {
+            return false;
+        }
+    }-*/;
+
+    private static native boolean nativeClientHasNavigatorCopyToClipboardSupport() /*-{
+        window.focus();
+        if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+            return true;
+        } else {
+            return false;
+        }
+    }-*/;
+}
