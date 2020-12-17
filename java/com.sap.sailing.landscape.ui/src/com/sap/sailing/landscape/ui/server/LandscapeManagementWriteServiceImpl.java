1f070295de9b23d3e5af733324cf1cbcde24eb83
Axel Uhl <axel.uhl@sap.com>
2020-12-17 22:15:59 +0100

    bug4811: first prototypical access to AwsLandscape instance from LandscapeManagementWriteServiceImpl

--- java/com.sap.sailing.landscape.ui/src/com/sap/sailing/landscape/ui/server/LandscapeManagementWriteServiceImpl.java
+++ java/com.sap.sailing.landscape.ui/src/com/sap/sailing/landscape/ui/server/LandscapeManagementWriteServiceImpl.java
@@ -3,15 +3,23 @@ package com.sap.sailing.landscape.ui.server;
 import java.util.ArrayList;
 
 import com.sap.sailing.landscape.ui.client.LandscapeManagementWriteService;
+import com.sap.sse.common.Util;
 import com.sap.sse.gwt.server.ResultCachingProxiedRemoteServiceServlet;
+import com.sap.sse.landscape.aws.AwsLandscape;
 
 public class LandscapeManagementWriteServiceImpl extends ResultCachingProxiedRemoteServiceServlet
         implements LandscapeManagementWriteService {
     private static final long serialVersionUID = -3332717645383784425L;
+    private final AwsLandscape<?, ?, ?> landscape;
 
+    public LandscapeManagementWriteServiceImpl() {
+        landscape = AwsLandscape.obtain();
+    }
+    
     @Override
     public ArrayList<String> getRegions() {
-        // TODO Implement LandscapeManagementWriteService.getRegions(...)
-        return null;
+        final ArrayList<String> result = new ArrayList<>();
+        Util.addAll(Util.map(landscape.getRegions(), r->r.getId()), result);
+        return result;
     }
 }
