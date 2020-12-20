df6b482c1ec9867ec3be083f5573a9c6c9551b25
Tim Hessenmüller <tim.hessenmueller@sap.com>
2020-12-20 18:22:11 +0100

    Added BearingWithConfidenceDTO

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/actions/GetManeuverAngleAction.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/actions/GetManeuverAngleAction.java
@@ -1,8 +1,8 @@
 package com.sap.sailing.gwt.ui.actions;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.sap.sailing.domain.base.BoatClass;
 import com.sap.sailing.domain.common.ManeuverType;
+import com.sap.sailing.domain.common.dto.BoatClassDTO;
 import com.sap.sailing.gwt.ui.client.SailingServiceAsync;
 import com.sap.sailing.gwt.ui.shared.BearingWithConfidenceDTO;
 import com.sap.sse.common.Speed;
@@ -10,11 +10,11 @@ import com.sap.sse.gwt.client.async.AsyncAction;
 
 public class GetManeuverAngleAction implements AsyncAction<BearingWithConfidenceDTO> {
     private final SailingServiceAsync sailingService;
-    private final BoatClass boatClass;
+    private final BoatClassDTO boatClass;
     private final ManeuverType maneuverType;
     private final Speed windSpeed;
 
-    public GetManeuverAngleAction(SailingServiceAsync sailingService, BoatClass boatClass, ManeuverType maneuverType,
+    public GetManeuverAngleAction(SailingServiceAsync sailingService, BoatClassDTO boatClass, ManeuverType maneuverType,
             Speed windSpeed) {
         this.sailingService = sailingService;
         this.boatClass = boatClass;
@@ -24,6 +24,6 @@ public class GetManeuverAngleAction implements AsyncAction<BearingWithConfidence
 
     @Override
     public void execute(AsyncCallback<BearingWithConfidenceDTO> callback) {
-        //sailingService.getManeuverAngle(boatClass, maneuverType, windSpeed, callback);
+        sailingService.getManeuverAngle(boatClass, maneuverType, windSpeed, callback);
     }
 }
