7041fcf27b1a2658970dd9eb25c6ca513f19b210
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2021-01-12 16:33:11 +0100

    bug5201: Implemented YachtScoringCompetitorProvider

--- java/com.sap.sailing.yachtscoring.resultimport/src/com/sap/sailing/yachtscoring/resultimport/YachtScoringCompetitorProvider.java
+++ java/com.sap.sailing.yachtscoring.resultimport/src/com/sap/sailing/yachtscoring/resultimport/YachtScoringCompetitorProvider.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -205,6 +204,6 @@ public class YachtScoringCompetitorProvider extends AbstractYachtScoringProvider
 
     @Override
     public String getHint(Locale locale) {
-        return stringMessages.get(locale, "CompetitorImporterHint");
+      return stringMessages.get(locale, "CompetitorImporterHint");
     }
 }
