c3f202ff6d50be8e0cfc60f850123ba1e47a7702
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2021-01-13 13:22:25 +0100

    bug5201: Removing connection test, avoiding security workarounds

--- java/com.sap.sailing.yachtscoring.resultimport.test/src/com/sap/sailing/yachtscoring/resultimport/test/CompetitorImportTest.java
+++ java/com.sap.sailing.yachtscoring.resultimport.test/src/com/sap/sailing/yachtscoring/resultimport/test/CompetitorImportTest.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.when;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
 import java.util.Arrays;
 
 import javax.xml.bind.JAXBException;
@@ -16,20 +15,13 @@ import javax.xml.bind.JAXBException;
 import org.junit.Test;
 
 import com.sap.sailing.domain.common.CompetitorDescriptor;
-import com.sap.sailing.domain.persistence.DomainObjectFactory;
-import com.sap.sailing.domain.persistence.MongoObjectFactory;
 import com.sap.sailing.resultimport.ResultUrlRegistry;
-import com.sap.sailing.resultimport.impl.ResultUrlRegistryImpl;
-import com.sap.sailing.server.impl.RacingEventServiceImpl;
-import com.sap.sailing.server.interfaces.RacingEventService;
 import com.sap.sailing.xrr.resultimport.ParserFactory;
 import com.sap.sailing.yachtscoring.resultimport.AbstractYachtScoringProvider;
 import com.sap.sailing.yachtscoring.resultimport.YachtScoringCompetitorProvider;
 import com.sap.sse.common.Util;
 
-@SuppressWarnings("restriction")
 public class CompetitorImportTest extends AbstractCharlstonRaceWeek2015Test {
-    private static final String URL_EVENT= "J Fest 2020, St. Petersburg, FL, USA";
     
     @Test
     public void simpleCompetitorImportTest() throws FileNotFoundException, IOException, JAXBException {
@@ -45,25 +37,5 @@ public class CompetitorImportTest extends AbstractCharlstonRaceWeek2015Test {
         assertNotNull(competitorDescriptorsJ111);
         assertTrue(Util.size(competitorDescriptors) > Util.size(competitorDescriptorsJ111));
         assertEquals(353, Util.size(competitorDescriptorsJ111));
-    }
-    
-    @Test
-    public void simpleCompetitorImportConnectionTest() throws FileNotFoundException, IOException, JAXBException {
-        final RacingEventService racingEventService = new RacingEventServiceImpl();
-        final DomainObjectFactory domainObjectFactory = racingEventService.getDomainObjectFactory();
-        final MongoObjectFactory mongoObjectFactory = racingEventService.getMongoObjectFactory();
-        ResultUrlRegistry resultUrlRegistry = new ResultUrlRegistryImpl(mongoObjectFactory, domainObjectFactory);
-        URL url = new URL("https://www.yachtscoring.com/results_xrr_auto.cfm?eid=9649");
-        resultUrlRegistry.registerResultUrl(AbstractYachtScoringProvider.NAME, url);
-        final YachtScoringCompetitorProvider competitorProvider = new YachtScoringCompetitorProvider(ParserFactory.INSTANCE, resultUrlRegistry);
-        assertTrue(competitorProvider.getHasCompetitorsForRegattasInEvent().containsKey(URL_EVENT));
-        assertTrue(competitorProvider.getHasCompetitorsForRegattasInEvent().get(URL_EVENT).contains(BOAT_CLASS_J111));
-        final Iterable<CompetitorDescriptor> competitorDescriptors = competitorProvider.getCompetitorDescriptors(URL_EVENT, null); // get competitors for all regattas in event
-        assertNotNull(competitorDescriptors);
-//        assertEquals(62, Util.size(competitorDescriptors));
-        final Iterable<CompetitorDescriptor> competitorDescriptorsJ111 = competitorProvider.getCompetitorDescriptors(URL_EVENT, BOAT_CLASS_J111); // get competitors only for J111 regatta
-        assertNotNull(competitorDescriptorsJ111);
-        assertTrue(Util.size(competitorDescriptors) >Util.size(competitorDescriptorsJ111));
-//        assertEquals(134, Util.size(competitorDescriptorsJ111));
     }
 }
