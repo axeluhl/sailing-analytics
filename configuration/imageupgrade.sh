33d8f25e5a44d507ea4ecc76290f90bed91ab556
EC2 Server <axel.uhl@sap.com>
2021-02-04 16:23:24 +0000

    bug4811: added updating root's crontab to the imageupgrade.sh script

--- configuration/imageupgrade.sh
+++ configuration/imageupgrade.sh
@@ -44,6 +44,13 @@ clean_startup_logs() {
   rm "${REBOOT_INDICATOR}"
 }
 
+update_root_crontab() {
+  # The following assumes that /root/crontab is a symbolic link to /home/sailing/code/configuration/crontab
+  # which has previously been updated by a git pull:
+  cd /root
+  crontab crontab
+}
+
 clean_root_ssh_dir() {
   echo "Cleaning up /root/.ssh" >>/var/log/sailing.err
   rm -rf /root/.ssh/*
@@ -56,6 +63,7 @@ clean_logrotate_target
 clean_httpd_logs
 clean_sailing_logs
 clean_startup_logs
+update_root_crontab
 
 # Finally, shut down the node unless "no-shutdown" was provided in the user data, so that a new AMI can be constructed cleanly
 if /opt/aws/bin/ec2-metadata -d | grep "^no-shutdown$"; then
