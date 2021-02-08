8031b7b12ea8f69ca9891aa0c3af81217b6ddea8
Benjamin Barth <benjamin.barth@trivadis.com>
2021-02-08 16:04:33 +0100

    bug5429: Adjusted event overview card's navigation targets

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/managementconsole/places/event/overview/partials/EventCard.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/managementconsole/places/event/overview/partials/EventCard.java
@@ -1,5 +1,8 @@
 package com.sap.sailing.gwt.managementconsole.places.event.overview.partials;
 
+import static com.sap.sailing.gwt.ui.common.client.DateAndTimeFormatterUtil.formatDateRange;
+import static java.util.Optional.ofNullable;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -7,13 +10,14 @@ import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.managementconsole.places.event.overview.EventOverviewResources;
 import com.sap.sailing.gwt.managementconsole.places.event.overview.EventOverviewView.Presenter;
-import com.sap.sailing.gwt.ui.common.client.DateAndTimeFormatterUtil;
 import com.sap.sailing.gwt.ui.shared.EventDTO;
+import com.sap.sse.common.Named;
 import com.sap.sse.gwt.client.media.ImageDTO;
 
 public class EventCard extends Composite {
@@ -27,50 +31,32 @@ public class EventCard extends Composite {
     EventOverviewResources local_res;
 
     @UiField
-    Element card, title, details;
-
-    @UiField
-    Anchor advancedSettingsEventAnchor;
-
-    private final EventDTO event;
-    private final Presenter presenter;
+    Element card, container, title, details;
 
     public EventCard(final EventDTO event, final Presenter presenter) {
         initWidget(uiBinder.createAndBindUi(this));
-        this.event = event;
-        this.presenter = presenter;
         local_res.style().ensureInjected();
 
-        final String title = event.getName();
-        String venue = "-";
-        if (event.venue != null) {
-            venue = event.venue.getName();
-        }
-        String time = "-";
-        if (event.startDate != null && event.endDate != null) {
-            time = DateAndTimeFormatterUtil.formatDateRange(event.startDate, event.endDate);
-        } else if (event.startDate != null) {
-            time = DateAndTimeFormatterUtil.formatDateAndTime(event.startDate);
-        }
-        String imageUrl = null;
-        final ImageDTO teaserImage = event.getTeaserImage();
+        this.title.setInnerSafeHtml(SafeHtmlUtils.fromString(event.getName()));
 
-        if (teaserImage != null) {
-            imageUrl = teaserImage.getSourceRef();
-        }
-
-        this.title.setInnerSafeHtml(SafeHtmlUtils.fromString(title));
+        final String venue = ofNullable(event.venue).map(Named::getName).orElse("-");
+        final String time = formatDateRange(event.startDate, event.endDate);
         this.details.setInnerSafeHtml(SafeHtmlUtils.fromString(venue + ", " + time));
 
-        if (imageUrl != null) {
+        ofNullable(event.getTeaserImage()).map(ImageDTO::getSourceRef).ifPresent(imageUrl -> {
             this.card.getStyle().setBackgroundImage("url(' " + imageUrl + "')");
             this.card.addClassName(local_res.style().customTeaser());
-        }
+        });
+
+        Event.sinkEvents(card, Event.ONCLICK);
+        Event.setEventListener(card, e -> presenter.navigateToEvent(event));
     }
 
-    @UiHandler("advancedSettingsEventAnchor")
+    @UiHandler("contextMenu")
     void onSettingsIconClick(final ClickEvent event) {
-        presenter.navigateToEvent(this.event);
+        Window.alert("TODO: Open context menu ...");
+        event.stopPropagation();
+        event.preventDefault();
     }
 
 }
