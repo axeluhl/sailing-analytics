b91ac3a2e61c062aa98fe74798de71b08db1ea0a
root <root@ip-172-31-22-210.eu-west-2.compute.internal>
2021-03-02 16:16:47 +0000

    bug4811: fixed git pulling for MongoDB image

--- configuration/mongo_instance_setup/imageupgrade.sh
+++ configuration/mongo_instance_setup/imageupgrade.sh
@@ -5,8 +5,13 @@
 
 . imageupgrade_functions.sh
 
+run_git_pull_root() {
+  echo "Pulling git to /root/code" >>/var/log/sailing.err
+  "cd /root/code; git pull"
+}
+
 run_yum_update
-run_git_pull
+run_git_pull_root
 clean_startup_logs
 update_root_crontab
 finalize
