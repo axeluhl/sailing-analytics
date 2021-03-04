053c9a5fcd12578329bc70729595ef74499f5590
Axel Uhl <axel.uhl@sap.com>
2021-03-04 12:14:11 +0100

    bug5230: reverting serialVersionUIDs of role operations, making transitive a Boolean and adding readResolve() for backward compatibility

--- java/com.sap.sse.security/src/com/sap/sse/security/operations/AddRoleForUserOperation.java
+++ java/com.sap.sse.security/src/com/sap/sse/security/operations/AddRoleForUserOperation.java
@@ -1,16 +1,17 @@
 package com.sap.sse.security.operations;
 
+import java.io.ObjectStreamException;
 import java.util.UUID;
 
 import com.sap.sse.security.impl.ReplicableSecurityService;
 
 public class AddRoleForUserOperation implements SecurityOperation<Void> {
-    private static final long serialVersionUID = -391366213750626236L;
+    private static final long serialVersionUID = 3440561732671958186L;
     protected final String username;
     protected final UUID roleDefinitionId;
     protected final UUID idOfTenantQualifyingRole;
     protected final String nameOfUserQualifyingRole;
-    protected final boolean transitive;
+    protected final Boolean transitive;
 
     public AddRoleForUserOperation(String username, UUID roleDefinitionId, UUID idOfTenantQualifyingRole,
             String nameOfUserQualifyingRole, boolean transitive) {
@@ -21,6 +22,20 @@ public class AddRoleForUserOperation implements SecurityOperation<Void> {
         this.transitive = transitive;
     }
 
+    /**
+     * If {@link #transitive} is {@code null} on this instance, this method replaces this de-serialized object with one
+     * that has {@link #transitive} set to the default of {@code true}.
+     */
+    private Object readResolve() throws ObjectStreamException {
+        final AddRoleForUserOperation result;
+        if (this.transitive == null) {
+            result = new AddRoleForUserOperation(username, roleDefinitionId, idOfTenantQualifyingRole, nameOfUserQualifyingRole, /* transitive */ true);
+        } else {
+            result = this;
+        }
+        return result;
+    }
+    
     @Override
     public Void internalApplyTo(ReplicableSecurityService toState) throws Exception {
         toState.internalAddRoleForUser(username, roleDefinitionId, idOfTenantQualifyingRole, nameOfUserQualifyingRole, transitive);
