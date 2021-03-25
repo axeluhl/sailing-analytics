92af5bb86cd43fe77ca9b10959280572e49474c0
Tim Hessenmüller <tim.hessenmueller@sap.com>
2021-03-25 17:06:11 +0100

    bug5389: Added finish time columns to RegattaOverview

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/regattaoverview/client/RegattaRaceStatesComponent.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/regattaoverview/client/RegattaRaceStatesComponent.java
@@ -112,6 +112,8 @@ public class RegattaRaceStatesComponent extends AbstractCompositeComponent<Regat
     private Column<RegattaOverviewEntryDTO, SafeHtml> raceCourseColumn;
     private TextColumn<RegattaOverviewEntryDTO> boatClass;
     private TextColumn<RegattaOverviewEntryDTO> startTimeColumn;
+    private TextColumn<RegattaOverviewEntryDTO> finishingTimeColumn;
+    private TextColumn<RegattaOverviewEntryDTO> finishedTimeColumn;
     private SimplePanel tableHolder = new SimplePanel();
     private final long _1_HOUR = 60 /* seconds */* 60 /* minutes */* 1 /* hour */;
     private final long HIDE_COL_TIME_THRESHOLD = _1_HOUR;
@@ -182,11 +184,16 @@ public class RegattaRaceStatesComponent extends AbstractCompositeComponent<Regat
         boolean canRemoveBoatClass = true;
         boolean canRemoveLastUpdate = true;
         boolean canRemoveProtestTime = true;
+        boolean canRemoveFinishingTime = true;
         boolean first = true;
         for (RegattaOverviewEntryDTO loopEntryDTO : allEntries) {
             final RaceInfoDTO loopRaceInfo = loopEntryDTO.raceInfo;
             if (canRemoveLastUpdate && timePassedInSeconds(loopRaceInfo.lastUpdateTime) <= HIDE_COL_TIME_THRESHOLD) {
                 canRemoveLastUpdate = false;
+            }
+            if (canRemoveFinishingTime && loopRaceInfo.finishingTime != null
+                    && timePassedInSeconds(loopRaceInfo.finishingTime) <= HIDE_COL_TIME_THRESHOLD) {
+                canRemoveFinishingTime = false;
             }
             if (canRemoveProtestTime && loopRaceInfo.protestFinishTime != null
                     && timePassedInSeconds(loopRaceInfo.protestFinishTime) <= HIDE_COL_TIME_THRESHOLD) {
@@ -292,6 +299,10 @@ public class RegattaRaceStatesComponent extends AbstractCompositeComponent<Regat
             table.addColumn(lastUpdateColumn, stringMessages.lastUpdate());
         }
         table.addColumn(startTimeColumn, stringMessages.startTime());
+        if (!canRemoveFinishingTime) {
+            table.addColumn(finishingTimeColumn, stringMessages.finishingTime());
+        }
+        table.addColumn(finishedTimeColumn, stringMessages.finishTimeString());
         if (!canRemoveProtestTime) {
             table.addColumn(endOfProtestTime, stringMessages.protestTime());
         }
@@ -505,6 +516,52 @@ public class RegattaRaceStatesComponent extends AbstractCompositeComponent<Regat
                     result = -1;
                 } else {
                     result = 0;
+                }
+                return result;
+            }
+        });
+        finishingTimeColumn = new TextColumn<RegattaOverviewEntryDTO>() {
+            @Override
+            public String getValue(RegattaOverviewEntryDTO entryDTO) {
+                return formatTime(entryDTO.raceInfo.finishingTime);
+            }
+        };
+        finishingTimeColumn.setSortable(true);
+        regattaOverviewListHandler.setComparator(finishingTimeColumn, new Comparator<RegattaOverviewEntryDTO>() {
+            @Override
+            public int compare(RegattaOverviewEntryDTO left, RegattaOverviewEntryDTO right) {
+                int result = 0;
+                if (left.raceInfo.finishingTime != null && right.raceInfo.finishingTime != null) {
+                    result = left.raceInfo.finishingTime.compareTo(right.raceInfo.finishingTime);
+                } else if (left.raceInfo.finishingTime == null && right.raceInfo.finishingTime == null) {
+                    result = 0;
+                } else if (left.raceInfo.finishingTime == null) {
+                    result = 1;
+                } else if (right.raceInfo.finishingTime == null) {
+                    result = -1;
+                }
+                return result;
+            }
+        });
+        finishedTimeColumn = new TextColumn<RegattaOverviewEntryDTO>() {
+            @Override
+            public String getValue(RegattaOverviewEntryDTO entryDTO) {
+                return formatTime(entryDTO.raceInfo.finishedTime);
+            }
+        };
+        finishedTimeColumn.setSortable(true);
+        regattaOverviewListHandler.setComparator(finishedTimeColumn, new Comparator<RegattaOverviewEntryDTO>() {
+            @Override
+            public int compare(RegattaOverviewEntryDTO left, RegattaOverviewEntryDTO right) {
+                int result = 0;
+                if (left.raceInfo.finishedTime != null && right.raceInfo.finishedTime != null) {
+                    result = left.raceInfo.finishedTime.compareTo(right.raceInfo.finishedTime);
+                } else if (left.raceInfo.finishedTime == null && right.raceInfo.finishedTime == null) {
+                    result = 0;
+                } else if (left.raceInfo.finishedTime == null) {
+                    result = 1;
+                } else if (right.raceInfo.finishedTime == null) {
+                    result = -1;
                 }
                 return result;
             }
