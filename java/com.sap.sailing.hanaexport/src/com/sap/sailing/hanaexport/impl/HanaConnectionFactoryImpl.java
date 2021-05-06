ab7d4b288b42fd7a8494f1027a8dc0ebe0f01497
Axel Uhl <axel.uhl@sap.com>
2021-05-06 18:22:56 +0200

    bug5451: introduced system properties for HANA DB parameters

--- java/com.sap.sailing.hanaexport/src/com/sap/sailing/hanaexport/impl/HanaConnectionFactoryImpl.java
+++ java/com.sap.sailing.hanaexport/src/com/sap/sailing/hanaexport/impl/HanaConnectionFactoryImpl.java
@@ -4,10 +4,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class HanaConnectionFactoryImpl {
+    private static final String HANADB_ENDPOINT_PROPERTY_NAME = "sap.hana.endpoint";
+    private static final String HANADB_USERNAME_SYSTEM_PROPERTY_NAME = "sap.hana.username";
+    private static final String HANADB_PASSWORD_SYSTEM_PROPERTY_NAME = "sap.hana.password";
+    
     public void test() throws SQLException {
         DriverManager.getConnection(
-                "jdbc:sap://myhdb:30715/?autocommit=false", "myName",
-                   "mySecret");
+                System.getProperty(HANADB_ENDPOINT_PROPERTY_NAME), System.getProperty(HANADB_USERNAME_SYSTEM_PROPERTY_NAME),
+                   System.getProperty(HANADB_PASSWORD_SYSTEM_PROPERTY_NAME));
         com.sap.db.jdbcext.XidSAP test = null; // make sure this compiles, meaning we have com.sap.db.jdbc on the classpath
         if (test == null) {
             System.out.println("All as expected");
