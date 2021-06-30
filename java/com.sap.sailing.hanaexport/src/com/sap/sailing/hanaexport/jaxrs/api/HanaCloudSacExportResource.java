8af6a01edbafe9720c9c9ea49968986de007d0c2
Axel Uhl <axel.uhl@sap.com>
2021-06-30 11:34:52 +0200

    bug5451: started preparing a REST resource to trigger HANA export / DB clearing

--- java/com.sap.sailing.hanaexport/src/com/sap/sailing/hanaexport/jaxrs/api/HanaCloudSacExportResource.java
+++ java/com.sap.sailing.hanaexport/src/com/sap/sailing/hanaexport/jaxrs/api/HanaCloudSacExportResource.java
@@ -1,6 +1,7 @@
 package com.sap.sailing.hanaexport.jaxrs.api;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
@@ -11,6 +12,7 @@ import javax.ws.rs.core.Response;
 
 import org.apache.shiro.SecurityUtils;
 
+import com.sap.sailing.domain.base.BoatClass;
 import com.sap.sailing.hanaexport.HanaConnectionFactory;
 import com.sap.sailing.server.interfaces.RacingEventService;
 import com.sap.sailing.shared.server.gateway.jaxrs.SharedAbstractSailingServerResource;
@@ -30,10 +32,30 @@ public class HanaCloudSacExportResource extends SharedAbstractSailingServerResou
                 SecuredSecurityTypes.ServerActions.CAN_EXPORT_MASTERDATA,
                 new TypeRelativeObjectIdentifier(ServerInfo.getName())));
         logger.info("Clearing HANA Cloud SAILING DB on behalf of user "+SecurityUtils.getSubject().getPrincipal());
-        final RacingEventService racingEventService = getService();
         final Connection connection = HanaConnectionFactory.INSTANCE.getConnection();
         connection.createStatement().execute("DELETE FROM SAILING.BOAT_CLASS");
         logger.info("Done learing HANA Cloud SAILING DB on behalf of user "+SecurityUtils.getSubject().getPrincipal());
+        return Response.ok().build();
+    }
+
+    @GET
+    @Produces("application/json;charset=UTF-8")
+    @Path("export")
+    public Response export() throws SQLException {
+        SecurityUtils.getSubject().checkPermission(SecuredSecurityTypes.SERVER.getStringPermissionForTypeRelativeIdentifier(
+                SecuredSecurityTypes.ServerActions.CAN_EXPORT_MASTERDATA,
+                new TypeRelativeObjectIdentifier(ServerInfo.getName())));
+        logger.info("Exporting HANA Cloud SAILING DB content on behalf of user "+SecurityUtils.getSubject().getPrincipal());
+        final RacingEventService racingEventService = getService();
+        final Connection connection = HanaConnectionFactory.INSTANCE.getConnection();
+        final PreparedStatement insertBoatClasses = connection.prepareStatement("INSERT INTO SAILING.BOAT_CLASS (\"id\", \"description\") VALUES (?, ?);");
+        for (final BoatClass boatClass : racingEventService.getBaseDomainFactory().getBoatClasses()) {
+            insertBoatClasses.setString(1, boatClass.getName());
+            insertBoatClasses.setString(2, "Type "+boatClass.getHullType().name()+", length "+
+                    boatClass.getHullLength().getMeters()+"m, beam "+boatClass.getHullBeam().getMeters()+"m");
+            insertBoatClasses.execute();
+        }
+        logger.info("Done exporting HANA Cloud SAILING DB content on behalf of user "+SecurityUtils.getSubject().getPrincipal());
         return Response.ok().build();
     }
 }
