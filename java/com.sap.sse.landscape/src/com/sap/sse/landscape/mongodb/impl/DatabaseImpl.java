276f3aa3ad703e83dcf1963d5ac034c59d547b48
Axel Uhl <axel.uhl@sap.com>
2021-08-11 17:56:11 +0200

    on replicas use mongodb://localhost:27017/{dbName}?replicaSet=replica&... as the default DB configuration, in line with new AMI

--- java/com.sap.sse.landscape/src/com/sap/sse/landscape/mongodb/impl/DatabaseImpl.java
+++ java/com.sap.sse.landscape/src/com/sap/sse/landscape/mongodb/impl/DatabaseImpl.java
@@ -1,14 +1,10 @@
 package com.sap.sse.landscape.mongodb.impl;
 
-import java.net.URISyntaxException;
-import java.util.logging.Logger;
-
 import com.sap.sse.common.impl.NamedImpl;
 import com.sap.sse.landscape.mongodb.Database;
 import com.sap.sse.landscape.mongodb.MongoEndpoint;
 
 public class DatabaseImpl extends NamedImpl implements Database {
-    private static final Logger logger = Logger.getLogger(DatabaseImpl.class.getName());
     private static final long serialVersionUID = -3816220771822620058L;
     private final MongoEndpoint endpoint;
 
@@ -20,12 +16,6 @@ public class DatabaseImpl extends NamedImpl implements Database {
     @Override
     public MongoEndpoint getEndpoint() {
         return endpoint;
-    }
-
-    @Override
-    public void drop() throws URISyntaxException {
-        logger.info("Dropping database "+this);
-        getMongoDatabase().drop();
     }
 
     @Override
