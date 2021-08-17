79aa94fb478dcae05c3c0eba9e2f1beb5befcaf1
Axel Uhl <axel.uhl@sap.com>
2021-08-17 18:07:23 +0200

    refactored HANA export and introduced parameters to select leaderboard groups / events by their ID

---
+++ java/com.sap.sailing.hanaexport/src/com/sap/sailing/hanaexport/jaxrs/api/InsertBoatClassStatement.java
@@ -0,0 +1,28 @@
+package com.sap.sailing.hanaexport.jaxrs.api;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.sap.sailing.domain.base.BoatClass;
+
+public class InsertBoatClassStatement extends AbstractPreparedInsertStatement<BoatClass> {
+    protected InsertBoatClassStatement(Connection connection) throws SQLException {
+        super(connection.prepareStatement(
+                "INSERT INTO SAILING.\"BoatClass\" (\"id\", \"description\", \"hullLengthInMeters\", \"hullBeamInMeters\", \"hullType\") VALUES (?, ?, ?, ?, ?);"));
+    }
+
+    @Override
+    public void parameterizeStatement(BoatClass boatClass) throws SQLException {
+        if (boatClass.getName() == null) {
+            getPreparedStatement().setString(1, "<null>");
+        } else {
+            getPreparedStatement().setString(1, boatClass.getName().substring(0, Math.min(boatClass.getName().length(), 255)));
+        }
+        getPreparedStatement().setString(2, "Type "+boatClass.getHullType().name()+", length "+
+                boatClass.getHullLength().getMeters()+"m, beam "+boatClass.getHullBeam().getMeters()+"m");
+        setDouble(3, boatClass.getHullLength().getMeters());
+        setDouble(4, boatClass.getHullBeam().getMeters());
+        getPreparedStatement().setString(5, boatClass.getHullType().name());
+        getPreparedStatement().execute();
+    }
+}
