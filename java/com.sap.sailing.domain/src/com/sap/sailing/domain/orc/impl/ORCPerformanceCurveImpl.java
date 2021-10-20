7c6c5b566187b6466032c65c7cb4964e160f4b40
Axel Uhl <axel.uhl@sap.com>
2021-10-20 18:02:20 +0200

    fixed performance issue with ORC PCS, passing through cache more thoroughly

--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/orc/impl/ORCPerformanceCurveImpl.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/orc/impl/ORCPerformanceCurveImpl.java
@@ -162,7 +162,7 @@ public class ORCPerformanceCurveImpl implements Serializable, ORCPerformanceCurv
         final LinkedHashMap<Speed, Duration> result = new LinkedHashMap<>();
         final Map<ORCPerformanceCurveLeg, Map<Speed, Duration>> allowancesPerLeg = new HashMap<>();
         for (final ORCPerformanceCurveLeg leg : course.getLegs()) {
-            allowancesPerLeg.put(leg, createAllowancePerLeg(leg, certificate));
+            allowancesPerLeg.put(leg, createAllowancePerLeg(leg, certificate, cache));
         }
         for (final Speed tws : certificate.getTrueWindSpeeds()) {
             Duration allowancePerTws = new SecondsDurationImpl(0);
@@ -192,23 +192,23 @@ public class ORCPerformanceCurveImpl implements Serializable, ORCPerformanceCurv
         Map<Speed, Speed> runVMGPredictionPerTrueWindSpeed = certificate.getRunVMGPredictions();
         Map<Speed, Duration> runAllowancePerTrueWindSpeed = certificate.getRunAllowances();
         final Map<Speed, Duration> result = new HashMap<>();
-        
         switch (leg.getType()) {
         case TWA:
+            final Bearing absoluteLegTwa = leg.getTwa(cache).abs();
             for (final Speed tws : certificate.getTrueWindSpeeds()) {
                 // Case switching on TWA (0. TWA == 0; 1. TWA < Beat; 2. Beat < TWA < Gybe; 3. Gybe < TWA; 4. TWA == 180)
-                if (leg.getTwa(cache).abs().compareTo(beatAngles.get(tws)) <= 0) {
+                if (absoluteLegTwa.compareTo(beatAngles.get(tws)) <= 0) {
                     // Case 0 & 1 - result = beatVMG * distance * cos(TWA)
-                    result.put(tws, beatAllowancePerTrueWindSpeed.get(tws).times(leg.getLength().getNauticalMiles()).times(Math.cos(leg.getTwa().abs().getRadians())));
-                } else if (leg.getTwa().abs().compareTo(runAngles.get(tws)) >= 0) {
+                    result.put(tws, beatAllowancePerTrueWindSpeed.get(tws).times(leg.getLength().getNauticalMiles()).times(Math.cos(absoluteLegTwa.getRadians())));
+                } else if (absoluteLegTwa.compareTo(runAngles.get(tws)) >= 0) {
                     // Case 3 & 4 - result = runVMG * distance * cos(TWA)
-                    result.put(tws, runAllowancePerTrueWindSpeed.get(tws).times(leg.getLength().getNauticalMiles()).times(Math.cos(Math.PI - leg.getTwa().abs().getRadians())));
+                    result.put(tws, runAllowancePerTrueWindSpeed.get(tws).times(leg.getLength().getNauticalMiles()).times(Math.cos(Math.PI - absoluteLegTwa.getRadians())));
                 } else {
                     // Case 2 - result is given through the laGrange Interpolation, between the Beat and Gybe Angles
                     result.put(tws,
                             getLagrangeSpeedPredictionForTrueWindSpeedAndAngle(twaAllowances, beatAngles,
                                     beatVMGPredictionPerTrueWindSpeed, runAngles, runVMGPredictionPerTrueWindSpeed, tws,
-                                    leg.getTwa().abs()).getDuration(leg.getLength()));
+                                    absoluteLegTwa).getDuration(leg.getLength()));
                 }
             }
             break;
