8b54f65ca67272c870b6fedd86f0695dea170bda
Tim Hessenmüller <tim.hessenmueller@sap.com>
2021-12-10 14:24:39 +0100

    NotEnoughDataHasBeenAddedException: Fixed typo in error message

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/polars/NotEnoughDataHasBeenAddedException.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/polars/NotEnoughDataHasBeenAddedException.java
@@ -6,7 +6,7 @@ public class NotEnoughDataHasBeenAddedException extends Exception implements Ser
     private static final long serialVersionUID = 9191379705907500676L;
 
     public NotEnoughDataHasBeenAddedException() {
-        super("No data has been added to the Processor yet. At least to value-pairs have to be added.");
+        super("No data has been added to the Processor yet. At least two value-pairs have to be added.");
     }
 
     public NotEnoughDataHasBeenAddedException(String string) {
