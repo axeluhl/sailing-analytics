4ce74a0225739ce74066921ef2f73f8f959e309d
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2022-03-08 17:23:13 +0100

    bug5665: Introduced PremiumButton as Subclass of PremiumUIElement

---
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/common/client/premium/SailingPremiumButton.java
@@ -0,0 +1,34 @@
+package com.sap.sailing.gwt.common.client.premium;
+
+import static com.sap.sailing.gwt.common.client.premium.SailingPremiumIconRessource.INSTANCE;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Image;
+import com.sap.sailing.gwt.ui.client.EntryPointLinkFactory;
+import com.sap.sse.security.shared.HasPermissions.Action;
+import com.sap.sse.security.ui.client.premium.PaywallResolver;
+import com.sap.sse.security.ui.client.premium.PremiumButton;
+
+public class SailingPremiumButton extends PremiumButton {
+
+    public SailingPremiumButton(final String label, final Action action, final PaywallResolver paywallResolver) {
+        super(label, action, paywallResolver);
+    }
+
+    @Override
+    protected Image createPremiumIcon() {
+        return new Image();
+    }
+
+    @Override
+    protected void onUserPermissionUpdate(final boolean isPermitted) {
+        super.onUserPermissionUpdate(isPermitted);
+        image.setUrl((isPermitted ? INSTANCE.premiumIconPermitted() : INSTANCE.premiumIcon()).getSafeUri());
+    }
+
+    @Override
+    protected void onSubscribeDialogConfirmation(final Iterable<String> unlockingPlans) {
+        Window.open(EntryPointLinkFactory.createSubscriptionPageLink(unlockingPlans), "_blank", "");
+    }
+
+}
---
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/client/premium/PremiumButton.java
@@ -0,0 +1,123 @@
+package com.sap.sse.security.ui.client.premium;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasAllKeyHandlers;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.FocusWidget;
+import com.google.gwt.user.client.ui.Image;
+import com.sap.sse.gwt.client.dialog.ConfirmationDialog;
+import com.sap.sse.security.shared.HasPermissions.Action;
+import com.sap.sse.security.ui.client.i18n.StringMessages;
+
+public abstract class PremiumButton extends PremiumUiElement implements HasAllKeyHandlers, HasClickHandlers {
+
+    private static PremiumButtonUiBinder uiBinder = GWT.create(PremiumButtonUiBinder.class);
+
+    interface PremiumButtonUiBinder extends UiBinder<FocusPanel, PremiumButton> {
+    }
+
+    interface Style extends CssResource {
+        @ClassName("premium-container")
+        String premiumContainer();
+
+        @ClassName("premium-check-box")
+        String premiumCheckBox();
+
+        @ClassName("premium-permitted")
+        String premiumPermitted();
+
+        @ClassName("premium-icon")
+        String premiumIcon();
+    }
+
+    @UiField
+    StringMessages i18n;
+    @UiField
+    Style style;
+    @UiField
+    FocusPanel container;
+    @UiField(provided = true)
+    protected final Image image;
+    @UiField(provided = true)
+    protected final Button button;
+
+    private final ConfirmationDialog subscribeDialog;
+
+    /**
+     * A Composite component, that includes a checkbox and an additional premium icon, indicating that the feature to be
+     * enabled is a premium feature if the user does not have the permission.
+     */
+    protected PremiumButton(final String label, final Action action, final PaywallResolver paywallResolver) {
+        super(action, paywallResolver);
+        this.image = createPremiumIcon();
+        this.button = new Button(label);
+        initWidget(uiBinder.createAndBindUi(this));
+        this.subscribeDialog = ConfirmationDialog.create(i18n.subscriptionSuggestionTitle(),
+                i18n.pleaseSubscribeToUse(), i18n.takeMeToSubscriptions(), i18n.cancel(),
+                () -> paywallResolver.getUnlockingSubscriptionPlans(action, this::onSubscribeDialogConfirmation));
+        updateUserPermission();
+    }
+
+    protected abstract void onSubscribeDialogConfirmation(Iterable<String> unlockingPlans);
+
+    @Override
+    protected void onEnsureDebugId(final String baseID) {
+        this.button.ensureDebugId(baseID);
+    }
+
+    @UiHandler("container")
+    void onContainerClicked(final ClickEvent event) {
+        if (!hasPermission()) {
+            this.updateUserPermission();
+            subscribeDialog.center();
+        }
+    }
+
+    @Override
+    protected void onUserPermissionUpdate(final boolean isPermitted) {
+        button.setEnabled(isEnabled() && isPermitted);
+        container.setStyleName(style.premiumPermitted(), isPermitted);
+    }
+
+    @Override
+    public void setEnabled(final boolean enabled) {
+        super.setEnabled(enabled);
+        button.setEnabled(enabled);
+    }
+
+    @Override
+    public HandlerRegistration addKeyDownHandler(final KeyDownHandler handler) {
+        return container.addKeyDownHandler(handler);
+    }
+
+    @Override
+    public HandlerRegistration addKeyPressHandler(final KeyPressHandler handler) {
+        return container.addKeyPressHandler(handler);
+    }
+
+    @Override
+    public HandlerRegistration addKeyUpHandler(final KeyUpHandler handler) {
+        return container.addKeyUpHandler(handler);
+    }
+    
+    @Override
+    public HandlerRegistration addClickHandler(final ClickHandler handler) {
+        return button.addClickHandler(handler);
+    }
+
+    public FocusWidget getFocusWidget() {
+        return this.button;
+    }
+}
