9b158a24425bc26a74cb20a17f3df9e33185d183
Dennis Aulenbacher <dennis.aulenbacher@trivadis.com>
2022-05-09 11:27:01 +0200

    bug5706: Intermediary commit. Started introducing paywallResolver to

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/raceviewerlaunchpad/RaceviewerLaunchPad.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/raceviewerlaunchpad/RaceviewerLaunchPad.java
@@ -8,12 +8,21 @@ import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.sap.sailing.gwt.home.communication.race.SimpleRaceMetadataDTO;
+import com.sap.sailing.gwt.ui.client.EntryPointLinkFactory;
 import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sailing.gwt.ui.raceboard.RaceBoardModes;
+import com.sap.sse.gwt.client.dialog.ConfirmationDialog;
+import com.sap.sse.security.shared.HasPermissions.Action;
+import com.sap.sse.security.shared.dto.SecuredDTO;
+import com.sap.sse.security.shared.dto.UserDTO;
+import com.sap.sse.security.ui.client.UserStatusEventHandler;
+import com.sap.sse.security.ui.client.premium.PaywallResolver;
+import com.sap.sailing.domain.common.security.SecuredDomainType.TrackedRaceActions;
 
 class RaceviewerLaunchPad<T extends SimpleRaceMetadataDTO> extends Composite {
 
@@ -28,6 +37,7 @@ class RaceviewerLaunchPad<T extends SimpleRaceMetadataDTO> extends Composite {
     private final Function<? super T, String> mapAndWindChartUrlFactory;
 
     private final PopupPanel parent;
+    private final PaywallResolver paywallResolver;
 
     RaceviewerLaunchPad(final T data, final BiFunction<? super T, String, String> raceboardUrlFactory,
             final Function<? super T, String> mapAndWindChartUrlFactory, final PopupPanel parent) {
@@ -36,9 +46,21 @@ class RaceviewerLaunchPad<T extends SimpleRaceMetadataDTO> extends Composite {
         this.parent = parent;
         initWidget(uiBinder.createAndBindUi(this));
         local_res.css().ensureInjected();
+        paywallResolver.registerUserStatusEventHandler((dto, notifyOtherInstances) -> {
+            removeItems();
+            initItems(data);
+        });
         initItems(data);
         initStyles(data);
         sinkEvents(Event.ONCLICK);
+    }
+
+    private ConfirmationDialog createSubscribeDialog(Action action, SecuredDTO contextDTO) {
+        final StringMessages i18n = StringMessages.INSTANCE;
+        return ConfirmationDialog.create(i18n.subscriptionSuggestionTitle(),
+                i18n.pleaseSubscribeToUse(), i18n.takeMeToSubscriptions(), i18n.cancel(),
+                () -> paywallResolver.getUnlockingSubscriptionPlans(action, contextDTO, (unlockingPlans) -> 
+                Window.open(EntryPointLinkFactory.createSubscriptionPageLink(unlockingPlans), "_blank", "")));
     }
 
     private void initItems(final T data) {
@@ -74,23 +96,34 @@ class RaceviewerLaunchPad<T extends SimpleRaceMetadataDTO> extends Composite {
     }
 
     private void addItem(final T data, final RaceviewerLaunchPadMenuItem item) {
-        final String url = RaceviewerLaunchPadMenuItem.WIND_AND_COURSE == item ? mapAndWindChartUrlFactory.apply(data)
-                : raceboardUrlFactory.apply(data, item.raceBoardMode);
-        itemContainerUi.appendChild(new RaceviewerLaunchPadItem(item.label, item.icon, url).getElement());
+        if(paywallResolver.hasPermission(item.action, data)){
+            final String url = RaceviewerLaunchPadMenuItem.WIND_AND_COURSE == item ? 
+                    mapAndWindChartUrlFactory.apply(data)
+                    : raceboardUrlFactory.apply(data, item.raceBoardMode);
+            itemContainerUi.appendChild(new RaceviewerLaunchPadItem(item.label, item.icon, url).getElement());
+        }else {
+            createSubscribeDialog(item.action, data);
+        }
     }
 
     private enum RaceviewerLaunchPadMenuItem {
-        REPLAY(StringMessages.INSTANCE.replay(), "launch-play", RaceBoardModes.PLAYER.name()),
-        WATCH_LIVE(StringMessages.INSTANCE.watchLive(), "launch-play", RaceBoardModes.PLAYER.name()),
-        RACE_ANALYSIS(StringMessages.INSTANCE.raceAnalysis(), "launch-loupe", RaceBoardModes.FULL_ANALYSIS.name()),
-        START_ANALYSIS(StringMessages.INSTANCE.startAnalysis(), "launch-start", RaceBoardModes.START_ANALYSIS.name()),
-        WINNING_LANES(StringMessages.INSTANCE.winningLanes(), "launch-winning-lanes", RaceBoardModes.WINNING_LANES.name()),
-        WIND_AND_COURSE(StringMessages.INSTANCE.windAndCourse(), "launch-wind-course", null);
+        REPLAY(StringMessages.INSTANCE.replay(), "launch-play", RaceBoardModes.PLAYER.name(), null),
+        WATCH_LIVE(StringMessages.INSTANCE.watchLive(), "launch-play", RaceBoardModes.PLAYER.name(), null),
+        RACE_ANALYSIS(StringMessages.INSTANCE.raceAnalysis(), "launch-loupe", 
+                RaceBoardModes.FULL_ANALYSIS.name(), null),
+        START_ANALYSIS(StringMessages.INSTANCE.startAnalysis(), "launch-start", RaceBoardModes.START_ANALYSIS.name(), 
+                TrackedRaceActions.VIEWANALYSISCHARTS),
+        WINNING_LANES(StringMessages.INSTANCE.winningLanes(), "launch-winning-lanes", 
+                RaceBoardModes.WINNING_LANES.name(), null),
+        WIND_AND_COURSE(StringMessages.INSTANCE.windAndCourse(), "launch-wind-course", null, null);
 
         private String label, icon, raceBoardMode;
+        private final Action action;
 
-        private RaceviewerLaunchPadMenuItem(final String label, final String iconKey, final String raceBoardMode) {
+        private RaceviewerLaunchPadMenuItem(final String label, final String iconKey, final String raceBoardMode, 
+                final Action action) {
             this.label = label;
+            this.action = action;
             this.icon = "<svg><use xlink:href=\"#" + iconKey + "\"></use></svg>";
             this.raceBoardMode = raceBoardMode;
         }
