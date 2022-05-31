3a37912893fea4970f3e332b6b19a9f4da969ac1
Axel Uhl <axel.uhl@sap.com>
2022-05-31 07:38:41 +0200

    put RRS26_3MIN to the end of the enum to remain backward compatible when reading by ordinal

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/racelog/RacingProcedureType.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/racelog/RacingProcedureType.java
@@ -11,12 +11,12 @@ import java.util.List;
 public enum RacingProcedureType {
     UNKNOWN("Unknown"),
     RRS26("Fix Line Start (RRS26)"),
-    RRS26_3MIN("Fix Line Start with 3min Sequence (RRS26/3)"),
     SWC("Sailing World Cup Start"),
     GateStart("Gate Start"),
     ESS("\"Extreme Sailing Series\"-Start"),
     BASIC("Basic Countdown Start"),
-    LEAGUE("League Start");
+    LEAGUE("League Start"),
+    RRS26_3MIN("Fix Line Start with 3min Sequence (RRS26/3)");
     
     private String displayName;
 
