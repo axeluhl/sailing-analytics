051f54b2bcdfd33e4b9bc4490c4d6516bb19f0e1
Kevin Wiesner <kevin.wiesner@sap.com>
2022-09-26 15:02:08 +0200

    bug5767: added test for GPSFixMovingJsonDeserializer

---
+++ java/com.sap.sailing.server.gateway.serialization.test/src/com/sap/sailing/server/gateway/deserialization/test/GPSFixMovingJsonDeserializerTest.java
@@ -0,0 +1,35 @@
+package com.sap.sailing.server.gateway.deserialization.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sap.sailing.domain.common.tracking.GPSFixMoving;
+import com.sap.sailing.domain.common.tracking.impl.GPSFixMovingImpl;
+import com.sap.sailing.server.gateway.deserialization.impl.GPSFixMovingJsonDeserializer;
+import com.sap.sailing.server.gateway.serialization.impl.GPSFixMovingJsonSerializer;
+import com.sap.sse.shared.json.JsonDeserializationException;
+
+public class GPSFixMovingJsonDeserializerTest {
+    private GPSFixMovingJsonSerializer serializer;
+    private GPSFixMovingJsonDeserializer deserializer;
+
+    @Before
+    public void setup() {
+        serializer = new GPSFixMovingJsonSerializer();
+        deserializer = new GPSFixMovingJsonDeserializer();
+    }
+
+    @Test
+    public void testGPSFixMovingDeserializer() throws ParseException, JsonDeserializationException {
+        GPSFixMoving gpsFix = GPSFixMovingImpl.create(8.0, 55.0, System.currentTimeMillis(), 5.0, 14.0);
+        
+        JSONObject gpsFixSerialized = serializer.serialize(gpsFix);
+        GPSFixMoving gpsFixDeserialized = deserializer.deserialize(gpsFixSerialized);
+        
+        assertEquals(gpsFix, gpsFixDeserialized);
+    }
+}
