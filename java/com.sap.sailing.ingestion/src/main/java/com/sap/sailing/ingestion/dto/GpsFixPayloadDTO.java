32143eb633fab1c6228f0e421f524b9a0ee50533
Kevin Wiesner <kevin.wiesner@sap.com>
2022-09-30 12:29:12 +0200

    bug5440: adjusted GpsFixPayloadDTO to implement Comparable

--- java/com.sap.sailing.ingestion/src/main/java/com/sap/sailing/ingestion/dto/GpsFixPayloadDTO.java
+++ java/com.sap.sailing.ingestion/src/main/java/com/sap/sailing/ingestion/dto/GpsFixPayloadDTO.java
@@ -2,7 +2,7 @@ package com.sap.sailing.ingestion.dto;
 
 import java.io.Serializable;
 
-public class GpsFixPayloadDTO implements Serializable {
+public class GpsFixPayloadDTO implements Serializable, Comparable<GpsFixPayloadDTO> {
     private static final long serialVersionUID = 6802355060150334552L;
 
     private long timestamp;
@@ -49,5 +49,10 @@ public class GpsFixPayloadDTO implements Serializable {
 
     public void setCourse(double course) {
         this.course = course;
+    }
+    
+    @Override
+    public int compareTo(GpsFixPayloadDTO o) {
+        return (int) (this.getTimestamp() - o.getTimestamp());
     }
 }
