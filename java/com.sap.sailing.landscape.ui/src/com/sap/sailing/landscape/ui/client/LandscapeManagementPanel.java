f020e1a690522cf0c2a75f7ec32fa622c81c7557
Niklas Opiela <niklas.opiela@sap.com>
2022-11-07 16:04:09 +0100

    Changed layout

--- java/com.sap.sailing.landscape.ui/src/com/sap/sailing/landscape/ui/client/LandscapeManagementPanel.java
+++ java/com.sap.sailing.landscape.ui/src/com/sap/sailing/landscape/ui/client/LandscapeManagementPanel.java
@@ -29,6 +29,8 @@ import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -125,6 +127,9 @@ public class LandscapeManagementPanel extends SimplePanel {
      */
     private static final Duration DEFAULT_DURATION_TO_WAIT_BEFORE_COMPARE_SERVERS = Duration.ONE_MINUTE.times(5);
     private static final int DEFAULT_NUMBER_OF_COMPARE_SERVERS_ATTEMPTS = 5;
+    
+    //Spacing for Setupbar (AWS/SSH/REGION)
+    private static final int DEFAULT_SETUPBAR_HEIGHT = 300;
 
     public LandscapeManagementPanel(StringMessages stringMessages, UserService userService,
             AdminConsoleTableResources tableResources, ErrorReporter errorReporter) {
@@ -136,6 +141,7 @@ public class LandscapeManagementPanel extends SimplePanel {
         final HorizontalPanel awsCredentialsAndSshKeys = new HorizontalPanel();
         mainPanel.add(awsCredentialsAndSshKeys);
         final CaptionPanel awsCredentialsPanel = new CaptionPanel(stringMessages.awsCredentials());
+        awsCredentialsPanel.setHeight("" + DEFAULT_SETUPBAR_HEIGHT + "px");
         awsCredentialsAndSshKeys.add(awsCredentialsPanel);
         mfaLoginWidget = new AwsMfaLoginWidget(landscapeManagementService, errorReporter, userService, stringMessages);
         mfaLoginWidget.addListener(validSession->refreshAllThatNeedsAwsCredentials());
@@ -151,10 +157,9 @@ public class LandscapeManagementPanel extends SimplePanel {
             }
         };
         sshKeyManagementPanel = new SshKeyManagementPanel(stringMessages, userService,
-                landscapeManagementService, tableResources, errorReporter, /* access key provider */ mfaLoginWidget, regionsTable.getSelectionModel());
+                landscapeManagementService, tableResources, errorReporter, /* access key provider */ mfaLoginWidget, regionsTable.getSelectionModel());       
         final CaptionPanel sshKeysCaptionPanel = new CaptionPanel(stringMessages.sshKeys());
-        awsCredentialsAndSshKeys.add(sshKeysCaptionPanel);
-        sshKeysCaptionPanel.add(sshKeyManagementPanel);
+        sshKeysCaptionPanel.setHeight("" + DEFAULT_SETUPBAR_HEIGHT + "px");
         regionsTable.addColumn(new TextColumn<String>() {
             @Override
             public String getValue(String s) {
@@ -162,9 +167,19 @@ public class LandscapeManagementPanel extends SimplePanel {
             }
         }, stringMessages.region(), new NaturalComparator());
         final CaptionPanel regionsCaptionPanel = new CaptionPanel(stringMessages.region());
-        regionsCaptionPanel.add(regionsTable); 
-        mainPanel.add(regionsCaptionPanel);
+        final SimplePanel regionPanel = new  SimplePanel();
+        final ScrollPanel regionScrollPanel = new ScrollPanel();
+        regionsCaptionPanel.setHeight("" + DEFAULT_SETUPBAR_HEIGHT + "px");
+        regionScrollPanel.setHeight("" + (DEFAULT_SETUPBAR_HEIGHT - 35)  + "px");
+        regionScrollPanel.setAlwaysShowScrollBars(true);
+        regionPanel.add(regionsTable);
+        regionScrollPanel.add(regionPanel);
+        regionsCaptionPanel.add(regionScrollPanel); 
+        awsCredentialsAndSshKeys.add(regionsCaptionPanel);
         refreshRegionsTable(userService);
+        awsCredentialsAndSshKeys.add(sshKeysCaptionPanel);
+        sshKeysCaptionPanel.add(sshKeyManagementPanel);
+        
         // MongoDB endpoints:
         mongoEndpointsTable = new TableWrapperWithSingleSelectionAndFilter<MongoEndpointDTO, StringMessages, AdminConsoleTableResources>(
                 stringMessages, errorReporter, /* enablePager */ false,
