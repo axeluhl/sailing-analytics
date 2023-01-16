c166380e0f6b605867e35364812469089addefdb
Dennis <dennis.aulenbacher@accenture.com>
2023-01-16 09:22:20 +0100

    bug5774: New pointer to moved premium icon

--- java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/client/premium/uielements/PremiumIconRessource.java
+++ java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/client/premium/uielements/PremiumIconRessource.java
@@ -8,7 +8,7 @@ import com.google.gwt.resources.client.DataResource.MimeType;
 public interface PremiumIconRessource extends ClientBundle {
     public static final PremiumIconRessource INSTANCE = GWT.create(PremiumIconRessource.class);
     
-    @Source("../images/icon_premium.svg")
+    @Source("icon_premium.svg")
     @MimeType("image/svg+xml")
     DataResource premiumIcon();
 }
