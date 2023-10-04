d56b1e8e569879260498117cda6b1405f4a24377
Thomas Stokes <thomas.stokes@sap.com>
2023-10-04 17:32:26 +0200

    Revert "Fix ssh enabled issue temporarily"
    This reverts commit ca21d8f3dc5c36fa582dd465c7d068b15999cb8f.

--- java/com.sap.sailing.landscape.ui/src/com/sap/sailing/landscape/ui/client/AwsAccessKeyProvider.java
+++ java/com.sap.sailing.landscape.ui/src/com/sap/sailing/landscape/ui/client/AwsAccessKeyProvider.java
@@ -1,8 +1,5 @@
 package com.sap.sailing.landscape.ui.client;
 
-import com.sap.sailing.landscape.ui.client.AwsMfaLoginWidget.AwsMfaLoginListener;
-
 public interface AwsAccessKeyProvider {
     boolean hasValidSessionCredentials();
-    void addListener(AwsMfaLoginListener listener);
 }
