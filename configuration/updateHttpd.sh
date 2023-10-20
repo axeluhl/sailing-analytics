1a4b088212e29a85db53caf2e329bc1f90990ab2
Thomas Stokes <thomas.stokes@sap.com>
2023-10-20 15:51:43 +0200

    Alternative git update solution
    This solution instead uses git diff, which won't work
    if the content is the same but a later commit exists (eg. ours merge).
    It does however, work without the git api.

--- configuration/updateHttpd.sh
+++ configuration/updateHttpd.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 BASE_URL=""
 BEARER_TOKEN=""
-NEW=$(curl -s -H "Authorization: Bearer $BEARER_TOKEN" https://api.github.com/repos/$BASE_URL/commits | grep 'sha' | head -n1 | sed -r 's/"sha": "(.*)",/\1/' | tr -d '[:space:]')
-CURRENT=$(cd /etc/httpd/conf && git rev-parse HEAD | tr -d '[:space:]')
 
-if [[ $NEW != $CURRENT ]]
+cd /etc/httpd/conf && git fetch
+if [[ -n "$(git diff HEAD origin/main)" ]]
 then
     echo "changing"
-    cd /etc/httpd/conf && git pull git@github.com:$BASE_URL.git
+    cd /etc/httpd/conf && git merge origin/main #fastforward merge occurs
     sleep 2
     sudo service httpd reload
 fi
+
 
 #crontab */2 * * * * /<location/updateHttpd.sh
