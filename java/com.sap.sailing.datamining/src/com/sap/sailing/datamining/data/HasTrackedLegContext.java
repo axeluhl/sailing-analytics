cb54d08972d2344ab0e42f1c910b783bbcaaede4
Lisa <Lisa.braun@sap.com>
2023-11-10 14:12:19 +0100

    adding_Leglength

--- java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/data/HasTrackedLegContext.java
+++ java/com.sap.sailing.datamining/src/com/sap/sailing/datamining/data/HasTrackedLegContext.java
@@ -2,8 +2,10 @@ package com.sap.sailing.datamining.data;
 
 import com.sap.sailing.domain.common.LegType;
 import com.sap.sailing.domain.tracking.TrackedLeg;
+import com.sap.sse.common.Distance;
 import com.sap.sse.datamining.annotations.Connector;
 import com.sap.sse.datamining.annotations.Dimension;
+import com.sap.sse.datamining.annotations.Statistic;
 
 public interface HasTrackedLegContext {
     
@@ -16,5 +18,6 @@ public interface HasTrackedLegContext {
     public LegType getLegType();
     @Dimension(messageKey="LegNumber", ordinal=7)
     public int getLegNumber();
-
+    @Statistic(messageKey = "LengthoftheLeg", resultDecimals = 2)
+    Distance getLegLength();
 }
