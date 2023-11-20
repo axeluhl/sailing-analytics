4a4aa3f37757e1c70c77afe6be247dcf20b6e3e4
Thomas Stokes <thomas.stokes@sap.com>
2023-11-20 17:28:11 +0000

    Added php, mfa logon alias and log rotate

--- configuration/setupDisposableProxy.sh
+++ configuration/setupDisposableProxy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 BEARER_TOKEN=$1
+MFA_NAME=$2
 yum update -y
-yum install -y perl httpd mod_proxy_html tmux nfs-utils git whois jq
+yum install -y perl httpd mod_proxy_html tmux nfs-utils git whois jq php
 sudo amazon-linux-extras install epel -y && yum install -y apachetop
 sed -i 's/.*sleep 10" //g' ~/.ssh/authorized_keys
 sed -i 's/#PermitRootLogin yes/PermitRootLogin without-password/' /etc/ssh/sshd_config
@@ -15,7 +16,7 @@ cat <<EOF > /var/www/html/index.html
 <!DOCTYPE html><html lang="en"><head><title>Health check</title><meta charset="UTF-8"></head><body><h1>Test page</h1></body></html>
 EOF
 systemctl enable httpd
-echo "net.ipv4.ip_conntrac_max = 131072" > /etc/sysctl.conf   #should I have appended to limits.conf
+echo "net.ipv4.ip_conntrac_max = 131072" > /etc/sysctl.conf   #should I have appended to limits.conf too
 yum install -y fail2ban
 cat <<EOF > /etc/fail2ban/jail.d/customisation.local
 [ssh-iptables]
@@ -47,3 +48,10 @@ ExecStart=/usr/local/bin/mountnvmeswap
 EOF
 ln -s /home/wiki/gitwiki/configuration/archive_instance_setup/mountnvmeswap /usr/local/bin/mountnvmeswap
 sed -i "s/nvme0n1/nvme4n1" /usr/local/bin/mountnvmeswap
+ln -s /home/wiki/gitwiki/configuration/aws-automation/awsmfalogon.sh /usr/local/bin
+sed -i 's/rotate 4/rotate 20 \n\nolddir \/var\/log\/logrotate-target/' /etc/logrotate.conf
+echo "alias awsmfa='echo -n "Token: "; read aws_mfa_token; . awsmfalogon.sh "${MFA_NAME}" ${aws_mfa_token}' " >> root/.bashrc
+source /root/.bashrc
+
+# will aws credentials need to be set? or will the ami store these details? session tokens? we will need a user without mfa.
+
