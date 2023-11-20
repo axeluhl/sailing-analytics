98ecffd5cd66cbd2b6d90a6943d8a94107a49c22
Thomas Stokes <thomas.stokes@sap.com>
2023-11-20 11:08:30 +0000

    Remove redundant lines

--- configuration/httpdHookScript.sh
+++ configuration/httpdHookScript.sh
@@ -2,8 +2,3 @@
 for IP in $(aws ec2 --region eu-west-2 describe-instances --filters Name=tag-key,Values="ReverseProxy" | jq -r '.Reservations[].Instances[].PublicIpAddress'); do 
   ssh ec2-user@${IP} 'cd ~ && ./updateHttpd.sh'; 
 done;
-
-
-
-
-#scope of proxy ProxyPass
