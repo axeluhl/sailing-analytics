727667cacac224ecbdf854c132d9dc02076bd9ab
Thomas Stokes <thomas.stokes@sap.com>
2023-12-04 17:24:31 +0100

    Move and tweak cgi script, which calls bash script to check if in the same region as the archive

--- configuration/httpd/cgi-bin/healthDeterminer.pl
+++ configuration/httpd/cgi-bin/healthDeterminer.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #Calls another bash script to determine whether or not an instance should display itself as healthy. 
 #The script then returns the right error code for the aws healthcheck to pass or fail.
-my $stat=`./script.sh`;
+my $stat=`./reverseProxyHealthCheck.sh`;
 $stat =~ s/^\s+|\s+$//g; #trims leading and trailing whitespace
 if ( $stat eq 'unhealthy' ) {
         printf("Status: 503 Not in the same az\n");
