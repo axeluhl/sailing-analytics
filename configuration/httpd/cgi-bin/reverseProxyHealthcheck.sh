b5465ef9e20f77aaebf7b4d3e913e2e393c53dd5
Thomas Stokes <thomas.stokes@sap.com>
2023-12-06 13:56:10 +0100

    b1873:Improve healthcheck and remove perl version

--- configuration/httpd/cgi-bin/reverseProxyHealthcheck.sh
+++ configuration/httpd/cgi-bin/reverseProxyHealthcheck.sh
@@ -3,41 +3,46 @@
 # Purpose: This script identifies whether the instance the script runs on is in the same AZ as the archive.
 # The user it is installed on must have aws credentials that don't need mfa.
 
-
 outputMessage() {
-    #parameter 1: the message to display
+    # parameter 1: the message to display on the site
     echo "Content-type: text/html" 
     echo ""
     echo $1
 }
 
+status() {
+    # parameter 1: status code and messages
+    echo "Status: $1"
+}
 
 # The names of the variables in the macros file.
 ARCHIVE_IP_NAME="ARCHIVE_IP"
 ARCHIVE_FAILOVER_IP_NAME="ARCHIVE_FAILOVER_IP"
-# The regex to extract the ip from a define var.
+PRODUCTION_IP_NAME="PRODUCTION_ARCHIVE"
+MACROS_PATH="/etc/httpd/conf.d/000-macros.conf"
+# The regex to extract the ip from a line ending in an ip.
 IP_REGEX="[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$"
-ARCHIVE_IP=$(grep -m 1 "^Define ${ARCHIVE_IP_NAME}\> .*" /etc/httpd/conf.d/000-macros.conf | grep -o "${IP_REGEX}")
-ARCHIVE_FAILOVER=$(grep -m 1 "^Define ${ARCHIVE_FAILOVER_IP_NAME}\> .*" /etc/httpd/conf.d/000-macros.conf | grep -o "${IP_REGEX}")
-store=$(curl -s --connect-timeout 4 "http://${ARCHIVE_IP}:8888/gwt/status")
-
-if [[ 0 -eq $? ]]
+# Extracts which IP is in production.
+PRODUCTION_IP=$(sed -n -e  "s/^Define ${PRODUCTION_IP_NAME}\> \(.*\)$/\1/p" ${MACROS_PATH})
+if [[ "$PRODUCTION_IP" == "\${${ARCHIVE_IP_NAME}}" ]]
 then
-	# check if main archive is in the same az by fetching the metadata
+	# Check if main archive is in the same az by fetching the metadata
 	MYAZ=$(curl -s  http://169.254.169.254/latest/meta-data/placement/availability-zone)
+    # Extract the actual IP of the archive
+    ARCHIVE_IP=$(grep -m 1 "^Define ${ARCHIVE_IP_NAME}\> .*"  ${MACROS_PATH} | grep -o "${IP_REGEX}")
     # Fetch the instances in the region and then extract arrays, iterate through them, select those with the correct ip, and then gets the AZ.
 	OTHERAZ=$(aws ec2 describe-instances | jq  --arg ip "$ARCHIVE_IP"   '..  | arrays | .[] | select(.PrivateIpAddress==$ip)' | jq '.. |  select(.AvailabilityZone?) | .AvailabilityZone')
-	OTHERAZ=$(echo $OTHERAZ | grep -o "[a-z0-9-]*[^\"]")  #removes speech marks
+	OTHERAZ=$(echo $OTHERAZ | grep -o "[a-z0-9-]*[^\"]")  # Removes speech marks
 	if [[ "$MYAZ" == "$OTHERAZ" ]]
 	then
-		echo "Status: 200 Healthy"
+		status "200"
         outputMessage "Healthy: in the same az as the archive"
 	else 
-		echo "Status: 503 Not in the same az"
+		status "503 Not in the same az"
         outputMessage "Unhealthy: Not in the same az as the archive"
 	fi
 else
-	# check if failover archive is in the same az
-    echo "Status: 200 Healthy"
+	# We don't check if failover archive is in the same az
+    status "200"
 	outputMessage "failover at play: force healthy"
 fi
