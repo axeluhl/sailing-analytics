9cb33886ea7c7812f6ee82cac137dd2bc879e933
Thomas Stokes <thomas.stokes@sap.com>
2023-12-06 21:54:40 +0100

    b5914: Add --fail and -L to  script

--- configuration/switchoverArchive.sh
+++ configuration/switchoverArchive.sh
@@ -88,11 +88,13 @@ setFailoverIfNotSet() {
 }
 
 logger -t archive "begin check"
-curl -s --connect-timeout ${TIMEOUT1_IN_SECONDS} "http://${archiveIp}:${ARCHIVE_PORT}/gwt/status" >> /dev/null
+# --fail option ensures that, if a server error is returned (ie. 5xx/4xx status code), then the status code (stored in $?) will be non zero. 
+# -L follows redirects
+curl -s -L --fail --connect-timeout ${TIMEOUT1_IN_SECONDS} "http://${archiveIp}:${ARCHIVE_PORT}/gwt/status" >> /dev/null
 if [[ $? -ne 0 ]]
 then
     logger -t archive "first check failed"
-    curl -s --connect-timeout ${TIMEOUT2_IN_SECONDS} "http://${archiveIp}:${ARCHIVE_PORT}/gwt/status" >> /dev/null
+    curl -s -L --fail --connect-timeout ${TIMEOUT2_IN_SECONDS} "http://${archiveIp}:${ARCHIVE_PORT}/gwt/status" >> /dev/null
     if [[ $? -ne 0 ]]
     then
         setFailoverIfNotSet
