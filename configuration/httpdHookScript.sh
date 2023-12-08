31c2a180d93d898f7984fb2d7c1717379896cf0c
Thomas Stokes <thomas.stokes@sap.com>
2023-12-09 00:29:31 +0100

    Revert "Switch to use post" and "Use pre-receive to only notify." commits
    This reverts commits 0261359c395794116fffddd0fc6b73059926a9b4 and
    8fb6e635d8ea51b212840f03905d806b8b89477f

--- configuration/httpdHookScript.sh
+++ configuration/httpdHookScript.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # Purpose: Deployed in a file named "post-receive", in the hooks dir of a bare git repo. Upon push completion, this script runs and
-# triggers an update to all instances' local repos (if the instance has a specific aws tag). A command is ran in the repo
+# triggers an update to all instances' local repos (if the instance has a specific aws tag). A command is ran in the repo 
 # after the merge completes. The user, in which the bare repo and hook are installed, must have aws credentials, that
 # don't need mfa.
-if [[ -f "/home/httpdConf/repo.git/hooks/notify" ]]; then
 
 TAG="TESTPROXY"
 DIR="/etc/httpd"
 COMMAND="sudo service httpd reload"
-# Gets all public ips for the instances with the chosen tag and iterates over the IPs.
-for IP in $(aws ec2 describe-instances --filters Name=tag-key,Values="${TAG}" | jq -r '.Reservations[].Instances[].PublicIpAddress'); do
-        # strictHostKey... means no authenticity check. The sync-repo... script must be installed in the root user's home.
-        ssh -o "StrictHostKeyChecking=no" root@${IP} "cd ~ && ./sync-repo-and-execute-cmd.sh '${DIR}' '${COMMAND}' ";
-done;
+MAIN_NAME="main"
+while read oldrev newrev refname; do
+    # Check if the update is on a branch
+    if [[ $refname == "refs/heads/"* ]]; then
+        branch_name=$(echo $refname | sed "s|refs/heads/\(.*\)$|\1|")
+        echo "Update on branch: $branch_name"
+        if [[ "$branch_name" == "$MAIN_NAME" ]]; then
+            # Gets all public ips for the instances with the chosen tag and iterates over the IPs.
+            for IP in $(aws ec2 describe-instances --filters Name=tag-key,Values="${TAG}" | jq -r '.Reservations[].Instances[].PublicIpAddress'); do
+                # strictHostKey... means no authenticity check. The sync-repo... script must be installed in the root user's home.
+                ssh -o "StrictHostKeyChecking=no" root@${IP} "cd ~ && ./sync-repo-and-execute-cmd.sh '${DIR}' '${COMMAND}' ";
+            done;
+        fi
+    fi
+done
 
-else
-        echo "no notify"
-fi
-rm -rf "/home/httpdConf/repo.git/hooks/notify"
