081a8279ec5bcd321ddbd8ce78190365f2886487
Thomas Stokes <thomas.stokes@sap.com>
2023-12-14 00:39:41 +0100

    b1873: Modify httpd local script to fetch

--- configuration/setupHttpdGitLocal.sh
+++ configuration/setupHttpdGitLocal.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 REMOTE=$1
 cd /etc/httpd
-git status
-if [[ $? -eq 0 ]]; then 
-    rm -rf conf   # Perhaps, this script should instead mv to a backup location
-    rm -rf conf.d
-    git init
-    git branch -m main
-    git remote add origin "${REMOTE}"
-    git checkout main
-else 
-    echo "fail"
-fi
+
+rm -rf conf   # Perhaps, this script should instead mv to a backup location
+rm -rf conf.d
+git init
+git remote add origin "${REMOTE}"
+git fetch -o "StrictHostKeyChecking=no"
+git checkout main
+
