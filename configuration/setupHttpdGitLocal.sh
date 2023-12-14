335c447f0364c0a054a830ca877f9030adc8aba7
Thomas Stokes <thomas.stokes@sap.com>
2023-12-14 11:32:08 +0100

    b1873: remove old git and alter git ssh command

--- configuration/setupHttpdGitLocal.sh
+++ configuration/setupHttpdGitLocal.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 REMOTE=$1
 cd /etc/httpd
-
+rm -rf .git
 rm -rf conf   # Perhaps, this script should instead mv to a backup location
 rm -rf conf.d
 git init
 git remote add origin "${REMOTE}"
-git fetch -o "StrictHostKeyChecking=no"
+GIT_SSH_COMMAND="ssh -A -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"  git fetch 
 git checkout main
 
