f7927009b372fe2f79f9664f5b2dcf710deb73ea
Thomas Stokes <thomas.stokes@sap.com>
2023-12-19 15:42:18 +0100

    Make update-trac-trac-urls add essentials
    The update-trac-trac-urls script only commits the edited file.

--- configuration/update-tractrac-urls-to-archive.sh
+++ configuration/update-tractrac-urls-to-archive.sh
@@ -2,5 +2,6 @@
 GIT_ROOT=/home/wiki/gitwiki
 mongo --quiet "mongodb://dbserver.internal.sapsailing.com:10201/winddb?replicaSet=archive" --eval 'db.TRACTRAC_CONFIGURATIONS.find({}, {TT_CONFIG_JSON_URL : 1}).toArray()' | grep -v ObjectId | jq -r '.[].TT_CONFIG_JSON_URL' | sort -u >"${GIT_ROOT}/configuration/tractrac-json-urls"
 cd "${GIT_ROOT}"
-git commit -m "Updated tractrac-json-urls" -a
+git add "${GIT_ROOT}/configuration/tractrac-json-urls"
+git commit -m "Updated tractrac-json-urls"
 git push
