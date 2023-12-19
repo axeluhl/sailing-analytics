39565ae84272a37a3305a2a67ca33abfd5020aa2
Thomas Stokes <thomas.stokes@sap.com>
2023-12-19 15:32:38 +0100

    b1873: add extra variables to make changes easier

--- configuration/setupDisposableProxy.sh
+++ configuration/setupDisposableProxy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Setup script for Amazon Linux 2.
+# Setup script for Amazon Linux 2. May need to update macro definitions for the archive IP. 
 BEARER_TOKEN=$1
 INSTANCE_IP4=`ec2-metadata -v | cut -f2 -d " "`
 HTTP_LOGROTATE=/etc/logrotate.d/httpd
+GIT_COPY_USER="trac"
+HTTPD_GIT_REPO_IP="18.135.5.168"
+AWS_CREDENTIALS_IP="52.17.217.83"
 # fstab setup
 mkdir /var/log/old
 echo "logfiles.internal.sapsailing.com:/var/log/old   /var/log/old    nfs     tcp,intr,timeo=100,retry=0" >> /etc/fstab
@@ -21,20 +24,21 @@ sed -i 's/#PermitRootLogin yes/PermitRootLogin without-password\nExitOnForwardFa
 # setup other users and crontabs to keep repo updated
 cd /home
 GIT_SSH_COMMAND="ssh -A -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"  git clone ssh://trac@sapsailing.com/home/trac/git
-adduser wiki
-mv git wiki
-scp -o StrictHostKeyChecking=no -r root@sapsailing.com:/home/wiki/.ssh /home/wiki
-## crontab -u wiki /home/wiki/git/configuration/crontabs/crontab-wiki
-ln -s   /home/wiki/git/configuration/syncgit /home/wiki
-chown -R wiki:wiki wiki
+adduser ${GIT_COPY_USER}
+mv git ${GIT_COPY_USER}
+scp -o StrictHostKeyChecking=no -r "root@sapsailing.com:/home/wiki/.ssh" "/home/${GIT_COPY_USER}"  # copies wiki users passwordless keys
+## crontab -u ${GIT_COPY_USER} "/home/${GIT_COPY_USER}/git/configuration/crontabs/users/crontab-wiki-user"
+ln -s "/home/${GIT_COPY_USER}/git/configuration/syncgit" "/home/${GIT_COPY_USER}"
+chown -R "${GIT_COPY_USER}":"${GIT_COPY_USER}" "${GIT_COPY_USER}"
 # setup symbolic links
 cd /usr/local/bin
-ln -s  /home/wiki/git/configuration/update_authorized_keys_for_landscape_managers /usr/local/bin/update_authorized_keys_for_landscape_managers
-ln -s  /home/wiki/git/configuration/update_authorized_keys_for_landscape_managers_if_changed /usr/local/bin/update_authorized_keys_for_landscape_managers_if_changed
-ln -s  /home/wiki/git/configuration/on-site-scripts/paris2024/notify-operators
-ln -s  /home/wiki/git/configuration/sync-repo-and-execute-cmd.sh
-## ln -s  /home/wiki/git/configuration/switchoverArchive.sh 
-ln -s  /home/wiki/git/configuration/crontabs/crontab-reverse-proxy /root/crontab   # make sure to check the correct crontab is used
+ln -s  "/home/${GIT_COPY_USER}/git/configuration/update_authorized_keys_for_landscape_managers" /usr/local/bin/update_authorized_keys_for_landscape_managers
+ln -s  "/home/${GIT_COPY_USER}/git/configuration/update_authorized_keys_for_landscape_managers_if_changed" /usr/local/bin/update_authorized_keys_for_landscape_managers_if_changed
+ln -s  "/home/${GIT_COPY_USER}/git/configuration/on-site-scripts/paris2024/notify-operators"
+ln -s  "/home/${GIT_COPY_USER}/git/configuration/sync-repo-and-execute-cmd.sh" /root
+ln -s  /home/${GIT_COPY_USER}/git/configuration/switchoverArchive.sh 
+ln -s  /home/${GIT_COPY_USER}/git/configuration/crontabs/environments/crontab-reverse-proxy /root/crontab   # make sure to check the correct crontab is used
+cp  /home/${GIT_COPY_USER}/git/configuration/httpd/cgi-bin/reverseProxyHealthcheck.sh /var/www/cgi-bin
 echo $BEARER_TOKEN > /root/ssh-key-reader.token
 crontab /root/crontab
 # add basic test page which won't cause redirect error code if used as a health check.
@@ -43,7 +47,7 @@ cat <<EOF > /var/www/html/index.html
 EOF
 # ensure httpd starts on startup
 systemctl enable httpd
-echo "net.ipv4.ip_conntrac_max = 131072" >> /etc/sysctl.conf   # should I have appended to limits.conf too
+echo "net.ipv4.ip_conntrac_max = 131072" >> /etc/sysctl.conf
 # setup fail2ban
 yum install -y fail2ban
 cat <<EOF > /etc/fail2ban/jail.d/customisation.local
@@ -60,9 +64,9 @@ chkconfig --level 23 fail2ban on
 service fail2ban start
 yum install -y mod_ssl
 # setup mounting of nvme
-ln -s /home/wiki/git/configuration/archive_instance_setup/mountnvmeswap.service  /etc/systemd/system/mountnvmeswap.service
+ln -s "/home/${GIT_COPY_USER}/git/configuration/archive_instance_setup/mountnvmeswap.service"  /etc/systemd/system/mountnvmeswap.service
 
-## ln -s /home/wiki/git/configuration/archive_instance_setup/mountnvmeswap /usr/local/bin/mountnvmeswap
+## ln -s "/home/${GIT_COPY_USER}/git/configuration/archive_instance_setup/mountnvmeswap" /usr/local/bin/mountnvmeswap
 ln -s /root/mountnvmeswap /usr/local/bin/mountnvmeswap
 source /root/.bashrc
 ./mountnvmeswap
@@ -70,7 +74,7 @@ source /root/.bashrc
 mkdir /var/log/logrotate-target
 echo "Patching $HTTP_LOGROTATE so that old logs go to /var/log/old/$INSTANCE_IP4" >>/var/log/sailing.out
 rm $HTTP_LOGROTATE
-ln -s /home/wiki/git/configuration/logrotate-httpd /etc/logrotate.d/httpd
+ln -s "/home/${GIT_COPY_USER}/git/configuration/logrotate-httpd /etc/logrotate.d/httpd"
 mkdir --parents "/var/log/old/REVERSE_PROXIES/${INSTANCE_IP4}"
 sed -i -e "s|\/var\/log\/old|\/var\/log\/old\/REVERSE_PROXIES\/${INSTANCE_IP4}|" $HTTP_LOGROTATE 
 # logrotate.conf setup
@@ -84,9 +88,10 @@ rm -rf awscliv2.zip
 cd ~ && sudo ./aws/install
 
 # setup git
-## /home/wiki/git/configuration/setupHttpdGitLocal.sh "httpdConf@18.135.5.168:repo.git"
+## "/home/${GIT_COPY_USER}/git/configuration/setupHttpdGitLocal.sh" "httpdConf@18.135.5.168:repo.git"
 /root/setupHttpdGitLocal.sh "httpdConf@18.135.5.168:repo.git"
-
+# copy key accross
+scp -o StrictHostKeyChecking=no "root@${HTTPD_GIT_REPO_IP}:/root/.ssh/id_ed25519" /root/.ssh
 # certs setup
 cd /etc
 mkdir letsencrypt
