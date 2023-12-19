72aed95cf42fc9d4a0b9d3d1d08648e58d106b5b
Thomas Stokes <thomas.stokes@sap.com>
2023-12-19 15:31:58 +0100

    Add rsync releases+p2 suggestion+aws cred install

--- configuration/setupDisposableProxy.sh
+++ configuration/setupDisposableProxy.sh
@@ -94,5 +94,12 @@ mkdir letsencrypt/live
 mkdir letsencrypt/live/sail-insight.com
 scp -o StrictHostKeyChecking=no -r root@sapsailing.com:/etc/letsencrypt/live/sail-insight.com/* /etc/letsencrypt/live/sail-insight.com
 systemctl start httpd
+#copy aws credentials to apache user
+scp -o StrictHostKeyChecking=no  -r "root@${AWS_CREDENTIALS_IP}:~/.aws"  /usr/share/httpd
+chown -R apache:apache /usr/share/httpd
+# # setup releases
+# rsync -av --delete root@sapsailing.com:/var/www/static/releases /home/trac
+# # setup p2
+# rsync -av --delete trac@sapsailing.com:p2-repositories /home/trac
 
 
