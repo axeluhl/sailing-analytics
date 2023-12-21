88edaaba0097cade15b2dab194d0b27ab1527d31
Thomas Stokes <thomas.stokes@sap.com>
2023-12-21 09:41:31 +0000

    b5938: Add notify operators to update_tra...urls

--- configuration/update-tractrac-urls-to-archive.sh
+++ configuration/update-tractrac-urls-to-archive.sh
@@ -3,9 +3,10 @@ GIT_ROOT=/home/wiki/gitwiki
 PATH_TO_TRAC_TRAC_URLS="configuration/tractrac-json-urls"
 urls=$(mongo --quiet "mongodb://dbserver.internal.sapsailing.com:10201/winddb?replicaSet=archive" --eval 'db.TRACTRAC_CONFIGURATIONS.find({}, {TT_CONFIG_JSON_URL : 1}).toArray()' | grep -v ObjectId | jq -r '.[].TT_CONFIG_JSON_URL' )
 if [[ $urls == "null" ]]; then
+    echo "Mongo db returns null for tractrac url discovery" | notify-operators "MongoDB/tractrac urls issue"
     exit 1
 else 
-   echo ${urls} | sort -u >"${GIT_ROOT}/${PATH_TO_TRAC_TRAC_URLS}"
+    echo ${urls} | sort -u >"${GIT_ROOT}/${PATH_TO_TRAC_TRAC_URLS}"
 fi
 cd "${GIT_ROOT}"
 git add "${GIT_ROOT}/${PATH_TO_TRAC_TRAC_URLS}"
