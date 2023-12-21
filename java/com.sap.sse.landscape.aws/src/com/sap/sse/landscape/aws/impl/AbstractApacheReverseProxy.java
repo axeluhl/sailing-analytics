fc58adbfd82b4547dfce9694cc2a98b21eec6d98
Thomas Stokes <thomas.stokes@sap.com>
2023-12-21 17:20:34 +0000

    b1873: Update internal server status

--- java/com.sap.sse.landscape.aws/src/com/sap/sse/landscape/aws/impl/AbstractApacheReverseProxy.java
+++ java/com.sap.sse.landscape.aws/src/com/sap/sse/landscape/aws/impl/AbstractApacheReverseProxy.java
@@ -9,7 +9,7 @@ import com.sap.sse.landscape.aws.ReverseProxy;
 public abstract class AbstractApacheReverseProxy<ShardingKey, MetricsT extends ApplicationProcessMetrics,
 ProcessT extends ApplicationProcess<ShardingKey, MetricsT, ProcessT>>
 implements ReverseProxy<ShardingKey, MetricsT, ProcessT, RotatingFileBasedLog> {
-    protected static final String INTERNAL_SERVER_STATUS = "internal-server-status";
+    protected static final String INTERNAL_SERVER_STATUS = "/cgi-bin/reverseProxyHealthcheck.sh";
     private final AwsLandscape<ShardingKey> landscape;
     
     public AbstractApacheReverseProxy(AwsLandscape<ShardingKey> landscape) {
