8c85e1e861097e6136588a4bbdae1b66b86fbfe4
Thomas Stokes <thomas.stokes@sap.com>
2024-01-08 15:28:39 +0100

    mailinglist script only uses dir and not user

--- configuration/update_landscape_managers_mailing_list.sh
+++ configuration/update_landscape_managers_mailing_list.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Purpose: Create a mailing list in home_dir/mailinglists/landscapeManagersMailingList that contains the emails of all landscape managers, ie. those who
+# Purpose: Create a mailing list in PATH_TO_STORE/landscapeManagersMailingList that contains the emails of all landscape managers, ie. those who
 # have complete admin privileges in the aws environment.
 BEARER_TOKEN="$1"
-HOME_DIR="$2"
-FOLDER_WITHIN_HOME="mailinglists"
+PATH_TO_STORE="$2"
 NAME_TO_STORE_IN="landscapeManagersMailingList"
 BASE_URL="https://security-service.sapsailing.com"
 curl_output=$( curl -H 'X-SAPSSE-Forward-Request-To: master' -H 'Authorization: Bearer '${BEARER_TOKEN} "${BASE_URL}/security/api/restsecurity/users_with_permission?permission=LANDSCAPE:MANAGE:AWS" 2>/dev/null )
-if [[ -f "${HOME_DIR}/${FOLDER_WITHIN_HOME}/${NAME_TO_STORE_IN}" ]]; then
-    mv -f ${HOME_DIR}/${FOLDER_WITHIN_HOME}/${NAME_TO_STORE_IN} ${HOME_DIR}/${FOLDER_WITHIN_HOME}/${NAME_TO_STORE_IN}.bak
+if [[ -f "${PATH_TO_STORE}/${NAME_TO_STORE_IN}" ]]; then
+    mv -f ${PATH_TO_STORE}/${NAME_TO_STORE_IN} ${PATH_TO_STORE}/${NAME_TO_STORE_IN}.bak
 fi
-touch ${HOME_DIR}/${FOLDER_WITHIN_HOME}/${NAME_TO_STORE_IN}
+touch ${PATH_TO_STORE}/${NAME_TO_STORE_IN}
 echo $curl_output | jq -r .[] | while read user; do
     email=$(curl -H 'X-SAPSSE-Forward-Request-To: master' -H 'Authorization: Bearer '${BEARER_TOKEN} "${BASE_URL}/security/api/restsecurity/user?username=$user" | jq -r '.email')
-    echo $email >> ${HOME_DIR}/${FOLDER_WITHIN_HOME}/${NAME_TO_STORE_IN}
+    echo $email >> ${PATH_TO_STORE}/${NAME_TO_STORE_IN}
 done
