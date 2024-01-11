935520f978c4cb129fa7e6a5a1daa00350c9d14e
Axel Uhl <axel.uhl@sap.com>
2024-01-11 11:54:02 +0100

    added missing -y option to apt commands for rabbit set-up

--- configuration/rabbitmq_instance_setup/setup-rabbitmq-server.sh
+++ configuration/rabbitmq_instance_setup/setup-rabbitmq-server.sh
@@ -18,9 +18,9 @@ else
   sudo chgrp admin /home/admin/ssh-key-reader.token
   sudo chmod 600 /home/admin/ssh-key-reader.token
   # Install packages for MariaDB and cron/anacron/crontab:
-  sudo apt-get update
-  sudo apt-get upgrade
-  sudo apt-get install rabbitmq-server systemd-cron
+  sudo apt-get -y update
+  sudo apt-get -y upgrade
+  sudo apt-get -y install rabbitmq-server systemd-cron
   crontab /home/admin/crontab
   # Wait for RabbitMQ to become available; note that install under apt also means start...
   sleep 10
