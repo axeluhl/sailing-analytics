cced40a33ed0878d5919335c8f2d0e453718b772
Thomas <trac@ip-172-31-21-237.eu-west-2.compute.internal>
2024-01-12 12:47:55 +0000

    b5938: setup image_upgrade functions and sailing server

--- configuration/imageupgrade.sh
+++ configuration/imageupgrade.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-# Upgrades the AWS EC2 instance that this script is assumed to be executed on.
-# The steps are as follows:
-
-. `dirname $0`/imageupgrade_functions.sh
-
-run_yum_update
-run_git_pull
-download_and_install_latest_sap_jvm_8
-clean_logrotate_target
-clean_httpd_logs
-clean_servers_dir
-clean_startup_logs
-update_root_crontab
-finalize
+environments_scripts/sailing_server/files/usr/local/bin/imageupgrade.sh
