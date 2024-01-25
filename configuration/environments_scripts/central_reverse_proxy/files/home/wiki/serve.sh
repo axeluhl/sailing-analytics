251dd477e6a0331c3c88822d63ed759dd5fc2397
Thomas Stokes <thomas.stokes@sap.com>
2024-01-25 15:05:51 +0100

    b5940: Use sigterm and add stop parameter

--- configuration/environments_scripts/central_reverse_proxy/files/home/wiki/serve.sh
+++ configuration/environments_scripts/central_reverse_proxy/files/home/wiki/serve.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # kill any running gollums
-kill -9 `ps axlw | grep rackup | grep ruby | awk '{ print $3; }'`
+command="ps axlw | grep rackup | grep ruby | awk '{ print \$3; }'"
+kill -SIGTERM $(eval ${command})
+sleep 10
+if [[ -n "$(eval ${command})" ]]; then
+  echo "Not terminated gracefully"
+  kill -SIGKILL $(eval ${command})
+fi
+if [[ $# -eq 1 && "$1" == "stop" ]]; then
+  echo "Stop command in parameter"
+  exit 0
+fi
 # start gollum as a background process
 # you can pipe output to /dev/null instead, if you don't want a log
 cd /home/wiki
