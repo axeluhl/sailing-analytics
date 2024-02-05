c8b3fc3f45cbd703507ed405e466bbab095cd3ea
Thomas Stokes <thomas.stokes@sap.com>
2024-02-05 21:59:51 +0100

    b1873: Add build-crontab to image_upgrade_func..

--- configuration/environments_scripts/repo/usr/local/bin/imageupgrade_functions.sh
+++ configuration/environments_scripts/repo/usr/local/bin/imageupgrade_functions.sh
@@ -78,6 +78,18 @@ update_root_crontab() {
   crontab crontab
 }
 
+build_crontab() {
+    PATH_TO_WIKI_KEY="$1"
+    if [[ -n "$PATH_TO_WIKI_KEY" ]]; then 
+        scp -i "${PATH_TO_WIKI_KEY}"-o StrictHostKeyChecking=no -r "wiki@sapsailing.com:~/gitwiki/configuration/environments_scripts" /root
+        cd /root/
+        chown root:root environments_scripts
+        cd environments_scripts
+        ./build_crontab "${BUILD_TYPE}" "${GIT_USER}" "${RELATIVE_PATH_TO_GIT}"
+        rm -rf /root/environments_scripts
+    fi
+}
+
 clean_root_ssh_dir_and_tmp() {
   echo "Cleaning up ${LOGON_USER_HOME}/.ssh" >>/var/log/sailing.err
   rm -rf ${LOGON_USER_HOME}/.ssh/*
