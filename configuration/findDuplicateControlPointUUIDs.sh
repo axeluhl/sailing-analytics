45b992660fe6865020841b87545c54ed117dffc2
Generic Wiki User <wiki@sapsailing.com>
2024-02-13 23:35:31 +0000

    fixed findDuplicateControlPointUUIDs.sh script

--- configuration/findDuplicateControlPointUUIDs.sh
+++ configuration/findDuplicateControlPointUUIDs.sh
@@ -6,7 +6,7 @@ find "${BASE_FOLDER}" -name '*.txt' | while read FILE; do
   echo "Analyzing marks of event ${EVENT} ..."
   UUIDs=$( cat "${FILE}" \
    | grep "\(^ControlPoint[0-9]*UUID:\)\|\(ControlPoint[0-9]*DataSheet:.*###P[12]\.UUID=\)" \
-   | sed -e 's/^ControlPoint[0-9]*UUID://' -e 's/ControlPoint[0-9]*DataSheet:.*P1\.UUID=\([^#]*\).*P2\.UUID=\([^#]*\).*$/\1 \2/' )
+   | sed -e 's/^ControlPoint[0-9]*UUID://' -e 's/ControlPoint[0-9]*DataSheet:.*P1\.UUID=\([^#]*\).*\(P2\.UUID=\([^#]*\)\)\?.*$/\1 \3/' )
   for UUID in ${UUIDs}; do
     UUID_FILE="${TMP_DIR}/${UUID}"
     if [ -f "${UUID_FILE}" ]; then
@@ -18,10 +18,10 @@ find "${BASE_FOLDER}" -name '*.txt' | while read FILE; do
     fi
   done
 done
-for UUID_FILE in $( find "${TMP_DIR}" -type f ); do
-  NUMBER_OF_EVENTS_FOR_UUID=$( wc -l "${UUID_FILE}" )
-  if [ ${NUMBER_OF_EVENTS_FOR_UUID} != 1 ]; then
-    echo "ControlPoint ID ${UUID} used in multiple events: $( cat "${UUID_FILE}" | tr '\n' ' ' )" >&2
+find "${TMP_DIR}" -type f | while read UUID_FILE; do
+  NUMBER_OF_EVENTS_FOR_UUID=$( cat "${UUID_FILE}" | wc -l )
+  if [ "${NUMBER_OF_EVENTS_FOR_UUID}" != "1" ]; then
+    echo "ControlPoint ID $( basename ${UUID_FILE} ) used in multiple events: $( cat "${UUID_FILE}" | tr '\n' ' ' )" >&2
   fi
 done
 rm -rf "${TMP_DIR}"
