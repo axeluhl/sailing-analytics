5da16d7a5c2f010fb27ac45dfe6c13ded68b17f7
Thomas <sailing@ip-172-31-8-116.eu-west-2.compute.internal>
2024-02-29 14:43:52 +0000

    b1873: Fix minor typos in setupHttpdGit, add new imageupgrade functions and make some comments
    I also improved the reverseProxyHealthcheck to use subnet masks. This will be removed by b1873 but hopefully readded after further development.

--- configuration/environments_scripts/reverse_proxy/files/root/setupHttpdGitLocal.sh
+++ configuration/environments_scripts/reverse_proxy/files/root/setupHttpdGitLocal.sh
@@ -3,8 +3,7 @@ REMOTE=$1
 STATUS_DEFINITION_FILE="internal-server-status.conf"
 SELF_IP=$( ec2-metadata --local-ipv4 | grep   -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\>")
 cd /etc/httpd
-git status
-if [[ "$?" -ne 0 ]]; then
+if ! git status; then
     echo "Currently not a git repo, creating and pulling latest changes"
     rm -rf .git
     rm -rf conf   # Perhaps, this script should instead mv to a backup location
@@ -16,5 +15,3 @@ if [[ "$?" -ne 0 ]]; then
     git checkout main
 fi
 echo "Use Status ${SELF_IP} internal-server-status" > /etc/httpd/conf.d/${STATUS_DEFINITION_FILE}
-
-
