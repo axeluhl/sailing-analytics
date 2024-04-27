f4557efe054d21aa39ce401b9a075d8c6f413e25
Thomas Stokes <thomas.stokes@sap.com>
2024-04-27 10:19:26 +0200

    b5912: rename and alter add-to-necessary-target-groups.
    It passes basic testing.

--- configuration/environments_scripts/central_reverse_proxy/files/root/add-to-necessary-target-groups-and-setup-route53.sh
+++ configuration/environments_scripts/central_reverse_proxy/files/root/add-to-necessary-target-groups-and-setup-route53.sh
@@ -4,13 +4,15 @@ LOCAL_IPV4=$(ec2-metadata --local-ipv4 | sed "s/local-ipv4: *//")
 INSTANCE_ID=$(ec2-metadata --instance-id | sed "s/instance-id: *//")
 nlbArn=$(aws elbv2 describe-tags --resource-arns $(echo "$target_groups" | jq -r '.TargetGroups | .[] | select(.LoadBalancerArns | .[] | contains("loadbalancer/net")  ) | .TargetGroupArn') | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="allReverseProxies") ) | .ResourceArn')
 aws elbv2 register-targets --target-group-arn "$nlbArn"  --targets Id="${LOCAL_IPV4}",Port=80
-for tgArn in $(aws elbv2 describe-tags --resource-arns $( echo "$target_groups" | jq -r '.TargetGroups | .[] | .TargetGroupArn') | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="allReverseProxies") ) | .ResourceArn'); do
+describe_tags=$(aws elbv2 describe-tags --resource-arns $(echo "$target_groups" | jq -r '.TargetGroups | .[] | .TargetGroupArn'))
+for tgArn in $(echo "$describe_tags" | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="allReverseProxies") ) | .ResourceArn'); do
     # adding to all groups tagged as reverse proxy (via allReverseProxies), except the NLB as it is registered as an IP target above.
     if [[ "$tgArn" != "$nlbArn" ]]; then
-        aws elbv2 register-targets --targget-group-arns "$tgArn" --targets Id="${INSTANCE_ID}"
+        aws elbv2 register-targets --target-group-arn "$tgArn" --targets Id="${INSTANCE_ID}"
     fi
 done
-for tgArn in $(aws elbv2 describe-tags --resource-arns $(echo "$target_groups" | jq -r '.TargetGroups | .[] | .TargetGroupArn') | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="CentralReverseProxy") ) | .ResourceArn'); do
+for tgArn in $(echo "$describe_tags"  | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="CentralReverseProxy") ) | .ResourceArn'); do
     # adding to all groups tagged as central reverse proxy only (via CentralReverseProxy).
-    aws elbv2 register-targets --targget-group-arns "$tgArn" --targets Id="${INSTANCE_ID}"
+    aws elbv2 register-targets --target-group-arn "$tgArn" --targets Id="${INSTANCE_ID}"
 done
+###### DO NOT ENABLE WHILST TESTING: aws route53 change-resource-record-sets --hosted-zone-id Z2JYWXYWLLRLTE --change-batch file://batch.json
