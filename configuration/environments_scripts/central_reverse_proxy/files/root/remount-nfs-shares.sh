addf2dd6f25a6c188a8ee8c81e7e0ac468487a1c
Thomas Stokes <thomas.stokes@sap.com>
2024-04-29 18:07:32 +0200

    b5912: Improve remount nfs script, by factoring out repeated code.

--- configuration/environments_scripts/central_reverse_proxy/files/root/remount-nfs-shares.sh
+++ configuration/environments_scripts/central_reverse_proxy/files/root/remount-nfs-shares.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-for instanceIp in $( aws ec2 describe-instances --filter Name=tag-key,Values=sailing-analytics-server | jq -r '.Reservations | .[] | .Instances | .[] | .PublicIpAddress' | grep -v null); do
+extract_public_ip() {
+    jq -r ' .Instances | .[] | .PublicIpAddress' | grep -v null
+}
+select_instances_by_tag() {
+    # $1: tag
+    jq -r '.Reservations | .[] | select(.Instances | .[] | .Tags| any (.Key=="'"$1"'"))'
+}
+echo "Describing instances"
+describe_instances=$(aws ec2 describe-instances)
+for instanceIp in $(echo "$describe_instances"  | select_instances_by_tag  "sailing-analytics-server" | extract_public_ip); do
+    echo "Remounting on $instanceIp"
     ssh root@"${instanceIp}"  "umount -l -f /var/log/old; umount -l -f /home/scores;  mount -a"
 done
-for instanceIp in $( aws ec2 describe-instances --filter Name=tag-key,Values=DisposableProxy | jq -r '.Reservations | .[] | .Instances | .[] | .PublicIpAddress' | grep -v null); do
-    ssh root@"${instanceIp}"  "umount -l -f /var/log/old mount -a"
+for instanceIp in $(echo "$describe_instances"  | select_instances_by_tag  "DisposableProxy" | extract_public_ip); do
+    echo "Remounting on $instanceIp"
+    ssh root@"${instanceIp}"  "umount -l -f /var/log/old; mount -a"
 done
