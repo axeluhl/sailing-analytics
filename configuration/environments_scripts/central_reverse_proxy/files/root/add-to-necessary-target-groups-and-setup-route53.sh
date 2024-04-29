90794c29187ed41571c43e877d2d6ed3a515aa7b
Thomas Stokes <thomas.stokes@sap.com>
2024-04-29 18:11:16 +0200

    b5912: Improve add-to-necessary target groups by factoring out repetition.

--- configuration/environments_scripts/central_reverse_proxy/files/root/add-to-necessary-target-groups-and-setup-route53.sh
+++ configuration/environments_scripts/central_reverse_proxy/files/root/add-to-necessary-target-groups-and-setup-route53.sh
@@ -2,17 +2,21 @@
 target_groups=$(aws elbv2 describe-target-groups)
 LOCAL_IPV4=$(ec2-metadata --local-ipv4 | sed "s/local-ipv4: *//")
 INSTANCE_ID=$(ec2-metadata --instance-id | sed "s/instance-id: *//")
+TAGS=("allReverseProxies" "CentralReverseProxy")
+# The nlb is the exception case as we use the load balancer arn to further identify it.
 nlbArn=$(aws elbv2 describe-tags --resource-arns $(echo "$target_groups" | jq -r '.TargetGroups | .[] | select(.LoadBalancerArns | .[] | contains("loadbalancer/net")  ) | .TargetGroupArn') | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="allReverseProxies") ) | .ResourceArn')
+echo "Registering with nlb"
 aws elbv2 register-targets --target-group-arn "$nlbArn"  --targets Id="${LOCAL_IPV4}",Port=80
+echo "Fetching tags"
 describe_tags=$(aws elbv2 describe-tags --resource-arns $(echo "$target_groups" | jq -r '.TargetGroups | .[] | .TargetGroupArn'))
-for tgArn in $(echo "$describe_tags" | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="allReverseProxies") ) | .ResourceArn'); do
-    # adding to all groups tagged as reverse proxy (via allReverseProxies), except the NLB as it is registered as an IP target above.
-    if [[ "$tgArn" != "$nlbArn" ]]; then
-        aws elbv2 register-targets --target-group-arn "$tgArn" --targets Id="${INSTANCE_ID}"
-    fi
-done
-for tgArn in $(echo "$describe_tags"  | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="CentralReverseProxy") ) | .ResourceArn'); do
-    # adding to all groups tagged as central reverse proxy only (via CentralReverseProxy).
-    aws elbv2 register-targets --target-group-arn "$tgArn" --targets Id="${INSTANCE_ID}"
+for tag in "${tags[@]}"; do
+    echo "Adding to target groups with $tag"
+    for tgArn in $(echo "$describe_tags" | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="'"$tag"'") ) | .ResourceArn'); do
+        if [[ "$tgArn" != "$nlbArn" ]]; then
+            echo "Registering in $tgArn as it has the correct tag"
+            aws elbv2 register-targets --target-group-arn "$tgArn" --targets Id="${INSTANCE_ID}"
+            [[ "$?" -eq 0 ]] || echo "Register target not successful"
+        fi
+    done
 done
 ###### DO NOT ENABLE WHILST TESTING: aws route53 change-resource-record-sets --hosted-zone-id Z2JYWXYWLLRLTE --change-batch file://batch.json
