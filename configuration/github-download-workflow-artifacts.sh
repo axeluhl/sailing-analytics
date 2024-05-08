91b8c4cbe5b1f546510d1124f0f2434d1b5b48b9
Axel Uhl <axel.uhl@sap.com>
2024-05-08 13:02:38 +0200

    github-test: script to download and unpack workflow artifacts

---
+++ configuration/github-download-workflow-artifacts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BRANCH="${1}"
+BEARER_TOKEN="${2}"
+# Get the artifacts URL of the last workflow run triggered by a branch push for ${BRANCH}:
+ARTIFACTS_URL=$( curl --silent -L -H 'Authorization: Bearer '${BEARER_TOKEN} https://api.github.com/repos/SAP/sailing-analytics/actions/runs 2>/dev/null | jq -r '.workflow_runs | map(select(.head_branch | startswith("'${BRANCH}'"))) | sort_by(.created_at) | reverse | .[0].artifacts_url' )
+ARTIFACTS_JSON=$( curl --silent -H 'Authorization: Bearer '${BEARER_TOKEN} "${ARTIFACTS_URL}" )
+BUILD_LOG_URL=$( echo "${ARTIFACTS_JSON}" | jq -r '.artifacts | map(select(.name == "build.log"))[0].archive_download_url' )
+echo "Downloading build.log ZIP from ${BUILD_LOG_URL}"
+curl --silent --output build.log.zip -L -H 'Authorization: Bearer '${BEARER_TOKEN} "${BUILD_LOG_URL}"
+TEST_RESULTS_URL=$( echo "${ARTIFACTS_JSON}" | jq -r '.artifacts | map(select(.name == "test-results"))[0].archive_download_url' )
+echo "Downloading test-results ZIP from ${TEST_RESULTS_URL}"
+curl --silent --output test-results.zip -L -H 'Authorization: Bearer '${BEARER_TOKEN} "${TEST_RESULTS_URL}"
+
--- configuration/github-scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-BRANCH="${1}"
-BEARER_TOKEN="${2}"
-# Find the last workflow run triggered by a branch push for ${BRANCH}:
-curl -L -H 'Authorization: Bearer '${BEARER_TOKEN} https://api.github.com/repos/SAP/sailing-analytics/actions/runs 2>/dev/null | jq '.workflow_runs | map(select(.head_branch | startswith("'${BRANCH}'"))) | sort_by(.created_at) | reverse | .[0]'
