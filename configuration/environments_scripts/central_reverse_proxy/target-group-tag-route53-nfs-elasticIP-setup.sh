9fe9840581e990143d9a1969f8020372ede65a69
Thomas Stokes <thomas.stokes@sap.com>
2024-05-13 14:03:52 +0200

    b5912: Fix max 20 resources for describe tags issue.

--- configuration/environments_scripts/central_reverse_proxy/target-group-tag-route53-nfs-elasticIP-setup.sh
+++ configuration/environments_scripts/central_reverse_proxy/target-group-tag-route53-nfs-elasticIP-setup.sh
@@ -31,7 +31,7 @@ nlbArn=$(aws elbv2 describe-tags --resource-arns $(echo "$target_groups" | jq -r
 echo "Registering with nlb"
 aws elbv2 register-targets --target-group-arn "$nlbArn"  --targets Id="${LOCAL_IPV4}",Port=80
 echo "Fetching tags"
-describe_tags=$(aws elbv2 describe-tags --resource-arns $(echo "$target_groups" | jq -r '.TargetGroups | .[] | .TargetGroupArn'))
+describe_tags=$(aws elbv2 describe-tags --resource-arns $(echo "$target_groups" | jq -r '.TargetGroups | .[] | select(.TargetGroupName | startswith("S-") | not ) | .TargetGroupArn'))
 for tag in "${TARGET_GROUP_TAGS[@]}"; do
     echo "Adding to target groups with $tag"
     for tgArn in $(echo "$describe_tags" | jq -r '.TagDescriptions | .[] | select(.Tags | any(.Key=="'"$tag"'") ) | .ResourceArn'); do
@@ -45,6 +45,7 @@ done
 # alter records using batch file.
 sed -i "s/LOGFILES_INTERNAL_IP/$LOCAL_IPV4/" batch-for-route53-dns-record-update.json
 sed -i "s/SMTP_INTERNAL_IP/$LOCAL_IPV4/" batch-for-route53-dns-record-update.json
+read -n 1  -p "Check the instance has the correct tags and is in the correct target group. Furthermore, check the batch file has been modified correctly. Press a key to continue.." key_pressed
 ###### DO NOT ENABLE WHILST TESTING: aws route53 change-resource-record-sets --hosted-zone-id Z2JYWXYWLLRLTE --change-batch file://batch-for-route53-dns-record-update.json
 # reload the nfs mountpoints.
 echo "Describing instances for remounting."
