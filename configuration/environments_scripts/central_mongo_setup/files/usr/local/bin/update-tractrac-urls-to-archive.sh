288277fad4b1c37d2cdb072147536b9691bd9599
Thomas Stokes <thomas.stokes@sap.com>
2024-05-22 11:26:01 +0200

    b5938: Add comment to top of update-trac-trac-urls and reorder check of exit status.

--- configuration/environments_scripts/central_mongo_setup/files/usr/local/bin/update-tractrac-urls-to-archive.sh
+++ configuration/environments_scripts/central_mongo_setup/files/usr/local/bin/update-tractrac-urls-to-archive.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# Fetches the latest trac trac urls from the Mongo archive replica set, currently residing on the central mongo instance, 
+# and commits the changes to the master branch of the main Git. Note that local state is prioritised if there is a conflict.
+# Parameter 1 is an optional absolute path to the Git root. If unspecified, then the default is /home/wiki/gitwiki.
 if [[ $# -eq 0 ]]; then
     GIT_ROOT=/home/wiki/gitwiki
 else
@@ -7,7 +9,7 @@ else
 fi
 PATH_TO_TRAC_TRAC_URLS="configuration/tractrac-json-urls"
 urls=$(mongo --quiet "mongodb://dbserver.internal.sapsailing.com:10201/winddb?replicaSet=archive" --eval 'db.TRACTRAC_CONFIGURATIONS.find({}, {TT_CONFIG_JSON_URL : 1}).toArray()' | grep -v ObjectId | jq -r '.[].TT_CONFIG_JSON_URL' )
-if [[ $urls == "null" || $? -ne 0 ]]; then
+if [[ "$?" -ne 0 || "$urls" == "null" ]]; then
     echo "Mongo db returns null for tractrac url discovery" | notify-operators "MongoDB/tractrac urls issue"
     exit 1
 else
