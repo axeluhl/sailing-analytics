a8488f55fe818d915ebc39e90303b07e6da7a05d
Thomas S <sailing@ip-172-31-39-35.eu-west-2.compute.internal>
2024-06-11 22:47:11 +0000

    b5938: Setup imageupgrade for central_mongo and remove archiveDb from mongo replicas

--- configuration/environments_scripts/central_mongo_setup/files/usr/local/bin/imageupgrade.sh
+++ configuration/environments_scripts/central_mongo_setup/files/usr/local/bin/imageupgrade.sh
@@ -1,1 +1,24 @@
-../../../../../mongo_instance_setup/files/usr/local/bin/imageupgrade.sh
+#!/bin/bash
+
+# Upgrades the AWS EC2 MongoDB instance that this script is assumed to be executed on.
+# The steps are as follows:
+
+. imageupgrade_functions.sh
+
+run_git_pull_root() {
+  echo "Pulling git to /root/code" >>/var/log/sailing.err
+  cd /root/code
+  git pull
+}
+
+clean_mongo_pid() {
+  rm -f /var/run/mongodb/mongod.pid
+}
+
+LOGON_USER_HOME=/home/ec2-user
+
+run_yum_update
+build_crontab_and_setup_files central_mongo_setup root code
+clean_startup_logs
+clean_mongo_pid
+finalize
