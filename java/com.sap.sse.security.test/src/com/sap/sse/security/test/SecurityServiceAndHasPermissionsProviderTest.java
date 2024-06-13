95ed2233d3cf60736801731a278b4cebd2e2175f
Axel Uhl <axel.uhl@sap.com>
2024-06-13 15:33:38 +0200

    fixed workflow file syntax for new skip_tests input

--- java/com.sap.sse.security.test/src/com/sap/sse/security/test/SecurityServiceAndHasPermissionsProviderTest.java
+++ java/com.sap.sse.security.test/src/com/sap/sse/security/test/SecurityServiceAndHasPermissionsProviderTest.java
@@ -1,5 +1,6 @@
 package com.sap.sse.security.test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.function.Function;
@@ -11,15 +12,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sap.sse.common.mail.MailException;
 import com.sap.sse.security.SecurityService;
 import com.sap.sse.security.impl.SecurityServiceImpl;
+import com.sap.sse.security.shared.UserGroupManagementException;
+import com.sap.sse.security.shared.UserManagementException;
 import com.sap.sse.security.shared.UserStoreManagementException;
 import com.sap.sse.security.shared.WildcardPermission;
 import com.sap.sse.security.shared.impl.SecuredSecurityTypes;
+import com.sap.sse.security.shared.impl.User;
 import com.sap.sse.security.shared.subscription.SSESubscriptionPlan;
 import com.sap.sse.security.userstore.mongodb.AccessControlStoreImpl;
 import com.sap.sse.security.userstore.mongodb.PersistenceFactory;
 import com.sap.sse.security.userstore.mongodb.UserStoreImpl;
+import com.sap.sse.security.shared.subscription.Subscription;
+import com.sap.sse.security.shared.subscription.chargebee.ChargebeeSubscription;
 
 public class SecurityServiceAndHasPermissionsProviderTest {
 
@@ -99,4 +106,26 @@ public class SecurityServiceAndHasPermissionsProviderTest {
                 securityService->securityService.hasCurrentUserMetaPermissionWithOwnershipLookup(permission)));
     }
 
+    @Test
+    public void testSubscriptionCancellation() throws UserManagementException, UserGroupManagementException, MailException {
+        final SecurityService securityService = createSecurityServiceWithHasPermissionsProvider();
+        final String testUserName = "testUser";
+        final User user = securityService.createSimpleUser(testUserName, /* email */ null, /* password */ "laeriuayelxuyn", /* fullName */ null, /* company */ null, /* locale */ null, /* validationBaseURL */ null, /* userOwner */ null);
+        assertNotNull(user);
+        // The subscription the user is supposed to have so far:
+        // 
+        // [subscriptionId=16BVIKT7dg6r8BrQI, planId=yearly_premium, customerId=diogocosta470, trialStart=Thu Jan 01
+        // 00:00:00 UTC 1970, trialEnd=Thu Jan 01 00:00:00 UTC 1970, subscriptionStatus=active,
+        // paymentStatus=no_success, invoiceId=286, invoiceStatus=payment_due, transactionType=payment,
+        // transactionStatus=failure, subscriptionCreatedAt=Thu Jun 02 21:44:06 UTC 2022, reoccuringPaymentValue=4999,
+        // currencyCode=USD, cancelledAt=Thu Jan 01 00:00:00 UTC 1970, subscriptionActivatedAt=Thu Jun 02 21:44:06 UTC
+        // 2022, nextBillingAt=Mon Jun 02 21:44:06 UTC 2025, currentTermEnd=Mon Jun 02 21:44:06 UTC 2025,
+        // subscriptionUpdatedAt=Sun Jun 02 21:44:15 UTC 2024, latestEventTime=Tue Jun 11 20:21:47 UTC 2024 (+909ms),
+        // manualUpdatedAt=Thu Jan 01 00:00:00 UTC 1970, providerName=chargebee]
+        final Subscription userSubscription = new ChargebeeSubscription(subscriptionId, planId, customerId, trialStart,
+                trialEnd, subscriptionStatus, paymentStatus, transactionType, transactionStatus, invoiceId,
+                invoiceStatus, reoccuringPaymentValue, currencyCode, subscriptionCreatedAt, subscriptionUpdatedAt,
+                subscriptionActivatedAt, nextBillingAt, currentTermEnd, cancelledAt, latestEventTime, manualUpdatedAt);
+        securityService.updateUserSubscription(testUserName, userSubscription);
+    }
 }
