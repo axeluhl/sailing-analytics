3a17220c71ae7f729574db1c3335d8b03d930d90
Axel Uhl <axel.uhl@sap.com>
2024-06-19 13:44:27 +0200

    bug6031: added overallLeaderboardName / hasOverallLeaderboard fields to REST API

--- java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/jaxrs/api/LeaderboardGroupsResource.java
+++ java/com.sap.sailing.server.gateway/src/com/sap/sailing/server/gateway/jaxrs/api/LeaderboardGroupsResource.java
@@ -147,6 +147,10 @@ public class LeaderboardGroupsResource extends AbstractSailingServerResource {
                 jsonLeaderboardGroup.put(LeaderboardGroupConstants.DESCRIPTION, leaderboardGroup.getDescription());
                 jsonLeaderboardGroup.put(LeaderboardGroupConstants.TIMEPOINT, timePoint.toString());
                 jsonLeaderboardGroup.put(LeaderboardGroupConstants.TIMEPOINT_MILLIS, timePoint.asMillis());
+                jsonLeaderboardGroup.put(LeaderboardGroupConstants.HAS_OVERALL_LEADERBOARD, leaderboardGroup.hasOverallLeaderboard());
+                if (leaderboardGroup.hasOverallLeaderboard()) {
+                    jsonLeaderboardGroup.put(LeaderboardGroupConstants.OVERALL_LEADERBOARD_NAME, leaderboardGroup.getOverallLeaderboardName());
+                }
                 final Set<Event> eventsReferencingLeaderboardGroup = new HashSet<>();
                 final JSONArray idsOfEventsReferencingLeaderboardGroup = new JSONArray();
                 for (final Event event : getService().getAllEvents()) {
