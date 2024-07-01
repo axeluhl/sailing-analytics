2a41010a2326d487596c2c9efe9742eeb2ce85c5
Axel Uhl <axel.uhl@sap.com>
2024-07-01 12:56:57 +0200

    bug6033: try to explicitly specify sdkmanager version 8.0

--- configuration/buildAndUpdateProduct.sh
+++ configuration/buildAndUpdateProduct.sh
@@ -667,7 +667,7 @@ if [[ "$@" == "build" ]] || [[ "$@" == "all" ]]; then
         echo "ANDROID_HOME=$ANDROID_HOME"
         PATH=$PATH:$ANDROID_HOME/tools/bin
         PATH=$PATH:$ANDROID_HOME/platform-tools
-        SDK_MANAGER="$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager"
+        SDK_MANAGER="$ANDROID_HOME/cmdline-tools/8.0/bin/sdkmanager"
         if [ \! -x "$SDK_MANAGER" ]; then
             SDK_MANAGER="$ANDROID_HOME/tools/bin/sdkmanager.bat"
         fi
