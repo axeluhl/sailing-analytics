f3d504549757e1abdceaa7a39de988940a31be81
Axel Uhl <axel.uhl@sap.com>
2024-07-30 21:36:03 +0200

    bug6039: added a failing test case proving that the CrossTrackErrorCache is not registered as listener after de-serialization

---
+++ java/com.sap.sailing.domain.test/src/com/sap/sailing/domain/test/DynamicTrackedRaceImplSerializationTest.java
@@ -0,0 +1,48 @@
+package com.sap.sailing.domain.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.sap.sailing.domain.base.Boat;
+import com.sap.sailing.domain.base.Competitor;
+import com.sap.sailing.domain.base.CompetitorWithBoat;
+import com.sap.sailing.domain.base.DomainFactory;
+import com.sap.sailing.domain.tracking.impl.CrossTrackErrorCache;
+import com.sap.sailing.domain.tracking.impl.DynamicTrackedRaceImpl;
+import com.sap.sailing.domain.tracking.impl.TrackedRaceImpl;
+import com.sap.sse.common.TimePoint;
+
+public class DynamicTrackedRaceImplSerializationTest extends AbstractSerializationTest {
+    @Test
+    public void testCrossTrackErrorCacheStaysListenerThroughSerialization() throws ClassNotFoundException, IOException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        final CompetitorWithBoat competitor = TrackBasedTest.createCompetitorWithBoat("Hasso");
+        final Map<Competitor, Boat> competitorsAndBoats = new HashMap<>();
+        competitorsAndBoats.put(competitor, competitor.getBoat());
+        final DynamicTrackedRaceImpl dtr = TrackBasedTest.createTestTrackedRace("Test", "Test", "470", competitorsAndBoats,
+                /* timePointForFixes */ TimePoint.now(), /* useMarkPassingCalculator */ false);
+        final DynamicTrackedRaceImpl dtrClone = cloneBySerialization(dtr, DomainFactory.INSTANCE);
+        assertNotNull(dtrClone);
+        final Field crossTrackErrorCacheField = TrackedRaceImpl.class.getDeclaredField("crossTrackErrorCache");
+        crossTrackErrorCacheField.setAccessible(true);
+        final CrossTrackErrorCache cted = (CrossTrackErrorCache) crossTrackErrorCacheField.get(dtr);
+        final CrossTrackErrorCache ctedClone = (CrossTrackErrorCache) crossTrackErrorCacheField.get(dtrClone);
+        assertNotNull(cted);
+        assertNotNull(ctedClone);
+        final Method getListeners = DynamicTrackedRaceImpl.class.getDeclaredMethod("getListeners");
+        getListeners.setAccessible(true);
+        final Set<?> dtrListeners = (Set<?>) getListeners.invoke(dtr);
+        final Set<?> dtrCloneListeners = (Set<?>) getListeners.invoke(dtrClone);
+        assertTrue(dtrListeners.contains(cted));
+        assertTrue(dtrCloneListeners.contains(ctedClone));
+    }
+}
