c39fc1f5bc694c8282a011cefc4ac647c9440124
Axel Uhl <axel.uhl@sap.com>
2024-08-22 12:04:15 +0200

    bug5587: introduced WindTrack.add(Iterable) for mass insert

--- java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/Wind.java
+++ java/com.sap.sailing.domain.common/src/com/sap/sailing/domain/common/Wind.java
@@ -4,21 +4,26 @@ import com.sap.sse.common.Bearing;
 import com.sap.sse.common.Timed;
 
 /**
- * Records a wind observation made at a certain position at a given time with an observed speed and a bearing.
- * Note that while nautically wind is usually specified in the direction it's coming <em>from</em>, for
- * computational purposes internally we measure the wind in the direction <em>to</em> which it blows.
- * Use {@link #getFrom} to get the "inverse" bearing.<p>
+ * Records a wind observation made at a certain position at a given time with an observed speed and a bearing. Note that
+ * while nautically wind is usually specified in the direction it's coming <em>from</em>, for computational purposes
+ * internally we measure the wind in the direction <em>to</em> which it blows. Use {@link #getFrom} to get the "inverse"
+ * bearing.
+ * <p>
  * 
- * Note that sometimes a wind measurement is transmitted without its position. While this is useless
- * for wind field interpolation, it can still serve good uses when some wind direction is better than
- * no wind direction at all. In this case, {@link #getPosition()} will return <code>null</code>.<p>
+ * Note that sometimes a wind measurement is transmitted without its position. While this is useless for wind field
+ * interpolation, it can still serve good uses when some wind direction is better than no wind direction at all. In this
+ * case, {@link #getPosition()} will return <code>null</code>.
+ * <p>
  * 
- * When processing wind in the scope of a {@link TrackedRace}, it should always use true, not magnetic,
- * directions. However, during capturing wind, e.g., on a committee boat, sometimes it is easier for
- * users to capture wind using a magnetic direction which needs to be corrected by the magnetic
- * variation / declination in order to produce a true direction. It depends on the context in which
- * objects of this type are produced and used whether this represents a true or magnetic wind direction.
- * For conversion, use a {@link DeclinationService} instance.
+ * When processing wind in the scope of a {@link TrackedRace}, it should always use true, not magnetic, directions.
+ * However, during capturing wind, e.g., on a committee boat, sometimes it is easier for users to capture wind using a
+ * magnetic direction which needs to be corrected by the magnetic variation / declination in order to produce a true
+ * direction. It depends on the context in which objects of this type are produced and used whether this represents a
+ * true or magnetic wind direction. For conversion, use a {@link DeclinationService} instance.
+ * <p>
+ * 
+ * For two {@link Wind} objects to be equal, they need to have equal {@link #getTimePoint() time points},
+ * {@link #getPosition() positions}, as well as wind speeds and directions.
  * 
  * @author Axel Uhl (d043530)
  * 
--- java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/WindListener.java
+++ java/com.sap.sailing.domain/src/com/sap/sailing/domain/tracking/WindListener.java
@@ -7,6 +7,12 @@ import com.sap.sailing.domain.common.Wind;
 public interface WindListener {
     void windDataReceived(Wind wind);
     
+    default void windDataReceived(Iterable<Wind> winds) {
+        for (final Wind wind : winds) {
+            windDataReceived(wind);
+        }
+    }
+    
     void windDataRemoved(Wind wind);
 
     void windAveragingChanged(long oldMillisecondsOverWhichToAverage, long newMillisecondsOverWhichToAverage);
