8b456cfb488d97b65ce17776f7f3c74d8337dd56
Axel Uhl <axel.uhl@sap.com>
2024-09-16 21:28:17 +0200

    bug6057: added missing message interfaces

---
+++ java/com.sap.sailing.domain.queclinkadapter/src/com/sap/sailing/domain/queclinkadapter/BATReport.java
@@ -0,0 +1,5 @@
+package com.sap.sailing.domain.queclinkadapter;
+
+public interface BATReport extends Report {
+
+}
---
+++ java/com.sap.sailing.domain.queclinkadapter/src/com/sap/sailing/domain/queclinkadapter/IGFReport.java
@@ -0,0 +1,5 @@
+package com.sap.sailing.domain.queclinkadapter;
+
+public interface IGFReport extends Report {
+
+}
