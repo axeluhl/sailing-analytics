c23f0aa9e7f83f900fe931706cee4f9ccfdf0510
Axel Uhl <axel.uhl@sap.com>
2024-11-18 17:27:31 +0100

    disable firewalld after enabling fail2ban; then use only start for fail2ban

--- configuration/environments_scripts/repo/usr/local/bin/imageupgrade_functions.sh
+++ configuration/environments_scripts/repo/usr/local/bin/imageupgrade_functions.sh
@@ -309,19 +309,19 @@ setup_fail2ban() {
     # Expects setup_mail_sending to have been invoked for fail2ban e-mails being sent properly
     sudo dnf install -y fail2ban whois
     sudo sed -i 's|^backend *= *auto *$|backend = systemd|' /etc/fail2ban/jail.conf
-    sudo systemctl enable fail2ban
     # The fail2ban service may depend on firewalld which then gets installed and
     # injects all sorts of unwanted nftables/iptables rules that can make our services
     # unreachable (MongoDB port 27017, MariaDB port 3306, application server port 8888, etc.).
     # We use security groups in our VPC to control which ports can be reached from where.
     # Therefore, we disable firewalld, should it have been installed
     sudo systemctl disable firewalld
+    sudo systemctl enable fail2ban
     # the /etc/fail2ban/jail.d/ contents are expected to be provided by the files/etc/fail2ban/jail.d
     # folders in the respective environments_scripts sub-folder; use, e.g., a symbolic link to
     # configuration/environments_scripts/repo/etc/fail2ban/jail.d/customisation.local for a
     # systemd-based sshd-iptables filter.
     sudo touch /var/log/fail2ban.log
-    sudo systemctl restart fail2ban
+    sudo systemctl start fail2ban
 }
 
 setup_mail_sending() {
