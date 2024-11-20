561f656a43fccb5f342573ce42356a2f93880ffb
Axel Uhl <axel.uhl@sap.com>
2024-11-20 01:11:00 +0100

    fixed security and session handling in Data Mining;
    so far, user sessions were not properly propagated to the executor tasks
    / instructions used to run in particular the data retrieval process.
    With this, checking for the current subject/user session was a random
    process. This is now fixed by attaching the subject to each instruction
    before handing it to the executor

--- java/com.sap.sailing.polars.datamining/src/com/sap/sailing/polars/datamining/components/PolarLeaderboardGroupRetrievalProcessor.java
+++ java/com.sap.sailing.polars.datamining/src/com/sap/sailing/polars/datamining/components/PolarLeaderboardGroupRetrievalProcessor.java
@@ -5,12 +5,16 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
 import com.sap.sailing.datamining.data.HasLeaderboardGroupContext;
 import com.sap.sailing.datamining.impl.data.LeaderboardGroupWithContext;
 import com.sap.sailing.domain.leaderboard.LeaderboardGroup;
 import com.sap.sailing.server.interfaces.RacingEventService;
 import com.sap.sse.datamining.components.Processor;
 import com.sap.sse.datamining.impl.components.AbstractRetrievalProcessor;
+import com.sap.sse.security.shared.HasPermissions.DefaultActions;
 
 public class PolarLeaderboardGroupRetrievalProcessor extends AbstractRetrievalProcessor<RacingEventService, HasLeaderboardGroupContext> {
     public PolarLeaderboardGroupRetrievalProcessor(ExecutorService executor,
@@ -27,7 +31,10 @@ public class PolarLeaderboardGroupRetrievalProcessor extends AbstractRetrievalPr
             if (isAborted()) {
                 break;
             }
-            data.add(new LeaderboardGroupWithContext(leaderboardGroup, element.getPolarDataService(), element.getBaseDomainFactory(), element.getSecurityService()));
+            final Subject subject = SecurityUtils.getSubject();
+            if (subject.isPermitted(leaderboardGroup.getIdentifier().getStringPermission(DefaultActions.READ))) {
+                data.add(new LeaderboardGroupWithContext(leaderboardGroup, element.getPolarDataService(), element.getBaseDomainFactory(), element.getSecurityService()));
+            }
         }
         return data;
     }
