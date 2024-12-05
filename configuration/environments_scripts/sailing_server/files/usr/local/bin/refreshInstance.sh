328a8664989ab8f83a2f1d09db6e46eb8e56cc08
Axel Uhl <axel.uhl@sap.com>
2024-12-05 13:37:44 +0100

    using new generateMailProperties.sh to support "start docker"

--- configuration/environments_scripts/sailing_server/files/usr/local/bin/refreshInstance.sh
+++ configuration/environments_scripts/sailing_server/files/usr/local/bin/refreshInstance.sh
@@ -296,26 +296,7 @@ auto_install ()
         append_default_envsh_rules
         # make sure to reload data, this time including defaults from release's env.sh, environment settings and user data
         source `pwd`/env.sh
-        # Append mail-related environment variables to configuration/mail.properties to override defaults
-        echo "mail.enabled = true" >>configuration/mail.properties
-        if [ -n "$MAIL_FROM" ]; then
-          echo "mail.from = $MAIL_FROM" >>configuration/mail.properties
-        fi
-        if [ -n "$MAIL_SMTP_HOST" ]; then
-          echo "mail.smtp.host = $MAIL_SMTP_HOST" >>configuration/mail.properties
-        fi
-        if [ -n "$MAIL_SMTP_PORT" ]; then
-          echo "mail.smtp.port = $MAIL_SMTP_PORT" >>configuration/mail.properties
-        fi
-        if [ -n "$MAIL_SMTP_AUTH" ]; then
-          echo "mail.smtp.auth = $MAIL_SMTP_AUTH" >>configuration/mail.properties
-        fi
-        if [ -n "$MAIL_SMTP_USER" ]; then
-          echo "mail.smtp.user = $MAIL_SMTP_USER" >>configuration/mail.properties
-        fi
-        if [ -n "$MAIL_SMTP_PASSWORD" ]; then
-          echo "mail.smtp.password = $MAIL_SMTP_PASSWORD" >>configuration/mail.properties
-        fi
+        . generateMailProperties.sh
         echo ""
         echo "INSTALL_FROM_RELEASE: $INSTALL_FROM_RELEASE"
         echo "DEPLOY_TO: $DEPLOY_TO"
