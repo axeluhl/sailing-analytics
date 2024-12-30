c55c4774318bbe4f6194ca51740b86fbdf363dfd
Axel Uhl <axel.uhl@sap.com>
2024-12-30 17:00:03 +0100

    bug6059: removed the Igtimi workaround for incorrect latitude reporting and corresponding test case

--- java/com.sap.sailing.domain.igtimiadapter/src/com/sap/sailing/domain/igtimiadapter/datatypes/GpsLatLong.java
+++ java/com.sap.sailing.domain.igtimiadapter/src/com/sap/sailing/domain/igtimiadapter/datatypes/GpsLatLong.java
@@ -9,7 +9,6 @@ import com.sap.sailing.domain.igtimiadapter.Sensor;
 import com.sap.sse.common.TimePoint;
 
 public class GpsLatLong extends Fix {
-    public static final String IGTIMI_ENABLE_WORKAROUND_FOR_SINGLE_DIGIT_LATITUDES = "igtimi.enableWorkaroundForSingleDigitLatitudes";
     private static final long serialVersionUID = 5056284867725893553L;
     private final Position position;
     
@@ -24,21 +23,7 @@ public class GpsLatLong extends Fix {
 
     private static Position getPosition(Map<Integer, Object> valuesPerSubindex) {
         final double longitudeInDegrees = ((Number) valuesPerSubindex.get(1)).doubleValue();
-        final double preliminaryLatitudeInDegrees = ((Number) valuesPerSubindex.get(2)).doubleValue();
-        final double latitudeInDegrees;
-        if (Boolean.valueOf(System.getProperty(IGTIMI_ENABLE_WORKAROUND_FOR_SINGLE_DIGIT_LATITUDES, "false"))) {
-            final int degrees = (int) preliminaryLatitudeInDegrees;
-            final double minutes = (preliminaryLatitudeInDegrees - degrees)*60.0;
-            if (Math.abs(minutes) >= 10) {
-                // the minutes have two digits; cannot have been a mis-parse
-                latitudeInDegrees = preliminaryLatitudeInDegrees;
-            } else {
-                final double correctedMinutes = minutes + 10.0*(degrees%10);
-                latitudeInDegrees = ((int) (degrees/10)) + correctedMinutes/60.0;
-            }
-        } else {
-            latitudeInDegrees = preliminaryLatitudeInDegrees;
-        }
+        final double latitudeInDegrees = ((Number) valuesPerSubindex.get(2)).doubleValue();
         return new DegreePosition(latitudeInDegrees, longitudeInDegrees);
     }
     
