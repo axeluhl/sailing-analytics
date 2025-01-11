ae52f3b0ff1b7c8bfb31c88f88fc69ddf73797b8
Axel Uhl <axel.uhl@sap.com>
2025-01-11 15:44:18 +0100

    improved env var javadocs

--- java/com.sap.sailing.landscape/src/com/sap/sailing/landscape/procedures/SailingProcessConfigurationVariables.java
+++ java/com.sap.sailing.landscape/src/com/sap/sailing/landscape/procedures/SailingProcessConfigurationVariables.java
@@ -10,24 +10,24 @@ public enum SailingProcessConfigurationVariables implements ProcessConfiguration
     EXPEDITION_PORT,
     
     /**
-     * The user data variable that sets the TCP port to listen on for Igtimi Riot (WindBot) connections;
-     * a typical port would be 6000 which is the default for the WindBot devices. If not specified, the
-     * Riot server will listen on any available, unused server port. Maps to the igtimi.riot.port property.
+     * The user data variable that sets the TCP port to listen on for Igtimi Riot (WindBot) connections; a typical port
+     * would be 6000 which is the default for the WindBot devices. If not specified, the Riot server will listen on any
+     * available, unused server port. Maps to the {@code igtimi.riot.port} property.
      */
     IGTIMI_RIOT_PORT,
     
     /**
-     * Use this variable to override the default {@code https://wind.sapsailing.com} base URL for obtaining
-     * wind data from Igtimi devices. Authentication to this service by default will assume shared security
-     * and therefore shared user bases with shared access tokens. If this does not apply to your set-up,
-     * consider using {@link #IGTIMI_BEARER_TOKEN} in addition.
+     * Use this variable to override the default {@code https://wind.sapsailing.com} base URL for obtaining wind data
+     * from Igtimi devices. Authentication to this service by default will assume shared security and therefore shared
+     * user bases with shared access tokens. If this does not apply to your set-up, consider using
+     * {@link #IGTIMI_BEARER_TOKEN} in addition. Maps to the {@code igtimi.base.url} property.
      */
     IGTIMI_BASE_URL,
     
     /**
-     * Overrides the default authentication scheme for requests against the remote "Riot" service for Igtimi
-     * wind connectivity whose base URL may be overridden using {@link #IGTIMI_BASE_URL}. Specify a bearer token
-     * valid in the context of the security service of the remote Riot service.
+     * Overrides the default authentication scheme for requests against the remote "Riot" service for Igtimi wind
+     * connectivity whose base URL may be overridden using {@link #IGTIMI_BASE_URL}. Specify a bearer token valid in the
+     * context of the security service of the remote Riot service. Maps to the {@code igtimi.bearer.token} property.
      */
     IGTIMI_BEARER_TOKEN,
     
