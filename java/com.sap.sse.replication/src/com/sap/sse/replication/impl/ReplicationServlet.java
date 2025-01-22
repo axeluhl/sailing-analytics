7256e8bec749671a3764f4dad8f85de00fe768bf
Axel Uhl <axel.uhl@sap.com>
2025-01-22 17:36:50 +0100

    see https://github.com/SAP/sailing-analytics/security/code-scanning/120

--- java/com.sap.sse.replication/src/com/sap/sse/replication/impl/ReplicationServlet.java
+++ java/com.sap.sse.replication/src/com/sap/sse/replication/impl/ReplicationServlet.java
@@ -169,7 +169,7 @@ public class ReplicationServlet extends AbstractHttpServlet {
                     logger.info("Error obtaining replication status: " + e.getMessage());
                     logger.log(Level.SEVERE, "doGet", e);
                     resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                    e.printStackTrace(resp.getWriter());
+                    resp.getWriter().append(e.getMessage());
                 }
                 break;
             case STOP_REPLICATING:
