c3b9d4d5189f7cd8d1f96e7a65727dda116db920
Axel Uhl <axel.uhl@sap.com>
2025-02-05 08:25:09 +0100

    temporarily @Ignore NOAA import tests; they have a planned downtime until 2025-02-07T01:00:00Z

--- java/com.sap.sailing.declination.test/src/com/sap/sailing/declination/test/NOAADeclinationImportTest.java
+++ java/com.sap.sailing.declination.test/src/com/sap/sailing/declination/test/NOAADeclinationImportTest.java
@@ -1,9 +1,11 @@
 package com.sap.sailing.declination.test;
 
 import org.junit.Before;
+import org.junit.Ignore;
 
 import com.sap.sailing.declination.impl.NOAAImporterForTesting;
 
+@Ignore("Due to scheduled maintenance, many NCEI systems will be unavailable February 4th, 12:00 PM ET - February 6th, 8:00 PM ET. (2025)")
 public class NOAADeclinationImportTest extends DeclinationImportTest<NOAAImporterForTesting> {
     @Before
     public void setUp() {
--- java/com.sap.sailing.declination.test/src/com/sap/sailing/declination/test/NOAASimpleDeclinationTest.java
+++ java/com.sap.sailing.declination.test/src/com/sap/sailing/declination/test/NOAASimpleDeclinationTest.java
@@ -1,9 +1,11 @@
 package com.sap.sailing.declination.test;
 
 import org.junit.Before;
+import org.junit.Ignore;
 
 import com.sap.sailing.declination.impl.NOAAImporter;
 
+@Ignore("Due to scheduled maintenance, many NCEI systems will be unavailable February 4th, 12:00 PM ET - February 6th, 8:00 PM ET. (2025)")
 public class NOAASimpleDeclinationTest extends SimpleDeclinationTest<NOAAImporter> {
     @Before
     public void setUp() {
