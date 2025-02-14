5f91b02688e913aa3348118eecba71120269ddc2
Axel Uhl <axel.uhl@sap.com>
2025-02-14 15:41:28 +0100

    highcharts-12: improved install-highcharts.sh script to allow specifying version tag

--- configuration/install-highcharts.sh
+++ configuration/install-highcharts.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Usage: install-highcharts.sh [ {tag-name } ]
+# If no {tag-name} parameter is specified, the latest tip of the default branch
+# from the git@github.com:highcharts/highcharts-dist.git repository will be used.
+# Example: install-highcharts.sh v12.1.2
 if [ -z "${TMP}" ]; then
     TMP=/tmp
 fi
@@ -7,7 +11,10 @@ TARGET="$( realpath $( dirname ${0} ))/../java/com.sap.sse.gwt/src/com/sap/sse/g
 echo "Will copy git files to ${TARGET}"
 echo "Cloning highcharts-dist to $HIGHCHARTS ..."
 cd "${HIGHCHARTS}"
-git clone git@github.com:highcharts/highcharts-dist.git
+if [ -n "${1}" ]; then
+    ADDITIONAL_CLONE_ARGS="--branch ${1}"
+fi
+git clone git@github.com:highcharts/highcharts-dist.git --depth 1 ${ADDITIONAL_CLONE_ARGS}
 cd "${TARGET}"
 echo "Now in directory $(pwd)"
 for i in $( find . -type f ); do
