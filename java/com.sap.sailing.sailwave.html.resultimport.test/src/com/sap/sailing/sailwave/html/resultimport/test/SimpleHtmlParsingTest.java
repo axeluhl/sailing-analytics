6888709c30479bf077f7e3a570cd6e5141410b48
Axel Uhl <axel.uhl@sap.com>
2025-02-20 17:48:59 +0100

    bug6096: four test cases, all green

--- java/com.sap.sailing.sailwave.html.resultimport.test/src/com/sap/sailing/sailwave/html/resultimport/test/SimpleHtmlParsingTest.java
+++ java/com.sap.sailing.sailwave.html.resultimport.test/src/com/sap/sailing/sailwave/html/resultimport/test/SimpleHtmlParsingTest.java
@@ -4,7 +4,6 @@ package com.sap.sailing.sailwave.html.resultimport.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
@@ -38,109 +37,88 @@ public class SimpleHtmlParsingTest {
         assertFalse(result.isEmpty());
         assertEquals(17, result.size());
         assertTrue(regattaResults.getMetadata().get("seriestitle").contains("Results are provisional as of  9:43 on February 16, 2025"));
+        int competitorsChecked = 0;
         for (CompetitorRow row : result) {
             if (Util.contains(row.getNames(), "Daniel Kin Chung Chan")) {
+                competitorsChecked++;
                 assertEquals("HKG 223750", row.getSailID());
                 assertEquals(25.0, row.getScoreAfterDiscarding(), 0.000000001);
                 assertEquals(40.0, row.getTotalPointsBeforeDiscarding(), 0.000000001);
             }
-        }
-    }
-    
-    @Test
-    public void testEmptyAndDashedScores() throws IOException {
-        final InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("ILCA7.html");
-        SailwaveHtmlParser parser = new SailwaveHtmlParser();
-        List<CompetitorRow> result = parser.getRegattaResults(resourceAsStream).getCompetitorResults();
-        assertFalse(result.isEmpty());
-        assertEquals(117, result.size());
-        for (CompetitorRow row : result) {
-            if (Util.contains(row.getNames(), "Nicholas Halliday")) {
-                assertEquals("HKG 171979", row.getSailID());
-                assertEquals(7.0, row.getScoreAfterDiscarding(), 0.000000001);
-                assertEquals(10.0, row.getTotalPointsBeforeDiscarding(), 0.000000001);
-                assertNull(Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getRank());
-                assertNull(Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getScore());
-                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).isDiscarded());
-                assertNull(Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).getRank());
-                assertEquals((double) 118.00, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).getScore(), 0.000000001);
-                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).isDiscarded());
-            } else if (Util.contains(row.getNames(), "HAGAN Douglas")) {
-                assertEquals("AUS 8817", row.getSailID());
-                assertEquals(235.00, row.getScoreAfterDiscarding(), 0.000000001);
-                assertEquals(235.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
-                assertNull(Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getRank());
-                assertEquals("DNF", Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getMaxPointsReason());
-                assertEquals((double) 117.00, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getScore(), 0.000000001);
-                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).isDiscarded());
-                assertNull(Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).getRank());
-                assertEquals("DNF", Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).getMaxPointsReason());
-                assertEquals((double) 118.00, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).getScore(), 0.000000001);
-                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).isDiscarded());
+            if (Util.contains(row.getNames(), "Haolun Xia")) {
+                competitorsChecked++;
+                assertEquals("CHN 210199", row.getSailID());
+                assertEquals(47.0, row.getScoreAfterDiscarding(), 0.000000001);
+                assertEquals(69.0, row.getTotalPointsBeforeDiscarding(), 0.000000001);
+                assertEquals((double) 11.00, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getScore(), 0.000000001);
+                assertEquals("SCP", Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getMaxPointsReason());
+                assertEquals(true, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).isDiscarded());
             }
         }
+        assertEquals(2, competitorsChecked);
     }
     
-    @Test
-    public void testGetCompetitorRows505() throws IOException {
-        SailwaveHtmlParser parser = new SailwaveHtmlParser();
-        final InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("29er.html");
-        List<CompetitorRow> result = parser.getRegattaResults(resourceAsStream).getCompetitorResults();
-        assertFalse(result.isEmpty());
-        assertEquals(75, result.size());
-        for (CompetitorRow row : result) {
-            if (Util.contains(row.getNames(), "HOLZAPFEL Alexander")) {
-                assertEquals("GER 8975", row.getSailID());
-                assertEquals(60.00, row.getScoreAfterDiscarding(), 0.000000001);
-                assertEquals(74.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
-                assertEquals((int) 11, (int) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getRank());
-                assertEquals((double) 11, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getScore(), 0.000000001);
-                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).isDiscarded());
-            } else if (Util.contains(row.getNames(), "BART Cedric")) {
-                assertEquals("SUI 8543", row.getSailID());
-                assertEquals(55.00, row.getScoreAfterDiscarding(), 0.000000001);
-                assertEquals(74.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
-                assertEquals((int) 2, (int) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 2).getRank());
-                assertEquals((double) 2, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 2).getScore(), 0.000000001);
-                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 2).isDiscarded());
-                assertEquals((int) 19, (int) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 3).getRank());
-                assertEquals((double) 19, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 3).getScore(), 0.000000001);
-                assertEquals(true, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 3).isDiscarded());
-            } else if (Util.contains(row.getNames(), "LEWNS Chris")) {
-                assertEquals("GBR 9057", row.getSailID());
-                assertEquals(64.00, row.getScoreAfterDiscarding(), 0.000000001);
-                assertEquals(140.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
-                assertNull(Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 7).getRank());
-                assertEquals("DNF", Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 7).getMaxPointsReason());
-                assertEquals((double) 76, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 7).getScore(), 0.000000001);
-                assertEquals(true, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 7).isDiscarded());
-            }
-        }
-    }
-
     @Test
     public void testGetCompetitorRows29er() throws IOException {
         SailwaveHtmlParser parser = new SailwaveHtmlParser();
         final InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("29er.html");
         List<CompetitorRow> result = parser.getRegattaResults(resourceAsStream).getCompetitorResults();
         assertFalse(result.isEmpty());
-        assertEquals(62, result.size());
+        assertEquals(19, result.size());
+        int competitorsChecked = 0;
         for (CompetitorRow row : result) {
-            if (Util.contains(row.getNames(), "HOLSTE Kim (M1993)")) {
-                assertEquals("GER 1864", row.getSailID());
-                assertEquals(58.00, row.getScoreAfterDiscarding(), 0.000000001);
-                assertEquals(169.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
-                assertEquals((int) 25, (int) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getRank());
-                assertEquals((double) 25, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getScore(), 0.000000001);
+            if (Util.contains(row.getNames(), "Ethan Kiu")) {
+                competitorsChecked++;
+                assertEquals("HKG 3354", row.getSailID());
+                assertEquals(41.00, row.getScoreAfterDiscarding(), 0.000000001);
+                assertEquals(58.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
+                assertEquals((double) 2, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getScore(), 0.000000001);
                 assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).isDiscarded());
-                assertEquals((int) 38, (int) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).getRank());
-                assertEquals((double) 38, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).getScore(), 0.000000001);
-                assertEquals(true, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).isDiscarded());
-                assertNull(Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).getRank());
-                assertEquals("DSQ", Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).getMaxPointsReason());
-                assertEquals((double) 63, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).getScore(), 0.000000001);
+            } else if (Util.contains(row.getNames(), "Kaden Chan")) {
+                competitorsChecked++;
+                assertEquals("HKG 3405", row.getSailID());
+                assertEquals(80.00, row.getScoreAfterDiscarding(), 0.000000001);
+                assertEquals(107.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
+                assertEquals((double) 8, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 2).getScore(), 0.000000001);
+                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 2).isDiscarded());
+                assertEquals((double) 15, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 4).getScore(), 0.000000001);
+                assertEquals(true, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 4).isDiscarded());
+            } else if (Util.contains(row.getNames(), "Chloe Kong")) {
+                competitorsChecked++;
+                assertEquals("HKG 2566", row.getSailID());
+                assertEquals(67.00, row.getScoreAfterDiscarding(), 0.000000001);
+                assertEquals(107.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
+                assertEquals("SCP", Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).getMaxPointsReason());
+                assertEquals((double) 20, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).getScore(), 0.000000001);
                 assertEquals(true, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).isDiscarded());
             }
         }
+        assertEquals(3, competitorsChecked);
+    }
+
+    @Test
+    public void testGetCompetitorRowsILCA4() throws IOException {
+        SailwaveHtmlParser parser = new SailwaveHtmlParser();
+        final InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("ILCA4.html");
+        List<CompetitorRow> result = parser.getRegattaResults(resourceAsStream).getCompetitorResults();
+        assertFalse(result.isEmpty());
+        assertEquals(50, result.size());
+        int competitorsChecked = 0;
+        for (CompetitorRow row : result) {
+            if (Util.contains(row.getNames(), "YONG LIANG Chuang")) {
+                competitorsChecked++;
+                assertEquals("TPE 221094", row.getSailID());
+                assertEquals(198.00, row.getScoreAfterDiscarding(), 0.000000001);
+                assertEquals(300.00, row.getTotalPointsBeforeDiscarding(), 0.000000001);
+                assertEquals((double) 17, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).getScore(), 0.000000001);
+                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 0).isDiscarded());
+                assertEquals((double) 30, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).getScore(), 0.000000001);
+                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 1).isDiscarded());
+                assertEquals("DNC", Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).getMaxPointsReason());
+                assertEquals((double) 51, (double) Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).getScore(), 0.000000001);
+                assertEquals(false, Util.get(row.getRankAndMaxPointsReasonAndPointsAndDiscarded(), 6).isDiscarded());
+            }
+        }
+        assertEquals(1, competitorsChecked);
     }
 }
