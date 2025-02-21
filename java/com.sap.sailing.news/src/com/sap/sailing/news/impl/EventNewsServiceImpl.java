2c8b9623dbade7f16476d575f5de2b444e91d855
Axel Uhl <axel.uhl@sap.com>
2025-02-21 14:43:49 +0100

    bug6095: removed more unused code from com.sap.sailing.news

--- java/com.sap.sailing.news/src/com/sap/sailing/news/impl/EventNewsServiceImpl.java
+++ java/com.sap.sailing.news/src/com/sap/sailing/news/impl/EventNewsServiceImpl.java
@@ -3,7 +3,6 @@ package com.sap.sailing.news.impl;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 import com.sap.sailing.domain.base.Event;
@@ -28,31 +27,6 @@ public class EventNewsServiceImpl implements EventNewsService {
             result.addAll(news);
         }
         Collections.sort(result);
-        if (result.size() <= 25) {
-            return result;
-        }
-        return result.subList(0, 25);
+        return result.subList(0, Math.min(LIMIT, result.size()));
     }
-
-    @Override
-    public List<EventNewsItem> getNews(Event event, Date startingFrom) {
-        List<EventNewsItem> result = new ArrayList<>();
-        for(EventNewsProvider provider: providerRegistry.getEventNewsProvider()) {
-            Collection<? extends EventNewsItem> news = provider.getNews(event, startingFrom);
-            result.addAll(news);
-        }
-        Collections.sort(result); // sorts by the NewsItem natural order, based on creation date
-        return result.subList(0, Math.min(result.size(), LIMIT));
-    }
-
-    @Override
-    public List<EventNewsItem> getNewsByCategory(Event event, String category) {
-        return getNews(event);
-    }
-
-    @Override
-    public List<EventNewsItem> getNewsByCategory(Event event, Date startingFrom, String category) {
-        return getNews(event, startingFrom);
-    }
-
 }
