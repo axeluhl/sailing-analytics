b146c6d0d3c384a592fb1bfe3ddae3cfd022daeb
Axel Uhl <axel.uhl@sap.com>
2025-02-24 17:39:11 +0100

    bug6095: added AICore and RacingEventService tracker to aiagent activator

--- java/com.sap.sailing.aiagent/src/com/sap/sailing/aiagent/Activator.java
+++ java/com.sap.sailing.aiagent/src/com/sap/sailing/aiagent/Activator.java
@@ -2,21 +2,46 @@ package com.sap.sailing.aiagent;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+import com.sap.sailing.server.interfaces.RacingEventService;
+import com.sap.sse.aicore.AICore;
+import com.sap.sse.util.ServiceTrackerFactory;
 
 public class Activator implements BundleActivator {
+    private static BundleContext context;
+    private static Activator instance;
+    
+    private ServiceTracker<RacingEventService, RacingEventService> racingEventServiceTracker;
+    private AICore aiCore;
 
-	private static BundleContext context;
+    public Activator() {
+        instance = this;
+    }
+    
+    static BundleContext getContext() {
+        return context;
+    }
+    
+    public static Activator getInstance() {
+        return instance;
+    }
 
-	static BundleContext getContext() {
-		return context;
-	}
+    public void start(BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
+        racingEventServiceTracker = ServiceTrackerFactory.createAndOpen(context, RacingEventService.class);
+        aiCore = AICore.getDefault();
+    }
+    
+    public RacingEventService getRacingEventService() {
+        return racingEventServiceTracker.getService();
+    }
+    
+    public AICore getAICore() {
+        return aiCore;
+    }
 
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-	}
-
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
-
+    public void stop(BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
 }
