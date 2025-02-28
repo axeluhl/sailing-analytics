1876992eba182cb63461d44e580e28d0c3f90831
Axel Uhl <axel.uhl@sap.com>
2025-02-28 11:15:55 +0100

    bug6095: consolidated imports

--- java/com.sap.sailing.aiagent.persistence/src/com/sap/sailing/aiagent/persistence/impl/DomainObjectFactoryImpl.java
+++ java/com.sap.sailing.aiagent.persistence/src/com/sap/sailing/aiagent/persistence/impl/DomainObjectFactoryImpl.java
@@ -1,8 +1,6 @@
 package com.sap.sailing.aiagent.persistence.impl;
 
 import java.util.UUID;
-import java.util.logging.Logger;
-
 
 import org.bson.Document;
 
--- java/com.sap.sailing.aiagent.persistence/src/com/sap/sailing/aiagent/persistence/impl/PersistenceFactoryImpl.java
+++ java/com.sap.sailing.aiagent.persistence/src/com/sap/sailing/aiagent/persistence/impl/PersistenceFactoryImpl.java
@@ -7,7 +7,6 @@ import com.sap.sse.mongodb.MongoDBConfiguration;
 import com.sap.sse.mongodb.MongoDBService;
 
 public class PersistenceFactoryImpl implements PersistenceFactory {
-
     @Override
     public DomainObjectFactory getDomainObjectFactory(MongoDBService mongoDbService) {
         return new DomainObjectFactoryImpl(mongoDbService.getDB());
@@ -27,5 +26,4 @@ public class PersistenceFactoryImpl implements PersistenceFactory {
     public MongoObjectFactory getDefaultMongoObjectFactory() {
         return getMongoObjectFactory(MongoDBConfiguration.getDefaultConfiguration().getService());
     }
-
 }
