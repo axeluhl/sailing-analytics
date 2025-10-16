dbdbd36462a181f4c6f3d8dcedc9eb1ce61ed46c
Axel Uhl <axel.uhl@sap.com>
2025-10-16 18:22:45 +0200

    bug6167: use a <span> for the pipe symbol in the mobile footer

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/footer/Footer.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/mobile/partials/footer/Footer.java
@@ -5,6 +5,7 @@ import static com.google.gwt.dom.client.Style.Display.NONE;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.AnchorElement;
 import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -40,7 +41,7 @@ public class Footer extends Composite {
     @UiField AnchorElement desktopUi;
     @UiField AnchorElement sapJobsAnchor;
     @UiField AnchorElement privacyAnchor;
-    @UiField AnchorElement pipe;
+    @UiField SpanElement pipe;
 
     private final MobilePlacesNavigator placeNavigator;
 
