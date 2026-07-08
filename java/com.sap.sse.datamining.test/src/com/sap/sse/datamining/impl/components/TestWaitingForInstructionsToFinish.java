4cac492293155f2afd9b0650c7a30ed0914595ba
Axel Uhl <axel.uhl@sap.com>
2026-07-08 18:08:43 +0200

    bug6241: improved test; added test with many result receivers and many elements to process

--- java/com.sap.sse.datamining.test/src/com/sap/sse/datamining/impl/components/TestWaitingForInstructionsToFinish.java
+++ java/com.sap.sse.datamining.test/src/com/sap/sse/datamining/impl/components/TestWaitingForInstructionsToFinish.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.CountDownLatch;
 
 import org.junit.jupiter.api.Test;
 
@@ -30,8 +30,95 @@ public class TestWaitingForInstructionsToFinish {
     @Test
     public void testSimpleParallelProcessorWithBlockingResultReceiver() throws InterruptedException, BrokenBarrierException {
         final Collection<Processor<Integer, ?>> resultReceivers = new ArrayList<>();
-        final CyclicBarrier barrier = new CyclicBarrier(2);
-        resultReceivers.add(new AbstractProcessor<Integer, Integer>(Integer.class, Integer.class) {
+        final CountDownLatch barrier = new CountDownLatch(2);
+        resultReceivers.add(createProcessorUnblockingCyclicBarrier(barrier));
+        final AbstractParallelProcessor<String, Integer> p = createAbstractParallelProcessor(resultReceivers);
+        final boolean[] waitSucceeded = new boolean[1];
+        p.processElement("Humba");
+        p.runWhenFinishedProcessing(()->new Thread(()->{
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            synchronized (waitSucceeded) {
+                waitSucceeded[0] = true;
+                waitSucceeded.notifyAll();
+            }
+        }).start());
+        assertFalse(waitSucceeded[0]); // because we haven't yet unblocked the barrier
+        barrier.countDown();
+        synchronized (waitSucceeded) {
+            while (!waitSucceeded[0]) {
+                waitSucceeded.wait();
+            }
+        }
+        assertTrue(waitSucceeded[0]);
+    }
+
+    @Test
+    public void testSimpleParallelProcessorWithManyBlockingResultReceivers() throws InterruptedException, BrokenBarrierException {
+        final int NUMBER_OF_RECEIVERS = 10000;
+        final int NUMBER_OF_ELEMENTS_TO_PROCESS = 1000;
+        final Collection<Processor<Integer, ?>> resultReceivers = new ArrayList<>();
+        final CountDownLatch barrier = new CountDownLatch(NUMBER_OF_RECEIVERS*NUMBER_OF_ELEMENTS_TO_PROCESS+1);
+        for (int i=0; i<NUMBER_OF_RECEIVERS; i++) {
+            resultReceivers.add(createProcessorUnblockingCyclicBarrier(barrier));
+        }
+        final AbstractParallelProcessor<String, Integer> p = createAbstractParallelProcessor(resultReceivers);
+        final boolean[] waitSucceeded = new boolean[1];
+        for (int i=0; i<NUMBER_OF_ELEMENTS_TO_PROCESS; i++) {
+            p.processElement("Humba");
+        }
+        p.runWhenFinishedProcessing(()->new Thread(()->{
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            synchronized (waitSucceeded) {
+                waitSucceeded[0] = true;
+                waitSucceeded.notifyAll();
+            }
+        }).start());
+        assertFalse(waitSucceeded[0]); // because we haven't yet unblocked the barrier
+        barrier.countDown();
+        synchronized (waitSucceeded) {
+            while (!waitSucceeded[0]) {
+                waitSucceeded.wait();
+            }
+        }
+        assertTrue(waitSucceeded[0]);
+    }
+
+    private AbstractParallelProcessor<String, Integer> createAbstractParallelProcessor(
+            final Collection<Processor<Integer, ?>> resultReceivers) {
+        final ProcessorInstructionHandler<?>[] handler = new ProcessorInstructionHandler<?>[1];
+        final AbstractParallelProcessor<String, Integer> result = new AbstractParallelProcessor<String, Integer>(
+                String.class, Integer.class, ThreadPoolUtil.INSTANCE.getDefaultBackgroundTaskThreadPoolExecutor(),
+                resultReceivers) {
+            @Override
+            protected ProcessorInstruction<Integer> createInstruction(String element) {
+                @SuppressWarnings("unchecked")
+                final ProcessorInstructionHandler<Integer> h = (ProcessorInstructionHandler<Integer>) handler[0];
+                return new AbstractProcessorInstruction<Integer>(h) {
+                    @Override
+                    protected Integer computeResult() throws Exception {
+                        return element.length();
+                    }
+                };
+            }
+
+            @Override
+            protected void setAdditionalData(AdditionalResultDataBuilder additionalDataBuilder) {
+            }
+        };
+        handler[0] = result;
+        return result;
+    }
+    
+    private AbstractProcessor<Integer, Integer> createProcessorUnblockingCyclicBarrier(final CountDownLatch barrier) {
+        return new AbstractProcessor<Integer, Integer>(Integer.class, Integer.class) {
             @Override
             public boolean canProcessElements() {
                 return true;
@@ -39,11 +126,7 @@ public class TestWaitingForInstructionsToFinish {
 
             @Override
             public void processElement(Integer element) {
-                try {
-                    barrier.await();
-                } catch (InterruptedException | BrokenBarrierException e) {
-                    throw new RuntimeException(e);
-                }
+                barrier.countDown();
             }
 
             @Override
@@ -72,42 +155,6 @@ public class TestWaitingForInstructionsToFinish {
             public AdditionalResultDataBuilder getAdditionalResultData(AdditionalResultDataBuilder additionalDataBuilder) {
                 return additionalDataBuilder;
             }
-            
-        });
-        final ProcessorInstructionHandler<?>[] handler = new ProcessorInstructionHandler<?>[1];
-        final AbstractParallelProcessor<String, Integer> p = new AbstractParallelProcessor<String, Integer>(String.class, Integer.class, ThreadPoolUtil.INSTANCE.getDefaultBackgroundTaskThreadPoolExecutor(), resultReceivers) {
-            @Override
-            protected ProcessorInstruction<Integer> createInstruction(String element) {
-                @SuppressWarnings("unchecked")
-                final ProcessorInstructionHandler<Integer> h = (ProcessorInstructionHandler<Integer>) handler[0];
-                return new AbstractProcessorInstruction<Integer>(h) {
-                    @Override
-                    protected Integer computeResult() throws Exception {
-                        return element.length();
-                    }
-                };
-            }
-
-            @Override
-            protected void setAdditionalData(AdditionalResultDataBuilder additionalDataBuilder) {
-            }
         };
-        handler[0] = p;
-        final boolean[] waitSucceeded = new boolean[1];
-        p.processElement("Humba");
-        p.runWhenFinishedProcessing(()->{
-            synchronized (waitSucceeded) {
-                waitSucceeded[0] = true;
-                waitSucceeded.notifyAll();
-            }
-        });
-        assertFalse(waitSucceeded[0]); // because we haven't yet unblocked the barrier
-        barrier.await();
-        synchronized (waitSucceeded) {
-            while (!waitSucceeded[0]) {
-                waitSucceeded.wait();
-            }
-        }
-        assertTrue(waitSucceeded[0]);
     }
 }
