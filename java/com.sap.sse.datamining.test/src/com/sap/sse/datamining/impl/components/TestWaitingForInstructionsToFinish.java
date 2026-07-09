1e4a084a70c8d4bacaebfdccb44895b27a139d78
Axel Uhl <axel.uhl@sap.com>
2026-07-09 11:23:16 +0200

    bug6241: failing test case proving incorrect callback execution order

--- java/com.sap.sse.datamining.test/src/com/sap/sse/datamining/impl/components/TestWaitingForInstructionsToFinish.java
+++ java/com.sap.sse.datamining.test/src/com/sap/sse/datamining/impl/components/TestWaitingForInstructionsToFinish.java
@@ -5,11 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 import org.junit.jupiter.api.Test;
-
+import com.sap.sailing.polars.mining.AbstractEnrichingProcessor;
 import com.sap.sse.datamining.components.AdditionalResultDataBuilder;
 import com.sap.sse.datamining.components.Processor;
 import com.sap.sse.datamining.components.ProcessorInstruction;
@@ -28,7 +30,7 @@ public class TestWaitingForInstructionsToFinish {
     }
     
     @Test
-    public void testEarlyCallbackWhenResultReceiversHaventStartedYet() {
+    public void testEarlyCallbackWhenResultReceiversHaventStartedYet() throws InterruptedException, BrokenBarrierException {
         // TODO implement a test with the first parallel processor taking a while in the executor to compute its result
         // (e.g., blocked on a barrier/latch), but we register the callback on it immediately after calling processElement(...).
         // The result receivers haven't yet received the result. This test verifies that the callback isn't fired before the
@@ -41,6 +43,89 @@ public class TestWaitingForInstructionsToFinish {
         // To solve this, the waiting would probably need to be reversed. We know that the entry-point processor will decrement
         // its unfinished instructions count only *after* invoking processElement(...) on all result receivers, which will then
         // already in turn increment theirs.
+        //
+        // Set-up:
+        final Collection<Processor<Integer, ?>> resultReceivers = new ArrayList<>();
+        final CyclicBarrier barrier = new CyclicBarrier(2);
+        final CyclicBarrier afterForwardingToResultReceivers = new CyclicBarrier(2);
+        final CyclicBarrier afterFinishingEntryPoint = new CyclicBarrier(2);
+        final CyclicBarrier resultReceiverBarrier = new CyclicBarrier(2);
+        // construct some parallel processor as result receiver, so it processes results it received in executor tasks:
+        resultReceivers.add(new AbstractEnrichingProcessor<Integer, Integer>(Integer.class, Integer.class,
+                ThreadPoolUtil.INSTANCE.getDefaultBackgroundTaskThreadPoolExecutor(), Collections.emptySet()) {
+            @Override
+            protected Integer enrich(Integer element) {
+                try {
+                    resultReceiverBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+                return element * 2;
+            }
+        });
+        final ProcessorInstructionHandler<?>[] handler = new ProcessorInstructionHandler<?>[1];
+        final AbstractParallelProcessor<String, Integer> p = new AbstractParallelProcessor<String, Integer>(
+                String.class, Integer.class, ThreadPoolUtil.INSTANCE.getDefaultBackgroundTaskThreadPoolExecutor(),
+                resultReceivers) {
+            @Override
+            protected ProcessorInstruction<Integer> createInstruction(String element) {
+                @SuppressWarnings("unchecked")
+                final ProcessorInstructionHandler<Integer> h = (ProcessorInstructionHandler<Integer>) handler[0];
+                return new AbstractProcessorInstruction<Integer>(h) {
+                    @Override
+                    protected Integer computeResult() throws Exception {
+                        barrier.await(); // hold back computation in entry-point processor until released by test
+                        return element.length();
+                    }
+                };
+            }
+
+            @Override
+            public void instructionSucceeded(Integer result) {
+                super.instructionSucceeded(result);
+                try {
+                    afterForwardingToResultReceivers.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public synchronized void afterInstructionFinished(ProcessorInstruction<Integer> instruction) {
+                super.afterInstructionFinished(instruction);
+                try {
+                    afterFinishingEntryPoint.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            protected void setAdditionalData(AdditionalResultDataBuilder additionalDataBuilder) {
+            }
+        };
+        handler[0] = p;
+        // running the test now:
+        final boolean[] waitSucceeded = new boolean[1];
+        p.processElement("Humba"); // schedules a background executor task for the instruction that blocks on the barrier
+        // now register the callback while the result receiver hasn't received anything yet:
+        p.runWhenFinishedProcessing(()->{
+            synchronized (waitSucceeded) {
+                waitSucceeded[0] = true;
+                waitSucceeded.notifyAll();
+            }
+        });
+        barrier.await(); // this releases the entry-point task's computeResult
+        afterForwardingToResultReceivers.await(); // after this we know the result receivers have received the result
+        afterFinishingEntryPoint.await(); // after this we know the entry-point callback has fired
+        assertFalse(waitSucceeded[0]); // because we haven't yet unblocked the barrier for the result receiver
+        resultReceiverBarrier.await(); // now the result receiver can continue
+        synchronized (waitSucceeded) {
+            while (!waitSucceeded[0]) {
+                waitSucceeded.wait();
+            }
+        }
+        assertTrue(waitSucceeded[0]);
     }
     
     @Test
