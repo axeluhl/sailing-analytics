0ec690baa096b03164c70eb097817b00dbdecf39
Axel Uhl <axel.uhl@sap.com>
2026-08-13 19:50:42 +0200

    re-enabled NOAA tests

--- java/com.sap.sailing.declination.test/src/com/sap/sailing/declination/test/NOAASimpleDeclinationTest.java
+++ java/com.sap.sailing.declination.test/src/com/sap/sailing/declination/test/NOAASimpleDeclinationTest.java
@@ -1,11 +1,9 @@
 package com.sap.sailing.declination.test;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 
 import com.sap.sailing.declination.impl.NOAAImporter;
 
-@Disabled("NOAA seems down as of 2026-08-12")
 public class NOAASimpleDeclinationTest extends SimpleDeclinationTest<NOAAImporter> {
     @BeforeEach
     public void setUp() {
